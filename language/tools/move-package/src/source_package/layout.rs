b7e5942642bac4fc5c0b3d9fafceec64c6eea17d
Tim Zakian <tzakian@calibra.com>
2021-06-29 15:30:58 -0700

    [move-package] Source manifest parsing and definition
    Closes: #8663

---
+++ language/tools/move-package/src/source_package/layout.rs
@@ -0,0 +1,46 @@
+// Copyright (c) The Diem Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use std::path::Path;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum SourcePackageLayout {
+    Sources,
+    Specifications,
+    Tests,
+    Scripts,
+    Examples,
+    Manifest,
+}
+
+impl SourcePackageLayout {
+    /// A Move source package is laid out on-disk as
+    /// a_move_package
+    /// ├── Move.toml      (required)
+    /// ├── sources        (required)
+    /// ├── examples       (optional)
+    /// ├── scripts        (optional)
+    /// ├── specifications (optional)
+    /// └── tests          (optional)
+    pub fn path(&self) -> &Path {
+        &Path::new(self.location_str())
+    }
+
+    pub fn location_str(&self) -> &'static str {
+        match self {
+            Self::Sources => "sources",
+            Self::Manifest => "Move.toml",
+            Self::Tests => "tests",
+            Self::Scripts => "scripts",
+            Self::Examples => "examples",
+            Self::Specifications => "specifications",
+        }
+    }
+
+    pub fn is_optional(&self) -> bool {
+        match self {
+            Self::Sources | Self::Manifest => false,
+            Self::Tests | Self::Scripts | Self::Examples | Self::Specifications => true,
+        }
+    }
+}
