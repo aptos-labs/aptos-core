e88d51e1560b94a5939e32b4adc75ae90923f358
Brandon Williams <bmwill@fb.com>
2021-07-16 11:05:26 -0700

    forge: introduce the concept of a node Version

--- testsuite/forge/src/interface/mod.rs
+++ testsuite/forge/src/interface/mod.rs
@@ -17,3 +17,26 @@ mod node;
 pub use node::*;
 mod chain_info;
 pub use chain_info::*;
+
+/// A wrapper around a usize in order to represent an opaque version of a Node.
+///
+/// It is intended that backends will be able to take this opaque version identifier and lookup the
+/// appropriate version information internally to be able to determine the version of node software
+/// to use.
+///
+/// It's expected that `Version`s returned by querying a `Factory` or a `Swarm` will be sort-able
+/// such that they'll be ordered with older versions first, e.g. older -> newer.
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Version(usize, String);
+
+impl Version {
+    pub fn new(version: usize, display_string: String) -> Self {
+        Self(version, display_string)
+    }
+}
+
+impl std::fmt::Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.1)
+    }
+}
