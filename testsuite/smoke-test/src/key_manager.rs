856e4f8f0dec1221c5e7f3be35a09b023dcd5a62
Brandon Williams <bmwill@fb.com>
2021-08-24 11:38:06 -0700

    smoke-test: convert key manager tests to use LocalSwarm

--- testsuite/smoke-test/src/key_manager.rs
+++ testsuite/smoke-test/src/key_manager.rs
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    smoke_test_environment::SmokeTestEnvironment,
-    test_utils::diem_swarm_utils::load_backend_storage,
+    smoke_test_environment::new_local_swarm,
+    test_utils::diem_swarm_utils::load_validators_backend_storage,
 };
 use diem_config::config::NodeConfig;
 use diem_global_constants::CONSENSUS_KEY;
@@ -12,30 +12,27 @@ use diem_key_manager::{
     KeyManager,
 };
 use diem_secure_storage::{CryptoStorage, Storage};
-use diem_smoke_test_attribute::smoke_test;
 use diem_time_service::TimeService;
 use diem_types::chain_id::ChainId;
+use forge::Node;
 use std::{convert::TryInto, thread, thread::sleep, time::Duration};
 
-#[smoke_test]
+#[test]
 fn test_key_manager_consensus_rotation() {
     // Create and launch a local validator swarm
-    let mut env = SmokeTestEnvironment::new(1);
-    env.validator_swarm.launch();
+    let swarm = new_local_swarm(1);
+    let validator = swarm.validators().next().unwrap();
 
     // Fetch the first node config in the swarm
-    let node_config_path = env.validator_swarm.config.config_files.get(0).unwrap();
+    let node_config_path = validator.config_path();
     let node_config = NodeConfig::load(&node_config_path).unwrap();
 
     // Load validator's on disk storage
-    let secure_backend = load_backend_storage(&env.validator_swarm, 0);
+    let secure_backend = load_validators_backend_storage(validator);
     let storage: Storage = (&secure_backend).try_into().unwrap();
 
     // Create a json-rpc connection to the blockchain and verify storage matches the on-chain state.
-    let json_rpc_endpoint = format!(
-        "http://127.0.0.1:{}",
-        env.validator_swarm.get_client_port(0)
-    );
+    let json_rpc_endpoint = validator.json_rpc_endpoint().to_string();
     let diem_interface = JsonRpcDiemInterface::new(json_rpc_endpoint.clone());
     let account = node_config.validator_network.unwrap().peer_id();
     let current_consensus = storage.get_public_key(CONSENSUS_KEY).unwrap().public_key;
