ed3f607c54809e555cc452827781dd57e897e07c
Brian Gesiak <bgesiak@fb.com>
2021-08-24 23:04:54 -0400

    [move] Remove unused diagnostics
    The initial 2019 implementation of the disassembler included dead code to
    report diagnostics via the codespan crate, presumably to be used at a
    later time. It's not used, so remove it.
    
    Closes: #9023

--- language/compiler/bytecode-source-map/src/utils.rs
+++ language/compiler/bytecode-source-map/src/utils.rs
@@ -1,21 +1,10 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{mapping::SourceMapping, source_map::SourceMap};
+use crate::source_map::SourceMap;
 use anyhow::{format_err, Result};
-use codespan_reporting::{
-    diagnostic::{Diagnostic, Label},
-    files::SimpleFiles,
-    term::{
-        emit,
-        termcolor::{ColorChoice, StandardStream},
-        Config,
-    },
-};
 use move_ir_types::location::Loc;
 use std::{fs::File, io::Read, path::Path};
-
-type FileId = usize;
 
 pub type Error = (Loc, String);
 pub type Errors = Vec<Error>;
@@ -28,25 +17,4 @@ pub fn source_map_from_file(file_path: &Path) -> Result<SourceMap> {
         .ok_or_else(|| format_err!("Error while reading in source map information"))?;
     bcs::from_bytes::<SourceMap>(&bytes)
         .map_err(|_| format_err!("Error deserializing into source map"))
-}
-
-pub fn render_errors(source_mapper: &SourceMapping, errors: Errors) -> Result<()> {
-    if let Some((source_file_name, source_string)) = &source_mapper.source_code {
-        let mut codemap = SimpleFiles::new();
-        let id = codemap.add(source_file_name, source_string.to_string());
-        for err in errors {
-            let diagnostic = create_diagnostic(id, err);
-            let writer = &mut StandardStream::stderr(ColorChoice::Auto);
-            emit(writer, &Config::default(), &codemap, &diagnostic).unwrap();
-        }
-        Ok(())
-    } else {
-        Err(format_err!(
-            "Unable to render errors since source file information is not available"
-        ))
-    }
-}
-
-pub fn create_diagnostic(id: FileId, (loc, msg): Error) -> Diagnostic<FileId> {
-    Diagnostic::error().with_labels(vec![Label::primary(id, loc.usize_range()).with_message(msg)])
 }
