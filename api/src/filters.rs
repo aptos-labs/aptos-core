07dd2a65c29ad968ba0debb2de952e3c939f7ba3
Xiao Li <ilx@fb.com>
2021-09-15 10:14:50 -0700

    fix lint

--- api/src/filters.rs
+++ api/src/filters.rs
@@ -9,7 +9,7 @@ use warp::{Filter, Rejection, Reply};
 pub fn routes(
     context: Arc<Context>,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    index(context.clone())
+    index(context)
 }
 
 // GET /
