19cf06715e03be9dbd1f0b4650cebcb0e223e80a
sherry-x <sherryxiao@fb.com>
2021-10-07 16:17:01 -0700

    [json-rpc] increase the default content limit size
    Closes: #9367

--- config/src/config/json_rpc_config.rs
+++ config/src/config/json_rpc_config.rs
@@ -22,7 +22,7 @@ pub const DEFAULT_JSON_RPC_ADDRESS: &str = "127.0.0.1";
 pub const DEFAULT_JSON_RPC_PORT: u16 = 8080;
 pub const DEFAULT_BATCH_SIZE_LIMIT: u16 = 20;
 pub const DEFAULT_PAGE_SIZE_LIMIT: u16 = 1000;
-pub const DEFAULT_CONTENT_LENGTH_LIMIT: usize = 32 * 1024; // 32kb
+pub const DEFAULT_CONTENT_LENGTH_LIMIT: usize = 128 * 1024; // 128kb
 
 impl Default for JsonRpcConfig {
     fn default() -> JsonRpcConfig {
