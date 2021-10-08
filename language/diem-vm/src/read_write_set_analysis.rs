7cc0d9602be489187e33fa6791cdca4b09d101d2
Runtian Zhou <runtianz@fb.com>
2021-10-08 15:37:33 -0700

    [read-write-set] Implement read write set analysis for scripts and block metadaa transaction
    Closes: #9430

--- language/diem-vm/src/read_write_set_analysis.rs
+++ language/diem-vm/src/read_write_set_analysis.rs
@@ -3,6 +3,7 @@
 
 use crate::{
     adapter_common::PreprocessedTransaction,
+    script_to_script_function::remapping,
     system_module_names::{
         BLOCK_PROLOGUE, DIEM_BLOCK_MODULE, SCRIPT_PROLOGUE_NAME, USER_EPILOGUE_NAME,
     },
@@ -15,14 +16,15 @@ use diem_types::{
 use move_core_types::{
     ident_str,
     identifier::{IdentStr, Identifier},
-    language_storage::{ModuleId, ResourceKey, StructTag},
+    language_storage::{ModuleId, ResourceKey, StructTag, TypeTag},
     resolver::MoveResolver,
+    transaction_argument::convert_txn_args,
     value::{serialize_values, MoveValue},
 };
 use read_write_set_dynamic::NormalizedReadWriteSetAnalysis;
 use std::ops::Deref;
 
-pub struct ReadWriteSetAnalysis(NormalizedReadWriteSetAnalysis);
+pub struct ReadWriteSetAnalysis<'a>(&'a NormalizedReadWriteSetAnalysis);
 
 const TRANSACTION_FEES_NAME: &IdentStr = ident_str!("TransactionFee");
 
@@ -40,10 +42,10 @@ pub fn add_on_functions_list() -> Vec<(ModuleId, Identifier)> {
     ]
 }
 
-impl ReadWriteSetAnalysis {
+impl<'a> ReadWriteSetAnalysis<'a> {
     /// Create a Diem transaction read/write set analysis from a generic Move module read/write set
     /// analysis
-    pub fn new(rw: NormalizedReadWriteSetAnalysis) -> Self {
+    pub fn new(rw: &'a NormalizedReadWriteSetAnalysis) -> Self {
         ReadWriteSetAnalysis(rw)
     }
 
@@ -75,108 +77,33 @@ impl ReadWriteSetAnalysis {
             .0)
     }
 
-    fn get_concretized_keys_user_transaction(
+    pub fn get_concretized_keys_user_transaction(
         &self,
         tx: &SignedTransaction,
         blockchain_view: &impl MoveResolver,
     ) -> Result<(Vec<ResourceKey>, Vec<ResourceKey>)> {
         match tx.payload() {
-            TransactionPayload::ScriptFunction(s) => {
-                let signers = vec![tx.sender()];
-                let gas_currency = account_config::type_tag_for_currency_code(
-                    account_config::from_currency_code_string(tx.gas_currency_code())?,
-                );
-                let prologue_accesses = self.get_concretized_summary(
-                    &account_config::constants::ACCOUNT_MODULE,
-                    SCRIPT_PROLOGUE_NAME,
-                    &signers,
-                    &serialize_values(&vec![
-                        MoveValue::U64(tx.sequence_number()),
-                        MoveValue::vector_u8(tx.authenticator().sender().public_key_bytes()),
-                        MoveValue::U64(tx.gas_unit_price()),
-                        MoveValue::U64(tx.max_gas_amount()),
-                        MoveValue::U64(tx.expiration_timestamp_secs()),
-                        MoveValue::U8(tx.chain_id().id()),
-                        MoveValue::vector_u8(vec![]), // script_hash; it's ignored
-                    ]),
-                    &[gas_currency.clone()],
-                    blockchain_view,
-                )?;
-                let epilogue_accesses = self.get_concretized_summary(
-                    &account_config::constants::ACCOUNT_MODULE,
-                    USER_EPILOGUE_NAME,
-                    &signers,
-                    &serialize_values(&vec![
-                        MoveValue::U64(tx.sequence_number()),
-                        MoveValue::U64(tx.gas_unit_price()),
-                        MoveValue::U64(tx.max_gas_amount()),
-                        MoveValue::U64(0), // gas_units_remaining
-                    ]),
-                    &[gas_currency.clone()],
-                    blockchain_view,
-                )?;
-                let script_accesses = self.get_concretized_summary(
-                    s.module(),
-                    s.function(),
-                    &signers,
-                    s.args(),
-                    s.ty_args(),
-                    blockchain_view,
-                )?;
-
-                let mut keys_read = vec![];
-                let mut keys_written = vec![];
-
-                keys_read.extend(
-                    prologue_accesses
-                        .get_keys_read()
-                        .ok_or_else(|| anyhow!("Failed to get read set for prologue"))?,
-                );
-                keys_read.extend(
-                    epilogue_accesses
-                        .get_keys_read()
-                        .ok_or_else(|| anyhow!("Failed to get read set for epilogue"))?,
-                );
-                keys_read.extend(
-                    script_accesses
-                        .get_keys_read()
-                        .ok_or_else(|| anyhow!("Failed to get read set for script body"))?,
-                );
-
-                keys_written.extend(
-                    prologue_accesses
-                        .get_keys_written()
-                        .ok_or_else(|| anyhow!("Failed to get read set for prologue"))?,
-                );
-                keys_written.extend(
-                    epilogue_accesses
-                        .get_keys_written()
-                        .ok_or_else(|| anyhow!("Failed to get read set for epilogue"))?,
-                );
-                keys_written.extend(
-                    script_accesses
-                        .get_keys_written()
-                        .ok_or_else(|| anyhow!("Failed to get read set for script body"))?,
-                );
-
-                keys_read.sort();
-                keys_read.dedup();
-
-                keys_written.sort();
-                keys_written.dedup();
-                // Hack: remove GasFees accesses from epilogue if gas_price is zero. This is sound
-                // to do as of Diem 1.3, but should be re-evaluated if the epilogue changes
-                if tx.gas_unit_price() == 0 {
-                    let tx_fees_tag = StructTag {
-                        address: account_config::CORE_CODE_ADDRESS,
-                        module: TRANSACTION_FEES_NAME.to_owned(),
-                        name: TRANSACTION_FEES_NAME.to_owned(),
-                        type_params: vec![gas_currency],
-                    };
-                    keys_written.retain(|r| r.type_() != &tx_fees_tag);
+            TransactionPayload::ScriptFunction(s) => self.get_concretized_keys_script_function(
+                tx,
+                s.module(),
+                s.function(),
+                s.args(),
+                s.ty_args(),
+                blockchain_view,
+            ),
+            TransactionPayload::Script(s) => {
+                if let Some((module, func_name)) = remapping(s.code()) {
+                    self.get_concretized_keys_script_function(
+                        tx,
+                        module,
+                        func_name,
+                        convert_txn_args(s.args()).as_slice(),
+                        s.ty_args(),
+                        blockchain_view,
+                    )
+                } else {
+                    bail!("Unsupported transaction script type {:?}", s)
                 }
-
-                Ok((keys_read, keys_written))
             }
             payload => {
                 // TODO: support tx scripts here. Slightly tricky since we will need to run
@@ -228,12 +155,118 @@ impl ReadWriteSetAnalysis {
             }
         }
     }
+
+    fn get_concretized_keys_script_function(
+        &self,
+        tx: &SignedTransaction,
+        module_name: &ModuleId,
+        script_name: &IdentStr,
+        actuals: &[Vec<u8>],
+        type_actuals: &[TypeTag],
+        blockchain_view: &impl MoveResolver,
+    ) -> Result<(Vec<ResourceKey>, Vec<ResourceKey>)> {
+        let signers = vec![tx.sender()];
+        let gas_currency = account_config::type_tag_for_currency_code(
+            account_config::from_currency_code_string(tx.gas_currency_code())?,
+        );
+        let prologue_accesses = self.get_concretized_summary(
+            &account_config::constants::ACCOUNT_MODULE,
+            SCRIPT_PROLOGUE_NAME,
+            &signers,
+            &serialize_values(&vec![
+                MoveValue::U64(tx.sequence_number()),
+                MoveValue::vector_u8(tx.authenticator().sender().public_key_bytes()),
+                MoveValue::U64(tx.gas_unit_price()),
+                MoveValue::U64(tx.max_gas_amount()),
+                MoveValue::U64(tx.expiration_timestamp_secs()),
+                MoveValue::U8(tx.chain_id().id()),
+                MoveValue::vector_u8(vec![]), // script_hash; it's ignored
+            ]),
+            &[gas_currency.clone()],
+            blockchain_view,
+        )?;
+        let epilogue_accesses = self.get_concretized_summary(
+            &account_config::constants::ACCOUNT_MODULE,
+            USER_EPILOGUE_NAME,
+            &signers,
+            &serialize_values(&vec![
+                MoveValue::U64(tx.sequence_number()),
+                MoveValue::U64(tx.gas_unit_price()),
+                MoveValue::U64(tx.max_gas_amount()),
+                MoveValue::U64(0), // gas_units_remaining
+            ]),
+            &[gas_currency.clone()],
+            blockchain_view,
+        )?;
+        let script_accesses = self.get_concretized_summary(
+            module_name,
+            script_name,
+            &signers,
+            actuals,
+            type_actuals,
+            blockchain_view,
+        )?;
+
+        let mut keys_read = vec![];
+        let mut keys_written = vec![];
+
+        keys_read.extend(
+            prologue_accesses
+                .get_keys_read()
+                .ok_or_else(|| anyhow!("Failed to get read set for prologue"))?,
+        );
+        keys_read.extend(
+            epilogue_accesses
+                .get_keys_read()
+                .ok_or_else(|| anyhow!("Failed to get read set for epilogue"))?,
+        );
+        keys_read.extend(
+            script_accesses
+                .get_keys_read()
+                .ok_or_else(|| anyhow!("Failed to get read set for script body"))?,
+        );
+
+        keys_written.extend(
+            prologue_accesses
+                .get_keys_written()
+                .ok_or_else(|| anyhow!("Failed to get read set for prologue"))?,
+        );
+        keys_written.extend(
+            epilogue_accesses
+                .get_keys_written()
+                .ok_or_else(|| anyhow!("Failed to get read set for epilogue"))?,
+        );
+        keys_written.extend(
+            script_accesses
+                .get_keys_written()
+                .ok_or_else(|| anyhow!("Failed to get read set for script body"))?,
+        );
+
+        keys_read.sort();
+        keys_read.dedup();
+
+        keys_written.sort();
+        keys_written.dedup();
+        // Hack: remove GasFees accesses from epilogue if gas_price is zero. This is sound
+        // to do as of Diem 1.4, but should be re-evaluated if the epilogue changes
+        if tx.gas_unit_price() == 0 {
+            let tx_fees_tag = StructTag {
+                address: account_config::CORE_CODE_ADDRESS,
+                module: TRANSACTION_FEES_NAME.to_owned(),
+                name: TRANSACTION_FEES_NAME.to_owned(),
+                type_params: vec![gas_currency],
+            };
+            keys_written.retain(|r| r.type_() != &tx_fees_tag);
+        }
+
+        Ok((keys_read, keys_written))
+    }
 }
 
-impl Deref for ReadWriteSetAnalysis {
+impl<'a> Deref for ReadWriteSetAnalysis<'a> {
     type Target = NormalizedReadWriteSetAnalysis;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        self.0
     }
 }
