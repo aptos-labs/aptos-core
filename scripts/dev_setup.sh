2ac615750cd4aa61bb96bee01153e5a334015b35
Brandon Williams <bmwill@fb.com>
2021-11-01 09:59:24 -0700

    dev-setup: cargo install with '--locked'
    Closes: #9594

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -19,6 +19,7 @@ HADOLINT_VERSION=1.17.4
 SCCACHE_VERSION=0.2.16-alpha.0
 #If installing sccache from a git repp set url@revision.
 SCCACHE_GIT='https://github.com/diem/sccache.git@ef50d87a58260c30767520045e242ccdbdb965af'
+GRCOV_VERSION=0.8.2
 GUPPY_GIT='https://github.com/facebookincubator/cargo-guppy@39ec940f36b0a0df96a330243d127cbe2db9f919'
 KUBECTL_VERSION=1.18.6
 TERRAFORM_VERSION=0.12.26
@@ -402,9 +403,9 @@ function install_sccache {
     if [[ -n "${SCCACHE_GIT}" ]]; then
       git_repo=$( echo "$SCCACHE_GIT" | cut -d "@" -f 1 );
       git_hash=$( echo "$SCCACHE_GIT" | cut -d "@" -f 2 );
-      cargo install sccache --git "$git_repo" --rev "$git_hash" --features s3;
+      cargo install sccache --git "$git_repo" --rev "$git_hash" --features s3 --locked
     else
-      cargo install sccache --version="${SCCACHE_VERSION}" --features s3;
+      cargo install sccache --version="${SCCACHE_VERSION}" --features s3 --locked
     fi
   fi
 }
@@ -413,13 +414,13 @@ function install_cargo_guppy {
   if ! command -v cargo-guppy &> /dev/null; then
     git_repo=$( echo "$GUPPY_GIT" | cut -d "@" -f 1 );
     git_hash=$( echo "$GUPPY_GIT" | cut -d "@" -f 2 );
-    cargo install cargo-guppy --git "$git_repo" --rev "$git_hash";
+    cargo install cargo-guppy --git "$git_repo" --rev "$git_hash" --locked
   fi
 }
 
 function install_grcov {
   if ! command -v grcov &> /dev/null; then
-    cargo install grcov
+    cargo install grcov --version="${GRCOV_VERSION}" --locked
   fi
 }
 
