0e953a6c74c24cd07a95e37ec376237cecb84c24
Xiao Li <ilx@fb.com>
2021-11-03 15:22:33 -0700

    [api] api spec test job
    Closes: #9631

--- scripts/api_lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) The Diem Core Contributors
-# SPDX-License-Identifier: Apache-2.0
-
-set -e
-
-npx @redocly/openapi-cli lint api/doc/openapi.yaml --skip-rule no-empty-servers
