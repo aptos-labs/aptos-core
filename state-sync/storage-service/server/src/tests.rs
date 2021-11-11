5411aa3763b555433aedf6f8bbd57d92d52cdd8f
Joshua Lind <joshlind@fb.com>
2021-11-11 09:58:01 -0800

    [State Sync] Read the first stored transaction version.

--- state-sync/storage-service/server/src/tests.rs
+++ state-sync/storage-service/server/src/tests.rs
@@ -124,7 +124,7 @@ async fn test_get_storage_server_summary() {
                 highest_version,
             )),
             epoch_ending_ledger_infos: Some(CompleteDataRange::from_genesis(highest_epoch - 1)),
-            transactions: Some(CompleteDataRange::from_genesis(highest_version)),
+            transactions: Some(CompleteDataRange::new(10, highest_version).unwrap()),
             transaction_outputs: Some(CompleteDataRange::new(20, highest_version).unwrap()),
             account_states: Some(CompleteDataRange::from_genesis(highest_version)),
         },
@@ -475,6 +475,10 @@ impl DbReader<DpnProto> for MockDbReader {
 
     fn get_first_write_set_version(&self) -> Result<Option<Version>> {
         Ok(Some(20))
+    }
+
+    fn get_first_txn_version(&self) -> Result<Option<Version>> {
+        Ok(Some(10))
     }
 }
 
