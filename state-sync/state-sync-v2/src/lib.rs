2ffbd0536461d292b7687359c7dbe6defeef2dfc
Joshua Lind <joshlind@fb.com>
2021-11-11 10:27:37 -0800

    [State Sync] Remove outer state-sync-v2 files.

--- state-sync/state-sync-v2/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) The Diem Core Contributors
-// SPDX-License-Identifier: Apache-2.0
-
-//! This is an empty skeleton library for state sync v2.
-//!
-//! TODO(joshlind): update this description when we start implementing state sync v2.
-#![forbid(unsafe_code)]
