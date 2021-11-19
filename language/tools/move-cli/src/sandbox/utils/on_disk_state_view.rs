56ab033cc403b489e891424a629e76f643d4fb6b
Xiao Li <ilx@fb.com>
2021-11-19 15:53:49 -0800

    [move-cli] fix a compile error
    Closes: #9835

--- language/tools/move-cli/src/sandbox/utils/on_disk_state_view.rs
+++ language/tools/move-cli/src/sandbox/utils/on_disk_state_view.rs
@@ -287,7 +287,10 @@ impl OnDiskStateView {
     /// Returns Err if the path does not hold a resource value or the resource cannot be deserialized
     pub fn view_resource(&self, resource_path: &Path) -> Result<Option<AnnotatedMoveStruct>> {
         if resource_path.is_dir() {
-            bail!("Bad resource path {:?}. Needed file, found directory")
+            bail!(
+                "Bad resource path {:?}. Needed file, found directory",
+                resource_path
+            )
         }
         match resource_path.file_stem() {
             None => bail!(
@@ -330,7 +333,7 @@ impl OnDiskStateView {
 
     fn view_bytecode(path: &Path, is_module: bool) -> Result<Option<String>> {
         if path.is_dir() {
-            bail!("Bad bytecode path {:?}. Needed file, found directory")
+            bail!("Bad bytecode path {:?}. Needed file, found directory", path)
         }
 
         Ok(match Self::get_bytes(path)? {
