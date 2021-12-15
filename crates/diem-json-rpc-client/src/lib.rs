8e43617656a61b6fc34497cb3ca5b8cbdb64f257
Brandon Williams <bmwill@fb.com>
2021-12-15 09:06:19 -0800

    remove unused diem-json-rpc-client crate

--- crates/diem-json-rpc-client/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) The Diem Core Contributors
-// SPDX-License-Identifier: Apache-2.0
-
-pub use diem_json_rpc_types::{errors, views};
-pub use diem_types::{account_address::AccountAddress, transaction::SignedTransaction};
-
-mod broadcast_client;
-pub use broadcast_client::BroadcastingClient;
