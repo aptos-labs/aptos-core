52304723dd95afcda78c85cf4dbc69d780a92575
Joshua Lind <joshlind@fb.com>
2021-12-15 21:31:24 -0800

    [State Sync] Add smoke test for validator syncing.

--- testsuite/smoke-test/src/state_sync_v2.rs
+++ testsuite/smoke-test/src/state_sync_v2.rs
@@ -9,7 +9,10 @@ use diem_config::config::{BootstrappingMode, ContinuousSyncingMode, NodeConfig};
 use diem_rest_client::Client as RestClient;
 use diem_sdk::{transaction_builder::TransactionFactory, types::LocalAccount};
 use forge::{LocalSwarm, NodeExt, Swarm, SwarmExt};
-use std::time::{Duration, Instant};
+use std::{
+    fs,
+    time::{Duration, Instant},
+};
 use tokio::runtime::Runtime;
 
 const MAX_CATCH_UP_SECS: u64 = 60; // The max time we'll wait for nodes to catch up
@@ -30,7 +33,7 @@ fn test_full_node_bootstrap_outputs() {
         .continuous_syncing_mode = ContinuousSyncingMode::ApplyTransactionOutputs;
 
     // Test the ability of the fullnode to sync
-    test_full_node_sync(vfn_config, swarm, true)
+    test_full_node_sync(vfn_config, swarm, true);
 }
 
 #[test]
@@ -49,7 +52,7 @@ fn test_full_node_bootstrap_transactions() {
         .continuous_syncing_mode = ContinuousSyncingMode::ExecuteTransactions;
 
     // Test the ability of the fullnode to sync
-    test_full_node_sync(vfn_config, swarm, true)
+    test_full_node_sync(vfn_config, swarm, true);
 }
 
 #[test]
@@ -66,7 +69,7 @@ fn test_full_node_continuous_sync_outputs() {
         .continuous_syncing_mode = ContinuousSyncingMode::ApplyTransactionOutputs;
 
     // Test the ability of the fullnode to sync
-    test_full_node_sync(vfn_config, swarm, false)
+    test_full_node_sync(vfn_config, swarm, false);
 }
 
 #[test]
@@ -83,7 +86,7 @@ fn test_full_node_continuous_sync_transactions() {
         .continuous_syncing_mode = ContinuousSyncingMode::ExecuteTransactions;
 
     // Test the ability of the fullnode to sync
-    test_full_node_sync(vfn_config, swarm, false)
+    test_full_node_sync(vfn_config, swarm, false);
 }
 
 /// A helper method that tests that a full node can sync from a validator after
@@ -142,6 +145,109 @@ fn test_full_node_sync(full_node_config: NodeConfig, mut swarm: LocalSwarm, epoc
         &mut account_1,
         &account_0,
         epoch_changes,
+    );
+    swarm
+        .wait_for_all_nodes_to_catchup(Instant::now() + Duration::from_secs(MAX_CATCH_UP_SECS))
+        .unwrap();
+}
+
+#[test]
+fn test_validator_bootstrap_outputs() {
+    // Create a swarm of 4 validators with state sync v2 enabled (output syncing)
+    let mut swarm = new_local_swarm(4);
+    for validator in swarm.validators_mut() {
+        let mut config = validator.config().clone();
+        config.state_sync.state_sync_driver.enable_state_sync_v2 = true;
+        config.state_sync.state_sync_driver.bootstrapping_mode =
+            BootstrappingMode::ApplyTransactionOutputsFromGenesis;
+        config.state_sync.state_sync_driver.continuous_syncing_mode =
+            ContinuousSyncingMode::ApplyTransactionOutputs;
+        config.save(validator.config_path()).unwrap();
+        validator.restart().unwrap();
+    }
+
+    // Test the ability of the validators to sync
+    test_validator_sync(swarm);
+}
+
+#[test]
+fn test_validator_bootstrap_transactions() {
+    // Create a swarm of 4 validators with state sync v2 enabled (transaction syncing)
+    let mut swarm = new_local_swarm(4);
+    for validator in swarm.validators_mut() {
+        let mut config = validator.config().clone();
+        config.state_sync.state_sync_driver.enable_state_sync_v2 = true;
+        config.state_sync.state_sync_driver.bootstrapping_mode =
+            BootstrappingMode::ExecuteTransactionsFromGenesis;
+        config.state_sync.state_sync_driver.continuous_syncing_mode =
+            ContinuousSyncingMode::ExecuteTransactions;
+        config.save(validator.config_path()).unwrap();
+        validator.restart().unwrap();
+    }
+
+    // Test the ability of the validators to sync
+    test_validator_sync(swarm);
+}
+
+/// A helper method that tests that a validator can sync after a failure and
+/// continue to stay up-to-date.
+fn test_validator_sync(mut swarm: LocalSwarm) {
+    // Launch the swarm and wait for it to be ready
+    swarm.launch().unwrap();
+
+    // Execute multiple transactions through validator 0
+    let validator_peer_ids = swarm.validators().map(|v| v.peer_id()).collect::<Vec<_>>();
+    let validator_client_0 = swarm
+        .validator(validator_peer_ids[0])
+        .unwrap()
+        .rest_client();
+    let transaction_factory = swarm.chain_info().transaction_factory();
+    let mut account_0 = create_and_fund_account(&mut swarm, 1000);
+    let mut account_1 = create_and_fund_account(&mut swarm, 1000);
+    execute_transactions(
+        &mut swarm,
+        &validator_client_0,
+        &transaction_factory,
+        &mut account_0,
+        &account_1,
+        true,
+    );
+    swarm
+        .wait_for_all_nodes_to_catchup(Instant::now() + Duration::from_secs(MAX_CATCH_UP_SECS))
+        .unwrap();
+
+    // Stop validator 1 and delete the storage
+    let validator_1 = validator_peer_ids[1];
+    swarm.validator_mut(validator_1).unwrap().stop();
+    let node_config = swarm.validator_mut(validator_1).unwrap().config().clone();
+    let state_db_path = node_config.storage.dir().join("diemdb");
+    assert!(state_db_path.as_path().exists());
+    fs::remove_dir_all(state_db_path).unwrap();
+
+    // Execute more transactions
+    execute_transactions(
+        &mut swarm,
+        &validator_client_0,
+        &transaction_factory,
+        &mut account_1,
+        &account_0,
+        true,
+    );
+
+    // Restart validator 1 and wait for all nodes to catchup
+    swarm.validator_mut(validator_1).unwrap().start().unwrap();
+    swarm
+        .wait_for_all_nodes_to_catchup(Instant::now() + Duration::from_secs(MAX_CATCH_UP_SECS))
+        .unwrap();
+
+    // Execute multiple transactions and verify validator 1 can sync
+    execute_transactions(
+        &mut swarm,
+        &validator_client_0,
+        &transaction_factory,
+        &mut account_0,
+        &account_1,
+        true,
     );
     swarm
         .wait_for_all_nodes_to_catchup(Instant::now() + Duration::from_secs(MAX_CATCH_UP_SECS))
