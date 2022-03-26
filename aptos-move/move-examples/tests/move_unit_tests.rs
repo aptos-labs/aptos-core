3a40d567b70cc0709ff69383fdeb8225e4405639
David <isaac.wolinsky@gmail.com>
2022-03-26 09:17:22 -0400

    [move-examples] Cleanup examples tests
    The example used by the framework for running tests is a lot better than
    the current move_unit_tests example, so let's borrow!!

--- aptos-move/move-examples/tests/move_unit_tests.rs
+++ aptos-move/move-examples/tests/move_unit_tests.rs
@@ -1,27 +1,18 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use aptos_vm::natives::aptos_natives;
-use framework::diem_framework_named_addresses;
-use move_compiler::shared::NumericalAddress;
-use move_unit_test::UnitTestingConfig;
-
 #[test]
 fn move_unit_tests() {
-    let mut named_addresses = diem_framework_named_addresses();
-    named_addresses.insert(
-        "HelloBlockchain".to_owned(),
-        NumericalAddress::parse_str("0xe110").unwrap(),
-    );
-
-    let config =
-        UnitTestingConfig::default_with_bound(Some(100_000)).with_named_addresses(named_addresses);
-
-    move_unit_test::cargo_runner::run_tests_with_config_and_filter(
-        config,
-        "sources",
-        r".*\.move$",
-        Some(&move_stdlib::move_stdlib_modules_full_path()),
-        Some(aptos_natives()),
-    );
+    move_cli::package::cli::run_move_unit_tests(
+        &std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")),
+        move_package::BuildConfig {
+            test_mode: true,
+            install_dir: Some(tempfile::tempdir().unwrap().path().to_path_buf()),
+            ..Default::default()
+        },
+        move_unit_test::UnitTestingConfig::default_with_bound(Some(100_000)),
+        aptos_vm::natives::aptos_natives(),
+        /* compute_coverage */ false,
+    )
+    .unwrap();
 }
