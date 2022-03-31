272bcb2ae3049a0be820ab5f2bb96d262d60f93a
magnum6actual <wayne.culbreth@gmail.com>
2022-03-30 19:09:52 -0500

    Artifact build

--- aptos-move/framework/aptos-framework/releases/artifacts/current/transaction_script_builder.rs
+++ aptos-move/framework/aptos-framework/releases/artifacts/current/transaction_script_builder.rs
@@ -45,7 +45,9 @@ pub enum ScriptFunctionCall {
     },
 
     /// Create delegated token for the address so the account could claim MintCapability later.
-    DelegateMintCapability { to: AccountAddress },
+    DelegateMintCapability {
+        to: AccountAddress,
+    },
 
     /// Mint coins with capability.
     Mint {
@@ -54,7 +56,9 @@ pub enum ScriptFunctionCall {
     },
 
     /// Rotate the authentication key for the account under cap.account_address
-    RotateAuthenticationKey { new_authentication_key: Bytes },
+    RotateAuthenticationKey {
+        new_authentication_key: Bytes,
+    },
 
     SetGasConstants {
         global_memory_per_byte_cost: u64,
@@ -71,10 +75,14 @@ pub enum ScriptFunctionCall {
     },
 
     /// Updates the major version to a larger version.
-    SetVersion { major: u64 },
+    SetVersion {
+        major: u64,
+    },
 
-    /// Transfers `amount` of tokens from `from` to `to`.
-    Transfer { to: AccountAddress, amount: u64 },
+    Transfer {
+        to: AccountAddress,
+        amount: u64,
+    },
 }
 
 impl ScriptFunctionCall {
@@ -264,7 +272,6 @@ pub fn encode_set_version_script_function(major: u64) -> TransactionPayload {
     ))
 }
 
-/// Transfers `amount` of tokens from `from` to `to`.
 pub fn encode_transfer_script_function(to: AccountAddress, amount: u64) -> TransactionPayload {
     TransactionPayload::ScriptFunction(ScriptFunction::new(
         ModuleId::new(
