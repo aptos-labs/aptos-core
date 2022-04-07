2074cae3a4bcb66d6cd9867d6cfdbf68da4c1987
aldenhu <msmouse@gmail.com>
2022-04-06 23:03:11 -0700

    MoveResolverExt that depends on TableResolver
    Closes: #448

--- aptos-move/aptos-vm/src/move_vm_ext/mod.rs
+++ aptos-move/aptos-vm/src/move_vm_ext/mod.rs
@@ -3,10 +3,12 @@
 
 ///! MoveVM and Session wrapped, to make sure Aptos natives and extensions are always installed and
 ///! taken care of after session finish.
+mod resolver;
 mod session;
 mod vm;
 
 pub use crate::move_vm_ext::{
+    resolver::MoveResolverExt,
     session::{SessionExt, SessionId, SessionOutput},
     vm::MoveVmExt,
 };
---
+++ aptos-move/aptos-vm/src/move_vm_ext/resolver.rs
@@ -0,0 +1,14 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use move_core_types::resolver::MoveResolver;
+use move_table_extension::TableResolver;
+use std::fmt::Debug;
+
+pub trait MoveResolverExt: MoveResolver<Err = Self::ExtError> + TableResolver {
+    type ExtError: Debug;
+}
+
+impl<E: Debug, T: MoveResolver<Err = E> + TableResolver + ?Sized> MoveResolverExt for T {
+    type ExtError = E;
+}
