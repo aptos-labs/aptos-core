c967f2edace07bd637818bf2e912dd08b7eb1f1a
Greg Nazario <greg@gnazar.io>
2022-04-19 11:29:16 -0700

    [aptos-cli] Add ability to publish modules
    Modules can now be published to the blockchain via the CLI.
    Running modules for now has to run elsewhere
    
    Closes: #561

--- crates/aptos/src/common/types.rs
+++ crates/aptos/src/common/types.rs
@@ -9,9 +9,11 @@ use aptos_crypto::{
 use aptos_logger::{debug, info};
 use aptos_types::transaction::authenticator::AuthenticationKey;
 use clap::{ArgEnum, Parser};
+use itertools::Itertools;
 use move_core_types::account_address::AccountAddress;
 use serde::{Deserialize, Serialize};
 use std::{
+    collections::BTreeMap,
     fmt::Debug,
     path::{Path, PathBuf},
     str::FromStr,
@@ -327,6 +329,9 @@ impl SaveFile {
 
 #[derive(Debug, Parser)]
 pub struct NodeOptions {
+    /// URL to a fullnode on the network
+    ///
+    /// Defaults to https://fullnode.devnet.aptoslabs.com
     #[clap(
         long,
         parse(try_from_str),
@@ -342,6 +347,48 @@ pub struct MovePackageDir {
     #[clap(long, parse(from_os_str))]
     pub package_dir: PathBuf,
     /// Path to save the compiled move package
+    ///
+    /// Defaults to `<package_dir>/build`
     #[clap(long, parse(from_os_str))]
     pub output_dir: Option<PathBuf>,
+    /// Named addresses for the move binary
+    ///
+    /// Example: alice=0x1234, bob=0x5678
+    ///
+    /// Note: This will fail if there are duplicates in the Move.toml file remove those first.
+    #[clap(long, parse(try_from_str = parse_map), default_value = "")]
+    pub named_addresses: BTreeMap<String, AccountAddress>,
+}
+
+const PARSE_MAP_SYNTAX_MSG: &str = "Invalid syntax for map.  Example: Name=Value,Name2=Value";
+
+/// Parses an inline map of values
+///
+/// Example: Name=Value,Name2=Value
+pub fn parse_map<K: FromStr + Ord, V: FromStr>(str: &str) -> anyhow::Result<BTreeMap<K, V>>
+where
+    K::Err: 'static + std::error::Error + Send + Sync,
+    V::Err: 'static + std::error::Error + Send + Sync,
+{
+    let mut map = BTreeMap::new();
+
+    // Split pairs by commas
+    for pair in str.split_terminator(',') {
+        // Split pairs by = then trim off any spacing
+        let (first, second): (&str, &str) = pair
+            .split_terminator('=')
+            .collect_tuple()
+            .ok_or_else(|| anyhow::Error::msg(PARSE_MAP_SYNTAX_MSG))?;
+        let first = first.trim();
+        let second = second.trim();
+        if first.is_empty() || second.is_empty() {
+            return Err(anyhow::Error::msg(PARSE_MAP_SYNTAX_MSG));
+        }
+
+        // At this point, we just give error messages appropriate to parsing
+        let key: K = K::from_str(first)?;
+        let value: V = V::from_str(second)?;
+        map.insert(key, value);
+    }
+    Ok(map)
 }
--- crates/aptos/src/move_tool/mod.rs
+++ crates/aptos/src/move_tool/mod.rs
@@ -7,14 +7,26 @@
 //!
 
 use crate::{
-    common::{types::MovePackageDir, utils::to_common_result},
+    common::{
+        types::{EncodingOptions, MovePackageDir, NodeOptions, PrivateKeyInputOptions},
+        utils::to_common_result,
+    },
     CliResult, Error,
+};
+use aptos_crypto::{ed25519::Ed25519PrivateKey, PrivateKey};
+use aptos_rest_client::{Client, Transaction};
+use aptos_sdk::{transaction_builder::TransactionFactory, types::LocalAccount};
+use aptos_types::{
+    chain_id::ChainId,
+    transaction::{authenticator::AuthenticationKey, ModuleBundle, TransactionPayload},
 };
 use aptos_vm::natives::aptos_natives;
 use clap::{Parser, Subcommand};
 use move_cli::package::cli::{run_move_unit_tests, UnitTestResult};
+use move_core_types::account_address::AccountAddress;
 use move_package::{compilation::compiled_package::CompiledPackage, BuildConfig};
 use move_unit_test::UnitTestingConfig;
+use reqwest::Url;
 use std::path::Path;
 
 /// CLI tool for performing Move tasks
@@ -22,6 +34,7 @@ use std::path::Path;
 #[derive(Subcommand)]
 pub enum MoveTool {
     Compile(CompilePackage),
+    Publish(PublishPackage),
     Test(TestPackage),
 }
 
@@ -29,6 +42,7 @@ impl MoveTool {
     pub async fn execute(self) -> CliResult {
         match self {
             MoveTool::Compile(tool) => to_common_result(tool.execute().await),
+            MoveTool::Publish(tool) => to_common_result(tool.execute().await),
             MoveTool::Test(tool) => to_common_result(tool.execute().await),
         }
     }
@@ -44,12 +58,12 @@ pub struct CompilePackage {
 impl CompilePackage {
     pub async fn execute(&self) -> Result<Vec<String>, Error> {
         let build_config = BuildConfig {
+            additional_named_addresses: self.move_options.named_addresses.clone(),
             generate_docs: true,
             install_dir: self.move_options.output_dir.clone(),
             ..Default::default()
         };
         let compiled_package = compile_move(build_config, self.move_options.package_dir.as_path())?;
-        // TODO: This can be serialized once move is updated
         let mut ids = Vec::new();
         compiled_package
             .compiled_modules()
@@ -70,6 +84,7 @@ pub struct TestPackage {
 impl TestPackage {
     pub async fn execute(&self) -> Result<&'static str, Error> {
         let config = BuildConfig {
+            additional_named_addresses: self.move_options.named_addresses.clone(),
             test_mode: true,
             install_dir: self.move_options.output_dir.clone(),
             ..Default::default()
@@ -97,4 +112,94 @@ fn compile_move(build_config: BuildConfig, package_dir: &Path) -> Result<Compile
     build_config
         .compile_package(package_dir, &mut Vec::new())
         .map_err(|err| Error::MoveCompiliationError(err.to_string()))
+}
+
+/// Publishes the modules in a Move package
+#[derive(Parser)]
+pub struct PublishPackage {
+    #[clap(flatten)]
+    encoding_options: EncodingOptions,
+    #[clap(flatten)]
+    private_key_options: PrivateKeyInputOptions,
+    #[clap(flatten)]
+    move_options: MovePackageDir,
+    #[clap(flatten)]
+    node_options: NodeOptions,
+    /// ChainId for the network
+    #[clap(long)]
+    chain_id: ChainId,
+    /// Maximum gas to be used to publish the package
+    ///
+    /// Defaults to 1000 gas units
+    #[clap(long, default_value_t = 1000)]
+    max_gas: u64,
+}
+
+impl PublishPackage {
+    pub async fn execute(&self) -> Result<aptos_rest_client::Transaction, Error> {
+        let build_config = BuildConfig {
+            additional_named_addresses: self.move_options.named_addresses.clone(),
+            generate_abis: false,
+            generate_docs: true,
+            install_dir: self.move_options.output_dir.clone(),
+            ..Default::default()
+        };
+        let package = compile_move(build_config, self.move_options.package_dir.as_path())?;
+        let compiled_units: Vec<Vec<u8>> = package
+            .compiled_units
+            .iter()
+            .map(|unit_with_source| unit_with_source.unit.serialize())
+            .collect();
+        let compiled_payload = TransactionPayload::ModuleBundle(ModuleBundle::new(compiled_units));
+
+        // Now that it's compiled, lets send it
+        let sender_key = self
+            .private_key_options
+            .extract_private_key(self.encoding_options.encoding)?;
+
+        submit_transaction(
+            self.node_options.url.clone(),
+            self.chain_id,
+            sender_key,
+            compiled_payload,
+            self.max_gas,
+        )
+        .await
+    }
+}
+
+/// Submits a [`TransactionPayload`] as signed by the `sender_key`
+async fn submit_transaction(
+    url: Url,
+    chain_id: ChainId,
+    sender_key: Ed25519PrivateKey,
+    payload: TransactionPayload,
+    max_gas: u64,
+) -> Result<Transaction, Error> {
+    let client = Client::new(url);
+
+    // Get sender address
+    let sender_address = AuthenticationKey::ed25519(&sender_key.public_key()).derived_address();
+    let sender_address = AccountAddress::new(*sender_address);
+
+    // Get account to get the sequence number
+    let account_response = client
+        .get_account(sender_address)
+        .await
+        .map_err(|err| Error::UnexpectedError(err.to_string()))?;
+    let account = account_response.inner();
+    let sequence_number = account.sequence_number;
+
+    let transaction_factory = TransactionFactory::new(chain_id)
+        .with_gas_unit_price(1)
+        .with_max_gas_amount(max_gas);
+    let sender_account = &mut LocalAccount::new(sender_address, sender_key, sequence_number);
+    let transaction =
+        sender_account.sign_with_transaction_builder(transaction_factory.payload(payload));
+    let response = client
+        .submit_and_wait(&transaction)
+        .await
+        .map_err(|err| Error::UnexpectedError(err.to_string()))?;
+
+    Ok(response.inner().clone())
 }
