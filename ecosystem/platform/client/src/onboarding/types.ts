36865aa6ac204e03eeb75527f10d505f5f9bb8cd
Zach Denton <z@chdenton.com>
2022-04-20 17:45:15 -0400

    Add client-side scaffolding for onboarding flow.

---
+++ ecosystem/platform/client/src/onboarding/types.ts
@@ -0,0 +1,34 @@
+export type Persona = "operator";
+
+export type AptosAddress = string;
+
+export type TosAcceptance = {
+  date: number;
+  ip: string;
+};
+
+export type SocialAccount = {
+  service: "github" | "discord";
+  username: string;
+};
+
+export type Identity = {
+  personas: Persona[];
+  socialAccounts: SocialAccount[];
+  tosAcceptance: TosAcceptance;
+  aptosAddress: AptosAddress;
+};
+
+export function isValidIdentity(
+  identity: Partial<Identity>,
+): identity is Identity {
+  return (
+    identity.personas != null &&
+    identity.personas.length > 0 &&
+    identity.socialAccounts != null &&
+    identity.socialAccounts.length > 0 &&
+    identity.tosAcceptance?.date != null &&
+    identity.tosAcceptance?.ip != null &&
+    identity.aptosAddress != null
+  );
+}
