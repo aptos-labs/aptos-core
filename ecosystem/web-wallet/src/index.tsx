76f641b4eeb515fa57571529c001f36e2d030b4e
Kent White <kent@aptoslabs.com>
2022-04-26 14:31:53 -0700

    Enable extension

--- ecosystem/web-wallet/src/index.tsx
+++ ecosystem/web-wallet/src/index.tsx
@@ -3,19 +3,19 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
 import Wallet from './pages/Wallet'
 import Login from './pages/Login'
 import { GlobalStateProvider } from './GlobalState'
 
 ReactDOM.render(
   <GlobalStateProvider>
-    <BrowserRouter>
+    <MemoryRouter>
       <Routes>
         <Route path='/' element={<Login/>}></Route>
         <Route path='/wallet' element={<Wallet/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </MemoryRouter>
   </GlobalStateProvider>,
   document.getElementById('root')
 )
