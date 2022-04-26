38b518745f7740b588f665690e7a04844e6328af
Josh Lind <josh@aptoslabs.com>
2022-04-26 15:23:29 -0400

    [Configs] Enable state sync v2 by default.

--- config/src/config/state_sync_config.rs
+++ config/src/config/state_sync_config.rs
@@ -91,7 +91,7 @@ impl Default for StateSyncDriverConfig {
     fn default() -> Self {
         Self {
             bootstrapping_mode: BootstrappingMode::ApplyTransactionOutputsFromGenesis,
-            enable_state_sync_v2: false,
+            enable_state_sync_v2: true,
             continuous_syncing_mode: ContinuousSyncingMode::ApplyTransactionOutputs,
             progress_check_interval_ms: 100,
             max_connection_deadline_secs: 10,
@@ -122,7 +122,7 @@ impl Default for StorageServiceConfig {
             max_network_channel_size: 1000,
             max_transaction_chunk_size: 1000,
             max_transaction_output_chunk_size: 1000,
-            storage_summary_refresh_interval_ms: 1000,
+            storage_summary_refresh_interval_ms: 250,
         }
     }
 }
@@ -156,7 +156,7 @@ pub struct DataStreamingServiceConfig {
 impl Default for DataStreamingServiceConfig {
     fn default() -> Self {
         Self {
-            global_summary_refresh_interval_ms: 300,
+            global_summary_refresh_interval_ms: 250,
             max_concurrent_requests: 1,
             max_data_stream_channel_sizes: 1000,
             max_request_retry: 3,
@@ -177,7 +177,7 @@ impl Default for AptosDataClientConfig {
     fn default() -> Self {
         Self {
             response_timeout_ms: 10000,
-            summary_poll_interval_ms: 1000,
+            summary_poll_interval_ms: 250,
         }
     }
 }
