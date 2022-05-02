1518c96c60e4b437a96c2928fa997f6eab3e57ed
Rustie Lin <rustie117@gmail.com>
2022-05-02 16:21:52 -0700

    [tf][indexer] add publicly accessible flag

--- terraform/testnet/indexer.tf
+++ terraform/testnet/indexer.tf
@@ -11,10 +11,11 @@ module "indexer" {
 
   oidc_provider = module.validator.oidc_provider
 
-  subnet_ids = module.validator.aws_subnet_private.*.id
+  subnet_ids = var.indexer_db_publicly_accessible ? module.validator.aws_subnet_public.*.id : module.validator.aws_subnet_private.*.id
   vpc_id     = module.validator.vpc_id
 
-  db_password = var.indexer_db_password
+  db_password            = var.indexer_db_password
+  db_publicly_accessible = var.indexer_db_publicly_accessible
 
   indexer_helm_values = var.indexer_helm_values
 }
