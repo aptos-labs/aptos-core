fb4d3c164cd52ed6bd6b30a02ff2d6586436e4d4
cs <cs@cs.cs>
2022-05-03 13:06:14 +0200

    [tf][do] fullnode tf on digital ocean

---
+++ terraform/fullnode/digital_ocean/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
---
+++ terraform/fullnode/digital_ocean/variables.tf
@@ -0,0 +1,67 @@
+variable "helm_values" {
+  description = "Map of values to pass to Helm"
+  type        = any
+  default     = {}
+}
+
+variable "fullnode_helm_values" {
+  description = "Map of values to pass to public fullnode Helm"
+  type        = any
+  default     = {}
+}
+
+variable "do_token" {
+  type = string
+  description = "Digital Notion API token"
+}
+
+variable "region" {
+  description = "Digital Ocean region of nodes"
+  type        = string
+}
+
+variable "fullnode_helm_values_list" {
+  description = "List of values to pass to public fullnode, for setting different value per node. length(fullnode_helm_values_list) must equal var.num_fullnodes"
+  type        = any
+  default     = {}
+}
+
+variable "helm_force_update" {
+  description = "Force Terraform to update the Helm deployment"
+  default     = false
+}
+
+variable "k8s_namespace" {
+  default     = "aptos"
+  description = "Kubernetes namespace that the fullnode will be deployed into"
+}
+
+variable "k8s_api_sources" {
+  description = "List of CIDR subnets which can access the Kubernetes API endpoint"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "num_fullnodes" {
+  default     = 1
+  description = "Number of fullnodes"
+}
+
+variable "image_tag" {
+  default     = "devnet"
+  description = "Docker image tag to use for the fullnode"
+}
+
+variable "era" {
+  description = "Chain era, used to start a clean chain"
+  default     = 1
+}
+
+variable "chain_id" {
+  description = "aptos chain ID"
+  default     = "DEVNET"
+}
+
+variable "machine_type" {
+  description = "Machine type for running fullnode"
+  default     = "s-4vcpu-8gb"
+}
