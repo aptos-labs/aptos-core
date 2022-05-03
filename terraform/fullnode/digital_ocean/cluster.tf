b28a4f16088a9b2535d9ef0b9d1ec4a383355dbf
cs <cs@cs.cs>
2022-05-03 13:06:14 +0200

    [tf][do] fullnode tf on digital ocean
    Closes: #782

---
+++ terraform/fullnode/digital_ocean/cluster.tf
@@ -0,0 +1,12 @@
+resource "digitalocean_kubernetes_cluster" "aptos" {
+  name   = "aptos-${terraform.workspace}"
+  region = var.region
+  version = "1.22.8-do.1"
+
+  node_pool {
+    name       = "fullnodes"
+    size       = var.machine_type
+    node_count = var.num_fullnodes
+    tags            = ["fullnodes"]
+  }
+}
