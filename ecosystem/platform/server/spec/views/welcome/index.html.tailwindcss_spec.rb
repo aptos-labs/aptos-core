78658f522dfb7ca5554c2afcdbb4d8f9738144d0
Zach Denton <z@chdenton.com>
2022-05-06 13:30:02 -0700

    Cleanup empty specs.

--- ecosystem/platform/server/spec/views/welcome/index.html.tailwindcss_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'welcome/index.html.tailwindcss', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
