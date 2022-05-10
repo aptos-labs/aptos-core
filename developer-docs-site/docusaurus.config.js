be9a4e778bbac068f1c053ebb1c07288a64f31ee
Raj Karamchedu <raj@aptoslabs.com>
2022-05-10 10:37:37 -0700

    [Documentation] Cleaning up the config file for redirects plugin.
    Closes: #896

--- developer-docs-site/docusaurus.config.js
+++ developer-docs-site/docusaurus.config.js
@@ -131,33 +131,12 @@ const config = {
     [
       '@docusaurus/plugin-client-redirects',
       {
-        // fromExtensions: ['html', 'htm'], // /myPage.html -> /myPage
-        // toExtensions: ['exe', 'zip'], // /myAsset -> /myAsset.zip (if latter exists)
         redirects: [
-          // /docs/oldDoc -> /docs/newDoc
           {
             to: '/tutorials/full-node/run-a-fullnode',
             from: '/tutorials/run-a-fullnode',
           },
-          // Redirect from multiple old paths to the new path
-          /*
-          {
-            to: '/docs/newDoc2',
-            from: ['/docs/oldDocFrom2019', '/docs/legacyDocFrom2016'],
-          },
-          */
         ],
-        /*
-        createRedirects(existingPath) {
-          if (existingPath.includes('/community')) {
-            // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
-            return [
-              existingPath.replace('/community', '/docs/team'),
-              existingPath.replace('/community', '/docs/support'),
-            ];
-          }
-          return undefined; // Return a falsy value: no redirect created
-        }, */
       },
     ],
   ],
