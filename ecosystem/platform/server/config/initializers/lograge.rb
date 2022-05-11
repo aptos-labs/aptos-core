7cbf624d6d9d72db4f2e232c54893071303b4fe0
Zach Denton <z@chdenton.com>
2022-05-11 18:35:22 -0400

    Emit JSON logs instead of key=value logs.
    Closes: #900

--- ecosystem/platform/server/config/initializers/lograge.rb
+++ ecosystem/platform/server/config/initializers/lograge.rb
@@ -5,10 +5,11 @@
 
 Rails.application.configure do
   config.lograge.enabled = !Rails.env.development? || ENV.fetch('LOGRAGE_IN_DEVELOPMENT', nil) == 'true'
+  config.lograge.formatter = Lograge::Formatters::Json.new
 
   config.lograge.custom_options = lambda do |event|
     result = {}
-    result[:time] = Time.now.to_i
+    result[:time] = Time.now.to_f
     result[:request_id] = event.payload[:request_id]
     result[:user_id] = event.payload[:user_id] if event.payload[:user_id].present?
     result
--- ecosystem/platform/server/lib/logging/logs.rb
+++ ecosystem/platform/server/lib/logging/logs.rb
@@ -9,28 +9,28 @@ module Logging
     USER_ID_KEY = 'user_id'
 
     def log(message_or_object, message = nil)
-      result = []
-      result << "time=#{Time.now.to_i}"
-      result << "class=#{self.class}"
+      result = {}
+
+      result[:time] = Time.now.to_f
+      result[:class] = self.class
 
       request_id = Thread.current.thread_variable_get(REQUEST_ID_KEY)
-      result << "request_id=#{request_id}"
+      result[:request_id] = request_id
 
       user_id = Thread.current.thread_variable_get(USER_ID_KEY)
-      result << "user_id=#{user_id}" if user_id.present?
+      result[:user_id] = user_id if user_id.present?
 
       if message.nil?
         message = message_or_object
       else
         object = message_or_object
-        result << "object_class=#{object.class}"
-        result << "object_id=#{object.id}" if object.respond_to?(:id)
+        result[:object_class] = object.class
+        result[:object_id] = object.id if object.respond_to?(:id)
       end
 
-      message = message.gsub('"', '\"')
-      result << "message=\"#{message}\""
+      result[:message] = message
 
-      Rails.logger.info(result.join(' '))
+      Rails.logger.info(result.to_json)
     end
   end
 end
