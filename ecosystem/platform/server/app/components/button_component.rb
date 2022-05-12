bd4a5151fb29bcd6de29d6f118bc7b6a29ea0751
Zach Denton <z@chdenton.com>
2022-05-12 19:39:44 -0400

    Extract button component.

---
+++ ecosystem/platform/server/app/components/button_component.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Copyright (c) Aptos
+# SPDX-License-Identifier: Apache-2.0
+
+SCHEME_CLASSES = {
+  primary: 'bg-teal-400 hover:bg-teal-300 text-neutral-800 text-center font-mono uppercase font-bold',
+  secondary: 'border border-teal-400 hover:border-teal-300 text-center text-white font-mono uppercase'
+}.freeze
+
+SIZE_CLASSES = {
+  large: 'px-8 py-4 text-lg rounded',
+  medium: 'p-2 text-lg rounded',
+  small: 'py-1 text-sm rounded'
+}.freeze
+
+class ButtonComponent < ViewComponent::Base
+  def initialize(scheme: :primary,
+                 size: :medium,
+                 **rest)
+    @rest = rest
+
+    @tag = rest[:href] ? :a : :button
+    rest[:class] = [
+      SCHEME_CLASSES[scheme],
+      SIZE_CLASSES[size],
+      rest[:class]
+    ]
+  end
+end
--- ecosystem/platform/server/config/tailwind.config.js
+++ ecosystem/platform/server/config/tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
+    './app/components/**/*.{rb,erb}',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
