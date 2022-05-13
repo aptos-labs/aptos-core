ee76c107c066f57713817f6c3e7a48fe5f21e681
Zach Denton <z@chdenton.com>
2022-05-13 09:03:10 -0700

    Add a field for fullnode network key. (#969)

--- ecosystem/platform/server/db/schema.rb
+++ ecosystem/platform/server/db/schema.rb
@@ -14,7 +14,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_512_031_021) do
+ActiveRecord::Schema[7.0].define(version: 20_220_513_141_301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pgcrypto'
   enable_extension 'plpgsql'
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_512_031_021) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.boolean 'terms_accepted', default: false
+    t.string 'fullnode_network_key'
     t.index ['user_id'], name: 'index_it1_profiles_on_user_id'
   end
 
