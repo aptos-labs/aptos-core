89fe66d0313bb6697d144b35ec03ee8de86a5dad
Sital Kedia <skedia@aptoslabs.com>
2022-05-13 22:20:04 -0700

    [Storage][Pruner] Enable transaction accumulator pruning
    Closes: #1036

--- storage/aptosdb/src/pruner/transaction_store/transaction_store_pruner.rs
+++ storage/aptosdb/src/pruner/transaction_store/transaction_store_pruner.rs
@@ -35,6 +35,11 @@ impl DBSubPruner for TransactionStorePruner {
             target_version,
             db_batch,
         )?;
+        self.transaction_store.prune_transaction_accumulator(
+            least_readable_version,
+            target_version,
+            db_batch,
+        )?;
         Ok(())
     }
 }
