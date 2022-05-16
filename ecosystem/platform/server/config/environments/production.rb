ce8010d12225d6fffda1430d2b9b218ecf3af9ae
Zach Denton <z@chdenton.com>
2022-05-16 15:00:58 -0400

    Add cache-control headers for assets in prod.
    Closes: #1020

--- ecosystem/platform/server/config/environments/production.rb
+++ ecosystem/platform/server/config/environments/production.rb
@@ -28,6 +28,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000, immutable',
+    'Expires' => 1.year.from_now.to_formatted_s(:rfc822)
+  }
 
   # Compress CSS using a preprocessor.
   # Must set up dart-sass before re-enabling
