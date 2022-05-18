669dc11605d30ce46894e46c2bb0a11566ab71df
Zach Denton <z@chdenton.com>
2022-05-18 18:03:56 -0400

    Consolidate icons into a single component.

--- ecosystem/platform/server/app/components/button_component.rb
+++ ecosystem/platform/server/app/components/button_component.rb
@@ -3,27 +3,31 @@
 # Copyright (c) Aptos
 # SPDX-License-Identifier: Apache-2.0
 
-SCHEME_CLASSES = {
-  primary: 'bg-teal-400 hover:brightness-105 text-neutral-800 font-mono uppercase font-bold flex items-center justify-center',
-  secondary: 'border border-teal-400 hover:border-teal-300 text-white font-mono uppercase flex items-center justify-center'
-}.freeze
-
-SIZE_CLASSES = {
-  large: 'px-8 py-4 text-lg rounded-lg gap-4',
-  medium: 'px-8 py-2 text-lg rounded gap-2',
-  small: 'py-1 text-sm rounded gap-1'
-}.freeze
-
 class ButtonComponent < ViewComponent::Base
+  SCHEME_CLASSES = {
+    primary: 'bg-teal-400 hover:brightness-105 text-neutral-800 font-mono uppercase font-bold flex items-center justify-center',
+    secondary: 'border border-teal-400 hover:border-teal-300 text-white font-mono uppercase flex items-center justify-center'
+  }.freeze
+
+  SIZE_CLASSES = {
+    large: 'px-8 py-4 text-lg rounded-lg gap-4',
+    medium: 'px-8 py-2 text-lg rounded gap-2',
+    small: 'py-1 text-sm rounded gap-1'
+  }.freeze
+
   def initialize(scheme: :primary,
                  size: :medium,
                  **rest)
-    rest[:class] = [
+    @rest = rest
+    @rest[:class] = [
       SCHEME_CLASSES[scheme],
       SIZE_CLASSES[size],
-      rest[:class]
+      @rest[:class]
     ]
-    @rest = rest
-    @tag = rest[:href] ? :a : :button
+    @tag = @rest[:href] ? :a : :button
+  end
+
+  def call
+    content_tag @tag, content, **@rest
   end
 end
