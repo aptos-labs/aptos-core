4f8e068d0d023976a92ae1a63a0e301c0ed34173
Max Kaplan <1482859+CapCap@users.noreply.github.com>
2022-05-19 13:26:19 -0700

    [CP] Admin powers for authz (#1089)

--- ecosystem/platform/server/app/admin/authorizations.rb
+++ ecosystem/platform/server/app/admin/authorizations.rb
@@ -5,9 +5,9 @@
 
 ActiveAdmin.register Authorization do
   menu priority: 3
-  actions :all, except: %i[destroy edit new]
+  actions :all, except: %i[new]
 
-  permit_params :consensus_key
+  permit_params :user_id
   includes :user
 
   index do
@@ -15,27 +15,25 @@ ActiveAdmin.register Authorization do
     id_column
 
     column :user
+    column 'User ID', :user_id
     column :provider
     column :uid
     column :email
     column :username
     column :full_name
     column(:profile_url) { |c| link_to 'view⇗', c.profile_url, target: '_blank' if c.profile_url.present? }
-    column :expires
-    column :expires_at
     column :created_at
     column :updated_at
 
     actions
   end
 
+  filter :user_id, label: 'User ID'
   filter :provider
   filter :uid
   filter :email
   filter :username
   filter :full_name
-  filter :expires
-  filter :expires_at
   filter :created_at
   filter :updated_at
 
@@ -43,5 +41,17 @@ ActiveAdmin.register Authorization do
     default_main_content do
       row :user
     end
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :user_id, label: 'User ID'
+      f.input :provider, input_html: { readonly: true, disabled: true }
+      f.input :email, input_html: { readonly: true, disabled: true }
+      f.input :username, input_html: { readonly: true, disabled: true }
+      f.input :full_name, input_html: { readonly: true, disabled: true }
+    end
+    f.actions
   end
 end
