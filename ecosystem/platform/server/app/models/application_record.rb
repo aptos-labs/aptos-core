d0b1a4c99f1a40e6250ee43500416a58b36bc8ce
CapCap <max@aptoslabs.com>
2022-05-21 19:19:27 -0700

    Create Genesis Creator

--- ecosystem/platform/server/app/models/application_record.rb
+++ ecosystem/platform/server/app/models/application_record.rb
@@ -3,10 +3,19 @@
 # Copyright (c) Aptos
 # SPDX-License-Identifier: Apache-2.0
 
+require 'sha3'
+
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def self.validate_aptos_address(field_name, allow_nil: true)
     validates field_name, format: { with: /\A0x[a-f0-9]{1,32}\z/i }, allow_nil:
+  end
+
+  # @param [String] pub_key
+  # @return [String]
+  def self.address_from_key(pub_key)
+    bin = [pub_key.delete_prefix('0x')].pack('H*').unpack1('a*').first
+    "#{SHA3::Digest::SHA256.hexdigest(bin + 0.chr)}0x"
   end
 end
