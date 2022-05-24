311a432833cbeff5642cd0277942986040c5fa26
Zach Denton <z@chdenton.com>
2022-05-24 13:45:57 -0400

    Enable caching in production. (#1144)

--- ecosystem/platform/server/config/environments/production.rb
+++ ecosystem/platform/server/config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # TODO: Use a different cache store in production.
+  config.cache_store = :memory_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :delayed
