2cda54221c3de63c58f077b7439c1b6e97be2899
Jijun Leng <jijunleng@gmail.com>
2022-06-07 13:43:55 -0700

    [sdk][typescript] Add comments and tests
    Closes: #1285

--- ecosystem/typescript/sdk/src/transaction_builder/aptos_types/authentication_key.ts
+++ ecosystem/typescript/sdk/src/transaction_builder/aptos_types/authentication_key.ts
@@ -2,8 +2,15 @@ import * as SHA3 from 'js-sha3';
 import { HexString } from '../../hex_string';
 import { Bytes } from '../bcs';
 import { AccountAddress } from './account_address';
-import { MultiEd25519PublicKey } from './authenticator';
+import { MultiEd25519PublicKey } from './multi_ed25519';
 
+/**
+ * Each account stores an authentication key. Authentication key enables account owners to rotate
+ * their private key(s) associated with the account without changing the address that hosts their account.
+ * @see {@link * https://aptos.dev/basics/basics-accounts | Account Basics}
+ *
+ * Account addresses can be derived from AuthenticationKey
+ */
 export class AuthenticationKey {
   static readonly LENGTH: number = 32;
 
@@ -18,6 +25,11 @@ export class AuthenticationKey {
     this.bytes = bytes;
   }
 
+  /**
+   * Converts a K-of-N MultiEd25519PublicKey to AuthenticationKey with:
+   * `auth_key = sha3-256(p_1 | … | p_n | K | 0x01)`. `K` represents the K-of-N required for
+   * authenticating the transaction. `0x01` is the 1-byte scheme for multisig.
+   */
   static fromMultiEd25519PublicKey(publicKey: MultiEd25519PublicKey): AuthenticationKey {
     const bytes = new Uint8Array([...publicKey.toBytes(), AuthenticationKey.MULTI_ED25519_SCHEME]);
     const hash = SHA3.sha3_256.create();
@@ -26,6 +38,10 @@ export class AuthenticationKey {
     return new AuthenticationKey(new Uint8Array(hash.arrayBuffer()));
   }
 
+  /**
+   * Derives an account address from AuthenticationKey. Since current AccountAddress is 32 bytes,
+   * AuthenticationKey bytes are directly translated to AccountAddress.
+   */
   derivedAddress(): HexString {
     return HexString.fromUint8Array(this.bytes.subarray(AuthenticationKey.LENGTH - AccountAddress.LENGTH));
   }
--- ecosystem/typescript/sdk/src/transaction_builder/aptos_types/index.ts
+++ ecosystem/typescript/sdk/src/transaction_builder/aptos_types/index.ts
@@ -3,3 +3,5 @@ export * from './authenticator';
 export * from './transaction';
 export * from './type_tag';
 export * from './identifier';
+export * from './ed25519';
+export * from './multi_ed25519';
