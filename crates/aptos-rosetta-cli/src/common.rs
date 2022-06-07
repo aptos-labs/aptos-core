a15787c793531a92bd0da0e379a8d22820211a69
Greg <greg@gnazar.io>
2022-06-07 13:06:12 -0700

    [aptos-rosetta-cli] Add a small CLI to test Rosetta locally

---
+++ crates/aptos-rosetta-cli/src/common.rs
@@ -0,0 +1,75 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{account, network};
+use aptos_rosetta::{
+    client::RosettaClient,
+    types::{NetworkIdentifier, NetworkRequest},
+};
+use aptos_types::chain_id::ChainId;
+use clap::Parser;
+use serde::Serialize;
+
+/// Aptos Rosetta CLI
+///
+/// Provides an implementation of [Rosetta](https://www.rosetta-api.org/docs/Reference.html) on Aptos.
+#[derive(Debug, Parser)]
+#[clap(name = "aptos-rosetta-cli", author, version, propagate_version = true)]
+pub enum RosettaCliArgs {
+    #[clap(subcommand)]
+    Account(account::AccountCommand),
+    #[clap(subcommand)]
+    Network(network::NetworkCommand),
+}
+
+impl RosettaCliArgs {
+    pub async fn execute(self) -> anyhow::Result<String> {
+        match self {
+            RosettaCliArgs::Account(inner) => inner.execute().await,
+            RosettaCliArgs::Network(inner) => inner.execute().await,
+        }
+    }
+}
+
+/// Format output to a human readable form
+pub fn format_output<T: Serialize>(input: anyhow::Result<T>) -> anyhow::Result<String> {
+    input.map(|value| serde_json::to_string_pretty(&value).unwrap())
+}
+
+#[derive(Debug, Parser)]
+pub struct UrlArgs {
+    /// URL for the Aptos Rosetta API. e.g. http://localhost:8080
+    #[clap(long, default_value = "http://localhost:8080")]
+    rosetta_api_url: url::Url,
+}
+
+impl UrlArgs {
+    pub fn client(self) -> RosettaClient {
+        RosettaClient::new(self.rosetta_api_url)
+    }
+}
+
+#[derive(Debug, Parser)]
+pub struct NetworkArgs {
+    /// ChainId to be used for the server e.g. TESTNET
+    #[clap(long, default_value = "TESTING")]
+    pub chain_id: ChainId,
+}
+
+impl NetworkArgs {
+    pub fn network_identifier(self) -> NetworkIdentifier {
+        self.chain_id.into()
+    }
+
+    pub fn network_request(self) -> NetworkRequest {
+        NetworkRequest {
+            network_identifier: self.network_identifier(),
+        }
+    }
+}
+
+/// Wrapper so that it's easy to tell that the output is an error
+#[derive(Serialize)]
+pub struct ErrorWrapper {
+    pub error: String,
+}
---
+++ crates/aptos-rosetta-cli/src/main.rs
@@ -0,0 +1,42 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+//! Aptos Rosetta CLI
+//!
+//! Why have an Aptos version of the Rosetta CLI?
+//!
+//! The Rosetta CLI doesn't build on my Mac easily and I just wanted something simple to test out
+//! the POST requests
+//!
+//! Why have a separate CLI?
+//!
+//! We want users to use the Aptos CLI over the Rosetta CLI because of the added complexity of a
+//! proxy server.  So, we split it out so general users aren't confused.
+//!
+//! TODO: Make Aptos CLI framework common among multiple CLIs
+
+#![forbid(unsafe_code)]
+
+mod account;
+mod common;
+mod network;
+
+use crate::common::{ErrorWrapper, RosettaCliArgs};
+use clap::Parser;
+
+#[tokio::main]
+async fn main() {
+    let args: RosettaCliArgs = RosettaCliArgs::parse();
+
+    let result = args.execute().await;
+
+    match result {
+        Ok(value) => println!("{}", value),
+        Err(error) => {
+            let error = ErrorWrapper {
+                error: error.to_string(),
+            };
+            println!("{}", serde_json::to_string_pretty(&error).unwrap())
+        }
+    }
+}
