70f341ff7579784213b61932b6f127788de4f1f4
Greg <greg@gnazar.io>
2022-06-07 23:31:54 -0700

    [aptos-rosetta] Add documentation for implementation specifics on spec

--- crates/aptos-rosetta/src/types/objects.rs
+++ crates/aptos-rosetta/src/types/objects.rs
@@ -7,26 +7,45 @@ use crate::types::{
 };
 use serde::{Deserialize, Serialize};
 
+/// A description of all types used by the Rosetta implementation.
 ///
+/// This is used to verify correctness of the implementation and to check things like
+/// operation names, and error names.
 ///
 /// [API Spec](https://www.rosetta-api.org/docs/models/Allow.html)
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Allow {
+    /// List of all possible operation statuses
     pub operation_statuses: Vec<OperationStatus>,
+    /// List of all possible writeset types
     pub operation_types: Vec<String>,
+    /// List of all possible errors
     pub errors: Vec<Error>,
+    /// If the server is allowed to lookup historical transactions
     pub historical_balance_lookup: bool,
+    /// All times after this are valid timestamps
+    /// TODO: Determine if we even need to bother with this
     #[serde(skip_serializing_if = "Option::is_none")]
     pub timestamp_start_index: Option<u64>,
+    /// All call methods supported
     pub call_methods: Vec<String>,
+    /// A list of balance exemptions, where these accounts change their balance
+    /// without an operation
     pub balance_exemptions: Vec<BalanceExemption>,
+    /// Determines if mempool can change the balance on an account
+    /// This should be set to false
+    pub mempool_coins: bool,
+    /// Case specifics for block hashes.  Set to None if case insensitive
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub block_hash_case: Option<Case>,
+    /// Case specifics for transaction hashes.  Set to None if case insensitive
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transaction_hash_case: Option<Case>,
 }
 
 /// Amount of a [`Currency`] in atomic units
 ///
 /// [API Spec](https://www.rosetta-api.org/docs/models/Amount.html)
-///
-/// TODO: Do we need optional metadata?
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Amount {
     /// Value of transaction as a String representation of an integer
@@ -86,6 +105,8 @@ pub struct BlockTransaction {
     transaction: Transaction,
 }
 
+/// Tells
+///
 ///[API Spec](https://www.rosetta-api.org/docs/models/Case.html)
 #[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(rename_all = "snake_case")]
@@ -93,7 +114,6 @@ pub enum Case {
     UpperCase,
     LowerCase,
     CaseSensitive,
-    Null,
 }
 
 /// Currency represented as atomic units including decimals
@@ -107,13 +127,14 @@ pub struct Currency {
     pub decimals: u64,
 }
 
+/// Various signing curves supported by Rosetta.  We only use [`CurveType::Edwards25519`]
 /// [API Spec](https://www.rosetta-api.org/docs/models/CurveType.html)
 #[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub enum CurveType {
+    Edwards25519,
     Secp256k1,
     Secp256r1,
-    Edwards25519,
     Tweedle,
     Pallas,
 }
@@ -135,21 +156,29 @@ pub enum ExemptionType {
     Dynamic,
 }
 
-/// [API Spec](https://www.rosetta-api.org/docs/models/Operation.html)
+/// A representation of a single account change in a transaction
 ///
-/// TODO: Metadata?
+/// This is known as a write set change within Aptos
+/// [API Spec](https://www.rosetta-api.org/docs/models/Operation.html)
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Operation {
+    /// Identifier of an operation within a transaction
     pub operation_identifier: OperationIdentifier,
+    /// Related operations e.g. multiple operations that are related to a transfer
     #[serde(skip_serializing_if = "Option::is_none")]
     pub related_operations: Option<Vec<OperationIdentifier>>,
     /// Type of operation
     #[serde(rename = "type")]
-    pub type_: String,
-    /// Status of operation
+    pub operation_type: String,
+    /// Status of operation.  Must be populated if the transaction is in the past.  If submitting
+    /// new transactions, it must NOT be populated.
     pub status: Option<String>,
+    /// AccountIdentifier should be provided to point at which account the change is
     #[serde(skip_serializing_if = "Option::is_none")]
     pub account: Option<AccountIdentifier>,
+    /// Amount in the operation
+    ///
+    /// TODO: Determine if this is required
     #[serde(skip_serializing_if = "Option::is_none")]
     pub amount: Option<Amount>,
 }
@@ -218,9 +247,9 @@ pub struct SigningPayload {
     pub signature_type: Option<SignatureType>,
 }
 
-/// [API Spec](https://www.rosetta-api.org/docs/models/Transaction.html)
 ///
-/// TODO: Metadata?
+///
+/// [API Spec](https://www.rosetta-api.org/docs/models/Transaction.html)
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Transaction {
     pub transaction_identifier: TransactionIdentifier,
