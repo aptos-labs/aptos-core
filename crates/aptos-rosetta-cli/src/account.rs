3f63d4404953c0acecb8e56fb1abdc0d9cfc10fc
Greg <greg@gnazar.io>
2022-06-08 15:16:48 -0700

    [aptos-rosetta] Remove account_balance_simple
    It gave too much confusion, and it didn't make it too much easier

--- crates/aptos-rosetta-cli/src/account.rs
+++ crates/aptos-rosetta-cli/src/account.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::common::{format_output, NetworkArgs, UrlArgs};
-use aptos_rosetta::types::AccountBalanceResponse;
+use aptos_rosetta::types::{AccountBalanceRequest, AccountBalanceResponse};
 use aptos_types::account_address::AccountAddress;
 use clap::{Parser, Subcommand};
 
@@ -39,8 +39,12 @@ pub struct AccountBalanceCommand {
 impl AccountBalanceCommand {
     pub async fn execute(self) -> anyhow::Result<AccountBalanceResponse> {
         let client = self.url_args.client();
-        client
-            .account_balance_simple(self.account, self.network_args.chain_id)
-            .await
+        let request = AccountBalanceRequest {
+            network_identifier: self.network_args.network_identifier(),
+            account_identifier: self.account.into(),
+            block_identifier: None,
+            currencies: None,
+        };
+        client.account_balance(&request).await
     }
 }
