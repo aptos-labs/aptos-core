9d0c87e3a51cb048a0b4c45ac308adebfb2f0990
David <isaac.wolinsky@gmail.com>
2022-06-12 15:20:10 -0700

    [framework] remove unused native destroy signer

--- aptos-move/framework/src/natives/account.rs
+++ aptos-move/framework/src/natives/account.rs
@@ -28,17 +28,3 @@ pub fn native_create_signer(
     let cost = native_gas(context.cost_table(), NativeCostIndex::CREATE_SIGNER, 0);
     Ok(NativeResult::ok(cost, smallvec![Value::signer(address)]))
 }
-
-/// NOTE: this function will be deprecated after the Aptos v3 release, but must
-/// remain for replaying old transactions
-pub fn native_destroy_signer(
-    context: &mut NativeContext,
-    ty_args: Vec<Type>,
-    arguments: VecDeque<Value>,
-) -> PartialVMResult<NativeResult> {
-    debug_assert!(ty_args.is_empty());
-    debug_assert!(arguments.len() == 1);
-
-    let cost = native_gas(context.cost_table(), NativeCostIndex::DESTROY_SIGNER, 0);
-    Ok(NativeResult::ok(cost, smallvec![]))
-}
