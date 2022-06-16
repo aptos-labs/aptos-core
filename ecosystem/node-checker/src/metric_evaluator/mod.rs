48a4e6aed2e6fa2aef17627b42677df32181c440
Daniel Porteous <danielporteous1@gmail.com>
2022-06-16 12:55:14 -0700

    Move generic evaluation structs into their own module

--- ecosystem/node-checker/src/metric_evaluator/mod.rs
+++ ecosystem/node-checker/src/metric_evaluator/mod.rs
@@ -5,7 +5,6 @@ mod build_evaluators;
 mod common;
 mod state_sync_evaluator;
 mod traits;
-mod types;
 
 pub use build_evaluators::build_evaluators;
 pub use common::parse_metrics;
@@ -13,4 +12,3 @@ pub use state_sync_evaluator::{
     StateSyncMetricsEvaluator, StateSyncMetricsEvaluatorArgs, NAME as STATE_SYNC_EVALUATOR_NAME,
 };
 pub use traits::{MetricsEvaluator, MetricsEvaluatorError};
-pub use types::{EvaluationResult, EvaluationSummary};
