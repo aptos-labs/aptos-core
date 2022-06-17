9ba8021158ecd8435627034e0da219ea1f4cf2c7
Kent White <kent@aptoslabs.com>
2022-06-16 23:23:34 -0700

    Remove unused code

--- src/core/utils/account.ts
+++ src/core/utils/account.ts
@@ -2,28 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { AptosAccount, AptosAccountObject } from 'aptos';
-import { Buffer } from 'buffer';
-import { KEY_LENGTH, WALLET_STATE_LOCAL_STORAGE_KEY } from 'core/constants';
-import {
-  AptosAccountState, LocalStorageState, Result, err, ok,
-} from 'core/types';
+import { WALLET_STATE_LOCAL_STORAGE_KEY } from 'core/constants';
+import { AptosAccountState, LocalStorageState } from 'core/types';
 
 import Browser from 'core/utils/browser';
-
-export function loginAccount(key: string): Result<AptosAccount, Error> {
-  if (key.length === KEY_LENGTH) {
-    try {
-      const encodedKey = Uint8Array.from(Buffer.from(key, 'hex'));
-      // todo: Ping API to check if a legit account
-      const account = new AptosAccount(encodedKey, undefined);
-      return ok(account);
-    } catch (e) {
-      return err(e as Error);
-    }
-  } else {
-    return err(new Error('Key not the correct the length'));
-  }
-}
 
 export function createNewAccount(): AptosAccount {
   const account = new AptosAccount();
