bb334724c7cad0601d37385ac16db2767bbab08f
Daniel Porteous <danielporteous1@gmail.com>
2022-06-23 15:13:09 -0700

    Implement nhc server generate-openapi

--- ecosystem/node-checker/src/server/generate_openapi.rs
+++ ecosystem/node-checker/src/server/generate_openapi.rs
@@ -1,16 +1,64 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-#![allow(unused_variables)]
-
+use crate::{metric_collector::ReqwestMetricCollector, runner::BlockingRunner};
 use anyhow::Result;
-use clap::Parser;
+use clap::{ArgEnum, Parser};
+use std::{collections::HashMap, path::PathBuf};
+use url::Url;
+
+use super::{
+    api::{build_openapi_service, Api},
+    configurations_manager::{ConfigurationsManager, NodeConfigurationWrapper},
+};
+
+#[derive(ArgEnum, Clone, Debug)]
+enum OutputFormat {
+    Json,
+    Yaml,
+}
 
 #[derive(Clone, Debug, Parser)]
-pub struct GenerateOpenapi {}
+pub struct GenerateOpenapi {
+    /// What address to listen on.
+    #[clap(long, default_value = "http://0.0.0.0")]
+    pub listen_address: Url,
 
-// TODO: To implement this, I first want to make fake implementations of the
-// major traits that we need to feed into Api.
+    /// What port to listen on.
+    #[clap(long, default_value = "20121")]
+    pub listen_port: u16,
+
+    #[clap(short, long)]
+    output_path: Option<PathBuf>,
+
+    #[clap(short, long, arg_enum, default_value = "yaml")]
+    format: OutputFormat,
+}
+
 pub async fn generate_openapi(args: GenerateOpenapi) -> Result<()> {
+    let configurations: HashMap<
+        _,
+        NodeConfigurationWrapper<ReqwestMetricCollector, BlockingRunner<ReqwestMetricCollector>>,
+    > = HashMap::new();
+
+    let api = Api {
+        configurations_manager: ConfigurationsManager { configurations },
+        target_metric_collector: None,
+        allow_preconfigured_test_node_only: false,
+    };
+
+    let api_service =
+        build_openapi_service(api, args.listen_address.clone(), args.listen_port, None);
+
+    let s = match args.format {
+        OutputFormat::Json => api_service.spec(),
+        OutputFormat::Yaml => api_service.spec_yaml(),
+    };
+
+    match args.output_path {
+        Some(path) => std::fs::write(path, s)?,
+        None => println!("{}", s),
+    }
+
     Ok(())
 }
