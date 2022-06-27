9d004089559ed18dd2d1b6a625fa975ca87d48ba
Greg <greg@gnazar.io>
2022-06-27 12:58:12 -0700

    [aptos-cli] Cleanup txn submission and unit tests
    Now gas prices are consistent across all txn submission APIs, and
    a lot of the duplicated logic is cleaned up

--- testsuite/smoke-test/src/aptos_cli.rs
+++ testsuite/smoke-test/src/aptos_cli.rs
@@ -3,39 +3,42 @@
 
 use crate::smoke_test_environment::new_local_swarm_with_aptos;
 use aptos::{account::create::DEFAULT_FUNDED_COINS, test::CliTestFramework};
-use aptos_config::keys::ConfigKey;
+use aptos_config::{keys::ConfigKey, utils::get_available_port};
 use aptos_crypto::ed25519::Ed25519PrivateKey;
 use aptos_faucet::FaucetArgs;
 use aptos_types::{account_config::aptos_root_address, chain_id::ChainId};
 use forge::{LocalSwarm, Node};
 use tokio::task::JoinHandle;
 
-pub async fn setup_test(num_nodes: usize) -> (LocalSwarm, CliTestFramework) {
+pub async fn setup_cli_test(num_nodes: usize) -> (LocalSwarm, CliTestFramework, JoinHandle<()>) {
     let swarm = new_local_swarm_with_aptos(num_nodes).await;
     let chain_id = swarm.chain_id();
     let validator = swarm.validators().next().unwrap();
     let root_key = swarm.root_key();
-    let _ = launch_faucet(validator.rest_api_endpoint(), root_key, chain_id);
-
-    // Connect the operator tool to the node's JSON RPC API
-    let tool = CliTestFramework::new(
+    let faucet_port = get_available_port();
+    let faucet = launch_faucet(
         validator.rest_api_endpoint(),
-        "http://localhost:9996".parse().unwrap(),
-        2,
-    )
-    .await;
+        root_key,
+        chain_id,
+        faucet_port,
+    );
+    let faucet_endpoint: reqwest::Url =
+        format!("http://localhost:{}", faucet_port).parse().unwrap();
+    // Connect the operator tool to the node's JSON RPC API
+    let tool = CliTestFramework::new(validator.rest_api_endpoint(), faucet_endpoint, 2).await;
 
-    (swarm, tool)
+    (swarm, tool, faucet)
 }
 
 pub fn launch_faucet(
     endpoint: reqwest::Url,
     mint_key: Ed25519PrivateKey,
     chain_id: ChainId,
+    port: u16,
 ) -> JoinHandle<()> {
     let faucet = FaucetArgs {
         address: "127.0.0.1".to_string(),
-        port: 9996,
+        port,
         server_url: endpoint.to_string(),
         mint_key_file_path: "".to_string(),
         mint_key: Some(ConfigKey::new(mint_key)),
@@ -49,7 +52,7 @@ pub fn launch_faucet(
 
 #[tokio::test]
 async fn test_account_flow() {
-    let (_swarm, cli) = setup_test(1).await;
+    let (_swarm, cli, _faucet) = setup_cli_test(1).await;
 
     assert_eq!(DEFAULT_FUNDED_COINS, cli.account_balance(0).await.unwrap());
     assert_eq!(DEFAULT_FUNDED_COINS, cli.account_balance(1).await.unwrap());
