05f5d7f40c477853edcf726bc0675c9b15ff051a
Daniel Porteous <danielporteous1@gmail.com>
2022-06-29 12:13:36 -0700

    Add support for verifying chain ID and role type

---
+++ ecosystem/node-checker/src/evaluators/direct/node_identity.rs
@@ -0,0 +1,153 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{
+    configuration::{EvaluatorArgs, NodeAddress},
+    evaluator::{EvaluationResult, Evaluator},
+};
+use anyhow::{anyhow, format_err, Result};
+use aptos_config::config::RoleType;
+use aptos_sdk::types::chain_id::ChainId;
+use clap::Parser;
+use poem_openapi::Object as PoemObject;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, str::FromStr};
+use thiserror::Error as ThisError;
+
+use super::DirectEvaluatorInput;
+
+pub const CATEGORY: &str = "node_identity";
+
+/// This function hits the `/` endpoint of the API and returns the chain ID
+/// and role type, extracted from the IndexResponse.
+pub async fn get_node_identity(node_address: &NodeAddress) -> Result<(ChainId, RoleType)> {
+    let mut url = node_address.url.clone();
+    url.set_port(Some(node_address.api_port))
+        .map_err(|_| format_err!("Failed to set port for URL"))?;
+
+    let response = reqwest::get(url)
+        .await
+        .map_err(|e| format_err!("Failed to get node identity {}", e))?;
+    let response_body = response
+        .text()
+        .await
+        .map_err(|e| format_err!("Failed to get body of node identity response {}", e))?;
+
+    let data: HashMap<String, serde_json::Value> =
+        serde_json::from_str(&response_body).map_err(|e| {
+            format_err!(
+                "Failed to process response body as valid JSON with string key/values {}",
+                e
+            )
+        })?;
+
+    let chain_id_raw: u8 = data
+        .get("chain_id")
+        .ok_or_else(|| format_err!("Failed to get chain_id from node identity"))?
+        .as_u64()
+        .ok_or_else(|| anyhow!("Failed to read chain ID from node identity as u8"))?
+        as u8;
+    let chain_id = ChainId::new(chain_id_raw);
+
+    let role_type_raw = data
+        .get("node_role")
+        .ok_or_else(|| format_err!("Failed to get node_role from node identity"))?
+        .as_str()
+        .ok_or_else(|| anyhow!("Failed to read node_role from node identity as str"))?;
+    let role_type = RoleType::from_str(role_type_raw)
+        .map_err(|e| format_err!("Failed to parse node_role {}", e))?;
+
+    Ok((chain_id, role_type))
+}
+
+#[derive(Debug, ThisError)]
+pub enum NodeIdentityEvaluatorError {
+    #[error("Failed to get node identity from node: {0}")]
+    GetNodeIdentityError(anyhow::Error),
+}
+
+// TODO: Consider taking chain_id and role_type here instead.
+#[derive(Clone, Debug, Deserialize, Parser, PoemObject, Serialize)]
+pub struct NodeIdentityEvaluatorArgs {}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct NodeIdentityEvaluator {
+    args: NodeIdentityEvaluatorArgs,
+}
+
+impl NodeIdentityEvaluator {
+    pub fn new(args: NodeIdentityEvaluatorArgs) -> Self {
+        Self { args }
+    }
+
+    fn build_evaluation_result(
+        &self,
+        headline: String,
+        score: u8,
+        explanation: String,
+    ) -> EvaluationResult {
+        EvaluationResult {
+            headline,
+            score,
+            explanation,
+            category: CATEGORY.to_string(),
+            evaluator_name: Self::get_name(),
+            links: vec![],
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl Evaluator for NodeIdentityEvaluator {
+    type Input = DirectEvaluatorInput;
+    type Error = NodeIdentityEvaluatorError;
+
+    /// Assert that the node identity (role type and chain ID) of the two
+    /// nodes match.
+    async fn evaluate(&self, input: &Self::Input) -> Result<Vec<EvaluationResult>, Self::Error> {
+        let (target_chain_id, target_role_type) = get_node_identity(&input.target_node_address)
+            .await
+            .map_err(NodeIdentityEvaluatorError::GetNodeIdentityError)?;
+
+        let evaluation_result = if (input.baseline_node_information.chain_id == target_chain_id)
+            && (input.baseline_node_information.role_type == target_role_type)
+        {
+            self.build_evaluation_result(
+                "Chain ID and role type reported by baseline and target match".to_string(),
+                100,
+                format!(
+                    "The node under investigation reported the same chain ID \"{}\" \
+                and role type \"{}\" as is reported by the baseline node",
+                    target_chain_id, target_role_type
+                ),
+            )
+        } else {
+            self.build_evaluation_result(
+                "Chain ID and/or role type reported by the target do not match the baseline"
+                    .to_string(),
+                0,
+                format!(
+                    "The node under investigation reported the chain ID \"{}\" \
+                and role type \"{}\" while the baseline reported \"{}\" and \"{}\". \
+                Each of these values should match. Confirm that the baseline you're \
+                using is appropriate for the node you're testing.",
+                    target_chain_id,
+                    target_role_type,
+                    input.baseline_node_information.chain_id,
+                    input.baseline_node_information.role_type
+                ),
+            )
+        };
+
+        Ok(vec![evaluation_result])
+    }
+
+    fn get_name() -> String {
+        CATEGORY.to_string()
+    }
+
+    fn from_evaluator_args(evaluator_args: &EvaluatorArgs) -> Self {
+        Self::new(evaluator_args.node_identity_args.clone())
+    }
+}
--- ecosystem/node-checker/src/server/mod.rs
+++ ecosystem/node-checker/src/server/mod.rs
@@ -5,13 +5,15 @@ mod api;
 mod common;
 mod configurations_manager;
 mod generate_openapi;
+mod node_information;
 mod run;
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-
 use generate_openapi::{generate_openapi, GenerateOpenapi};
 use run::{run, Run};
+
+pub use node_information::NodeInformation;
 
 #[derive(Clone, Debug, Parser)]
 pub struct Server {
