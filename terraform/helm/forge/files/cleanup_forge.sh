86ab836d06090c47911e285b5f07f36d0f3ed467
Rustie Lin <rustie117@gmail.com>
2022-06-29 17:53:54 -0700

    [tf/helm][forge] get forge to run on aptos-node

--- terraform/helm/forge/files/cleanup_forge.sh
+++ terraform/helm/forge/files/cleanup_forge.sh
@@ -3,4 +3,5 @@
 # Script to be run in a Forge pod after the test runs as cleanup
 # This separates the pod lifecycle from aptos-core
 
-echo "Forge clean"
+kubectl delete deployment,sts -l "app.kubernetes.io/part-of=aptos-node"
+echo "Forge cleaned"
