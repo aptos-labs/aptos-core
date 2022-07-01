01d50b2edca5c6f2a1fc008c60b25c91bbe5b2e8
zcchahaha <107430656+zcchahaha@users.noreply.github.com>
2022-06-30 17:35:30 -0700

    [storage] Delete definition of PRE_GENEIS_VERSION

--- types/src/transaction/mod.rs
+++ types/src/transaction/mod.rs
@@ -56,9 +56,6 @@ pub use transaction_argument::{parse_transaction_argument, TransactionArgument, 
 pub type Version = u64; // Height - also used for MVCC in StateDB
 pub type AtomicVersion = AtomicU64;
 
-// In StateDB, things readable by the genesis transaction are under this version.
-pub const PRE_GENESIS_VERSION: Version = u64::max_value();
-
 /// RawTransaction is the portion of a transaction that a client signs.
 #[derive(
     Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize, CryptoHasher, BCSCryptoHash,
