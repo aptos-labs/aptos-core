5fd2871893c96f7728cee5f8143227753e4fde43
Max Kaplan <1482859+CapCap@users.noreply.github.com>
2022-06-30 22:40:38 -0700

    [CP] Perform node validation asynchronously (#1695)

--- ecosystem/platform/server/app/helpers/node_helper.rb
+++ ecosystem/platform/server/app/helpers/node_helper.rb
@@ -224,7 +224,7 @@ module NodeHelper
       url = URI.join(@base_url, "/check_node?#{params}")
       url = "#{url}&baseline_configuration_name=#{baseline_configuration_name}" if baseline_configuration_name.present?
       puts "Calling node healthchecker: #{url}"
-      res = HTTParty.get(url, open_timeout: 3, read_timeout: ENV.fetch('NODE_CHECKER_TIMEOUT', 30), max_retries: 0)
+      res = HTTParty.get(url, open_timeout: 3, read_timeout: ENV.fetch('NODE_CHECKER_TIMEOUT', 300), max_retries: 0)
       data = JSON.parse(res.body)
 
       evaluation_results = data['evaluation_results'].map do |er|
