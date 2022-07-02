apscan/indexer

[api] BCS output for transactions and events

api/src/index.rs
@@ -53,6 +53,7 @@ pub fn routes(context: Context) -> impl Filter<Extract = impl Reply, Error = Inf
         .or(accounts::get_account_modules(context.clone()))
         .or(transactions::get_transaction(context.clone()))
         .or(transactions::get_transactions(context.clone()))
+        .or(transactions::get_transactions_bcs(context.clone()))
         .or(transactions::get_account_transactions(context.clone()))
         .or(transactions::simulate_bcs_transactions(context.clone()))
         .or(transactions::simulate_json_transactions(context.clone()))
@@ -61,6 +62,7 @@ pub fn routes(context: Context) -> impl Filter<Extract = impl Reply, Error = Inf
         .or(transactions::create_signing_message(context.clone()))
         .or(events::get_events_by_event_key(context.clone()))
         .or(events::get_events_by_event_handle(context.clone()))
+        .or(events::get_events_by_event_handle_bcs(context.clone()))
         .or(state::get_account_resource(context.clone()))
         .or(state::get_account_module(context.clone()))
         .or(state::get_table_item(context.clone()))

aptos-labs/aptos-core

api/src/index.rs
@@ -51,16 +51,20 @@ pub fn routes(context: Context) -> impl Filter<Extract = impl Reply, Error = Inf
         .or(accounts::get_account(context.clone()))
         .or(accounts::get_account_resources(context.clone()))
         .or(accounts::get_account_modules(context.clone()))
-        .or(transactions::get_transaction(context.clone()))
-        .or(transactions::get_transactions(context.clone()))
+        .or(transactions::get_bcs_transaction(context.clone()))
+        .or(transactions::get_json_transaction(context.clone()))
+        .or(transactions::get_bcs_transactions(context.clone()))
+        .or(transactions::get_json_transactions(context.clone()))
         .or(transactions::get_account_transactions(context.clone()))
         .or(transactions::simulate_bcs_transactions(context.clone()))
         .or(transactions::simulate_json_transactions(context.clone()))
         .or(transactions::submit_bcs_transactions(context.clone()))
         .or(transactions::submit_json_transactions(context.clone()))
         .or(transactions::create_signing_message(context.clone()))
-        .or(events::get_events_by_event_key(context.clone()))
-        .or(events::get_events_by_event_handle(context.clone()))
+        .or(events::get_bcs_events_by_event_key(context.clone()))
+        .or(events::get_json_events_by_event_key(context.clone()))
+        .or(events::get_bcs_events_by_event_handle(context.clone()))
+        .or(events::get_json_events_by_event_handle(context.clone()))
         .or(state::get_account_resource(context.clone()))
         .or(state::get_account_module(context.clone()))
         .or(state::get_table_item(context.clone()))
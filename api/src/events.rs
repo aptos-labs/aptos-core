4da195fc03f387a79aa7cb1d910509971e9f6cb8
Jijun Leng <jijunleng@gmail.com>
2022-07-01 10:04:08 -0700

    [api] BCS output for transactions and events

--- api/src/events.rs
+++ api/src/events.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
+    accept_type::AcceptType,
     accounts::Account,
     context::Context,
     failpoint::fail_point,
@@ -10,31 +11,84 @@ use crate::{
     param::{AddressParam, EventKeyParam, MoveIdentifierParam, MoveStructTagParam},
 };
 
-use aptos_api_types::{AsConverter, Error, LedgerInfo, Response};
+use aptos_api_types::{mime_types::BCS, AsConverter, Error, LedgerInfo, Response};
 
 use anyhow::Result;
 use aptos_types::event::EventKey;
-use warp::{filters::BoxedFilter, Filter, Rejection, Reply};
+use warp::{filters::BoxedFilter, http::header::ACCEPT, Filter, Rejection, Reply};
 
 // GET /events/<event_key>
-pub fn get_events_by_event_key(context: Context) -> BoxedFilter<(impl Reply,)> {
+pub fn get_json_events_by_event_key(context: Context) -> BoxedFilter<(impl Reply,)> {
     warp::path!("events" / EventKeyParam)
         .and(warp::get())
         .and(warp::query::<Page>())
         .and(context.filter())
+        .map(|event_key: EventKeyParam, page: Page, context: Context| {
+            (event_key, page, context, AcceptType::Json)
+        })
+        .untuple_one()
         .and_then(handle_get_events_by_event_key)
-        .with(metrics("get_events_by_event_key"))
+        .with(metrics("get_json_events_by_event_key"))
+        .boxed()
+}
+
+// GET /events/<event_key>
+pub fn get_bcs_events_by_event_key(context: Context) -> BoxedFilter<(impl Reply,)> {
+    warp::path!("events" / EventKeyParam)
+        .and(warp::get())
+        .and(warp::header::exact(ACCEPT.as_str(), BCS))
+        .and(warp::query::<Page>())
+        .and(context.filter())
+        .map(|event_key: EventKeyParam, page: Page, context: Context| {
+            (event_key, page, context, AcceptType::Bcs)
+        })
+        .untuple_one()
+        .and_then(handle_get_events_by_event_key)
+        .with(metrics("get_bcs_events_by_event_key"))
         .boxed()
 }
 
 // GET /accounts/<address>/events/<event_handle_struct>/<field_name>
-pub fn get_events_by_event_handle(context: Context) -> BoxedFilter<(impl Reply,)> {
+pub fn get_json_events_by_event_handle(context: Context) -> BoxedFilter<(impl Reply,)> {
     warp::path!("accounts" / AddressParam / "events" / MoveStructTagParam / MoveIdentifierParam)
         .and(warp::get())
         .and(warp::query::<Page>())
         .and(context.filter())
+        .map(|address, struct_tag, field_name, page, context| {
+            (
+                address,
+                struct_tag,
+                field_name,
+                page,
+                context,
+                AcceptType::Json,
+            )
+        })
+        .untuple_one()
         .and_then(handle_get_events_by_event_handle)
         .with(metrics("get_events_by_event_handle"))
+        .boxed()
+}
+
+pub fn get_bcs_events_by_event_handle(context: Context) -> BoxedFilter<(impl Reply,)> {
+    warp::path!("accounts" / AddressParam / "events" / MoveStructTagParam / MoveIdentifierParam)
+        .and(warp::get())
+        .and(warp::header::exact(ACCEPT.as_str(), BCS))
+        .and(warp::query::<Page>())
+        .and(context.filter())
+        .map(|address, struct_tag, field_name, page, context| {
+            (
+                address,
+                struct_tag,
+                field_name,
+                page,
+                context,
+                AcceptType::Bcs,
+            )
+        })
+        .untuple_one()
+        .and_then(handle_get_events_by_event_handle)
+        .with(metrics("get_bcs_events_by_event_handle"))
         .boxed()
 }
 
@@ -42,9 +96,10 @@ async fn handle_get_events_by_event_key(
     event_key: EventKeyParam,
     page: Page,
     context: Context,
+    accept_type: AcceptType,
 ) -> Result<impl Reply, Rejection> {
     fail_point("endpoint_get_events_by_event_key")?;
-    Ok(Events::new(event_key.parse("event key")?.into(), context)?.list(page)?)
+    Ok(Events::new(event_key.parse("event key")?.into(), context)?.list(page, accept_type)?)
 }
 
 async fn handle_get_events_by_event_handle(
@@ -53,11 +108,12 @@ async fn handle_get_events_by_event_handle(
     field_name: MoveIdentifierParam,
     page: Page,
     context: Context,
+    accept_type: AcceptType,
 ) -> Result<impl Reply, Rejection> {
     fail_point("endpoint_get_events_by_event_handle")?;
     let key =
         Account::new(None, address, context.clone())?.find_event_key(struct_tag, field_name)?;
-    Ok(Events::new(key, context)?.list(page)?)
+    Ok(Events::new(key, context)?.list(page, accept_type)?)
 }
 
 struct Events {
@@ -76,7 +132,7 @@ impl Events {
         })
     }
 
-    pub fn list(self, page: Page) -> Result<impl Reply, Error> {
+    pub fn list(self, page: Page, accept_type: AcceptType) -> Result<impl Reply, Error> {
         let contract_events = self.context.get_events(
             &self.key,
             page.start(0, u64::MAX)?,
@@ -86,6 +142,10 @@ impl Events {
 
         let resolver = self.context.move_resolver()?;
         let events = resolver.as_converter().try_into_events(&contract_events)?;
-        Response::new(self.ledger_info, &events)
+
+        match accept_type {
+            AcceptType::Json => Response::new(self.ledger_info, &events),
+            AcceptType::Bcs => Response::new_bcs(self.ledger_info, &events),
+        }
     }
 }
--- api/types/src/response.rs
+++ api/types/src/response.rs
@@ -1,10 +1,16 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{Error, LedgerInfo};
+use crate::{
+    mime_types::{BCS, JSON},
+    Error, LedgerInfo,
+};
 use anyhow::Result;
 use serde::Serialize;
-use warp::http::header::{HeaderValue, CONTENT_TYPE};
+use warp::{
+    http::header::{HeaderValue, CONTENT_TYPE},
+    hyper::StatusCode,
+};
 
 pub const X_APTOS_CHAIN_ID: &str = "X-Aptos-Chain-Id";
 pub const X_APTOS_EPOCH: &str = "X-Aptos-Epoch";
@@ -14,6 +20,7 @@ pub const X_APTOS_LEDGER_TIMESTAMP: &str = "X-Aptos-Ledger-TimestampUsec";
 pub struct Response {
     pub ledger_info: LedgerInfo,
     pub body: Vec<u8>,
+    pub is_bcs_response: bool,
 }
 
 impl Response {
@@ -21,6 +28,20 @@ impl Response {
         Ok(Self {
             ledger_info,
             body: serde_json::to_vec(body)?,
+            is_bcs_response: false,
+        })
+    }
+
+    pub fn new_bcs<T: Serialize>(ledger_info: LedgerInfo, body: &T) -> Result<Self, Error> {
+        Ok(Self {
+            ledger_info,
+            body: bcs::to_bytes(body).map_err(|_| {
+                Error::new(
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "data serialization error".to_string(),
+                )
+            })?,
+            is_bcs_response: true,
         })
     }
 }
@@ -30,7 +51,11 @@ impl warp::Reply for Response {
         let mut res = warp::reply::Response::new(self.body.into());
         let headers = res.headers_mut();
 
-        headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+        if self.is_bcs_response {
+            headers.insert(CONTENT_TYPE, HeaderValue::from_static(BCS));
+        } else {
+            headers.insert(CONTENT_TYPE, HeaderValue::from_static(JSON));
+        }
         headers.insert(X_APTOS_CHAIN_ID, (self.ledger_info.chain_id as u16).into());
         headers.insert(
             X_APTOS_LEDGER_VERSION,
