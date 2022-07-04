b6b5a184e1dacb69349f7009fe73f8e97b3217bd
Daniel Porteous <danielporteous1@gmail.com>
2022-07-03 20:07:08 -0700

    [NHC] Cleanup old files related to argument parsing

--- ecosystem/node-checker/src/evaluators/metrics/consensus/proposals.rs
+++ ecosystem/node-checker/src/evaluators/metrics/consensus/proposals.rs
@@ -6,7 +6,7 @@ use super::{
         common::{get_metric, GetMetricResult},
         types::{MetricsEvaluatorError, MetricsEvaluatorInput},
     },
-    types::CATEGORY,
+    CATEGORY,
 };
 use crate::{
     configuration::EvaluatorArgs,
