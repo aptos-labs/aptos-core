042880415ed9deccd7aae4774215a2d08d2b194e
Greg <greg@gnazar.io>
2022-07-05 16:55:58 -0700

    [aptos-rosetta] Add CLI tool for testing construction API

--- crates/aptos-rosetta/src/client.rs
+++ crates/aptos-rosetta/src/client.rs
@@ -5,11 +5,18 @@ use crate::{
     common::EmptyRequest,
     types::{
         AccountBalanceRequest, AccountBalanceResponse, BlockRequest, BlockResponse,
+        ConstructionCombineRequest, ConstructionCombineResponse, ConstructionDeriveRequest,
+        ConstructionDeriveResponse, ConstructionHashRequest, ConstructionMetadataRequest,
+        ConstructionMetadataResponse, ConstructionParseRequest, ConstructionParseResponse,
+        ConstructionPayloadsRequest, ConstructionPayloadsResponse, ConstructionPreprocessRequest,
+        ConstructionPreprocessResponse, ConstructionSubmitRequest, ConstructionSubmitResponse,
         NetworkListResponse, NetworkOptionsResponse, NetworkRequest, NetworkStatusResponse,
+        TransactionIdentifierResponse,
     },
 };
 use aptos_rest_client::aptos_api_types::mime_types::JSON;
 use reqwest::{header::CONTENT_TYPE, Client as ReqwestClient};
+use serde::{de::DeserializeOwned, Serialize};
 use url::Url;
 
 /// Client for testing & interacting with a Rosetta service
@@ -30,63 +37,93 @@ impl RosettaClient {
         &self,
         request: &AccountBalanceRequest,
     ) -> anyhow::Result<AccountBalanceResponse> {
-        let response = self
-            .inner
-            .post(self.address.join("account/balance").unwrap())
-            .header(CONTENT_TYPE, JSON)
-            .body(serde_json::to_string(request)?)
-            .send()
-            .await?;
-
-        self.json(response).await
+        self.make_call("account/balance", request).await
     }
 
     pub async fn block(&self, request: &BlockRequest) -> anyhow::Result<BlockResponse> {
-        let response = self
-            .inner
-            .post(self.address.join("block").unwrap())
-            .header(CONTENT_TYPE, JSON)
-            .body(serde_json::to_string(request)?)
-            .send()
-            .await?;
+        self.make_call("block", request).await
+    }
 
-        self.json(response).await
+    pub async fn combine(
+        &self,
+        request: &ConstructionCombineRequest,
+    ) -> anyhow::Result<ConstructionCombineResponse> {
+        self.make_call("construction/combine", request).await
+    }
+
+    pub async fn derive(
+        &self,
+        request: &ConstructionDeriveRequest,
+    ) -> anyhow::Result<ConstructionDeriveResponse> {
+        self.make_call("construction/derive", request).await
+    }
+
+    pub async fn hash(
+        &self,
+        request: &ConstructionHashRequest,
+    ) -> anyhow::Result<TransactionIdentifierResponse> {
+        self.make_call("construction/hash", request).await
+    }
+
+    pub async fn metadata(
+        &self,
+        request: &ConstructionMetadataRequest,
+    ) -> anyhow::Result<ConstructionMetadataResponse> {
+        self.make_call("construction/hash", request).await
+    }
+
+    pub async fn parse(
+        &self,
+        request: &ConstructionParseRequest,
+    ) -> anyhow::Result<ConstructionParseResponse> {
+        self.make_call("construction/parse", request).await
+    }
+    pub async fn payloads(
+        &self,
+        request: &ConstructionPayloadsRequest,
+    ) -> anyhow::Result<ConstructionPayloadsResponse> {
+        self.make_call("construction/payloads", request).await
+    }
+    pub async fn preprocess(
+        &self,
+        request: &ConstructionPreprocessRequest,
+    ) -> anyhow::Result<ConstructionPreprocessResponse> {
+        self.make_call("construction/preprocess", request).await
+    }
+
+    pub async fn submit(
+        &self,
+        request: &ConstructionSubmitRequest,
+    ) -> anyhow::Result<ConstructionSubmitResponse> {
+        self.make_call("construction/submit", request).await
     }
 
     pub async fn network_list(&self) -> anyhow::Result<NetworkListResponse> {
-        let response = self
-            .inner
-            .post(self.address.join("network/list").unwrap())
-            .header(CONTENT_TYPE, JSON)
-            .body(serde_json::to_string(&EmptyRequest)?)
-            .send()
-            .await?;
-
-        self.json(response).await
+        self.make_call("network/list", &EmptyRequest).await
     }
 
     pub async fn network_options(
         &self,
         request: &NetworkRequest,
     ) -> anyhow::Result<NetworkOptionsResponse> {
-        let response = self
-            .inner
-            .post(self.address.join("network/options").unwrap())
-            .header(CONTENT_TYPE, JSON)
-            .body(serde_json::to_string(request)?)
-            .send()
-            .await?;
-
-        self.json(response).await
+        self.make_call("network/options", request).await
     }
 
     pub async fn network_status(
         &self,
         request: &NetworkRequest,
     ) -> anyhow::Result<NetworkStatusResponse> {
+        self.make_call("network/status", request).await
+    }
+
+    async fn make_call<'a, I: Serialize, O: DeserializeOwned>(
+        &'a self,
+        path: &'static str,
+        request: &'a I,
+    ) -> anyhow::Result<O> {
         let response = self
             .inner
-            .post(self.address.join("network/status").unwrap())
+            .post(self.address.join(path)?)
             .header(CONTENT_TYPE, JSON)
             .body(serde_json::to_string(request)?)
             .send()
