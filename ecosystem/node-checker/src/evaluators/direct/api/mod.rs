43f38fe32973441f226fc33bf3752be95ea41bd5
Daniel Porteous <danielporteous1@gmail.com>
2022-07-05 14:27:51 -0700

    [NHC] Add transaction presence evaluator

--- ecosystem/node-checker/src/evaluators/direct/api/mod.rs
+++ ecosystem/node-checker/src/evaluators/direct/api/mod.rs
@@ -3,14 +3,20 @@
 
 mod latency;
 mod node_identity;
+mod transaction_presence;
 
+use anyhow::Error;
 pub use latency::{LatencyEvaluator, LatencyEvaluatorArgs};
 pub use node_identity::{
     get_node_identity, NodeIdentityEvaluator, NodeIdentityEvaluatorArgs, NodeIdentityEvaluatorError,
 };
 use thiserror::Error as ThisError;
+pub use transaction_presence::{TransactionPresenceEvaluator, TransactionPresenceEvaluatorArgs};
 
 pub const API_CATEGORY: &str = "api";
 
 #[derive(Debug, ThisError)]
-pub enum ApiEvaluatorError {}
+pub enum ApiEvaluatorError {
+    #[error("API returned an error for endpoint {0}: {1}")]
+    EndpointError(String, Error),
+}
