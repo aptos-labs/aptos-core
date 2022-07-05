3b53a78f1f1ca4aadd619c6d58125cc41f6a0dc0
Rustie Lin <rustie117@gmail.com>
2022-07-05 12:44:39 -0700

    [tf][helm] remove helm s3 resources

--- terraform/aptos-node-testnet/outputs.tf
+++ terraform/aptos-node-testnet/outputs.tf
@@ -1,7 +1,3 @@
-output "forge-s3-bucket" {
-  value = length(aws_s3_bucket.aptos-testnet-helm) > 0 ? aws_s3_bucket.aptos-testnet-helm[0].bucket : null
-}
-
 output "oidc_provider" {
   value     = module.validator.oidc_provider
   sensitive = true
