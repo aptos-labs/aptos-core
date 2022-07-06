5567efdcc4c5ca3f437fe17b3b8e5b60df075413
Greg <greg@gnazar.io>
2022-07-05 22:13:12 -0700

    [aptos-rosetta-cli] Add some more inputs for testing

--- crates/aptos-rosetta-cli/src/account.rs
+++ crates/aptos-rosetta-cli/src/account.rs
@@ -1,8 +1,8 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::common::{format_output, NetworkArgs, UrlArgs};
-use aptos_rosetta::types::{AccountBalanceRequest, AccountBalanceResponse};
+use crate::common::{format_output, BlockArgs, NetworkArgs, UrlArgs};
+use aptos_rosetta::types::{AccountBalanceRequest, AccountBalanceResponse, Currency};
 use aptos_types::account_address::AccountAddress;
 use clap::{Parser, Subcommand};
 
@@ -29,22 +29,34 @@ impl AccountCommand {
 pub struct AccountBalanceCommand {
     #[clap(flatten)]
     network_args: NetworkArgs,
-    /// Account to list the balance
-    #[clap(long)]
-    account: AccountAddress,
     #[clap(flatten)]
     url_args: UrlArgs,
+    #[clap(flatten)]
+    block_args: BlockArgs,
+    #[clap(long)]
+    filter_currency: bool,
+    /// Account to list the balance
+    #[clap(long, parse(try_from_str=aptos::common::types::load_account_arg))]
+    account: AccountAddress,
 }
 
 impl AccountBalanceCommand {
     pub async fn execute(self) -> anyhow::Result<AccountBalanceResponse> {
         let client = self.url_args.client();
-        let request = AccountBalanceRequest {
-            network_identifier: self.network_args.network_identifier(),
-            account_identifier: self.account.into(),
-            block_identifier: None,
-            currencies: None,
-        };
-        client.account_balance(&request).await
+        client
+            .account_balance(&AccountBalanceRequest {
+                network_identifier: self.network_args.network_identifier(),
+                account_identifier: self.account.into(),
+                block_identifier: self.block_args.into(),
+                currencies: if self.filter_currency {
+                    Some(vec![Currency {
+                        symbol: "TC".to_string(),
+                        decimals: 6,
+                    }])
+                } else {
+                    None
+                },
+            })
+            .await
     }
 }
