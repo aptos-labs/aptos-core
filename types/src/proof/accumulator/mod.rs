aptos-labs/aptos-core

commit a50eac3dd8132441d5b3d14d501aac23e23163c7

[storage] arb_blocks_to_commit: rely NOT on the DB
We intend to make the DB dump, as in save whatever the upper layer asks to save, potentially losing the ability to calculate any hash. This removes the dependance on the DB's ability to calculate any Merkle hash, instead, the test helper being changed here relies on the in mem merkle tree algorithms directly.

Closes: #10159

types/src/proof/accumulator/mod.rs
@@ -20,7 +20,9 @@ use super::MerkleTreeInternalNode;
 use crate::proof::definition::{LeafCount, MAX_ACCUMULATOR_LEAVES};
 use anyhow::{ensure, format_err, Result};
 use diem_crypto::{
-    hash::{CryptoHash, CryptoHasher, ACCUMULATOR_PLACEHOLDER_HASH},
+    hash::{
+        CryptoHash, CryptoHasher, ACCUMULATOR_PLACEHOLDER_HASH, SPARSE_MERKLE_PLACEHOLDER_HASH,
+    },
     HashValue,
 };
 use serde::{Deserialize, Serialize};
@@ -78,6 +80,15 @@ where
             root_hash,
             phantom: PhantomData,
         })
+    }
+
+    pub fn new_empty() -> Self {
+        Self {
+            frozen_subtree_roots: Vec::new(),
+            num_leaves: 0,
+            root_hash: *SPARSE_MERKLE_PLACEHOLDER_HASH,
+            phantom: PhantomData,
+        }
     }
 
     /// Constructs a new accumulator with given leaves.

types/src/transaction/mod.rs
@@ -993,6 +993,17 @@ impl TransactionInfo {
             status,
         ))
     }
+
+    #[cfg(any(test, feature = "fuzzing"))]
+    pub fn new_placeholder(gas_used: u64, status: KeptVMStatus) -> Self {
+        Self::new(
+            HashValue::default(),
+            HashValue::default(),
+            HashValue::default(),
+            gas_used,
+            status,
+        )
+    }
 }
 
 impl Deref for TransactionInfo {

execution/executor-types/src/lib.rs
@@ -391,6 +391,10 @@ impl ProofReader {
     pub fn new(account_to_proof: HashMap<HashValue, SparseMerkleProof>) -> Self {
         ProofReader { account_to_proof }
     }
+
+    pub fn new_empty() -> Self {
+        Self::new(HashMap::new())
+    }
 }
 
 impl ProofRead<AccountStateBlob> for ProofReader {

storage/diemdb/Cargo.toml
@@ -34,10 +34,12 @@ diem-proptest-helpers = { path = "../../crates/diem-proptest-helpers", optional 
 diem-temppath = { path = "../../crates/diem-temppath", optional = true }
 diem-types = { path = "../../types" }
 diem-workspace-hack = { version = "0.1", path = "../../crates/diem-workspace-hack" }
+executor-types = { path = "../../execution/executor-types", optional = true }
 move-core-types = {git = "https://github.com/diem/move", rev = "98ed299a7e3a9223019c9bdf4dd92fea9faef860"}
 
 num-variants = { path = "../../crates/num-variants" }
 schemadb = { path = "../schemadb" }
+scratchpad = { path = "../scratchpad", optional = true }
 storage-interface = { path = "../storage-interface" }
 
 [dev-dependencies]
@@ -49,9 +51,11 @@ diem-jellyfish-merkle = { path = "../jellyfish-merkle", features = ["fuzzing"] }
 diem-proptest-helpers = { path = "../../crates/diem-proptest-helpers" }
 diem-temppath = { path = "../../crates/diem-temppath" }
 diem-types = { path = "../../types", features = ["fuzzing"] }
+executor-types = { path = "../../execution/executor-types", features = ["fuzzing"] }
 schemadb = { path = "../schemadb", features = ["fuzzing"] }
+scratchpad = { path = "../scratchpad", features = ["fuzzing"] }
 
 [features]
 default = []
 diemsum = []
-fuzzing = ["proptest", "proptest-derive", "diem-proptest-helpers", "diem-temppath", "diem-crypto/fuzzing", "diem-jellyfish-merkle/fuzzing", "diem-types/fuzzing", "schemadb/fuzzing"]
+fuzzing = ["proptest", "proptest-derive", "diem-proptest-helpers", "diem-temppath", "diem-crypto/fuzzing", "diem-jellyfish-merkle/fuzzing", "diem-types/fuzzing", "executor-types/fuzzing", "schemadb/fuzzing", "scratchpad/fuzzing"]

storage/diemdb/src/diemdb_test.rs
@@ -4,8 +4,7 @@
 use super::*;
 #[allow(unused_imports)]
 use crate::{
-    schema::jellyfish_merkle_node::JellyfishMerkleNodeSchema,
-    test_helper::{arb_blocks_to_commit, arb_mock_genesis},
+    schema::jellyfish_merkle_node::JellyfishMerkleNodeSchema, test_helper::arb_blocks_to_commit,
 };
 use diem_crypto::hash::CryptoHash;
 #[allow(unused_imports)]

storage/diemdb/src/test_helper.rs
@@ -1,87 +1,18 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-//! This module provides reusable helpers in tests.
-
+///! This module provides reusable helpers in tests.
 use super::*;
-use diem_crypto::hash::CryptoHash;
-use diem_temppath::TempPath;
+use diem_crypto::hash::{CryptoHash, EventAccumulatorHasher, TransactionAccumulatorHasher};
 use diem_types::{
-    block_info::BlockInfo,
-    ledger_info::{LedgerInfo, LedgerInfoWithSignatures},
-    proptest_types::{AccountInfoUniverse, LedgerInfoGen, TransactionToCommitGen},
-    validator_signer::ValidatorSigner,
+    account_address::HashAccountAddress,
+    ledger_info::LedgerInfoWithSignatures,
+    proof::accumulator::InMemoryAccumulator,
+    proptest_types::{AccountInfoUniverse, BlockGen},
 };
+use executor_types::ProofReader;
 use proptest::{collection::vec, prelude::*};
-
-fn to_blocks_to_commit(
-    partial_blocks: Vec<(Vec<TransactionToCommit>, LedgerInfo, Vec<ValidatorSigner>)>,
-) -> Result<Vec<(Vec<TransactionToCommit>, LedgerInfoWithSignatures)>> {
-    // Use temporary DiemDB and STORE LEVEL APIs to calculate hashes on a per transaction basis.
-    // Result is used to test the batch PUBLIC API for saving everything, i.e. `save_transactions()`
-    let tmp_dir = TempPath::new();
-    let db = DiemDB::new_for_test(&tmp_dir);
-
-    let mut cur_ver = 0;
-    let mut cur_txn_accu_hash = HashValue::zero();
-    let blocks_to_commit = partial_blocks
-        .into_iter()
-        .map(|(mut txns_to_commit, partial_ledger_info, validator_set)| {
-            for txn_to_commit in txns_to_commit.iter_mut() {
-                let mut cs = ChangeSet::new();
-
-                let txn_hash = txn_to_commit.transaction().hash();
-                let state_root_hash = db.state_store.put_account_state_sets(
-                    vec![txn_to_commit.account_states().clone()],
-                    None,
-                    cur_ver,
-                    &mut cs,
-                )?[0];
-                let event_root_hash =
-                    db.event_store
-                        .put_events(cur_ver, txn_to_commit.events(), &mut cs)?;
-
-                let txn_info = TransactionInfo::new(
-                    txn_hash,
-                    state_root_hash,
-                    event_root_hash,
-                    txn_to_commit.gas_used(),
-                    txn_to_commit.status().clone(),
-                );
-                txn_to_commit.set_transaction_info(txn_info.clone());
-                let txn_accu_hash =
-                    db.ledger_store
-                        .put_transaction_infos(cur_ver, &[txn_info], &mut cs)?;
-                db.db.write_schemas(cs.batch)?;
-
-                cur_ver += 1;
-                cur_txn_accu_hash = txn_accu_hash;
-            }
-
-            assert_eq!(cur_ver, partial_ledger_info.version() + 1);
-
-            let block_info = BlockInfo::new(
-                partial_ledger_info.epoch(),
-                partial_ledger_info.round(),
-                partial_ledger_info.consensus_block_id(),
-                cur_txn_accu_hash,
-                partial_ledger_info.version(),
-                partial_ledger_info.timestamp_usecs(),
-                partial_ledger_info.next_epoch_state().cloned(),
-            );
-            let ledger_info =
-                LedgerInfo::new(block_info, partial_ledger_info.consensus_data_hash());
-            let signatures = validator_set
-                .iter()
-                .map(|signer| (signer.author(), signer.sign(&ledger_info)))
-                .collect();
-            let ledger_info_with_sigs = LedgerInfoWithSignatures::new(ledger_info, signatures);
-            Ok((txns_to_commit, ledger_info_with_sigs))
-        })
-        .collect::<Result<Vec<_>>>()?;
-
-    Ok(blocks_to_commit)
-}
+use scratchpad::SparseMerkleTree;
 
 prop_compose! {
     /// This returns a [`proptest`](https://altsysrq.github.io/proptest-book/intro.html)
@@ -90,44 +21,68 @@ prop_compose! {
     ///
     /// It is used in tests for both transaction block committing during normal running and
     /// transaction syncing during start up.
-    pub fn arb_blocks_to_commit_impl(
+    fn arb_blocks_to_commit_impl(
         num_accounts: usize,
-        max_txn_per_block: usize,
+        max_user_txns_per_block: usize,
         max_blocks: usize,
     )(
         mut universe in any_with::<AccountInfoUniverse>(num_accounts).no_shrink(),
-        batches in vec(
-            (
-                vec(any::<TransactionToCommitGen>(), 1..=max_txn_per_block),
-                any::<LedgerInfoGen>(),
-            ),
-            1..=max_blocks,
-        ),
-    ) ->
-        Vec<(
-            Vec<TransactionToCommit>,
-            LedgerInfoWithSignatures,
-        )>
-    {
-        let partial_blocks = batches
-            .into_iter()
-            .map(|(txn_gens, ledger_info_gen)| {
-                let block_size = txn_gens.len();
-                let txns_to_commit = txn_gens
-                        .into_iter()
-                        .map(|gen| gen.materialize(&mut universe))
-                        .collect();
-                let ledger_info = ledger_info_gen.materialize(&mut universe, block_size);
-                let validator_set = universe.get_validator_set(ledger_info.epoch()).to_vec();
-                (
-                    txns_to_commit,
-                    ledger_info,
-                    validator_set,
-                )
-            })
-            .collect();
+        block_gens in vec(any_with::<BlockGen>(max_user_txns_per_block), 1..=max_blocks),
+    ) -> Vec<(Vec<TransactionToCommit>, LedgerInfoWithSignatures)> {
+        type EventAccumulator = InMemoryAccumulator<EventAccumulatorHasher>;
+        type TxnAccumulator = InMemoryAccumulator<TransactionAccumulatorHasher>;
 
-        to_blocks_to_commit(partial_blocks).unwrap()
+        let mut smt = SparseMerkleTree::<AccountStateBlob>::default().freeze();
+        let mut txn_accumulator = TxnAccumulator::new_empty();
+        let mut result = Vec::new();
+
+        for block_gen in block_gens {
+            let (mut txns_to_commit, mut ledger_info) = block_gen.materialize(&mut universe);
+
+            // make real txn_info's
+            for txn in txns_to_commit.iter_mut() {
+                let placeholder_txn_info = txn.transaction_info();
+
+                // calculate event root hash
+                let event_hashes: Vec<_> = txn.events().iter().map(CryptoHash::hash).collect();
+                let event_root_hash = EventAccumulator::from_leaves(&event_hashes).root_hash();
+
+                // calcualte state checkpoint hash
+                let state_checkpoint_hash = if txn.account_states().is_empty() {
+                    None
+                } else {
+                    let updates: Vec<_> = txn.account_states().iter().map(|(addr, blob)| {
+                            ( HashAccountAddress::hash(addr), blob )
+                    }).collect();
+
+                    smt = smt.batch_update(updates, &ProofReader::new_empty()).unwrap();
+
+                    Some(smt.root_hash())
+                };
+
+                let txn_info = TransactionInfo::new(
+                    txn.transaction().hash(),
+                    state_checkpoint_hash.unwrap(),
+                    event_root_hash,
+                    placeholder_txn_info.gas_used(),
+                    placeholder_txn_info.status().clone(),
+                );
+                txn_accumulator = txn_accumulator.append(&[txn_info.hash()]);
+                txn.set_transaction_info(txn_info);
+            }
+
+            // updated ledger info with real root hash and sign
+            ledger_info.set_executed_state_id(txn_accumulator.root_hash());
+            let validator_set = universe.get_validator_set(ledger_info.epoch());
+            let signatures = validator_set
+                .iter()
+                .map(|signer| (signer.author(), signer.sign(&ledger_info)))
+                .collect();
+            let ledger_info_with_sigs = LedgerInfoWithSignatures::new(ledger_info, signatures);
+
+            result.push((txns_to_commit, ledger_info_with_sigs))
+        }
+        result
     }
 }
 
@@ -135,21 +90,7 @@ pub fn arb_blocks_to_commit(
 ) -> impl Strategy<Value = Vec<(Vec<TransactionToCommit>, LedgerInfoWithSignatures)>> {
     arb_blocks_to_commit_impl(
         5,  /* num_accounts */
-        2,  /* max_txn_per_block */
+        2,  /* max_user_txn_per_block */
         10, /* max_blocks */
     )
-}
-
-pub fn arb_mock_genesis() -> impl Strategy<Value = (TransactionToCommit, LedgerInfoWithSignatures)>
-{
-    arb_blocks_to_commit_impl(
-        1, /* num_accounts */
-        1, /* max_txn_per_block */
-        1, /* max_blocks */
-    )
-    .prop_map(|blocks| {
-        let (block, ledger_info_with_sigs) = &blocks[0];
-
-        (block[0].clone(), ledger_info_with_sigs.clone())
-    })
 }

types/src/block_info.rs
@@ -191,6 +191,11 @@ impl BlockInfo {
             && self.executed_state_id == *ACCUMULATOR_PLACEHOLDER_HASH
             && self.version == 0
     }
+
+    #[cfg(any(test, feature = "fuzzing"))]
+    pub fn set_executed_state_id(&mut self, id: HashValue) {
+        self.executed_state_id = id
+    }
 }
 
 impl Display for BlockInfo {

types/src/ledger_info.rs
@@ -127,6 +127,11 @@ impl LedgerInfo {
     pub fn set_consensus_data_hash(&mut self, consensus_data_hash: HashValue) {
         self.consensus_data_hash = consensus_data_hash;
     }
+
+    #[cfg(any(test, feature = "fuzzing"))]
+    pub fn set_executed_state_id(&mut self, id: HashValue) {
+        self.commit_info.set_executed_state_id(id)
+    }
 }
 
 /// Wrapper around LedgerInfoWithScheme to support future upgrades, this is the data being persisted.

commit 9f7a28af9df7d642197ed3161b4d2a0e29a8f123

[bug] Wrong in-mem accumulator placeholder_hash
`new_empty` function introduced in https://github.com/aptos-labs/aptos-core/commit/a50eac3dd8132441d5b3d14d501aac23e23163c7.
I don't know who reviewed this in Feb.

types/src/proof/accumulator/mod.rs
@@ -20,9 +20,7 @@ use super::MerkleTreeInternalNode;
 use crate::proof::definition::{LeafCount, MAX_ACCUMULATOR_LEAVES};
 use anyhow::{ensure, format_err, Result};
 use aptos_crypto::{
-    hash::{
-        CryptoHash, CryptoHasher, ACCUMULATOR_PLACEHOLDER_HASH, SPARSE_MERKLE_PLACEHOLDER_HASH,
-    },
+    hash::{CryptoHash, CryptoHasher, ACCUMULATOR_PLACEHOLDER_HASH},
     HashValue,
 };
 use serde::{Deserialize, Serialize};
@@ -86,7 +84,7 @@ where
         Self {
             frozen_subtree_roots: Vec::new(),
             num_leaves: 0,
-            root_hash: *SPARSE_MERKLE_PLACEHOLDER_HASH,
+            root_hash: *ACCUMULATOR_PLACEHOLDER_HASH,
             phantom: PhantomData,
         }
     }