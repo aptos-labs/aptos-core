5ef8a3203a60dfb3368bb3d492a488eab59070ec
Greg <greg@gnazar.io>
2022-07-06 23:13:21 -0700

    [aptos-rosetta] fix spacing

--- crates/aptos-rosetta/src/types/move_types.rs
+++ crates/aptos-rosetta/src/types/move_types.rs
@@ -25,21 +25,27 @@ pub fn create_account_identifier() -> Identifier {
 pub fn test_coin_identifier() -> Identifier {
     ident_str!("TestCoin").into()
 }
+
 pub fn sequence_number_identifier() -> Identifier {
     ident_str!("sequence_number").into()
 }
+
 pub fn deposit_events_identifier() -> Identifier {
     ident_str!("deposit_events").into()
 }
+
 pub fn withdraw_events_identifier() -> Identifier {
     ident_str!("withdraw_events").into()
 }
+
 pub fn transfer_identifier() -> Identifier {
     ident_str!("transfer").into()
 }
+
 pub fn decimals_identifier() -> Identifier {
     ident_str!("decimals").into()
 }
+
 pub fn symbol_identifier() -> Identifier {
     ident_str!("symbol").into()
 }
