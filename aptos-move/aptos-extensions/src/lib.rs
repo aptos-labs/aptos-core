2a99db90faa7ad8af4b2e1f216f01fc250895e65
George Mitenkov <george@aptoslabs.com>
2022-07-08 19:46:54 +0200

    [Framework] Addition support + improvements

--- aptos-move/aptos-extensions/src/lib.rs
+++ aptos-move/aptos-extensions/src/lib.rs
@@ -2,13 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use better_any::{Tid, TidAble};
-use tiny_keccak::{Hasher, Sha3};
-
 use move_deps::{
-    move_binary_format::errors::PartialVMResult,
+    move_binary_format::errors::{PartialVMResult, PartialVMError},
     move_core_types::{
         account_address::AccountAddress,
         gas_schedule::GasCost,
+        vm_status::StatusCode,
     },
     move_vm_runtime::{
         native_functions,
@@ -17,41 +16,162 @@ use move_deps::{
     move_vm_types::{
         loaded_data::runtime_types::Type,
         natives::function::NativeResult,
-        values::{Value, IntegerValue},
+        pop_arg,
+        values::{IntegerValue, Reference, StructRef, Value},
     },
 };
 use smallvec::smallvec;
 use std::cell::RefCell;
 use std::collections::{BTreeMap, VecDeque};
+use tiny_keccak::{Hasher, Sha3};
 
-enum AggregatorValue {
-    Data(IntegerValue),
-    Delta(IntegerValue),
+trait CheckedAdd: Sized {
+    fn checked_add(&self, v: &Self) -> Option<Self>;
 }
 
-type AggregatorHandle = u128;
-
-#[derive(Tid)]
-pub struct NativeAggregatorContext {
-    txn_hash: u128,
-    handles: RefCell<BTreeMap<AggregatorHandle, AggregatorValue>>,
+impl CheckedAdd for u8 {
+    #[inline]
+    fn checked_add(&self, v: &u8) -> Option<u8> {
+        u8::checked_add(*self, *v)
+    }
 }
 
-impl NativeAggregatorContext {
-    pub fn new(txn_hash: u128) -> Self {
-        Self {
-            txn_hash,
-            handles: RefCell::new(BTreeMap::new()),
+impl CheckedAdd for u64 {
+    #[inline]
+    fn checked_add(&self, v: &u64) -> Option<u64> {
+        u64::checked_add(*self, *v)
+    }
+}
+
+impl CheckedAdd for u128 {
+    #[inline]
+    fn checked_add(&self, v: &u128) -> Option<u128> {
+        u128::checked_add(*self, *v)
+    }
+}
+
+/// Error code for overflow.
+const E_OVERFLOW: u64 = 1500;
+
+/// Value of a single aggregator instance. `Data(val)` means that the value is
+/// known and equals to `val`, `PositiveDelta(val)` means that the actual value
+/// is not known and `val` should be added. `NegativeDelta(val)` is similar but
+/// `val` should be subtracted instead.
+enum AggregatorValue<T: CheckedAdd + PartialOrd> {
+    Data(T),
+    NegativeDelta(T),
+    PositiveDelta(T),
+}
+
+impl<T> AggregatorValue<T>
+where
+    T: CheckedAdd + PartialOrd
+{
+    /// Returns a new aggrerated value or error on overflow.
+    fn checked_add(&mut self, v: &T) -> PartialVMResult<()> {
+        *self = match self {
+            AggregatorValue::Data(u) => {
+                let result = u.checked_add(v);
+                if result.is_some() {
+                    AggregatorValue::Data(result.unwrap())
+                } else {
+                    return Err(partial_error(
+                        "aggregator data overflowed",
+                        E_OVERFLOW,
+                    ));
+                }
+            }
+            AggregatorValue::PositiveDelta(u) => {
+                let result = u.checked_add(v);
+                if result.is_some() {
+                    AggregatorValue::PositiveDelta(result.unwrap())
+                } else {
+                    return Err(partial_error(
+                        "aggregator delta overflowed",
+                        E_OVERFLOW,
+                    ));
+                }
+            }
+            _ => unimplemented!() 
+        };
+        Ok(())
+    }
+}
+
+/// Typed aggregator instance.
+enum Aggregator {
+    AggregatorU8(AggregatorValue<u8>),
+    AggregatorU64(AggregatorValue<u64>),
+    AggregatorU128(AggregatorValue<u128>),
+}
+
+impl Aggregator {
+    /// Creates aggregator with a known value, initialized to 0.
+    fn new_known(ty: Type) -> Self {
+        match ty {
+            Type::U8 => Aggregator::AggregatorU8(AggregatorValue::Data(0)),
+            Type::U64 => Aggregator::AggregatorU64(AggregatorValue::Data(0)),
+            Type::U128 => Aggregator::AggregatorU128(AggregatorValue::Data(0)),
+            _ => unreachable!(),
+        }
+    }
+
+    /// Creates aggregator with a unknown value.
+    fn new_unknown(ty: Type) -> Self {
+        match ty {
+            Type::U8 => Aggregator::AggregatorU8(AggregatorValue::PositiveDelta(0)),
+            Type::U64 => Aggregator::AggregatorU64(AggregatorValue::PositiveDelta(0)),
+            Type::U128 => Aggregator::AggregatorU128(AggregatorValue::PositiveDelta(0)),
+            _ => unreachable!(),
+        }
+    }
+
+    /// Adds a value to aggregator.
+    fn add(&mut self, value: IntegerValue) -> PartialVMResult<()> {
+        match (self, value) {
+            (Aggregator::AggregatorU8(a), IntegerValue::U8(v)) => a.checked_add(&v),
+            (Aggregator::AggregatorU64(a), IntegerValue::U64(v)) => a.checked_add(&v),
+            (Aggregator::AggregatorU128(a), IntegerValue::U128(v)) => a.checked_add(&v),
+            _ => unreachable!()
         }
     }
 }
 
+/// Every aggregator instance is uniquely specified by 128-bit handle.
+type AggregatorHandle = u128;
+const HANDLE_FIELD_IDX: usize = 0;
+
+/// Native context of aggregator that can be passed to VM extensions.
+/// Aggregators are wrapped in RefCell for internal mutability.
+#[derive(Tid)]
+pub struct NativeAggregatorContext {
+    txn_hash: u128,
+    aggregators: RefCell<BTreeMap<AggregatorHandle, Aggregator>>,
+}
+
+impl NativeAggregatorContext {
+    /// Creates new aggregator context.
+    pub fn new(txn_hash: u128) -> Self {
+        Self {
+            // TODO: add resolver once we suppord read and need to go to
+            // global storage!
+            txn_hash,
+            aggregators: RefCell::new(BTreeMap::new()),
+        }
+    }
+}
+
+// ================================= Natives =================================
+
+/// All aggregator native functions. For more details, refer to Aggregator.move
+/// code.
 pub fn aggregator_natives(aggregator_addr: AccountAddress) -> NativeFunctionTable {
     native_functions::make_table(
         aggregator_addr, 
         &[
             ("Aggregator", "is_integral", native_is_integral),
             ("Aggregator", "new_handle", native_new_handle),
+            ("Aggregator", "add", native_add),
         ],
     )
 }
@@ -65,52 +185,141 @@ fn native_is_integral(
     assert!(args.is_empty());
 
     let result = match ty_args[0] {
-        Type::U8 => true,
-        Type::U64 => true,
-        Type::U128 => true,
-        _ => false
+        Type::U8 | Type::U64 | Type::U128 => true,
+        _ => false,
     };
 
-    // Type check should not be charged for gas.
+    // TODO: charge gas properly.
     let cost = GasCost::new(0, 0).total();
+
     Ok(NativeResult::ok(cost, smallvec![Value::bool(result)]))
 }
 
 fn native_new_handle(
     context: &mut NativeContext,
-    ty_args: Vec<Type>,
+    mut ty_args: Vec<Type>,
     args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
     assert!(ty_args.len() == 1);
     assert!(args.is_empty());
 
-    let context = context.extensions().get::<NativeAggregatorContext>();
-    let mut handles = context.handles.borrow_mut();
+    let aggregator_context = context.extensions().get::<NativeAggregatorContext>();
+    let mut aggregators = aggregator_context.aggregators.borrow_mut();
 
     // Every Aggregator instance has a unique handle. Here we can reuse the
     // strategy from Table implementation: taking hash of transaction and
-    // number of aggregator instances created so far and truncating
-    let txn_hash_buffer = context.txn_hash.to_be_bytes();
-    let num_handles_buffer = handles.len().to_be_bytes();
+    // number of aggregator instances created so far and truncating them to
+    // 128 bits.
+    let txn_hash_buffer = aggregator_context.txn_hash.to_be_bytes();
+    let num_aggregators_buffer = aggregators.len().to_be_bytes();
 
     let mut sha3 = Sha3::v256();
     sha3.update(&txn_hash_buffer);
-    sha3.update(&num_handles_buffer);
+    sha3.update(&num_aggregators_buffer);
 
     let mut handle_bytes = [0_u8; 16];
     sha3.finalize(&mut handle_bytes);
     let handle = u128::from_be_bytes(handle_bytes);
 
-    // Aggregator is initialized as delta of 0.
-    let zero = match ty_args[0] {
-        Type::U8 => IntegerValue::U8(0),
-        Type::U64 => IntegerValue::U64(0),
-        Type::U128 => IntegerValue::U128(0),
-        _ => unreachable!()
-    };
-    handles.insert(handle, AggregatorValue::Delta(zero));
+    // If transaction initializes aggregator, then the actual value is known,
+    // and Data(0) is produced.
+    let int_ty = ty_args.pop().unwrap();
+    aggregators.insert(handle, Aggregator::new_known(int_ty));
 
-    // TODO: set the cost accordingly!
+    // TODO: charge gas properly.
     let cost = GasCost::new(0, 0).total();
+
     Ok(NativeResult::ok(cost, smallvec![Value::u128(handle)]))
+}
+
+fn native_add(
+    context: &mut NativeContext,
+    mut ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    assert!(ty_args.len() == 1);
+    assert!(args.len() == 2);
+
+    let aggregator_context = context.extensions().get::<NativeAggregatorContext>();
+    let mut aggregators = aggregator_context.aggregators.borrow_mut();
+
+    let value = pop_arg!(args, IntegerValue);
+    let handle = get_handle(&pop_arg!(args, StructRef))?;
+
+    // If aggregator has not been initialized in this context, create one
+    // with unkown value, i.e. with Delta(0).
+    if !aggregators.contains_key(&handle) {
+        let int_ty = ty_args.pop().unwrap();
+        aggregators.insert(handle, Aggregator::new_unknown(int_ty));
+    }
+
+    // Add a value to aggregator, and check for errors. 
+    let aggregator = aggregators.get_mut(&handle).unwrap();
+    if let Err(err) = aggregator.add(value) {
+        return Err(err);
+    }
+
+    // TODO: charge gas properly.
+    let cost = GasCost::new(0, 0).total();
+
+    Ok(NativeResult::ok(cost, smallvec![]))
+}
+
+/// Returns 128-bit handle of Move aggregator struct reference.
+fn get_handle(aggregator: &StructRef) -> PartialVMResult<AggregatorHandle> {
+    let field_ref =
+        aggregator
+            .borrow_field(HANDLE_FIELD_IDX)?
+            .value_as::<Reference>()?;
+    field_ref.read_ref()?.value_as::<AggregatorHandle>()
+}
+
+fn partial_error(message: impl ToString, code: u64) -> PartialVMError {
+    PartialVMError::new(StatusCode::ABORTED)
+        .with_message(message.to_string())
+        .with_sub_status(code)
+}
+
+// ================================== Tests ==================================
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "overflow")]
+    fn test_aggregator_data_overflow() {
+        let mut a = Aggregator::new_known(Type::U8);
+        _ = a.add(IntegerValue::U8(200));
+        let status = a.add(IntegerValue::U8(200));
+
+        if status.is_err() {
+            panic!("overflow")
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "overflow")]
+    fn test_aggregator_delta_overflow() {
+        let mut a = Aggregator::new_unknown(Type::U8);
+        _ = a.add(IntegerValue::U8(200));
+        let status = a.add(IntegerValue::U8(200));
+
+        if status.is_err() {
+            panic!("overflow")
+        }
+    }
+
+    #[test]
+    fn test_aggregator_can_add() {
+        let mut a_unkwon = Aggregator::new_unknown(Type::U8);
+        _ = a_unkwon.add(IntegerValue::U8(1));
+        let status = a_unkwon.add(IntegerValue::U8(2));
+        assert!(status.is_ok());
+
+        // TODO: what would be a better way to test this?
+        if let Aggregator::AggregatorU8(AggregatorValue::PositiveDelta(v)) = a_unkwon {
+            assert!(v == 3)
+        }
+    }
 }
--- aptos-move/framework/aptos-framework/sources/Aggregator.move
+++ aptos-move/framework/aptos-framework/sources/Aggregator.move
@@ -1,8 +1,11 @@
 module AptosFramework::Aggregator {
     use Std::Errors;
 
-    /// Aggregator type paramter is non-integral.
+    /// Aggregator type parameter is non-integral.
     const E_NON_INTEGRAL_TYPE: u64 = 0;
+
+    // Raised by native code.
+    const E_OVERFLOW: u64 = 1500;
 
     struct Aggregator<phantom IntTy> has drop, store {
         handle: u128,
@@ -11,9 +14,15 @@ module AptosFramework::Aggregator {
     /// Creates a new Aggregator instance, zero-initialized.
     public fun new<IntTy>(): Aggregator<IntTy> {
         assert!(is_integral<IntTy>(), Errors::invalid_argument(E_NON_INTEGRAL_TYPE));
-        Aggregator { handle: new_handle<IntTy>() }
+        Aggregator {
+            handle: new_handle<IntTy>(),
+        }
     }
 
+    /// Adds `value` to aggregator. Aborts on overflow as per `IntTy` semantics.
+    native public fun add<IntTy>(aggregator: &mut Aggregator<IntTy>, value: IntTy);
+
+    /// Private native functions for setting up a new Aggregator.
     native fun is_integral<IntTy>(): bool;
     native fun new_handle<IntTy>(): u128;
 
@@ -39,4 +48,11 @@ module AptosFramework::Aggregator {
         let _a3 = new<u128>();
     }
 
+    #[test]
+    #[expected_failure(abort_code = 1500)]
+    fun aggregator_add_overflow_test() {
+        let a = new<u8>();
+        add(&mut a, 100);
+        add(&mut a, 200);
+    }
 }
