2aba0f88b81c39b07f450a1766033c92c251f4e3
Greg Nazario <greg@gnazar.io>
2022-07-12 15:37:45 -0700

    [aptos-rosetta] Run fullnode alongside rosetta

--- crates/aptos-rosetta/src/main.rs
+++ crates/aptos-rosetta/src/main.rs
@@ -4,6 +4,7 @@
 #![forbid(unsafe_code)]
 
 use aptos_config::config::ApiConfig;
+use aptos_node::AptosNodeArgs;
 use aptos_rosetta::bootstrap;
 use aptos_types::chain_id::ChainId;
 use clap::Parser;
@@ -13,15 +14,58 @@ use std::{
         atomic::{AtomicBool, Ordering},
         Arc,
     },
+    thread,
+    time::Duration,
 };
+use tokio::time::Instant;
+
+const REST_API_WAIT_DURATION_MS: u64 = 60;
+const TOTAL_REST_API_WAIT_DURATION_S: u64 = 300;
 
 #[tokio::main]
 async fn main() {
-    aptos_logger::Logger::new().init();
     let args: CommandArgs = CommandArgs::parse();
 
+    // If we're in online mode, we run a full node side by side
+    let _maybe_node = if let CommandArgs::Online(OnlineLocalArgs {
+        ref node_args,
+        ref online_args,
+    }) = args
+    {
+        let node_args = node_args.clone();
+        let runtime = thread::spawn(move || node_args.run());
+
+        // Wait and ensure the node is running on the URL
+        let client = aptos_rest_client::Client::new(online_args.rest_api_url.clone());
+        let mut successful = false;
+        let total_wait_duration = Duration::from_secs(TOTAL_REST_API_WAIT_DURATION_S);
+        let start = Instant::now();
+        while start.elapsed() < total_wait_duration {
+            if client.get_index().await.is_ok() {
+                successful = true;
+                break;
+            }
+
+            tokio::time::sleep(Duration::from_millis(REST_API_WAIT_DURATION_MS)).await;
+        }
+
+        // If it didn't start up, we need to crash
+        if !successful {
+            panic!(
+                "Node didn't start up on time after {} seconds",
+                TOTAL_REST_API_WAIT_DURATION_S
+            )
+        }
+
+        Some(runtime)
+    } else {
+        // Offline mode we just need to start up the logger
+        aptos_logger::Logger::new().init();
+        None
+    };
+
     // Ensure runtime for Rosetta is up and running
-    let _runtime = bootstrap(args.chain_id(), args.api_config(), args.rest_client())
+    let _rosetta = bootstrap(args.chain_id(), args.api_config(), args.rest_client())
         .expect("Should bootstrap");
 
     // Run until there is an interrupt
@@ -50,7 +94,9 @@ trait ServerArgs {
 #[clap(name = "aptos-rosetta", author, version, propagate_version = true)]
 pub enum CommandArgs {
     /// Run a local online server that connects to a fullnode endpoint
-    Online(OnlineArgs),
+    OnlineRemote(OnlineRemoteArgs),
+    /// Run a local online server and a node that connects
+    Online(OnlineLocalArgs),
     /// Run a local online server that doesn't connect to a fullnode endpoint
     Offline(OfflineArgs),
 }
@@ -58,22 +104,25 @@ pub enum CommandArgs {
 impl ServerArgs for CommandArgs {
     fn api_config(&self) -> ApiConfig {
         match self {
-            CommandArgs::Online(args) => args.api_config(),
+            CommandArgs::OnlineRemote(args) => args.api_config(),
             CommandArgs::Offline(args) => args.api_config(),
+            CommandArgs::Online(args) => args.api_config(),
         }
     }
 
     fn rest_client(&self) -> Option<aptos_rest_client::Client> {
         match self {
-            CommandArgs::Online(args) => args.rest_client(),
+            CommandArgs::OnlineRemote(args) => args.rest_client(),
             CommandArgs::Offline(args) => args.rest_client(),
+            CommandArgs::Online(args) => args.rest_client(),
         }
     }
 
     fn chain_id(&self) -> ChainId {
         match self {
-            CommandArgs::Online(args) => args.chain_id(),
+            CommandArgs::OnlineRemote(args) => args.chain_id(),
             CommandArgs::Offline(args) => args.chain_id(),
+            CommandArgs::Online(args) => args.chain_id(),
         }
     }
 }
@@ -118,15 +167,15 @@ impl ServerArgs for OfflineArgs {
 }
 
 #[derive(Debug, Parser)]
-pub struct OnlineArgs {
+pub struct OnlineRemoteArgs {
     #[clap(flatten)]
     offline_args: OfflineArgs,
     /// URL for the Aptos REST API. e.g. https://fullnode.devnet.aptoslabs.com
-    #[clap(long, default_value = "https://fullnode.devnet.aptoslabs.com")]
+    #[clap(long, default_value = "https://localhost:8080")]
     rest_api_url: url::Url,
 }
 
-impl ServerArgs for OnlineArgs {
+impl ServerArgs for OnlineRemoteArgs {
     fn api_config(&self) -> ApiConfig {
         self.offline_args.api_config()
     }
@@ -137,5 +186,29 @@ impl ServerArgs for OnlineArgs {
 
     fn chain_id(&self) -> ChainId {
         self.offline_args.chain_id
+    }
+}
+
+#[derive(Debug, Parser)]
+pub struct OnlineLocalArgs {
+    #[clap(flatten)]
+    online_args: OnlineRemoteArgs,
+    #[clap(flatten)]
+    node_args: AptosNodeArgs,
+}
+
+impl ServerArgs for OnlineLocalArgs {
+    fn api_config(&self) -> ApiConfig {
+        self.online_args.offline_args.api_config()
+    }
+
+    fn rest_client(&self) -> Option<aptos_rest_client::Client> {
+        Some(aptos_rest_client::Client::new(
+            self.online_args.rest_api_url.clone(),
+        ))
+    }
+
+    fn chain_id(&self) -> ChainId {
+        self.online_args.offline_args.chain_id
     }
 }
