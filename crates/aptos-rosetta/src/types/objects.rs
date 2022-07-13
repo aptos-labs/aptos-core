f66d2c465a37835432bcbbc4b81eb9b608b08f2c
Greg Nazario <greg@gnazar.io>
2022-07-12 20:03:19 -0700

    [aptos-rosetta] Address TODOs around performance and features

--- crates/aptos-rosetta/src/types/objects.rs
+++ crates/aptos-rosetta/src/types/objects.rs
@@ -531,7 +531,6 @@ impl Transaction {
         };
 
         for change in &txn_info.changes {
-            // TODO: Handle delete resource ?
             if let WriteSetChange::WriteResource { address, data, .. } = change {
                 // Determine operation
                 let address = *address.inner();
@@ -614,7 +613,6 @@ impl Transaction {
                     None
                 };
 
-                // TODO: support coin creation?
                 match op_details {
                     Some(OperationDetails::CreateAccount) => {
                         operations.push(Operation::create_account(
