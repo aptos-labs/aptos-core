a8cf633b38d5cd9341e0e643ea8316d1d7a7c01e
Zach Denton <z@chdenton.com>
2022-07-14 11:24:06 -0400

    Update header to new design.

--- ecosystem/platform/server/app/components/header_component.rb
+++ ecosystem/platform/server/app/components/header_component.rb
@@ -5,12 +5,40 @@
 
 class HeaderComponent < ViewComponent::Base
   NavItem = Struct.new(:url, :name, :title)
-  NAV_ITEMS = [
-    NavItem.new('/it1', 'AIT1', 'Incentivized Testnet 1 Results'),
-    NavItem.new('/it2', 'AIT2', 'Incentivized Testnet 2'),
-    NavItem.new('https://aptos.dev/', 'Docs', 'Aptos Docs'),
-    NavItem.new('https://forum.aptoslabs.com/', 'Forum', 'Aptos Forum'),
-    NavItem.new('https://explorer.devnet.aptos.dev/', 'Explorer', 'Aptos Explorer')
+  NavGroup = Struct.new(:item, :children)
+
+  NAV_GROUPS = [
+    NavGroup.new(
+      NavItem.new('/', 'Community', 'Aptos Community'),
+      [
+        NavItem.new('/it1', 'AIT1', 'Incentivized Testnet 1 Results'),
+        NavItem.new('/it2', 'AIT2', 'Incentivized Testnet 2'),
+        NavItem.new('https://forum.aptoslabs.com/', 'Forum', 'Aptos Forum')
+      ]
+    ),
+    NavGroup.new(
+      NavItem.new('https://aptoslabs.com/developers', 'Developers', 'Aptos Developers'),
+      [
+        NavItem.new('https://aptos.dev/', 'Documentation', 'Aptos Documentation')
+      ]
+    ),
+    NavGroup.new(
+      NavItem.new('#', 'Network', 'Aptos Network'),
+      [
+        NavItem.new('https://explorer.devnet.aptos.dev/', 'Explorer', 'Aptos Explorer'),
+        NavItem.new('https://status.devnet.aptos.dev/', 'Network Status', 'Aptos Network Status')
+      ]
+    ),
+    NavGroup.new(
+      NavItem.new('#', 'About', 'About Aptos'),
+      [
+        NavItem.new('https://aptoslabs.com/careers', 'Careers', 'Aptos Careers')
+      ]
+    ),
+    NavGroup.new(
+      NavItem.new('https://aptoslabs.com/currents', 'Currents', 'Aptos Currents'),
+      []
+    )
   ].freeze
 
   USER_NAV_ITEMS = [
@@ -22,7 +50,7 @@ class HeaderComponent < ViewComponent::Base
     @user = user
     @rest = rest
     @rest[:class] = [
-      'bg-black text-white flex px-4 sm:px-6 items-center sticky top-0 z-10',
+      'bg-neutral-900 border-b border-black text-white flex px-4 sm:px-6 items-center sticky top-0 z-10',
       'flex-wrap gap-4',
       @rest[:class]
     ]
@@ -30,8 +58,8 @@ class HeaderComponent < ViewComponent::Base
     @rest[:data][:controller] = 'header'
   end
 
-  def nav_items
-    NAV_ITEMS
+  def nav_groups
+    NAV_GROUPS
   end
 
   def user_nav_items
