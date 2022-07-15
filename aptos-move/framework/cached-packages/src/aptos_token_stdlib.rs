d20e1ea269bd6c03996461e497e0d18e75890b76
Bo Wu <bo@aptoslabs.com>
2022-07-15 14:39:23 -0700

    [Token] Separate the token package from core-framework

---
+++ aptos-move/framework/cached-packages/src/aptos_token_stdlib.rs
@@ -0,0 +1,9 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(unused_imports)]
+
+include!(concat!(
+    concat!(env!("OUT_DIR"), "/token"),
+    "/transaction_script_builder.rs"
+));
