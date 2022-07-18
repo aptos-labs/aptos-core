25031dcd9f544e1583f84f635dff0952129f7e3c
Greg <greg@gnazar.io>
2022-07-18 14:48:29 -0700

    [aptos-genesis] Fix Validator builder secure storage path
    The path was wrong, which was making it crash when safety
    rules finally tried to write or load from storage.  This
    now ensures that it can write to an existing path that is
    in the correct place.

--- crates/aptos-genesis/src/builder.rs
+++ crates/aptos-genesis/src/builder.rs
@@ -39,7 +39,6 @@ const VFN_IDENTITY: &str = "vfn-identity.yaml";
 const PRIVATE_IDENTITY: &str = "private-identity.yaml";
 const CONFIG_FILE: &str = "node.yaml";
 const GENESIS_BLOB: &str = "genesis.blob";
-const SECURE_DATA: &str = "secure-data.json";
 
 /// Configuration to run a local validator node
 #[derive(Debug, Clone)]
@@ -546,7 +545,8 @@ impl Builder {
         // Ensure safety rules runs in a thread
         config.consensus.safety_rules.service = SafetyRulesService::Thread;
         let mut storage = OnDiskStorageConfig::default();
-        storage.set_data_dir(validator.dir.join(SECURE_DATA));
+        storage.set_data_dir(validator.dir.clone());
+
         config.consensus.safety_rules.backend = SecureBackend::OnDiskStorage(storage);
 
         if index > 0 || self.randomize_first_validator_ports {
