2d2ada31550d407f7da897b750676bb404200ea5
Sital Kedia <skedia@aptoslabs.com>
2022-07-18 16:34:11 -0700

    [forge] Tune TPS threshold and latency for forge

--- testsuite/run_forge.sh
+++ testsuite/run_forge.sh
@@ -13,8 +13,8 @@
 pwd | grep -qE 'aptos-core$' || (echo "Please run from aptos-core root directory" && exit 1)
 
 # for calculating regression
-TPS_THRESHOLD=4000
-P99_LATENCY_MS_THRESHOLD=5000
+TPS_THRESHOLD=5000
+P99_LATENCY_MS_THRESHOLD=6000
 
 FORGE_OUTPUT=${FORGE_OUTPUT:-forge_output.txt}
 FORGE_REPORT=${FORGE_REPORT:-forge_report.json}
