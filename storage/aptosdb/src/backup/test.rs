d3a9ecbf1d60e172ad02c9cb41a18557a8d67a22
Aaron Gao <lightmark@gmail.com>
2022-07-19 10:09:41 -0700

    [Storage] StateMerkleDB Lazy Commit

--- storage/aptosdb/src/backup/test.rs
+++ storage/aptosdb/src/backup/test.rs
@@ -18,8 +18,8 @@ proptest! {
     fn test_get_transaction_iter(input in arb_blocks_to_commit()) {
         let tmp_dir = TempPath::new();
         let db = AptosDB::new_for_test(&tmp_dir);
-        let mut in_memory_state = (*db.state_store.buffered_state().lock()).clone();
-        let _ancester = in_memory_state.current.clone().freeze();
+        let mut in_memory_state = db.state_store.buffered_state().lock().current_state().clone();
+        let _ancestor = in_memory_state.base.clone();
         let mut cur_ver: Version = 0;
         for (txns_to_commit, ledger_info_with_sigs) in input.iter() {
             update_in_memory_state(&mut in_memory_state, txns_to_commit.as_slice());
