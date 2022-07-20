04a17eda07916ea35ffd4db3bfc560299075f9b9
Junkil Park <jkpark@fb.com>
2022-07-20 16:15:59 -0700

    [Aptos CLI][Move Prover] Add the subcommand to run Move Prover in Aptos CLI (#2086)
    Enabled the Prover command in Aptos CLI (`$ aptos move prove`)
    
    Added a test package (`hello_prover`) and a test script in `move-examples`

---
+++ aptos-move/move-examples/tests/move_prover_tests.rs
@@ -0,0 +1,45 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use aptos_types::account_address::AccountAddress;
+use move_deps::move_cli::base::prove::run_move_prover;
+use move_deps::move_prover;
+use std::{collections::BTreeMap, path::PathBuf};
+use tempfile::tempdir;
+
+pub fn path_in_crate<S>(relative: S) -> PathBuf
+where
+    S: Into<String>,
+{
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push(relative.into());
+    path
+}
+
+pub fn run_prover_for_pkg(
+    path_to_pkg: impl Into<String>,
+    named_addr: BTreeMap<String, AccountAddress>,
+) {
+    let pkg_path = path_in_crate(path_to_pkg);
+    let config = move_deps::move_package::BuildConfig {
+        additional_named_addresses: named_addr,
+        test_mode: true,
+        install_dir: Some(tempdir().unwrap().path().to_path_buf()),
+        ..Default::default()
+    };
+    run_move_prover(
+        config,
+        &pkg_path,
+        &None,
+        true,
+        move_prover::cli::Options::default(),
+    )
+    .unwrap();
+}
+
+#[ignore] // TODO: Ignored because Prover dependencies are not installed in CI.
+#[test]
+fn test_hello_prover() {
+    let named_address = BTreeMap::new();
+    run_prover_for_pkg("hello_prover", named_address);
+}
