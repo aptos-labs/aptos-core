827a07870df4e4cb916f5d2346b91d062633e050
Raj Karamchedu <raj@aptoslabs.com>
2022-07-20 16:28:25 -0700

    [Docs] Yarn fmt

--- developer-docs-site/sidebars.js
+++ developer-docs-site/sidebars.js
@@ -127,7 +127,7 @@ const sidebars = {
           label: "Typescript SDK",
           href: "https://aptos-labs.github.io/ts-sdk-doc/",
         },
-        "sdks/aptos-sdk-overview", 
+        "sdks/aptos-sdk-overview",
         "sdks/transactions-with-ts-sdk",
       ],
     },
