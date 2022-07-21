bf26ff4b51d8fc67f1100458ae06145d7dbf9c9c
daoauth <57783762+daoauth@users.noreply.github.com>
2022-07-21 14:40:10 +0900

    remove bip39-light

--- ecosystem/typescript/sdk/src/aptos_account.ts
+++ ecosystem/typescript/sdk/src/aptos_account.ts
@@ -1,10 +1,10 @@
-import * as Nacl from 'tweetnacl';
-import * as SHA3 from 'js-sha3';
-import bip39 from 'bip39-light';
-import { Buffer } from 'buffer/'; // the trailing slash is important!
-import { derivePath } from 'ed25519-hd-key';
-import { HexString, MaybeHexString } from './hex_string';
-import { Types } from './types';
+import * as Nacl from "tweetnacl";
+import * as SHA3 from "js-sha3";
+import * as bip39 from "@scure/bip39";
+import { Buffer } from "buffer/"; // the trailing slash is important!
+import { derivePath } from "ed25519-hd-key";
+import { HexString, MaybeHexString } from "./hex_string";
+import { Types } from "./types";
 
 export interface AptosAccountObject {
   address?: string;
@@ -35,32 +35,32 @@ export class AptosAccount {
   static isValidPath = (path: string): boolean => {
     if (
       // eslint-disable-next-line prefer-regex-literals
-      !new RegExp('^m\\/44+\'\\/637+\'\\/[0-9]+\'\\/[0-9]+\'\\/[0-9]+\'+$').test(
+      !new RegExp("^m\\/44+'\\/637+'\\/[0-9]+'\\/[0-9]+'\\/[0-9]+'+$").test(
         path,
       )
     ) {
       return false;
     }
     return !path
-      .split('/')
+      .split("/")
       .slice(1)
-      .map((val: string): string => val.replace('\'', ''))
+      .map((val: string): string => val.replace("'", ""))
       // eslint-disable-next-line no-restricted-globals
       .some(isNaN as any);
   };
 
   static fromDerivePath(path: string, mnemonics: string): AptosAccount {
     if (!AptosAccount.isValidPath(path)) {
-      throw new Error('Invalid derivation path');
+      throw new Error("Invalid derivation path");
     }
 
     const normalizeMnemonics = mnemonics
       .trim()
       .split(/\s+/)
       .map((part) => part.toLowerCase())
-      .join(' ');
+      .join(" ");
 
-    const { key } = derivePath(path, bip39.mnemonicToSeedHex(normalizeMnemonics));
+    const { key } = derivePath(path, Buffer.from(bip39.mnemonicToSeedSync(normalizeMnemonics)).toString("hex"));
 
     return new AptosAccount(new Uint8Array(key));
   }
