6e026fd8b4a59260808197f1190f2645096bba2d
Jijun Leng <jijunleng@gmail.com>
2022-07-20 18:27:55 -0700

    [Move Test] Read table item

--- aptos-move/aptos-transactional-test-harness/tests/tests.rs
+++ aptos-move/aptos-transactional-test-harness/tests/tests.rs
@@ -3,4 +3,10 @@
 
 use aptos_transactional_test_harness::run_aptos_test;
 
-datatest_stable::harness!(run_aptos_test, "tests", r".*\.(mvir|move)$");
+// datatest_stable::harness!(run_aptos_test, "tests", r".*\.(mvir|move)$");
+
+datatest_stable::harness!(
+    run_aptos_test,
+    "/Users/jleng/Workspace/aptos-core/aptos-move/aptos-transactional-test-harness/tests",
+    r".*\.(mvir|move)$"
+);
