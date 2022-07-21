aac231f757b68446c8c5cdb156514efdb2aa6ba2
Wolfgang Grieskamp <wg@aptoslabs.comm>
2022-07-21 16:21:22 -0700

    [move] Code Publishing
    Basic implementation of the new Move API `code::publish_package`. This publishes code together with metadata about this code, and is intended to become the single way how code can get on chain. (So we can Move-programatically control deployment policies.) For internal background, see also the Notion design doc.

--- aptos-move/framework/tests/move_unit_test.rs
+++ aptos-move/framework/tests/move_unit_test.rs
@@ -1,13 +1,11 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use aptos_types::account_config::CORE_CODE_ADDRESS;
-use aptos_vm::move_vm_ext::test_transaction_context_natives;
+use aptos_vm::natives;
 use framework::path_in_crate;
 use move_deps::move_cli::base::test::run_move_unit_tests;
 use move_deps::{
-    move_stdlib, move_table_extension, move_unit_test::UnitTestingConfig,
-    move_vm_runtime::native_functions::NativeFunctionTable,
+    move_unit_test::UnitTestingConfig, move_vm_runtime::native_functions::NativeFunctionTable,
 };
 use tempfile::tempdir;
 
@@ -28,16 +26,11 @@ fn run_tests_for_pkg(path_to_pkg: impl Into<String>) {
     .unwrap();
 }
 
-// move_stdlib has the testing feature enabled to include debug native functions
 pub fn aptos_test_natives() -> NativeFunctionTable {
-    move_stdlib::natives::all_natives(CORE_CODE_ADDRESS)
-        .into_iter()
-        .chain(framework::natives::all_natives(CORE_CODE_ADDRESS))
-        .chain(framework::natives::patch_table_module(
-            move_table_extension::table_natives(CORE_CODE_ADDRESS),
-        ))
-        .chain(test_transaction_context_natives(CORE_CODE_ADDRESS))
-        .collect()
+    // By side effect, configure for unit tests
+    natives::configure_for_unit_test();
+    // move_stdlib has the testing feature enabled to include debug native functions
+    natives::aptos_natives()
 }
 
 #[test]
