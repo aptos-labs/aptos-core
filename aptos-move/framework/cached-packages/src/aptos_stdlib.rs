568aec84082f15cbacad7d39733b3db08cc5eae1
David <isaac.wolinsky@gmail.com>
2022-07-22 08:58:25 -0700

    [cached-framework] commit sdk builder files
    SDK files are useful for ides and general code readability. This stores
    them in the repo. They will automatically be rebuilt each time the user
    builds cached-framework to make it easier to keep them up to date
    without the user needing to explicitly figure this out.

--- aptos-move/framework/cached-packages/src/aptos_stdlib.rs
+++ aptos-move/framework/cached-packages/src/aptos_stdlib.rs
@@ -3,10 +3,10 @@
 
 #![allow(unused_imports)]
 
-include!(concat!(
-    concat!(env!("OUT_DIR"), "/framework"),
-    "/aptos_sdk_builder.rs",
-));
+use aptos_types::{account_address::AccountAddress, transaction::TransactionPayload};
+
+pub use crate::aptos_framework_sdk_builder::*;
+pub use crate::aptos_token_sdk_builder as token_lib;
 
 pub fn aptos_coin_transfer(to: AccountAddress, amount: u64) -> TransactionPayload {
     coin_transfer(
