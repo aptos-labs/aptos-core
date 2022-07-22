d45512be2034d5b08034c71e2526aa3aec53decf
aldenhu <msmouse@gmail.com>
2022-07-21 22:09:39 -0700

    [storage] add TableInfoSchema

--- types/src/state_store/table.rs
+++ types/src/state_store/table.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use core::num::ParseIntError;
+use move_deps::move_core_types::language_storage::TypeTag;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
@@ -22,4 +23,11 @@ impl From<move_deps::move_table_extension::TableHandle> for TableHandle {
     fn from(hdl: move_deps::move_table_extension::TableHandle) -> Self {
         Self(hdl.0)
     }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+#[cfg_attr(any(test, feature = "fuzzing"), derive(proptest_derive::Arbitrary))]
+pub struct TableInfo {
+    key_type: TypeTag,
+    value_type: TypeTag,
 }
