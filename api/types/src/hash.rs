719fce93992df7beb4eec999747b485257d54389
Daniel Porteous <danielporteous1@gmail.com>
2022-07-23 12:18:30 -0700

    [API] Add get_transaction for single transaction

--- api/types/src/hash.rs
+++ api/types/src/hash.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use aptos_openapi::impl_poem_type;
+use aptos_openapi::{impl_poem_parameter, impl_poem_type};
 use serde::{de::Error as _, Deserialize, Deserializer, Serialize, Serializer};
 use std::{
     fmt,
@@ -65,6 +65,7 @@ impl LowerHex for HashValue {
 }
 
 impl_poem_type!(HashValue);
+impl_poem_parameter!(HashValue);
 
 #[cfg(test)]
 mod tests {
