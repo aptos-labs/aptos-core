890ed247e996ad644e5c690ea7b8d0ab3a030e07
Guoteng Rao <3603304+grao1991@users.noreply.github.com>
2022-07-25 14:55:39 -0700

    [Storage] Serialize JMT updates in multiple threads since it is CPU heavy. (#2115)

--- storage/schemadb/tests/db.rs
+++ storage/schemadb/tests/db.rs
@@ -175,7 +175,7 @@ fn test_schema_put_get() {
 
 fn test_schemabatch_delete_range_util(begin: u32, end: u32, is_inclusive: bool) {
     let db = TestDB::new();
-    let mut db_batch = SchemaBatch::new();
+    let db_batch = SchemaBatch::new();
     for i in 0..100u32 {
         db_batch
             .put::<TestSchema1>(&TestField(i), &TestField(i))
@@ -275,7 +275,7 @@ fn gen_expected_values(values: &[(u32, u32)]) -> Vec<(TestField, TestField)> {
 fn test_single_schema_batch() {
     let db = TestDB::new();
 
-    let mut db_batch = SchemaBatch::new();
+    let db_batch = SchemaBatch::new();
     db_batch
         .put::<TestSchema1>(&TestField(0), &TestField(0))
         .unwrap();
@@ -313,7 +313,7 @@ fn test_single_schema_batch() {
 fn test_two_schema_batches() {
     let db = TestDB::new();
 
-    let mut db_batch1 = SchemaBatch::new();
+    let db_batch1 = SchemaBatch::new();
     db_batch1
         .put::<TestSchema1>(&TestField(0), &TestField(0))
         .unwrap();
@@ -331,7 +331,7 @@ fn test_two_schema_batches() {
         gen_expected_values(&[(0, 0), (1, 1)]),
     );
 
-    let mut db_batch2 = SchemaBatch::new();
+    let db_batch2 = SchemaBatch::new();
     db_batch2.delete::<TestSchema2>(&TestField(3)).unwrap();
     db_batch2
         .put::<TestSchema2>(&TestField(3), &TestField(3))
@@ -411,7 +411,7 @@ fn test_report_size() {
     let db = TestDB::new();
 
     for i in 0..1000 {
-        let mut db_batch = SchemaBatch::new();
+        let db_batch = SchemaBatch::new();
         db_batch
             .put::<TestSchema1>(&TestField(i), &TestField(i))
             .unwrap();
