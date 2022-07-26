8e02405e91ccfcc0af852388071177df6e0fc6dd
Zach Denton <z@chdenton.com>
2022-07-26 15:32:25 -0400

    Implement accordion mobile nav.

--- ecosystem/platform/server/app/javascript/controllers/header_controller.ts
+++ ecosystem/platform/server/app/javascript/controllers/header_controller.ts
@@ -9,15 +9,16 @@ export default class extends Controller {
 
   declare readonly navTarget: HTMLElement;
   declare readonly userTarget: HTMLElement;
+  declare readonly hasUserTarget: boolean;
 
   toggleNav() {
-    const open = this.navTarget.toggleAttribute('open');
-    if (open) this.userTarget.removeAttribute('open');
+    const open = this.navTarget.toggleAttribute("open");
+    if (open && this.hasUserTarget) this.userTarget.removeAttribute("open");
   }
 
   toggleUser() {
-    const open = this.userTarget.toggleAttribute('open');
-    if (open) this.navTarget.removeAttribute('open');
+    const open = this.userTarget.toggleAttribute("open");
+    if (open) this.navTarget.removeAttribute("open");
   }
 
   navGroupHover(event: MouseEvent) {
@@ -25,10 +26,19 @@ export default class extends Controller {
     // remove focus so that only one dropdown is visible at a time.
     if (!(event.target instanceof HTMLElement)) return;
     if (!(document.activeElement instanceof HTMLElement)) return;
-    const hoverGroup = event.target.closest('li.group');
-    const focusGroup = document.activeElement.closest('li.group');
+    const hoverGroup = event.target.closest("li.group");
+    const focusGroup = document.activeElement.closest("li.group");
     if (focusGroup && focusGroup != hoverGroup) {
       document.activeElement.blur();
     }
+  }
+
+  navGroupToggle(event: Event) {
+    if (!(event.target instanceof Element)) return;
+    const button = event.target?.closest('button');
+    if (!(button instanceof HTMLElement)) return;
+    button.classList.toggle('rotate-180');
+    const navGroup = button.nextElementSibling;
+    navGroup?.toggleAttribute("open");
   }
 }
