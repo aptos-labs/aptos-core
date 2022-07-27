be86884a4cf6859dba85d35eae1475b3273b96c1
CapCap <max@aptoslabs.com>
2022-07-27 10:13:00 -0700

    [SF-Stream] remove target address

--- config/src/config/sf_streamer_config.rs
+++ config/src/config/sf_streamer_config.rs
@@ -9,23 +9,16 @@ use std::net::SocketAddr;
 pub struct SfStreamerConfig {
     #[serde(default = "default_enabled")]
     pub enabled: bool,
-    pub target_address: SocketAddr,
 }
 
 fn default_enabled() -> bool {
     false
 }
 
-pub const DEFAULT_ADDRESS: &str = "127.0.0.1";
-pub const DEFAULT_PORT: u16 = 8083;
-
 impl Default for SfStreamerConfig {
     fn default() -> SfStreamerConfig {
         SfStreamerConfig {
             enabled: default_enabled(),
-            target_address: format!("{}:{}", DEFAULT_ADDRESS, DEFAULT_PORT)
-                .parse()
-                .unwrap(),
         }
     }
 }
