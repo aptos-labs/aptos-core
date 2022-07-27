29a105132d321fec5b7f35971432c57e83927ddf
Zach Denton <z@chdenton.com>
2022-07-27 16:22:34 -0400

    Add padding to container by default.

--- ecosystem/platform/server/tailwind.config.js
+++ ecosystem/platform/server/tailwind.config.js
@@ -1,45 +1,48 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './app/components/**/*.{rb,erb}',
-    './app/helpers/**/*.rb',
-    './app/form_builders/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./app/components/**/*.{rb,erb}",
+    "./app/helpers/**/*.rb",
+    "./app/form_builders/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       colors: {
-        'neutral': {
+        neutral: {
           ...defaultTheme.colors.neutral,
-          '100': '#f5f5f5',
-          '700': '#414141',
-          '800': '#262626',
-          '900': '#171717',
+          "100": "#f5f5f5",
+          "700": "#414141",
+          "800": "#262626",
+          "900": "#171717",
         },
         teal: {
-          50: '#eafff7',
-          100: '#cbffeb',
-          200: '#9cfedc',
-          300: '#5df8cc',
-          400: '#2ed8a7',
-          500: '#00d0a1',
-          600: '#00a380',
-          700: '#008068',
-          800: '#005c4b',
-          900: '#002e26',
+          50: "#eafff7",
+          100: "#cbffeb",
+          200: "#9cfedc",
+          300: "#5df8cc",
+          400: "#2ed8a7",
+          500: "#00d0a1",
+          600: "#00a380",
+          700: "#008068",
+          800: "#005c4b",
+          900: "#002e26",
         },
       },
       fontFamily: {
-        'sans': ['apparat', ...defaultTheme.fontFamily.sans],
-        'mono': ['lft-etica-mono', ...defaultTheme.fontFamily.mono],
-        'display': ['apparat-semicond', ...defaultTheme.fontFamily.sans],
+        sans: ["apparat", ...defaultTheme.fontFamily.sans],
+        mono: ["lft-etica-mono", ...defaultTheme.fontFamily.mono],
+        display: ["apparat-semicond", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ]
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
