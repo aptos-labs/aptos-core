17251c250dd22d66acfe19b08f5a516d5c4c591a
Wolfgang Grieskamp <wg@aptoslabs.comm>
2022-07-29 13:00:49 -0700

    [framework] Adds tests that generated Rust SDKs are up-to-date
    I also would have wished to remove the entire custom `build.rs` but there are various dependencies from abis, bytecode, etc. which are referenced through our code. Better wait with refactoring this until there is more clarity about our release process beyond running genesis every time again.

--- aptos-move/framework/cached-packages/src/lib.rs
+++ aptos-move/framework/cached-packages/src/lib.rs
@@ -12,6 +12,9 @@ pub mod aptos_framework_sdk_builder;
 pub mod aptos_stdlib;
 pub mod aptos_token_sdk_builder;
 
+// ================================================================================
+// Artifacts
+
 static PACKAGE: Dir<'static> = include_dir!("$OUT_DIR/framework");
 
 static MODULE_BLOBS: Lazy<Vec<Vec<u8>>> = Lazy::new(|| load_modules("build"));
@@ -92,24 +95,63 @@ pub fn module_blobs() -> &'static [Vec<u8>] {
 fn verify_load_framework() {
     module_blobs();
     error_map();
-    /*
-    std::fs::read(concat!(
-        env!("OUT_DIR"),
-        "/framework/transaction_script_builder.rs"
-    ))
-    .unwrap();
-     */
 }
 
 #[test]
 fn verify_load_token() {
     module_blobs();
     error_map();
-    /*
-    std::fs::read(concat!(
-        env!("OUT_DIR"),
-        "/token/transaction_script_builder.rs"
-    ))
-    .unwrap();
-     */
+}
+
+// ================================================================================
+// SDK builders
+
+#[cfg(test)]
+mod tests {
+    use move_deps::move_prover_test_utils::baseline_test::verify_or_update_baseline;
+    use std::path::PathBuf;
+
+    fn sdk_is_up_to_date(package_name: &str, built_file: &str, target_file: &str) {
+        let tempdir = tempfile::tempdir().unwrap();
+        let release = framework::release::ReleaseOptions {
+            no_check_layout_compatibility: false,
+            no_build_docs: true,
+            with_diagram: false,
+            no_script_builder: false,
+            no_script_abis: false,
+            no_errmap: true,
+            package: PathBuf::from(package_name),
+            output: tempdir.path().to_path_buf(),
+        };
+        release.create_release();
+        let current_content = std::fs::read_to_string(tempdir.path().join(built_file)).unwrap();
+        let target_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("src")
+            .join(target_file);
+        if verify_or_update_baseline(&target_path, &current_content).is_err() {
+            // Don't use original output of baseline test but specific one for this scenario
+            panic!(
+                "`{}` out of date. Please run `UPBL=1 cargo test -p cached-framework-packages`",
+                target_file
+            )
+        }
+    }
+
+    #[test]
+    fn aptos_sdk_up_to_date() {
+        sdk_is_up_to_date(
+            "aptos-framework",
+            "aptos_sdk_builder.rs",
+            "aptos_framework_sdk_builder.rs",
+        )
+    }
+
+    #[test]
+    fn token_sdk_up_to_date() {
+        sdk_is_up_to_date(
+            "aptos-token",
+            "aptos_sdk_builder.rs",
+            "aptos_token_sdk_builder.rs",
+        )
+    }
 }
