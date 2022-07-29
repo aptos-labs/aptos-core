5af4a2f78eaa9b2cc9444397d7c25530f6381130
Igor Kabiljo <ikabiljo@gmail.com>
2022-07-29 09:36:21 -0700

    Test for epoch change slowness

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -29,7 +29,7 @@ struct Args {
     wait_millis: u64,
     #[structopt(long)]
     burst: bool,
-    #[structopt(long, default_value = "300")]
+    #[structopt(long, default_value = "3600")]
     duration_secs: usize,
     #[structopt(flatten)]
     options: Options,
