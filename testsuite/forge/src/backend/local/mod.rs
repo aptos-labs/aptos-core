166109fda2e98fb52415373d679b00feb00bda97
Igor Kabiljo <ikabiljo@gmail.com>
2022-07-28 22:25:37 -0700

    [cli][smoke-test] Make genesis and stake configurable in swarm and test e2e validator runbook

--- testsuite/forge/src/backend/local/mod.rs
+++ testsuite/forge/src/backend/local/mod.rs
@@ -3,7 +3,7 @@
 
 use crate::{Factory, GenesisConfig, Result, Swarm, Version};
 use anyhow::{bail, Context};
-use aptos_genesis::builder::InitConfigFn;
+use aptos_genesis::builder::{InitConfigFn, InitGenesisConfigFn};
 use rand::rngs::StdRng;
 use std::{
     collections::HashMap,
@@ -16,7 +16,7 @@ mod cargo;
 mod node;
 mod swarm;
 pub use node::LocalNode;
-pub use swarm::{LocalSwarm, LocalSwarmBuilder, SwarmDirectory};
+pub use swarm::{LocalSwarm, SwarmDirectory};
 
 #[derive(Clone, Debug)]
 pub struct LocalVersion {
@@ -113,7 +113,7 @@ impl LocalFactory {
         R: ::rand::RngCore + ::rand::CryptoRng,
     {
         let version = self.versions.keys().max().unwrap();
-        self.new_swarm_with_version(rng, number_of_validators, version, None, 1, None)
+        self.new_swarm_with_version(rng, number_of_validators, version, None, None, None)
             .await
     }
 
@@ -123,23 +123,24 @@ impl LocalFactory {
         number_of_validators: NonZeroUsize,
         version: &Version,
         genesis_modules: Option<Vec<Vec<u8>>>,
-        min_price_per_gas_unit: u64,
         init_config: Option<InitConfigFn>,
+        init_genesis_config: Option<InitGenesisConfigFn>,
     ) -> Result<LocalSwarm>
     where
         R: ::rand::RngCore + ::rand::CryptoRng,
     {
         println!("Preparing a new swarm");
-        let mut builder = LocalSwarm::builder(self.versions.clone())
-            .number_of_validators(number_of_validators)
-            .initial_version(version.clone())
-            .min_price_per_gas_unit(min_price_per_gas_unit)
-            .with_init_config(init_config);
-        if let Some(genesis_modules) = genesis_modules {
-            builder = builder.genesis_modules(genesis_modules);
-        }
+        let mut swarm = LocalSwarm::build(
+            rng,
+            number_of_validators,
+            self.versions.clone(),
+            Some(version.clone()),
+            init_config,
+            init_genesis_config,
+            None,
+            genesis_modules,
+        )?;
 
-        let mut swarm = builder.build(rng)?;
         swarm
             .launch()
             .await
@@ -173,7 +174,7 @@ impl Factory for LocalFactory {
             None => None,
         };
         let swarm = self
-            .new_swarm_with_version(rng, node_num, version, genesis_modules, 1, None)
+            .new_swarm_with_version(rng, node_num, version, genesis_modules, None, None)
             .await?;
 
         Ok(Box::new(swarm))
