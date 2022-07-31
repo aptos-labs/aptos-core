d68b378696612ca8d3a375a17270793d665a430c
idok <1541115709@qq.com>
2022-07-31 13:06:11 +0800

    add rust example nft-tutorial

---
+++ developer-docs-site/static/examples/rust/first_nft/src/lib.rs
@@ -0,0 +1,209 @@
+use first_transaction::{Account, RestClient};
+use hex;
+use reqwest;
+use serde_json::Value;
+pub struct NftClient {
+    pub rest_client: RestClient,
+}
+
+impl NftClient {
+    /// Represents an account as well as the private, public key-pair for the Aptos blockchain.
+    pub fn new(url: String) -> Self {
+        Self {
+            rest_client: RestClient::new(url),
+        }
+    }
+    pub fn submit_transaction_helper(&self, account: &mut Account, payload: Value) {
+        self.rest_client
+            .execution_transaction_with_payload(account, payload);
+    }
+    pub fn create_collection(
+        &self,
+        account: &mut Account,
+        name: &str,
+        uri: &str,
+        description: &str,
+    ) {
+        let payload = serde_json::json!({
+            "type": "script_function_payload",
+            "function": "0x1::token::create_unlimited_collection_script",
+            "type_arguments": [],
+            "arguments": [
+                hex::encode(name.as_bytes()),
+                hex::encode(description.as_bytes()),
+                hex::encode(uri.as_bytes()),
+            ]
+        });
+        self.submit_transaction_helper(account, payload)
+    }
+    pub fn create_token(
+        &self,
+        account: &mut Account,
+        collection_name: &str,
+        name: &str,
+        description: &str,
+        supply: i32,
+        uri: &str,
+    ) {
+        let payload = serde_json::json!({
+            "type": "script_function_payload",
+            "function": "0x1::token::create_unlimited_token_script",
+            "type_arguments": [],
+            "arguments": [
+                hex::encode(collection_name.as_bytes()),
+                hex::encode(name.as_bytes()),
+                hex::encode(description.as_bytes()),
+                true,
+                supply.to_string().as_str(),
+                hex::encode(uri.as_bytes()),
+                "0",
+            ]
+        });
+        self.submit_transaction_helper(account, payload)
+    }
+    pub fn offer_token(
+        &self,
+        account: &mut Account,
+        receiver: &str,
+        creator: &str,
+        collection_name: &str,
+        token_name: &str,
+        amount: i32,
+    ) {
+        let payload = serde_json::json!({
+            "type": "script_function_payload",
+            "function": "0x1::token_transfers::offer_script",
+            "type_arguments": [],
+            "arguments": [
+                receiver,
+                creator,
+                hex::encode(collection_name.as_bytes()),
+                hex::encode(token_name.as_bytes()),
+                amount.to_string().as_str()
+            ]
+        });
+        self.submit_transaction_helper(account, payload)
+    }
+    pub fn claim_token(
+        &self,
+        account: &mut Account,
+        sender: &str,
+        creator: &str,
+        collection_name: &str,
+        token_name: &str,
+    ) {
+        let payload = serde_json::json!({
+            "type": "script_function_payload",
+            "function": "0x1::token_transfers::claim_script",
+            "type_arguments": [],
+            "arguments": [
+                sender,
+                creator,
+                hex::encode(collection_name.as_bytes()),
+                hex::encode(token_name.as_bytes())
+            ]
+        });
+        self.submit_transaction_helper(account, payload)
+    }
+    pub fn cancel_token_offer(
+        &self,
+        account: &mut Account,
+        receiver: &str,
+        creator: &str,
+        collection_name: &str,
+        token_name: &str,
+    ) {
+        let payload = serde_json::json!({
+            "type": "script_function_payload",
+            "function": "0x1::token_transfers::cancel_offer_script",
+            "type_arguments": [],
+            "arguments": [
+                receiver,
+                creator,
+                hex::encode(collection_name.as_bytes()),
+                hex::encode(token_name.as_bytes())
+            ]
+        });
+        self.submit_transaction_helper(account, payload)
+    }
+
+    pub fn get_table_item(
+        &self,
+        handle: &str,
+        key_type: &str,
+        value_type: &str,
+        key: Value,
+    ) -> Value {
+        let res = reqwest::blocking::Client::new()
+            .post(format!(
+                "{}/tables/{}/item",
+                self.rest_client.get_url(),
+                handle
+            ))
+            .json(&serde_json::json!({
+                "key_type": key_type,
+                "value_type": value_type,
+                "key": key
+            }))
+            .send()
+            .unwrap();
+        res.json().unwrap()
+    }
+    pub fn get_collection(&self, creator: &str, collection_name: &str) -> Value {
+        let collection = &self
+            .rest_client
+            .account_resource(creator, "0x1::token::Collections")
+            .unwrap()["data"]["collections"]["handle"];
+        match collection {
+            Value::String(s) => self.get_table_item(
+                s.as_str(),
+                "0x1::string::String",
+                "0x1::token::Collection",
+                Value::String(collection_name.to_string()),
+            ),
+            _ => panic!("get_collection:error"),
+        }
+    }
+    pub fn get_token_balance(
+        &self,
+        owner: &str,
+        creator: &str,
+        collection_name: &str,
+        token_name: &str,
+    ) -> Value {
+        let token_store = &self
+            .rest_client
+            .account_resource(owner, "0x1::token::TokenStore")
+            .unwrap()["data"]["tokens"]["handle"];
+        let token_id = serde_json::json!({
+            "creator": creator,
+            "collection": collection_name,
+            "name": token_name,
+        });
+        match token_store {
+            Value::String(s) => {
+                self.get_table_item(s, "0x1::token::TokenId", "0x1::token::Token", token_id)
+                    ["value"]
+                    .clone()
+            }
+            _ => panic!("get_token_balance:error"),
+        }
+    }
+    pub fn get_token_data(&self, creator: &str, collection_name: &str, token_name: &str) -> Value {
+        let token_data = &self
+            .rest_client
+            .account_resource(creator, "0x1::token::Collections")
+            .unwrap()["data"]["token_data"]["handle"];
+        let token_id = serde_json::json!({
+            "creator": creator,
+            "collection": collection_name,
+            "name": token_name,
+        });
+        match token_data {
+            Value::String(s) => self
+                .get_table_item(s, "0x1::token::TokenId", "0x1::token::TokenData", token_id)
+                .clone(),
+            _ => panic!("get_token_data:error"),
+        }
+    }
+}
---
+++ developer-docs-site/static/examples/rust/first_nft/src/main.rs
@@ -0,0 +1,95 @@
+use first_nft::NftClient;
+use first_transaction::{Account, FaucetClient, FAUCET_URL, TESTNET_URL};
+fn main() {
+    let client = NftClient::new(TESTNET_URL.to_string());
+    let faucet_client = FaucetClient::new(FAUCET_URL.to_string(), client.rest_client.clone());
+
+    let mut alice = Account::new(None);
+    let alice_address = alice.address();
+    let mut bob = Account::new(None);
+    let collection_name = "Alice's";
+    let token_name = "Alice's first token";
+    println!("\n=== Addresses ===");
+    println!("Alice: 0x{}", alice.address());
+    println!("Bob: 0x{}", bob.address());
+
+    faucet_client.fund_account(&alice.auth_key(), 10_000_000);
+    faucet_client.fund_account(&bob.auth_key(), 10_000_000);
+
+    println!("\n=== Initial Balances ===");
+    println!(
+        "Alice: {:?}",
+        client.rest_client.account_balance(&alice.address())
+    );
+    println!(
+        "Bob: {:?}",
+        client.rest_client.account_balance(&bob.address())
+    );
+    println!("\n=== Creating Collection and Token ===");
+    client.create_collection(
+        &mut alice,
+        collection_name,
+        "Alice's simple collection",
+        "https://aptos.dev",
+    );
+    client.create_token(
+        &mut alice,
+        collection_name,
+        token_name,
+        "Alice's simple token",
+        1,
+        "https://aptos.dev/img/nyan.jpeg",
+    );
+    println!(
+        "Alice's collection: {}",
+        client.get_collection(&alice.address(), collection_name)
+    );
+    println!(
+        "Alice's token balance: {}",
+        client.get_token_balance(
+            &alice.address(),
+            &alice.address(),
+            collection_name,
+            token_name
+        )
+    );
+    println!(
+        "Alice's token data: {}",
+        client.get_token_data(&alice.address(), collection_name, token_name)
+    );
+
+    println!("\n=== Transferring the token to Bob ===");
+    client.offer_token(
+        &mut alice,
+        &bob.address(),
+        alice_address.as_str(),
+        collection_name,
+        token_name,
+        1,
+    );
+    client.claim_token(
+        &mut bob,
+        &alice.address(),
+        &alice.address(),
+        collection_name,
+        token_name,
+    );
+    println!(
+        "Alice's token balance: {}",
+        client.get_token_balance(
+            &alice.address(),
+            &alice.address(),
+            collection_name,
+            token_name
+        )
+    );
+    println!(
+        "Bob's token balance: {}",
+        client.get_token_balance(
+            &bob.address(),
+            &alice.address(),
+            collection_name,
+            token_name
+        )
+    );
+}
