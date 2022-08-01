12c965851b404aa645a5004387c8bb3363d61c1f
bowenyang007 <carlyang007@gmail.com>
2022-08-01 15:47:41 -0700

    fix ilnt

--- ecosystem/indexer/src/models/mod.rs
+++ ecosystem/indexer/src/models/mod.rs
@@ -3,10 +3,10 @@
 
 pub mod collection;
 pub mod events;
+pub mod ledger_info;
 pub mod metadata;
 pub mod ownership;
 pub mod processor_statuses;
 pub mod token;
 pub mod transactions;
 pub mod write_set_changes;
-pub mod ledger_info;
