74097613f81a870447199353f8c655a066135c00
bowenyang007 <carlyang007@gmail.com>
2022-08-01 15:58:43 -0700

    fix linter

--- ecosystem/indexer/src/models/ledger_info.rs
+++ ecosystem/indexer/src/models/ledger_info.rs
@@ -1,6 +1,6 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
-
+#![allow(clippy::extra_unused_lifetimes)]
 use crate::schema::ledger_infos;
 
 #[derive(Debug, Identifiable, Insertable, Queryable)]
