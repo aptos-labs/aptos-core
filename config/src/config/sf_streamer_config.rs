261b4d76fc632685917be8e4d69bb9eddd704266
CapCap <max@aptoslabs.com>
2022-08-02 11:28:36 -0700

    [CP] Review fixes

--- config/src/config/sf_streamer_config.rs
+++ config/src/config/sf_streamer_config.rs
@@ -6,19 +6,14 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 #[serde(default, deny_unknown_fields)]
 pub struct SfStreamerConfig {
-    #[serde(default = "default_enabled")]
     pub enabled: bool,
     pub starting_version: u64,
-}
-
-fn default_enabled() -> bool {
-    false
 }
 
 impl Default for SfStreamerConfig {
     fn default() -> SfStreamerConfig {
         SfStreamerConfig {
-            enabled: default_enabled(),
+            enabled: false,
             starting_version: 0,
         }
     }
