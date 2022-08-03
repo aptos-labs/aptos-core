f95fce6b07385912e9cc2221a0450dbd581c4622
CapCap <max@aptoslabs.com>
2022-08-03 10:48:05 -0700

    [CP] Add comments

--- config/src/config/sf_streamer_config.rs
+++ config/src/config/sf_streamer_config.rs
@@ -7,6 +7,8 @@ use serde::{Deserialize, Serialize};
 #[serde(default, deny_unknown_fields)]
 pub struct SfStreamerConfig {
     pub enabled: bool,
+    // The version to start pushing out indexed data from, for the StreamingFast indexer
+    // Alternatively can set the `STARTING_VERSION` env var
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub starting_version: Option<u64>,
 }
