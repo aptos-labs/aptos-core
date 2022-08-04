bfe6ab87f78f436dad8b929b59bf7547ec0768bc
Daniel Porteous (dport) <danielporteous1@gmail.com>
2022-08-04 14:43:07 -0700

    Make TS and JS examples refer to package locally, fix tests accordingly (#2534)

--- ecosystem/typescript/sdk/examples/typescript/bcs_transaction.ts
+++ ecosystem/typescript/sdk/examples/typescript/bcs_transaction.ts
@@ -1,10 +1,17 @@
 /* eslint-disable no-console */
-import { AptosClient, AptosAccount, FaucetClient, BCS, TxnBuilderTypes } from "aptos";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import { AptosClient, AptosAccount, FaucetClient, TransactionBuilder, TxnBuilderTypes } from "aptos";
+import { aptosCoin } from "./constants";
+import isEqual from "lodash/isEqual";
 import assert from "assert";
 
 const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
 const FAUCET_URL = process.env.APTOS_FAUCET_URL || "https://faucet.devnet.aptoslabs.com";
 
+const { BCS } = TransactionBuilder;
 const {
   AccountAddress,
   TypeTagStruct,
@@ -24,19 +31,18 @@ const {
 
   // Generates key pair for a new account
   const account1 = new AptosAccount();
-  // Creates the account on Aptos chain and fund the account with 5000 AptosCoin
-  await faucetClient.fundAccount(account1.address(), 5000);
+  await faucetClient.fundAccount(account1.address(), 100000000);
   let resources = await client.getAccountResources(account1.address());
-  let accountResource = resources.find((r) => r.type === "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>");
+  let accountResource = resources.find((r) => isEqual(r.type, aptosCoin));
   let balance = parseInt((accountResource?.data as any).coin.value);
-  assert(balance === 5000);
-  console.log(`account2 coins: ${balance}. Should be 5000!`);
+  assert(balance === 100000000);
+  console.log(`account2 coins: ${balance}. Should be 100000000!`);
 
   const account2 = new AptosAccount();
   // Creates the second account and fund the account with 0 AptosCoin
   await faucetClient.fundAccount(account2.address(), 0);
   resources = await client.getAccountResources(account2.address());
-  accountResource = resources.find((r) => r.type === "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>");
+  accountResource = resources.find((r) => isEqual(r.type, aptosCoin));
   balance = parseInt((accountResource?.data as any).coin.value);
   assert(balance === 0);
   console.log(`account2 coins: ${balance}. Should be 0!`);
@@ -71,7 +77,7 @@ const {
     BigInt(sequenceNumber),
     scriptFunctionPayload,
     // Max gas unit to spend
-    1000n,
+    1000000n,
     // Gas price per unit
     1n,
     // Expiration timestamp. Transaction is discarded if it is not executed within 10 seconds from now.
@@ -87,7 +93,7 @@ const {
   await client.waitForTransaction(transactionRes.hash);
 
   resources = await client.getAccountResources(account2.address());
-  accountResource = resources.find((r) => r.type === "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>");
+  accountResource = resources.find((r) => isEqual(r.type, aptosCoin));
   balance = parseInt((accountResource?.data as any).coin.value);
   assert(balance === 717);
   console.log(`account2 coins: ${balance}. Should be 717!`);
--- ecosystem/typescript/sdk/src/index.ts
+++ ecosystem/typescript/sdk/src/index.ts
@@ -4,5 +4,6 @@ export * from "./hex_string";
 export * from "./aptos_client";
 export * from "./faucet_client";
 export * from "./token_client";
-export * as Generated from "./generated/index";
+export * as Types from "./generated/index";
 export * as TransactionBuilder from "./transaction_builder";
+export * as TxnBuilderTypes from "./transaction_builder/aptos_types";
