6294862e1b7669f5f1ad234ba397d0bdd0bd9716
Daniel Porteous (dport) <danielporteous1@gmail.com>
2022-08-04 15:15:54 -0700

    Regenerate TS client (#2546)

--- ecosystem/typescript/sdk/src/generated/AptosGeneratedClient.ts
+++ ecosystem/typescript/sdk/src/generated/AptosGeneratedClient.ts
@@ -26,7 +26,7 @@ export class AptosGeneratedClient {
     constructor(config?: Partial<OpenAPIConfig>, HttpRequest: HttpRequestConstructor = AxiosHttpRequest) {
         this.request = new HttpRequest({
             BASE: config?.BASE ?? '',
-            VERSION: config?.VERSION ?? '0.1.0',
+            VERSION: config?.VERSION ?? '1.0.0',
             WITH_CREDENTIALS: config?.WITH_CREDENTIALS ?? false,
             CREDENTIALS: config?.CREDENTIALS ?? 'include',
             TOKEN: config?.TOKEN,
