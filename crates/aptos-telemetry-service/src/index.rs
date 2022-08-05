70347042b85cf31427a456dada5c36eb02d91535
Balaji Arun <balaji@aptoslabs.com>
2022-08-05 09:44:39 -0700

    [aptos-telemetry] Big query ingestion endpoiint
    Introduces an endpoint to receive telemetry dumps from the node
    telemetry-service and dumps it to GCP Big Query.

--- crates/aptos-telemetry-service/src/index.rs
+++ crates/aptos-telemetry-service/src/index.rs
@@ -1,11 +1,12 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::{auth, context::Context, custom_telemetry, error};
 use std::convert::Infallible;
-
-use crate::{auth, context::Context, error};
 use warp::{Filter, Reply};
 
 pub fn routes(context: Context) -> impl Filter<Extract = impl Reply, Error = Infallible> + Clone {
-    auth::auth(context).recover(error::handle_rejection)
+    auth::auth(context.clone())
+        .or(custom_telemetry::custom_telemetry(context))
+        .recover(error::handle_rejection)
 }
