0cb51d010060b92b6d8f808f39971216dc03342d
zcc <107430656+zcchahaha@users.noreply.github.com>
2022-08-05 01:08:48 -0700

    [storage][pruner] Fix flaky tests (#2560)

--- storage/aptosdb/src/pruner/state_pruner_manager.rs
+++ storage/aptosdb/src/pruner/state_pruner_manager.rs
@@ -108,9 +108,6 @@ impl PrunerManager for StatePrunerManager {
 
             while Instant::now() < end {
                 if self.get_min_readable_version() >= min_readable_state_store_version {
-                    // A hack to make sure the items have been removed from DB as we update the
-                    // min_readable_version before finishing deleting items from DB.
-                    sleep(Duration::from_millis(100));
                     return Ok(());
                 }
                 sleep(Duration::from_millis(1));
