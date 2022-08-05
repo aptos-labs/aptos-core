535c46afaceec1ed68be6df3da8b99bacb250463
Kevin <kevin@aptoslabs.com>
2022-08-05 09:27:35 -0500

    [Aptos Framework] Add more e2e tests for Staking

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -24,6 +24,7 @@ pub mod proof;
 #[cfg(any(test, feature = "fuzzing"))]
 pub mod proptest_types;
 pub mod serde_helper;
+pub mod stake_pool;
 pub mod state_proof;
 #[cfg(any(test, feature = "fuzzing"))]
 pub mod test_helpers;
