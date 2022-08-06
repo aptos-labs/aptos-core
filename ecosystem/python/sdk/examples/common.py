1b41383859eb73970a085c6fc1be653979b504cd
David Wolinsky <isaac.wolinsky@gmail.com>
2022-08-06 16:29:04 -0700

    [python-sdk] improve utility of sdk and examples

---
+++ ecosystem/python/sdk/examples/common.py
@@ -0,0 +1,7 @@
+# Copyright (c) Aptos
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+NODE_URL = os.getenv("APTOS_NODE_URL", "https://fullnode.devnet.aptoslabs.com")
+FAUCET_URL = os.getenv("APTOS_FAUCET_URL", "https://faucet.devnet.aptoslabs.com")
