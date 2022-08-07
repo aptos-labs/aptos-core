3e147baadb2635d56e3ec4c03f46d4d7ce61f27e
George Mitenkov <georgemitenk0v@gmail.com>
2022-08-07 11:38:15 -0700

    [Aptos Framework][vm] Aggregator V1 (#2462)
    V1 implementation of `Aggregator`, which adds:
    
    * Basic Move logic for having an aggregator controlled by a core account. See
      more details in new Move file headers.
    
    * Native implementation that supports additions, reads, and subtractions.
      Currently, only additions are parallelizable, reads are materialize the
      value, subtractions materialize the value and then subtract.
    
    By default, `Aggregator`'s functionality is not supported. To turn it on, make
    sure to have this in Cargo file:
    
    ```
    features = ["aggregator-extension"]
    ```

--- aptos-move/aptos-vm/src/natives.rs
+++ aptos-move/aptos-vm/src/natives.rs
@@ -1,14 +1,19 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::move_vm_ext::{aggregator_natives, NativeAggregatorContext};
 use aptos_gas::NativeGasParameters;
 use aptos_types::account_config::CORE_CODE_ADDRESS;
 use framework::natives::{code::NativeCodeContext, transaction_context::NativeTransactionContext};
 use move_deps::move_unit_test;
 use move_deps::move_vm_runtime::native_extensions::NativeContextExtensions;
+use move_deps::move_vm_test_utils::BlankStorage;
 use move_deps::{
     move_stdlib, move_table_extension, move_vm_runtime::native_functions::NativeFunctionTable,
 };
+use once_cell::sync::Lazy;
+
+static DUMMY_RESOLVER: Lazy<BlankStorage> = Lazy::new(|| BlankStorage);
 
 pub fn aptos_natives(gas_params: NativeGasParameters) -> NativeFunctionTable {
     move_stdlib::natives::all_natives(CORE_CODE_ADDRESS, gas_params.move_stdlib)
@@ -18,6 +23,7 @@ pub fn aptos_natives(gas_params: NativeGasParameters) -> NativeFunctionTable {
             gas_params.aptos_framework,
         ))
         .chain(move_table_extension::table_natives(CORE_CODE_ADDRESS))
+        .chain(aggregator_natives(CORE_CODE_ADDRESS))
         // TODO(Gas): this isn't quite right yet...
         .chain(
             move_stdlib::natives::nursery_natives(
@@ -38,5 +44,6 @@ pub fn configure_for_unit_test() {
 
 fn unit_test_extensions_hook(exts: &mut NativeContextExtensions) {
     exts.add(NativeCodeContext::default());
-    exts.add(NativeTransactionContext::new(vec![1]))
+    exts.add(NativeTransactionContext::new(vec![1]));
+    exts.add(NativeAggregatorContext::new(0, &*DUMMY_RESOLVER));
 }
