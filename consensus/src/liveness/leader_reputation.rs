07246fb51765ead1108e0c42bbc48fefb4938443
Zekun Li <li.zekun@gmail.com>
2022-08-09 16:04:50 -0700

    [validator set] update the validator verifier to use index instead of relying on address ordering

--- consensus/src/liveness/leader_reputation.rs
+++ consensus/src/liveness/leader_reputation.rs
@@ -13,7 +13,7 @@ use aptos_logger::prelude::*;
 use aptos_types::account_config::{new_block_event_key, NewBlockEvent};
 use consensus_types::common::{Author, Round};
 use short_hex_str::AsShortHexStr;
-use std::{cmp::Ordering, collections::HashMap, convert::TryFrom, sync::Arc};
+use std::{collections::HashMap, convert::TryFrom, sync::Arc};
 use storage_interface::{DbReader, Order};
 
 /// Interface to query committed NewBlockEvent.
@@ -477,12 +477,6 @@ impl LeaderReputation {
         heuristic: Box<dyn ReputationHeuristic>,
         exclude_round: u64,
     ) -> Self {
-        // assert!(proposers.is_sorted()) implementation from new api
-        assert!(proposers.windows(2).all(|w| {
-            PartialOrd::partial_cmp(&&w[0], &&w[1])
-                .map(|o| o != Ordering::Greater)
-                .unwrap_or(false)
-        }));
         assert_eq!(proposers.len(), voting_powers.len());
 
         Self {
