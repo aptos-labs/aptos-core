6eba0dfd0b000a7564329f328aa07ad081c78e2b
Igor <igor-aptos@users.noreply.github.com>
2022-08-11 10:56:35 -0700

    [smoke-tests] Add concurrency control to LocalSwarm smoke tests

--- testsuite/forge/src/backend/local/mod.rs
+++ testsuite/forge/src/backend/local/mod.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{Factory, GenesisConfig, Result, Swarm, Version};
-use anyhow::{bail, Context};
+use anyhow::Context;
 use aptos_genesis::builder::{InitConfigFn, InitGenesisConfigFn};
 use rand::rngs::StdRng;
 use std::time::Duration;
@@ -18,6 +18,8 @@ mod node;
 mod swarm;
 pub use node::LocalNode;
 pub use swarm::{LocalSwarm, SwarmDirectory};
+
+pub use self::swarm::ActiveNodesGuard;
 
 #[derive(Clone, Debug)]
 pub struct LocalVersion {
@@ -105,19 +107,6 @@ impl LocalFactory {
         Self::with_revision_and_workspace(&merge_base)
     }
 
-    pub async fn new_swarm<R>(
-        &self,
-        rng: R,
-        number_of_validators: NonZeroUsize,
-    ) -> Result<LocalSwarm>
-    where
-        R: ::rand::RngCore + ::rand::CryptoRng,
-    {
-        let version = self.versions.keys().max().unwrap();
-        self.new_swarm_with_version(rng, number_of_validators, version, None, None, None)
-            .await
-    }
-
     pub async fn new_swarm_with_version<R>(
         &self,
         rng: R,
@@ -126,11 +115,11 @@ impl LocalFactory {
         genesis_modules: Option<Vec<Vec<u8>>>,
         init_config: Option<InitConfigFn>,
         init_genesis_config: Option<InitGenesisConfigFn>,
+        guard: ActiveNodesGuard,
     ) -> Result<LocalSwarm>
     where
         R: ::rand::RngCore + ::rand::CryptoRng,
     {
-        println!("Preparing a new swarm");
         let mut swarm = LocalSwarm::build(
             rng,
             number_of_validators,
@@ -140,6 +129,7 @@ impl LocalFactory {
             init_genesis_config,
             None,
             genesis_modules,
+            guard,
         )?;
 
         swarm
@@ -159,28 +149,15 @@ impl Factory for LocalFactory {
 
     async fn launch_swarm(
         &self,
-        rng: &mut StdRng,
-        num_validators: NonZeroUsize,
+        _rng: &mut StdRng,
+        _num_validators: NonZeroUsize,
         // TODO: support fullnodes in local forge
         _num_fullnodes: usize,
-        version: &Version,
+        _version: &Version,
         _genesis_version: &Version,
-        genesis_config: Option<&GenesisConfig>,
+        _genesis_config: Option<&GenesisConfig>,
         _cleanup_duration: Duration,
     ) -> Result<Box<dyn Swarm>> {
-        let genesis_modules = match genesis_config {
-            Some(config) => match config {
-                GenesisConfig::Bytes(bytes) => Some(bytes.clone()),
-                GenesisConfig::Path(_) => {
-                    bail!("local forge backend does not support flattened dir for genesis")
-                }
-            },
-            None => None,
-        };
-        let swarm = self
-            .new_swarm_with_version(rng, num_validators, version, genesis_modules, None, None)
-            .await?;
-
-        Ok(Box::new(swarm))
+        unimplemented!("new_swarm_with_version is used instead");
     }
 }
