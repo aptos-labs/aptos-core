6641253683392f73edfd9d4ce3fedb12b152e27d
Sherry Xiao <sherryxiao.py@gmail.com>
2022-08-12 17:40:49 -0700

    fix backup

--- terraform/fullnode/gcp/kubernetes.tf
+++ terraform/fullnode/gcp/kubernetes.tf
@@ -52,12 +52,26 @@ resource "helm_release" "fullnode" {
       }
       nodeSelector = {
         "cloud.google.com/gke-nodepool" = "fullnodes"
+        "iam.gke.io/gke-metadata-server-enabled" = "true"
       }
       storage = {
         class = kubernetes_storage_class.ssd.metadata[0].name
       }
       service = {
         type = "LoadBalancer"
+      }
+      backup = {
+        config = {
+          location = "gcs"
+          gcs = {
+            bucket = google_storage_bucket.backup.name
+          }
+        }
+      }
+      serviceAccount = {
+        annotations = {
+          "iam.gke.io/gcp-service-account" = google_service_account.backup.email
+        }
       }
     }),
     jsonencode(var.fullnode_helm_values),
