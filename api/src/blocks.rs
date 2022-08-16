b77787390a0607cc18143bfcf7e8d04411204923
Daniel Porteous <danielporteous1@gmail.com>
2022-08-15 22:19:06 -0700

    Delete v0 API

--- api/src/blocks.rs
+++ api/src/blocks.rs
@@ -1,66 +1,116 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{context::Context, failpoint::fail_point, metrics::metrics, param::LedgerVersionParam};
-use anyhow::Result;
-use aptos_api_types::{Error, LedgerInfo, Response, TransactionId, U64};
-use warp::{filters::BoxedFilter, Filter, Rejection, Reply};
+use crate::accept_type::AcceptType;
+use crate::context::Context;
+use crate::failpoint::fail_point_poem;
+use crate::response::{
+    BasicErrorWith404, BasicResponse, BasicResponseStatus, BasicResultWith404, InternalError,
+};
+use crate::ApiTags;
+use anyhow::Context as AnyhowContext;
+use aptos_api_types::Block;
+use poem_openapi::param::{Path, Query};
+use poem_openapi::OpenApi;
+use std::sync::Arc;
 
-// GET /blocks/<version>
-pub fn get_block_info(context: Context) -> BoxedFilter<(impl Reply,)> {
-    warp::path!("blocks" / LedgerVersionParam)
-        .and(warp::get())
-        .and(context.filter())
-        .and_then(handle_get_block_info)
-        .with(metrics("get_block_info"))
-        .boxed()
+pub struct BlocksApi {
+    pub context: Arc<Context>,
 }
 
-async fn handle_get_block_info(
-    ledger_version: LedgerVersionParam,
-    context: Context,
-) -> Result<impl Reply, Rejection> {
-    fail_point("endpoint_get_block_info")?;
-    Ok(Block::new(Some(ledger_version), context)?.find_block()?)
-}
-
-pub(crate) struct Block {
-    version: u64,
-    latest_ledger_info: LedgerInfo,
-    context: Context,
-}
-
-impl Block {
-    pub fn new(
-        ledger_version: Option<LedgerVersionParam>,
-        context: Context,
-    ) -> Result<Self, Error> {
-        let latest_ledger_info = context.get_latest_ledger_info()?;
-        let ledger_version = ledger_version
-            .map(|v| v.parse("ledger version"))
-            .unwrap_or_else(|| Ok(latest_ledger_info.version()))?;
-
-        if ledger_version > latest_ledger_info.version() {
-            return Err(Error::not_found(
-                "ledger",
-                TransactionId::Version(U64::from(ledger_version)),
-                latest_ledger_info.version(),
-            ));
-        }
-
-        Ok(Self {
-            version: ledger_version,
-            latest_ledger_info,
-            context,
-        })
+#[OpenApi]
+impl BlocksApi {
+    /// Get blocks by height
+    ///
+    /// This endpoint allows you to get the transactions in a block
+    /// and the corresponding block information.
+    #[oai(
+        path = "/blocks/by_height/:block_height",
+        method = "get",
+        operation_id = "get_block_by_height",
+        tag = "ApiTags::Blocks"
+    )]
+    async fn get_block_by_height(
+        &self,
+        accept_type: AcceptType,
+        block_height: Path<u64>,
+        with_transactions: Query<Option<bool>>,
+    ) -> BasicResultWith404<Block> {
+        fail_point_poem("endpoint_get_block_by_height")?;
+        self.get_by_height(
+            accept_type,
+            block_height.0,
+            with_transactions.0.unwrap_or_default(),
+        )
     }
 
-    /// Scans the DB for block boundaries, then retrieves all the transactions associated
-    pub fn find_block(self) -> Result<Response, Error> {
-        let ledger_version = self.latest_ledger_info.version();
-        Response::new(
-            self.latest_ledger_info,
-            &self.context.get_block_info(self.version, ledger_version)?,
+    /// Get blocks by version
+    ///
+    /// This endpoint allows you to get the transactions in a block
+    /// and the corresponding block information given a version in the block.
+    #[oai(
+        path = "/blocks/by_version/:version",
+        method = "get",
+        operation_id = "get_block_by_version",
+        tag = "ApiTags::Blocks"
+    )]
+    async fn get_block_by_version(
+        &self,
+        accept_type: AcceptType,
+        version: Path<u64>,
+        with_transactions: Query<Option<bool>>,
+    ) -> BasicResultWith404<Block> {
+        fail_point_poem("endpoint_get_block_by_version")?;
+        self.get_by_version(
+            accept_type,
+            version.0,
+            with_transactions.0.unwrap_or_default(),
         )
+    }
+}
+
+impl BlocksApi {
+    fn get_by_height(
+        &self,
+        accept_type: AcceptType,
+        block_height: u64,
+        with_transactions: bool,
+    ) -> BasicResultWith404<Block> {
+        let latest_ledger_info = self.context.get_latest_ledger_info()?;
+        let latest_version = latest_ledger_info.version();
+        let block = self
+            .context
+            .get_block_by_height(block_height, latest_version, with_transactions)
+            .context("Failed to retrieve block by height")
+            .map_err(BasicErrorWith404::internal)?;
+
+        BasicResponse::try_from_rust_value((
+            block,
+            &latest_ledger_info,
+            BasicResponseStatus::Ok,
+            &accept_type,
+        ))
+    }
+
+    fn get_by_version(
+        &self,
+        accept_type: AcceptType,
+        version: u64,
+        with_transactions: bool,
+    ) -> BasicResultWith404<Block> {
+        let latest_ledger_info = self.context.get_latest_ledger_info()?;
+        let latest_version = latest_ledger_info.version();
+        let block = self
+            .context
+            .get_block_by_version(version, latest_version, with_transactions)
+            .context("Failed to retrieve block by height")
+            .map_err(BasicErrorWith404::internal)?;
+
+        BasicResponse::try_from_rust_value((
+            block,
+            &latest_ledger_info,
+            BasicResponseStatus::Ok,
+            &accept_type,
+        ))
     }
 }
--- api/src/state.rs
+++ api/src/state.rs
@@ -1,223 +1,266 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{
-    context::Context,
-    failpoint::fail_point,
-    metrics::metrics,
-    param::{
-        AddressParam, LedgerVersionParam, MoveIdentifierParam, MoveStructTagParam, TableHandleParam,
-    },
-    version::Version,
+use crate::accept_type::AcceptType;
+use crate::context::Context;
+use crate::failpoint::fail_point_poem;
+use crate::response::{
+    build_not_found, BadRequestError, BasicErrorWith404, BasicResponse, BasicResponseStatus,
+    BasicResultWith404, InternalError, NotFoundError,
 };
-use anyhow::anyhow;
+use crate::ApiTags;
+use anyhow::Context as AnyhowContext;
 use aptos_api_types::{
-    AsConverter, Error, LedgerInfo, MoveModuleBytecode, Response, TableItemRequest, TransactionId,
-    U64,
+    Address, AsConverter, IdentifierWrapper, MoveModuleBytecode, MoveStructTag, MoveValue,
+    TableItemRequest, TransactionId, U128, U64,
 };
+use aptos_api_types::{LedgerInfo, MoveResource};
 use aptos_state_view::StateView;
+use aptos_types::access_path::AccessPath;
+use aptos_types::state_store::state_key::StateKey;
 use aptos_types::state_store::table::TableHandle;
-use aptos_types::{access_path::AccessPath, state_store::state_key::StateKey};
 use aptos_vm::data_cache::AsMoveResolver;
-use move_deps::move_core_types::{
-    account_address::AccountAddress,
-    identifier::Identifier,
-    language_storage::{ModuleId, ResourceKey, StructTag},
-};
+use move_deps::move_core_types::language_storage::{ModuleId, ResourceKey, StructTag};
+use poem_openapi::param::Query;
+use poem_openapi::payload::Json;
+use poem_openapi::{param::Path, OpenApi};
 use std::convert::TryInto;
 use std::sync::Arc;
 use storage_interface::state_view::DbStateView;
-use storage_interface::DbReader;
-use warp::{filters::BoxedFilter, Filter, Rejection, Reply};
 
-// GET /accounts/<address>/resource/<resource_type>
-pub fn get_account_resource(context: Context) -> BoxedFilter<(impl Reply,)> {
-    warp::path!("accounts" / AddressParam / "resource" / MoveStructTagParam)
-        .and(warp::get())
-        .and(context.filter())
-        .and(warp::query::<Version>())
-        .map(|address, struct_tag, ctx, version: Version| {
-            (version.version, address, struct_tag, ctx)
-        })
-        .untuple_one()
-        .and_then(handle_get_account_resource)
-        .with(metrics("get_account_resource"))
-        .boxed()
+pub struct StateApi {
+    pub context: Arc<Context>,
 }
 
-// GET /state/module/<address>/<module_name>
-pub fn get_account_module(context: Context) -> BoxedFilter<(impl Reply,)> {
-    warp::path!("accounts" / AddressParam / "module" / MoveIdentifierParam)
-        .and(warp::get())
-        .and(context.filter())
-        .and(warp::query::<Version>())
-        .map(|address, name, ctx, version: Version| (version.version, address, name, ctx))
-        .untuple_one()
-        .and_then(handle_get_account_module)
-        .with(metrics("get_account_module"))
-        .boxed()
+#[OpenApi]
+impl StateApi {
+    /// Get specific account resource
+    ///
+    /// This endpoint returns the resource of a specific type residing at a given
+    /// account at a specified ledger version (AKA transaction version). If the
+    /// ledger version is not specified in the request, the latest ledger version
+    /// is used.
+    ///
+    /// The Aptos nodes prune account state history, via a configurable time window (link).
+    /// If the requested data has been pruned, the server responds with a 404.
+    #[oai(
+        path = "/accounts/:address/resource/:resource_type",
+        method = "get",
+        operation_id = "get_account_resource",
+        tag = "ApiTags::Accounts"
+    )]
+    async fn get_account_resource(
+        &self,
+        accept_type: AcceptType,
+        address: Path<Address>,
+        resource_type: Path<MoveStructTag>,
+        ledger_version: Query<Option<U64>>,
+    ) -> BasicResultWith404<MoveResource> {
+        fail_point_poem("endpoint_get_account_resource")?;
+        self.resource(&accept_type, address.0, resource_type.0, ledger_version.0)
+    }
+
+    /// Get specific account module
+    ///
+    /// This endpoint returns the module with a specific name residing at a given
+    /// account at a specified ledger version (AKA transaction version). If the
+    /// ledger version is not specified in the request, the latest ledger version
+    /// is used.
+    ///
+    /// The Aptos nodes prune account state history, via a configurable time window (link).
+    /// If the requested data has been pruned, the server responds with a 404.
+    #[oai(
+        path = "/accounts/:address/module/:module_name",
+        method = "get",
+        operation_id = "get_account_module",
+        tag = "ApiTags::Accounts"
+    )]
+    async fn get_account_module(
+        &self,
+        accept_type: AcceptType,
+        address: Path<Address>,
+        module_name: Path<IdentifierWrapper>,
+        ledger_version: Query<Option<U64>>,
+    ) -> BasicResultWith404<MoveModuleBytecode> {
+        fail_point_poem("endpoint_get_account_module")?;
+        self.module(&accept_type, address.0, module_name.0, ledger_version.0)
+    }
+
+    /// Get table item
+    ///
+    /// Get a table item from the table identified by {table_handle} in the
+    /// path and the "key" (TableItemRequest) provided in the request body.
+    ///
+    /// This is a POST endpoint because the "key" for requesting a specific
+    /// table item (TableItemRequest) could be quite complex, as each of its
+    /// fields could themselves be composed of other structs. This makes it
+    /// impractical to express using query params, meaning GET isn't an option.
+    #[oai(
+        path = "/tables/:table_handle/item",
+        method = "post",
+        operation_id = "get_table_item",
+        tag = "ApiTags::Tables"
+    )]
+    async fn get_table_item(
+        &self,
+        accept_type: AcceptType,
+        table_handle: Path<U128>,
+        table_item_request: Json<TableItemRequest>,
+        ledger_version: Query<Option<U64>>,
+    ) -> BasicResultWith404<MoveValue> {
+        fail_point_poem("endpoint_get_table_item")?;
+        self.table_item(
+            &accept_type,
+            table_handle.0,
+            table_item_request.0,
+            ledger_version.0,
+        )
+    }
 }
 
-// GET /tables/<table_handle>/item
-pub fn get_table_item(context: Context) -> BoxedFilter<(impl Reply,)> {
-    warp::path!("tables" / TableHandleParam / "item")
-        .and(warp::post())
-        .and(warp::body::content_length_limit(
-            context.content_length_limit(),
-        ))
-        .and(warp::body::json::<TableItemRequest>())
-        .and(context.filter())
-        .and(warp::query::<Version>())
-        .map(|handle, body, ctx, version: Version| (version.version, handle, body, ctx))
-        .untuple_one()
-        .and_then(handle_get_table_item)
-        .with(metrics("get_table_item"))
-        .boxed()
-}
-
-async fn handle_get_account_resource(
-    ledger_version: Option<LedgerVersionParam>,
-    address: AddressParam,
-    struct_tag: MoveStructTagParam,
-    context: Context,
-) -> anyhow::Result<impl Reply, Rejection> {
-    fail_point("endpoint_query_resource")?;
-    let struct_tag = struct_tag.parse("struct tag")?;
-    Ok(State::new(ledger_version, context)?.resource(
-        address.parse("account address")?.into(),
-        struct_tag
-            .clone()
-            .try_into()
-            .map_err(|_| Error::invalid_param("resource_type", struct_tag))?,
-    )?)
-}
-
-async fn handle_get_account_module(
-    ledger_version: Option<LedgerVersionParam>,
-    address: AddressParam,
-    name: MoveIdentifierParam,
-    context: Context,
-) -> anyhow::Result<impl Reply, Rejection> {
-    fail_point("endpoint_get_account_module")?;
-    Ok(State::new(ledger_version, context)?.module(
-        address.parse("account address")?.into(),
-        name.parse("module name")?,
-    )?)
-}
-
-async fn handle_get_table_item(
-    ledger_version: Option<LedgerVersionParam>,
-    handle: TableHandleParam,
-    body: TableItemRequest,
-    context: Context,
-) -> Result<impl Reply, Rejection> {
-    fail_point("endpoint_get_table_item")?;
-    Ok(State::new(ledger_version, context)?.table_item(handle.parse("table handle")?, body)?)
-}
-
-pub(crate) struct State {
-    state_view: DbStateView,
-    ledger_version: aptos_types::transaction::Version,
-    latest_ledger_info: LedgerInfo,
-    db: Arc<dyn DbReader>,
-}
-
-impl State {
-    pub fn new(
-        ledger_version: Option<LedgerVersionParam>,
-        context: Context,
-    ) -> Result<Self, Error> {
-        let latest_ledger_info = context.get_latest_ledger_info()?;
-        let ledger_version = ledger_version
-            .map(|v| v.parse("ledger version"))
-            .unwrap_or_else(|| Ok(latest_ledger_info.version()))?;
+impl StateApi {
+    fn preprocess_request<E: NotFoundError + InternalError>(
+        &self,
+        requested_ledger_version: Option<U64>,
+    ) -> Result<(LedgerInfo, u64, DbStateView), E> {
+        let latest_ledger_info = self.context.get_latest_ledger_info()?;
+        let ledger_version: u64 = requested_ledger_version
+            .map(|v| v.0)
+            .unwrap_or_else(|| latest_ledger_info.version());
 
         if ledger_version > latest_ledger_info.version() {
-            return Err(Error::not_found(
+            return Err(build_not_found(
                 "ledger",
                 TransactionId::Version(U64::from(ledger_version)),
                 latest_ledger_info.version(),
             ));
         }
 
-        let state_view = context.state_view_at_version(ledger_version)?;
+        let state_view = self.context.state_view_at_version(ledger_version)
+            .context(format!("Failed to get state view at version {} even after confirming the ledger has advanced past that version to {}", ledger_version, latest_ledger_info.version()))
+            .map_err(E::internal)?;
 
-        Ok(Self {
-            state_view,
-            ledger_version,
-            latest_ledger_info,
-            db: context.db.clone(),
-        })
+        Ok((latest_ledger_info, ledger_version, state_view))
     }
 
-    pub fn resource(
-        self,
-        address: AccountAddress,
-        struct_tag: StructTag,
-    ) -> Result<impl Reply, Error> {
-        let resource_key = ResourceKey::new(address, struct_tag.clone());
+    fn resource(
+        &self,
+        accept_type: &AcceptType,
+        address: Address,
+        resource_type: MoveStructTag,
+        ledger_version: Option<U64>,
+    ) -> BasicResultWith404<MoveResource> {
+        let resource_type: StructTag = resource_type
+            .try_into()
+            .context("Failed to parse given resource type")
+            .map_err(BasicErrorWith404::bad_request)?;
+        let resource_key = ResourceKey::new(address.into(), resource_type.clone());
         let access_path = AccessPath::resource_access_path(resource_key.clone());
         let state_key = StateKey::AccessPath(access_path);
-        let bytes = self
-            .state_view
-            .get_state_value(&state_key)?
-            .ok_or_else(|| Error::not_found("Resource", resource_key, self.ledger_version))?;
+        let (ledger_info, ledger_version, state_view) = self.preprocess_request(ledger_version)?;
+        let bytes = state_view
+            .get_state_value(&state_key)
+            .context(format!("Failed to query DB to check for {:?}", state_key))
+            .map_err(BasicErrorWith404::internal)?
+            .ok_or_else(|| build_not_found("Resource", resource_key, ledger_version))?;
 
-        let resource = self
-            .state_view
+        let resource = state_view
             .as_move_resolver()
-            .as_converter(self.db.clone())
-            .try_into_resource(&struct_tag, &bytes)?;
-        Response::new(self.latest_ledger_info, &resource)
+            .as_converter(self.context.db.clone())
+            .try_into_resource(&resource_type, &bytes)
+            .context("Failed to deserialize resource data retrieved from DB")
+            .map_err(BasicErrorWith404::internal)?;
+
+        BasicResponse::try_from_rust_value((
+            resource,
+            &ledger_info,
+            BasicResponseStatus::Ok,
+            accept_type,
+        ))
     }
 
-    pub fn module(self, address: AccountAddress, name: Identifier) -> Result<impl Reply, Error> {
-        let module_id = ModuleId::new(address, name);
+    pub fn module(
+        &self,
+        accept_type: &AcceptType,
+        address: Address,
+        name: IdentifierWrapper,
+        ledger_version: Option<U64>,
+    ) -> BasicResultWith404<MoveModuleBytecode> {
+        let module_id = ModuleId::new(address.into(), name.into());
         let access_path = AccessPath::code_access_path(module_id.clone());
         let state_key = StateKey::AccessPath(access_path);
-        let bytes = self
-            .state_view
-            .get_state_value(&state_key)?
-            .ok_or_else(|| Error::not_found("Module", module_id, self.ledger_version))?;
+        let (ledger_info, ledger_version, state_view) = self.preprocess_request(ledger_version)?;
+        let bytes = state_view
+            .get_state_value(&state_key)
+            .context(format!("Failed to query DB to check for {:?}", state_key))
+            .map_err(BasicErrorWith404::internal)?
+            .ok_or_else(|| build_not_found("Module", module_id, ledger_version))?;
 
         let module = MoveModuleBytecode::new(bytes)
             .try_parse_abi()
-            .map_err(Error::internal)?;
-        Response::new(self.latest_ledger_info, &module)
+            .context("Failed to parse move module ABI from bytes retrieved from storage")
+            .map_err(BasicErrorWith404::internal)?;
+
+        BasicResponse::try_from_rust_value((
+            module,
+            &ledger_info,
+            BasicResponseStatus::Ok,
+            accept_type,
+        ))
     }
 
     pub fn table_item(
-        self,
-        handle: TableHandle,
-        body: TableItemRequest,
-    ) -> Result<impl Reply, Error> {
-        let TableItemRequest {
-            key_type,
-            value_type,
-            key,
-        } = body;
-        let key_type = key_type.try_into()?;
-        let value_type = value_type.try_into()?;
+        &self,
+        accept_type: &AcceptType,
+        table_handle: U128,
+        table_item_request: TableItemRequest,
+        ledger_version: Option<U64>,
+    ) -> BasicResultWith404<MoveValue> {
+        let key_type = table_item_request
+            .key_type
+            .try_into()
+            .context("Failed to parse key_type")
+            .map_err(BasicErrorWith404::bad_request)?;
+        let value_type = table_item_request
+            .value_type
+            .try_into()
+            .context("Failed to parse value_type")
+            .map_err(BasicErrorWith404::bad_request)?;
+        let key = table_item_request.key;
 
-        let resolver = self.state_view.as_move_resolver();
-        let converter = resolver.as_converter(self.db.clone());
+        let (ledger_info, ledger_version, state_view) = self.preprocess_request(ledger_version)?;
+
+        let resolver = state_view.as_move_resolver();
+        let converter = resolver.as_converter(self.context.db.clone());
 
         let vm_key = converter
             .try_into_vm_value(&key_type, key.clone())
-            .map_err(Error::bad_request)?;
-
+            .map_err(BasicErrorWith404::bad_request)?;
         let raw_key = vm_key
             .undecorate()
             .simple_serialize()
-            .ok_or_else(|| Error::internal(anyhow!("Key failed to serialize.")))?;
-        let state_key = StateKey::table_item(handle, raw_key);
+            .ok_or_else(|| BasicErrorWith404::internal_str("Failed to serialize table key"))?;
 
-        let bytes = self
-            .state_view
-            .get_state_value(&state_key)?
-            .ok_or_else(|| Error::not_found("table handle or item", key, self.ledger_version))?;
+        let state_key = StateKey::table_item(TableHandle(table_handle.0), raw_key);
+        let bytes = state_view
+            .get_state_value(&state_key)
+            .context(format!(
+                "Failed when trying to retrieve table item from the DB with key: {}",
+                key
+            ))
+            .map_err(BasicErrorWith404::internal)?
+            .ok_or_else(|| build_not_found("table handle or item", key, ledger_version))?;
 
-        let move_value = converter.try_into_move_value(&value_type, &bytes)?;
-        Response::new(self.latest_ledger_info, &move_value)
+        let move_value = converter
+            .try_into_move_value(&value_type, &bytes)
+            .context("Failed to deserialize table item retrieved from DB")
+            .map_err(BasicErrorWith404::internal)?;
+
+        BasicResponse::try_from_rust_value((
+            move_value,
+            &ledger_info,
+            BasicResponseStatus::Ok,
+            accept_type,
+        ))
     }
 }
