9174cef92157349ced73b14b38d5eefcad6fb7ff
Zach Denton <z@chdenton.com>
2022-08-16 12:53:50 -0400

    Delete unnecessary code.

--- ecosystem/platform/server/app/views/projects/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.partial! "projects/project", project: @project
--- ecosystem/platform/server/spec/factories/project_milestones.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :project_milestone do
-    project { nil }
-    title { "MyString" }
-    start_date { "2022-08-11" }
-    end_date { "2022-08-11" }
-  end
-end
