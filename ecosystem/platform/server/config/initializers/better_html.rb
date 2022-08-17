1d695f588decb2bde47545b6f2b91616b9aabf03
Zach Denton <z@chdenton.com>
2022-08-17 13:56:12 -0400

    Lint.

--- ecosystem/platform/server/config/initializers/better_html.rb
+++ ecosystem/platform/server/config/initializers/better_html.rb
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 BetterHtml.configure do |config|
-  config.allow_single_quoted_attributes = true
   # Ignore ERB files from gems or Rails itself.
   config.template_exclusion_filter = proc { |filename| !filename.start_with?(Rails.root.to_s) }
 end
