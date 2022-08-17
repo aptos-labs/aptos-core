a6b0596e2b7fbe4144e80e2fadb66c33c23c9ee9
Zach Denton <z@chdenton.com>
2022-08-17 13:48:28 -0400

    Update email template for notifications.

--- ecosystem/platform/server/config/initializers/better_html.rb
+++ ecosystem/platform/server/config/initializers/better_html.rb
@@ -3,4 +3,8 @@
 # Copyright (c) Aptos
 # SPDX-License-Identifier: Apache-2.0
 
-BetterHtml.config = BetterHtml::Config.new(YAML.load(File.read(Rails.root.join('.better-html.yml'))))
+BetterHtml.configure do |config|
+  config.allow_single_quoted_attributes = true
+  # Ignore ERB files from gems or Rails itself.
+  config.template_exclusion_filter = proc { |filename| !filename.start_with?(Rails.root.to_s) }
+end
