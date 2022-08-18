fe28b577445b4faf8f15aaa3d17d8cf643f48354
Sital Kedia <skedia@aptoslabs.com>
2022-08-17 18:25:54 -0700

    rebase

--- state-sync/storage-service/server/src/tests.rs
+++ state-sync/storage-service/server/src/tests.rs
@@ -10,9 +10,8 @@ use aptos_config::config::StorageServiceConfig;
 use aptos_crypto::{ed25519::Ed25519PrivateKey, HashValue, PrivateKey, SigningKey, Uniform};
 use aptos_logger::Level;
 use aptos_time_service::{MockTimeService, TimeService};
-use aptos_types::aggregated_signature::AggregatedSignature;
+use aptos_types::aggregate_signature::AggregateSignature;
 use aptos_types::write_set::WriteSet;
->>>>>>> 2875c22e2a (Addressed comments)
 use aptos_types::{
     account_address::AccountAddress,
     block_info::BlockInfo,
@@ -2032,7 +2031,8 @@ fn create_epoch_ending_ledger_infos_using_sizes(
     // Create the ledger infos with signatures
     (0..num_ledger_infos)
         .map(|_| {
-            let multi_signatures = MultiSignature::new(BitVec::from(random_bytes.clone()), None);
+            let multi_signatures =
+                AggregateSignature::new(BitVec::from(random_bytes.clone()), None);
             LedgerInfoWithSignatures::new(ledger_info.clone(), multi_signatures)
         })
         .collect()
