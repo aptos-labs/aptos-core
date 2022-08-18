24454f8e8cb62184e0339d4d9f75779c64de6f48
Greg <greg@gnazar.io>
2022-08-17 22:59:35 -0700

    [api] Add BCS account testing

--- testsuite/smoke-test/src/rest_api.rs
+++ testsuite/smoke-test/src/rest_api.rs
@@ -1,9 +1,13 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
+use aptos::common::types::account_address_from_public_key;
+use aptos_crypto::PrivateKey;
 use aptos_transaction_builder::aptos_stdlib;
 use aptos_types::account_config::CORE_CODE_ADDRESS;
+use aptos_types::transaction::authenticator::AuthenticationKey;
 use forge::Swarm;
+use std::convert::TryFrom;
 
 use crate::smoke_test_environment::new_local_swarm_with_aptos;
 
@@ -50,4 +54,35 @@ async fn test_basic_client() {
         .unwrap();
 
     info.client().get_transactions(None, None).await.unwrap();
+}
+
+#[tokio::test]
+async fn test_bcs() {
+    // FIXME: Use swarm instead of local node
+    // let mut swarm = new_local_swarm_with_aptos(1).await;
+    //let mut info = swarm.aptos_public_info();
+    //let client = info.client();
+    let rest_api = reqwest::Url::parse("http://localhost:8080/v1").unwrap();
+    let client = aptos_rest_client::Client::new(rest_api.clone());
+    let faucet_client = aptos_rest_client::FaucetClient::new(
+        reqwest::Url::parse("http://localhost:8081").unwrap(),
+        rest_api,
+    );
+
+    // Create account
+    let mut keygen = aptos_keygen::KeyGen::from_seed([0u8; 32]);
+    let private_key = keygen.generate_ed25519_private_key();
+    let public_key = private_key.public_key();
+    let account = account_address_from_public_key(&public_key);
+
+    // Fund account
+    faucet_client.fund(account, 10000000).await.unwrap();
+
+    // Check get account
+    let account_resource = client.get_account_bcs(account).await.unwrap().into_inner();
+    let expected_auth_key = AuthenticationKey::ed25519(&public_key);
+    let onchain_auth_key =
+        AuthenticationKey::try_from(account_resource.authentication_key()).unwrap();
+    assert_eq!(expected_auth_key, onchain_auth_key);
+    assert_eq!(0, account_resource.sequence_number());
 }
