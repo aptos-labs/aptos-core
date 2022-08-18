ef6724a3852f82b13427b1b1038dfe8cb9a4c7c8
bowenyang007 <carlyang007@gmail.com>
2022-08-18 16:10:56 -0700

    failed attempt at using buf generate w/ tonic

---
+++ ecosystem/sf-indexer/aptos-sf-indexer/src/pb/mod.rs
@@ -0,0 +1,10 @@
+// @generated
+pub mod sf {
+    pub mod substreams {
+        // @@protoc_insertion_point(attribute:sf.substreams.v1)
+        pub mod v1 {
+            include!("sf.substreams.v1.rs");
+            // @@protoc_insertion_point(sf.substreams.v1)
+        }
+    }
+}
