d03a126c6776057effa75d303b18f2b990e6c41b
Greg <greg@gnazar.io>
2022-08-17 22:10:01 -0700

    [aptos-cli] Remove all unnecessary clones

--- crates/aptos/src/account/list.rs
+++ crates/aptos/src/account/list.rs
@@ -107,15 +107,15 @@ impl CliCommand<Vec<serde_json::Value>> for ListAccount {
                 .map_err(map_err_func)?
                 .into_inner()
                 .into_iter()
-                .map(|module| (json!(module.try_parse_abi().unwrap())))
+                .map(|module| json!(module.try_parse_abi().unwrap()))
                 .collect::<Vec<serde_json::Value>>(),
             ListQuery::Resources => client
                 .get_account_resources(account)
                 .await
                 .map_err(map_err_func)?
                 .into_inner()
-                .iter()
-                .map_into(|resource| {
+                .into_iter()
+                .map(|resource| {
                     let mut map = serde_json::Map::new();
                     map.insert(resource.resource_type.to_string(), resource.data);
                     serde_json::Value::Object(map)
