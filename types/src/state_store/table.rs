70ab190f63b195e7bfc9b805829dd06a02e4edb3
Zekun Li <li.zekun@gmail.com>
2022-08-18 21:26:19 -0700

    [move] upgrade table handle to HashValue (u256)

--- types/src/state_store/table.rs
+++ types/src/state_store/table.rs
@@ -1,33 +1,48 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use core::num::ParseIntError;
+use aptos_crypto::{hash::HashValueParseError, HashValue};
 use move_deps::move_core_types::language_storage::TypeTag;
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
+use std::{convert::TryInto, str::FromStr};
 
 #[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 #[cfg_attr(any(test, feature = "fuzzing"), derive(proptest_derive::Arbitrary))]
-pub struct TableHandle(pub u128);
+pub struct TableHandle {
+    pub low: u128,
+    pub high: u128,
+}
 
 impl TableHandle {
     pub fn size(&self) -> usize {
-        std::mem::size_of_val(&self.0)
+        std::mem::size_of_val(&self)
     }
 }
 
 impl FromStr for TableHandle {
-    type Err = ParseIntError;
+    type Err = HashValueParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let handle = u128::from_str(s)?;
-        Ok(Self(handle))
+        let hash = HashValue::from_str(s)?;
+        Ok(hash.into())
     }
 }
 
 impl From<move_deps::move_table_extension::TableHandle> for TableHandle {
     fn from(hdl: move_deps::move_table_extension::TableHandle) -> Self {
-        Self(hdl.0)
+        Self {
+            low: hdl.low,
+            high: hdl.high,
+        }
+    }
+}
+
+impl From<HashValue> for TableHandle {
+    fn from(hash: HashValue) -> Self {
+        let bytes = hash.to_vec();
+        let low = u128::from_le_bytes(bytes[0..16].try_into().unwrap());
+        let high = u128::from_le_bytes(bytes[16..32].try_into().unwrap());
+        Self { low, high }
     }
 }
 
