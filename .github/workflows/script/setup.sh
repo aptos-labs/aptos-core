33d860a45ecad9586d3ff08cb223056a0e12a280
Xiaying Peng <xiaying.peng@coinbase.com>
2022-08-18 18:06:37 -0700

    Update setup.sh

--- .github/workflows/script/setup.sh
+++ .github/workflows/script/setup.sh
@@ -2,37 +2,28 @@
 
 git checkout rosetta-stable
 
-# # Build with the rosetta-stable branch (I use the git hash here)
-# export GIT_REF=dd2bfaf31fcd31b79b9f65e7fbc812338256bbd0
+# Build with the rosetta-stable branch (I use the git hash here)
+export GIT_REF=dd2bfaf31fcd31b79b9f65e7fbc812338256bbd0
 
-# nohup docker/rosetta/docker-build-rosetta.sh  > /dev/null 2>&1 &
+nohup docker/rosetta/docker-build-rosetta.sh  > /dev/null 2>&1 &
 
-# # Build data path for node & files
-# mkdir -p data
+# Build data path for node & files
+mkdir -p data
 
-# # Download node config (and fix the paths)
-# curl -s https://raw.githubusercontent.com/aptos-labs/aptos-core/rosetta-stable/config/src/config/test_data/public_full_node.yaml 2> /dev/null | sed 's/\.\//\/opt\/aptos\//g' > data/fullnode.yaml
+# Download node config (and fix the paths)
+curl -s https://raw.githubusercontent.com/aptos-labs/aptos-core/rosetta-stable/config/src/config/test_data/public_full_node.yaml 2> /dev/null | sed 's/\.\//\/opt\/aptos\//g' > data/fullnode.yaml
 
-# # Download waypoint and genesis necessary to start a node
-# curl -s -o data/genesis.blob https://rosetta.aptosdev.com/genesis.blob
-# curl -s -o data/waypoint.txt https://rosetta.aptosdev.com/waypoint.txt
+# Download waypoint and genesis necessary to start a node
+curl -s -o data/genesis.blob https://rosetta.aptosdev.com/genesis.blob
+curl -s -o data/waypoint.txt https://rosetta.aptosdev.com/waypoint.txt
 
-# # Run the node in online remote mode (detached mode)
-# docker run -d -p 8082:8082 --rm -v $(pwd)/data:/opt/aptos aptos-core:rosetta-$GIT_REF online-remote --rest-api-url https://rosetta.aptosdev.com
+# Run the node in online remote mode (detached mode)
+docker run -d -p 8082:8082 --rm -v $(pwd)/data:/opt/aptos aptos-core:rosetta-$GIT_REF online-remote --rest-api-url https://rosetta.aptosdev.com
 
 
-# sleep 1500
+sleep 1500
 
-# block_tip=($(curl -s --location --request POST 'http://localhost:8082/network/status' \
-# --header 'Content-Type: application/json' \
-# --data-raw '{
-#     "network_identifier": {
-#             "blockchain": "aptos",
-#             "network": "TESTING"
-#         }
-# }' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["current_block_identifier"]["index"])'))
-
-block_tip=($(curl -s --location --request POST 'https://rosetta.aptosdev.com/network/status' \
+block_tip=($(curl -s --location --request POST 'http://localhost:8082/network/status' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "network_identifier": {
