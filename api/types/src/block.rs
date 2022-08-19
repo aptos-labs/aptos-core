7c9b80909420c4c05c6fa94fdab4eb838c301cd7
Greg <greg@gnazar.io>
2022-08-18 19:58:17 -0700

    [api] Remove old BlockInfo

--- api/types/src/block.rs
+++ api/types/src/block.rs
@@ -8,19 +8,19 @@ use serde::{Deserialize, Serialize};
 // TODO: Consider including this in the API.
 // If we do that, change these u64s to U64.
 
-/// A description of a block
-#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
-pub struct BlockInfo {
-    pub block_height: u64,
+#[derive(Debug, Clone, Serialize, Deserialize, Object)]
+pub struct Block {
+    pub block_height: U64,
     pub block_hash: HashValue,
-    pub block_timestamp: u64,
-    pub start_version: u64,
-    pub end_version: u64,
-    pub num_transactions: u16,
+    pub block_timestamp: U64,
+    pub first_version: U64,
+    pub last_version: U64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transactions: Option<Vec<Transaction>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Object)]
-pub struct Block {
+pub struct BcsBlock {
     pub block_height: U64,
     pub block_hash: HashValue,
     pub block_timestamp: U64,
