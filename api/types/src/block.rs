4b87a41487c550139edd0fd6a70792fbd913ec07
Greg <greg@gnazar.io>
2022-08-18 19:59:37 -0700

    [api] Add BCS type for block output

--- api/types/src/block.rs
+++ api/types/src/block.rs
@@ -1,12 +1,9 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{HashValue, Transaction, U64};
+use crate::{HashValue, Transaction, TransactionOnChainData, U64};
 use poem_openapi::Object;
 use serde::{Deserialize, Serialize};
-
-// TODO: Consider including this in the API.
-// If we do that, change these u64s to U64.
 
 #[derive(Debug, Clone, Serialize, Deserialize, Object)]
 pub struct Block {
@@ -21,11 +18,11 @@ pub struct Block {
 
 #[derive(Debug, Clone, Serialize, Deserialize, Object)]
 pub struct BcsBlock {
-    pub block_height: U64,
-    pub block_hash: HashValue,
-    pub block_timestamp: U64,
-    pub first_version: U64,
-    pub last_version: U64,
+    pub block_height: u64,
+    pub block_hash: aptos_crypto::HashValue,
+    pub block_timestamp: u64,
+    pub first_version: u64,
+    pub last_version: u64,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub transactions: Option<Vec<Transaction>>,
+    pub transactions: Option<Vec<TransactionOnChainData>>,
 }
