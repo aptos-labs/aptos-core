c44c0cc965e6ebb5245f5460a9397fa1e7840f72
Greg <greg@gnazar.io>
2022-08-20 21:38:32 -0700

    [api] Reorder API error codes and add docs

--- api/types/src/error.rs
+++ api/types/src/error.rs
@@ -46,46 +46,72 @@ impl AptosError {
 #[oai(rename_all = "snake_case")]
 #[serde(rename_all = "snake_case")]
 pub enum AptosErrorCode {
+    /// Account not found at the requested version
+    AccountNotFound = 101,
+    /// Resource not found at the requested version
+    ResourceNotFound = 102,
+    /// Module not found at the requested version
+    ModuleNotFound = 103,
+    /// Struct field not found at the requested version
+    StructFieldNotFound = 104,
+    /// Ledger version not found at the requested version
+    ///
+    /// Usually means that the version is ahead of the latest version
+    VersionNotFound = 105,
+    /// Transaction not found at the requested version or with the requested hash
+    TransactionNotFound = 106,
+    /// Table item not found at the requested version
+    TableItemNotFound = 107,
+    /// Block not found at the requested version or height
+    ///
+    /// Usually means the block is fully or partially pruned or the height / version is ahead
+    /// of the latest version
+    BlockNotFound = 108,
+
+    /// Ledger version is pruned
+    VersionPruned = 200,
+    /// Block is fully or partially pruned
+    BlockPruned = 201,
+
+    /// The API's inputs were invalid
+    InvalidInput = 300,
+    /// The start param given for paging is invalid.
+    InvalidStartParam = 301,
+    /// The limit param given for paging is invalid.
+    InvalidLimitParam = 302,
+    /// The event key for events is invalid.
+    InvalidEventKey = 303,
+
+    /// The transaction submitted was invalid.
+    InvalidSubmittedTransaction = 400,
+    /// The transaction was an invalid update to an already submitted transaction.
+    InvalidTransactionUpdate = 401,
+    /// The sequence number for the transaction is behind the latest sequence number.
+    SequenceNumberTooOld = 402,
+    /// The submitted transaction failed VM checks.
+    VmError = 403,
+    /// The transaction failed to submit.
+    TransactionSubmissionFailed = 404,
+
+    /// Health check failed.
+    HealthCheckFailed = 500,
+    /// The mempool is full, no new transactions can be submitted.
+    MempoolIsFull = 501,
+    /// The mempool is full for this account's quota, no new transactions can be submitted.
+    MempoolIsFullForAccount = 502,
+
+    /// Internal server error
+    InternalError = 600,
     /// The API failed to read from storage for this request, not because of a
     /// bad request, but because of some internal error.
-    ReadFromStorageError = 1,
-
+    ReadFromStorageError = 601,
     /// The data we read from the DB was not valid BCS.
-    InvalidBcsInStorageError = 2,
-
+    InvalidBcsInStorageError = 602,
     /// We were unexpectedly unable to convert a Rust type to BCS.
-    BcsSerializationError = 3,
+    BcsSerializationError = 603,
+    /// Error from the web framework
+    WebFrameworkError = 604,
 
-    /// The start param given for paging is invalid.
-    InvalidStartParam = 4,
-
-    /// The limit param given for paging is invalid.
-    InvalidLimitParam = 5,
-
-    /// Health check failed
-    HealthCheckFailed = 6,
-
-    InternalError = 7,
-
-    AccountNotFound = 8,
-    ResourceNotFound = 9,
-    ModuleNotFound = 10,
-    StructFieldNotFound = 11,
-    VersionNotFound = 12,
-    VersionPruned = 13,
-    InvalidEventKey = 14,
-    BlockNotFound = 15,
-    TableItemNotFound = 16,
-    BlockPruned = 17,
-    InvalidInput = 18,
-    TransactionNotFound = 19,
-    TransactionSubmissionFailed = 20,
-    MempoolIsFull = 21,
-    VmError = 22,
-    InvalidTransactionUpdate = 23,
-    SequenceNumberTooOld = 24,
-    MempoolIsFullForAccount = 25,
-    BcsNotSupported = 26,
-    WebFrameworkError = 27,
-    InvalidSubmittedTransaction = 28,
+    /// BCS format is not supported on this API.
+    BcsNotSupported = 701,
 }
