3cd447adb2f2a298a42740e996b598976659e2de
aldenhu <msmouse@gmail.com>
2022-08-21 04:23:54 +0000

    [storage] save cross epoch stale node index

--- storage/aptosdb/src/state_store/mod.rs
+++ storage/aptosdb/src/state_store/mod.rs
@@ -41,6 +41,7 @@ use crate::{
     AptosDbError, LedgerStore, TransactionStore, OTHER_TIMERS_SECONDS,
 };
 
+use crate::epoch_by_version::EpochByVersionSchema;
 #[cfg(test)]
 use aptos_types::nibble::nibble_path::NibblePath;
 
@@ -169,6 +170,22 @@ impl StateDb {
             .next()
             .transpose()?
             .and_then(|((_, version), value_opt)| value_opt.map(|value| (version, value))))
+    }
+
+    /// Get the latest ended epoch strictly before required version, i.e. if the passed in version
+    /// ends an epoch, return one epoch early than that.
+    pub fn get_previous_epoch_ending(&self, version: Version) -> Result<Option<(u64, Version)>> {
+        if version == 0 {
+            return Ok(None);
+        }
+        let prev_version = version - 1;
+
+        let mut iter = self
+            .ledger_db
+            .iter::<EpochByVersionSchema>(ReadOptions::default())?;
+        // Search for the end of the previous epoch.
+        iter.seek_for_prev(&prev_version)?;
+        iter.next().transpose()
     }
 }
 
@@ -572,6 +589,7 @@ impl StateStore {
             node_hashes,
             version,
             base_version,
+            None, // previous epoch ending version
         )?;
         self.state_merkle_db.write_schemas(batch)?;
         Ok(hash)
