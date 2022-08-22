b11d4fc6ac408890b48b2de2dff555de871b03e6
David Wolinsky <isaac.wolinsky@gmail.com>
2022-08-21 20:16:35 -0700

    [aptos-cli] remove submit_entry_function
    this should never be used, it means our cli is programmed to call
    unknown entry points, which it shouldn't be doing -- at least not yet.
    
    unfortunately there has been use of this function with things that are
    in our standard library and it results in breakage too easily.

--- crates/aptos/src/common/types.rs
+++ crates/aptos/src/common/types.rs
@@ -22,17 +22,8 @@ use aptos_keygen::KeyGen;
 use aptos_rest_client::aptos_api_types::HashValue;
 use aptos_rest_client::error::RestError;
 use aptos_rest_client::{Client, Transaction};
-use aptos_sdk::{
-    move_types::{
-        ident_str,
-        language_storage::{ModuleId, TypeTag},
-    },
-    transaction_builder::TransactionFactory,
-    types::LocalAccount,
-};
-use aptos_types::transaction::{
-    authenticator::AuthenticationKey, EntryFunction, TransactionPayload,
-};
+use aptos_sdk::{transaction_builder::TransactionFactory, types::LocalAccount};
+use aptos_types::transaction::{authenticator::AuthenticationKey, TransactionPayload};
 use async_trait::async_trait;
 use clap::{ArgEnum, Parser};
 use hex::FromHexError;
@@ -1104,24 +1095,6 @@ impl TransactionOptions {
     pub fn sender_address(&self) -> CliTypedResult<AccountAddress> {
         let sender_key = self.private_key()?;
         Ok(account_address_from_public_key(&sender_key.public_key()))
-    }
-
-    /// Submits an entry function based on module name and function inputs
-    pub async fn submit_entry_function(
-        &self,
-        address: AccountAddress,
-        module: &'static str,
-        function: &'static str,
-        type_args: Vec<TypeTag>,
-        args: Vec<Vec<u8>>,
-    ) -> CliTypedResult<Transaction> {
-        let txn = TransactionPayload::EntryFunction(EntryFunction::new(
-            ModuleId::new(address, ident_str!(module).to_owned()),
-            ident_str!(function).to_owned(),
-            type_args,
-            args,
-        ));
-        self.submit_transaction(txn).await
     }
 
     /// Submit a transaction
