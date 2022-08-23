7eab7b9856042e3d9366df5a5f73b9534de071bc
Balaji Arun <balajia@vt.edu>
2022-08-23 11:37:49 -0700

    [aptos-telemetry] Authenticate Validator Full Nodes (#3145)
    * This commit adds support for authenticating validator full nodes using the fullnode_network_addresses in the onchain config.
    * Fix, refactor, and add unit tests

--- crates/aptos-telemetry-service/src/context.rs
+++ crates/aptos-telemetry-service/src/context.rs
@@ -3,9 +3,10 @@
 
 use std::{convert::Infallible, sync::Arc};
 
+use crate::validator_cache::PeerSetCache;
 use crate::{
-    clients::humio, clients::victoria_metrics_api::Client as MetricsClient,
-    validator_cache::ValidatorSetCache, GCPBigQueryConfig, TelemetryServiceConfig,
+    clients::humio, clients::victoria_metrics_api::Client as MetricsClient, GCPBigQueryConfig,
+    TelemetryServiceConfig,
 };
 use aptos_crypto::noise;
 use gcp_bigquery_client::Client as BQClient;
@@ -15,7 +16,8 @@ use warp::Filter;
 #[derive(Clone)]
 pub struct Context {
     noise_config: Arc<noise::NoiseConfig>,
-    validator_cache: ValidatorSetCache,
+    validator_cache: PeerSetCache,
+    vfn_cache: PeerSetCache,
 
     pub gcp_bq_client: Option<BQClient>,
     pub gcp_bq_config: GCPBigQueryConfig,
@@ -31,7 +33,8 @@ pub struct Context {
 impl Context {
     pub fn new(
         config: &TelemetryServiceConfig,
-        validator_cache: ValidatorSetCache,
+        validator_cache: PeerSetCache,
+        vfn_cache: PeerSetCache,
         gcp_bigquery_client: Option<BQClient>,
         victoria_metrics_client: Option<MetricsClient>,
         humio_client: humio::IngestClient,
@@ -40,6 +43,7 @@ impl Context {
         Self {
             noise_config: Arc::new(noise::NoiseConfig::new(private_key)),
             validator_cache,
+            vfn_cache,
 
             gcp_bq_client: gcp_bigquery_client,
             gcp_bq_config: config.gcp_bq_config.clone(),
@@ -57,8 +61,12 @@ impl Context {
         warp::any().map(move || self.clone())
     }
 
-    pub fn validator_cache(&self) -> ValidatorSetCache {
+    pub fn validator_cache(&self) -> PeerSetCache {
         self.validator_cache.clone()
+    }
+
+    pub fn vfn_cache(&self) -> PeerSetCache {
+        self.vfn_cache.clone()
     }
 
     pub fn noise_config(&self) -> Arc<noise::NoiseConfig> {
