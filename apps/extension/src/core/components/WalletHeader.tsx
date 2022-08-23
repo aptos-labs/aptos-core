d3424dd8544128b7d9ddf656a62908c791c8f757
Kent White <kent@aptoslabs.com>
2022-08-23 00:00:07 -0700

    Clean up unused old wallet header

--- apps/extension/src/core/components/WalletHeader.tsx
+++ apps/extension/src/core/components/WalletHeader.tsx
@@ -4,28 +4,14 @@
 import React, { MouseEventHandler } from 'react';
 import {
   Box,
-  Center,
-  Grid,
   HStack,
-  Input,
-  InputGroup,
-  InputLeftAddon,
-  InputRightAddon,
   Tooltip,
   Text,
-  useClipboard,
   useColorMode,
   useDisclosure,
 } from '@chakra-ui/react';
-import {
-  ChevronLeftIcon, DragHandleIcon,
-} from '@chakra-ui/icons';
-import {
-  secondaryBorderColor,
-  secondaryHeaderInputBgColor,
-  secondaryHeaderInputHoverBgColor,
-} from 'core/colors';
-import { IoIosWallet } from '@react-icons/all-files/io/IoIosWallet';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { secondaryBorderColor } from 'core/colors';
 import { useNavigate } from 'react-router-dom';
 import AccountDrawer from 'core/components/AccountDrawer';
 import useGlobalStateContext from 'core/hooks/useGlobalState';
@@ -65,17 +51,17 @@ function BackButton({ onClick }: ButtonProps) {
   );
 }
 
-interface NavigationBarProps {
+interface WalletHeaderProps {
   accessoryButton?: React.ReactNode,
   showBackButton?: boolean;
   title?: string
 }
 
-function NavigationBar({
+export default function WalletHeader({
   accessoryButton,
   showBackButton,
   title,
-}: NavigationBarProps) {
+}: WalletHeaderProps) {
   const navigate = useNavigate();
   const { colorMode } = useColorMode();
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -112,100 +98,5 @@ function NavigationBar({
       <AccountDrawer isOpen={isOpen} onClose={onClose} />
     </Box>
 
-  );
-}
-
-export default function WalletHeader({
-  accessoryButton,
-  showBackButton,
-  title,
-}: NavigationBarProps) {
-  const { activeAccount, activeAccountAddress } = useGlobalStateContext();
-  const { isOpen, onClose, onOpen } = useDisclosure();
-  const { colorMode } = useColorMode();
-  const { hasCopied, onCopy } = useClipboard(activeAccountAddress || '');
-  const navigate = useNavigate();
-
-  if ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development')) {
-    return (
-      <NavigationBar
-        accessoryButton={accessoryButton}
-        title={title}
-        showBackButton={showBackButton}
-      />
-    );
-  }
-
-  return (
-    <Grid
-      maxW="100%"
-      width="100%"
-      py={4}
-      height="64px"
-      templateColumns={showBackButton ? '40px 1fr' : '1fr'}
-      borderBottomColor={secondaryBorderColor[colorMode]}
-      borderBottomWidth="1px"
-    >
-      {(showBackButton) ? (
-        <Center cursor="pointer" onClick={() => navigate(-1)}>
-          <ChevronLeftIcon fontSize="xl" />
-        </Center>
-      ) : <Box />}
-      <Center width="100%">
-        <HStack
-          px={4}
-          pl={showBackButton ? 0 : 4}
-          width="100%"
-        >
-          <InputGroup size="sm">
-            <InputLeftAddon
-              borderLeftRadius=".5rem"
-              bgColor={secondaryHeaderInputBgColor[colorMode]}
-              borderColor={secondaryBorderColor[colorMode]}
-              borderWidth="0px"
-            >
-              <IoIosWallet />
-            </InputLeftAddon>
-            <Tooltip
-              label={hasCopied ? 'Copied!' : 'Copy address'}
-              closeDelay={300}
-            >
-              <Input
-                size="sm"
-                readOnly
-                value={`${activeAccount?.name} (${activeAccountAddress})`}
-                onClick={onCopy}
-                borderColor={secondaryBorderColor[colorMode]}
-                bgColor={secondaryHeaderInputBgColor[colorMode]}
-                borderWidth="0px"
-                borderRadius={0}
-                cursor="pointer"
-                textOverflow="ellipsis"
-                _hover={{
-                  backgroundColor: secondaryHeaderInputHoverBgColor[colorMode],
-                }}
-              />
-            </Tooltip>
-            <Tooltip label="Switch wallet" closeDelay={300}>
-              <InputRightAddon
-                onClick={onOpen}
-                borderRightRadius=".5rem"
-                borderColor={secondaryBorderColor[colorMode]}
-                bgColor={secondaryHeaderInputBgColor[colorMode]}
-                borderWidth="0px"
-                cursor="pointer"
-                _hover={{
-                  backgroundColor: secondaryHeaderInputHoverBgColor[colorMode],
-                }}
-              >
-                <DragHandleIcon />
-              </InputRightAddon>
-            </Tooltip>
-          </InputGroup>
-          <AccountDrawer isOpen={isOpen} onClose={onClose} />
-        </HStack>
-      </Center>
-      <Box />
-    </Grid>
   );
 }
