7528ccd51a5d8e6e571f3e64a2d470f57f7a7077
Greg <greg@gnazar.io>
2022-08-23 17:24:29 -0700

    [api/sdk] Add generated files for new gas estimation API

---
+++ ecosystem/typescript/sdk/src/generated/schemas/$GasEstimation.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export const $GasEstimation = {
+    properties: {
+        gas_estimate: {
+            type: 'number',
+            isRequired: true,
+            format: 'uint64',
+        },
+    },
+} as const;
