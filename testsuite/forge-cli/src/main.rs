99a19c131398c91531c55afa1b89742657bfe41a
Brian Cho <brian@aptoslabs.com>
2022-08-24 14:34:49 -0700

    Small network

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -458,8 +458,8 @@ fn single_test_suite(test_name: &str) -> Result<ForgeConfig<'static>> {
 
 fn land_blocking_test_suite() -> ForgeConfig<'static> {
     ForgeConfig::default()
-        .with_initial_validator_count(NonZeroUsize::new(20).unwrap())
-        .with_initial_fullnode_count(10)
+        .with_initial_validator_count(NonZeroUsize::new(4).unwrap())
+        .with_initial_fullnode_count(4)
         .with_network_tests(&[&PerformanceBenchmarkWithFN])
 }
 
