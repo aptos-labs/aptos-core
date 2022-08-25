8fa001b0b86cc901157644217d9ed89d2c69172b
Jijun Leng <jijunleng@gmail.com>
2022-08-24 18:01:37 -0700

    [cli] Command to rotate account auth keys

--- aptos-move/e2e-move-tests/tests/rotate_auth_key.rs
+++ aptos-move/e2e-move-tests/tests/rotate_auth_key.rs
@@ -8,27 +8,11 @@ use aptos_types::{
     transaction::authenticator::AuthenticationKey,
 };
 
+use aptos::account::key_rotation::RotationProofChallenge;
 use cached_packages::aptos_stdlib;
 use e2e_move_tests::{assert_success, MoveHarness};
 use language_e2e_tests::account::Account;
 use move_deps::move_core_types::parser::parse_struct_tag;
-use serde::{Deserialize, Serialize};
-
-// This struct includes TypeInfo (account_address, module_name, and struct_name)
-// and RotationProofChallenge-specific information (sequence_number, originator, current_auth_key, and new_public_key)
-// Since the struct RotationProofChallenge is defined in "0x1::account::RotationProofChallenge",
-// we will be passing in "0x1" to `account_address`, "account" to `module_name`, and "RotationProofChallenge" to `struct_name`
-// Originator refers to the user's address
-#[derive(Serialize, Deserialize)]
-struct RotationProofChallenge {
-    account_address: AccountAddress,
-    module_name: String,
-    struct_name: String,
-    sequence_number: u64,
-    originator: AccountAddress,
-    current_auth_key: AccountAddress,
-    new_public_key: Vec<u8>,
-}
 
 #[test]
 fn rotate_auth_key_ed25519_to_ed25519() {
