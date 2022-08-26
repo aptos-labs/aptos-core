b6249742b78915ee0f5b069032896329a6e5dee7
Kent White <kent@aptoslabs.com>
2022-08-25 21:42:51 -0700

    Fix for account namespace rename

--- apps/extension/src/core/constants.ts
+++ apps/extension/src/core/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-export const accountNamespace = '0x1::account';
+export const accountNamespace = '0x1::aptos_account';
 export const coinNamespace = '0x1::coin';
 export const aptosCoinStructTag = '0x1::aptos_coin::AptosCoin';
 export const coinStoreStructTag = `${coinNamespace}::CoinStore`;
