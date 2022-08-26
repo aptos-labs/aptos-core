356ae72514a205b812081fcc34f6b7f01d9b85ea
Wolfgang Grieskamp <wg@aptoslabs.com>
2022-08-25 22:40:06 -0700

    [framework] Consolidate conversions
    This PR introduces as the matching counterpart to `std::bcs` the module `aptos_std::from_bcs`. It seems to be appropriate to tie the byte conversions together under the `bcs` label because that is how we have implemented them.
    
    All safe conversions from primitive types + Strings bytes to values are supported. Naming is as in `from_bcs::to_<type>`. Example:
    
    ```
    use std::bcs;
    use aptos_std::from_bcs;
    
    assert!(from_bcs::to_address(bcs::to_bytes(&@0xabcdef)) == @0xabcdef;
    ```
    
    Also the friend native `from_bytes` is now defined in `from_bcs` instead of `any`.

--- aptos-move/framework/src/natives/any.rs
+++ aptos-move/framework/src/natives/any.rs
@@ -1,8 +1,6 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::natives::{util, GasParameters};
-use move_deps::move_vm_runtime::native_functions::NativeFunction;
 use serde::{Deserialize, Serialize};
 
 /// Rust representation of the Move Any type
@@ -11,15 +9,4 @@ pub struct Any {
     pub type_name: String,
     #[serde(with = "serde_bytes")]
     pub data: Vec<u8>,
-}
-
-// The Any module hijacks just one function, from_bytes, from the util module. This
-// is a friend function which cannot be used across packages, so we have it both
-// in aptos_std and aptos_framework.
-pub fn make_all(gas_params: GasParameters) -> impl Iterator<Item = (String, NativeFunction)> {
-    let natives = [(
-        "from_bytes",
-        util::make_native_from_bytes(gas_params.util.from_bytes),
-    )];
-    crate::natives::helpers::make_module_natives(natives)
 }
