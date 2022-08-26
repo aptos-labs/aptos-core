1b30206928336efc0f9b4bb18bcab0d9bc30c87e
Ian Macalinao <ian@aptos.is>
2022-08-26 07:40:19 -0500

    fix tests

--- ecosystem/typescript/sdk/src/index.ts
+++ ecosystem/typescript/sdk/src/index.ts
@@ -4,6 +4,7 @@
 // All parts of our package are accessible as imports, but we re-export our higher level API here for convenience
 export * from "./aptos_account";
 export * from "./aptos_client";
+export * from "./bytes_to_hex";
 export * from "./coin_client";
 export * from "./faucet_client";
 export * from "./hex_string";
