e12f0902daef39a16aeb85612fc907304d32f6bb
Christian Theilemann <christian@aptoslabs.com>
2022-08-27 20:25:37 -0700

    dummy change

--- consensus/src/experimental/buffer_manager.rs
+++ consensus/src/experimental/buffer_manager.rs
@@ -316,6 +316,7 @@ impl BufferManager {
         }
     }
 
+    
     /// It pops everything in the buffer and if reconfig flag is set, it stops the main loop
     async fn process_reset_request(&mut self, request: ResetRequest) {
         let ResetRequest { tx, stop } = request;
