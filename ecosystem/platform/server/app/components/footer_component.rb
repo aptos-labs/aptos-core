47b537a7e1f9b131e174774c05c0d57eae9d1951
Joe <98909677+moonclavedev@users.noreply.github.com>
2022-08-28 19:44:14 -0700

    [CP] Update Whitepaper URL to abstract link

--- ecosystem/platform/server/app/components/footer_component.rb
+++ ecosystem/platform/server/app/components/footer_component.rb
@@ -11,7 +11,7 @@ class FooterComponent < ViewComponent::Base
     NavItem.new('/currents', 'Currents'),
     NavItem.new('/careers', 'Careers'),
     NavItem.new('https://www.linkedin.com/company/aptoslabs', 'Team'),
-    NavItem.new('https://aptos.dev/papers/whitepaper.pdf',
+    NavItem.new('https://aptos.dev/aptos-white-paper/aptos-white-paper-index/',
                 'Whitepaper'),
     NavItem.new('/privacy', 'Privacy'),
     NavItem.new('/terms', 'Terms')
