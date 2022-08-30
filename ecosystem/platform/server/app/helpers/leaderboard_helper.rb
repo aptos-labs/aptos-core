6c4013f5d428ff62edf2439b9c0db3d0089275d2
Joe <98909677+moonclavedev@users.noreply.github.com>
2022-08-30 16:58:11 -0700

    [CP] Create hidden dummy address to allow for Ctrl+F full string

--- ecosystem/platform/server/app/helpers/leaderboard_helper.rb
+++ ecosystem/platform/server/app/helpers/leaderboard_helper.rb
@@ -39,11 +39,11 @@ module LeaderboardHelper
 
   def liveness_icon(liveness)
     if liveness >= 97
-      render IconComponent.new(:check_circle, class: 'text-teal-400 w-5 h-5')
+      render IconComponent.new(:check_circle, class: 'text-teal-400 w-4 h-4')
     elsif liveness >= 95
-      render IconComponent.new(:check_circle, class: 'text-yellow-500 w-5 h-5')
+      render IconComponent.new(:check_circle, class: 'text-yellow-500 w-4 h-4')
     else
-      render IconComponent.new(:x_circle, class: 'text-red-500 w-5 h-5')
+      render IconComponent.new(:x_circle, class: 'text-red-500 w-4 h-4')
     end
   end
 end
