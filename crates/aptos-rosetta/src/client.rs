892ed8580bf03c618acfcac4d86f83b7b4b5e8be
Greg <greg@gnazar.io>
2022-08-30 14:40:24 -0700

    [aptos-rosetta] Add set operator to rosetta cli

--- crates/aptos-rosetta/src/client.rs
+++ crates/aptos-rosetta/src/client.rs
@@ -216,6 +216,38 @@ impl RosettaClient {
         .await
     }
 
+    pub async fn set_operator(
+        &self,
+        network_identifier: &NetworkIdentifier,
+        private_key: &Ed25519PrivateKey,
+        new_operator: AccountAddress,
+        expiry_time_secs: u64,
+        sequence_number: Option<u64>,
+        max_gas: Option<u64>,
+        gas_unit_price: Option<u64>,
+    ) -> anyhow::Result<TransactionIdentifier> {
+        let sender = self
+            .get_account_address(network_identifier.clone(), private_key)
+            .await?;
+        let mut keys = HashMap::new();
+        keys.insert(sender, private_key);
+
+        // A transfer operation is made up of a withdraw and a deposit
+        let operations = vec![Operation::set_operator(0, None, sender, new_operator)];
+
+        self.submit_operations(
+            sender,
+            network_identifier.clone(),
+            &keys,
+            operations,
+            expiry_time_secs,
+            sequence_number,
+            max_gas,
+            gas_unit_price,
+        )
+        .await
+    }
+
     /// Retrieves the account address from the derivation path if there isn't an overriding account specified
     async fn get_account_address(
         &self,
