70fc56404bbbd1ae93c7382aa0947386dfbb07e1
Ian Macalinao <ian@aptos.is>
2022-08-30 11:31:07 -0500

    re-add ts-ignore

--- ecosystem/typescript/sdk/src/generated/core/request.ts
+++ ecosystem/typescript/sdk/src/generated/core/request.ts
@@ -49,6 +49,7 @@ const base64 = (str: string): string => {
     try {
         return btoa(str);
     } catch (err) {
+        // @ts-ignore
         return Buffer.from(str).toString('base64');
     }
 };
