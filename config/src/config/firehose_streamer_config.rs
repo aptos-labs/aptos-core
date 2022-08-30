d99a7e8d21baa27b221474f35d3ae16286e99513
Bowen Yang <carlyang007@gmail.com>
2022-08-30 13:17:58 -0700

    [Indexer] Switch starting version to starting block (#3607)

--- config/src/config/firehose_streamer_config.rs
+++ config/src/config/firehose_streamer_config.rs
@@ -7,8 +7,11 @@ use serde::{Deserialize, Serialize};
 #[serde(default, deny_unknown_fields)]
 pub struct FirehoseStreamerConfig {
     pub enabled: bool,
-    // The version to start pushing out indexed data from, for the StreamingFast Firehose indexer
-    // Alternatively can set the `STARTING_VERSION` env var
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[deprecated = "please use starting_block instead"]
     pub starting_version: Option<u64>,
+    // The block to start pushing out indexed data from, for the StreamingFast Firehose indexer
+    // Alternatively can set the `STARTING_BLOCK` env var
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub starting_block: Option<u64>,
 }
