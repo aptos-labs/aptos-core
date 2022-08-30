440284ab51c37ed60b14d9cc1058d98fd4795c3b
aldenhu <msmouse@gmail.com>
2022-08-30 04:56:07 +0000

    [storage] refacor: rename make metadata schemas extensible
    verified that bcs::to_bytes(E::A) is the same one byte representation
    before the change, so it's not breaking.

--- storage/indexer/src/schema/indexer_metadata/test.rs
+++ storage/indexer/src/schema/indexer_metadata/test.rs
@@ -8,8 +8,8 @@ use schemadb::{schema::fuzzing::assert_encode_decode, test_no_panic_decoding};
 proptest! {
     #[test]
     fn test_encode_decode(
-        tag in any::<MetadataTag>(),
-        metadata in any::<Metadata>(),
+        tag in any::<MetadataKey>(),
+        metadata in any::<MetadataValue>(),
     ) {
         assert_encode_decode::<IndexerMetadataSchema>(&tag, &metadata);
     }
