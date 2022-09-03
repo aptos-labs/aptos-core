1c7a86a4c871987a1de72c1a593fcf950324e94f
Igor <igor-aptos@users.noreply.github.com>
2022-09-03 10:21:47 -0700

    [epoch_manager] Panic on stuck, so node can restart

--- consensus/src/epoch_manager.rs
+++ consensus/src/epoch_manager.rs
@@ -68,12 +68,13 @@ use futures::{
 use itertools::Itertools;
 use network::protocols::network::{ApplicationNetworkSender, Event};
 use safety_rules::SafetyRulesManager;
+use std::thread;
 use std::{
     cmp::Ordering,
     collections::HashMap,
     mem::{discriminant, Discriminant},
     sync::Arc,
-    time::Duration,
+    time::{Duration, Instant},
 };
 
 /// Range of rounds (window) that we might be calling proposer election
@@ -822,6 +823,45 @@ impl EpochManager {
     ) {
         // initial start of the processor
         self.await_reconfig_notification().await;
+
+        let panic_duration_s = self.config.panic_on_stuck_epoch_manager_duration_s;
+
+        let _handle = if panic_duration_s > 0 {
+            Some(
+                thread::Builder::new()
+                    .name("panic_thread".to_string())
+                    .spawn(move || {
+                        let mut last_change = Instant::now();
+                        let mut prev_value = 0;
+                        loop {
+                            let cur_value = counters::PROGRESS_CHECK_COUNT.get();
+                            if prev_value != cur_value {
+                                debug!("Progress check passed {} < {}", prev_value, cur_value);
+                                last_change = Instant::now();
+                                prev_value = cur_value;
+                            } else {
+                                error!(
+                                    "No progress in {}s, at {}",
+                                    last_change.elapsed().as_secs(),
+                                    cur_value
+                                );
+                                if last_change.elapsed().as_secs() > panic_duration_s {
+                                    panic!(
+                                        "Crashing after {}s no progress",
+                                        last_change.elapsed().as_secs()
+                                    );
+                                }
+                            }
+
+                            std::thread::sleep(Duration::from_secs(30));
+                        }
+                    })
+                    .unwrap(),
+            )
+        } else {
+            None
+        };
+
         let epoch_manager_shared = Arc::new(tokio::sync::Mutex::new(self));
         let epoch_manager_shared_1 = epoch_manager_shared.clone();
         let epoch_manager_shared_2 = epoch_manager_shared.clone();
