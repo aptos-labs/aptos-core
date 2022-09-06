8607e7335cf89dfbdd5eb30a1c295c0d375d6839
icodezjb <icodezjb@users.noreply.github.com>
2022-09-06 14:17:24 +0800

    [cli] Fix lint

--- crates/aptos/src/move_tool/mod.rs
+++ crates/aptos/src/move_tool/mod.rs
@@ -804,7 +804,8 @@ impl FunctionArgType {
                     .map_err(|err| CliError::UnableToParse("u128", err.to_string()))?,
             ),
             FunctionArgType::Raw => {
-                let raw = hex::decode(arg).map_err(|err| CliError::UnableToParse("raw", err.to_string()))?;
+                let raw = hex::decode(arg)
+                    .map_err(|err| CliError::UnableToParse("raw", err.to_string()))?;
                 Ok(raw)
             }
         }
