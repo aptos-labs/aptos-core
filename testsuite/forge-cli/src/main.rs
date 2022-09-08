6c4c8a61176d4d00e73357233fb268e9f04be1f6
Igor <igor-aptos@users.noreply.github.com>
2022-09-08 15:26:37 -0700

    [test on forge] changing_working_quorum_test_max_load
    just for testing on forge

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -184,7 +184,7 @@ fn main() -> Result<()> {
     }
 
     let duration = Duration::from_secs(1800);
-    let suite_name = "changing_working_quorum_test";
+    let suite_name = "changing_working_quorum_test_high_load";
 
     let runtime = Runtime::new()?;
     match args.cli_cmd {
