51455ed44cb251cad06734d09f0e6b8b006f75cb
Zhou Runtian <runtian@aptoslabs.com>
2022-09-09 11:47:25 -0700

    [aptos-cli] Add a command to generate writeset for fire events

--- testsuite/smoke-test/src/test_utils.rs
+++ testsuite/smoke-test/src/test_utils.rs
@@ -80,38 +80,58 @@ pub async fn assert_balance(client: &RestClient, account: &LocalAccount, balance
 /// node swarm, or a public full node swarm.
 #[cfg(test)]
 pub mod swarm_utils {
-    use aptos_config::config::{NodeConfig, WaypointConfig};
+    use aptos_config::config::{
+        InitialSafetyRulesConfig, NodeConfig, SecureBackend, WaypointConfig,
+    };
+    use aptos_secure_storage::{KVStorage, Storage};
     use aptos_types::waypoint::Waypoint;
 
     pub fn insert_waypoint(node_config: &mut NodeConfig, waypoint: Waypoint) {
         node_config.base.waypoint = WaypointConfig::FromConfig(waypoint);
+        node_config
+            .consensus
+            .safety_rules
+            .initial_safety_rules_config = InitialSafetyRulesConfig::None;
+
+        let f = |backend: &SecureBackend| {
+            let mut storage: Storage = backend.into();
+            storage
+                .set(aptos_global_constants::WAYPOINT, waypoint)
+                .expect("Unable to write waypoint");
+            storage
+                .set(aptos_global_constants::GENESIS_WAYPOINT, waypoint)
+                .expect("Unable to write waypoint");
+        };
+        let backend = &node_config.consensus.safety_rules.backend;
+        f(backend);
     }
 }
 
 /// This helper function creates 3 new accounts, mints funds, transfers funds
 /// between the accounts and verifies that these operations succeed.
 pub async fn check_create_mint_transfer(swarm: &mut LocalSwarm) {
-    let client = swarm.validators().next().unwrap().rest_client();
+    check_create_mint_transfer_node(swarm, 0).await;
+}
+
+/// This helper function creates 3 new accounts, mints funds, transfers funds
+/// between the accounts and verifies that these operations succeed on one specific validator.
+pub async fn check_create_mint_transfer_node(swarm: &mut LocalSwarm, idx: usize) {
+    let client = swarm.validators().nth(idx).unwrap().rest_client();
 
     // Create account 0, mint 10 coins and check balance
-    let mut account_0 = create_and_fund_account(swarm, 10).await;
+    let transaction_factory = TransactionFactory::new(swarm.chain_id());
+    let mut info = swarm.aptos_public_info_for_node(idx);
+    let mut account_0 = info.create_and_fund_user_account(10).await.unwrap();
     assert_balance(&client, &account_0, 10).await;
 
     // Create account 1, mint 1 coin, transfer 3 coins from account 0 to 1, check balances
-    let account_1 = create_and_fund_account(swarm, 1).await;
-    transfer_coins(
-        &client,
-        &swarm.chain_info().transaction_factory(),
-        &mut account_0,
-        &account_1,
-        3,
-    )
-    .await;
+    let account_1 = info.create_and_fund_user_account(1).await.unwrap();
+    transfer_coins(&client, &transaction_factory, &mut account_0, &account_1, 3).await;
 
     assert_balance(&client, &account_0, 7).await;
     assert_balance(&client, &account_1, 4).await;
 
     // Create account 2, mint 15 coins and check balance
-    let account_2 = create_and_fund_account(swarm, 15).await;
+    let account_2 = info.create_and_fund_user_account(15).await.unwrap();
     assert_balance(&client, &account_2, 15).await;
 }
