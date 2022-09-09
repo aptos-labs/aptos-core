16bec80146da2f3fd28b948cf25f807e09e92e23
Christian Theilemann <christian@aptoslabs.com>
2022-09-09 01:58:46 -0700

    [observability] productionize humio vector config (#3929)

--- terraform/aptos-node/aws/variables.tf
+++ terraform/aptos-node/aws/variables.tf
@@ -221,16 +221,6 @@ variable "logger_helm_values" {
   default     = {}
 }
 
-variable "enable_vector_daemonset_logger" {
-  description = "Enable vector daemonset logger helm chart"
-  default     = false
-}
-
-variable "vector_daemonset_helm_values" {
-  description = "Map of helm values to pass to vector-daemonset chart"
-  type        = list(string)
-  default     = []
-}
 
 variable "enable_monitoring" {
   description = "Enable monitoring helm chart"
