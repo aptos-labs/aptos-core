2a3778e26b1f7cc200ec47ad1d384136ae12b42b
Balaji Arun <balajia@vt.edu>
2022-09-10 00:23:07 -0700

    [telemetry-service] Allowlist for PFNs (#3887)
    * [telemetry-service] return 403 if role is not allowed
    
    * [telemetry-service] allow PFNs with an allowlist

--- crates/aptos-telemetry-service/src/types/auth.rs
+++ crates/aptos-telemetry-service/src/types/auth.rs
@@ -1,10 +1,12 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use aptos_config::config::{PeerRole, RoleType};
+use aptos_config::config::RoleType;
 use aptos_crypto::x25519;
 use aptos_types::{chain_id::ChainId, PeerId};
 use serde::{Deserialize, Serialize};
+
+use super::common::NodeType;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AuthRequest {
@@ -25,7 +27,7 @@ pub struct AuthResponse {
 pub struct Claims {
     pub chain_id: ChainId,
     pub peer_id: PeerId,
-    pub peer_role: PeerRole,
+    pub node_type: NodeType,
     pub epoch: u64,
     pub exp: usize,
     pub iat: usize,
