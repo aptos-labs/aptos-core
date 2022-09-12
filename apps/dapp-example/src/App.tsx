65e053342727a294c58115e25a32e8c8ffeac123
Kent White <kent@aptoslabs.com>
2022-09-12 15:12:54 -0700

    Add verify example

--- apps/dapp-example/src/App.tsx
+++ apps/dapp-example/src/App.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable no-console */
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import nacl from 'tweetnacl';
+import { Buffer } from 'buffer';
 
 function App() {
   const [address, setAddress] = useState<string | undefined>(undefined);
+  const [publicKey, setPublicKey] = useState<string | undefined>(undefined);
   const [isConnected, setIsConnected] = useState<boolean | undefined>(undefined);
   const [network, setNetwork] = useState<string | undefined>(undefined);
   const [isSubmittingTransaction, setIsSubmittingTransaction] = useState<boolean>(false);
   const [isSigningTransaction, setIsSigningTransaction] = useState<boolean>(false);
   const [isSigningMessage, setIsSigningMessage] = useState<boolean>(false);
+  const [isVerifying, setIsVerifying] = useState<boolean>(false);
 
   const transaction = {
     arguments: [address, 717],
@@ -27,9 +31,11 @@ function App() {
           window.aptos.network(),
         ]);
         setAddress(activeAccount.address);
+        setPublicKey(activeAccount.publicKey);
         setNetwork(activeNetworkName);
       } else {
         setAddress(undefined);
+        setPublicKey(undefined);
         setNetwork(undefined);
       }
     }
@@ -38,10 +44,12 @@ function App() {
       if (account.address) {
         setIsConnected(true);
         setAddress(account.address);
+        setPublicKey(account.publicKey);
         setNetwork(await window.aptos.network());
       } else {
         setIsConnected(false);
         setAddress(undefined);
+        setPublicKey(undefined);
         setNetwork(undefined);
       }
     });
@@ -58,12 +66,14 @@ function App() {
       await window.aptos.disconnect();
       setIsConnected(false);
       setAddress(undefined);
+      setPublicKey(undefined);
       setNetwork(undefined);
     } else {
       const activeAccount = await window.aptos.connect();
       const activeNetworkName = await window.aptos.network();
       setIsConnected(true);
       setAddress(activeAccount.address);
+      setPublicKey(activeAccount.publicKey);
       setNetwork(activeNetworkName);
     }
   };
@@ -113,6 +123,26 @@ function App() {
     }
   };
 
+  const onVerifyClick = async () => {
+    if (!isVerifying && address) {
+      setIsVerifying(true);
+      try {
+        const nonce = Date.now().toString();
+        const response = await window.aptos.signMessage({
+          message: 'Hello',
+          nonce,
+        });
+        // Remove the 0x prefix
+        const key = publicKey!.slice(2, 66);
+        const verified = nacl.sign.detached.verify(Buffer.from(response.fullMessage), Buffer.from(response.signature, 'hex'), Buffer.from(key, 'hex'));
+        console.log(verified);
+      } catch (error) {
+        console.error(error);
+      }
+      setIsVerifying(false);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -126,6 +156,7 @@ function App() {
         <button className="Button" type="button" onClick={onSubmitTransactionClick}>{isSubmittingTransaction ? 'Submitting...' : 'Submit Transaction'}</button>
         <button className="Button" type="button" onClick={onSignTransactionClick}>{isSigningTransaction ? 'Sigining...' : 'Sign Transaction'}</button>
         <button className="Button" type="button" onClick={onSignMessageClick}>{isSigningMessage ? 'Signing...' : 'Sign Message'}</button>
+        <button className="Button" type="button" onClick={onVerifyClick}>{isVerifying ? 'Verifying...' : 'Verify Message'}</button>
       </header>
     </div>
   );
