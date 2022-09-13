69959ba938940df65279afa980f2025a390b174e
Greg Nazario <greg@gnazar.io>
2022-09-12 18:57:11 -0700

    [api/storage] Allow for a configurable max page size up to 10000
    This allows users to change their max page size to make their
    own choices.  Additionally, it's only 1000 by default.

--- storage/aptosdb/src/lib.rs
+++ storage/aptosdb/src/lib.rs
@@ -121,7 +121,7 @@ use storage_interface::{
 pub const LEDGER_DB_NAME: &str = "ledger_db";
 pub const STATE_MERKLE_DB_NAME: &str = "state_merkle_db";
 
-const MAX_LIMIT: u64 = 5000;
+const MAX_LIMIT: u64 = 10000;
 
 // TODO: Either implement an iteration API to allow a very old client to loop through a long history
 // or guarantee that there is always a recent enough waypoint and client knows to boot from there.
