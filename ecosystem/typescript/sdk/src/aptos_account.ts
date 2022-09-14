3ae0b2b686b1c0638d98458e32af5606e1e6fbcd
Jijun Leng <jijunleng@gmail.com>
2022-09-14 09:29:59 -0700

    refactor(ts-sdk): get rid of typescript-memoize dep

--- ecosystem/typescript/sdk/src/aptos_account.ts
+++ ecosystem/typescript/sdk/src/aptos_account.ts
@@ -5,10 +5,10 @@ import nacl from "tweetnacl";
 import sha3 from "js-sha3";
 import { derivePath } from "ed25519-hd-key";
 import * as bip39 from "@scure/bip39";
-import { Memoize } from "typescript-memoize";
 import { bytesToHex } from "./bytes_to_hex.js";
 import { HexString, MaybeHexString } from "./hex_string";
 import * as Gen from "./generated/index";
+import { Memoize } from "./utils";
 
 const { sha3_256: sha3Hash } = sha3;
 
