ecbf7f5f55aac1c0836ee2623f3b6de865a0904b
Jijun Leng <jijunleng@gmail.com>
2022-09-14 23:37:32 -0700

    build(ts-sdk): make iife build work for browsers

--- ecosystem/typescript/sdk/tsup.config.js
+++ ecosystem/typescript/sdk/tsup.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
   splitting: false,
   sourcemap: true,
-  clean: true,
-  dts: true,
-  globalName: "aptosSDK",
 });
