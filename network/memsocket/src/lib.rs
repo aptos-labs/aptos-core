9531de40595ae1c109958dcab2f01f5718d67df3
Josh Lind <josh.lind@hotmail.com>
2022-09-15 19:57:06 -0400

    Revert "[Network] Small readability fixes to memsocket."
    This reverts commit e8738968e8ae2a9a52dd1d58f7f7e0e39b88bc23.

--- network/memsocket/src/lib.rs
+++ network/memsocket/src/lib.rs
@@ -13,28 +13,10 @@ use futures::{
 use once_cell::sync::Lazy;
 use std::{collections::HashMap, num::NonZeroU16, pin::Pin};
 
-/// A global switch board of all in-memory socket servers
-static SWITCHBOARD: Lazy<Mutex<SwitchBoard>> = Lazy::new(|| Mutex::new(SwitchBoard::new()));
+static SWITCHBOARD: Lazy<Mutex<SwitchBoard>> =
+    Lazy::new(|| Mutex::new(SwitchBoard(HashMap::default(), 1)));
 
-/// The port counter at which to search for available ports
-const PORT_TO_START_AT: u16 = 1; // Port 0 is used to request a random port
-
-/// A struct that contains all in-memory socket servers
-/// that have already been bound, and a counter to search
-/// through available ports.
-struct SwitchBoard {
-    pub port_to_sender_map: HashMap<NonZeroU16, UnboundedSender<MemorySocket>>,
-    pub next_port_to_check: u16,
-}
-
-impl SwitchBoard {
-    pub fn new() -> Self {
-        SwitchBoard {
-            port_to_sender_map: HashMap::new(),
-            next_port_to_check: PORT_TO_START_AT,
-        }
-    }
-}
+struct SwitchBoard(HashMap<NonZeroU16, UnboundedSender<MemorySocket>>, u16);
 
 /// An in-memory socket server, listening for connections.
 ///
@@ -83,7 +65,7 @@ impl Drop for MemoryListener {
         let mut switchboard = (&*SWITCHBOARD).lock();
         // Remove the Sending side of the channel in the switchboard when
         // MemoryListener is dropped
-        switchboard.port_to_sender_map.remove(&self.port);
+        switchboard.0.remove(&self.port);
     }
 }
 
@@ -112,40 +94,34 @@ impl MemoryListener {
     pub fn bind(port: u16) -> Result<Self> {
         let mut switchboard = (&*SWITCHBOARD).lock();
 
-        // Get the port we should bind to. If 0 was given, use a random port.
-        let port = if port != 0 {
-            let port = NonZeroU16::new(port).unwrap_or_else(|| unreachable!());
-            if switchboard.port_to_sender_map.contains_key(&port) {
+        // Get the port we should bind to.  If 0 was given, use a random port
+        let port = if let Some(port) = NonZeroU16::new(port) {
+            if switchboard.0.contains_key(&port) {
                 return Err(ErrorKind::AddrInUse.into());
             }
             port
         } else {
-            // Check if the switchboard is full and if all ports are in use
-            if Some(switchboard.port_to_sender_map.len())
-                == u16::MAX.checked_sub(1).map(usize::from)
-            {
-                return Err(ErrorKind::AddrInUse.into());
-            }
-
-            // Find a random and unused port
             loop {
-                let port = NonZeroU16::new(switchboard.next_port_to_check)
-                    .unwrap_or_else(|| unreachable!());
+                let port = NonZeroU16::new(switchboard.1).unwrap_or_else(|| unreachable!());
+
+                // The switchboard is full and all ports are in use
+                if Some(switchboard.0.len()) == std::u16::MAX.checked_sub(1).map(usize::from) {
+                    return Err(ErrorKind::AddrInUse.into());
+                }
 
                 // Instead of overflowing to 0, resume searching at port 1 since port 0 isn't a
                 // valid port to bind to.
-                switchboard.next_port_to_check = switchboard
-                    .next_port_to_check
-                    .checked_add(1)
-                    .unwrap_or(PORT_TO_START_AT);
-                if !switchboard.port_to_sender_map.contains_key(&port) {
+
+                switchboard.1 = switchboard.1.checked_add(1).unwrap_or(1);
+
+                if !switchboard.0.contains_key(&port) {
                     break port;
                 }
             }
         };
 
         let (sender, receiver) = mpsc::unbounded();
-        switchboard.port_to_sender_map.insert(port, sender);
+        switchboard.0.insert(port, sender);
 
         Ok(Self {
             incoming: receiver,
@@ -320,7 +296,7 @@ impl MemorySocket {
         let port = NonZeroU16::new(port).ok_or(ErrorKind::AddrNotAvailable)?;
 
         let sender = switchboard
-            .port_to_sender_map
+            .0
             .get_mut(&port)
             .ok_or(ErrorKind::AddrNotAvailable)?;
 
