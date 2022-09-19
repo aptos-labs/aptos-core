199cdf28937ef5084bc2df002722d740262c67f0
Kent White <kent@aptoslabs.com>
2022-09-19 12:33:26 -0700

    Move faucet to 1 apt

--- apps/extension/src/core/components/Faucet.tsx
+++ apps/extension/src/core/components/Faucet.tsx
@@ -16,7 +16,7 @@ import { faucetEvents } from 'core/utils/analytics/events';
 import { aptosCoinStructTag } from 'core/constants';
 import { toast } from './Toast';
 
-const defaultFundAmount = 10000000;
+const defaultFundAmount = 100000000;
 
 export default function Faucet() {
   const { activeAccountAddress } = useActiveAccount();
