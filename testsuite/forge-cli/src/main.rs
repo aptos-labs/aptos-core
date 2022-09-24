ab33208dba19d11151c3d6611c1c3e719cf05e90
Brian Cho <brian@aptoslabs.com>
2022-09-23 17:04:24 -0700

    Revert "turn on graceful_overload for this pr"
    This reverts commit 305cc5a51693135be3a7e8156fd7d5732fd5a794.

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -380,7 +380,7 @@ fn get_changelog(prev_commit: Option<&String>, upstream_commit: &str) -> String 
 
 fn get_test_suite(suite_name: &str, duration: Duration) -> Result<ForgeConfig<'static>> {
     match suite_name {
-        "land_blocking" => Ok(single_test_suite("graceful_overload").unwrap()),
+        "land_blocking" => Ok(land_blocking_test_suite(duration)),
         "local_test_suite" => Ok(local_test_suite()),
         "pre_release" => Ok(pre_release_suite()),
         "run_forever" => Ok(run_forever()),
