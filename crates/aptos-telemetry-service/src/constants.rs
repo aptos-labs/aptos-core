725bae9862ca3ba3e303dc15bae8e9ec0d4bbea5
Balaji Arun <balajia@vt.edu>
2022-09-24 11:52:26 -0700

    [telemetry-service] fix trace id key in log entry

--- crates/aptos-telemetry-service/src/constants.rs
+++ crates/aptos-telemetry-service/src/constants.rs
@@ -5,4 +5,3 @@
 pub const MAX_CONTENT_LENGTH: u64 = 1024 * 1024;
 
 pub const GCP_CLOUD_TRACE_CONTEXT_HEADER: &str = "X-Cloud-Trace-Context";
-pub const LOG_TRACE_FIELD: &str = "logging.googleapis.com/trace";
