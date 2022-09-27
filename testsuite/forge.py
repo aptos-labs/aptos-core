768e21da82485ea4df14fead83448ba2afeb158d
Alex Markuze <alexm@aptoslabs.com>
2022-09-27 20:45:37 +0300

    2 cores

--- testsuite/forge.py
+++ testsuite/forge.py
@@ -1048,9 +1048,10 @@ def create_forge_command(
         forge_args.extend([
             "--suite", forge_test_suite
         ])
-    forge_args.extend([
-        "--duration-secs", "7200"
-    ])
+    if forge_runner_duration_secs:
+        forge_args.extend([
+            "--duration-secs", forge_runner_duration_secs
+        ])
     if forge_num_validators:
         forge_args.extend(["--num-validators", forge_num_validators])
     forge_args.extend(["--num-validators", "100"])
