566ef98d7c91f749ef7a515836a539b8cae98a50
Raj Karamchedu <raj@aptoslabs.com>
2022-09-29 13:32:48 -0700

    [Docs] Adding sitemap option.

--- developer-docs-site/docusaurus.config.js
+++ developer-docs-site/docusaurus.config.js
@@ -30,6 +30,12 @@ const config = {
           editUrl: "https://github.com/aptos-labs/aptos-core/tree/main/developer-docs-site/",
           remarkPlugins: [codeInjector],
         },
+        sitemap: {
+          changefreq: "daily",
+          priority: 0.5,
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
+        },
         blog: false,
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
