2c9cd65a37e11b2eab1461635fcbacff486b57b2
Raj Karamchedu <raj@aptoslabs.com>
2022-09-29 09:44:45 -0700

    [Docs] Consolidating all the troubleshooting sections into a new top-level doc.

--- developer-docs-site/docusaurus.config.js
+++ developer-docs-site/docusaurus.config.js
@@ -328,6 +328,10 @@ const config = {
             to: "/nodes/validator-node/operator/node-liveness-criteria",
             from: "/nodes/ait/node-liveness-criteria",
           },
+          {
+            to: "/issues-and-workarounds",
+            from: "/nodes/full-node/troubleshooting-fullnode-setup",
+          },
         ],
       },
     ],
