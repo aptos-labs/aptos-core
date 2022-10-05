1808dd84b29051018ed2e41b8232e351060fe9dd
CapCap <max@aptoslabs.com>
2022-10-05 16:51:08 -0700

    test fix

--- aptos-move/e2e-move-tests/src/tests/names.rs
+++ aptos-move/e2e-move-tests/src/tests/names.rs
@@ -3,9 +3,9 @@
 //
 
 use crate::{assert_success, MoveHarness};
-use aptos_crypto::{multi_ed25519::MultiEd25519PublicKey, ValidCryptoMaterialStringExt};
-use aptos_types::{account_address::AccountAddress, transaction::authenticator::AuthenticationKey};
+use aptos_types::account_address::AccountAddress;
 use cached_packages::aptos_names_sdk_builder;
+use vm_genesis::{get_test_ans_admin_address, get_test_ans_funds_address};
 
 /*
     Below values are for testing only!
@@ -16,26 +16,6 @@ use cached_packages::aptos_names_sdk_builder;
     multisig_auth_key: 0x4407b9a063ac530f8b621f7d80b527a79c626791b14b51c1118763ce941b99ce
     threshold: 1/2
 */
-fn get_test_ans_funds_address() -> AccountAddress {
-    AccountAddress::from_hex_literal(
-        "0x0ee16f0e4b47d5972f63a642385d52d301e53716b4e1fbd637b9a91a7f1979ba",
-    )
-    .unwrap()
-}
-
-fn get_test_ans_admin_multisig_auth_key() -> AuthenticationKey {
-    let pub_key = MultiEd25519PublicKey::from_encoded_string(
-        "0xc5547463e44c3ad8ad52018f0aaf237d39e396b22815cf712493dd61cffabebfeea1decaa37eb5cdcf99262c6518053126e34283f42ad74f7b91b75fa625c6f801",
-    )
-        .unwrap();
-    let auth_key = AuthenticationKey::multi_ed25519(&pub_key);
-    // Ensure the auth key matches the expected on in the comment above
-    assert_eq!(
-        auth_key.to_string(),
-        "4407b9a063ac530f8b621f7d80b527a79c626791b14b51c1118763ce941b99ce".to_string()
-    );
-    auth_key
-}
 
 #[test]
 fn test_names_end_to_end() {
