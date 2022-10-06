a79b41e809105b5859993da3915368de965c57ed
Balaji Arun <balajia@vt.edu>
2022-10-06 09:36:29 -0700

    [telemetry-service] Adds project label to service metrics

--- crates/aptos-telemetry-service/src/constants.rs
+++ crates/aptos-telemetry-service/src/constants.rs
@@ -11,3 +11,6 @@ pub const GCP_CLOUD_TRACE_CONTEXT_HEADER: &str = "X-Cloud-Trace-Context";
 pub const GCP_CLOUD_RUN_REVISION_ENV: &str = "K_REVISION";
 /// GCP Cloud Run env variable for service name
 pub const GCP_CLOUD_RUN_SERVICE_ENV: &str = "K_SERVICE";
+/// GCP Project within which this service is running.
+/// This variable must be set by calling the metadata server
+pub const GCP_SERVICE_PROJECT_ID: &str = "GCP_METADATA_PROJECT";
