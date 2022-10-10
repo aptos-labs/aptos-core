8fd114a746987c0adfcfd0c243c14a762828d527
K-Pomian <pomian.konrad@gmail.com>
2022-10-10 13:00:11 +0200

    Schema update

--- crates/indexer/src/schema.rs
+++ crates/indexer/src/schema.rs
@@ -157,6 +157,7 @@ diesel::table! {
     marketplace_collections (creator_address, collection_name) {
         creator_address -> Varchar,
         collection_name -> Text,
+        creation_timestamp -> Timestamp,
     }
 }
 
