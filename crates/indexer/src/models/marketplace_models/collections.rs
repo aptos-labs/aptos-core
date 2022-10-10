a2c467a973ec9acc261c097204c09161844308db
K-Pomian <pomian.konrad@gmail.com>
2022-10-10 13:00:39 +0200

    Marketplace collection model

---
+++ crates/indexer/src/models/marketplace_models/collections.rs
@@ -0,0 +1,29 @@
+#![allow(clippy::extra_unused_lifetimes)]
+#![allow(clippy::unused_unit)]
+
+use aptos_api_types::{TransactionPayload, UserTransaction};
+
+use crate::{schema::marketplace_collections, util::parse_timestamp};
+
+#[derive(Queryable, Identifiable, Insertable)]
+#[diesel(primary_key(creator_address, collection_name))]
+#[diesel(table_name = marketplace_collections)]
+pub struct MarketplaceCollection {
+    creator_address: String,
+    collection_name: String,
+    creation_timestamp: chrono::NaiveDateTime,
+}
+
+impl MarketplaceCollection {
+    pub fn from_transaction(txn: &UserTransaction) -> Option<Self> {
+        let version = txn.info.version.0;
+        match txn.request.payload {
+            TransactionPayload::EntryFunctionPayload(payload) => Some(Self {
+                creator_address: payload.arguments[0]["creator"].to_string(),
+                collection_name: payload.arguments[0]["collection_name"].to_string(),
+                creation_timestamp: parse_timestamp(txn.timestamp.0, version.try_into().unwrap()),
+            }),
+            _ => None,
+        }
+    }
+}
