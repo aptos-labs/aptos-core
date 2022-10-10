ef023a0180d9b510af6e61ded4f3778c4bcd9d68
K-Pomian <pomian.konrad@gmail.com>
2022-10-10 10:00:11 +0200

    SQL fix

--- crates/indexer/migrations/2022-10-06-113956_marketplace/up.sql
+++ crates/indexer/migrations/2022-10-06-113956_marketplace/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE marketplace_collections (
-    creator_address VARCHAR(66) NOT NULL,
-    collection_name TEXT NOT NULL,
+    creator_address VARCHAR(66) UNIQUE NOT NULL,
+    collection_name TEXT UNIQUE NOT NULL,
 
     PRIMARY KEY (
         creator_address,
@@ -8,7 +8,7 @@ CREATE TABLE marketplace_collections (
     ),
 
     CONSTRAINT collection_id UNIQUE(creator_address, collection_name)
-)
+);
 
 CREATE TABLE marketplace_offers (
     creator_address VARCHAR(66) NOT NULL,
@@ -16,24 +16,22 @@ CREATE TABLE marketplace_offers (
     token_name TEXT NOT NULL,
     property_version SMALLINT NOT NULL,
     price BIGINT NOT NULL,
-    seller VARCHAR(66) NOT NULL
-    "timestamp" TIMESTAMP NOT NULL
+    seller VARCHAR(66) NOT NULL,
+    "timestamp" TIMESTAMP NOT NULL,
 
     PRIMARY KEY (
         creator_address,
         collection_name
     ),
 
-    CONSTRAINT collection_id UNIQUE(creator_address, collection_name)
-
-    CONSTRAINT FK_creator_address 
+    CONSTRAINT FK_creator_address_offers
     FOREIGN KEY (creator_address)
-    REFERENCES creator_address (marketplace_collections)
+    REFERENCES marketplace_collections (creator_address),
 
-    CONSTRAINT FK_collection_name 
+    CONSTRAINT FK_collection_name_offers
     FOREIGN KEY (collection_name)
-    REFERENCES collection_name (marketplace_collections)
-)
+    REFERENCES marketplace_collections (collection_name)
+);
 
 CREATE TABLE marketplace_orders (
     creator_address VARCHAR(66) NOT NULL,
@@ -42,24 +40,22 @@ CREATE TABLE marketplace_orders (
     property_version SMALLINT NOT NULL,
     price BIGINT NOT NULL,
     quantity BIGINT NOT NULL,
-    maker VARCHAR(66) NOT NULL
-    "timestamp" TIMESTAMP NOT NULL
+    maker VARCHAR(66) NOT NULL,
+    "timestamp" TIMESTAMP NOT NULL,
 
     PRIMARY KEY (
         creator_address,
         collection_name
     ),
 
-    CONSTRAINT collection_id UNIQUE(creator_address, collection_name)
-
-    CONSTRAINT FK_creator_address 
+    CONSTRAINT FK_creator_address_orders
     FOREIGN KEY (creator_address)
-    REFERENCES creator_address (marketplace_collections)
+    REFERENCES marketplace_collections (creator_address),
 
-    CONSTRAINT FK_collection_name 
+    CONSTRAINT FK_collection_name_orders
     FOREIGN KEY (collection_name)
-    REFERENCES collection_name (marketplace_collections)
-)
+    REFERENCES marketplace_collections (collection_name)
+);
 
 CREATE TABLE marketplace_bids (
     creator_address VARCHAR(66) NOT NULL,
@@ -67,21 +63,19 @@ CREATE TABLE marketplace_bids (
     token_name TEXT NOT NULL,
     property_version SMALLINT NOT NULL,
     price BIGINT NOT NULL,
-    maker VARCHAR(66) NOT NULL
-    "timestamp" TIMESTAMP NOT NULL
+    maker VARCHAR(66) NOT NULL,
+    "timestamp" TIMESTAMP NOT NULL,
 
     PRIMARY KEY (
         creator_address,
         collection_name
     ),
 
-    CONSTRAINT collection_id UNIQUE(creator_address, collection_name)
-
-    CONSTRAINT FK_creator_address 
+    CONSTRAINT FK_creator_address_bids 
     FOREIGN KEY (creator_address)
-    REFERENCES creator_address (marketplace_collections)
+    REFERENCES marketplace_collections (creator_address),
 
-    CONSTRAINT FK_collection_name 
+    CONSTRAINT FK_collection_name_bids
     FOREIGN KEY (collection_name)
-    REFERENCES collection_name (marketplace_collections)
-)
+    REFERENCES marketplace_collections (collection_name)
+);
