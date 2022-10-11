77e2fd2f5b50e673943417bff7e4f20f5ede834e
Greg Nazario <greg@gnazar.io>
2022-10-11 10:46:53 -0700

    [mempool] Update broadcast buckets

--- config/src/config/mempool_config.rs
+++ config/src/config/mempool_config.rs
@@ -44,7 +44,7 @@ impl Default for MempoolConfig {
             system_transaction_timeout_secs: 600,
             system_transaction_gc_interval_ms: 60_000,
             shared_mempool_validator_broadcast: true,
-            broadcast_buckets: vec![0, 151, 301, 901, 2001, 10001], // (1, 1.5, 3, 9, 20, 100)
+            broadcast_buckets: vec![0, 150, 300, 500, 700, 1000, 3000, 5000, 7000, 9000],
         }
     }
 }
