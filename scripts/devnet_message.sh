95c21097dcc4be95ee798f277858fec599cb5a8a
Perry Randall <perryjrandall@gmail.com>
2022-10-13 16:26:15 -0700

    [devnet] Add script to generate devnet release message

---
+++ scripts/devnet_message.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+TEMP="$(mktemp)"
+
+curl https://fullnode.devnet.aptoslabs.com/v1 > "$TEMP"
+
+COMMIT="$(jq -r .git_hash "$TEMP")"
+CHAIN_ID="$(jq -r .chain_id "$TEMP")"
+
+DIGEST="$(crane digest aptoslabs/validator:devnet_"$COMMIT")"
+GENESIS_SHA="$(curl https://devnet.aptoslabs.com/genesis.blob | shasum -a 256 | awk '{print $1}')"
+WAYPOINT="$(curl https://devnet.aptoslabs.com/waypoint.txt)"
+
+cat <<EOF
+
+Hey @everyone devnet finished release, please update your fullnodes now!
+
+For upgrade, make sure you pulled the latest docker image, or build the rust binary from the latest devnet branch. To confirm:
+
+- Devnet branch commit: $COMMIT
+- Docker image tag: devnet_$COMMIT
+- Docker image digest: $DIGEST
+- genesis.blob sha256: $GENESIS_SHA
+- waypoint: $WAYPOINT
+- Chain ID: $CHAIN_ID
+You can follow the instructions here for upgrade: https://aptos.dev/tutorials/run-a-fullnode#update-fullnode-with-new-releases/
+
+EOF
