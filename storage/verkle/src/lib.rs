bed7c59a82f3ff73d16ec6361568a13fcc11cbd8
zhoujun.ma <zhoujun@aptoslabs.com>
2022-10-17 17:53:31 -0700

    verkle placeholders

---
+++ storage/verkle/src/lib.rs
@@ -0,0 +1,20 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod iterator;
+pub mod types;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ storage/verkle/src/types.rs
@@ -0,0 +1,6 @@
+// Copyright (c) Aptos
+// SPDX-License-Identifier: Apache-2.0
+
+use curve25519_dalek_ng::ristretto::{CompressedRistretto, RistrettoPoint};
+
+pub type Digest = CompressedRistretto;
