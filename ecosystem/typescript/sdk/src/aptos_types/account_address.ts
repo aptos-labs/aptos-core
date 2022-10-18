b153eab8498b504406b2f18621ca582168643651
Greg Nazario <greg@gnazar.io>
2022-10-18 11:59:33 -0700

    [ts-sdk] Add an isValid function for AccountAddress

--- ecosystem/typescript/sdk/src/aptos_types/account_address.ts
+++ ecosystem/typescript/sdk/src/aptos_types/account_address.ts
@@ -47,6 +47,29 @@ export class AccountAddress {
     return new AccountAddress(res);
   }
 
+  /**
+   * Checks if the string is a valid AccountAddress
+   * @param addr Hex string can be with a prefix or without a prefix,
+   *   e.g. '0x1aa' or '1aa'. Hex string will be left padded with 0s if too short.
+   */
+  static isValid(addr: MaybeHexString): boolean {
+    let address = HexString.ensure(addr);
+
+    // If an address hex has odd number of digits, padd the hex string with 0
+    // e.g. '1aa' would become '01aa'.
+    if (address.noPrefix().length % 2 !== 0) {
+      address = new HexString(`0${address.noPrefix()}`);
+    }
+
+    const addressBytes = address.toUint8Array();
+
+    if (addressBytes.length > AccountAddress.LENGTH) {
+      return false;
+    }
+
+    return true;
+  }
+
   serialize(serializer: Serializer): void {
     serializer.serializeFixedBytes(this.address);
   }
