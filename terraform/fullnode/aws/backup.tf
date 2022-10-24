e96fb0fe88caa4afa0fdb5deacbf9172db2247d4
Rustie Lin <rustie117@gmail.com>
2022-10-24 13:45:18 -0700

    [tf/aws][fullnode] public-read acl for public backups (#5252)

--- terraform/fullnode/aws/backup.tf
+++ terraform/fullnode/aws/backup.tf
@@ -14,6 +14,12 @@ resource "aws_s3_bucket_public_access_block" "backup" {
   restrict_public_buckets = !var.enable_public_backup
 }
 
+resource "aws_s3_bucket_acl" "public-backup" {
+  count  = var.enable_public_backup ? 1 : 0
+  bucket = aws_s3_bucket.backup.id
+  acl    = "public-read"
+}
+
 data "aws_iam_policy_document" "backup-assume-role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
