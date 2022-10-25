64478f94c971d618187afd7e138b07070fc28741
Zhou Runtian <runtian@aptoslabs.com>
2022-10-25 15:59:25 -0700

    [replay] implement a replay tooling

--- aptos-move/aptos-validator-interface/src/storage_interface.rs
+++ aptos-move/aptos-validator-interface/src/storage_interface.rs
@@ -10,14 +10,12 @@ use aptos_config::config::{
 use aptos_types::{
     account_address::AccountAddress,
     account_state::AccountState,
-    contract_event::EventWithVersion,
-    event::EventKey,
     state_store::{state_key::StateKey, state_key_prefix::StateKeyPrefix, state_value::StateValue},
     transaction::{Transaction, Version},
 };
 use aptosdb::AptosDB;
 use std::{path::Path, sync::Arc};
-use storage_interface::{DbReader, Order, MAX_REQUEST_LIMIT};
+use storage_interface::{DbReader, MAX_REQUEST_LIMIT};
 
 pub struct DBDebuggerInterface(Arc<dyn DbReader>);
 
@@ -35,8 +33,9 @@ impl DBDebuggerInterface {
     }
 }
 
+#[async_trait::async_trait]
 impl AptosValidatorInterface for DBDebuggerInterface {
-    fn get_account_state_by_version(
+    async fn get_account_state_by_version(
         &self,
         account: AccountAddress,
         version: Version,
@@ -58,7 +57,7 @@ impl AptosValidatorInterface for DBDebuggerInterface {
         AccountState::from_access_paths_and_values(account, &kvs)
     }
 
-    fn get_state_value_by_version(
+    async fn get_state_value_by_version(
         &self,
         state_key: &StateKey,
         version: Version,
@@ -69,25 +68,18 @@ impl AptosValidatorInterface for DBDebuggerInterface {
             .0)
     }
 
-    fn get_events(
+    async fn get_committed_transactions(
         &self,
-        key: &EventKey,
-        start_seq: u64,
+        start: Version,
         limit: u64,
-        ledger_version: Version,
-    ) -> Result<Vec<EventWithVersion>> {
-        self.0
-            .get_events(key, start_seq, Order::Ascending, limit, ledger_version)
-    }
-
-    fn get_committed_transactions(&self, start: Version, limit: u64) -> Result<Vec<Transaction>> {
+    ) -> Result<Vec<Transaction>> {
         Ok(self
             .0
-            .get_transactions(start, limit, self.get_latest_version()?, false)?
+            .get_transactions(start, limit, self.get_latest_version().await?, false)?
             .transactions)
     }
 
-    fn get_latest_version(&self) -> Result<Version> {
+    async fn get_latest_version(&self) -> Result<Version> {
         let (version, _) = self
             .0
             .get_latest_transaction_info_option()?
@@ -95,12 +87,12 @@ impl AptosValidatorInterface for DBDebuggerInterface {
         Ok(version)
     }
 
-    fn get_version_by_account_sequence(
+    async fn get_version_by_account_sequence(
         &self,
         account: AccountAddress,
         seq: u64,
     ) -> Result<Option<Version>> {
-        let ledger_version = self.get_latest_version()?;
+        let ledger_version = self.get_latest_version().await?;
         Ok(self
             .0
             .get_account_transaction(account, seq, false, ledger_version)?
