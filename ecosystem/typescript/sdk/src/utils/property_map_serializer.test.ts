44aa6a5cc1b368ffda97dfe836c0f09753d87689
Bo Wu <bo@aptoslabs.com>
2022-10-25 10:35:44 -0700

    [token] add transfer with opt in

--- ecosystem/typescript/sdk/src/utils/property_map_serializer.test.ts
+++ ecosystem/typescript/sdk/src/utils/property_map_serializer.test.ts
@@ -1,23 +1,32 @@
 import { getPropertyValueRaw } from "./property_map_serializer";
-import { bcsSerializeBool, bcsSerializeStr, bcsSerializeU128, bcsSerializeU8, bcsSerializeUint64, Bytes } from "../bcs";
-import assert from "assert";
+import {
+  bcsSerializeBool,
+  bcsSerializeStr,
+  bcsSerializeU128,
+  bcsSerializeU8,
+  bcsSerializeUint64,
+  bcsToBytes,
+  Bytes,
+} from "../bcs";
+import { AccountAddress } from "../aptos_types";
+import { HexString } from "../hex_string";
 
 test("test property_map_serializer", () => {
-  var is_same: (array1: Bytes, array2: Bytes) => boolean = function (array1, array2): boolean {
+  function isSame(array1: Bytes, array2: Bytes): boolean {
     return (
       array1.length == array2.length &&
-      array1.every(function (element, index) {
+      array1.every((element, index) => {
         return element === array2[index];
       })
     );
-  };
-  let values = ["false", "10", "1", "30", "hello", "0x1"];
+  }
+  let values = ["false", "10", "18446744073709551615", "340282366920938463463374607431768211455", "hello", "0x1"];
   let types = ["bool", "u8", "u64", "u128", "0x1::string::String", "address"];
   let newValues = getPropertyValueRaw(values, types);
-  assert(is_same(newValues[0], bcsSerializeBool(false)));
-  assert(is_same(newValues[1], bcsSerializeU8(10)));
-  assert(is_same(newValues[2], bcsSerializeUint64(1)));
-  assert(is_same(newValues[3], bcsSerializeU128(30)));
-  assert(is_same(newValues[4], bcsSerializeStr(values[4])));
-  assert(is_same(newValues[5], bcsSerializeStr(values[5])));
+  expect(isSame(newValues[0], bcsSerializeBool(false))).toBe(true);
+  expect(isSame(newValues[1], bcsSerializeU8(10))).toBe(true);
+  expect(isSame(newValues[2], bcsSerializeUint64(18446744073709551615n))).toBe(true);
+  expect(isSame(newValues[3], bcsSerializeU128(340282366920938463463374607431768211455n))).toBe(true);
+  expect(isSame(newValues[4], bcsSerializeStr(values[4]))).toBe(true);
+  expect(isSame(newValues[5], bcsToBytes(AccountAddress.fromHex(new HexString("0x1"))))).toBe(true);
 });
--- ecosystem/typescript/sdk/src/utils/property_map_serializer.ts
+++ ecosystem/typescript/sdk/src/utils/property_map_serializer.ts
@@ -1,28 +1,23 @@
-import assert from "assert";
-import { bcsSerializeBool, bcsSerializeStr, bcsSerializeU128, bcsSerializeU8, bcsSerializeUint64, Bytes } from "../bcs";
+import { Bytes, Serializer } from "../bcs";
+import { TypeTagParser } from "../transaction_builder";
+import { serializeArg } from "../transaction_builder/builder_utils";
 
 export function getPropertyValueRaw(values: Array<string>, types: Array<string>): Array<Bytes> {
-  assert(values.length === types.length);
+  if (values.length !== types.length) {
+    throw new Error("Length of property values and types not match");
+  }
+
   const results = new Array<Bytes>();
   types.forEach((typ, index) => {
-    if (typ === "bool") {
-      const res: boolean = JSON.parse(values[index]);
-      results.push(bcsSerializeBool(res));
-    } else if (typ === "0x1::string::String") {
-      results.push(bcsSerializeStr(values[index]));
-    } else if (typ === "u8") {
-      const res: number = JSON.parse(values[index]);
-      results.push(bcsSerializeU8(res));
-    } else if (typ === "u64") {
-      const res: number = JSON.parse(values[index]);
-      results.push(bcsSerializeUint64(res));
-    } else if (typ === "u128") {
-      const res: number = JSON.parse(values[index]);
-      results.push(bcsSerializeU128(res));
-    } else if (typ === "address") {
-      results.push(bcsSerializeStr(values[index]));
-    } else {
-      results.push(bcsSerializeStr(values[index]));
+    try {
+      const typeTag = new TypeTagParser(typ).parseTypeTag();
+
+      const serializer = new Serializer();
+      serializeArg(values[index], typeTag, serializer);
+      results.push(serializer.getBytes());
+    } catch (error) {
+      // if not support type, just use the raw string bytes
+      results.push(new TextEncoder().encode(values[index]));
     }
   });
   return results;
