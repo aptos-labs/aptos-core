91ec76c9f7745a6fd370fa03b6fb6816a8417580
Wolfgang Grieskamp <wg@aptoslabs.com>
2022-11-15 12:56:24 -0800

    [hotfix] Update for hotfix of Move VM issues (#5575)
    This specifically updates us to the fixes [here](https://github.com/move-language/move/pull/669). Some updates on test related infra are also needed in order to be able to take this PR.

--- aptos-move/framework/src/error_map.rs
+++ aptos-move/framework/src/error_map.rs
@@ -35,7 +35,7 @@ pub(crate) fn generate_error_map(
         test_mode: false,
         force_recompilation: false,
         fetch_deps_only: false,
-        fetch_latest_git_deps: false,
+        skip_fetch_latest_git_deps: true,
     };
     if let Ok(model) = build_config.move_model_for_package(
         package_path,
