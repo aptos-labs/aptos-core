3dbf117fa050042dcefcd2768929717235e49166
Bo Wu <bo@aptoslabs.com>
2022-11-22 16:04:35 -0800

    [token] update doc (#5651)

--- ecosystem/typescript/sdk/src/token_client.ts
+++ ecosystem/typescript/sdk/src/token_client.ts
@@ -356,7 +356,7 @@ export class TokenClient {
   }
 
   /**
-   * User opt-in or out direct transfer through a boolean flag
+   * Directly transfer token to a receiver. The receiver should have opted in to direct transfer
    *
    * @param sender AptosAccount where the token will be transferred
    * @param creator  address of the token creator
