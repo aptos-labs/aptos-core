a28ea5cb47b67f352c1b2e002c971934cdd36d13
Larry Liu <larry@aptoslabs.com>
2022-12-01 16:41:16 -0800

    reformat the data preparation.

--- crates/aptos-datastream-service/src/main.rs
+++ crates/aptos-datastream-service/src/main.rs
@@ -2,13 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use aptos_datastream_service::service::DatastreamServer;
+use aptos_datastream_worker::redis_pool_client::RedisClient;
 use aptos_protos::datastream::v1::node_data_service_server::NodeDataServiceServer;
 use std::net::ToSocketAddrs;
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let server = DatastreamServer {};
+    let redis_client = RedisClient::new("localhost:6379".to_string());
+
+    let server = DatastreamServer { redis_client };
+
     Server::builder()
         .add_service(NodeDataServiceServer::new(server))
         .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
--- crates/aptos-datastream-service/src/service.rs
+++ crates/aptos-datastream-service/src/service.rs
@@ -3,7 +3,9 @@
 
 use tonic::{Request, Response, Status};
 
+use aptos_datastream_worker::redis_pool_client::RedisClient;
 use aptos_protos::datastream::v1::node_data_service_server::NodeDataService;
+use aptos_protos::datastream::v1::transactions_data::TransactionData;
 use aptos_protos::datastream::v1::{RawDatastreamRequest, RawDatastreamResponse, TransactionsData};
 use futures::Stream;
 use std::{pin::Pin, time::Duration};
@@ -12,40 +14,33 @@ use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 
 type ResponseStream = Pin<Box<dyn Stream<Item = Result<RawDatastreamResponse, Status>> + Send>>;
 
-#[derive(Debug)]
-pub struct DatastreamServer {}
+pub struct DatastreamServer {
+    pub redis_client: RedisClient,
+}
 
 #[tonic::async_trait]
 impl NodeDataService for DatastreamServer {
     type RawDatastreamStream = ResponseStream;
-
     async fn raw_datastream(
         &self,
         req: Request<RawDatastreamRequest>,
     ) -> Result<Response<Self::RawDatastreamStream>, Status> {
-        println!("EchoServer::server_streaming_echo");
-        println!("\tclient connected from: {:?}", req.remote_addr());
-
-        // creating infinite stream with requested message
-        let repeat = std::iter::repeat(RawDatastreamResponse {
-            data: Some(TransactionsData {
-                ..TransactionsData::default()
-            }),
-            ..RawDatastreamResponse::default()
-        });
-        let mut stream = Box::pin(tokio_stream::iter(repeat).throttle(Duration::from_millis(200)));
-
-        // spawn and channel are required if you want handle "disconnect" functionality
-        // the `out_stream` will not be polled after client disconnect
         let (tx, rx) = mpsc::channel(128);
         tokio::spawn(async move {
-            while let Some(item) = stream.next().await {
-                match tx.send(Result::<_, Status>::Ok(item)).await {
-                    Ok(_) => {
-                        // item (server response) was queued to be send to client
-                    }
-                    Err(_item) => {
-                        // output_stream was build from rx and both are dropped
+            let item = RawDatastreamResponse {
+                data: Some(TransactionsData {
+                    transactions: vec![TransactionData {
+                        encoded_proto_data: "dummy_txn_data".to_string(),
+                        version: 123,
+                    }],
+                }),
+                ..RawDatastreamResponse::default()
+            };
+            loop {
+                match tx.send(Result::<_, Status>::Ok(item.clone())).await {
+                    Ok(_) => {}
+                    Err(_) => {
+                        // Client disconnects.
                         break;
                     }
                 }
