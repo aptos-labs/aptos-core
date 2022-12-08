98f6aeb7f67050ff52b1b68b92d21e7627f5a52a
Sital Kedia <skedia@aptoslabs.com>
2022-12-07 18:59:22 -0800

    fix counters

--- storage/storage-interface/src/cached_state_view.rs
+++ storage/storage-interface/src/cached_state_view.rs
@@ -197,7 +197,7 @@ impl StateView for CachedStateView {
     fn get_state_value(&self, state_key: &StateKey) -> Result<Option<Vec<u8>>> {
         let _timer = TIMER.with_label_values(&["get_state_value"]).start_timer();
         // First check if the cache has the state value.
-        let read_timer = TIMER.with_label_values(&["get_state_value"]).start_timer();
+        let read_timer = TIMER.with_label_values(&["read_lock"]).start_timer();
         if let Some(contents) = self.state_cache.get(state_key) {
             // This can return None, which means the value has been deleted from the DB.
             return Ok(contents.as_ref().map(|v| v.bytes().to_vec()));
@@ -209,9 +209,7 @@ impl StateView for CachedStateView {
         let state_value_option = self.get_state_value_internal(state_key)?;
         drop(read_internal_timer);
         // Update the cache if still empty
-        let write_timer = TIMER
-            .with_label_values(&["get_state_value_internal"])
-            .start_timer();
+        let write_timer = TIMER.with_label_values(&["write_lock"]).start_timer();
 
         let new_value = self
             .state_cache
