fd42d13734b337d1253ff4ac2aa814c805bc42b6
Rustie Lin <rustie117@gmail.com>
2022-12-07 18:49:34 -0700

    [gha] sync changes to workflow deps

--- docker/release-images.mjs
+++ docker/release-images.mjs
@@ -22,12 +22,42 @@
 // 2. docker login - with authorization to push to the `aptoslabs` org
 // 3. gcloud auth configure-docker us-west1-docker.pkg.dev
 // 4. gcloud auth login --update-adc
-// 5. aws-mfa
+// 5. AWS CLI credentials configured
 //
 // Once you have all prerequisites fulfilled, you can run this script via:
 // GIT_SHA=${{ github.sha }} GCP_DOCKER_ARTIFACT_REPO="${{ secrets.GCP_DOCKER_ARTIFACT_REPO }}" AWS_ACCOUNT_ID="${{ secrets.AWS_ECR_ACCOUNT_NUM }}" IMAGE_TAG_PREFIX="${{ inputs.image_tag_prefix }}" ./docker/release_images.sh --wait-for-image-seconds=1800
 
-const IMAGES_TO_RELEASE = ["validator", "forge", "tools", "faucet", "node-checker"];
+const Features = {
+  Default: "default",
+  Indexer: "indexer",
+};
+
+const IMAGES_TO_RELEASE = {
+  validator: {
+    performance: [
+      Features.Default,
+    ],
+    release: [
+      Features.Default,
+      Features.Indexer,
+    ],
+  },
+  forge: {
+    release: [
+      Features.Default,
+    ],
+  },
+  tools: {
+    release: [
+      Features.Default,
+    ],
+  },
+  "node-checker": {
+    release: [
+      Features.Default,
+    ],
+  }
+};
 
 import { execSync } from "node:child_process";
 import { dirname } from "node:path";
@@ -72,7 +102,7 @@ if (process.env.CI === "true") {
   }
   crane = "./crane";
 } else {
-  if ((await $`command -v cranes`.exitCode) !== 0) {
+  if ((await $`command -v crane`.exitCode) !== 0) {
     console.log(
       chalk.red(
         "ERROR: could not find crane binary in PATH - follow https://github.com/google/go-containerregistry/tree/main/cmd/crane#installation to install",
@@ -92,15 +122,32 @@ const TARGET_REGISTRIES = [
 // default 10 seconds
 parsedArgs.WAIT_FOR_IMAGE_SECONDS = parseInt(parsedArgs.WAIT_FOR_IMAGE_SECONDS ?? 10, 10);
 
-for (const image of IMAGES_TO_RELEASE) {
-  for (const targetRegistry of TARGET_REGISTRIES) {
-    const imageSource = `${parsedArgs.GCP_DOCKER_ARTIFACT_REPO}/${image}:${parsedArgs.GIT_SHA}`;
-    const imageTarget = `${targetRegistry}/${image}:${parsedArgs.IMAGE_TAG_PREFIX}`;
-    console.info(chalk.green(`INFO: copying ${imageSource} to ${imageTarget}`));
-    await waitForImageToBecomeAvailable(imageSource, parsedArgs.WAIT_FOR_IMAGE_SECONDS);
-    await $`${crane} copy ${imageSource} ${imageTarget}`;
-    await $`${crane} copy ${imageSource} ${imageTarget + "_" + parsedArgs.GIT_SHA}`;
+for (const [image, imageConfig] of Object.entries(IMAGES_TO_RELEASE)) {
+  for (const [profile, features] of Object.entries(imageConfig)) {
+    // build profiles that are not the default "release" will have a separate prefix
+    const profilePrefix = profile === "release" ? "" : profile;
+    for (const feature of features) {
+      const featureSuffix = feature === Features.Default ? "" : feature;
+
+      for (const targetRegistry of TARGET_REGISTRIES) {
+        const imageSource = `${parsedArgs.GCP_DOCKER_ARTIFACT_REPO}/${image}:${joinTagSegments(
+          profilePrefix,
+          featureSuffix,
+          parsedArgs.GIT_SHA,
+        )}`;
+        const imageTarget = `${targetRegistry}/${image}:${joinTagSegments(parsedArgs.IMAGE_TAG_PREFIX, profilePrefix, featureSuffix)}`;
+        console.info(chalk.green(`INFO: copying ${imageSource} to ${imageTarget}`));
+        await waitForImageToBecomeAvailable(imageSource, parsedArgs.WAIT_FOR_IMAGE_SECONDS);
+        await $`${crane} copy ${imageSource} ${imageTarget}`;
+        await $`${crane} copy ${imageSource} ${joinTagSegments(imageTarget, parsedArgs.GIT_SHA)}`;
+      }
+    }
   }
+}
+
+// joinTagSegments joins tag segments with a dash, but only if the segment is not empty
+function joinTagSegments(...segments) {
+  return segments.filter((s) => s).join("_");
 }
 
 async function waitForImageToBecomeAvailable(imageToWaitFor, waitForImageSeconds) {
@@ -119,7 +166,7 @@ async function waitForImageToBecomeAvailable(imageToWaitFor, waitForImageSeconds
         console.log(
           chalk.yellow(
             // prettier-ignore
-            `WARN: Image ${imageToWaitFor} not available yet - waiting ${ WAIT_TIME_IN_BETWEEN_ATTEMPTS / 1000 } seconds to try again. Time elapsed: ${timeElapsedSeconds().toFixed( 0, )} seconds. Max wait time: ${waitForImageSeconds} seconds`,
+            `WARN: Image ${imageToWaitFor} not available yet - waiting ${WAIT_TIME_IN_BETWEEN_ATTEMPTS / 1000} seconds to try again. Time elapsed: ${timeElapsedSeconds().toFixed(0,)} seconds. Max wait time: ${waitForImageSeconds} seconds`,
           ),
         );
         await sleep(WAIT_TIME_IN_BETWEEN_ATTEMPTS);
--- docker/rust-all.Dockerfile
+++ docker/rust-all.Dockerfile
@@ -7,9 +7,10 @@ ADD https://github.com/krallin/tini/releases/download/v0.19.0/tini /tini
 RUN chmod +x /tini
 ENTRYPOINT ["/tini", "--"]
 
-FROM rust:1.63.0-buster@sha256:0110d1b4193029735f1db1c0ed661676ed4b6f705b11b1ebe95c655b52e6906f AS rust-base
+FROM rust:1.64.0-bullseye@sha256:5cf09a76cb9baf4990d121221bbad64927cc5690ee54f246487e302ddc2ba300 AS rust-base
 WORKDIR /aptos
 RUN apt-get update && apt-get install -y cmake curl clang git pkg-config libssl-dev libpq-dev
+RUN apt-get update && apt-get install binutils lld
 
 ### Build Rust code ###
 FROM rust-base as builder
@@ -253,6 +254,7 @@ RUN apt-get update && apt-get install -y \
     iproute2 \
     netcat \
     libpq-dev \
+    curl \
     && apt-get clean && rm -r /var/lib/apt/lists/*
 
 COPY --link --from=builder /aptos/dist/aptos-telemetry-service /usr/local/bin/aptos-telemetry-service
