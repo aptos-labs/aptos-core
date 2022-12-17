743bfc5285da94f26dfd662f588003af109f9c56
Daniel Porteous (dport) <danielporteous1@gmail.com>
2022-12-17 21:41:56 +0000

    [NHC] Overhaul request model (#5784)

--- ecosystem/node-checker/ts-client/src/models/BlockingRunnerArgs.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type BlockingRunnerArgs = {
-    metrics_fetch_delay_secs: number;
-    api_client_timeout_secs: number;
-};
-
