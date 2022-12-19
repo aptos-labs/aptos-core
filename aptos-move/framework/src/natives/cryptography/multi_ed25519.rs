29f352f8c380cb05ccf0c29662a93929130aac7e
Alin Tomescu <tomescu.alin@gmail.com>
2022-12-19 09:16:26 -0800

    [move] fix MultiEd25519 PK validation small bug (#5822)

--- aptos-move/framework/src/natives/cryptography/multi_ed25519.rs
+++ aptos-move/framework/src/natives/cryptography/multi_ed25519.rs
@@ -9,8 +9,6 @@ use crate::natives::util::make_test_only_native_from_func;
 use aptos_crypto::ed25519::{Ed25519PrivateKey, Ed25519PublicKey};
 use aptos_crypto::ed25519::{ED25519_PUBLIC_KEY_LENGTH, ED25519_SIGNATURE_LENGTH};
 #[cfg(feature = "testing")]
-use aptos_crypto::multi_ed25519::{MultiEd25519PrivateKey, MultiEd25519PublicKey};
-#[cfg(feature = "testing")]
 use aptos_crypto::test_utils::KeyPair;
 use aptos_crypto::{multi_ed25519, traits::*};
 use curve25519_dalek::edwards::CompressedEdwardsY;
@@ -28,6 +26,11 @@ use rand_core::OsRng;
 use smallvec::smallvec;
 use std::{collections::VecDeque, convert::TryFrom};
 
+/// DEPRECATED: See `public_key_validate_internal` comments in `multi_ed25519.move`.
+///
+/// Simply put, this function should have checked that `num_sub_pks > 0` and should abort PK
+/// validation as soon as an invalid sub-PK is found, charging gas accordingly, rather than charge
+/// gas for validating all `num_sub_pks` sub-PKs.
 fn native_public_key_validate(
     gas_params: &GasParameters,
     _context: &mut NativeContext,
@@ -66,6 +69,29 @@ fn native_public_key_validate(
     let num_checked = NumArgs::new(num_checked as u64);
     cost += gas_params.per_pubkey_deserialize * num_checked
         + gas_params.per_pubkey_small_order_check * num_checked;
+
+    Ok(NativeResult::ok(cost, smallvec![Value::bool(all_valid)]))
+}
+
+/// See `public_key_validate_v2_internal` comments in `multi_ed25519.move`.
+fn native_public_key_validate_v2(
+    gas_params: &GasParameters,
+    _context: &mut NativeContext,
+    _ty_args: Vec<Type>,
+    mut arguments: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(_ty_args.is_empty());
+    debug_assert!(arguments.len() == 1);
+
+    let pks_bytes = pop_arg!(arguments, Vec<u8>);
+
+    let mut cost = gas_params.base;
+
+    let (all_valid, num_deserializations, num_small_order_checks) =
+        multi_ed25519::MultiEd25519PublicKey::validate_bytes_and_count_checks(pks_bytes.as_slice());
+
+    cost += gas_params.per_pubkey_deserialize * NumArgs::new(num_deserializations as u64)
+        + gas_params.per_pubkey_small_order_check * NumArgs::new(num_small_order_checks as u64);
 
     Ok(NativeResult::ok(cost, smallvec![Value::bool(all_valid)]))
 }
@@ -137,8 +163,8 @@ fn native_generate_keys(
         .iter()
         .map(|pair| pair.public_key.clone())
         .collect();
-    let group_sk = MultiEd25519PrivateKey::new(private_keys, threshold).unwrap();
-    let group_pk = MultiEd25519PublicKey::new(public_keys, threshold).unwrap();
+    let group_sk = multi_ed25519::MultiEd25519PrivateKey::new(private_keys, threshold).unwrap();
+    let group_pk = multi_ed25519::MultiEd25519PublicKey::new(public_keys, threshold).unwrap();
     Ok(NativeResult::ok(
         InternalGas::zero(),
         smallvec![
@@ -156,7 +182,7 @@ fn native_sign(
 ) -> PartialVMResult<NativeResult> {
     let message = pop_arg!(arguments, Vec<u8>);
     let sk_bytes = pop_arg!(arguments, Vec<u8>);
-    let group_sk = MultiEd25519PrivateKey::try_from(sk_bytes.as_slice()).unwrap();
+    let group_sk = multi_ed25519::MultiEd25519PrivateKey::try_from(sk_bytes.as_slice()).unwrap();
     let sig = group_sk.sign_arbitrary_message(message.as_slice());
     Ok(NativeResult::ok(
         InternalGas::zero(),
@@ -175,6 +201,10 @@ pub fn make_all(gas_params: GasParameters) -> impl Iterator<Item = (String, Nati
         (
             "public_key_validate_internal",
             make_native_from_func(gas_params.clone(), native_public_key_validate),
+        ),
+        (
+            "public_key_validate_v2_internal",
+            make_native_from_func(gas_params.clone(), native_public_key_validate_v2),
         ),
         (
             "signature_verify_strict_internal",
--- crates/aptos-crypto/src/multi_ed25519.rs
+++ crates/aptos-crypto/src/multi_ed25519.rs
@@ -17,6 +17,7 @@ use crate::{
 use anyhow::{anyhow, Result};
 use aptos_crypto_derive::{DeserializeKey, SerializeKey, SilentDebug, SilentDisplay};
 use core::convert::TryFrom;
+use curve25519_dalek::edwards::CompressedEdwardsY;
 use rand::Rng;
 use serde::Serialize;
 use std::{convert::TryInto, fmt};
@@ -115,6 +116,61 @@ impl MultiEd25519PublicKey {
     /// Serialize a MultiEd25519PublicKey.
     pub fn to_bytes(&self) -> Vec<u8> {
         to_bytes(&self.public_keys, self.threshold)
+    }
+
+    /// Checks that all sub PKs would deserialize correctly and are NOT in the small order subgroup.
+    ///
+    /// This function is called by our MultiEd25519 PK validation APIs in Move.
+    ///
+    /// We cannot rely on `TryFrom<&[u8]> for MultiEd25519PublicKey` since it does not exclude
+    /// sub-PKs that are of small order. (Due to limitations in `ed25519_dalek`'s APIs, we cannot
+    /// call the small-subgroup check API on an `ed25519_dalek::PublicKey` struct.) As a result, we
+    /// are forced to replicate some of its code here.
+    ///
+    /// Returns a tuple (`success`, `num_deserializations`, `num_small_order_checks`), where
+    /// `success` is true if the bytes represent a valid MultiEd25519 PK and false otherwise,
+    /// `num_deserializations` is the number of deserialization attempts on a sub PK, and
+    /// `num_small_order_checks` is the number of small order checks on a successfully-deserialized
+    /// PK.
+    ///
+    /// This function returns early as soon as a sub PK fails a check.
+    pub fn validate_bytes_and_count_checks(bytes: &[u8]) -> (bool, usize, usize) {
+        let mut num_deserializations = 0;
+        let mut num_small_order_checks = 0;
+
+        if bytes.is_empty() {
+            return (false, num_deserializations, num_small_order_checks);
+        }
+
+        // Checks that the threshold is correctly encoded in the last bytes of the PK, and that the
+        // # of sub-PKs is > 0 and <= MAX_NUM_OF_KEYS.
+        match check_and_get_threshold(bytes, ED25519_PUBLIC_KEY_LENGTH) {
+            Err(_) => return (false, num_deserializations, num_small_order_checks),
+            _ => {}
+        };
+
+        for chunk in bytes.chunks_exact(ED25519_PUBLIC_KEY_LENGTH) {
+            // Parse as a slice
+            let slice = match <[u8; ED25519_PUBLIC_KEY_LENGTH]>::try_from(chunk) {
+                Ok(slice) => slice,
+                Err(_) => return (false, num_deserializations, num_small_order_checks), // This should never happen because this is a ChunksExact iterator
+            };
+
+            // First, check this is a valid point on the curve.
+            num_deserializations += 1;
+            let point = match CompressedEdwardsY(slice).decompress() {
+                Some(point) => point,
+                None => return (false, num_deserializations, num_small_order_checks),
+            };
+
+            // Second, check this is NOT a small order point.
+            num_small_order_checks += 1;
+            if point.is_small_order() {
+                return (false, num_deserializations, num_small_order_checks);
+            }
+        }
+
+        (true, num_deserializations, num_small_order_checks)
     }
 }
 
