0e2d969f4cc10c04e8809e543037510076c37b6a
David Wolinsky <isaac.wolinsky@gmail.com>
2022-12-23 16:12:17 -0800

    Update aptos-move/aptos-gas/src/gas_meter.rs

--- aptos-move/aptos-gas/src/gas_meter.rs
+++ aptos-move/aptos-gas/src/gas_meter.rs
@@ -48,7 +48,7 @@ use std::collections::BTreeMap;
 //       global operations.
 // - V1
 //   - TBA
-pub const LATEST_GAS_FEATURE_VERSION: u64 = 6;
+pub const LATEST_GAS_FEATURE_VERSION: u64 = 5;
 
 pub(crate) const EXECUTION_GAS_MULTIPLIER: u64 = 20;
 
