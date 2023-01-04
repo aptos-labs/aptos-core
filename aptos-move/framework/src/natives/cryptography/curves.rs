4aa7512033c0da040fffe367707157c47b7ad85e
zhoujun.ma <zhoujun@aptoslabs.com>
2023-01-03 18:23:55 -0800

    gt test case

--- aptos-move/framework/src/natives/cryptography/curves.rs
+++ aptos-move/framework/src/natives/cryptography/curves.rs
@@ -28,6 +28,7 @@ use move_vm_types::pop_arg;
 use move_vm_types::values::Value;
 use move_vm_types::values::{Struct, Vector};
 use num_traits::identities::Zero;
+use num_traits::One;
 use smallvec::smallvec;
 use std::collections::VecDeque;
 use std::iter::Map;
@@ -294,14 +295,24 @@ fn deserialize_element_uncompressed_internal(
             }
         }
         "0x1::curves::BLS12_381_Gt" => {
-            let point = ark_bls12_381::Fq12::deserialize_uncompressed(bytes.as_slice());
+            let point = ark_bls12_381::Fq12::deserialize(bytes.as_slice());
             match point {
                 Ok(point) => {
-                    let handle = context
-                        .extensions_mut()
-                        .get_mut::<ArksContext>()
-                        .add_gt_point(point);
-                    (true, handle)
+                    let r_buf = hex::decode(
+                        "01000000fffffffffe5bfeff02a4bd5305d8a10908d83933487d9d2953a7ed73",
+                    )
+                    .unwrap();
+                    let r =
+                        ark_ff::BigInteger256::deserialize_uncompressed(r_buf.as_slice()).unwrap();
+                    if ark_bls12_381::Fq12::one() == point.pow(r) {
+                        let handle = context
+                            .extensions_mut()
+                            .get_mut::<ArksContext>()
+                            .add_gt_point(point);
+                        (true, handle)
+                    } else {
+                        (false, 0)
+                    }
                 }
                 _ => (false, 0),
             }
@@ -356,11 +367,21 @@ fn deserialize_element_compressed_internal(
             let point = ark_bls12_381::Fq12::deserialize(bytes.as_slice());
             match point {
                 Ok(point) => {
-                    let handle = context
-                        .extensions_mut()
-                        .get_mut::<ArksContext>()
-                        .add_gt_point(point);
-                    (true, handle)
+                    let r_buf = hex::decode(
+                        "01000000fffffffffe5bfeff02a4bd5305d8a10908d83933487d9d2953a7ed73",
+                    )
+                    .unwrap();
+                    let r =
+                        ark_ff::BigInteger256::deserialize_uncompressed(r_buf.as_slice()).unwrap();
+                    if ark_bls12_381::Fq12::one() == point.pow(r) {
+                        let handle = context
+                            .extensions_mut()
+                            .get_mut::<ArksContext>()
+                            .add_gt_point(point);
+                        (true, handle)
+                    } else {
+                        (false, 0)
+                    }
                 }
                 _ => (false, 0),
             }
@@ -385,7 +406,7 @@ fn scalar_from_bytes_internal(
         .to_string();
     let bytes = pop_arg!(args, Vec<u8>);
     let (succ, handle) = match type_tag.as_str() {
-        "0x1::curves::BLS12_381_G1" | "0x1::curves::BLS12_381_G2" | "0x1::curves::BLS12_381_G2" => {
+        "0x1::curves::BLS12_381_G1" | "0x1::curves::BLS12_381_G2" | "0x1::curves::BLS12_381_Gt" => {
             let scalar = ark_bls12_381::Fr::deserialize_uncompressed(bytes.as_slice());
             match (scalar) {
                 Ok(scalar) => {
@@ -670,7 +691,7 @@ fn point_identity_internal(
             handle
         }
         "0x1::curves::BLS12_381_Gt" => {
-            let point = ark_bls12_381::Fq12::zero();
+            let point = ark_bls12_381::Fq12::one();
             let handle = context
                 .extensions_mut()
                 .get_mut::<ArksContext>()
@@ -715,7 +736,8 @@ fn point_generator_internal(
             handle
         }
         "0x1::curves::BLS12_381_Gt" => {
-            let point = ark_bls12_381::Fq12::zero();
+            let buf = hex::decode("b68917caaa0543a808c53908f694d1b6e7b38de90ce9d83d505ca1ef1b442d2727d7d06831d8b2a7920afc71d8eb50120f17a0ea982a88591d9f43503e94a8f1abaf2e4589f65aafb7923c484540a868883432a5c60e75860b11e5465b1c9a08873ec29e844c1c888cb396933057ffdd541b03a5220eda16b2b3a6728ea678034ce39c6839f20397202d7c5c44bb68134f93193cec215031b17399577a1de5ff1f5b0666bdd8907c61a7651e4e79e0372951505a07fa73c25788db6eb8023519a5aa97b51f1cad1d43d8aabbff4dc319c79a58cafc035218747c2f75daf8f2fb7c00c44da85b129113173d4722f5b201b6b4454062e9ea8ba78c5ca3cadaf7238b47bace5ce561804ae16b8f4b63da4645b8457a93793cbd64a7254f150781019de87ee42682940f3e70a88683d512bb2c3fb7b2434da5dedbb2d0b3fb8487c84da0d5c315bdd69c46fb05d23763f2191aabd5d5c2e12a10b8f002ff681bfd1b2ee0bf619d80d2a795eb22f2aa7b85d5ffb671a70c94809f0dafc5b73ea2fb0657bae23373b4931bc9fa321e8848ef78894e987bff150d7d671aee30b3931ac8c50e0b3b0868effc38bf48cd24b4b811a2995ac2a09122bed9fd9fa0c510a87b10290836ad06c8203397b56a78e9a0c61c77e56ccb4f1bc3d3fcaea7550f3503efe30f2d24f00891cb45620605fcfaa4292687b3a7db7c1c0554a93579e889a121fd8f72649b2402996a084d2381c5043166673b3849e4fd1e7ee4af24aa8ed443f56dfd6b68ffde4435a92cd7a4ac3bc77e1ad0cb728606cf08bf6386e5410f").unwrap();
+            let point = ark_bls12_381::Fq12::deserialize(buf.as_slice()).unwrap();
             let handle = context
                 .extensions_mut()
                 .get_mut::<ArksContext>()
