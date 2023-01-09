b41e9e5944dedde4a1139dc9b63deb7140b3d63e
Sital Kedia <skedia@aptoslabs.com>
2023-01-09 15:51:17 -0800

    Increase block size and decrease backpressure

--- config/src/config/consensus_config.rs
+++ config/src/config/consensus_config.rs
@@ -48,10 +48,10 @@ pub struct ChainHealthBackoffValues {
 impl Default for ConsensusConfig {
     fn default() -> ConsensusConfig {
         ConsensusConfig {
-            max_sending_block_txns: 3500,
+            max_sending_block_txns: 7000,
             // defaulting to under 0.5s to broadcast the proposal to 100 validators
             // over 1gbps link
-            max_sending_block_bytes: 2 * 1024 * 1024, // 600 KB
+            max_sending_block_bytes: 4 * 1024 * 1024, // 600 KB
             max_receiving_block_txns: 10000,
             max_receiving_block_bytes: 3 * 1024 * 1024, // 3MB
             max_pruned_blocks_in_mem: 100,
