2af4416d011311894704268279235e0307385ee2
zhoujun.ma <zhoujun@aptoslabs.com>
2023-01-18 10:16:14 -0800

    revert to single bls12381 backend (but depend on both ark and blst)

--- aptos-move/aptos-vm/src/natives.rs
+++ aptos-move/aptos-vm/src/natives.rs
@@ -5,7 +5,7 @@ use aptos_gas::{AbstractValueSizeGasParameters, NativeGasParameters, LATEST_GAS_
 use aptos_types::account_config::CORE_CODE_ADDRESS;
 use move_vm_runtime::native_functions::NativeFunctionTable;
 
-use aptos_framework::natives::cryptography::groups::arkworks_backend::Bls12381Context;
+use aptos_framework::natives::cryptography::groups::Bls12381Context;
 #[cfg(feature = "testing")]
 use aptos_types::chain_id::ChainId;
 #[cfg(feature = "testing")]
