de25f2c60b1d5067cd7b388d79c21d4842705ea4
Balaji Arun <balajia@vt.edu>
2023-01-18 12:12:00 -0800

    aggressive back pressure

--- config/src/config/quorum_store_config.rs
+++ config/src/config/quorum_store_config.rs
@@ -43,7 +43,7 @@ impl Default for QuorumStoreConfig {
             batch_request_num_peers: 2,
             mempool_pulling_interval: 100,
             end_batch_ms: 500,
-            max_batch_counts: 1000,
+            max_batch_counts: 300,
             max_batch_bytes: 1000000,
             batch_request_timeout_ms: 10000,
             batch_expiry_round_gap_when_init: 100,
@@ -56,7 +56,7 @@ impl Default for QuorumStoreConfig {
             mempool_txn_pull_max_bytes: 1000000,
             num_nodes_per_worker_handles: 10,
             back_pressure_factor: 1, // back pressure limit for QS is back_pressure_factor * num_validator
-            back_pressure_local_batch_num: 10, // QS will be backpressured if the remaining local batches is more than this number
+            back_pressure_local_batch_num: 2, // QS will be backpressured if the remaining local batches is more than this number
         }
     }
 }
