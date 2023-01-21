8820e4bf3c2fe04a4278cc2106aa2c379603ea62
bowenyang007 <carlyang007@gmail.com>
2023-01-20 19:40:45 -0800

    lint

--- ecosystem/indexer-grpc/indexer-grpc-fullnode/src/stream_coordinator.rs
+++ ecosystem/indexer-grpc/indexer-grpc-fullnode/src/stream_coordinator.rs
@@ -88,7 +88,6 @@ impl IndexerStreamCoordinator {
                         response: Some(raw_datastream_response::Response::Data(
                             TransactionsOutput {
                                 transactions: chunk.to_vec(),
-                                ..TransactionsOutput::default()
                             },
                         )),
                     };
