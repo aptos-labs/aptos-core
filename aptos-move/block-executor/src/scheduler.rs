b09a1f84c8847eb83f1aaad24eac3e3180c2db33
danielxiangzl <xiangzhuolun@gmail.com>
2023-01-26 16:10:14 -0800

    clean up

--- aptos-move/block-executor/src/scheduler.rs
+++ aptos-move/block-executor/src/scheduler.rs
@@ -266,11 +266,6 @@ impl Scheduler {
         (commit_state.0, commit_state.1)
     }
 
-    /// Return the number of transactions to be executed from the block.
-    pub fn num_txn_to_execute(&self) -> usize {
-        self.num_txns
-    }
-  
     /// Try to abort version = (txn_idx, incarnation), called upon validation failure.
     /// When the invocation manages to update the status of the transaction, it changes
     /// Executed(incarnation) => Aborting(incarnation), it returns true. Otherwise,
