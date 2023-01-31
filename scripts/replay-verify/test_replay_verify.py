2a9321971c6e09b2c911e8b87a2a2d06d538b73e
Rustie Lin <rustie117@gmail.com>
2023-01-31 15:58:30 -0800

    [replay-verify] port to python

---
+++ scripts/replay-verify/test_replay_verify.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Aptos
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import unittest
+import subprocess
+
+from .replay_verify import find_latest_version_from_db_backup_output
+
+
+class ReplayVerifyHarnessTests(unittest.TestCase):
+    def testFindLatestVersionFromDbBackupOutput(self) -> None:
+        proc = subprocess.Popen(
+            f"cat {os.path.dirname(__file__)}/fixtures/backup_oneshot.fixture", shell=True, stdout=subprocess.PIPE
+        )
+        latest_version = find_latest_version_from_db_backup_output(proc.stdout)
+        self.assertEqual(latest_version, 417000000)
+        proc.communicate()
