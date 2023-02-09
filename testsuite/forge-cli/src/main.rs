e73034db850e6f5664c920000a999707513c30c2
Balaji Arun <balajia@vt.edu>
2023-02-08 22:18:36 -0800

    [forge] 50 nodes three region sim

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -979,9 +979,9 @@ fn three_region_simulation_with_different_node_speed(config: ForgeConfig) -> For
 
 fn three_region_simulation(config: ForgeConfig) -> ForgeConfig {
     config
-        .with_initial_validator_count(NonZeroUsize::new(12).unwrap())
-        .with_initial_fullnode_count(12)
-        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::ConstTps { tps: 5000 }))
+        .with_initial_validator_count(NonZeroUsize::new(50).unwrap())
+        .with_initial_fullnode_count(50)
+        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::ConstTps { tps: 10000 }))
         .with_network_tests(vec![&ThreeRegionSimulationTest {
             add_execution_delay: None,
         }])
