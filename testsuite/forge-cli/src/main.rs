4f83f67661db576e8fdbdfc4c694dd83ce46df41
Balaji Arun <balajia@vt.edu>
2023-02-10 07:58:08 -0800

    [forge] three region sim reduce to 5k tps

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -980,7 +980,7 @@ fn three_region_simulation(config: ForgeConfig) -> ForgeConfig {
     config
         .with_initial_validator_count(NonZeroUsize::new(100).unwrap())
         .with_initial_fullnode_count(100)
-        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::ConstTps { tps: 6000 }))
+        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::ConstTps { tps: 5000 }))
         .with_network_tests(vec![&ThreeRegionSimulationTest {
             add_execution_delay: None,
         }])
