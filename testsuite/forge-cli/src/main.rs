e11d891307cdd25a25e7ede398985e803f32e3aa
Balaji Arun <balajia@vt.edu>
2023-02-15 10:20:32 -0800

    5.2k max tps and 5k block size

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1148,7 +1148,7 @@ fn land_blocking_test_suite(duration: Duration) -> ForgeConfig<'static> {
         .with_initial_validator_count(NonZeroUsize::new(100).unwrap())
         .with_initial_fullnode_count(100)
         .with_network_tests(vec![&PerformanceBenchmarkWithFN])
-        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::ConstTps { tps: 5500 }))
+        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::ConstTps { tps: 5200 }))
         .with_genesis_helm_config_fn(Arc::new(|helm_values| {
             // Have single epoch change in land blocking
             helm_values["chain"]["epoch_duration_secs"] = 300.into();
