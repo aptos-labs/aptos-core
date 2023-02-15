85f67f14daa756d49ca6496528aee47e7c493dd0
Larry Liu <larry@aptoslabs.com>
2023-02-14 22:42:53 -0800

    add the explicity logging when retry happens.

--- ecosystem/indexer-grpc/indexer-grpc-cache-worker/src/lib.rs
+++ ecosystem/indexer-grpc/indexer-grpc-cache-worker/src/lib.rs
@@ -70,16 +70,17 @@ pub fn get_ttl_in_seconds(timestamp_in_seconds: u64) -> u64 {
 /// Create a gRPC client with exponential backoff.
 pub async fn create_grpc_client(address: String) -> GrpcClientType {
     backoff::future::retry(backoff::ExponentialBackoff::default(), || async {
-        Ok(
-            match datastream::indexer_stream_client::IndexerStreamClient::connect(address.clone())
-                .await {
-                    Ok(client) => client,
-                    Err(e) => {
-                        aptos_logger::error!(address=address.clone(), "[Indexer Cache] Failed to connect to indexer gRPC server: {}", e);
-                        e
-                    }
+        match datastream::indexer_stream_client::IndexerStreamClient::connect(address.clone())
+            .await {
+                Ok(client) => {
+                    aptos_logger::info!(address=address.clone(), "[Indexer Cache] Connected to indexer gRPC server.");
+                    Ok(client)
                 },
-        )
+                Err(e) => {
+                    aptos_logger::error!(address=address.clone(), "[Indexer Cache] Failed to connect to indexer gRPC server: {}", e);
+                    Err(backoff::Error::transient(e))
+                }
+            }
     })
     .await
     .unwrap()
