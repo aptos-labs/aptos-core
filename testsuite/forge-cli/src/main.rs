f742cb025d4419c17d86c70f0a0dce6103f3009f
Balaji Arun <balajia@vt.edu>
2023-02-15 09:22:54 -0800

    40k backlog, 100 nodes, wait for full block, no epoch change

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1141,12 +1141,15 @@ fn validators_join_and_leave(forge_config: ForgeConfig<'static>) -> ForgeConfig<
 
 fn land_blocking_test_suite(duration: Duration) -> ForgeConfig<'static> {
     ForgeConfig::default()
-        .with_initial_validator_count(NonZeroUsize::new(20).unwrap())
-        .with_initial_fullnode_count(10)
+        .with_initial_validator_count(NonZeroUsize::new(100).unwrap())
+        .with_initial_fullnode_count(100)
         .with_network_tests(vec![&PerformanceBenchmarkWithFN])
+        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::MaxLoad {
+            mempool_backlog: 40000,
+        }))
         .with_genesis_helm_config_fn(Arc::new(|helm_values| {
             // Have single epoch change in land blocking
-            helm_values["chain"]["epoch_duration_secs"] = 300.into();
+            helm_values["chain"]["epoch_duration_secs"] = (60 * 60).into();
         }))
         .with_success_criteria(
             SuccessCriteria::new(
