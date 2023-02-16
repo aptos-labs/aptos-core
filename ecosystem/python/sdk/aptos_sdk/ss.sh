171d576a318d240fb9878ceff4a07637e2aee480
alnoki <43892045+alnoki@users.noreply.github.com>
2023-02-15 17:19:03 -0800

    Update poetry dependencies

--- ecosystem/python/sdk/aptos_sdk/ss.sh
+++ ecosystem/python/sdk/aptos_sdk/ss.sh
@@ -3,18 +3,18 @@ if test $1 = r; then
     rm -rf tmp # Clear temp dir.
     mkdir tmp # Make temp dir.
     # Generate Ace keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Ace \
         --vanity-prefix 0xace \
         --outfile tmp/ace.keyfile
     # Generate Bee keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Bee \
         --vanity-prefix 0xbee \
         --outfile tmp/bee.keyfile
-    python amee.py keyfile fund tmp/ace.keyfile # Fund Ace.
+    poetry run python amee.py keyfile fund tmp/ace.keyfile # Fund Ace.
     # Incorporate into 1-of-2 multisig.
-    python amee.py metafile incorporate \
+    poetry run python amee.py metafile incorporate \
         1 \
         Initial \
         --keyfiles \
@@ -22,87 +22,87 @@ if test $1 = r; then
             tmp/bee.keyfile \
         --outfile tmp/initial.multisig
     # Propose rotation challenge for rotating to multisig.
-    python amee.py rotate challenge propose \
+    poetry run python amee.py rotate challenge propose \
         tmp/ace.keyfile \
         tmp/initial.multisig \
         2030-01-01 \
         Initial \
         --outfile tmp/initial.challenge_proposal
     # Have Ace sign challenge proposal.
-    python amee.py rotate challenge sign \
+    poetry run python amee.py rotate challenge sign \
         tmp/initial.challenge_proposal \
         tmp/ace.keyfile \
         Ace initial \
         --outfile tmp/ace_initial.challenge_signature
     # Have Ace execute rotation from single-signer account.
-    python amee.py rotate execute single \
+    poetry run python amee.py rotate execute single \
         tmp/ace.keyfile \
         tmp/initial.multisig \
         tmp/ace_initial.challenge_signature
     # Increase metafile threshold to two signatures.
-    python amee.py metafile threshold \
+    poetry run python amee.py metafile threshold \
         tmp/initial.multisig \
         2 \
         Increased \
         --outfile tmp/increased.multisig
     # Propose rotation challenge for increasing threshold.
-    python amee.py rotate challenge propose \
+    poetry run python amee.py rotate challenge propose \
         tmp/initial.multisig \
         tmp/increased.multisig \
         2030-01-01 \
         Increase \
         --outfile tmp/increase.challenge_proposal
     # Have Ace sign challenge proposal.
-    python amee.py rotate challenge sign \
+    poetry run python amee.py rotate challenge sign \
         tmp/increase.challenge_proposal \
         tmp/ace.keyfile \
         Ace increase \
         --outfile tmp/ace_increase.challenge_signature
     # Have Bee sign challenge proposal.
-    python amee.py rotate challenge sign \
+    poetry run python amee.py rotate challenge sign \
         tmp/increase.challenge_proposal \
         tmp/bee.keyfile \
         Bee increase \
         --outfile tmp/bee_increase.challenge_signature
     # Propose rotation transaction.
-    python amee.py rotate transaction propose \
+    poetry run python amee.py rotate transaction propose \
         Increase transaction \
         --from-signatures tmp/ace_increase.challenge_signature \
         --to-signatures tmp/ace_increase.challenge_signature \
             tmp/bee_increase.challenge_signature \
         --outfile tmp/increase.rotation_transaction_proposal
     # Have Bee only sign rotation transaction proposal (1-of-2).
-    python amee.py rotate transaction sign \
+    poetry run python amee.py rotate transaction sign \
         tmp/increase.rotation_transaction_proposal \
         tmp/bee.keyfile \
         Bee increase \
         --outfile tmp/bee_increase.rotation_transaction_signature
     # Submit rotation transaction.
-    python amee.py rotate execute multisig \
+    poetry run python amee.py rotate execute multisig \
         tmp/initial.multisig \
         --signatures tmp/bee_increase.rotation_transaction_signature \
         --to-metafile tmp/increased.multisig
     # Propose rotation challenge for rotating back to Ace.
-    python amee.py rotate challenge propose \
+    poetry run python amee.py rotate challenge propose \
         tmp/increased.multisig \
         tmp/ace.keyfile \
         2030-01-01 \
         Return \
         --outfile tmp/return.challenge_proposal
     # Have Ace sign challenge proposal.
-    python amee.py rotate challenge sign \
+    poetry run python amee.py rotate challenge sign \
         tmp/return.challenge_proposal \
         tmp/ace.keyfile \
         Ace return \
         --outfile tmp/ace_return.challenge_signature
     # Have Bee sign challenge proposal.
-    python amee.py rotate challenge sign \
+    poetry run python amee.py rotate challenge sign \
         tmp/return.challenge_proposal \
         tmp/bee.keyfile \
         Bee return \
         --outfile tmp/bee_return.challenge_signature
     # Propose rotation transaction.
-    python amee.py rotate transaction propose \
+    poetry run python amee.py rotate transaction propose \
         Return \
         --from-signatures \
             tmp/ace_return.challenge_signature \
@@ -110,19 +110,19 @@ if test $1 = r; then
         --to-signatures tmp/ace_return.challenge_signature \
         --outfile tmp/return.rotation_transaction_proposal
     # Have Ace sign rotation transaction proposal.
-    python amee.py rotate transaction sign \
+    poetry run python amee.py rotate transaction sign \
         tmp/return.rotation_transaction_proposal \
         tmp/ace.keyfile \
         Ace return \
         --outfile tmp/ace_return.rotation_transaction_signature
     # Have Bee sign rotation transaction proposal.
-    python amee.py rotate transaction sign \
+    poetry run python amee.py rotate transaction sign \
         tmp/return.rotation_transaction_proposal \
         tmp/bee.keyfile \
         Bee return \
         --outfile tmp/bee_return.rotation_transaction_signature
     # Submit rotation transaction.
-    python amee.py rotate execute multisig \
+    poetry run python amee.py rotate execute multisig \
         tmp/increased.multisig \
         --signatures \
             tmp/ace_return.rotation_transaction_signature \
@@ -134,17 +134,17 @@ elif test $1 = m; then
     rm -rf tmp # Clear temp dir.
     mkdir tmp # Make temp dir.
     # Generate Ace keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Ace \
         --vanity-prefix 0xace \
         --outfile tmp/ace.keyfile
     # Generate Bee keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Bee \
         --vanity-prefix 0xbee \
         --outfile tmp/bee.keyfile
     # Incorporate into 1-of-2 multisig.
-    python amee.py metafile incorporate \
+    poetry run python amee.py metafile incorporate \
         1 \
         Initial \
         --keyfiles \
@@ -152,17 +152,17 @@ elif test $1 = m; then
             tmp/bee.keyfile \
         --outfile tmp/initial.multisig
     # Generate Ace keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Cad \
         --vanity-prefix 0xcad \
         --outfile tmp/cad.keyfile
     # Generate Dee keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Dee \
         --vanity-prefix 0xdee \
         --outfile tmp/dee.keyfile
     # Append Cad and Dee to create 3-of-4 multisig.
-    python amee.py metafile append \
+    poetry run python amee.py metafile append \
         tmp/initial.multisig \
         3 \
         Increased \
@@ -171,14 +171,14 @@ elif test $1 = m; then
             tmp/dee.keyfile \
         --outfile tmp/increased.multisig
     # Remove Ace and Dee to create 1-of-2 multisig.
-    python amee.py metafile remove \
+    poetry run python amee.py metafile remove \
         tmp/increased.multisig \
         1 \
         Removed \
         --signatories 0 3 \
         --outfile tmp/removed.multisig
     # Change threshold to create 2-of-2 multisig.
-    python amee.py metafile threshold \
+    poetry run python amee.py metafile threshold \
         tmp/removed.multisig \
         2 \
         Changed \
@@ -190,17 +190,17 @@ elif test $1 = p; then
     rm -rf tmp # Clear temp dir.
     mkdir tmp # Make temp dir.
     # Generate Ace keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Ace \
         --vanity-prefix 0xace \
         --outfile tmp/ace.keyfile
     # Generate Bee keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Bee \
         --vanity-prefix 0xbee \
         --outfile tmp/bee.keyfile
     # Incorporate into 1-of-2 multisig.
-    python amee.py metafile incorporate \
+    poetry run python amee.py metafile incorporate \
         1 \
         Protocol \
         --keyfiles \
@@ -208,9 +208,9 @@ elif test $1 = p; then
             tmp/bee.keyfile \
         --outfile tmp/protocol.multisig
     # Fund multisig.
-    python amee.py metafile fund tmp/protocol.multisig
+    poetry run python amee.py metafile fund tmp/protocol.multisig
     # Propose publication.
-    python amee.py publish propose \
+    poetry run python amee.py publish propose \
         tmp/protocol.multisig \
         alnoki \
         aptos-core \
@@ -221,15 +221,15 @@ elif test $1 = p; then
         Genesis \
         --outfile tmp/genesis.publication_proposal
     # Sign publication.
-    python amee.py publish sign \
+    poetry run python amee.py publish sign \
         tmp/genesis.publication_proposal \
         tmp/ace.keyfile \
         Genesis \
         --outfile tmp/genesis.publication_signature
     # Execute publication.
-    python amee.py publish execute tmp/genesis.publication_signature
+    poetry run python amee.py publish execute tmp/genesis.publication_signature
     # Propose upgrade.
-    python amee.py publish propose \
+    poetry run python amee.py publish propose \
         tmp/protocol.multisig \
         alnoki \
         aptos-core \
@@ -240,13 +240,13 @@ elif test $1 = p; then
         Upgrade \
         --outfile tmp/upgrade.publication_proposal
     # Sign upgrade publication.
-    python amee.py publish sign \
+    poetry run python amee.py publish sign \
         tmp/upgrade.publication_proposal \
         tmp/ace.keyfile \
         Genesis \
         --outfile tmp/upgrade.publication_signature
     # Execute upgrade publication.
-    python amee.py publish execute tmp/upgrade.publication_signature
+    poetry run python amee.py publish execute tmp/upgrade.publication_signature
     rm -rf tmp # Clear temp dir.
 
 # Invoke script.
@@ -254,17 +254,17 @@ elif test $1 = s; then
     rm -rf tmp # Clear temp dir.
     mkdir tmp # Make temp dir.
     # Generate Ace keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Ace \
         --vanity-prefix 0xace \
         --outfile tmp/ace.keyfile
     # Generate Bee keyfile.
-    python amee.py keyfile generate \
+    poetry run python amee.py keyfile generate \
         Bee \
         --vanity-prefix 0xbee \
         --outfile tmp/bee.keyfile
     # Incorporate into 1-of-2 multisig.
-    python amee.py metafile incorporate \
+    poetry run python amee.py metafile incorporate \
         1 \
         Protocol \
         --keyfiles \
@@ -272,9 +272,9 @@ elif test $1 = s; then
             tmp/bee.keyfile \
         --outfile tmp/protocol.multisig
     # Fund multisig.
-    python amee.py metafile fund tmp/protocol.multisig
+    poetry run python amee.py metafile fund tmp/protocol.multisig
     # Propose publication.
-    python amee.py publish propose \
+    poetry run python amee.py publish propose \
         tmp/protocol.multisig \
         alnoki \
         aptos-core \
@@ -285,15 +285,15 @@ elif test $1 = s; then
         Genesis \
         --outfile tmp/genesis.publication_proposal
     # Sign publication.
-    python amee.py publish sign \
+    poetry run python amee.py publish sign \
         tmp/genesis.publication_proposal \
         tmp/ace.keyfile \
         Genesis \
         --outfile tmp/genesis.publication_signature
     # Execute publication.
-    python amee.py publish execute tmp/genesis.publication_signature
+    poetry run python amee.py publish execute tmp/genesis.publication_signature
     # Propose script invocation.
-    python amee.py script propose \
+    poetry run python amee.py script propose \
         tmp/protocol.multisig \
         alnoki \
         aptos-core \
@@ -305,13 +305,13 @@ elif test $1 = s; then
         Invoke \
         --outfile tmp/invoke.script_proposal
     # Sign invocation proposal.
-    python amee.py script sign \
+    poetry run python amee.py script sign \
         tmp/invoke.script_proposal \
         tmp/ace.keyfile \
         Invoke \
         --outfile tmp/invoke.script_signature
     # Execute invocation.
-    python amee.py script execute tmp/invoke.script_signature
+    poetry run python amee.py script execute tmp/invoke.script_signature
     rm -rf tmp # Clear temp dir.
 
 fi
