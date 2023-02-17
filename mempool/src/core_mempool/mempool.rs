80682eca8190c7f0f77c817d3495b057311fabf4
Josh Lind <josh.lind@hotmail.com>
2023-02-17 15:55:45 -0500

    [Mempool] Add shared license to relevant files.

--- mempool/src/core_mempool/mempool.rs
+++ mempool/src/core_mempool/mempool.rs
@@ -1,4 +1,5 @@
 // Copyright © Aptos Foundation
+// Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 //! Mempool is used to track transactions which have been submitted but not yet
