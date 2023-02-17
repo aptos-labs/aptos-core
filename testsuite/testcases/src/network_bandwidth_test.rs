83c958a404aee30235548d4d8e9370e4c9644c32
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2023-02-16 17:40:10 -0800

    [forge] Change default to send traffic only to FN (if any are defined) (#6600)
    and to validators otherwise

--- testsuite/testcases/src/network_bandwidth_test.rs
+++ testsuite/testcases/src/network_bandwidth_test.rs
@@ -34,7 +34,7 @@ impl NetworkLoadTest for NetworkBandwidthTest {
         );
         println!("{}", msg);
         ctx.report.report_text(msg);
-        Ok(LoadDestination::AllNodes)
+        Ok(LoadDestination::FullnodesOtherwiseValidators)
     }
 
     fn finish(&self, swarm: &mut dyn Swarm) -> anyhow::Result<()> {
