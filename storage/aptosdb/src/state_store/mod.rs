091538ffca2c55e267b08768fbf864f6ce3faacf
Josh Lind <josh.lind@hotmail.com>
2023-02-17 18:54:45 -0500

    [Storage] Add shared license to relevant files.

--- storage/aptosdb/src/state_store/mod.rs
+++ storage/aptosdb/src/state_store/mod.rs
@@ -1,4 +1,5 @@
 // Copyright © Aptos Foundation
+// Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 //! This file defines state store APIs that are related account state Merkle tree.
