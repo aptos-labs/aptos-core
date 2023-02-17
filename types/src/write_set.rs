d1839f8fd1b31e49ebe8c1e49f6e234eac176524
Josh Lind <josh.lind@hotmail.com>
2023-02-17 16:51:37 -0500

    [Types] Add shared license to relevant files.

--- types/src/write_set.rs
+++ types/src/write_set.rs
@@ -1,4 +1,5 @@
 // Copyright © Aptos Foundation
+// Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 //! For each transaction the VM executes, the VM will output a `WriteSet` that contains each access
