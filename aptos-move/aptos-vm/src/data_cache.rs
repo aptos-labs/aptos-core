285aed4d92c51b8b6de13e3940bee31c057b1677
Josh Lind <josh.lind@hotmail.com>
2023-02-18 16:53:58 -0500

    [Aptos Move] Add shared license to relevant files.

--- aptos-move/aptos-vm/src/data_cache.rs
+++ aptos-move/aptos-vm/src/data_cache.rs
@@ -1,4 +1,5 @@
 // Copyright © Aptos Foundation
+// Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 //! Scratchpad for on chain values during the execution.
 
--- aptos-move/e2e-tests/src/common_transactions.rs
+++ aptos-move/e2e-tests/src/common_transactions.rs
@@ -1,4 +1,5 @@
 // Copyright © Aptos Foundation
+// Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 //! Support for encoding transactions for common situations.
