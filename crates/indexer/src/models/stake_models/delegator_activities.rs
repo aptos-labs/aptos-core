828129aa007689eff3cc788c6a474de3669e124c
bowenyang007 <carlyang007@gmail.com>
2023-02-24 08:22:29 -0800

    Loop through BMT as well for staking events

--- crates/indexer/src/models/stake_models/delegator_activities.rs
+++ crates/indexer/src/models/stake_models/delegator_activities.rs
@@ -26,59 +26,63 @@ impl DelegatedStakingActivity {
     /// Pretty straightforward parsing from known delegated staking events
     pub fn from_transaction(transaction: &APITransaction) -> anyhow::Result<Vec<Self>> {
         let mut delegator_activities = vec![];
-        if let APITransaction::UserTransaction(user_txn) = transaction {
-            for (index, event) in user_txn.events.iter().enumerate() {
-                let txn_version = user_txn.info.version.0 as i64;
-                let event_type = event.typ.to_string();
-                let event_index = index as i64;
-                if let Some(staking_event) =
-                    StakeEvent::from_event(event_type.as_str(), &event.data, txn_version)?
-                {
-                    let activity = match staking_event {
-                        StakeEvent::AddStakeEvent(inner) => DelegatedStakingActivity {
-                            transaction_version: txn_version,
-                            event_index,
-                            delegator_address: inner.delegator_address,
-                            pool_address: inner.pool_address,
-                            event_type: event_type.clone(),
-                            amount: u64_to_bigdecimal(inner.amount_added),
-                        },
-                        StakeEvent::UnlockStakeEvent(inner) => DelegatedStakingActivity {
-                            transaction_version: txn_version,
-                            event_index,
-                            delegator_address: inner.delegator_address,
-                            pool_address: inner.pool_address,
-                            event_type: event_type.clone(),
-                            amount: u64_to_bigdecimal(inner.amount_unlocked),
-                        },
-                        StakeEvent::WithdrawStakeEvent(inner) => DelegatedStakingActivity {
-                            transaction_version: txn_version,
-                            event_index,
-                            delegator_address: inner.delegator_address,
-                            pool_address: inner.pool_address,
-                            event_type: event_type.clone(),
-                            amount: u64_to_bigdecimal(inner.amount_withdrawn),
-                        },
-                        StakeEvent::ReactivateStakeEvent(inner) => DelegatedStakingActivity {
-                            transaction_version: txn_version,
-                            event_index,
-                            delegator_address: inner.delegator_address,
-                            pool_address: inner.pool_address,
-                            event_type: event_type.clone(),
-                            amount: u64_to_bigdecimal(inner.amount_reactivated),
-                        },
-                        StakeEvent::DistributeRewardsEvent(inner) => DelegatedStakingActivity {
-                            transaction_version: txn_version,
-                            event_index,
-                            delegator_address: "".to_string(),
-                            pool_address: inner.pool_address,
-                            event_type: event_type.clone(),
-                            amount: u64_to_bigdecimal(inner.rewards_amount),
-                        },
-                        _ => continue,
-                    };
-                    delegator_activities.push(activity);
-                }
+        let (txn_version, events) = match transaction {
+            APITransaction::UserTransaction(txn) => (txn.info.version.0 as i64, &txn.events),
+            APITransaction::BlockMetadataTransaction(txn) => {
+                (txn.info.version.0 as i64, &txn.events)
+            },
+            _ => return Ok(delegator_activities),
+        };
+        for (index, event) in events.iter().enumerate() {
+            let event_type = event.typ.to_string();
+            let event_index = index as i64;
+            if let Some(staking_event) =
+                StakeEvent::from_event(event_type.as_str(), &event.data, txn_version)?
+            {
+                let activity = match staking_event {
+                    StakeEvent::AddStakeEvent(inner) => DelegatedStakingActivity {
+                        transaction_version: txn_version,
+                        event_index,
+                        delegator_address: inner.delegator_address,
+                        pool_address: inner.pool_address,
+                        event_type: event_type.clone(),
+                        amount: u64_to_bigdecimal(inner.amount_added),
+                    },
+                    StakeEvent::UnlockStakeEvent(inner) => DelegatedStakingActivity {
+                        transaction_version: txn_version,
+                        event_index,
+                        delegator_address: inner.delegator_address,
+                        pool_address: inner.pool_address,
+                        event_type: event_type.clone(),
+                        amount: u64_to_bigdecimal(inner.amount_unlocked),
+                    },
+                    StakeEvent::WithdrawStakeEvent(inner) => DelegatedStakingActivity {
+                        transaction_version: txn_version,
+                        event_index,
+                        delegator_address: inner.delegator_address,
+                        pool_address: inner.pool_address,
+                        event_type: event_type.clone(),
+                        amount: u64_to_bigdecimal(inner.amount_withdrawn),
+                    },
+                    StakeEvent::ReactivateStakeEvent(inner) => DelegatedStakingActivity {
+                        transaction_version: txn_version,
+                        event_index,
+                        delegator_address: inner.delegator_address,
+                        pool_address: inner.pool_address,
+                        event_type: event_type.clone(),
+                        amount: u64_to_bigdecimal(inner.amount_reactivated),
+                    },
+                    StakeEvent::DistributeRewardsEvent(inner) => DelegatedStakingActivity {
+                        transaction_version: txn_version,
+                        event_index,
+                        delegator_address: "".to_string(),
+                        pool_address: inner.pool_address,
+                        event_type: event_type.clone(),
+                        amount: u64_to_bigdecimal(inner.rewards_amount),
+                    },
+                    _ => continue,
+                };
+                delegator_activities.push(activity);
             }
         }
         Ok(delegator_activities)
