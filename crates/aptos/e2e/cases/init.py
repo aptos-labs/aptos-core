a256ea2e2356f5a2ea81b2f3782d910783832663
Daniel Porteous <danielporteous1@gmail.com>
2023-02-28 13:23:51 +0000

    Add CI for running Aptos CLI test suite

--- crates/aptos/e2e/cases/init.py
+++ crates/aptos/e2e/cases/init.py
@@ -1,6 +1,10 @@
 # Copyright © Aptos Foundation
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+
+from cases.shared import TestError
+
 
 def test_init(run_helper):
     run_helper.run_command(
@@ -8,3 +12,10 @@ def test_init(run_helper):
         ["aptos", "init", "--assume-yes", "--network", "local"],
         input="\n",
     )
+    config_path = os.path.join(
+        run_helper.host_working_directory, ".aptos", "config.yaml"
+    )
+    if not os.path.exists(config_path):
+        raise TestError(
+            f"{config_path} not found (in host working dir) after running aptos init"
+        )
--- crates/aptos/e2e/main.py
+++ crates/aptos/e2e/main.py
@@ -17,6 +17,9 @@ Example (testing locally built CLI binary):
 
 This means, run the CLI test suite using a CLI built from mainnet_0431e2251d0b42920d89a52c63439f7b9eda6ac3 against a local testnet built from the testnet branch of aptos-core.
 
+Example (using a different image repo):
+  See ~/.github/workflows/cli-e2e-tests.yaml
+
 When the test suite is complete, it will tell you which tests passed and which failed. To further debug a failed test, you can check the output in --working-directory, there will be files for each test containing the command run, stdout, stderr, and any exception.
 """
 
@@ -29,8 +32,8 @@ import sys
 from cases.account import test_account_create, test_account_fund_with_faucet
 from cases.init import test_init
 from common import Network
-from test_helpers import RunHelper
 from local_testnet import run_node, stop_node, wait_for_startup
+from test_helpers import RunHelper
 
 logging.basicConfig(
     stream=sys.stderr,
@@ -50,8 +53,15 @@ def parse_args():
     )
     parser.add_argument("-d", "--debug", action="store_true")
     parser.add_argument(
-        "--image-repo",
-        help="What docker image repo to use for the local testnet. By default we use Docker Hub.",
+        "--image-repo-with-project",
+        default="aptoslabs",
+        help=(
+            "What docker image repo (+ project) to use for the local testnet. "
+            "By default we use Docker Hub: %(default)s (so, just aptoslabs for the "
+            "project since Docker Hub is the implied default repo). If you want to "
+            "specify a different repo, it might look like this: "
+            "docker.pkg.github.com/aptoslabs/aptos-core"
+        ),
     )
     parser.add_argument(
         "--base-network",
@@ -81,7 +91,6 @@ def parse_args():
         help="Where we'll run CLI commands from (in the host system). Default: %(default)s",
     )
     args = parser.parse_args()
-    args.image_repo = args.image_repo or ""
     return args
 
 
@@ -104,7 +113,7 @@ def main():
         logging.getLogger().setLevel(logging.INFO)
 
     # Run a node + faucet and wait for them to start up.
-    container_name = run_node(args.base_network, args.image_repo)
+    container_name = run_node(args.base_network, args.image_repo_with_project)
     wait_for_startup(container_name, args.base_startup_timeout)
 
     # Create the dir the test CLI will run from.
@@ -114,7 +123,7 @@ def main():
     # Build the RunHelper object.
     run_helper = RunHelper(
         host_working_directory=args.working_directory,
-        image_repo=args.image_repo,
+        image_repo_with_project=args.image_repo_with_project,
         image_tag=args.test_cli_tag,
         cli_path=args.test_cli_path,
     )
