304cad7bf5834ec1b1567c4a0317c023366b37fe
maayan <maayansavir@gmail.com>
2023-03-02 15:09:56 -0800

    add provider class tests in examples folder

--- ecosystem/typescript/sdk/examples/typescript/simple_nft.ts
+++ ecosystem/typescript/sdk/examples/typescript/simple_nft.ts
@@ -6,7 +6,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { AptosClient, AptosAccount, FaucetClient, TokenClient, CoinClient } from "aptos";
+import { AptosClient, AptosAccount, FaucetClient, TokenClient, CoinClient, Network, Provider } from "aptos";
 import { NODE_URL, FAUCET_URL } from "./common";
 
 (async () => {
@@ -160,4 +160,13 @@ import { NODE_URL, FAUCET_URL } from "./common";
   const bobBalance3 = await tokenClient.getTokenForAccount(bob.address(), tokenId);
   console.log(`Alice's token balance: ${aliceBalance3["amount"]}`);
   console.log(`Bob's token balance: ${bobBalance3["amount"]}`);
+
+  const provider = new Provider(Network.DEVNET);
+  console.log("\n=== Getting Alices's NFTs ===");
+  const aliceNfts = await provider.getAccountNFTs(alice.address().hex());
+  console.log(`Alice current token ownership: ${aliceNfts.current_token_ownerships[0].amount}. Should be 1`);
+
+  console.log("\n=== Getting Bob's NFTs ===");
+  const bobNfts = await provider.getAccountNFTs(bob.address().hex());
+  console.log(`Alice current token ownership: ${bobNfts.current_token_ownerships.length}. Should be 0`);
 })();
