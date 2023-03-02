eef180dce06aab95f84ca02ed4db9dff3a39beeb
Victor Gao <10379359+vgao1996@users.noreply.github.com>
2023-03-01 21:32:23 -0800

    [gas] fix gas formulae for exists_at & table natives (load resource) (#6856)
    Co-authored-by: David Wolinsky <isaac.wolinsky@gmail.com>

--- aptos-move/aptos-gas/src/aptos_framework.rs
+++ aptos-move/aptos-gas/src/aptos_framework.rs
@@ -116,5 +116,8 @@ crate::natives::define_gas_parameters_for_natives!(GasParameters, "aptos_framewo
     [.aggregator.destroy.base, "aggregator.destroy.base", 500 * MUL],
     [.aggregator_factory.new_aggregator.base, "aggregator_factory.new_aggregator.base", 500 * MUL],
 
-    [.object.exists_at.base_cost, { 7.. => "object.exists_at.base" }, 250 * MUL]
+    [.object.exists_at.base, { 7.. => "object.exists_at.base" }, 250 * MUL],
+    // These are dummy value, they copied from storage gas in aptos-core/aptos-vm/src/aptos_vm_impl.rs
+    [.object.exists_at.per_byte_loaded, { 7.. => "object.exists_at.per_byte_loaded" }, 1000],
+    [.object.exists_at.per_item_loaded, { 7.. => "object.exists_at.per_item_loaded" }, 8000]
 ]);
