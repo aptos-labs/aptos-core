c6593247c58ce195e4f8165ba95a79115eb83f00
Sital Kedia <skedia@aptoslabs.com>
2023-03-04 12:46:09 -0800

    [Executor-benchmark] Add support for pushing metrics to victoriametrics

--- crates/aptos-push-metrics/src/lib.rs
+++ crates/aptos-push-metrics/src/lib.rs
@@ -11,7 +11,15 @@ pub use aptos_metrics_core::{
     HistogramTimer, HistogramVec, IntCounter, IntCounterVec, IntGauge, IntGaugeVec,
 };
 use aptos_metrics_core::{Encoder, TextEncoder};
-use std::{env, sync::mpsc, thread, thread::JoinHandle, time::Duration};
+use std::{
+    env,
+    ops::Sub,
+    sync::mpsc,
+    thread,
+    thread::JoinHandle,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+use url::Url;
 
 const DEFAULT_PUSH_FREQUENCY_SECS: u64 = 15;
 
@@ -25,7 +33,11 @@ pub struct MetricsPusher {
 }
 
 impl MetricsPusher {
-    fn push(push_metrics_endpoint: &str, api_token: Option<&str>) {
+    fn push(
+        push_metrics_endpoint: &str,
+        api_token: Option<&str>,
+        push_metrics_extra_labels: &[String],
+    ) {
         let mut buffer = Vec::new();
 
         if let Err(e) = TextEncoder::new().encode(&aptos_metrics_core::gather(), &mut buffer) {
@@ -35,12 +47,16 @@ impl MetricsPusher {
             if let Some(token) = api_token {
                 request.set("Authorization", format!("Bearer {}", token).as_str());
             }
+            push_metrics_extra_labels.iter().for_each(|label| {
+                request.query("extra_label", label);
+            });
             let response = request.timeout_connect(10_000).send_bytes(&buffer);
-            if let Some(error) = response.synthetic_error() {
+            if !response.ok() {
                 warn!(
-                    "Failed to push metrics to {}. Error: {}",
-                    push_metrics_endpoint, error
-                );
+                    "Failed to push metrics to {},  resp: {}",
+                    push_metrics_endpoint,
+                    response.status_text()
+                )
             }
         }
     }
@@ -50,19 +66,33 @@ impl MetricsPusher {
         push_metrics_endpoint: String,
         push_metrics_frequency_secs: u64,
         push_metrics_api_token: Option<String>,
+        push_metrics_extra_labels: Vec<String>,
     ) {
         while quit_receiver
             .recv_timeout(Duration::from_secs(push_metrics_frequency_secs))
             .is_err()
         {
             // Timeout, no quit signal received.
-            Self::push(&push_metrics_endpoint, push_metrics_api_token.as_deref());
+            Self::push(
+                &push_metrics_endpoint,
+                push_metrics_api_token.as_deref(),
+                &push_metrics_extra_labels,
+            );
         }
         // final push
-        Self::push(&push_metrics_endpoint, push_metrics_api_token.as_deref());
+        Self::push(
+            &push_metrics_endpoint,
+            push_metrics_api_token.as_deref(),
+            &push_metrics_extra_labels,
+        );
     }
 
-    fn start_worker_thread(quit_receiver: mpsc::Receiver<()>) -> Option<JoinHandle<()>> {
+    fn start_worker_thread(
+        quit_receiver: mpsc::Receiver<()>,
+        push_metrics_extra_labels: Vec<String>,
+        chain_name: &str,
+        namespace: &str,
+    ) -> Option<JoinHandle<()>> {
         // eg value for PUSH_METRICS_ENDPOINT: "http://pushgateway.server.com:9091/metrics/job/safety_rules"
         let push_metrics_endpoint = match env::var("PUSH_METRICS_ENDPOINT") {
             Ok(s) => s,
@@ -86,20 +116,71 @@ impl MetricsPusher {
             "Starting push metrics loop. Sending metrics to {} with a frequency of {} seconds",
             push_metrics_endpoint, push_metrics_frequency_secs
         );
+
+        info!(
+            "Execution dashboard link: {} ",
+            Self::get_dashboard_link(chain_name, namespace)
+        );
         Some(thread::spawn(move || {
             Self::worker(
                 quit_receiver,
                 push_metrics_endpoint,
                 push_metrics_frequency_secs,
                 push_metrics_api_token,
+                push_metrics_extra_labels,
             )
         }))
     }
 
+    fn get_dashboard_link(chain_name: &str, namespace: &str) -> Url {
+        let end_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            .to_string();
+        let start_time = SystemTime::now()
+            .sub(Duration::from_secs(600))
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            .to_string();
+        let mut url =
+            Url::parse("https://o11y.aptosdev.com/grafana/d/execution/execution").unwrap();
+        url.query_pairs_mut()
+            .append_pair("from", &start_time)
+            .append_pair("to", &end_time)
+            .append_pair("var-Datasource", "VictoriaMetrics Global")
+            .append_pair("var-metrics_source", "All")
+            .append_pair("var-chain_name", chain_name)
+            .append_pair("var-cluster", "All")
+            .append_pair("var-namespace", namespace)
+            .append_pair("var-role", "All");
+
+        url
+    }
+
+    fn push_metrics_extra_labels(chain_name: &str, namespace: &str) -> Vec<String> {
+        vec![
+            format!("chain_name={}", chain_name),
+            "cluster=unknown".into(),
+            "metrics_source=unknown".into(),
+            "kubernetes_pod_name=unknown".into(),
+            "role=unknown".into(),
+            format!("namespace={}", namespace),
+        ]
+    }
+
     /// start starts a new thread and periodically pushes the metrics to a pushgateway endpoint
-    pub fn start() -> Self {
+    pub fn start(chain_name: &str) -> Self {
         let (tx, rx) = mpsc::channel();
-        let worker_thread = Self::start_worker_thread(rx);
+        let namespace = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_nanos()
+            .to_string();
+        let push_metrics_labels = Self::push_metrics_extra_labels(chain_name, &namespace);
+        let worker_thread =
+            Self::start_worker_thread(rx, push_metrics_labels, chain_name, &namespace);
 
         Self {
             worker_thread,
