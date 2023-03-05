c800eadc8d79bc59c68fd094ceb56be73fe2e6d8
Sital Kedia <sitalkedia@users.noreply.github.com>
2023-03-04 21:25:12 -0800

    [Executor-benchmark] Add support for pushing metrics to victoriametrics (#6927)

--- storage/db-tool/src/restore.rs
+++ storage/db-tool/src/restore.rs
@@ -67,7 +67,7 @@ pub enum Oneoff {
 impl Command {
     pub async fn run(self) -> Result<()> {
         Logger::new().level(Level::Info).init();
-        let _mp = MetricsPusher::start();
+        let _mp = MetricsPusher::start("db-restore");
 
         match self {
             Command::Oneoff(oneoff) => {
