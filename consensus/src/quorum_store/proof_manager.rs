9546ffd34879764f926fc928f6290a1ed6f0033e
Brian (Sunghoon) Cho <brian@aptoslabs.com>
2023-03-08 13:03:26 -0800

    [Quorum Store] poll mempool at a faster pace to reduce mempool queueing time, except when backpressure (#6969)
    We poll mempool for transactions at a fast rate (default 25 ms), unless there is backpressure. If there is backpressure then the next poll will be tried in + 25ms, until a max of 250 ms is reached. Since this makes the poll duration variable, we compute the max txns to pull based on the time passed since the last poll.
    
    The backpressure is based on the number of proofs left in proof manager. So proofs are used for the rate of pulling, and txns are used for the max number of txns on the pull.
    
    Running tests with low TPS (2 TPS per validator), confirmed that the time spent in mempool reduced: roughly from an average of 125ms (from the previous 250ms poll time) to an average of 12.5ms (fro the 25ms faster poll time).

--- consensus/src/quorum_store/proof_manager.rs
+++ consensus/src/quorum_store/proof_manager.rs
@@ -3,7 +3,7 @@
 
 use crate::{
     network::{NetworkSender, QuorumStoreSender},
-    quorum_store::{counters, utils::ProofQueue},
+    quorum_store::{batch_generator::BackPressure, counters, utils::ProofQueue},
 };
 use aptos_consensus_types::{
     common::{Payload, PayloadFilter, ProofWithData},
@@ -29,15 +29,23 @@ pub struct ProofManager {
     latest_logical_time: LogicalTime,
     back_pressure_total_txn_limit: u64,
     remaining_total_txn_num: u64,
+    back_pressure_total_proof_limit: u64,
+    remaining_total_proof_num: u64,
 }
 
 impl ProofManager {
-    pub fn new(epoch: u64, back_pressure_total_txn_limit: u64) -> Self {
+    pub fn new(
+        epoch: u64,
+        back_pressure_total_txn_limit: u64,
+        back_pressure_total_proof_limit: u64,
+    ) -> Self {
         Self {
             proofs_for_consensus: ProofQueue::new(),
             latest_logical_time: LogicalTime::new(epoch, 0),
             back_pressure_total_txn_limit,
             remaining_total_txn_num: 0,
+            back_pressure_total_proof_limit,
+            remaining_total_proof_num: 0,
         }
     }
 
@@ -100,9 +108,12 @@ impl ProofManager {
                     max_bytes,
                     return_non_full,
                 );
-                self.remaining_total_txn_num = self
+                (self.remaining_total_txn_num, self.remaining_total_proof_num) = self
                     .proofs_for_consensus
-                    .num_total_txns(LogicalTime::new(self.latest_logical_time.epoch(), round));
+                    .num_total_txns_and_proofs(LogicalTime::new(
+                        self.latest_logical_time.epoch(),
+                        round,
+                    ));
 
                 let res = GetPayloadResponse::GetPayloadResponse(
                     if proof_block.is_empty() {
@@ -125,18 +136,24 @@ impl ProofManager {
     }
 
     /// return true when quorum store is back pressured
-    pub(crate) fn qs_back_pressure(&self) -> bool {
-        self.remaining_total_txn_num > self.back_pressure_total_txn_limit
+    pub(crate) fn qs_back_pressure(&self) -> BackPressure {
+        BackPressure {
+            txn_count: self.remaining_total_txn_num > self.back_pressure_total_txn_limit,
+            proof_count: self.remaining_total_proof_num > self.back_pressure_total_proof_limit,
+        }
     }
 
     pub async fn start(
         mut self,
         mut network_sender: NetworkSender,
-        back_pressure_tx: tokio::sync::mpsc::Sender<bool>,
+        back_pressure_tx: tokio::sync::mpsc::Sender<BackPressure>,
         mut proposal_rx: Receiver<GetPayloadCommand>,
         mut proof_rx: tokio::sync::mpsc::Receiver<ProofManagerCommand>,
     ) {
-        let mut back_pressure = false;
+        let mut back_pressure = BackPressure {
+            txn_count: false,
+            proof_count: false,
+        };
 
         loop {
             // TODO: additional main loop counter
@@ -172,7 +189,8 @@ impl ProofManager {
                             self.handle_commit_notification(logical_time, digests);
 
                             // update the backpressure upon new commit round
-                            self.remaining_total_txn_num = self.proofs_for_consensus.num_total_txns(logical_time);
+                            (self.remaining_total_txn_num, self.remaining_total_proof_num) =
+                                self.proofs_for_consensus.num_total_txns_and_proofs(logical_time);
                             // TODO: keeping here for metrics, might be part of the backpressure in the future?
                             self.proofs_for_consensus.clean_local_proofs(logical_time);
                             let updated_back_pressure = self.qs_back_pressure();
