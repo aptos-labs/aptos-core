0d60b2888c718779c20de76d6a1d2f204f2d2dc0
bowenyang007 <carlyang007@gmail.com>
2023-03-08 17:04:26 -0800

    added nft_points custom contract parsing

---
+++ crates/indexer/migrations/2023-03-08-205402_nft_points/up.sql
@@ -0,0 +1,11 @@
+-- Your SQL goes here
+CREATE TABLE nft_points (
+  transaction_version BIGINT UNIQUE PRIMARY KEY NOT NULL,
+  owner_address VARCHAR(66) NOT NULL,
+  amount NUMERIC NOT NULL,
+  transaction_timestamp TIMESTAMP NOT NULL,
+  inserted_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+CREATE INDEX np_oa_idx ON nft_points (owner_address);
+CREATE INDEX np_tt_oa_idx ON nft_points (transaction_timestamp, owner_address);
+CREATE INDEX np_insat_idx ON nft_points (inserted_at);
