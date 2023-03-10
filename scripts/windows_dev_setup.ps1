dd629a247e3738fa47f394d4b63751f853c2c4cb
Christian Sahar <125399153+saharct@users.noreply.github.com>
2023-03-09 18:46:29 -0800

    Improved method of finding MSVC install path and modified the corresponding functions for cleaner execution

--- scripts/windows_dev_setup.ps1
+++ scripts/windows_dev_setup.ps1
@@ -1,4 +1,4 @@
-﻿# This script installs the necessary dependencies to build in Aptos.
+# This script installs the necessary dependencies to build in Aptos.
 
 $ErrorActionPreference = 'Stop'
 
@@ -6,23 +6,36 @@ Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path) | Out-Null; Set-L
 
 $global:os = $null
 $global:architecture = $null
+$global:msvcpath = $null
+
+function verify_architecture {  # Checks whether the Windows machine is 32-bit or 64-bit
+	$result = Get-WmiObject -Class Win32_Processor | Select-Object AddressWidth |ConvertTo-Json -Compress
+	if ($result.Contains("64")) {
+		$global:architecture = "64"
+		Write-Host "64-bit system detected"
+		}
+	else {
+		$global:architecture = "86"
+		Write-Host "32-bit system detected"
+		}
+}
 
 function check_os {
 	$osName = (Get-WMIObject win32_operatingsystem).name
 	if ($osName.Contains("Windows 10")) {
-		Write-Host "Supported Windows OS"
+		Write-Host "Supported Windows OS detected"
 		$global:os = "Windows 10"
 	}
 	elseif ($osName.Contains("Windows 11")) {
-		Write-Host "Supported Windows OS"
+		Write-Host "Supported Windows OS detected"
 		$global:os = "Windows 11"
 	}
 	elseif ($osName.Contains("Windows Server 2022")) {
 		Write-Host "Supported Windows OS"
-		$global:os = "Windows Server 2022"
+		$global:os = "Windows Server 2022 detected"
 	}
 	else {
-		Write-Host "Unsupported Windows OS"
+		Write-Host "Unsupported Windows OS detected. Stopping script..."
 		Exit
 	}
 }
@@ -31,23 +44,24 @@ function check_package {
   param(
     [string]$package
   )
-  if ($(winget list --name $package) -match 'No installed package found matching input criteria.') {
+  if ($(winget list --name $package --accept-source-agreements) -match 'No installed package found matching input criteria.') {
     Write-Host "Installing $package..."
     return $true
   }   
   else {
-    Write-Host "$package is already installed..."
+    Write-Host "$package is already installed."
   }
 }
 
 function install_msvc_build_tools {  # Installs C++ build tools, CMake, and Windows 10/11 SDK
-  $result = check_package "buildtools"
+  $result = check_package "Visual Studio Build Tools"
   if ($result) {
-	install_variant
+	select_msvc_variant
+    set_msvc_env_variables
 	}
 }
 
-function install_variant {  # Decides between the Windows 10 SDK and Windows 11 SDK based on your OS
+function select_msvc_variant {  # Decides between the Windows 10 SDK and Windows 11 SDK based on your OS
 	if ($global:os -eq "Windows 11") {
 		winget install Microsoft.VisualStudio.2022.BuildTools --accept-source-agreements --silent --override "--wait --quiet --add ProductLang En-us --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Component.VC.CMake.Project --includeRecommended"
 		}
@@ -56,44 +70,42 @@ function install_variant {  # Decides between the Windows 10 SDK and Windows 11 
 		}
 }
 
+function get_msvc_install_path {
+	$msvcpath = Get-CimInstance -ClassName "MSFT_VSInstance" | Select-Object -ExpandProperty InstallLocation
+	return $msvcpath
+}
+
 function get_msvc_version {  # Finds the MSVC version number and creates a valid filepath to add as an environment variable
-    $pathpattern = "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\*\bin\Hostx64\x64\link.exe"
+    $global:msvcpath = get_msvc_install_path
+	$pathpattern = "$msvcpath\VC\Tools\MSVC\*\bin\Hostx64\x64\link.exe"
 
     # Get the file path that matches the pattern
     $filepath = Get-ChildItem -Path $pathpattern -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1
 
     if ($filepath) {
         # Extract the version number from the file path using regex
-        $global:msvcversion = $filepath.FullName -replace ".*MSVC\\(\d+\.\d+\.\d+)\\.*", '$1'
+        $msvcversion = $filepath.FullName -replace ".*MSVC\\(\d+\.\d+\.\d+)\\.*", '$1'
+		return $msvcversion
     } else {
         Write-Warning "MSVC not found: $pathpattern"
         return $null
     }
 }
 
-function verify_architecture {  # Checks whether the Windows machine is 32-bit or 64-bit
-	$result = Get-WmiObject -Class Win32_Processor | Select-Object AddressWidth
-	if ($result.Contains("64")) {
-		$global:architecture = "64"
-		}
-	else {
-		$global:architecture = "86"
-		}
-}
-
 function set_msvc_env_variables {  # Sets the environment variables based on the architecture and MSVC version
-	[Environment]::SetEnvironmentVariable("PATH", "$env:PATH;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\$global:msvcversion\bin\Hostx$global:architecture\x$global:architecture\link.exe", "User")
-	[Environment]::SetEnvironmentVariable("PATH", "$env:PATH;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\$global:msvcversion\bin\Hostx$global:architecture\x$global:architecture\cl.exe", "User")
+    $msvcversion = get_msvc_version
+	[Environment]::SetEnvironmentVariable("PATH", "$env:PATH;$global:msvcpath\VC\Tools\MSVC\$msvcversion\bin\Hostx$global:architecture\x$global:architecture\link.exe", "User")
+	[Environment]::SetEnvironmentVariable("PATH", "$env:PATH;$global:msvcpath\VC\Tools\MSVC\$msvcversion\bin\Hostx$global:architecture\x$global:architecture\cl.exe", "User")
 	Write-Host "Environment variables set"
 }
 
 function install_rustup {
-  $result = check_package "rustup"
+  $result = check_package "Rustup"
   if ($result) {
 	winget install Rustlang.Rustup --silent
 	Exit
-	Powershell
 	}
+  Write-Host "Configuring Rustup..."
   rustup update
   rustup component add rustfmt
   rustup component add clippy
@@ -102,21 +114,21 @@ function install_rustup {
 }
 
 function install_llvm {
-  $result = check_package "llvm"
+  $result = check_package "LLVM"
   if ($result) {
 	winget install LLVM.LLVM --silent
 	}
 }
 
 function install_openssl {
-  $result = check_package "openssl"
+  $result = check_package "OpenSSL"
   if ($result) {
 	winget install ShiningLight.OpenSSL --silent
 	}
 }
 
 function install_nodejs {
-  $result = check_package "nodejs"
+  $result = check_package "Node.js"
   if ($result) {
 	winget install OpenJS.NodeJS --silent
 	}
@@ -130,21 +142,19 @@ function install_pnpm {
 }
 
 function install_postgresql {
-  $result = check_package "postgresql"
+  $result = check_package "PostgreSQL"
   if ($result) {
 	winget install PostgreSQL.PostgreSQL --silent
 	}
 }
 
 function existing_package {
-	Write-Host "This package is already installed..."
+	Write-Host "This package is already installed."
 }
 
-
 check_os
-install_msvc_build_tools
 verify_architecture
-set_msvc_env_variables
+install_msvc_build_tools
 install_llvm
 install_openssl
 install_nodejs
