33d7b9f49563e6140543f538d70130b042540320
zhoujunma <zjma@users.noreply.github.com>
2023-03-11 09:51:02 +0800

    Zjma/generic algebra feature flags (#7015)
    * add feature flags for the algebra module
    
    * update
    
    * also enums in rust
    
    * update

--- aptos-move/aptos-release-builder/src/components/feature_flags.rs
+++ aptos-move/aptos-release-builder/src/components/feature_flags.rs
@@ -28,6 +28,7 @@ pub enum FeatureFlag {
     ResourceGroups,
     MultisigAccounts,
     DelegationPools,
+    Bls12381BasicOperations,
 }
 
 fn generate_features_blob(writer: &CodeWriter, data: &[u64]) {
@@ -128,6 +129,7 @@ impl From<FeatureFlag> for AptosFeatureFlag {
             FeatureFlag::ResourceGroups => AptosFeatureFlag::RESOURCE_GROUPS,
             FeatureFlag::MultisigAccounts => AptosFeatureFlag::MULTISIG_ACCOUNTS,
             FeatureFlag::DelegationPools => AptosFeatureFlag::DELEGATION_POOLS,
+            FeatureFlag::Bls12381BasicOperations => AptosFeatureFlag::BLS12381_BASIC_OPERATIONS,
         }
     }
 }
@@ -153,6 +155,7 @@ impl From<AptosFeatureFlag> for FeatureFlag {
             AptosFeatureFlag::RESOURCE_GROUPS => FeatureFlag::ResourceGroups,
             AptosFeatureFlag::MULTISIG_ACCOUNTS => FeatureFlag::MultisigAccounts,
             AptosFeatureFlag::DELEGATION_POOLS => FeatureFlag::DelegationPools,
+            AptosFeatureFlag::BLS12381_BASIC_OPERATIONS => FeatureFlag::Bls12381BasicOperations,
         }
     }
 }
--- aptos-move/framework/move-stdlib/sources/configs/features.move
+++ aptos-move/framework/move-stdlib/sources/configs/features.move
@@ -140,6 +140,18 @@ module std::features {
         is_enabled(DELEGATION_POOLS)
     }
 
+    /// Whether the basic operations over some BLS12381 structures are enabled in the algebra module.
+    ///
+    /// Basic operations include element (de)serialization, field/group arithmetic, hash-to-structure, casting, etc.
+    /// BLS12381 structures controlled by this flag includes `Fq12`, `G1`, `G2`, `Gt`, `Fr`.
+    ///
+    /// Lifetime: transient
+    const BLS12381_BASIC_OPERATIONS: u64 = 12;
+    public fun get_bls12381_basic_operations_feature(): u64 { BLS12381_BASIC_OPERATIONS }
+    public fun bls12381_basic_operations_enabled(): bool acquires Features {
+        is_enabled(BLS12381_BASIC_OPERATIONS)
+    }
+
     // ============================================================================================
     // Feature Flag Implementation
 
