4619d1fc8ca86c64940ff271929c1db221aa2bdb
Daniel Porteous <danielporteous1@gmail.com>
2023-03-14 16:08:38 +0000

    Revert "Replace legacy faucet with code from aptos-tap (#6748)"
    This reverts commit 21fff94546c9109d60923c70fdfe9a4b2cb2c941.

--- docker/build-rust-all.sh
+++ docker/build-rust-all.sh
@@ -14,7 +14,7 @@ echo "FEATURES: $FEATURES"
 cargo build --locked --profile=$PROFILE \
     -p aptos \
     -p aptos-backup-cli \
-    -p aptos-faucet-service \
+    -p aptos-faucet \
     -p aptos-forge-cli \
     -p aptos-fn-check-client \
     -p aptos-node-checker \
@@ -42,7 +42,7 @@ fi
 # After building, copy the binaries we need to `dist` since the `target` directory is used as docker cache mount and only available during the RUN step
 BINS=(
     aptos
-    aptos-faucet-service
+    aptos-faucet
     aptos-node
     aptos-node-checker
     aptos-openapi-spec-generator
