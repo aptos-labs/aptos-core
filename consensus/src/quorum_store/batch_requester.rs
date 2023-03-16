55a7c711a83adbfb30f909d59dd6edb477ead4a2
danielxiangzl <xiangzhuolun@gmail.com>
2023-03-16 11:02:47 -0700

    resolve comment

--- consensus/src/quorum_store/batch_requester.rs
+++ consensus/src/quorum_store/batch_requester.rs
@@ -152,10 +152,10 @@ impl<T: QuorumStoreSender + Sync + 'static> BatchRequester<T> {
                         }
                         Some(response) = futures.next() => {
                             if let Ok(batch) = response {
-                                counters::RECEIVED_BATCH_COUNT.inc();
+                                counters::RECEIVED_BATCH_RESPONSE_COUNT.inc();
                                 if batch.verify().is_ok() {
-                                    let digest = batch.digest();
-                                    let payload = batch.into_payload();
+                                    let digest = *batch.digest();
+                                    let payload = batch.into_transactions();
                                     request_state.serve_request(digest, Some(payload));
                                     return;
                                 }
