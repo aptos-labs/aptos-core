a4d4e12a60ac2e65b20a697f29c2cc140e6ee87f
qdrs <qdrs@proton.me>
2023-03-24 20:33:49 +0800

    cache markets

--- crates/indexer/src/processors/econia_processor.rs
+++ crates/indexer/src/processors/econia_processor.rs
@@ -1,9 +1,7 @@
-use std::sync::RwLock;
+use std::{collections::HashMap, sync::RwLock};
 
 use crate::{
-    database::{
-        clean_data_for_db, execute_with_better_error, get_chunks, PgDbPool, PgPoolConnection,
-    },
+    database::{clean_data_for_db, execute_with_better_error, get_chunks, PgDbPool},
     indexer::{
         errors::TransactionProcessingError, processing_result::ProcessingResult,
         transaction_processor::TransactionProcessor,
@@ -130,6 +128,26 @@ impl From<MarketRegistrationEvent> for NewMarketRegistrationEvent {
     }
 }
 
+impl From<MarketRegistrationEvent> for models::market::MarketRegistrationEvent {
+    fn from(value: MarketRegistrationEvent) -> Self {
+        Self {
+            market_id: value.market_id.into(),
+            time: *CURRENT_BLOCK_TIME.read().unwrap(),
+            base_account_address: Some(value.base_type.account_address),
+            base_module_name: Some(value.base_type.module_name),
+            base_struct_name: Some(value.base_type.struct_name),
+            base_name_generic: Some(value.base_name_generic),
+            quote_account_address: value.quote_type.account_address,
+            quote_module_name: value.quote_type.module_name,
+            quote_struct_name: value.quote_type.struct_name,
+            lot_size: value.lot_size.into(),
+            tick_size: value.tick_size.into(),
+            min_size: value.min_size.into(),
+            underwriter_id: value.underwriter_id.into(),
+        }
+    }
+}
+
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct TradingPair {
@@ -163,18 +181,255 @@ enum EventWrapper {
     RecognizedMarket(RecognizedMarketEvent),
 }
 
+fn is_event_type_valid(e: &EventModel) -> bool {
+    EVENT_TYPES.iter().any(|t| t == &e.type_)
+}
+
+type BaseQuoteKey = (String, String, String, String, String, String);
+
 pub struct EconiaTransactionProcessor {
     connection_pool: PgDbPool,
+    markets: HashMap<BigDecimal, models::market::MarketRegistrationEvent>,
+    base_quote_to_market_id: HashMap<BaseQuoteKey, BigDecimal>,
 }
 
 impl EconiaTransactionProcessor {
     pub fn new(connection_pool: PgDbPool) -> Self {
-        Self { connection_pool }
-    }
-}
+        let mut conn = connection_pool
+            .get()
+            .expect("failed connecting to db on startup");
+        let mkts = Self::fetch_all_markets(&mut conn).expect("failed loading markets on startup");
+        let mut markets = HashMap::new();
+        let mut base_quote_to_market_id = HashMap::new();
+        for m in mkts.into_iter() {
+            let key = (
+                m.base_account_address.clone().unwrap_or_default(),
+                m.base_module_name.clone().unwrap_or_default(),
+                m.base_struct_name.clone().unwrap_or_default(),
+                m.quote_account_address.clone(),
+                m.quote_module_name.clone(),
+                m.quote_struct_name.clone(),
+            );
+            base_quote_to_market_id.insert(key, m.market_id.clone());
+            markets.insert(m.market_id.clone(), m);
+        }
 
-fn is_event_type_valid(e: &EventModel) -> bool {
-    EVENT_TYPES.iter().find(|t| *t == &e.type_).is_some()
+        Self {
+            connection_pool,
+            markets,
+            base_quote_to_market_id,
+        }
+    }
+
+    fn fetch_all_markets(
+        conn: &mut PgConnection,
+    ) -> Result<Vec<models::market::MarketRegistrationEvent>, Error> {
+        use diesel::prelude::*;
+        use econia_db::schema::market_registration_events::dsl::*;
+        market_registration_events.load::<models::market::MarketRegistrationEvent>(conn)
+    }
+
+    fn insert_to_db(
+        &self,
+        start_version: u64,
+        end_version: u64,
+        events: Vec<EventModel>,
+    ) -> Result<ProcessingResult, Error> {
+        aptos_logger::trace!(
+            name = NAME,
+            start_version = start_version,
+            end_version = end_version,
+            "Inserting to db",
+        );
+        if self.insert_events_transaction(&events).is_err() {
+            let events = clean_data_for_db(events, true);
+            self.insert_events_transaction(&events)?;
+        }
+        Ok(ProcessingResult::new(NAME, start_version, end_version))
+    }
+
+    fn insert_events_transaction(&self, events: &[EventModel]) -> Result<(), Error> {
+        let mut conn = self.get_conn();
+        conn.build_transaction()
+            .read_write()
+            .run::<_, Error, _>(|pg_conn| {
+                self.insert_events(pg_conn, events)?;
+                Ok(())
+            })?;
+        Ok(())
+    }
+
+    fn insert_events(&self, conn: &mut PgConnection, ev: &[EventModel]) -> Result<(), Error> {
+        let mut maker = vec![];
+        let mut taker = vec![];
+        let mut market_registration = vec![];
+        let mut recognized_market = vec![];
+
+        for e in ev.iter() {
+            let event_wrapper: EventWrapper = serde_json::from_value(e.data.clone())
+                .map_err(|e| Error::DeserializationError(Box::new(e)))?;
+            match event_wrapper {
+                EventWrapper::Maker(e) => maker.push(e),
+                EventWrapper::Taker(e) => taker.push(e),
+                EventWrapper::MarketRegistration(e) => market_registration.push(e),
+                EventWrapper::RecognizedMarket(e) => recognized_market.push(e),
+            }
+        }
+
+        self.insert_maker_events(conn, maker)?;
+        self.insert_taker_events(conn, taker)?;
+        self.insert_market_registration_events(conn, market_registration)?;
+        self.insert_recognized_market_events(conn, recognized_market)?;
+        Ok(())
+    }
+
+    fn insert_maker_events(
+        &self,
+        conn: &mut PgConnection,
+        ev: Vec<MakerEvent>,
+    ) -> Result<(), Error> {
+        let ev = ev
+            .into_iter()
+            .map(models::events::NewMakerEvent::from)
+            .collect::<Vec<_>>();
+        let chunks = get_chunks(ev.len(), models::events::NewMakerEvent::field_count());
+        let table = econia_db::schema::maker_events::table;
+        for (start_ind, end_ind) in chunks {
+            execute_with_better_error(
+                conn,
+                diesel::insert_into(table)
+                    .values(&ev[start_ind..end_ind])
+                    .on_conflict_do_nothing(),
+                None,
+            )?;
+        }
+        Ok(())
+    }
+
+    fn insert_taker_events(
+        &self,
+        conn: &mut PgConnection,
+        ev: Vec<TakerEvent>,
+    ) -> Result<(), Error> {
+        let ev = ev
+            .into_iter()
+            .map(models::events::NewTakerEvent::from)
+            .collect::<Vec<_>>();
+        let chunks = get_chunks(ev.len(), models::events::NewTakerEvent::field_count());
+        let table = econia_db::schema::taker_events::table;
+        for (start_ind, end_ind) in chunks {
+            execute_with_better_error(
+                conn,
+                diesel::insert_into(table)
+                    .values(&ev[start_ind..end_ind])
+                    .on_conflict_do_nothing(),
+                None,
+            )?;
+        }
+        Ok(())
+    }
+
+    // TODO update cache as these come in
+    fn insert_market_registration_events(
+        &self,
+        conn: &mut PgConnection,
+        ev: Vec<MarketRegistrationEvent>,
+    ) -> Result<(), Error> {
+        let ev = ev
+            .into_iter()
+            .map(models::market::NewMarketRegistrationEvent::from)
+            .collect::<Vec<_>>();
+        let chunks = get_chunks(
+            ev.len(),
+            models::market::NewMarketRegistrationEvent::field_count(),
+        );
+        let table = econia_db::schema::market_registration_events::table;
+        for (start_ind, end_ind) in chunks {
+            execute_with_better_error(
+                conn,
+                diesel::insert_into(table)
+                    .values(&ev[start_ind..end_ind])
+                    .on_conflict_do_nothing(),
+                None,
+            )?;
+        }
+        Ok(())
+    }
+
+    fn convert_recognized_market_events_to_db(
+        &self,
+        ev: Vec<RecognizedMarketEvent>,
+    ) -> Result<Vec<NewRecognizedMarketEvent>, Error> {
+        let mut events = vec![];
+        for e in ev.into_iter() {
+            if let Some(r) = e.recognized_market_info {
+                let mkt = self
+                    .markets
+                    .get(&r.market_id.into())
+                    .ok_or(Error::NotFound)?;
+                let new_lot_size = BigDecimal::from(r.lot_size);
+                let new_tick_size = BigDecimal::from(r.tick_size);
+                let new_min_size = BigDecimal::from(r.min_size);
+                events.push(NewRecognizedMarketEvent {
+                    market_id: mkt.market_id.clone(),
+                    time: *CURRENT_BLOCK_TIME.read().unwrap(),
+                    event_type: if mkt.lot_size == new_lot_size
+                        && mkt.tick_size == new_tick_size
+                        && mkt.min_size == new_min_size
+                    {
+                        MarketEventType::Add
+                    } else {
+                        MarketEventType::Update
+                    },
+                    lot_size: Some(new_lot_size),
+                    tick_size: Some(new_tick_size),
+                    min_size: Some(new_min_size),
+                })
+            } else {
+                let key: BaseQuoteKey = (
+                    e.trading_pair.base_type.account_address,
+                    e.trading_pair.base_type.module_name,
+                    e.trading_pair.base_type.struct_name,
+                    e.trading_pair.quote_type.account_address,
+                    e.trading_pair.quote_type.module_name,
+                    e.trading_pair.quote_type.struct_name,
+                );
+                let mkt_id = self.base_quote_to_market_id.get(&key).unwrap();
+                events.push(NewRecognizedMarketEvent {
+                    market_id: mkt_id.clone(),
+                    time: *CURRENT_BLOCK_TIME.read().unwrap(),
+                    event_type: MarketEventType::Remove,
+                    lot_size: None,
+                    tick_size: None,
+                    min_size: None,
+                })
+            }
+        }
+        Ok(events)
+    }
+
+    fn insert_recognized_market_events(
+        &self,
+        conn: &mut PgConnection,
+        ev: Vec<RecognizedMarketEvent>,
+    ) -> Result<(), Error> {
+        let events = self.convert_recognized_market_events_to_db(ev)?;
+        let chunks = get_chunks(
+            events.len(),
+            models::market::NewRecognizedMarketEvent::field_count(),
+        );
+        let table = econia_db::schema::recognized_market_events::table;
+        for (start_ind, end_ind) in chunks {
+            execute_with_better_error(
+                conn,
+                diesel::insert_into(table)
+                    .values(&events[start_ind..end_ind])
+                    .on_conflict_do_nothing(),
+                None,
+            )?;
+        }
+        Ok(())
+    }
 }
 
 impl std::fmt::Debug for EconiaTransactionProcessor {
@@ -186,195 +441,6 @@ impl std::fmt::Debug for EconiaTransactionProcessor {
             state.connections, state.idle_connections
         )
     }
-}
-
-fn insert_events_transaction(
-    conn: &mut PgPoolConnection,
-    events: &[EventModel],
-) -> Result<(), Error> {
-    conn.build_transaction()
-        .read_write()
-        .run::<_, Error, _>(|pg_conn| {
-            insert_events(pg_conn, events)?;
-            Ok(())
-        })?;
-    Ok(())
-}
-
-fn insert_to_db(
-    conn: &mut PgPoolConnection,
-    start_version: u64,
-    end_version: u64,
-    events: Vec<EventModel>,
-) -> Result<ProcessingResult, Error> {
-    aptos_logger::trace!(
-        name = NAME,
-        start_version = start_version,
-        end_version = end_version,
-        "Inserting to db",
-    );
-    if insert_events_transaction(conn, &events).is_err() {
-        let events = clean_data_for_db(events, true);
-        insert_events_transaction(conn, &events)?;
-    }
-    Ok(ProcessingResult::new(NAME, start_version, end_version))
-}
-
-fn insert_events(conn: &mut PgConnection, ev: &[EventModel]) -> Result<(), Error> {
-    let mut maker = vec![];
-    let mut taker = vec![];
-    let mut market_registration = vec![];
-    let mut recognized_market = vec![];
-
-    for e in ev.iter() {
-        let event_wrapper: EventWrapper = serde_json::from_value(e.data.clone())
-            .map_err(|e| Error::DeserializationError(Box::new(e)))?;
-        match event_wrapper {
-            EventWrapper::Maker(e) => maker.push(e),
-            EventWrapper::Taker(e) => taker.push(e),
-            EventWrapper::MarketRegistration(e) => market_registration.push(e),
-            EventWrapper::RecognizedMarket(e) => recognized_market.push(e),
-        }
-    }
-
-    insert_maker_events(conn, maker)?;
-    insert_taker_events(conn, taker)?;
-    insert_market_registration_events(conn, market_registration)?;
-    insert_recognized_market_events(conn, recognized_market)?;
-
-    Ok(())
-}
-
-fn insert_maker_events(conn: &mut PgConnection, ev: Vec<MakerEvent>) -> Result<(), Error> {
-    let ev = ev
-        .into_iter()
-        .map(|e| models::events::NewMakerEvent::from(e))
-        .collect::<Vec<_>>();
-    let chunks = get_chunks(ev.len(), models::events::NewMakerEvent::field_count());
-    let table = econia_db::schema::maker_events::table;
-    for (start_ind, end_ind) in chunks {
-        execute_with_better_error(
-            conn,
-            diesel::insert_into(table)
-                .values(&ev[start_ind..end_ind])
-                .on_conflict_do_nothing(),
-            None,
-        )?;
-    }
-    Ok(())
-}
-
-fn insert_taker_events(conn: &mut PgConnection, ev: Vec<TakerEvent>) -> Result<(), Error> {
-    let ev = ev
-        .into_iter()
-        .map(|e| models::events::NewTakerEvent::from(e))
-        .collect::<Vec<_>>();
-    let chunks = get_chunks(ev.len(), models::events::NewTakerEvent::field_count());
-    let table = econia_db::schema::taker_events::table;
-    for (start_ind, end_ind) in chunks {
-        execute_with_better_error(
-            conn,
-            diesel::insert_into(table)
-                .values(&ev[start_ind..end_ind])
-                .on_conflict_do_nothing(),
-            None,
-        )?;
-    }
-    Ok(())
-}
-
-fn insert_market_registration_events(
-    conn: &mut PgConnection,
-    ev: Vec<MarketRegistrationEvent>,
-) -> Result<(), Error> {
-    let ev = ev
-        .into_iter()
-        .map(|e| models::market::NewMarketRegistrationEvent::from(e))
-        .collect::<Vec<_>>();
-    let chunks = get_chunks(
-        ev.len(),
-        models::market::NewMarketRegistrationEvent::field_count(),
-    );
-    let table = econia_db::schema::market_registration_events::table;
-    for (start_ind, end_ind) in chunks {
-        execute_with_better_error(
-            conn,
-            diesel::insert_into(table)
-                .values(&ev[start_ind..end_ind])
-                .on_conflict_do_nothing(),
-            None,
-        )?;
-    }
-    Ok(())
-}
-
-// TODO cache recognized markets on startup rather than doing this query below
-fn insert_recognized_market_events(
-    conn: &mut PgConnection,
-    ev: Vec<RecognizedMarketEvent>,
-) -> Result<(), Error> {
-    use diesel::prelude::*;
-    use econia_db::schema::market_registration_events::dsl::*;
-
-    let mut events = vec![];
-
-    for e in ev.into_iter() {
-        let market = market_registration_events
-            .filter(base_account_address.eq(e.trading_pair.base_type.account_address))
-            .filter(base_module_name.eq(e.trading_pair.base_type.module_name))
-            .filter(base_struct_name.eq(e.trading_pair.base_type.struct_name))
-            .filter(quote_account_address.eq(e.trading_pair.quote_type.account_address))
-            .filter(quote_module_name.eq(e.trading_pair.quote_type.module_name))
-            .filter(quote_struct_name.eq(e.trading_pair.quote_type.struct_name))
-            .load::<models::market::MarketRegistrationEvent>(conn)?;
-
-        let mkt = &market[0];
-        if let Some(r) = e.recognized_market_info {
-            let new_lot_size = BigDecimal::from(r.lot_size);
-            let new_tick_size = BigDecimal::from(r.tick_size);
-            let new_min_size = BigDecimal::from(r.min_size);
-            events.push(NewRecognizedMarketEvent {
-                market_id: mkt.market_id.clone(),
-                time: *CURRENT_BLOCK_TIME.read().unwrap(),
-                event_type: if mkt.lot_size == new_lot_size
-                    && mkt.tick_size == new_tick_size
-                    && mkt.min_size == new_min_size
-                {
-                    MarketEventType::Add
-                } else {
-                    MarketEventType::Update
-                },
-                lot_size: Some(new_lot_size),
-                tick_size: Some(new_tick_size),
-                min_size: Some(new_min_size),
-            })
-        } else {
-            events.push(NewRecognizedMarketEvent {
-                market_id: mkt.market_id.clone(),
-                time: *CURRENT_BLOCK_TIME.read().unwrap(),
-                event_type: MarketEventType::Remove,
-                lot_size: None,
-                tick_size: None,
-                min_size: None,
-            })
-        }
-    }
-
-    let chunks = get_chunks(
-        events.len(),
-        models::market::NewRecognizedMarketEvent::field_count(),
-    );
-    let table = econia_db::schema::recognized_market_events::table;
-    for (start_ind, end_ind) in chunks {
-        execute_with_better_error(
-            conn,
-            diesel::insert_into(table)
-                .values(&events[start_ind..end_ind])
-                .on_conflict_do_nothing(),
-            None,
-        )?;
-    }
-    Ok(())
 }
 
 #[async_trait]
@@ -395,15 +461,15 @@ impl TransactionProcessor for EconiaTransactionProcessor {
             .filter(is_event_type_valid)
             .collect::<Vec<EventModel>>();
 
-        let mut conn = self.get_conn();
-        insert_to_db(&mut conn, start_version, end_version, events).map_err(|err| {
-            TransactionProcessingError::TransactionCommitError((
-                anyhow::Error::from(err),
-                start_version,
-                end_version,
-                NAME,
-            ))
-        })
+        self.insert_to_db(start_version, end_version, events)
+            .map_err(|err| {
+                TransactionProcessingError::TransactionCommitError((
+                    anyhow::Error::from(err),
+                    start_version,
+                    end_version,
+                    NAME,
+                ))
+            })
     }
 
     fn connection_pool(&self) -> &PgDbPool {
