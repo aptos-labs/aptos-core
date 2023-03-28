b5df7643a7ca9d465e315fbdc99fec6e71a789d8
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2023-03-27 21:49:42 -0700

    try network configs (#7421)

--- config/src/config/network_config.rs
+++ config/src/config/network_config.rs
@@ -42,11 +42,11 @@ pub const MAX_CONCURRENT_NETWORK_REQS: usize = 200;
 pub const MAX_CONNECTION_DELAY_MS: u64 = 60_000; /* 1 minute */
 pub const MAX_FULLNODE_OUTBOUND_CONNECTIONS: usize = 20;
 pub const MAX_INBOUND_CONNECTIONS: usize = 100; /* At 5k TPS this could easily hit ~50MiB a second */
-pub const MAX_MESSAGE_METADATA_SIZE: usize = 128 * 1024; /* 128 KiB: a buffer for metadata that might be added to messages by networking */
-pub const MESSAGE_PADDING_SIZE: usize = 2 * 1024 * 1024; /* 2 MiB: a safety buffer to allow messages to get larger during serialization */
+pub const MAX_MESSAGE_METADATA_SIZE: usize = 1 * 1024; /* 128 KiB: a buffer for metadata that might be added to messages by networking */
+pub const MESSAGE_PADDING_SIZE: usize = 2 * 1024; /* 2 MiB: a safety buffer to allow messages to get larger during serialization */
 pub const MAX_APPLICATION_MESSAGE_SIZE: usize =
     (MAX_MESSAGE_SIZE - MAX_MESSAGE_METADATA_SIZE) - MESSAGE_PADDING_SIZE; /* The message size that applications should check against */
-pub const MAX_FRAME_SIZE: usize = 20 * 1024 * 1024; /* 20 MiB large messages will be chunked into multiple frames and streamed */
+pub const MAX_FRAME_SIZE: usize = 512 * 1024; /* 4 MiB large messages will be chunked into multiple frames and streamed */
 pub const MAX_MESSAGE_SIZE: usize = 64 * 1024 * 1024; /* 64 MiB */
 pub const CONNECTION_BACKOFF_BASE: u64 = 2;
 pub const IP_BYTE_BUCKET_RATE: usize = 102400 /* 100 KiB */;
