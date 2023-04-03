93cd48a4bb639adde3d16f3b2ec41c639b53138c
maayan <maayansavir@gmail.com>
2023-04-03 15:00:20 -0700

    use pre created account

--- ecosystem/typescript/sdk/scripts/publish_ans_contract.ts
+++ ecosystem/typescript/sdk/scripts/publish_ans_contract.ts
@@ -1,4 +1,5 @@
 const { execSync } = require("child_process");
+require("dotenv").config();
 
 /**
  * TS SDK supports ANS. Since ANS contract is not part of aptos-framework
@@ -16,13 +17,11 @@ const ANS_CORE_FOLDER = "/aptos-names-contracts/core";
 // on local publishing we want to use `aptos` commnads and on CI we want to use `docker`
 const APTOS_INVOCATION = process.env.APTOS_INVOCATION || "aptos";
 // environment we use when testing
-const APTOS_NODE_URL = process.env.APTOS_NODE_URL || "http://localhost:8080/v1";
-const APTOS_FAUCET_URL = process.env.APTOS_FAUCET_URL || "http://localhost:8081";
+const APTOS_NODE_URL = process.env.APTOS_NODE_URL;
+const APTOS_FAUCET_URL = process.env.APTOS_FAUCET_URL;
 // ans account we use to publish the contract
-const ANS_TEST_ACCOUNT_PRIVATE_KEY =
-  process.env.ANS_TEST_ACCOUNT_PRIVATE_KEY || "0x37368b46ce665362562c6d1d4ec01a08c8644c488690df5a17e13ba163e20221";
-const ANS_TEST_ACCOUNT_ADDRESS =
-  process.env.ANS_TEST_ACCOUNT_ADDRESS || "0x585fc9f0f0c54183b039ffc770ca282ebd87307916c215a3e692f2f8e4305e82";
+const ANS_TEST_ACCOUNT_PRIVATE_KEY = process.env.ANS_TEST_ACCOUNT_PRIVATE_KEY;
+const ANS_TEST_ACCOUNT_ADDRESS = process.env.ANS_TEST_ACCOUNT_ADDRESS;
 
 try {
   // delete aptos-names-contracts folder
