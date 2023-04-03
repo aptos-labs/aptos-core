32b5cf3fd038677a910ef32f2a8a06012bc235b9
maayan <maayansavir@gmail.com>
2023-04-03 12:21:57 -0700

    use pre created account

--- ecosystem/typescript/sdk/scripts/publish_ans_contract.ts
+++ ecosystem/typescript/sdk/scripts/publish_ans_contract.ts
@@ -1,69 +1,57 @@
 const { execSync } = require("child_process");
-
-const ANS_CORE_FOLDER = "/aptos-names-contracts/core";
-const APTOS_INVOCATION = process.env.APTOS_INVOCATION || "aptos";
-const APTOS_NODE_URL = process.env.APTOS_NODE_URL || "http://localhost:8080/v1";
-const APTOS_FAUCET_URL = process.env.APTOS_FAUCET_URL || "http://localhost:8081";
-
-const APTOS_INIT_COMMAND = `${APTOS_INVOCATION} init --network custom --rest-url ${APTOS_NODE_URL} --faucet-url ${APTOS_FAUCET_URL} --assume-yes`;
-const GET_DEFAULT_PROFILE_COMMAND = `${APTOS_INVOCATION} config show-profiles --profile default`;
+require("dotenv").config();
 
 /**
  * TS SDK supports ANS. Since ANS contract is not part of aptos-framework
  * we need to get the ANS contract, publish it to local testnet and test against it.
  * This script clones the aptos-names-contracts repo {@link https://github.com/aptos-labs/aptos-names-contracts},
- * creates a default profile using `aptos init` and then use that profile to publish the contract
- * to the local testnet.
+ * uses a pre created account address and private key to fund that account and
+ * then publish the contract under that account.
  * After the contract is published, we delete the cloned repo folder.
  *
- * We run this script whenever we run `pnpm test` in the TS SDK.
+ * This script runs when testing locally and on CI (as part of sdk-release.yaml) using `pnpm test`.
  */
+
+// ans core folder where Move.toml and sources folder are
+const ANS_CORE_FOLDER = "/aptos-names-contracts/core";
+// on local publishing we want to use `aptos` commnads and on CI we want to use `docker`
+const APTOS_INVOCATION = process.env.APTOS_INVOCATION || "aptos";
+// environment we use when testing
+const APTOS_NODE_URL = process.env.APTOS_NODE_URL;
+const APTOS_FAUCET_URL = process.env.APTOS_FAUCET_URL;
+// ans account we use to publish the contract
+const ANS_TEST_ACCOUNT_PRIVATE_KEY = process.env.ANS_TEST_ACCOUNT_PRIVATE_KEY;
+const ANS_TEST_ACCOUNT_ADDRESS = process.env.ANS_TEST_ACCOUNT_ADDRESS;
 
 try {
   // delete aptos-names-contracts folder
   console.log("---deleting aptos-names-contracts folder---");
   deleteAnsFolder();
   // 1. Clone ANS repository into the current directory
-  console.log("working directory", __dirname);
-  console.log("ANS_CORE_FOLDER", __dirname + ANS_CORE_FOLDER);
-
   console.log("---clone ANS repository---");
   execSync("git clone https://github.com/aptos-labs/aptos-names-contracts.git", {
     cwd: __dirname,
   });
 
-  // 2. initialize a default profile
-  console.log("---initialize a default profile---");
-  execSync(APTOS_INIT_COMMAND, {
-    cwd: __dirname + ANS_CORE_FOLDER,
-  });
+  // 2. fund ans account
+  console.log("---funding account---");
+  execSync(
+    `${APTOS_INVOCATION} account fund-with-faucet --account ${ANS_TEST_ACCOUNT_ADDRESS} --faucet-url ${APTOS_FAUCET_URL} --url ${APTOS_NODE_URL}`,
+  );
 
   // Sleep for 10 seconds to make sure account has funded
   console.log("---sleeps for 10 seconds to make sure account has funded---");
   execSync("sleep 10");
 
-  // 3. get default profile info
-  console.log("---get default profile info---");
-  const data = execSync(GET_DEFAULT_PROFILE_COMMAND, {
-    cwd: __dirname + ANS_CORE_FOLDER,
-  })
-    .toString()
-    .trim();
-
-  // 4. get default profile account address
-  console.log("---get default profile account address---");
-  const profileAccountAddress = JSON.parse(data).Result.default.account;
-
-  // 5. publish ans modules under the default profile
+  // 3. publish ans modules under the ans account
   console.log("---publish ans modules---");
   execSync(
-    `${APTOS_INVOCATION} move publish --named-addresses aptos_names=0x${profileAccountAddress},aptos_names_admin=0x${profileAccountAddress},aptos_names_funds=0x${profileAccountAddress} --assume-yes`,
-    {
-      cwd: __dirname + ANS_CORE_FOLDER,
-    },
+    `${APTOS_INVOCATION} move publish --private-key=${ANS_TEST_ACCOUNT_PRIVATE_KEY} --named-addresses aptos_names=0x${ANS_TEST_ACCOUNT_ADDRESS},aptos_names_admin=0x${ANS_TEST_ACCOUNT_ADDRESS},aptos_names_funds=0x${ANS_TEST_ACCOUNT_ADDRESS} --url=${APTOS_NODE_URL} --package-dir=${
+      __dirname + ANS_CORE_FOLDER
+    } --assume-yes`,
   );
 
-  // 6. Delete aptos-names-contracts folder created by the git clone command
+  // 4. Delete aptos-names-contracts folder created by the git clone command
   console.log("---module published, deleting aptos-names-contracts folder---");
   deleteAnsFolder();
 } catch (error: any) {
