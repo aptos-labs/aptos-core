ec771fd82809588b991a67d2637fb7d8f9c8f607
gerben <stavenga@gmail.com>
2023-04-05 14:55:40 -0700

    gas feature number

--- aptos-move/aptos-gas/src/aptos_framework.rs
+++ aptos-move/aptos-gas/src/aptos_framework.rs
@@ -120,6 +120,6 @@ crate::natives::define_gas_parameters_for_natives!(GasParameters, "aptos_framewo
     // These are dummy value, they copied from storage gas in aptos-core/aptos-vm/src/aptos_vm_impl.rs
     [.object.exists_at.per_byte_loaded, { 7.. => "object.exists_at.per_byte_loaded" }, 1000],
     [.object.exists_at.per_item_loaded, { 7.. => "object.exists_at.per_item_loaded" }, 8000],
-    [.string_utils.base, "string_utils.format.base", 300 * MUL],
-    [.string_utils.per_byte, "string_utils.format.per_byte", 1 * MUL],
+    [.string_utils.base, {8.. => "string_utils.format.base"}, 300 * MUL],
+    [.string_utils.per_byte, {8.. =>"string_utils.format.per_byte"}, 1 * MUL],
 ]);
