01beeabae4341db38ebf183006de2f194d44322d
Daniel Porteous (dport) <danielporteous1@gmail.com>
2023-04-07 16:59:06 -0700

    [CLI] Do not suggest people use a faucet for mainnet accounts (#7524)

--- crates/aptos/src/common/init.rs
+++ crates/aptos/src/common/init.rs
@@ -224,9 +224,9 @@ impl CliCommand<()> for InitTool {
         } else if account_exists {
             eprintln!("Account {} has been already found onchain", address);
         } else if network == Network::Mainnet {
-            eprintln!("Account {} does not exist, you will need to create and fund the account through a faucet or by transferring funds from another account", address);
+            eprintln!("Account {} does not exist, you will need to create and fund the account by transferring funds from another account", address);
         } else {
-            eprintln!("Account {} has been initialized locally, but you must have coins transferred to it to create the account onchain", address);
+            eprintln!("Account {} has been initialized locally, but you must transfer coins to it to create the account onchain", address);
         }
 
         // Ensure the loaded config has profiles setup for a possible empty file
