ef414afa053db5024f95c8476e72c96a23c755ab
maayan <maayansavir@gmail.com>
2023-04-16 22:19:10 -0700

    add x-aptos-client header

--- ecosystem/typescript/sdk/src/providers/aptos_client.ts
+++ ecosystem/typescript/sdk/src/providers/aptos_client.ts
@@ -35,6 +35,7 @@ import {
   AnyNumber,
 } from "../bcs";
 import { Ed25519PublicKey, MultiEd25519PublicKey } from "../aptos_types";
+import { VERSION } from "../version";
 
 export interface OptionalTransactionArgs {
   maxGasAmount?: Uint64;
@@ -85,6 +86,7 @@ export class AptosClient {
     } else {
       conf.WITH_CREDENTIALS = true;
     }
+    conf.HEADERS = { ...conf.HEADERS, "X-Aptos-Client": `aptos-ts-sdk/${VERSION}` };
     this.client = new Gen.AptosGeneratedClient(conf);
   }
 
