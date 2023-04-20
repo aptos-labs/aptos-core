581b2d7702c43418690e71190ffcd8a9dad2c4ba
Brian Cho <brian@aptoslabs.com>
2023-04-20 11:40:29 -0700

    Land blocking to use quorum store and three regions

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -11,7 +11,11 @@ use aptos_forge::{
 };
 use aptos_logger::Level;
 use aptos_rest_client::Client as RestClient;
-use aptos_sdk::{move_types::account_address::AccountAddress, transaction_builder::aptos_stdlib};
+use aptos_sdk::{
+    move_types::account_address::AccountAddress,
+    transaction_builder::aptos_stdlib,
+    types::on_chain_config::{ConsensusConfigV1, OnChainConsensusConfig},
+};
 use aptos_testcases::{
     compatibility_test::SimpleValidatorUpgrade,
     consensus_reliability_tests::ChangingWorkingQuorumTest,
@@ -402,7 +406,7 @@ fn get_changelog(prev_commit: Option<&String>, upstream_commit: &str) -> String 
 
 fn get_test_suite(suite_name: &str, duration: Duration) -> Result<ForgeConfig<'static>> {
     match suite_name {
-        "land_blocking" => Ok(land_blocking_test_suite(duration)),
+        "land_blocking" => Ok(land_blocking_three_region_test_suite(duration)),
         "local_test_suite" => Ok(local_test_suite()),
         "pre_release" => Ok(pre_release_suite()),
         "run_forever" => Ok(run_forever()),
@@ -1067,6 +1071,45 @@ fn three_region_simulation(config: ForgeConfig) -> ForgeConfig {
                     max_no_progress_secs: 20.0,
                     max_round_gap: 6,
                 }),
+        )
+}
+
+fn land_blocking_three_region_test_suite(duration: Duration) -> ForgeConfig<'static> {
+    ForgeConfig::default()
+        .with_initial_validator_count(NonZeroUsize::new(20).unwrap())
+        .with_initial_fullnode_count(10)
+        .with_network_tests(vec![&ThreeRegionSameCloudSimulationTest])
+        .with_genesis_helm_config_fn(Arc::new(|helm_values| {
+            // Using V2 turns quorum store on by default
+            let consensus_config = OnChainConsensusConfig::V2(ConsensusConfigV1::default());
+            helm_values["chain"]["consensus_config"] =
+                serde_yaml::to_value(consensus_config).unwrap();
+            // Have single epoch change in land blocking
+            helm_values["chain"]["epoch_duration_secs"] = 300.into();
+        }))
+        .with_success_criteria(
+            SuccessCriteria::new(
+                if duration.as_secs() > 1200 {
+                    4500
+                } else {
+                    5000
+                },
+            )
+            .add_no_restarts()
+            .add_wait_for_catchup_s(
+                // Give at least 60s for catchup, give 10% of the run for longer durations.
+                (duration.as_secs() / 10).max(60),
+            )
+            .add_system_metrics_threshold(SystemMetricsThreshold::new(
+                // Check that we don't use more than 12 CPU cores for 30% of the time.
+                MetricsThreshold::new(12, 30),
+                // Check that we don't use more than 10 GB of memory for 30% of the time.
+                MetricsThreshold::new(10 * 1024 * 1024 * 1024, 30),
+            ))
+            .add_chain_progress(StateProgressThreshold {
+                max_no_progress_secs: 10.0,
+                max_round_gap: 4,
+            }),
         )
 }
 
