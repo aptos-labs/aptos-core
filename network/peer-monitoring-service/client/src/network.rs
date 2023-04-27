575e4c7439e5bce027ea984145eb7bf75c6b60b8
Josh Lind <josh.lind@hotmail.com>
2023-04-26 20:17:05 -0400

    [Peer Monitor] Add server support for performance requests.

--- network/peer-monitoring-service/client/src/network.rs
+++ network/peer-monitoring-service/client/src/network.rs
@@ -93,7 +93,7 @@ pub async fn send_request_to_peer(
     let result = peer_monitoring_client
         .send_request(
             *peer_network_id,
-            request,
+            request.clone(),
             Duration::from_millis(request_timeout_ms),
         )
         .await;
@@ -108,7 +108,7 @@ pub async fn send_request_to_peer(
             );
             metrics::increment_request_counter(
                 &metrics::SUCCESS_RESPONSES,
-                request.get_label(),
+                request.clone().get_label(),
                 peer_network_id,
             );
             Ok(response)
