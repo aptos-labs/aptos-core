5ea15c9d28c28b17ea1a306446e77531b323403b
Jin <jin@aptoslabs.com>
2023-04-30 14:18:16 -0700

    update call_aptos_cli file

--- ecosystem/typescript/sdk/examples/typescript/call_aptos_cli.ts
+++ ecosystem/typescript/sdk/examples/typescript/call_aptos_cli.ts
@@ -8,6 +8,7 @@ const lib = ffi.Library('../../../../../target/release/libaptos', {
 });
 
 const args = ['aptos', 'node', 'run-local-testnet', '--with-faucet'];
+const args1 = ['aptos', 'info'];
 
 (async () => {
     console.log("Running print from typescript");
@@ -16,7 +17,7 @@ const args = ['aptos', 'node', 'run-local-testnet', '--with-faucet'];
     console.log("Finish");
 
     console.log("Running aptos from typescript");
-    const result2 = lib.run_aptos_from_ts(args.join(' '));
+    const result2 = lib.run_aptos_from_ts(args1.join(' '));
     console.log(`Result: ${result2}`);
     console.log("Finish aptos");
 })();
