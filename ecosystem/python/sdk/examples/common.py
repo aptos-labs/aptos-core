6ae82662db6fb942a5a4f16ab5b5a77778687155
David Wolinsky <isaac.wolinsky@gmail.com>
2023-04-29 20:07:55 -0700

    [python] support inserting sequence numbers in transaction helpers

--- ecosystem/python/sdk/examples/common.py
+++ ecosystem/python/sdk/examples/common.py
@@ -9,3 +9,9 @@ FAUCET_URL = os.getenv(
     "APTOS_FAUCET_URL",
     "https://faucet.devnet.aptoslabs.com",
 )  # <:!:section_1
+
+NODE_URL = os.getenv("APTOS_NODE_URL", "http://127.0.0.1:8080/v1")
+FAUCET_URL = os.getenv(
+    "APTOS_FAUCET_URL",
+    "http://127.0.0.1:8081",
+)  # <:!:section_1
