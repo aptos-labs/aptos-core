4a04eae9db4887669860b601457a737ab9f7e66b
bowenyang007 <carlyang007@gmail.com>
2023-05-02 17:07:57 -0700

    remove token activities v2

--- crates/indexer/migrations/2023-04-28-053048_object_token_v2/up.sql
+++ crates/indexer/migrations/2023-04-28-053048_object_token_v2/up.sql
@@ -163,30 +163,6 @@ CREATE TABLE IF NOT EXISTS current_token_datas_v2 (
 );
 CREATE INDEX IF NOT EXISTS cur_td2_cid_name_index ON current_token_datas_v2 (collection_id, token_name);
 CREATE INDEX IF NOT EXISTS cur_td2_insat_index ON current_token_datas_v2 (inserted_at);
-CREATE TABLE IF NOT EXISTS token_activities_v2 (
-  transaction_version BIGINT NOT NULL,
-  event_index BIGINT NOT NULL,
-  event_account_address VARCHAR(66) NOT NULL,
-  token_data_id VARCHAR(66),
-  property_version_v1 NUMERIC NOT NULL,
-  type VARCHAR(50) NOT NULL,
-  from_address VARCHAR(66),
-  to_address VARCHAR(66),
-  token_amount NUMERIC,
-  before_value TEXT,
-  after_value TEXT,
-  entry_function_id_str VARCHAR(100),
-  token_standard VARCHAR(10) NOT NULL,
-  is_fungible_v2 BOOLEAN,
-  transaction_timestamp TIMESTAMP NOT NULL,
-  inserted_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  PRIMARY KEY (transaction_version, event_index)
-);
-CREATE INDEX IF NOT EXISTS ta2_owner_type_index ON token_activities_v2 (event_account_address, type);
-CREATE INDEX IF NOT EXISTS ta2_from_type_index ON token_activities_v2 (from_address, type);
-CREATE INDEX IF NOT EXISTS ta2_to_type_index ON token_activities_v2 (to_address, type);
-CREATE INDEX IF NOT EXISTS ta2_tid_index ON token_activities_v2 (token_data_id);
-CREATE INDEX IF NOT EXISTS ta2_insat_index ON token_activities_v2 (inserted_at);
 -- Add ID (with 0x prefix)
 ALTER TABLE current_token_pending_claims
 ADD COLUMN IF NOT EXISTS token_data_id VARCHAR(66) NOT NULL DEFAULT '';
