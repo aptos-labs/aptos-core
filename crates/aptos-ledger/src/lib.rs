46bd96650f93b969cac591e66c64345198913cdf
Jin <jin@aptoslabs.com>
2023-05-09 22:29:35 -0700

    Create skeleton aptos-ledger crate

---
+++ crates/aptos-ledger/src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
