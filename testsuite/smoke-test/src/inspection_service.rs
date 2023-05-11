7a40d7c8a4e89a11a9c0a59f23df6d3d0182d19e
Satya Vusirikala <satyasatya123456@gmail.com>
2023-05-11 15:49:28 -0500

    Update testsuite/smoke-test/src/inspection_service.rs

--- testsuite/smoke-test/src/inspection_service.rs
+++ testsuite/smoke-test/src/inspection_service.rs
@@ -8,7 +8,7 @@ use aptos_forge::Swarm;
 async fn test_inspection_service_connection() {
     let mut swarm = new_local_swarm_with_aptos(1).await;
     let info = swarm.aptos_public_info();
-
+// Ping the inspection service index page and verify we get a successful response
     let resp = reqwest::get(info.inspection_service_url().to_owned())
         .await
         .unwrap();
