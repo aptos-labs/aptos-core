f3a1c86d831ea295535eceba518b15ccebf7537a
zhoujun.ma <zhoujun@aptoslabs.com>
2023-05-16 02:13:08 -0700

    a little documentation

--- consensus/src/dag/dag_storage/mod.rs
+++ consensus/src/dag/dag_storage/mod.rs
@@ -23,25 +23,46 @@ use crate::dag::types::peer_status_list::{PeerStatusList, PeerStatusListItem, Pe
 
 pub type ItemId = [u8; 16];
 
-pub(crate) trait DagStorageItem<T:DagStorage>: Sized {
-    type Brief;
+/// Schema details and helper methods a DAG struct should specify
+/// for persisting its objects using a DagStorage solution `T`.
+pub(crate) trait DagStorageItem<T: DagStorage>: Sized {
     type Id;
-    fn deserialize_id(cursor: &mut Cursor<&[u8]>) -> Result<Self::Id>;
-    fn deserialize_brief(cursor: &mut Cursor<&[u8]>) -> Result<Self::Brief>;
+    /// Get the ID of the object. This usually becomes the DB key.
     fn id(&self) -> Self::Id;
-    fn brief(&self) -> Self::Brief;
     fn serialize_id(id: &Self::Id) -> Vec<u8>;
+    fn deserialize_id(cursor: &mut Cursor<&[u8]>) -> Result<Self::Id>;
+
+    type Brief;
+    /// Get the brief of the object. This usually becomes the DB value.
+    /// For small objects, this should capture everything except the ID.
+    /// For large objects, this should include small fields, references to sub-objects, or some metadata (e.g. for a list, the length).
+    fn brief(&self) -> Self::Brief;
     fn serialize_brief(brief: &Self::Brief) -> Vec<u8>;
+    fn deserialize_brief(cursor: &mut Cursor<&[u8]>) -> Result<Self::Brief>;
+
+    /// Load a DAG object of type `Self` by ID from a `DagStorage`.
     fn load(store: Arc<dyn DagStorage>, id :&Self::Id) -> Result<Option<Self>>;
+
+    /// Update/insert the current DAG object and recursively its fields.
     fn deep_save(&self, write_batch: &mut Box<dyn DagStoreWriteBatch>) -> Result<()>;
+
+    /// Update/insert the current DAG object, brief-only.
     fn shallow_save(&self, write_batch: &mut Box<dyn DagStoreWriteBatch>) -> Result<()>;
+
+    /// Delete the brief of an DAG object, if exists.
     fn shallow_delete(id: &Self::Id, write_batch: &mut Box<dyn DagStoreWriteBatch>) -> Result<()>;
+
     //TODO: deep-deletion is not trivial, but do we need it? If so, how? Reference counting? Pruner?
 }
 
 pub(crate) trait DagStorage: Sync + Send {
+    /// Needed by casting.
     fn as_any(&self) -> &dyn Any;
+
+    /// Create an associated write batch of the associated type `DagStoreWriteBatch`,
     fn new_write_batch(&self) -> Box<dyn DagStoreWriteBatch>;
+
+    /// Try committing all some storage diff..
     fn commit_write_batch(&self, batch: Box<dyn DagStoreWriteBatch>) -> Result<()>;
 }
 
