10fd43de5f32ec849dd3cf9e1f9bcba33e58bbc1
Xinding Wei <xinding@aptoslabs.com>
2023-05-18 12:24:54 -0700

    Enable forge_enable_performance

--- testsuite/run_forge.sh
+++ testsuite/run_forge.sh
@@ -6,5 +6,5 @@
 # A light wrapper for the new forge python script
 
 echo "Warning: run_forge.sh is deprecated. Please use forge.py instead."
-echo "Executing python testsuite/forge.py test $@"
+echo "Executing python testsuite/forge.py test" "$@"
 exec python3 testsuite/forge.py test "$@"
