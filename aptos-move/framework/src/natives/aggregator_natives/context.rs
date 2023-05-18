71a6d457e712edb5a107ecf17bf5a1f61beb0fa3
Rati Gelashvili <gelash@aptoslabs.com>
2023-05-17 20:23:14 -0400

    Minor comment fixes add-on

--- aptos-move/framework/src/natives/aggregator_natives/context.rs
+++ aptos-move/framework/src/natives/aggregator_natives/context.rs
@@ -204,9 +204,9 @@ mod test {
     //     |  200  |               |   yes     | yes |         |
     //     |  300  |               |   yes     |     |   yes   |
     //     |  400  |               |   yes     |     |         |
-    //     |  500  |               |   Yes     | yes |   yes   |
-    //     |  600  |               |   Yes     | yes |         |
-    //     |  700  |               |   Yes     | yes |         |
+    //     |  500  |               |   yes     | yes |   yes   |
+    //     |  600  |               |   yes     | yes |         |
+    //     |  700  |               |   yes     | yes |         |
     //     |  800  |               |           |     |   yes   |
     //     +-------+---------------+-----------+-----+---------+
     fn test_set_up(context: &NativeAggregatorContext, aggregator_enabled: bool) {
