40aae1ff36c392065aeb6377d02b673d5b6a2685
Brian Cho <brian@aptoslabs.com>
2023-05-22 15:44:20 +0900

    For measuring the overhead of a very naive, sequential, implementation of duplicate intra-block filtering.
    quorum store sharded cache
    
    fix
    
    fix 2
    
    fix metrics
    
    Require permission check before running determine-docker-build-metadata
    
    add TODO
    
    First filter duplicate account+seqno

--- aptos-move/aptos-vm/src/block_executor/mod.rs
+++ aptos-move/aptos-vm/src/block_executor/mod.rs
@@ -8,8 +8,8 @@ use crate::{
     adapter_common::{preprocess_transaction, PreprocessedTransaction},
     block_executor::vm_wrapper::AptosExecutorTask,
     counters::{
-        BLOCK_EXECUTOR_CONCURRENCY, BLOCK_EXECUTOR_EXECUTE_BLOCK_SECONDS,
-        BLOCK_EXECUTOR_SIGNATURE_VERIFICATION_SECONDS,
+        BLOCK_EXECUTOR_CONCURRENCY, BLOCK_EXECUTOR_DUPLICATES_FILTERED,
+        BLOCK_EXECUTOR_EXECUTE_BLOCK_SECONDS, BLOCK_EXECUTOR_SIGNATURE_VERIFICATION_SECONDS,
     },
     AptosVM,
 };
@@ -22,6 +22,7 @@ use aptos_block_executor::{
         TransactionOutput as BlockExecutorTransactionOutput,
     },
 };
+use aptos_crypto::{hash::CryptoHash, HashValue};
 use aptos_infallible::Mutex;
 use aptos_state_view::StateView;
 use aptos_types::{
@@ -30,10 +31,11 @@ use aptos_types::{
     write_set::{WriteOp, WriteSet},
 };
 use aptos_vm_logging::{flush_speculative_logs, init_speculative_logs};
+use moka::sync::SegmentedCache;
 use move_core_types::vm_status::VMStatus;
 use once_cell::sync::OnceCell;
 use rayon::{prelude::*, ThreadPool};
-use std::sync::Arc;
+use std::{collections::HashSet, sync::Arc};
 
 impl BlockExecutorTransaction for PreprocessedTransaction {
     type Key = StateKey;
@@ -135,6 +137,15 @@ impl BlockExecutorTransactionOutput for AptosTransactionOutput {
 
 pub struct BlockAptosVM();
 
+/// Insert a transaction hash into `TXN_CACHE` if not present.
+/// Return whether the insertion happened (i.e., cache miss).
+fn insert_into_txn_cache(cache: &SegmentedCache<HashValue, u64>, txn: &Transaction) -> bool {
+    let key = txn.hash();
+    let nonce_in = rand::random::<u64>();
+    let nonce_out = cache.get_with(key, || nonce_in);
+    nonce_in == nonce_out
+}
+
 impl BlockAptosVM {
     pub fn execute_block<S: StateView + Sync>(
         executor_thread_pool: Arc<ThreadPool>,
@@ -144,19 +155,53 @@ impl BlockAptosVM {
         maybe_gas_limit: Option<u64>,
     ) -> Result<Vec<TransactionOutput>, VMStatus> {
         let _timer = BLOCK_EXECUTOR_EXECUTE_BLOCK_SECONDS.start_timer();
+
         // Verify the signatures of all the transactions in parallel.
         // This is time consuming so don't wait and do the checking
         // sequentially while executing the transactions.
         let signature_verification_timer =
             BLOCK_EXECUTOR_SIGNATURE_VERIFICATION_SECONDS.start_timer();
+
+        // Optimization to only check hashes for txns with matching (sender, sequence_number)
+        let mut possible_duplicate_map = HashSet::new();
+        {
+            let mut seen = HashSet::new();
+            for txn in &transactions {
+                if let Transaction::UserTransaction(ref inner) = txn {
+                    if !seen.insert((inner.sender(), inner.sequence_number())) {
+                        possible_duplicate_map.insert((inner.sender(), inner.sequence_number()));
+                    }
+                }
+            }
+        }
+
+        let duplicate_map = SegmentedCache::new(100000, 256);
         let signature_verified_block: Vec<PreprocessedTransaction> =
             executor_thread_pool.install(|| {
                 transactions
                     .into_par_iter()
                     .with_min_len(25)
-                    .map(preprocess_transaction::<AptosVM>)
+                    .map(|txn| match txn {
+                        Transaction::UserTransaction(ref inner) => {
+                            if possible_duplicate_map
+                                .contains(&(inner.sender(), inner.sequence_number()))
+                                && !insert_into_txn_cache(&duplicate_map, &txn)
+                            {
+                                return PreprocessedTransaction::Duplicate;
+                            }
+                            preprocess_transaction::<AptosVM>(txn)
+                        },
+                        _ => preprocess_transaction::<AptosVM>(txn),
+                    })
                     .collect()
             });
+
+        // Get the number for metrics
+        let num_filtered = signature_verified_block
+            .iter()
+            .filter(|txn| matches!(txn, PreprocessedTransaction::Duplicate))
+            .count();
+        BLOCK_EXECUTOR_DUPLICATES_FILTERED.observe(num_filtered as f64);
         drop(signature_verification_timer);
 
         let num_txns = signature_verified_block.len();
