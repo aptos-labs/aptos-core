0aa0250f2e71b4ba4d338309ca0b7e06cc950ce6
George Mitenkov <georgemitenk0v@gmail.com>
2023-05-31 09:14:06 +0200

    [move] Hook for VM session output format (#8189)
    Move `ChangeSet` and `AccountChangeSet` types are now generic. The
    motivation for this change is to avoid any particular representation being used
    to communicate changes.
    
    All Move code is fully compatible with the change, all old APIs are preserved.
    However, additionally session can take a hook which dictates how resources
    should be outputed. The hook is implemented as a callback which the user can
    pass on the adapter's side.

--- third_party/move/move-vm/integration-tests/src/tests/bad_storage_tests.rs
+++ third_party/move/move-vm/integration-tests/src/tests/bad_storage_tests.rs
@@ -106,7 +106,7 @@ fn test_malformed_resource() {
     let (changeset, _) = sess.finish().unwrap();
     storage.apply(changeset).unwrap();
 
-    // Execut the second script and make sure it succeeds. This script simply checks
+    // Execute the second script and make sure it succeeds. This script simply checks
     // that the published resource is what we expect it to be. This inital run is to ensure
     // the testing environment is indeed free of errors without external interference.
     let mut script_blob = vec![];
