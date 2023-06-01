486f79af430f05df44ed289f75230b6708870633
Jin <jin@aptoslabs.com>
2023-06-01 10:40:45 -0700

    poetry run isort, poetry run black

--- ecosystem/python/sdk/aptos_sdk/metadata.py
+++ ecosystem/python/sdk/aptos_sdk/metadata.py
@@ -3,9 +3,10 @@ import importlib.metadata as metadata
 # constants
 PACKAGE_NAME = "aptos-sdk"
 
+
 class Metadata:
     APTOS_HEADER = "x-aptos-client"
-    
+
     @staticmethod
     def get_aptos_header_val():
         version = metadata.version(PACKAGE_NAME)
