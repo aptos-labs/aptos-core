bd3eb542ef22a2067653417295a02a2fb571efd8
Rustie Lin <rustie117@gmail.com>
2023-06-02 11:19:28 -0700

    [indexer-grpc] remove server_name from all configs (#8497)

--- ecosystem/indexer-grpc/indexer-grpc-file-store/src/lib.rs
+++ ecosystem/indexer-grpc/indexer-grpc-file-store/src/lib.rs
@@ -13,7 +13,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct IndexerGrpcFileStoreWorkerConfig {
-    pub server_name: String,
     pub file_store_config: IndexerGrpcFileStoreConfig,
     pub redis_main_instance_address: String,
 }
@@ -30,6 +29,6 @@ impl RunnableConfig for IndexerGrpcFileStoreWorkerConfig {
     }
 
     fn get_server_name(&self) -> String {
-        self.server_name.clone()
+        "idxfile".to_string()
     }
 }
