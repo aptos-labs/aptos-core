a7480fe790e39b9d33a2d8959bc5c371c8904c6c
zhoujun.ma <zhoujun@aptoslabs.com>
2023-06-07 11:08:43 -0700

    revert

--- execution/executor/src/components/chunk_output.rs
+++ execution/executor/src/components/chunk_output.rs
@@ -76,7 +76,7 @@ impl ChunkOutput {
             maybe_block_gas_limit,
         )?;
 
-        update_counters_for_processed_chunk(&transactions, &transaction_outputs, "executed");
+        // TODO(skedia) add logic to emit counters per shard instead of doing it globally.
 
         // Unwrapping here is safe because the execution has finished and it is guaranteed that
         // the state view is not used anymore.
