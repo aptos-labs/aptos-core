f2d1d6b031132474d0b19a1c800cd14ce2d64a13
Zekun Li <li.zekun@gmail.com>
2023-06-08 16:44:13 -0700

    [dag] add dag store
    This commit adds basic nodes types and Dag store that handles indexing and produces links for new round.
    
    TODO: support garbage collection

--- consensus/src/dag/mod.rs
+++ consensus/src/dag/mod.rs
@@ -1,8 +1,9 @@
 // Copyright © Aptos Foundation
 // Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
+#![allow(dead_code)]
 
-#[allow(dead_code)]
+mod dag_store;
 mod reliable_broadcast;
 #[cfg(test)]
 mod tests;
