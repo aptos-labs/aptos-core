d368af1daf3069c262f373e5f6dfd0cb5f9df4a0
Balaji Arun <balajia@vt.edu>
2023-06-12 16:52:58 -0700

    lint

--- testsuite/test_framework/cluster.py
+++ testsuite/test_framework/cluster.py
@@ -60,11 +60,11 @@ class ForgeCluster:
     def set_kubeconf(self, kubeconf: str) -> ForgeCluster:
         self.kubeconf = kubeconf
         return self
-    
+
     @property
     def kubectl_create_context_arg(self) -> str:
         return f"--context=karmada-apiserver" if self.is_multiregion else ""
-        
+
     async def write(self, shell: Shell) -> None:
         assert self.kubeconf is not None, "kubeconf must be set"
         await self.write_cluster_config(shell, self.name, self.kubeconf)
