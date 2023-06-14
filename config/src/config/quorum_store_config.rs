1538df406ba613949ec6e23780990bfec718f130
Balaji Arun <balajia@vt.edu>
2023-06-13 20:21:05 -0700

    reduce time

--- config/src/config/quorum_store_config.rs
+++ config/src/config/quorum_store_config.rs
@@ -32,7 +32,7 @@ impl Default for QuorumStoreBackPressureConfig {
             decrease_duration_ms: 1000,
             increase_duration_ms: 1000,
             decrease_fraction: 0.5,
-            dynamic_min_txn_per_s: 3000,
+            dynamic_min_txn_per_s: 4000,
             dynamic_max_txn_per_s: 6000,
         }
     }
