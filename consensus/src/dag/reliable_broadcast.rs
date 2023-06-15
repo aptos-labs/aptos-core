cb89d2da04f07100300c98537b09e010eca5b940
Zekun Li <li.zekun@gmail.com>
2023-06-15 16:24:29 -0700

    [dag] basic dag driver

--- consensus/src/dag/reliable_broadcast.rs
+++ consensus/src/dag/reliable_broadcast.rs
@@ -1,24 +1,16 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::network_interface::ConsensusMsg;
+use crate::{dag::types::DAGMessage, network_interface::ConsensusMsg};
 use aptos_consensus_types::common::Author;
 use async_trait::async_trait;
 use futures::{stream::FuturesUnordered, StreamExt};
 use std::{future::Future, sync::Arc, time::Duration};
 
-pub trait DAGMessage: Sized + Clone {
-    fn from_network_message(msg: ConsensusMsg) -> anyhow::Result<Self>;
-
-    fn into_network_message(self) -> ConsensusMsg;
-}
-
 pub trait BroadcastStatus {
-    type Message: DAGMessage;
     type Ack: DAGMessage;
     type Aggregated;
-
-    fn empty(validators: Vec<Author>) -> Self;
+    type Message: DAGMessage;
 
     fn add(&mut self, peer: Author, ack: Self::Ack) -> anyhow::Result<Option<Self::Aggregated>>;
 }
@@ -49,12 +41,11 @@ impl ReliableBroadcast {
     pub fn broadcast<S: BroadcastStatus>(
         &self,
         message: S::Message,
+        mut aggregating: S,
     ) -> impl Future<Output = S::Aggregated> {
         let receivers: Vec<_> = self.validators.clone();
-        let network_message = message.into_network_message();
         let network_sender = self.network_sender.clone();
         async move {
-            let mut aggregating = S::empty(receivers.clone());
             let mut fut = FuturesUnordered::new();
             let send_message = |receiver, message| {
                 let network_sender = network_sender.clone();
@@ -67,6 +58,7 @@ impl ReliableBroadcast {
                     )
                 }
             };
+            let network_message = message.into_network_message();
             for receiver in receivers {
                 fut.push(send_message(receiver, network_message.clone()));
             }
