8a7cf4b6d3ce29e882c1e87075ab1c37f5cac2be
maayan <maayansavir@gmail.com>
2023-06-15 10:30:53 -0400

    add transactions folder

---
+++ ecosystem/typescript/sdk/v2/src/transactions/builder_utils.ts
@@ -0,0 +1,196 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  TypeTag,
+  TypeTagBool,
+  TypeTagU8,
+  TypeTagU16,
+  TypeTagU32,
+  TypeTagU64,
+  TypeTagU128,
+  TypeTagU256,
+  TypeTagAddress,
+  AccountAddress,
+  TypeTagVector,
+  TypeTagStruct,
+  TransactionArgument,
+  TransactionArgumentBool,
+  TransactionArgumentU16,
+  TransactionArgumentU32,
+  TransactionArgumentU64,
+  TransactionArgumentU128,
+  TransactionArgumentU256,
+  TransactionArgumentAddress,
+  TransactionArgumentU8,
+  TransactionArgumentU8Vector,
+  HexString,
+} from "../../types";
+import { Serializer } from "../../bcs";
+
+function assertType(val: any, types: string[] | string, message?: string) {
+  if (!types?.includes(typeof val)) {
+    throw new Error(
+      message || `Invalid arg: ${val} type should be ${types instanceof Array ? types.join(" or ") : types}`,
+    );
+  }
+}
+
+export function ensureBoolean(val: boolean | string): boolean {
+  assertType(val, ["boolean", "string"]);
+  if (typeof val === "boolean") {
+    return val;
+  }
+
+  if (val === "true") {
+    return true;
+  }
+  if (val === "false") {
+    return false;
+  }
+
+  throw new Error("Invalid boolean string.");
+}
+
+export function ensureNumber(val: number | string): number {
+  assertType(val, ["number", "string"]);
+  if (typeof val === "number") {
+    return val;
+  }
+
+  const res = Number.parseInt(val, 10);
+  if (Number.isNaN(res)) {
+    throw new Error("Invalid number string.");
+  }
+
+  return res;
+}
+
+export function ensureBigInt(val: number | bigint | string): bigint {
+  assertType(val, ["number", "bigint", "string"]);
+  return BigInt(val);
+}
+
+export function serializeArg(argVal: any, argType: TypeTag, serializer: Serializer) {
+  if (argType instanceof TypeTagBool) {
+    serializer.serializeBool(ensureBoolean(argVal));
+    return;
+  }
+  if (argType instanceof TypeTagU8) {
+    serializer.serializeU8(ensureNumber(argVal));
+    return;
+  }
+  if (argType instanceof TypeTagU16) {
+    serializer.serializeU16(ensureNumber(argVal));
+    return;
+  }
+  if (argType instanceof TypeTagU32) {
+    serializer.serializeU32(ensureNumber(argVal));
+    return;
+  }
+  if (argType instanceof TypeTagU64) {
+    serializer.serializeU64(ensureBigInt(argVal));
+    return;
+  }
+  if (argType instanceof TypeTagU128) {
+    serializer.serializeU128(ensureBigInt(argVal));
+    return;
+  }
+  if (argType instanceof TypeTagU256) {
+    serializer.serializeU256(ensureBigInt(argVal));
+    return;
+  }
+  if (argType instanceof TypeTagAddress) {
+    let addr: AccountAddress;
+    if (typeof argVal === "string" || argVal instanceof HexString) {
+      addr = AccountAddress.fromHex(argVal);
+    } else if (argVal instanceof AccountAddress) {
+      addr = argVal;
+    } else {
+      throw new Error("Invalid account address.");
+    }
+    addr.serialize(serializer);
+    return;
+  }
+  if (argType instanceof TypeTagVector) {
+    // We are serializing a vector<u8>
+    if (argType.value instanceof TypeTagU8) {
+      if (argVal instanceof Uint8Array) {
+        serializer.serializeBytes(argVal);
+        return;
+      }
+
+      if (typeof argVal === "string") {
+        serializer.serializeStr(argVal);
+        return;
+      }
+    }
+
+    if (!Array.isArray(argVal)) {
+      throw new Error("Invalid vector args.");
+    }
+
+    serializer.serializeU32AsUleb128(argVal.length);
+
+    argVal.forEach((arg) => serializeArg(arg, argType.value, serializer));
+    return;
+  }
+
+  if (argType instanceof TypeTagStruct) {
+    const { address, module_name: moduleName, name } = (argType as TypeTagStruct).value;
+    if (
+      `${HexString.fromUint8Array(address.address).toShortString()}::${moduleName.value}::${name.value}` !==
+      "0x1::string::String"
+    ) {
+      throw new Error("The only supported struct arg is of type 0x1::string::String");
+    }
+    assertType(argVal, ["string"]);
+
+    serializer.serializeStr(argVal);
+    return;
+  }
+  throw new Error("Unsupported arg type.");
+}
+
+export function argToTransactionArgument(argVal: any, argType: TypeTag): TransactionArgument {
+  if (argType instanceof TypeTagBool) {
+    return new TransactionArgumentBool(ensureBoolean(argVal));
+  }
+  if (argType instanceof TypeTagU8) {
+    return new TransactionArgumentU8(ensureNumber(argVal));
+  }
+  if (argType instanceof TypeTagU16) {
+    return new TransactionArgumentU16(ensureNumber(argVal));
+  }
+  if (argType instanceof TypeTagU32) {
+    return new TransactionArgumentU32(ensureNumber(argVal));
+  }
+  if (argType instanceof TypeTagU64) {
+    return new TransactionArgumentU64(ensureBigInt(argVal));
+  }
+  if (argType instanceof TypeTagU128) {
+    return new TransactionArgumentU128(ensureBigInt(argVal));
+  }
+  if (argType instanceof TypeTagU256) {
+    return new TransactionArgumentU256(ensureBigInt(argVal));
+  }
+  if (argType instanceof TypeTagAddress) {
+    let addr: AccountAddress;
+    if (typeof argVal === "string" || argVal instanceof HexString) {
+      addr = AccountAddress.fromHex(argVal);
+    } else if (argVal instanceof AccountAddress) {
+      addr = argVal;
+    } else {
+      throw new Error("Invalid account address.");
+    }
+    return new TransactionArgumentAddress(addr);
+  }
+  if (argType instanceof TypeTagVector && argType.value instanceof TypeTagU8) {
+    if (!(argVal instanceof Uint8Array)) {
+      throw new Error(`${argVal} should be an instance of Uint8Array`);
+    }
+    return new TransactionArgumentU8Vector(argVal);
+  }
+
+  throw new Error("Unknown type for TransactionArgument.");
+}
