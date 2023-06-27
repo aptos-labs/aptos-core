60ef5dda76e104aed9a0c60efeebbbfa817d444c
Balaji Arun <balaji@aptoslabs.com>
2023-06-27 15:15:17 -0700

    [dag] reliable broadcast node receiver (#8783)

--- consensus/src/dag/mod.rs
+++ consensus/src/dag/mod.rs
@@ -4,10 +4,13 @@
 
 mod dag_driver;
 mod dag_fetcher;
+mod dag_handler;
+mod dag_network;
 mod dag_store;
 mod reliable_broadcast;
 #[cfg(test)]
 mod tests;
 mod types;
 
+pub use dag_network::RpcHandler;
 pub use types::DAGNetworkMessage;
--- consensus/src/dag/reliable_broadcast.rs
+++ consensus/src/dag/reliable_broadcast.rs
@@ -1,15 +1,26 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::dag::types::{DAGMessage, DAGNetworkSender};
-use aptos_consensus_types::common::Author;
+use crate::{
+    dag::{
+        dag_network::{DAGNetworkSender, RpcHandler},
+        dag_store::Dag,
+        types::{Node, NodeCertificate, NodeDigest, NodeDigestSignature, TDAGMessage},
+    },
+    network::TConsensusMsg,
+};
+use anyhow::{bail, ensure};
+use aptos_consensus_types::common::{Author, Round};
+use aptos_infallible::RwLock;
+use aptos_types::{validator_signer::ValidatorSigner, validator_verifier::ValidatorVerifier};
 use futures::{stream::FuturesUnordered, StreamExt};
-use std::{future::Future, sync::Arc, time::Duration};
+use std::{collections::BTreeMap, future::Future, sync::Arc, time::Duration};
+use thiserror::Error as ThisError;
 
 pub trait BroadcastStatus {
-    type Ack: DAGMessage;
+    type Ack: TDAGMessage;
     type Aggregated;
-    type Message: DAGMessage;
+    type Message: TDAGMessage;
 
     fn add(&mut self, peer: Author, ack: Self::Ack) -> anyhow::Result<Option<Self::Aggregated>>;
 }
@@ -47,16 +58,18 @@ impl ReliableBroadcast {
                     )
                 }
             };
-            let network_message = message.into_network_message();
+            let network_message = message.into().into_network_message();
             for receiver in receivers {
                 fut.push(send_message(receiver, network_message.clone()));
             }
             while let Some((receiver, result)) = fut.next().await {
                 match result {
                     Ok(msg) => {
-                        if let Ok(ack) = S::Ack::from_network_message(msg) {
-                            if let Ok(Some(aggregated)) = aggregating.add(receiver, ack) {
-                                return aggregated;
+                        if let Ok(dag_msg) = msg.try_into() {
+                            if let Ok(ack) = S::Ack::try_from(dag_msg) {
+                                if let Ok(Some(aggregated)) = aggregating.add(receiver, ack) {
+                                    return aggregated;
+                                }
                             }
                         }
                     },
@@ -64,6 +77,110 @@ impl ReliableBroadcast {
                 }
             }
             unreachable!("Should aggregate with all responses");
+        }
+    }
+}
+
+#[derive(ThisError, Debug)]
+pub enum NodeBroadcastHandleError {
+    #[error("invalid parent in node")]
+    InvalidParent,
+    #[error("missing parents")]
+    MissingParents,
+    #[error("parents do not meet quorum voting power")]
+    NotEnoughParents,
+}
+
+pub struct NodeBroadcastHandler {
+    dag: Arc<RwLock<Dag>>,
+    signatures_by_round_peer: BTreeMap<Round, BTreeMap<Author, NodeDigestSignature>>,
+    signer: ValidatorSigner,
+    verifier: ValidatorVerifier,
+}
+
+impl NodeBroadcastHandler {
+    pub fn new(
+        dag: Arc<RwLock<Dag>>,
+        signer: ValidatorSigner,
+        verifier: ValidatorVerifier,
+    ) -> Self {
+        Self {
+            dag,
+            signatures_by_round_peer: BTreeMap::new(),
+            signer,
+            verifier,
+        }
+    }
+
+    pub fn gc_before_round(&mut self, min_round: Round) {
+        self.signatures_by_round_peer.retain(|r, _| r >= &min_round);
+    }
+
+    fn validate(&self, node: &Node) -> anyhow::Result<()> {
+        let current_round = node.metadata().round();
+        // round 0 is a special case and does not require any parents
+        if current_round == 0 {
+            return Ok(());
+        }
+
+        node.verify(&self.verifier)?;
+
+        let prev_round = current_round - 1;
+
+        let dag_reader = self.dag.read();
+        // check if the parent round is missing in the DAG
+        ensure!(
+            prev_round >= dag_reader.lowest_round(),
+            NodeBroadcastHandleError::MissingParents
+        );
+
+        // check which parents are missing in the DAG
+        let missing_parents: Vec<NodeCertificate> = node
+            .parents()
+            .iter()
+            .filter(|parent| !dag_reader.exists(parent.metadata().digest()))
+            .cloned()
+            .collect();
+        if !missing_parents.is_empty() {
+            // For each missing parent, verify their signatures and voting power
+            ensure!(
+                missing_parents.iter().all(|parent| {
+                    let node_digest = NodeDigest::new(*parent.metadata().digest());
+                    self.verifier
+                        .verify_multi_signatures(&node_digest, parent.signatures())
+                        .is_ok()
+                }),
+                NodeBroadcastHandleError::InvalidParent
+            );
+            // TODO: notify dag fetcher to fetch missing node and drop this node
+            bail!(NodeBroadcastHandleError::MissingParents);
+        }
+
+        Ok(())
+    }
+}
+
+impl RpcHandler for NodeBroadcastHandler {
+    type Request = Node;
+    type Response = NodeDigestSignature;
+
+    fn process(&mut self, node: Self::Request) -> anyhow::Result<Self::Response> {
+        self.validate(&node)?;
+
+        let signatures_by_peer = self
+            .signatures_by_round_peer
+            .entry(node.metadata().round())
+            .or_insert(BTreeMap::new());
+        // TODO(ibalajiarun): persist node before voting
+        match signatures_by_peer.get(node.metadata().author()) {
+            None => {
+                let signature = node.sign(&self.signer)?;
+                let digest_signature =
+                    NodeDigestSignature::new(node.metadata().epoch(), node.digest(), signature);
+                signatures_by_peer.insert(*node.metadata().author(), digest_signature.clone());
+                Ok(digest_signature)
+            },
+            Some(ack) => Ok(ack.clone()),
         }
     }
 }
