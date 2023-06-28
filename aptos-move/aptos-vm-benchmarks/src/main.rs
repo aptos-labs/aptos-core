cbae57f0821019ac12d3d01ec9072006420cd0c5
William Law <williamlaw.wtl@gmail.com>
2023-06-28 13:20:25 -0700

    [vm-benchmarking] execute body only based on ModuleId

--- aptos-move/aptos-vm-benchmarks/src/main.rs
+++ aptos-move/aptos-vm-benchmarks/src/main.rs
@@ -8,6 +8,7 @@ use aptos_language_e2e_tests::{account::Account, executor::FakeExecutor};
 use aptos_types::transaction::{EntryFunction, TransactionPayload};
 use clap::Parser;
 use move_binary_format::CompiledModule;
+use move_core_types::language_storage::ModuleId;
 use std::fs::read_dir;
 use std::path::PathBuf;
 use std::time::Instant;
@@ -47,7 +48,7 @@ fn generate_entry_fun_payloads(
 }
 
 //// sign transaction and return transaction status
-fn sign_txn(
+fn sign_module_txn(
     executor: &mut FakeExecutor,
     account: &Account,
     payload: TransactionPayload,
@@ -68,11 +69,18 @@ fn sign_txn(
     // apply write set to avoid LINKER_ERROR
     executor.apply_write_set(txn_output.write_set());
     let elapsed = start.elapsed();
-    println!("running time (ms): {}", elapsed.as_millis());
+    println!("running time (microseconds): {}", elapsed.as_micros());
 
     let txn_status = txn_output.status().to_owned();
     assert!(txn_output.status().status().unwrap().is_success());
     println!("txn status: {:?}", txn_status);
+}
+
+fn sign_user_txn(executor: &mut FakeExecutor, module_name: &ModuleId, function_name: &str) {
+    let start = Instant::now();
+    executor.exec_module(module_name, function_name, vec![], vec![]);
+    let elapsed = start.elapsed();
+    println!("running time (microseconds): {}", elapsed.as_micros());
 }
 
 #[derive(Parser, Debug)]
@@ -190,7 +198,7 @@ fn main() {
                 // publish package similar to create_publish_package in harness.rs
                 let module_payload = generate_module_payload(&package);
                 print!("Signing txn for module... ");
-                sign_txn(
+                sign_module_txn(
                     &mut executor,
                     &creator,
                     module_payload,
@@ -199,15 +207,25 @@ fn main() {
                 sequence_num_counter = sequence_num_counter + 1;
 
                 //// send a txn that invokes the entry function 0x{address}::{name}::benchmark
-                let entry_fun_payload =
-                    generate_entry_fun_payloads(&creator, identifier, func_identifier);
+                //let entry_fun_payload = generate_entry_fun_payloads(&creator, identifier, func_identifier);
                 print!("Signing user txn... ");
-                sign_txn(
+                /*sign_txn(
                     &mut executor,
                     &creator,
                     entry_fun_payload,
                     sequence_num_counter,
-                );
+                );*/
+                let MemberId {
+                    module_id,
+                    member_id: _function_id,
+                } = str::parse(&format!(
+                    "0x{}::{}::{}",
+                    address.to_string(),
+                    identifier,
+                    func_identifier,
+                ))
+                .unwrap();
+                sign_user_txn(&mut executor, &module_id, func_identifier);
             }
         }
     }
