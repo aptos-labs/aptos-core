2129dbec62d1c5c1b683ce52d377f903b29f5519
ngkuru <giraykuru@gmail.com>
2023-06-30 15:41:59 -0700

    use ensure for checks

--- crates/aptos-api-tester/src/main.rs
+++ crates/aptos-api-tester/src/main.rs
@@ -3,9 +3,9 @@
 
 #![forbid(unsafe_code)]
 
-use anyhow::{Context, Result};
-use aptos_api_types::U64;
-use aptos_rest_client::{Client, FaucetClient};
+use anyhow::{ensure, Context, Result};
+use aptos_api_types::{Block, HashValue, U64};
+use aptos_rest_client::{Account, Client, FaucetClient};
 use aptos_sdk::coin_client::CoinClient;
 use aptos_sdk::types::LocalAccount;
 use aptos_types::account_address::AccountAddress;
@@ -16,7 +16,7 @@ use std::str::FromStr;
 use url::Url;
 
 // global parameters (todo: make into clap)
-static HAS_FAUCET_ACCESS: bool = true;
+static HAS_FAUCET_ACCESS: bool = false;
 
 // network urls
 static DEVNET_NODE_URL: Lazy<Url> =
@@ -27,6 +27,7 @@ static TESTNET_NODE_URL: Lazy<Url> =
     Lazy::new(|| Url::parse("https://fullnode.testnet.aptoslabs.com").unwrap());
 
 // static accounts to use
+// don't send coins to TEST_ACCOUNT_1
 static TEST_ACCOUNT_1: Lazy<AccountAddress> = Lazy::new(|| {
     AccountAddress::from_hex_literal(
         "0x7fed760c508a8885e1438c90f81dea6940ad53b05527fadddd3851f53342d7c4",
@@ -50,9 +51,15 @@ static FAIL_WRONG_AUTH_KEY: &str = "Returned wrong authentication key.";
 static FAIL_WRONG_SEQ_NUMBER: &str = "Returned wrong sequence number.";
 static FAIL_WRONG_BALANCE: &str = "Returned wrong balance.";
 static FAIL_WRONG_BALANCE_AT_VERSION: &str = "Returned wrong balance at the given version.";
+static FAIL_WRONG_BLOCK_HEIGHT: &str = "Returned wrong block height.";
+static FAIL_WRONG_BLOCK_HASH: &str = "Returned wrong block hash.";
+static FAIL_WRONG_BLOCK_TIMESTAMP: &str = "Returned wrong block timestamp.";
+static FAIL_WRONG_FIRST_VERSION: &str = "Returned wrong block first version.";
+static FAIL_WRONG_LAST_VERSION: &str = "Returned wrong block last version.";
+static FAIL_WRONG_TRANSACTIONS: &str = "Returned wrong transactions.";
 static SUCCESS: &str = "success";
 
-// Calls get_account on a newly created account. Requires faucet.
+// Calls get_account on a newly created account on devnet. Requires faucet.
 async fn probe_getaccount_1() -> Result<&'static str> {
     // check faucet access
     if !HAS_FAUCET_ACCESS {
@@ -75,26 +82,33 @@ async fn probe_getaccount_1() -> Result<&'static str> {
         .get_account(giray.address())
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
-    // check authentication key
-    let expected_auth_key = giray.authentication_key();
-    let actual_auth_key = response.authentication_key;
-    if !(actual_auth_key == expected_auth_key) {
-        return Ok(FAIL_WRONG_AUTH_KEY);
-    }
+    // check account data
+    let expected_account = Account {
+        authentication_key: giray.authentication_key(),
+        sequence_number: giray.sequence_number(),
+    };
+    let actual_account = response.inner();
 
-    // check sequence number
-    let expected_seq_num = giray.sequence_number();
-    let actual_seq_num = response.sequence_number;
-    if !(actual_seq_num == expected_seq_num) {
-        return Ok(FAIL_WRONG_SEQ_NUMBER);
-    }
+    ensure!(
+        expected_account.authentication_key == actual_account.authentication_key,
+        "{} expected {}, got {}",
+        FAIL_WRONG_AUTH_KEY,
+        expected_account.authentication_key,
+        actual_account.authentication_key
+    );
+    ensure!(
+        expected_account.sequence_number == actual_account.sequence_number,
+        "{} expected {}, got {}",
+        FAIL_WRONG_SEQ_NUMBER,
+        expected_account.sequence_number,
+        actual_account.sequence_number
+    );
 
     Ok(SUCCESS)
 }
 
-// Calls get_account on a static account.
+// Calls get_account on a static account on testnet.
 async fn probe_getaccount_2() -> Result<&'static str> {
     // create the rest client
     let client = Client::new(TESTNET_NODE_URL.clone());
@@ -104,29 +118,36 @@ async fn probe_getaccount_2() -> Result<&'static str> {
         .get_account(*TEST_ACCOUNT_1)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
-    // check authentication key
-    let expected_auth_key = AuthenticationKey::from_str(
-        "7fed760c508a8885e1438c90f81dea6940ad53b05527fadddd3851f53342d7c4",
-    )
-    .context(ERROR_OTHER)?;
-    let actual_auth_key = response.authentication_key;
-    if !(actual_auth_key == expected_auth_key) {
-        return Ok(FAIL_WRONG_AUTH_KEY);
-    }
+    // check account data
+    let expected_account = Account {
+        authentication_key: AuthenticationKey::from_str(
+            "7fed760c508a8885e1438c90f81dea6940ad53b05527fadddd3851f53342d7c4",
+        )
+        .context(ERROR_OTHER)?,
+        sequence_number: 1,
+    };
+    let actual_account = response.inner();
 
-    // check sequence number
-    let expected_seq_num = 1;
-    let actual_seq_num = response.sequence_number;
-    if !(actual_seq_num == expected_seq_num) {
-        return Ok(FAIL_WRONG_SEQ_NUMBER);
-    }
+    ensure!(
+        expected_account.authentication_key == actual_account.authentication_key,
+        "{} expected {}, got {}",
+        FAIL_WRONG_AUTH_KEY,
+        expected_account.authentication_key,
+        actual_account.authentication_key
+    );
+    ensure!(
+        expected_account.sequence_number == actual_account.sequence_number,
+        "{} expected {}, got {}",
+        FAIL_WRONG_SEQ_NUMBER,
+        expected_account.sequence_number,
+        actual_account.sequence_number
+    );
 
     Ok(SUCCESS)
 }
 
-// Calls get_account_balance on a newly created account. Requires faucet.
+// Calls get_account_balance on a newly created account on devnet. Requires faucet.
 async fn probe_getaccountbalance_1() -> Result<&'static str> {
     // check faucet access
     if !HAS_FAUCET_ACCESS {
@@ -146,26 +167,33 @@ async fn probe_getaccountbalance_1() -> Result<&'static str> {
 
     // fund the account further with some random amount
     let random_number: u64 = rand::thread_rng().gen_range(50_000_000, 100_000_000);
-    faucet_client.fund(giray.address(), random_number).await?;
+    faucet_client
+        .fund(giray.address(), random_number)
+        .await
+        .context(ERROR_FAUCET_FUND)?;
 
     // ask for account balance
     let response = client
         .get_account_balance(giray.address())
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance
     let expected_balance = U64(100_000_000 + random_number);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE,
+        expected_balance,
+        actual_balance,
+    );
 
     Ok(SUCCESS)
 }
 
-// Calls get_account_balance on a static account.
+// Calls get_account_balance on a static account on testnet.
 async fn probe_getaccountbalance_2() -> Result<&'static str> {
     // create the rest client
     let client = Client::new(TESTNET_NODE_URL.clone());
@@ -175,19 +203,23 @@ async fn probe_getaccountbalance_2() -> Result<&'static str> {
         .get_account_balance(*TEST_ACCOUNT_1)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance
     let expected_balance = U64(176_767_177);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE,
+        expected_balance,
+        actual_balance,
+    );
 
     Ok(SUCCESS)
 }
 
-// Calls get_account_balance_at_version on a newly created account. Requires faucet.
+// Calls get_account_balance_at_version on a newly created account on devnet. Requires faucet.
 async fn probe_getaccountbalanceatversion_1() -> Result<&'static str> {
     // check faucet access
     if !HAS_FAUCET_ACCESS {
@@ -231,47 +263,59 @@ async fn probe_getaccountbalanceatversion_1() -> Result<&'static str> {
         .get_account_balance_at_version(giray2.address(), version - 1)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance before transaction
     let expected_balance = U64(100_000_000);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE_AT_VERSION);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE_AT_VERSION,
+        expected_balance,
+        actual_balance,
+    );
 
     // ask for account balance with the given version number
     let response = client
         .get_account_balance_at_version(giray2.address(), version)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance right after transaction
     let expected_balance = U64(100_001_000);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE_AT_VERSION);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE_AT_VERSION,
+        expected_balance,
+        actual_balance,
+    );
 
     // ask for account balance with a higher version number
     let response = client
         .get_account_balance_at_version(giray2.address(), version + 1)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance long after transaction
     let expected_balance = U64(100_001_000);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE_AT_VERSION);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE_AT_VERSION,
+        expected_balance,
+        actual_balance,
+    );
 
     Ok(SUCCESS)
 }
 
-// Calls get_account_balance_at_version on a static account.
+// Calls get_account_balance_at_version on a static account on testnet.
 async fn probe_getaccountbalanceatversion_2() -> Result<&'static str> {
     // create the rest client
     let client = Client::new(TESTNET_NODE_URL.clone());
@@ -286,51 +330,139 @@ async fn probe_getaccountbalanceatversion_2() -> Result<&'static str> {
     // check balance before funding
     let expected_balance = U64(200_000_000);
     let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE_AT_VERSION);
-    }
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE_AT_VERSION,
+        expected_balance,
+        actual_balance,
+    );
 
     // ask for account balance right after funding
     let response = client
         .get_account_balance_at_version(*TEST_ACCOUNT_1, 564_589_720)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance right after funding
     let expected_balance = U64(300_000_000);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE_AT_VERSION);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE_AT_VERSION,
+        expected_balance,
+        actual_balance,
+    );
 
     // ask for account balance long after funding
     let response = client
         .get_account_balance_at_version(*TEST_ACCOUNT_1, 564_589_721)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance right after funding
     let expected_balance = U64(300_000_000);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE_AT_VERSION);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE_AT_VERSION,
+        expected_balance,
+        actual_balance,
+    );
 
     // ask for account balance right after transfer
     let response = client
         .get_account_balance_at_version(*TEST_ACCOUNT_1, 564_591_130)
         .await
         .context(ERROR_CLIENT_RESPONSE)?;
-    let response = response.inner();
 
     // check balance right after funding
     let expected_balance = U64(176_767_177);
-    let actual_balance = response.coin.value;
-    if !(actual_balance == expected_balance) {
-        return Ok(FAIL_WRONG_BALANCE_AT_VERSION);
-    }
+    let actual_balance = response.inner().coin.value;
+
+    ensure!(
+        expected_balance == actual_balance,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BALANCE_AT_VERSION,
+        expected_balance,
+        actual_balance,
+    );
+
+    Ok(SUCCESS)
+}
+
+// Calls get_block_by_height by setting with_transactions=False on a fixed block on testnet.
+async fn probe_getblockbyheight_1() -> Result<&'static str> {
+    // create the rest client
+    let client: Client = Client::new(TESTNET_NODE_URL.clone());
+
+    // ask for block
+    let response = client
+        .get_block_by_height(98669388, false)
+        .await
+        .context(ERROR_CLIENT_RESPONSE)?;
+
+    // check block
+    let actual_block = response.inner();
+    let expected_block = Block {
+        block_height: U64(98669388),
+        block_hash: HashValue::from_str(
+            "b40362602dccfa1dccebb94adfd4c06d4b0ffe1eadb07659556744db3fe1d0e5",
+        )
+        .context(ERROR_OTHER)?,
+        block_timestamp: U64(1688146946653187),
+        first_version: U64(565538916),
+        last_version: U64(565538919),
+        transactions: None,
+    };
+
+    ensure!(
+        actual_block.block_height == expected_block.block_height,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BLOCK_HEIGHT,
+        expected_block.block_height,
+        actual_block.block_height,
+    );
+    ensure!(
+        actual_block.block_hash == expected_block.block_hash,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BLOCK_HASH,
+        expected_block.block_hash,
+        actual_block.block_hash,
+    );
+    ensure!(
+        actual_block.block_timestamp == expected_block.block_timestamp,
+        "{} expected {}, got {}",
+        FAIL_WRONG_BLOCK_TIMESTAMP,
+        expected_block.block_timestamp,
+        actual_block.block_timestamp,
+    );
+    ensure!(
+        actual_block.first_version == expected_block.first_version,
+        "{} expected {}, got {}",
+        FAIL_WRONG_FIRST_VERSION,
+        expected_block.first_version,
+        actual_block.first_version,
+    );
+    ensure!(
+        actual_block.last_version == expected_block.last_version,
+        "{} expected {}, got {}",
+        FAIL_WRONG_LAST_VERSION,
+        expected_block.last_version,
+        actual_block.last_version,
+    );
+    ensure!(
+        actual_block.transactions == expected_block.transactions,
+        "{} expected {:?}, got {:?}",
+        FAIL_WRONG_TRANSACTIONS,
+        expected_block.transactions,
+        actual_block.transactions,
+    );
 
     Ok(SUCCESS)
 }
@@ -338,27 +470,31 @@ async fn probe_getaccountbalanceatversion_2() -> Result<&'static str> {
 #[tokio::main]
 async fn main() -> Result<()> {
     match probe_getaccount_1().await {
-        Ok(result) => println!("{}", result),
-        Err(e) => println!("{}", e),
+        Ok(result) => println!("{:?}", result),
+        Err(e) => println!("{:?}", e),
     }
     match probe_getaccount_2().await {
-        Ok(result) => println!("{}", result),
-        Err(e) => println!("{}", e),
+        Ok(result) => println!("{:?}", result),
+        Err(e) => println!("{:?}", e),
     }
     match probe_getaccountbalance_1().await {
-        Ok(result) => println!("{}", result),
-        Err(e) => println!("{}", e),
+        Ok(result) => println!("{:?}", result),
+        Err(e) => println!("{:?}", e),
     }
     match probe_getaccountbalance_2().await {
-        Ok(result) => println!("{}", result),
-        Err(e) => println!("{}", e),
+        Ok(result) => println!("{:?}", result),
+        Err(e) => println!("{:?}", e),
     }
     match probe_getaccountbalanceatversion_1().await {
-        Ok(result) => println!("{}", result),
+        Ok(result) => println!("{:?}", result),
         Err(e) => println!("{:?}", e),
     }
     match probe_getaccountbalanceatversion_2().await {
-        Ok(result) => println!("{}", result),
+        Ok(result) => println!("{:?}", result),
+        Err(e) => println!("{:?}", e),
+    }
+    match probe_getblockbyheight_1().await {
+        Ok(result) => println!("{:?}", result),
         Err(e) => println!("{:?}", e),
     }
 
