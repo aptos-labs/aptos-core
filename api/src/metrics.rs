b3a0a7bbee8418029a5759bc938a9a046be69050
Brian Cho <brian@aptoslabs.com>
2023-07-03 10:26:18 -0700

    Add buckets between 100-250ms

--- api/src/metrics.rs
+++ api/src/metrics.rs
@@ -9,9 +9,11 @@ use once_cell::sync::Lazy;
 
 /// In addition to DEFAULT_BUCKETS, add histogram buckets that are < 5ms:
 /// 0.0001, 0.00025, 0.0005, 0.001, 0.0025
-const SUB_MS_BUCKETS: &[f64; 16] = &[
-    0.0001, 0.00025, 0.0005, 0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5,
-    5.0, 10.0,
+/// and some more granularity between 100-250 ms:
+/// 0.125, 0.15, 0.2
+const SUB_MS_BUCKETS: &[f64; 19] = &[
+    0.0001, 0.00025, 0.0005, 0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.125, 0.15, 0.2, 0.25,
+    0.5, 1.0, 2.5, 5.0, 10.0,
 ];
 
 pub static HISTOGRAM: Lazy<HistogramVec> = Lazy::new(|| {
