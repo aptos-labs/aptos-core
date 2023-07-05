a29f56684aecf46b3aa8fa2193c93223493d9aa2
Maayan <maayan@aptoslabs.com>
2023-07-05 09:46:34 -0700

    [TS SDK] Use client that is not generated by openAPI (#8738)
    * add new client
    
    Revert "add new client"
    
    This reverts commit 86f2d7c4c01a7e2f2879e49035fa56c18587d95f.
    
    add new client
    
    build new client
    
    write tests for the new client
    
    delete old generated client
    
    modify openapi npm command to only generate response types
    
    export new client files
    
    use new client
    
    use new client
    
    use devnet when testing indexer
    
    use devnet when testing provider
    
    remove uneeded only in tests
    
    modify aptos client node url tests
    
    modify imports
    
    remove unneeded space
    
    support override config on each request
    
    fix lint
    
    more client test file to e2e folder
    
    fix pagination with cursor and add test
    
    * support client config on IndexerClient
    
    * skip indexer tests if devnet not in sync
    
    * better client structure
    
    * paginate with cursor test change
    
    * update changelog

--- ecosystem/typescript/sdk/src/plugins/faucet_client.ts
+++ ecosystem/typescript/sdk/src/plugins/faucet_client.ts
@@ -3,15 +3,16 @@
 
 /** Faucet creates and funds accounts. This is a thin wrapper around that. */
 import { AptosClient } from "../providers/aptos_client";
-import { OpenAPIConfig } from "../generated";
-import { AxiosHttpRequest } from "../generated/core/AxiosHttpRequest";
 import { HexString, MaybeHexString, DEFAULT_TXN_TIMEOUT_SEC } from "../utils";
+import { post, ClientConfig } from "../client";
 
 /**
  * Class for requsting tokens from faucet
  */
 export class FaucetClient extends AptosClient {
-  faucetRequester: AxiosHttpRequest;
+  readonly faucetUrl: string;
+
+  readonly config: ClientConfig | undefined;
 
   /**
    * Establishes a connection to Aptos node
@@ -20,24 +21,14 @@ export class FaucetClient extends AptosClient {
    * @param config An optional config for inner axios instance
    * Detailed config description: {@link https://github.com/axios/axios#request-config}
    */
-  constructor(nodeUrl: string, faucetUrl: string, config?: Partial<OpenAPIConfig>) {
+  constructor(nodeUrl: string, faucetUrl: string, config?: ClientConfig) {
     super(nodeUrl, config);
 
     if (!faucetUrl) {
       throw new Error("Faucet URL cannot be empty.");
     }
-    // Build a requester configured to talk to the faucet.
-    this.faucetRequester = new AxiosHttpRequest({
-      BASE: faucetUrl,
-      VERSION: config?.VERSION ?? "0.1.0",
-      WITH_CREDENTIALS: config?.WITH_CREDENTIALS ?? false,
-      CREDENTIALS: config?.CREDENTIALS ?? "include",
-      TOKEN: config?.TOKEN,
-      USERNAME: config?.USERNAME,
-      PASSWORD: config?.PASSWORD,
-      HEADERS: config?.HEADERS,
-      ENCODE_PATH: config?.ENCODE_PATH,
-    });
+    this.faucetUrl = faucetUrl;
+    this.config = config;
   }
 
   /**
@@ -49,21 +40,24 @@ export class FaucetClient extends AptosClient {
    * @returns Hashes of submitted transactions
    */
   async fundAccount(address: MaybeHexString, amount: number, timeoutSecs = DEFAULT_TXN_TIMEOUT_SEC): Promise<string[]> {
-    const tnxHashes = await this.faucetRequester.request<Array<string>>({
-      method: "POST",
-      url: "/mint",
-      query: {
+    const { data } = await post<any, Array<string>>({
+      url: this.faucetUrl,
+      endpoint: "mint",
+      body: null,
+      params: {
         address: HexString.ensure(address).noPrefix(),
         amount,
       },
+      overrides: { ...this.config },
+      originMethod: "fundAccount",
     });
 
     const promises: Promise<void>[] = [];
-    for (let i = 0; i < tnxHashes.length; i += 1) {
-      const tnxHash = tnxHashes[i];
+    for (let i = 0; i < data.length; i += 1) {
+      const tnxHash = data[i];
       promises.push(this.waitForTransaction(tnxHash, { timeoutSecs }));
     }
     await Promise.all(promises);
-    return tnxHashes;
+    return data;
   }
 }
