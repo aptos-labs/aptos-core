98d5aca03b72f41c0d0cc5c94c8663a507057e84
Zekun Li <li.zekun@gmail.com>
2023-07-06 00:01:21 -0700

    [dag] commit rule content 1/n

--- consensus/src/dag/commit_rule.rs
+++ consensus/src/dag/commit_rule.rs
@@ -15,7 +15,7 @@ use std::sync::Arc;
 pub struct CommitRule {
     epoch_state: Arc<EpochState>,
     ordered_block_id: HashValue,
-    ordered_round: Round,
+    lowest_unordered_round: Round,
     dag: Arc<RwLock<Dag>>,
     anchor_election: Box<dyn AnchorElection>,
     commit_sender: UnboundedSender<OrderedBlocks>,
@@ -33,7 +33,7 @@ impl CommitRule {
         Self {
             epoch_state,
             ordered_block_id: latest_ledger_info.commit_info().id(),
-            ordered_round: latest_ledger_info.commit_info().round(),
+            lowest_unordered_round: latest_ledger_info.commit_info().round() + 1,
             dag,
             anchor_election,
             commit_sender,
@@ -42,11 +42,10 @@ impl CommitRule {
 
     pub fn new_node(&mut self, node: &CertifiedNode) {
         let round = node.round();
-        while self.ordered_round <= round {
+        while self.lowest_unordered_round <= round {
             if let Some(direct_anchor) = self.find_first_anchor_with_enough_votes(round) {
                 let commit_anchor = self.find_first_anchor_to_commit(direct_anchor);
-                let commit_nodes = self.order_anchor(commit_anchor);
-                self.push_to_execution(commit_nodes);
+                self.finalize_order(commit_anchor);
             } else {
                 break;
             }
@@ -58,20 +57,46 @@ impl CommitRule {
         target_round: Round,
     ) -> Option<Arc<CertifiedNode>> {
         let dag_reader = self.dag.read();
-        let mut current_round = self.ordered_round;
+        let mut current_round = self.lowest_unordered_round;
         while current_round < target_round {
             let anchor_author = self.anchor_election.get_anchor(current_round);
+            // I "think" it's impossible to get ordered/committed node here but to double check
+            if let Some(anchor_node) =
+                dag_reader.get_node_by_round_author(current_round, &anchor_author)
+            {
+                // f+1 or 2f+1?
+                if dag_reader
+                    .check_votes_for_node(anchor_node.metadata(), &self.epoch_state.verifier)
+                {
+                    return Some(anchor_node);
+                }
+            }
+            current_round += 2;
         }
         None
     }
 
-    pub fn find_first_anchor_to_commit(&mut self, node: Arc<CertifiedNode>) -> Arc<CertifiedNode> {
-        node
+    pub fn find_first_anchor_to_commit(
+        &mut self,
+        current_anchor: Arc<CertifiedNode>,
+    ) -> Arc<CertifiedNode> {
+        let dag_reader = self.dag.read();
+        let anchor_round = current_anchor.round();
+        let first_anchor = dag_reader
+            .reachable(&current_anchor)
+            .filter(|node| node.round() >= self.lowest_unordered_round)
+            // the same parity of the current anchor round
+            .filter(|node| (node.round() ^ anchor_round) & 1 == 0)
+            // anchor node, we can cache the election result per round
+            .filter(|node| *node.author() == self.anchor_election.get_anchor(node.round()))
+            .last()
+            .unwrap();
+        self.lowest_unordered_round = first_anchor.round() + 1;
+        first_anchor.clone()
     }
 
-    pub fn order_anchor(&mut self, anchor: Arc<CertifiedNode>) -> Vec<Arc<CertifiedNode>> {
-        vec![anchor]
+    pub fn finalize_order(&mut self, anchor: Arc<CertifiedNode>) {
+        let dag_writer = self.dag.write();
+        let _commit_nodes: Vec<_> = dag_writer.reachable(&anchor).collect();
     }
-
-    pub fn push_to_execution(&mut self, nodes: Vec<Arc<CertifiedNode>>) {}
 }
