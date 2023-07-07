9dfcf310cdda7fbf54d0a4aedbabb4c23d05f94e
danielxiangzl <daniel@aptoslabs.com>
2023-07-07 16:18:52 -0700

    increase mempool backlog

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1463,7 +1463,7 @@ fn realistic_env_max_load_test(duration: Duration) -> ForgeConfig {
         .add_network_test(wrap_with_realistic_env(TwoTrafficsTest {
             inner_traffic: EmitJobRequest::default()
                 .mode(EmitJobMode::MaxLoad {
-                    mempool_backlog: 40000,
+                    mempool_backlog: 200000,
                 })
                 .init_gas_price_multiplier(20),
             inner_success_criteria: SuccessCriteria::new(5000),
