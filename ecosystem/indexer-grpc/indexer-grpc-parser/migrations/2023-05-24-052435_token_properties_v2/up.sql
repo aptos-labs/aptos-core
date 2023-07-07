367ac8012905989df79a5701b68663760ceac39d
bowenyang007 <carlyang007@gmail.com>
2023-07-06 22:50:30 -0700

    fix side effect cause by rust upgrade

--- ecosystem/indexer-grpc/indexer-grpc-parser/migrations/2023-05-24-052435_token_properties_v2/up.sql
+++ ecosystem/indexer-grpc/indexer-grpc-parser/migrations/2023-05-24-052435_token_properties_v2/up.sql
@@ -30,3 +30,29 @@ ALTER TABLE token_ownerships_v2
 ADD COLUMN IF NOT EXISTS non_transferrable_by_owner BOOLEAN;
 ALTER TABLE current_token_ownerships_v2
 ADD COLUMN IF NOT EXISTS non_transferrable_by_owner BOOLEAN;
+-- These are needed b/c for some reason we're getting build errors when setting
+-- type field with a length limit
+ALTER TABLE signatures
+ALTER COLUMN type TYPE VARCHAR;
+ALTER TABLE token_activities_v2
+ALTER COLUMN type TYPE VARCHAR;
+DROP VIEW IF EXISTS transactions_view;
+ALTER TABLE transactions
+ALTER COLUMN type TYPE VARCHAR;
+CREATE VIEW transactions_view AS
+SELECT "version",
+  block_height,
+  "hash",
+  "type",
+  payload#>>'{}' AS json_payload,
+  state_change_hash,
+  event_root_hash,
+  state_checkpoint_hash,
+  gas_used,
+  success,
+  vm_status,
+  accumulator_root_hash,
+  num_events,
+  num_write_set_changes,
+  inserted_at
+FROM transactions;
