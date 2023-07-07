861a91c21dd49ba710dd4545116037287a83e265
yunuseozer <yunusozer@berkeley.edu>
2023-07-07 23:18:09 +0000

    thread dump

--- crates/aptos-inspection-service/src/server/memory_text.rs
+++ crates/aptos-inspection-service/src/server/memory_text.rs
@@ -9,7 +9,7 @@ use std::fs;
 
 pub fn handle_memory_txt_request() -> (StatusCode, Body, String) {
 
-    let content = fs::read_to_string("/home/yunusozer/aptos-core/crates/aptos-inspection-service/src/server/memory_profile/heap.txt").expect("Failed to read input");
+    let content = fs::read_to_string("./crates/aptos-inspection-service/src/server/memory_profile/heap.txt").expect("Failed to read input");
 
 
     (
--- crates/aptos-inspection-service/src/server/thread_dump.rs
+++ crates/aptos-inspection-service/src/server/thread_dump.rs
@@ -1,1 +1,52 @@
 // Copyright © Aptos Foundation
+
+use hyper::{Body, StatusCode};
+use crate::server::utils::CONTENT_TYPE_TEXT;
+use std::env;
+use std::process::{Command, exit};
+use std::io::Write;
+use std::fs::OpenOptions;
+use std::process;
+extern crate rstack_self;
+use std::thread;
+use std::time::Duration;
+
+pub fn handle_thread_dump_request(status: &str) -> (StatusCode, Body, String) {
+    let mut output = Vec::new();
+    for _ in 0..30 {
+        let trace = rstack_self::trace(Command::new("cargo").arg("run").arg("-p").arg("aptos-iuchild").arg("--release")).unwrap();
+
+        for thread in trace.threads() {
+            writeln!(output, "{} - {}", thread.id(), thread.name()).unwrap();
+            for frame in thread.frames() {
+                writeln!(output, "{:#016x}", frame.ip()).unwrap();
+
+                for symbol in frame.symbols() {
+                    write!(output, "    - {}", symbol.name().unwrap_or("????")).unwrap();
+                    if let Some(file) = symbol.file() {
+                        write!(output, " {}:{}", file.display(), symbol.line().unwrap_or(0)).unwrap();
+                    }
+                    writeln!(output).unwrap();
+                }
+            }
+            writeln!(output).unwrap();
+        }
+        thread::sleep(Duration::from_secs(2));
+    }
+
+    // Write the thread information to a file
+    let file_path = "thread_dump.txt";
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file_path)
+        .expect("Failed to open the file");
+
+    file.write_all(&output).expect("Failed to write to the file");
+
+    (
+        StatusCode::OK,
+        Body::from("555"),
+        CONTENT_TYPE_TEXT.into(),
+    )
+}
