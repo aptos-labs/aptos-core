6735aaf6041137ff06a26c9554e55d1599af5f7f
danielxiangzl <daniel@aptoslabs.com>
2023-07-07 17:12:48 -0700

    use throughput test

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -490,7 +490,7 @@ fn single_test_suite(test_name: &str, duration: Duration) -> Result<ForgeConfig>
     let single_test_suite = match test_name {
         // Land-blocking tests to be run on every PR:
         "land_blocking" => land_blocking_test_suite(duration), // to remove land_blocking, superseeded by the below
-        "realistic_env_max_load" => realistic_env_max_load_test(duration),
+        "realistic_env_max_load" => realistic_network_tuned_for_throughput_test(), // realistic_env_max_load_test(duration),
         "compat" => compat(),
         "framework_upgrade" => upgrade(),
         // Rest of the tests:
