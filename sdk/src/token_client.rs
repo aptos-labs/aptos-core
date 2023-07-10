51944d658c3678dad3872f67b842bd9b451ba7a5
ngkuru <giraykuru@gmail.com>
2023-07-10 16:04:21 -0700

    update create_token

--- sdk/src/token_client.rs
+++ sdk/src/token_client.rs
@@ -2,20 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    bcs,
-    move_types::{identifier::Identifier, language_storage::ModuleId},
     rest_client::{Client as ApiClient, PendingTransaction},
-    transaction_builder::{TransactionBuilder, TransactionFactory},
+    transaction_builder::TransactionFactory,
     types::{
         account_address::AccountAddress,
         chain_id::ChainId,
-        transaction::{EntryFunction, TransactionPayload},
         LocalAccount,
     },
 };
 use anyhow::{Context, Result};
 use aptos_cached_packages::aptos_token_sdk_builder::EntryFunctionCall;
-use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Clone, Debug)]
 pub struct TokenClient<'a> {
@@ -27,6 +23,18 @@ impl<'a> TokenClient<'a> {
         Self { api_client }
     }
 
+    async fn get_chain_id(&self) -> Result<ChainId> {
+        let id = self
+            .api_client
+            .get_index()
+            .await
+            .context("Failed to get chain ID")?
+            .inner()
+            .chain_id;
+
+        Ok(ChainId::new(id))
+    }
+
     pub async fn create_collection(
         &self,
         account: &mut LocalAccount,
@@ -36,19 +44,9 @@ impl<'a> TokenClient<'a> {
         max_amount: u64,
         options: Option<TransactionOptions>,
     ) -> Result<PendingTransaction> {
-        let options = options.unwrap_or_default();
-
-        // get chain id
-        let chain_id = self
-            .api_client
-            .get_index()
-            .await
-            .context("Failed to get chain ID")?
-            .inner()
-            .chain_id;
-
         // create factory
-        let factory = TransactionFactory::new(ChainId::new(chain_id))
+        let options = options.unwrap_or_default();
+        let factory = TransactionFactory::new(self.get_chain_id().await?)
             .with_gas_unit_price(options.gas_unit_price)
             .with_max_gas_amount(options.max_gas_amount)
             .with_transaction_expiration_time(options.timeout_secs);
@@ -88,60 +86,53 @@ impl<'a> TokenClient<'a> {
         supply: u64,
         uri: &str,
         max_amount: u64,
-        royalty_payee_address: &AccountAddress,
-        royalty_points_denominator: u64,
-        royalty_points_numerator: u64,
-        property_keys: Box<Vec<&str>>,
-        property_values: Box<Vec<&str>>,
-        property_types: Box<Vec<&str>>,
+        royalty_options: Option<RoyaltyOptions>,
         options: Option<TransactionOptions>,
     ) -> Result<PendingTransaction> {
+        // create factory
         let options = options.unwrap_or_default();
+        let factory = TransactionFactory::new(self.get_chain_id().await?)
+            .with_gas_unit_price(options.gas_unit_price)
+            .with_max_gas_amount(options.max_gas_amount)
+            .with_transaction_expiration_time(options.timeout_secs);
 
-        let chain_id = self
-            .api_client
-            .get_index()
-            .await
-            .context("Failed to get chain ID")?
-            .inner()
-            .chain_id;
-        let transaction_builder = TransactionBuilder::new(
-            TransactionPayload::EntryFunction(EntryFunction::new(
-                ModuleId::new(
-                    AccountAddress::from_hex_literal("0x3").unwrap(),
-                    Identifier::new("token").unwrap(),
-                ),
-                Identifier::new("create_token_script").unwrap(),
-                vec![],
-                vec![
-                    bcs::to_bytes(&collection_name).unwrap(),
-                    bcs::to_bytes(&name).unwrap(),
-                    bcs::to_bytes(&description).unwrap(),
-                    bcs::to_bytes(&supply).unwrap(),
-                    bcs::to_bytes(&max_amount).unwrap(),
-                    bcs::to_bytes(&uri).unwrap(),
-                    bcs::to_bytes(&royalty_payee_address).unwrap(),
-                    bcs::to_bytes(&royalty_points_denominator).unwrap(),
-                    bcs::to_bytes(&royalty_points_numerator).unwrap(),
-                    bcs::to_bytes(&vec![false, false, false, false, false]).unwrap(),
-                    bcs::to_bytes(property_keys.as_ref()).unwrap(),
-                    // TODO: this is wrong
-                    bcs::to_bytes(property_values.as_ref()).unwrap(),
-                    bcs::to_bytes(property_types.as_ref()).unwrap(),
-                ],
-            )),
-            SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs()
-                + options.timeout_secs,
-            ChainId::new(chain_id),
-        )
-        .sender(account.address())
-        .sequence_number(account.sequence_number())
-        .max_gas_amount(options.max_gas_amount)
-        .gas_unit_price(options.gas_unit_price);
-        let signed_txn = account.sign_with_transaction_builder(transaction_builder);
+        // set default royalty options
+        let royalty_options = match royalty_options {
+            Some(opt) => opt,
+            None => RoyaltyOptions {
+                royalty_payee_address: account.address(),
+                royalty_points_denominator: 0,
+                royalty_points_numerator: 0,
+            },
+        };
+
+        // create payload
+        let payload = EntryFunctionCall::TokenCreateTokenScript {
+            collection: collection_name.to_owned().into_bytes(),
+            name: name.to_owned().into_bytes(),
+            description: description.to_owned().into_bytes(),
+            balance: supply,
+            maximum: max_amount,
+            uri: uri.to_owned().into_bytes(),
+            royalty_payee_address: royalty_options.royalty_payee_address,
+            royalty_points_denominator: royalty_options.royalty_points_denominator,
+            royalty_points_numerator: royalty_options.royalty_points_numerator,
+            mutate_setting: vec![false, false, false, false, false],
+            // todo: add property support
+            property_keys: vec![],
+            property_values: vec![],
+            property_types: vec![],
+        }
+        .encode();
+
+        // create transaction
+        let builder = factory
+            .payload(payload)
+            .sender(account.address())
+            .sequence_number(account.sequence_number());
+        let signed_txn = account.sign_with_transaction_builder(builder);
+
+        // submit and return
         Ok(self
             .api_client
             .submit(&signed_txn)
@@ -169,4 +160,10 @@ impl Default for TransactionOptions {
             timeout_secs: 10,
         }
     }
+}
+
+pub struct RoyaltyOptions {
+    pub royalty_payee_address: AccountAddress,
+    pub royalty_points_denominator: u64,
+    pub royalty_points_numerator: u64,
 }
