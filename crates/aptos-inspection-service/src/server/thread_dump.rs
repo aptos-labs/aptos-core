0dd32e362f23d8f5665c8d90715e0dd78278a9ae
yunuseozer <yunusozer@berkeley.edu>
2023-07-11 23:48:03 +0000

    remove debug prints

--- crates/aptos-inspection-service/src/server/thread_dump.rs
+++ crates/aptos-inspection-service/src/server/thread_dump.rs
@@ -16,74 +16,13 @@ use std::fs::File;
 pub fn handle_thread_dump_request() -> (StatusCode, Body, String) {
 
 
-    println!("
-
-
-
-
-
-
-
-    sdfsdfsdf
-
-
-
-
-
-
-
-
-
-    ");
-
     let trace = rstack_self::trace(Command::new("cargo").arg("run").arg("-p").arg("aptos-iuchild").arg("--release")).unwrap();
-
-    println!("
-
-
-
-
-
-
-
-    hahahahah
-
-
-
-
-
-
-
-
-
-    ");
 
     // Open a file for writing
     let mut file = File::create("./thread_dump.txt").unwrap();
 
     // Write the trace information to the file
     write!(file, "{:#?}", trace).unwrap();
-
-    println!("
-
-
-
-
-
-
-
-    uyuyuyuyuyuy
-
-
-
-
-
-
-
-
-
-    ");
-
 
     (
         StatusCode::OK,
