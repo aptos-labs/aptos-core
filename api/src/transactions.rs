f7a88c819263682391e21f4ada6de7a6125c51ed
Satya Vusirikala <satyasatya123456@gmail.com>
2023-07-12 12:37:22 -0500

    Resetting api/src/transaction.rs

--- api/src/transactions.rs
+++ api/src/transactions.rs
@@ -1182,8 +1182,8 @@ impl TransactionsApi {
         // Simulate transaction
         let state_view = self.context.latest_state_view_poem(&ledger_info)?;
         let move_resolver = state_view.as_move_resolver();
-        let version = ledger_info.version();
         let (_, output) = AptosVM::simulate_signed_transaction(&txn, &move_resolver);
+        let version = ledger_info.version();
 
         // Ensure that all known statuses return their values in the output (even if they aren't supposed to)
         let exe_status = match output.status().clone() {
