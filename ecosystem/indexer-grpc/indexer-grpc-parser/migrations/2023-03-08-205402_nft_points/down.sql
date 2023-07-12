bd36842aef1a681185151b9fb0a5e2e2de470d31
bowenyang007 <carlyang007@gmail.com>
2023-07-11 21:20:43 -0700

    [Indexer] Migrate to grpc processors (#8841)
    * add from old indexer
    
    * fix end version
    
    * tweak comments on crash
    
    * strip out coin type in a different way
    
    * fix side effect cause by rust upgrade
    
    * cargo lock update

---
+++ ecosystem/indexer-grpc/indexer-grpc-parser/migrations/2023-03-08-205402_nft_points/down.sql
@@ -0,0 +1,5 @@
+-- This file should undo anything in `up.sql`\
+DROP TABLE IF EXISTS nft_points;
+DROP INDEX IF EXISTS np_oa_idx;
+DROP INDEX IF EXISTS np_tt_oa_idx;
+DROP INDEX IF EXISTS np_insat_idx;
