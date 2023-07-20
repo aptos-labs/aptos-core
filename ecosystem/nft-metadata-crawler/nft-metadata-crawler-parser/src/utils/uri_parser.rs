7afd5fc3deefdb10b5cb910e0a00571732940049
Justin Chang <justin@aptoslabs.com>
2023-07-20 15:33:26 -0700

    uri parser

--- ecosystem/nft-metadata-crawler/nft-metadata-crawler-parser/src/utils/uri_parser.rs
+++ ecosystem/nft-metadata-crawler/nft-metadata-crawler-parser/src/utils/uri_parser.rs
@@ -1,5 +1,9 @@
 // Copyright © Aptos Foundation
 
+use regex::Regex;
+use std::env;
+use url::Url;
+
 pub struct URIParser;
 
 impl URIParser {
@@ -7,7 +11,41 @@ impl URIParser {
      * Attempts to parse IPFS URI to use dedicated gateway.
      * Returns the original URI if parsing fails.
      */
-    pub fn parse(_uri: String) -> String {
-        todo!();
+    pub fn parse(uri: String) -> String {
+        match Self::parse_uri(uri.clone()) {
+            Ok(parsed_uri) => parsed_uri,
+            Err(_) => uri,
+        }
+    }
+
+    /**
+     * Parses a URI for IPFS CID and path using regex
+     */
+    fn parse_uri(uri: String) -> anyhow::Result<String> {
+        let modified_uri = if uri.starts_with("ipfs://") {
+            uri.replace("ipfs://", "https://ipfs.com/ipfs/")
+        } else {
+            uri.to_string()
+        };
+
+        let re = Regex::new(r"^(ipfs/)(?P<cid>[a-zA-Z0-9]+)(?P<path>/.*)?$")?;
+
+        let path = Url::parse(&modified_uri)?
+            .path_segments()
+            .map(|segments| segments.collect::<Vec<_>>().join("/"));
+
+        if let Some(captures) = re.captures(&path.unwrap_or_default()) {
+            let cid = captures["cid"].to_string();
+            let path = captures.name("path").map(|m| m.as_str().to_string());
+
+            Ok(format!(
+                "{}/{}{}",
+                env::var("IPFS_PREFIX")?,
+                cid,
+                path.unwrap_or_default()
+            ))
+        } else {
+            Err(anyhow::anyhow!("Invalid IPFS URI"))
+        }
     }
 }
