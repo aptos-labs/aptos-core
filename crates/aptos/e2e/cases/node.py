a74b23ee525bfd43f16d620bb7407e822362f0e1
Jin <jin@aptoslabs.com>
2023-07-21 11:22:48 -0700

    add more detail to e2e test

--- crates/aptos/e2e/cases/node.py
+++ crates/aptos/e2e/cases/node.py
@@ -27,3 +27,15 @@ def test_node_show_validator_set(run_helper: RunHelper, test_name=None):
         raise TestError(
             "Node show validator set command failed: Did not return scheme and active_validators"
         )
+
+    validator_0 = result.get("active_validators")[0]
+    validator_config = validator_0.get("config")
+    if (
+        validator_0.get("account_address") == None
+        or validator_config.get("consensus_public_key") == None
+        or validator_config.get("validator_network_addresses") == None
+        or validator_config.get("fullnode_network_addresses") == None
+    ):
+        raise TestError(
+            "Node show validator set command failed: Did not return account_address, consensus_public_key, or config"
+        )
