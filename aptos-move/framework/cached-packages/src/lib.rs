a2c00bbd400e2faacb58c5a7a252668ccb9ccb43
Wolfgang Grieskamp <wg@aptoslabs.com>
2023-07-21 11:05:09 -0700

    [cached packages] Put .mrb back into OUT_DIR (target)
    Too much trouble checking it in, we may come back with a different solution over time.

--- aptos-move/framework/cached-packages/src/lib.rs
+++ aptos-move/framework/cached-packages/src/lib.rs
@@ -10,11 +10,9 @@ pub mod aptos_token_objects_sdk_builder;
 pub mod aptos_token_sdk_builder;
 
 #[cfg(unix)]
-const HEAD_RELEASE_BUNDLE_BYTES: &[u8] =
-    include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/generated/head.mrb"));
+const HEAD_RELEASE_BUNDLE_BYTES: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/head.mrb"));
 #[cfg(windows)]
-const HEAD_RELEASE_BUNDLE_BYTES: &[u8] =
-    include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "\\generated\\head.mrb"));
+const HEAD_RELEASE_BUNDLE_BYTES: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "\\head.mrb"));
 
 static HEAD_RELEASE_BUNDLE: Lazy<ReleaseBundle> = Lazy::new(|| {
     bcs::from_bytes::<ReleaseBundle>(HEAD_RELEASE_BUNDLE_BYTES).expect("bcs succeeds")
