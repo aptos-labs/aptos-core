d27e17c5b903b3f1b1c4d8c3a03497fb30e2467d
Brian Cho <brian@aptoslabs.com>
2023-07-24 16:42:59 -0700

    try bumping up the mempool backlog since it's spread out so much

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1493,7 +1493,7 @@ fn realistic_env_max_load_test(
         .add_network_test(wrap_with_realistic_env(TwoTrafficsTest {
             inner_traffic: EmitJobRequest::default()
                 .mode(EmitJobMode::MaxLoad {
-                    mempool_backlog: 40000,
+                    mempool_backlog: 160000,
                 })
                 .init_gas_price_multiplier(20),
             inner_success_criteria: SuccessCriteria::new(if ha_proxy { 4600 } else { 5500 }),
