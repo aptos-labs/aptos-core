c9b79e63f3474e2c8215a8b350314a97048f6b3c
Satya Vusirikala <satyasatya123456@gmail.com>
2023-07-25 11:26:48 -0500

    Initial draft for aggregator v2 natives

---
+++ aptos-move/framework/src/natives/aggregator_natives/aggregator_v2.rs
@@ -0,0 +1,220 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{
+    natives::{
+        aggregator_natives::{helpers_v2::aggregator_info, NativeAggregatorContext},
+        helpers::{make_safe_native, SafeNativeContext, SafeNativeResult},
+    },
+    safely_pop_arg,
+};
+use aptos_types::on_chain_config::{Features, TimedFeatures};
+use move_core_types::gas_algebra::InternalGas;
+use move_vm_runtime::native_functions::NativeFunction;
+use move_vm_types::{
+    loaded_data::runtime_types::Type,
+    values::{StructRef, Value},
+};
+use smallvec::{smallvec, SmallVec};
+use std::{collections::VecDeque, sync::Arc};
+
+/***************************************************************************************************
+ * native fun try_add(aggregator: &mut Aggregator, value: u128): bool;
+ *
+ *   gas cost: base_cost
+ *
+ **************************************************************************************************/
+#[derive(Debug, Clone)]
+pub struct TryAddGasParameters {
+    pub base: InternalGas,
+}
+
+fn native_try_add(
+    gas_params: &TryAddGasParameters,
+    context: &mut SafeNativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> SafeNativeResult<SmallVec<[Value; 1]>> {
+    debug_assert_eq!(args.len(), 2);
+
+    context.charge(gas_params.base)?;
+
+    // Get aggregator information and a value to add.
+    let value = safely_pop_arg!(args, u128);
+    let (id, limit) = aggregator_info(&safely_pop_arg!(args, StructRef))?;
+
+    // Get aggregator.
+    let aggregator_context = context.extensions().get::<NativeAggregatorContext>();
+    let mut aggregator_data = aggregator_context.aggregator_data.borrow_mut();
+    let aggregator = aggregator_data.get_aggregator(
+        id,
+        limit,
+        aggregator_context.resolver,
+        aggregator_context.aggregator_enabled,
+    )?;
+
+    Ok(smallvec![Value::bool(aggregator.try_add(value).is_ok())])
+}
+
+/***************************************************************************************************
+ * native fun try_sub(aggregator: &mut Aggregator, value: u128): bool;
+ *
+ *   gas cost: base_cost
+ *
+ **************************************************************************************************/
+#[derive(Debug, Clone)]
+pub struct TrySubGasParameters {
+    pub base: InternalGas,
+}
+
+fn native_try_sub(
+    gas_params: &TrySubGasParameters,
+    context: &mut SafeNativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> SafeNativeResult<SmallVec<[Value; 1]>> {
+    debug_assert_eq!(args.len(), 2);
+
+    context.charge(gas_params.base)?;
+
+    // Get aggregator information and a value to subtract.
+    let value = safely_pop_arg!(args, u128);
+    let (id, limit) = aggregator_info(&safely_pop_arg!(args, StructRef))?;
+
+    // Get aggregator.
+    let aggregator_context = context.extensions().get::<NativeAggregatorContext>();
+    let mut aggregator_data = aggregator_context.aggregator_data.borrow_mut();
+    let aggregator = aggregator_data.get_aggregator(
+        id,
+        limit,
+        aggregator_context.resolver,
+        aggregator_context.aggregator_enabled,
+    )?;
+    Ok(smallvec![Value::bool(aggregator.try_sub(value).is_ok())])
+}
+
+/***************************************************************************************************
+ * native fun read(aggregator: &Aggregator): u128;
+ *
+ *   gas cost: base_cost
+ *
+ **************************************************************************************************/
+#[derive(Debug, Clone)]
+pub struct ReadGasParameters {
+    pub base: InternalGas,
+}
+
+fn native_read(
+    gas_params: &ReadGasParameters,
+    context: &mut SafeNativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> SafeNativeResult<SmallVec<[Value; 1]>> {
+    debug_assert_eq!(args.len(), 1);
+
+    context.charge(gas_params.base)?;
+
+    // Extract information from aggregator struct reference.
+    let (id, limit) = aggregator_info(&safely_pop_arg!(args, StructRef))?;
+
+    // Get aggregator.
+    let aggregator_context = context.extensions().get::<NativeAggregatorContext>();
+    let mut aggregator_data = aggregator_context.aggregator_data.borrow_mut();
+    let aggregator = aggregator_data.get_aggregator(
+        id,
+        limit,
+        aggregator_context.resolver,
+        aggregator_context.aggregator_enabled,
+    )?;
+
+    let value = aggregator.read_and_materialize(aggregator_context.resolver, &id)?;
+
+    Ok(smallvec![Value::u128(value)])
+}
+
+/***************************************************************************************************
+ * native fun destroy(aggregator: Aggregator);
+ *
+ *   gas cost: base_cost
+ *
+ **************************************************************************************************/
+#[derive(Debug, Clone)]
+pub struct DestroyGasParameters {
+    pub base: InternalGas,
+}
+
+fn native_destroy(
+    gas_params: &DestroyGasParameters,
+    context: &mut SafeNativeContext,
+    _ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> SafeNativeResult<SmallVec<[Value; 1]>> {
+    debug_assert_eq!(args.len(), 1);
+
+    context.charge(gas_params.base)?;
+
+    // Extract information from aggregator struct reference.
+    let (id, _limit) = aggregator_info(&safely_pop_arg!(args, StructRef))?;
+
+    // Get aggregator data.
+    let aggregator_context = context.extensions().get::<NativeAggregatorContext>();
+    let mut aggregator_data = aggregator_context.aggregator_data.borrow_mut();
+
+    // Actually remove the aggregator.
+    aggregator_data.remove_aggregator(id);
+
+    Ok(smallvec![])
+}
+
+/***************************************************************************************************
+ * module
+ *
+ **************************************************************************************************/
+#[derive(Debug, Clone)]
+pub struct GasParameters {
+    pub try_add: TryAddGasParameters,
+    pub read: ReadGasParameters,
+    pub try_sub: TrySubGasParameters,
+    pub destroy: DestroyGasParameters,
+}
+
+pub fn make_all(
+    gas_params: GasParameters,
+    timed_features: TimedFeatures,
+    features: Arc<Features>,
+) -> impl Iterator<Item = (String, NativeFunction)> {
+    let natives = [
+        (
+            "try_add",
+            make_safe_native(
+                gas_params.try_add,
+                timed_features.clone(),
+                features.clone(),
+                native_try_add,
+            ),
+        ),
+        (
+            "read",
+            make_safe_native(
+                gas_params.read,
+                timed_features.clone(),
+                features.clone(),
+                native_read,
+            ),
+        ),
+        (
+            "try_sub",
+            make_safe_native(
+                gas_params.try_sub,
+                timed_features.clone(),
+                features.clone(),
+                native_try_sub,
+            ),
+        ),
+        (
+            "destroy",
+            make_safe_native(gas_params.destroy, timed_features, features, native_destroy),
+        ),
+    ];
+    crate::natives::helpers::make_module_natives(natives)
+}
---
+++ aptos-move/framework/src/natives/aggregator_natives/helpers_v2.rs
@@ -0,0 +1,35 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use aptos_aggregator::aggregator_extension::AggregatorID;
+use move_binary_format::errors::PartialVMResult;
+use move_vm_types::values::{Reference, StructRef, Value};
+
+/// Indices of `value` and `limit` fields in the `Aggregator` Move
+/// struct.
+const VALUE_FIELD_INDEX: usize = 0;
+const LIMIT_FIELD_INDEX: usize = 1;
+
+/// Given a reference to `Aggregator` Move struct returns a field value at `index`.
+pub(crate) fn get_aggregator_field(aggregator: &StructRef, index: usize) -> PartialVMResult<Value> {
+    let field_ref = aggregator.borrow_field(index)?.value_as::<Reference>()?;
+    field_ref.read_ref()
+}
+
+/// Returns ID and a limit of aggrgegator based on a reference to `Aggregator` Move struct.
+pub(crate) fn aggregator_info(aggregator: &StructRef) -> PartialVMResult<(AggregatorID, u128)> {
+    let (value, limit) = get_aggregator_fields(aggregator)?;
+    assert!(
+        value & 0xFFFFFFFFFFFFFFFF == 0,
+        "value in aggregator exceeds u64::MAX"
+    );
+    Ok((AggregatorID::ephemeral(value as u64), limit))
+}
+
+/// Given a reference to `Aggregator` Move struct, returns a tuple of its
+/// fields: (`handle`, `key`, `limit`).
+pub fn get_aggregator_fields(aggregator: &StructRef) -> PartialVMResult<(u128, u128)> {
+    let value = get_aggregator_field(aggregator, VALUE_FIELD_INDEX)?.value_as::<u128>()?;
+    let limit = get_aggregator_field(aggregator, LIMIT_FIELD_INDEX)?.value_as::<u128>()?;
+    Ok((value, limit))
+}
