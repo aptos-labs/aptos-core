2f795cf065bf4fb9f6a246178abc1590acecaae9
Balaji Arun <balaji@aptoslabs.com>
2023-07-26 19:27:42 -0700

    [rb] Generic Reliable Broadcast module (#9295)

--- consensus/src/dag/tests/reliable_broadcast_tests.rs
+++ consensus/src/dag/tests/reliable_broadcast_tests.rs
@@ -2,184 +2,26 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::dag::{
-    dag_network::{DAGNetworkSender, RpcWithFallback},
     dag_store::Dag,
     reliable_broadcast::{
-        BroadcastStatus, CertifiedNodeHandleError, CertifiedNodeHandler, NodeBroadcastHandleError,
-        NodeBroadcastHandler, ReliableBroadcast,
+        CertifiedNodeHandleError, CertifiedNodeHandler, NodeBroadcastHandleError,
+        NodeBroadcastHandler,
     },
     storage::DAGStorage,
     tests::{
         dag_test::MockStorage,
         helpers::{new_certified_node, new_node},
     },
-    types::{CertifiedAck, DAGMessage, NodeCertificate, TestAck, TestMessage},
+    types::{CertifiedAck, NodeCertificate},
     NodeId, RpcHandler, Vote,
 };
-use anyhow::bail;
-use aptos_consensus_types::common::Author;
-use aptos_infallible::{Mutex, RwLock};
+use aptos_infallible::RwLock;
 use aptos_types::{
     aggregate_signature::PartialSignatures, epoch_state::EpochState,
     validator_verifier::random_validator_verifier,
 };
-use async_trait::async_trait;
 use claims::{assert_ok, assert_ok_eq};
-use futures::{
-    future::{AbortHandle, Abortable},
-    FutureExt,
-};
-use std::{
-    collections::{hash_map::Entry, BTreeMap, HashMap, HashSet},
-    sync::Arc,
-    time::Duration,
-};
-use tokio::sync::oneshot;
-
-struct TestBroadcastStatus {
-    threshold: usize,
-    received: HashSet<Author>,
-}
-
-impl BroadcastStatus for TestBroadcastStatus {
-    type Ack = TestAck;
-    type Aggregated = HashSet<Author>;
-    type Message = TestMessage;
-
-    fn add(&mut self, peer: Author, _ack: Self::Ack) -> anyhow::Result<Option<Self::Aggregated>> {
-        self.received.insert(peer);
-        if self.received.len() == self.threshold {
-            Ok(Some(self.received.clone()))
-        } else {
-            Ok(None)
-        }
-    }
-}
-
-struct TestDAGSender {
-    failures: Mutex<HashMap<Author, u8>>,
-    received: Mutex<HashMap<Author, TestMessage>>,
-}
-
-impl TestDAGSender {
-    fn new(failures: HashMap<Author, u8>) -> Self {
-        Self {
-            failures: Mutex::new(failures),
-            received: Mutex::new(HashMap::new()),
-        }
-    }
-}
-
-#[async_trait]
-impl DAGNetworkSender for TestDAGSender {
-    async fn send_rpc(
-        &self,
-        receiver: Author,
-        message: DAGMessage,
-        _timeout: Duration,
-    ) -> anyhow::Result<DAGMessage> {
-        match self.failures.lock().entry(receiver) {
-            Entry::Occupied(mut entry) => {
-                let count = entry.get_mut();
-                *count -= 1;
-                if *count == 0 {
-                    entry.remove();
-                }
-                bail!("simulated failure");
-            },
-            Entry::Vacant(_) => (),
-        };
-        let message: TestMessage = message.try_into()?;
-        self.received.lock().insert(receiver, message.clone());
-        Ok(TestAck(message.0).into())
-    }
-
-    async fn send_rpc_with_fallbacks(
-        &self,
-        _responders: Vec<Author>,
-        _message: DAGMessage,
-        _retry_timeout: Duration,
-        _rpc_timeout: Duration,
-    ) -> RpcWithFallback {
-        unimplemented!();
-    }
-}
-
-#[tokio::test]
-async fn test_reliable_broadcast() {
-    let (_, validator_verifier) = random_validator_verifier(5, None, false);
-    let validators = validator_verifier.get_ordered_account_addresses();
-    let failures = HashMap::from([(validators[0], 1), (validators[2], 3)]);
-    let sender = Arc::new(TestDAGSender::new(failures));
-    let rb = ReliableBroadcast::new(validators.clone(), sender);
-    let message = TestMessage(vec![42; validators.len() - 1]);
-    let aggregating = TestBroadcastStatus {
-        threshold: validators.len(),
-        received: HashSet::new(),
-    };
-    let fut = rb.broadcast::<TestBroadcastStatus>(message, aggregating);
-    assert_eq!(fut.await, validators.into_iter().collect());
-}
-
-#[tokio::test]
-async fn test_chaining_reliable_broadcast() {
-    let (_, validator_verifier) = random_validator_verifier(5, None, false);
-    let validators = validator_verifier.get_ordered_account_addresses();
-    let failures = HashMap::from([(validators[0], 1), (validators[2], 3)]);
-    let sender = Arc::new(TestDAGSender::new(failures));
-    let rb = ReliableBroadcast::new(validators.clone(), sender);
-    let message = TestMessage(vec![42; validators.len()]);
-    let expected = validators.iter().cloned().collect();
-    let aggregating = TestBroadcastStatus {
-        threshold: validators.len(),
-        received: HashSet::new(),
-    };
-    let fut = rb
-        .broadcast::<TestBroadcastStatus>(message.clone(), aggregating)
-        .then(|aggregated| async move {
-            assert_eq!(aggregated, expected);
-            let aggregating = TestBroadcastStatus {
-                threshold: validator_verifier.len(),
-                received: HashSet::new(),
-            };
-            rb.broadcast::<TestBroadcastStatus>(message, aggregating)
-                .await
-        });
-    assert_eq!(fut.await, validators.into_iter().collect());
-}
-
-#[tokio::test]
-async fn test_abort_reliable_broadcast() {
-    let (_, validator_verifier) = random_validator_verifier(5, None, false);
-    let validators = validator_verifier.get_ordered_account_addresses();
-    let failures = HashMap::from([(validators[0], 1), (validators[2], 3)]);
-    let sender = Arc::new(TestDAGSender::new(failures));
-    let rb = ReliableBroadcast::new(validators.clone(), sender);
-    let message = TestMessage(vec![42; validators.len()]);
-    let (tx, rx) = oneshot::channel();
-    let (abort_handle, abort_registration) = AbortHandle::new_pair();
-    let aggregating = TestBroadcastStatus {
-        threshold: validators.len(),
-        received: HashSet::new(),
-    };
-    let fut = Abortable::new(
-        rb.broadcast::<TestBroadcastStatus>(message.clone(), aggregating)
-            .then(|_| async move {
-                let aggregating = TestBroadcastStatus {
-                    threshold: validators.len(),
-                    received: HashSet::new(),
-                };
-                let ret = rb
-                    .broadcast::<TestBroadcastStatus>(message, aggregating)
-                    .await;
-                tx.send(ret)
-            }),
-        abort_registration,
-    );
-    tokio::spawn(fut);
-    abort_handle.abort();
-    assert!(rx.await.is_err());
-}
+use std::{collections::BTreeMap, sync::Arc};
 
 #[tokio::test]
 async fn test_node_broadcast_receiver_succeed() {
