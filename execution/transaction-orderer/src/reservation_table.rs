8269cb7b6d75c0d9fb3306c9e15f426c66c7a96e
Andrei Tonkikh <andrei@aptoslabs.com>
2023-07-31 19:17:20 +0400

    Replace BTreeSet with queues

--- execution/transaction-orderer/src/reservation_table.rs
+++ execution/transaction-orderer/src/reservation_table.rs
@@ -1,13 +1,7 @@
 // Copyright © Aptos Foundation
 
-use std::collections::{BTreeSet, HashMap};
+use std::collections::{VecDeque, HashMap, HashSet};
 use std::hash::Hash;
-
-#[derive(Default)]
-struct TableEntry<Id> {
-    reservations: BTreeSet<Id>,
-    requests: BTreeSet<Id>,
-}
 
 /// For each key, maintains a set of reservations and a set of pending requests,
 /// each identified by a transaction ID.
@@ -92,6 +86,13 @@ pub trait ReservationTable {
     }
 }
 
+#[derive(Default)]
+struct TableEntry<Id> {
+    reservations: VecDeque<Id>,
+    requests: VecDeque<Id>,
+    delayed_removes: HashSet<Id>,
+}
+
 pub struct HashMapReservationTable<K, Id>(HashMap<K, TableEntry<Id>>);
 
 impl<K, Id> Default for HashMapReservationTable<K, Id> {
@@ -103,22 +104,22 @@ impl<K, Id> Default for HashMapReservationTable<K, Id> {
 impl<K, Id> ReservationTable for HashMapReservationTable<K, Id>
 where
     K: Ord + Hash + Eq + Clone,
-    Id: Ord + Eq + Default + Copy,
+    Id: Ord + Eq + Hash + Default + Copy,
 {
     type Key = K;
     type TxnId = Id;
 
     fn make_reservation(&mut self, idx: Id, key: &K) {
-        self.0
-            .entry(key.clone())
-            .or_default()
-            .reservations
-            .insert(idx);
+        let reservations = &mut self.0.entry(key.clone()).or_default().reservations;
+        debug_assert!(reservations.back().map_or(true, |&last| last <= idx));
+        reservations.push_back(idx);
     }
 
     fn make_request(&mut self, idx: Id, key: &K) -> bool {
         if !self.is_satisfied(idx, key) {
-            self.0.get_mut(key).unwrap().requests.insert(idx);
+            let requests = &mut self.0.get_mut(key).unwrap().requests;
+            debug_assert!(requests.back().map_or(true, |&last| last <= idx));
+            self.0.get_mut(key).unwrap().requests.push_back(idx);
             true
         } else {
             false
@@ -127,15 +128,23 @@ where
 
     fn is_satisfied(&self, idx: Id, request_key: &K) -> bool {
         let Some(entry) = self.0.get(&request_key) else { return true };
-        let Some(&first_reservation) = entry.reservations.first() else { return true };
+        let Some(&first_reservation) = entry.reservations.front() else { return true };
         first_reservation >= idx
     }
 
     fn remove_reservation(&mut self, idx: Id, key: &K) -> Vec<(Id, K)> {
         let entry = self.0.get_mut(key).unwrap();
-        entry.reservations.remove(&idx);
+        entry.delayed_removes.insert(idx);
 
-        match entry.reservations.first() {
+        while let Some(&first_reservation) = entry.reservations.front() {
+            if !entry.delayed_removes.contains(&first_reservation) {
+                break;
+            }
+            entry.delayed_removes.remove(&first_reservation);
+            entry.reservations.pop_front();
+        }
+
+        match entry.reservations.front() {
             None => {
                 let res = entry
                     .requests
@@ -147,11 +156,14 @@ where
             },
             Some(first_reservation) => {
                 let mut res = Vec::new();
-                while !entry.requests.is_empty()
-                    && entry.requests.first().unwrap() <= first_reservation
-                {
-                    res.push((entry.requests.pop_first().unwrap(), key.clone()));
+
+                while let Some(&first_request) = entry.requests.front() {
+                    if first_request > *first_reservation {
+                        break;
+                    }
+                    res.push((entry.requests.pop_front().unwrap(), key.clone()));
                 }
+
                 res
             },
         }
