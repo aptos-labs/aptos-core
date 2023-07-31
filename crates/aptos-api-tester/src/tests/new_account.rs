ad2dcb105bb7c5fc4600bb793d7cbefae0d63aa8
ngkuru <giraykuru@gmail.com>
2023-07-31 12:47:23 -0700

    make new account test persistent

--- crates/aptos-api-tester/src/tests/new_account.rs
+++ crates/aptos-api-tester/src/tests/new_account.rs
@@ -1,68 +1,133 @@
 // Copyright © Aptos Foundation
 
-use crate::utils::{TestFailure, NetworkName, get_client, get_faucet_client, create_and_fund_account};
+use crate::utils::{get_client, get_faucet_client, NetworkName, TestFailure};
+use anyhow::anyhow;
 use aptos_api_types::U64;
 use aptos_logger::info;
-use aptos_rest_client::{Account, Client};
+use aptos_rest_client::{Account, Client, FaucetClient};
 use aptos_sdk::types::LocalAccount;
+use futures::Future;
+use std::time::Duration;
+use tokio::time::Instant;
 
-static FAIL_ACCOUNT_DATA: &str = "wrong account data";
-static FAIL_BALANCE: &str = "wrong balance";
+static FAIL_WRONG_ACCOUNT_DATA: &str = "wrong account data";
+static FAIL_WRONG_BALANCE: &str = "wrong balance";
+static ERROR_COULD_NOT_CREATE_ACCOUNT: &str = "faucet client failed to create account";
+static ERROR_COULD_NOT_CHECK: &str = "persistency check never started";
+static ERROR_NO_ACCOUNT_DATA: &str = "can't find account data";
 
 /// Tests new account creation. Checks that:
 ///   - account data exists
 ///   - account balance reflects funded amount
-pub async fn test_newaccount(
-    client: &Client,
-    account: &LocalAccount,
-    amount_funded: u64,
-) -> Result<(), TestFailure> {
-    // ask for account data
-    let response = client.get_account(account.address()).await?;
+pub async fn test_newaccount(network_name: NetworkName) -> Result<(), TestFailure> {
+    // setup
+    let (client, faucet_client, account) = setup(network_name).await?;
 
-    // check account data
-    let expected_account = Account {
+    // check account data persistently
+    persistent_check(check_account_data, &client, &account).await?;
+
+    // fund account
+    faucet_client.fund(account.address(), 100_000_000).await?;
+
+    // check account balance persistently
+    persistent_check(check_account_balance, &client, &account).await?;
+
+    Ok(())
+}
+
+async fn persistent_check<'a, 'b, F, Fut>(
+    f: F,
+    client: &'a Client,
+    account: &'b LocalAccount,
+) -> Result<(), TestFailure>
+where
+    F: Fn(&'a Client, &'b LocalAccount) -> Fut,
+    Fut: Future<Output = Result<(), TestFailure>>,
+{
+    // set a default error in case checks never start
+    let mut result: Result<(), TestFailure> = Err(anyhow!(ERROR_COULD_NOT_CHECK).into());
+    let timer = Instant::now();
+
+    // try to get a good result for 30 seconds
+    while Instant::now().duration_since(timer) < Duration::from_secs(30) {
+        result = f(client, account).await;
+        if result.is_ok() {
+            break;
+        }
+    }
+
+    // return last failure if no good result occurs
+    result
+}
+
+async fn setup(
+    network_name: NetworkName,
+) -> Result<(Client, FaucetClient, LocalAccount), TestFailure> {
+    // spin up clients
+    let client = get_client(network_name);
+    let faucet_client = get_faucet_client(network_name);
+
+    // create account
+    let account = LocalAccount::generate(&mut rand::rngs::OsRng);
+    if let Err(e) = faucet_client.create_account(account.address()).await {
+        info!("test: new_account part: setup ERROR: {}, with error {:?}", ERROR_COULD_NOT_CREATE_ACCOUNT, e);
+        return Err(e.into());
+    };
+
+    Ok((client, faucet_client, account))
+}
+
+async fn check_account_data(client: &Client, account: &LocalAccount) -> Result<(), TestFailure> {
+    // expected
+    let expected = Account {
         authentication_key: account.authentication_key(),
         sequence_number: account.sequence_number(),
     };
-    let actual_account = response.inner();
 
-    if &expected_account != actual_account {
+    // actual
+    let actual = match client.get_account(account.address()).await {
+        Ok(response) => response.into_inner(),
+        Err(e) => {
+            info!(
+                "test: new_account part: check_account_data ERROR: {}, with error {:?}",
+                ERROR_NO_ACCOUNT_DATA, e
+            );
+            return Err(e.into());
+        },
+    };
+
+    // compare
+    if expected != actual {
         info!(
-            "fail: {}, expected {:?}, got {:?}",
-            FAIL_ACCOUNT_DATA, expected_account, actual_account
+            "test: new_account part: check_account_data FAIL: {}, expected {:?}, got {:?}",
+            FAIL_WRONG_ACCOUNT_DATA, expected, actual
         );
-        return Err(TestFailure::Fail(FAIL_ACCOUNT_DATA));
-    }
-
-    // check account balance
-    let expected_balance = U64(amount_funded);
-    let actual_balance = client
-        .get_account_balance(account.address())
-        .await?
-        .inner()
-        .coin
-        .value;
-
-    if expected_balance != actual_balance {
-        info!(
-            "fail: {}, expected {:?}, got {:?}",
-            FAIL_BALANCE, expected_balance, actual_balance
-        );
-        return Err(TestFailure::Fail(FAIL_BALANCE));
+        return Err(TestFailure::Fail(FAIL_WRONG_ACCOUNT_DATA));
     }
 
     Ok(())
 }
 
-pub async fn setup_and_run_newaccount(network_name: NetworkName) -> Result<(), TestFailure> {
-    // spin up clients
-    let client = get_client(network_name);
-    let faucet_client = get_faucet_client(network_name);
+async fn check_account_balance(client: &Client, account: &LocalAccount) -> Result<(), TestFailure> {
+    // expected
+    let expected = U64(100_000_000);
 
-    // create and fund account
-    let account = create_and_fund_account(&faucet_client).await?;
+    // actual
+    let actual = client
+        .get_account_balance(account.address())
+        .await?
+        .into_inner()
+        .coin
+        .value;
 
-    // run test
-    test_newaccount(&client, &account, 100_000_000).await
+    // compare
+    if expected != actual {
+        info!(
+            "fail: {}, expected {:?}, got {:?}",
+            FAIL_WRONG_BALANCE, expected, actual
+        );
+        return Err(TestFailure::Fail(FAIL_WRONG_BALANCE));
+    }
+
+    Ok(())
 }
