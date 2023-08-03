bbd85e05ab2165bece03ca9be617033442fdb59c
ngkuru <giraykuru@gmail.com>
2023-08-03 13:00:30 -0700

    cleanup

--- crates/aptos-api-tester/src/counters.rs
+++ crates/aptos-api-tester/src/counters.rs
@@ -7,55 +7,50 @@ pub static API_TEST_SUCCESS: Lazy<HistogramVec> = Lazy::new(|| {
     register_histogram_vec!(
         "api_test_success",
         "Number of user flows which succesfully passed",
-        &["test_name", "network_name", "start_time"],
+        &["test_name", "network_name", "run_id"],
     )
     .unwrap()
 });
 
-pub fn test_success(test_name: &str, network_name: &str, start_time: &str) -> Histogram {
-    API_TEST_SUCCESS.with_label_values(&[test_name, network_name, start_time])
+pub fn test_success(test_name: &str, network_name: &str, run_id: &str) -> Histogram {
+    API_TEST_SUCCESS.with_label_values(&[test_name, network_name, run_id])
 }
 
 pub static API_TEST_FAIL: Lazy<HistogramVec> = Lazy::new(|| {
     register_histogram_vec!(
         "api_test_fail",
         "Number of user flows which failed checks",
-        &["test_name", "network_name", "start_time"],
+        &["test_name", "network_name", "run_id"],
     )
     .unwrap()
 });
 
-pub fn test_fail(test_name: &str, network_name: &str, start_time: &str) -> Histogram {
-    API_TEST_FAIL.with_label_values(&[test_name, network_name, start_time])
+pub fn test_fail(test_name: &str, network_name: &str, run_id: &str) -> Histogram {
+    API_TEST_FAIL.with_label_values(&[test_name, network_name, run_id])
 }
 
 pub static API_TEST_ERROR: Lazy<HistogramVec> = Lazy::new(|| {
     register_histogram_vec!("api_test_error", "Number of user flows which crashed", &[
         "test_name",
         "network_name",
-        "start_time"
+        "run_id"
     ],)
     .unwrap()
 });
 
-pub fn test_error(test_name: &str, network_name: &str, start_time: &str) -> Histogram {
-    API_TEST_ERROR.with_label_values(&[test_name, network_name, start_time])
+pub fn test_error(test_name: &str, network_name: &str, run_id: &str) -> Histogram {
+    API_TEST_ERROR.with_label_values(&[test_name, network_name, run_id])
 }
 
 pub static API_TEST_LATENCY: Lazy<HistogramVec> = Lazy::new(|| {
     register_histogram_vec!(
         "api_test_latency",
         "Time it takes to complete a user flow",
-        &["test_name", "network_name", "start_time", "result"],
+        &["test_name", "network_name", "run_id", "result"],
     )
     .unwrap()
 });
 
-pub fn test_latency(
-    test_name: &str,
-    network_name: &str,
-    start_time: &str,
-    result: &str,
-) -> Histogram {
-    API_TEST_LATENCY.with_label_values(&[test_name, network_name, start_time, result])
+pub fn test_latency(test_name: &str, network_name: &str, run_id: &str, result: &str) -> Histogram {
+    API_TEST_LATENCY.with_label_values(&[test_name, network_name, run_id, result])
 }
