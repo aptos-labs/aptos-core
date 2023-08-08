9a4928f73fddc853a18190b3dac2647f2ecae777
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2023-08-08 11:56:59 -0700

    [NFT Metadata Crawler] Use separate DB schema (#9539)

--- ecosystem/nft-metadata-crawler-parser/src/schema.rs
+++ ecosystem/nft-metadata-crawler-parser/src/schema.rs
@@ -2,17 +2,19 @@
 
 // @generated automatically by Diesel CLI.
 
-diesel::table! {
-    nft_metadata_crawler_uris (token_uri) {
-        token_uri -> Varchar,
-        raw_image_uri -> Nullable<Varchar>,
-        raw_animation_uri -> Nullable<Varchar>,
-        cdn_json_uri -> Nullable<Varchar>,
-        cdn_image_uri -> Nullable<Varchar>,
-        cdn_animation_uri -> Nullable<Varchar>,
-        json_parser_retry_count -> Int4,
-        image_optimizer_retry_count -> Int4,
-        animation_optimizer_retry_count -> Int4,
-        inserted_at -> Timestamp,
+pub mod nft_metadata_crawler {
+    diesel::table! {
+        nft_metadata_crawler.parsed_token_uris (token_uri) {
+            token_uri -> Varchar,
+            raw_image_uri -> Nullable<Varchar>,
+            raw_animation_uri -> Nullable<Varchar>,
+            cdn_json_uri -> Nullable<Varchar>,
+            cdn_image_uri -> Nullable<Varchar>,
+            cdn_animation_uri -> Nullable<Varchar>,
+            json_parser_retry_count -> Int4,
+            image_optimizer_retry_count -> Int4,
+            animation_optimizer_retry_count -> Int4,
+            inserted_at -> Timestamp,
+        }
     }
 }
