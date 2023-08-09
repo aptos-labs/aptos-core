3fd05c5ff49cae250e477f84d2210f7fda0583df
Junkil Park <juki14@gmail.com>
2023-08-09 08:52:49 -0700

    Added the gas fee payer React example that works with Petra

---
+++ ecosystem/typescript/sdk/examples/typescript/gas_fee_payer_wallet/client/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
+
+const wallets = [new PetraWallet()];
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+    <App />
+  </AptosWalletAdapterProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
