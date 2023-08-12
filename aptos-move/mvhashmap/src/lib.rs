579a04e6a1cc0ca68bf66ea50b0b92d03138ebc3
Rati Gelashvili <gelash@users.noreply.github.com>
2023-08-12 11:31:54 -0400

    Multi-versioned data structure specifically for aggregators / aggregator snapshots (#9472)

--- aptos-move/mvhashmap/src/lib.rs
+++ aptos-move/mvhashmap/src/lib.rs
@@ -16,6 +16,7 @@ use std::{fmt::Debug, hash::Hash};
 pub mod types;
 pub mod unsync_map;
 mod utils;
+pub mod versioned_aggregators;
 pub mod versioned_data;
 pub mod versioned_group_data;
 pub mod versioned_modules;
