419abd58a6a0c6934039ead936ce932308f474bb
George Mitenkov <georgemitenk0v@gmail.com>
2023-08-13 16:03:13 +0200

    Moved is_resource_group to API

--- aptos-move/aptos-vm/src/move_vm_ext/resolver.rs
+++ aptos-move/aptos-vm/src/move_vm_ext/resolver.rs
@@ -5,7 +5,6 @@ use aptos_framework::natives::state_storage::StateStorageUsageResolver;
 use aptos_state_view::StateView;
 use aptos_table_natives::TableResolver;
 use aptos_types::on_chain_config::ConfigStorage;
-use aptos_utils::aptos_try;
 use move_binary_format::errors::VMResult;
 use move_core_types::{
     account_address::AccountAddress, language_storage::StructTag, resolver::MoveResolver,
@@ -30,20 +29,6 @@ pub trait MoveResolverExt:
     fn release_resource_group_cache(
         &self,
     ) -> BTreeMap<AccountAddress, BTreeMap<StructTag, BTreeMap<StructTag, Vec<u8>>>>;
-
-    // Move to API does not belong here
-    fn is_resource_group(&self, struct_tag: &StructTag) -> bool {
-        aptos_try!({
-            let md =
-                aptos_framework::get_metadata(&self.get_module_metadata(&struct_tag.module_id()))?;
-            md.struct_attributes
-                .get(struct_tag.name.as_ident_str().as_str())?
-                .iter()
-                .find(|attr| attr.is_resource_group())?;
-            Some(())
-        })
-        .is_some()
-    }
 }
 
 pub trait MoveResolverExt: AptosMoveResolver + StateView {}
