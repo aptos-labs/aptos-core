ac8f0e2ffee32ca21a092fcc128fec06b9fba1b8
zhoujun.ma <zhoujun@aptoslabs.com>
2023-08-15 07:26:55 -0700

    doc

--- execution/block-partitioner/src/pre_partition/mod.rs
+++ execution/block-partitioner/src/pre_partition/mod.rs
@@ -4,6 +4,8 @@ use crate::v2::types::PreParedTxnIdx;
 use aptos_types::transaction::analyzed_transaction::AnalyzedTransaction;
 
 pub trait PrePartitioner: Send {
+    /// The initial partitioning phase for `ShardedBlockPartitioner`/`PartitionerV2` to divide a block into `num_shards` sub-blocks.
+    /// See `PartitionerV2::partition()` for more details.
     fn pre_partition(
         &self,
         transactions: &[AnalyzedTransaction],
