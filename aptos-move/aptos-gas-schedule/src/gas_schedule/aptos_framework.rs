3a298d55ef76110c1a34c3df3d98cbe06692d588
Satya Vusirikala <satyasatya123456@gmail.com>
2023-08-16 08:12:31 -0700

    Adding skeleton for evm native functions

--- aptos-move/aptos-gas-schedule/src/gas_schedule/aptos_framework.rs
+++ aptos-move/aptos-gas-schedule/src/gas_schedule/aptos_framework.rs
@@ -205,6 +205,8 @@ crate::gas_schedule::macros::define_gas_parameters!(
         [aggregator_v2_copy_snapshot_base: InternalGas, {11.. => "aggregator_v2.copy_snapshot.base"}, 6000],
         [aggregator_v2_read_snapshot_base: InternalGas, {11.. => "aggregator_v2.read_snapshot.base"}, 6000],
         [aggregator_v2_string_concat_base: InternalGas, {11.. => "aggregator_v2.string_concat.base"}, 6000],
+        [evm_create_base: InternalGas, "evm.create.per_byte", 100],
+        [evm_call_base: InternalGas, "evm.call.per_byte", 100],
 
         [object_exists_at_base: InternalGas, { 7.. => "object.exists_at.base" }, 5000],
         // These are dummy value, they copied from storage gas in aptos-core/aptos-vm/src/aptos_vm_impl.rs
