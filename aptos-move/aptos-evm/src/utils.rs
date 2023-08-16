4dbd7394f791bdfd7c323a3ea98e6a4e14fdf13c
Sital Kedia <skedia@aptoslabs.com>
2023-08-16 15:24:39 -0700

    Add support for changing U256 to MoveValue

--- aptos-move/aptos-evm/src/utils.rs
+++ aptos-move/aptos-evm/src/utils.rs
@@ -1,11 +1,17 @@
 // Copyright © Aptos Foundation
 
 use primitive_types::{H256, U256};
+use move_core_types::value::{MoveTypeLayout, MoveValue};
 
 pub fn u256_to_arr(value: &U256) -> [u8; 32] {
     let mut result = [0u8; 32];
     value.to_big_endian(&mut result);
     result
+}
+
+pub fn u256_to_move_arr(value: &U256) -> Vec<u8> {
+    let move_value = MoveValue::U256(move_core_types::u256::U256::from_inner(value.clone()));
+    move_value.simple_serialize().unwrap()
 }
 
 pub fn h256_to_arr(value: &H256) -> [u8; 32] {
@@ -23,6 +29,11 @@ pub fn read_u256_from_bytes(bytes: &[u8]) -> U256 {
     U256::from_big_endian(bytes)
 }
 
+pub fn read_u256_from_move_bytes(bytes: &[u8]) -> U256 {
+    let move_value: MoveValue = MoveValue::simple_deserialize(&bytes, &MoveTypeLayout::U256).unwrap();
+    move_value.to_u256().into_inner()
+}
+
 pub fn read_h256_from_bytes(bytes: &[u8]) -> H256 {
     if bytes.len() != 32 {
         panic!("InvalidU256 length expected 32, got {}", bytes.len());
@@ -30,4 +41,19 @@ pub fn read_h256_from_bytes(bytes: &[u8]) -> H256 {
     let mut buf = [0u8; 32];
     buf.copy_from_slice(&bytes);
     H256(buf)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use primitive_types::U256;
+
+    #[test]
+    fn test_ser_de() {
+        let value = U256::from("111000");
+        let bytes  = super::u256_to_move_arr(&value);
+        let value2 = super::read_u256_from_move_bytes(&bytes);
+        assert_eq!(value, value2);
+    }
 }
