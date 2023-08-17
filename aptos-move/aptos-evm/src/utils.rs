992e8e0237419917caa1de0a2dfbc1060fcdeffc
Sital Kedia <sitalkedia@users.noreply.github.com>
2023-08-16 21:11:34 -0700

    Test deploying smart contract (#9680)

--- aptos-move/aptos-evm/src/utils.rs
+++ aptos-move/aptos-evm/src/utils.rs
@@ -15,12 +15,6 @@ pub fn vec_to_h160(value: &[u8]) -> H160 {
     H160::from_slice(&result)
 }
 
-pub fn u256_to_arr(value: &U256) -> [u8; 32] {
-    let mut result = [0u8; 32];
-    value.to_big_endian(&mut result);
-    result
-}
-
 pub fn u256_to_move_arr(value: &U256) -> Vec<u8> {
     let move_value = MoveValue::U256(move_core_types::u256::U256::from_inner(value.clone()));
     move_value.simple_serialize().unwrap()
@@ -30,15 +24,6 @@ pub fn h256_to_arr(value: &H256) -> [u8; 32] {
     let mut result = [0u8; 32];
     result.copy_from_slice(value.as_bytes());
     result
-}
-
-/// Convenience function to read a 256-bit unsigned integer from storage
-/// (assumes big-endian encoding).
-pub fn read_u256_from_bytes(bytes: &[u8]) -> U256 {
-    if bytes.len() != 32 {
-        panic!("InvalidU256 length expected 32, got {}", bytes.len());
-    }
-    U256::from_big_endian(bytes)
 }
 
 pub fn read_u256_from_move_bytes(bytes: &[u8]) -> U256 {
