4b12220b3ecae7c77be97008c63a12970e3870e1
David Wolinsky <isaac.wolinsky@gmail.com>
2023-08-21 20:43:20 -0700

    [python] wrap up large package publishing
    This now supports chunking of even large modules. Will publish to
    mainnet once approved.
    
    added ability to test on a local testnet too

--- ecosystem/python/sdk/examples/large_package_publisher.py
+++ ecosystem/python/sdk/examples/large_package_publisher.py
@@ -10,15 +10,47 @@ One method to do so is to use the CLI:
 """
 import asyncio
 import os
+import sys
 
 from aptos_sdk.account import Account
+from aptos_sdk.account_address import AccountAddress
+from aptos_sdk.aptos_cli_wrapper import AptosCLIWrapper
 from aptos_sdk.async_client import ClientConfig, FaucetClient, RestClient
-from aptos_sdk.package_publisher import PackagePublisher
+from aptos_sdk.package_publisher import MODULE_ADDRESS, PackagePublisher
 
 from .common import FAUCET_URL, NODE_URL
 
 
-async def main():
+async def publish_large_packages(large_packages_dir) -> AccountAddress:
+    rest_client = RestClient(NODE_URL)
+    faucet_client = FaucetClient(FAUCET_URL, rest_client)
+
+    alice = Account.generate()
+    await faucet_client.fund_account(alice.address(), 1_000_000_000)
+    AptosCLIWrapper.compile_package(
+        large_packages_dir, {"large_packages": alice.address()}
+    )
+
+    package_build_path = os.path.join(large_packages_dir, "build", "LargePackages")
+    module_path = os.path.join(
+        package_build_path, "bytecode_modules", "large_packages.mv"
+    )
+    with open(module_path, "rb") as f:
+        module = f.read()
+
+    metadata_path = os.path.join(package_build_path, "package-metadata.bcs")
+    with open(metadata_path, "rb") as f:
+        metadata = f.read()
+
+    publisher = PackagePublisher(rest_client)
+    await publisher.publish_package_experimental(alice, metadata, [module])
+    return alice.address()
+
+
+async def main(
+    large_package_example_dir,
+    large_packages_account: AccountAddress = MODULE_ADDRESS,
+):
     client_config = ClientConfig()
     client_config.transaction_wait_in_seconds = 120
     client_config.max_gas_amount = 1_000_000
@@ -26,12 +58,23 @@ async def main():
     faucet_client = FaucetClient(FAUCET_URL, rest_client)
 
     alice = Account.generate()
-    await faucet_client.fund_account(alice.address(), 1_000_000_000)
+    req0 = faucet_client.fund_account(alice.address(), 1_000_000_000)
+    req1 = faucet_client.fund_account(alice.address(), 1_000_000_000)
+    req2 = faucet_client.fund_account(alice.address(), 1_000_000_000)
+    await asyncio.gather(*[req0, req1, req2])
     alice_balance = await rest_client.account_balance(alice.address())
     print(f"Alice: {alice.address()} {alice_balance}")
-    input("\nPress Enter to continue...")
 
-    package_build_path = "../../../aptos-move/move-examples/large_packages/large_package_example/build/LargePackageExample"
+    if AptosCLIWrapper.does_cli_exist():
+        AptosCLIWrapper.compile_package(
+            large_package_example_dir, {"large_package_example": alice.address()}
+        )
+    else:
+        input("\nUpdate the module with Alice's address, compile, and press Enter.")
+
+    package_build_path = os.path.join(
+        large_package_example_dir, "build", "LargePackageExample"
+    )
     module_directory = os.path.join(package_build_path, "bytecode_modules")
     module_paths = os.listdir(module_directory)
     modules = []
@@ -48,8 +91,22 @@ async def main():
         metadata = f.read()
 
     publisher = PackagePublisher(rest_client)
-    await publisher.publish_package_experimental(alice, metadata, modules)
+    await publisher.publish_package_experimental(
+        alice, metadata, modules, large_packages_account
+    )
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    if len(sys.argv) == 2:
+        large_package_example_dir = sys.argv[1]
+    else:
+        large_package_example_dir = os.path.join(
+            "..",
+            "..",
+            "..",
+            "aptos-move",
+            "move-examples",
+            "large_packages",
+            "large_package_example",
+        )
+    asyncio.run(main(large_package_example_dir))
