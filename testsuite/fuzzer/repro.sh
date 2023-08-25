55d3c8dc5dfd5c6eadaf0ab17cde24421bb65397
Gerardo Di Giacomo <gerardo@aptoslabs.com>
2023-08-25 21:20:29 +0000

    add ser/de to CompiledModule focused fuzzers

--- testsuite/fuzzer/repro.sh
+++ testsuite/fuzzer/repro.sh
@@ -7,11 +7,10 @@ fi
 
 FUZZER_NAME="$1"
 TESTCASE="$2"
-export RUSTFLAGS="--cfg tokio_unstable"
 
 if [ ! -f "$TESTCASE" ]; then
   echo "Testcase not found: $TESTCASE"
   exit 1
 fi
 
-cargo +nightly fuzz run $FUZZER_NAME $TESTCASE
+RUSTFLAGS="--cfg tokio_unstable" cargo +nightly fuzz run $FUZZER_NAME $TESTCASE
