de2cc9e1f492eab890386cc35ccdb4517ac61571
Balaji Arun <balajia@vt.edu>
2023-08-29 22:02:44 -0700

    [dag] state sync good case unit test

--- consensus/src/dag/tests/order_rule_tests.rs
+++ consensus/src/dag/tests/order_rule_tests.rs
@@ -8,8 +8,8 @@ use crate::{
         dag_state_sync::DAG_WINDOW,
         dag_store::Dag,
         order_rule::OrderRule,
-        tests::{dag_test::MockStorage, helpers::new_certified_node},
-        types::{NodeCertificate, NodeMetadata},
+        tests::{dag_test::MockStorage, helpers::generate_dag_nodes},
+        types::NodeMetadata,
         CertifiedNode,
     },
     test_utils::placeholder_ledger_info,
@@ -17,8 +17,7 @@ use crate::{
 use aptos_consensus_types::common::{Author, Round};
 use aptos_infallible::{Mutex, RwLock};
 use aptos_types::{
-    aggregate_signature::AggregateSignature, epoch_change::EpochChangeProof,
-    epoch_state::EpochState, ledger_info::LedgerInfoWithSignatures,
+    epoch_change::EpochChangeProof, epoch_state::EpochState, ledger_info::LedgerInfoWithSignatures,
     validator_verifier::random_validator_verifier,
 };
 use async_trait::async_trait;
@@ -79,46 +78,6 @@ fn generate_permutations(
         Just((0..total_number).collect::<Vec<_>>()).prop_shuffle(),
         num_perm,
     )
-}
-
-/// Generate certified nodes for dag given the virtual dag
-fn generate_dag_nodes(
-    dag: &[Vec<Option<Vec<bool>>>],
-    validators: &[Author],
-) -> Vec<Vec<Option<CertifiedNode>>> {
-    let mut nodes = vec![];
-    let mut previous_round: Vec<Option<CertifiedNode>> = vec![];
-    for (round, round_nodes) in dag.iter().enumerate() {
-        let mut nodes_at_round = vec![];
-        for (idx, author) in validators.iter().enumerate() {
-            if let Some(bitmask) = &round_nodes[idx] {
-                // the bitmask is compressed (without the holes), we need to flatten the previous round nodes
-                // to match the index
-                let parents: Vec<_> = previous_round
-                    .iter()
-                    .flatten()
-                    .enumerate()
-                    .filter(|(idx, _)| *bitmask.get(*idx).unwrap_or(&false))
-                    .map(|(_, node)| {
-                        NodeCertificate::new(node.metadata().clone(), AggregateSignature::empty())
-                    })
-                    .collect();
-                if round > 1 {
-                    assert_eq!(parents.len(), validators.len() * 2 / 3 + 1);
-                }
-                nodes_at_round.push(Some(new_certified_node(
-                    (round + 1) as u64,
-                    *author,
-                    parents,
-                )));
-            } else {
-                nodes_at_round.push(None);
-            }
-        }
-        previous_round = nodes_at_round.clone();
-        nodes.push(nodes_at_round);
-    }
-    nodes
 }
 
 pub struct TestNotifier {
