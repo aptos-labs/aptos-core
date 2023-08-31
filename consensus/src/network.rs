dad0bae5170cb0ba56ab4ee2f2cc230d8ca9862b
Brian Cho <brian@aptoslabs.com>
2023-08-31 13:35:48 -0700

    [Quorum Store] check digest match, and log sender if mismatch

--- consensus/src/network.rs
+++ consensus/src/network.rs
@@ -363,6 +363,7 @@ impl QuorumStoreSender for NetworkSender {
         recipient: Author,
         timeout: Duration,
     ) -> anyhow::Result<Batch> {
+        let request_digest = request.digest();
         let msg = ConsensusMsg::BatchRequestMsg(Box::new(request));
         let response = self
             .consensus_network_client
@@ -370,7 +371,7 @@ impl QuorumStoreSender for NetworkSender {
             .await?;
         match response {
             ConsensusMsg::BatchResponse(batch) => {
-                batch.verify()?;
+                batch.verify_with_digest(request_digest)?;
                 Ok(*batch)
             },
             _ => Err(anyhow!("Invalid batch response")),
