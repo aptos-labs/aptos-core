a5501a4bb097523a817058b1ea5b3a520d8c7ec3
xbtmatt <xbtchunky@gmail.com>
2023-09-04 11:40:49 -0700

    WIP: `from` method in Vector.

--- ecosystem/typescript/sdk_v2/src/bcs/serializable.ts
+++ ecosystem/typescript/sdk_v2/src/bcs/serializable.ts
@@ -1,39 +1,13 @@
 import { from } from "form-data";
-import { Deserializable, Deserializer, Serializer } from ".";
+import { Deserializer, Serializer } from ".";
 import { AnyNumber, Uint16, Uint32, Uint64, Uint128, Uint256 } from "./types";
 import { HexInput } from "../types";
 import { Hex } from "../core";
+import { assert } from "console";
 
-// Before, we had TypeTags with enum variant indexes, but since the primitives here aren't enums in Rust,
-// they aren't actually serialized like this with the native Rust BCS/Serde implementation.
-//
-// To emulate the Rust-like BCS serialization, we can use classes that implement Serializable, and then
-// serialize them by fetching the runtime metadata, stored initially at runtime with the Serialize() decorator.
-// This will facilitate serializing vectors and nested objects/types.
-// Normally this wouldn't be possible because typescript doesn't have runtime reflection, but we can use the Serialize() decorator for this.
-//
-// To ensure type safety, always extend Serializable whenever you use the @Serialize() decorator, or you will likely get runtime errors.
-// 
-// The bcsTypeRegistry is the metadata storage for all runtime type reflection of serialize and deserialize functions.
-const bcsTypeRegistry = new Map<string, { serialize?: Function, deserialize?: Function }>();
 
-// This is the decorator that will register the serialize functions for each class that implements Serializable.
-export function Serialize(): ClassDecorator {
-  return (target: Function) => {
-    const entry = bcsTypeRegistry.get(target.name) || {};
-    entry.serialize = target.prototype.serialize;
-    bcsTypeRegistry.set(target.name, entry);
-  };
-}
-
-// This is the decorator that will register the deserialize functions for each class that implements Deserializable.
-export function Deserialize(): ClassDecorator {
-  return (target: Function) => {
-    const entry = bcsTypeRegistry.get(target.name) || {};
-    entry.deserialize = target.prototype.deserialize;
-    bcsTypeRegistry.set(target.name, entry);
-  };
-}
+// TODO: Evaluate if using `undefined` as a primitive for auto-typecasting Options makes sense. It may result in unexpected behavior.
+export type SerializableOrPrimitive = Serializable | boolean | number | string | bigint | Uint8Array | undefined | null;
 
 // Instead of enums, we can use classes that implement Serializable, to facilitate composable serialization.
 // This lets us serialize vectors and nested objects/types very easily. They will all implement the
@@ -50,84 +24,138 @@ export abstract class Serializable {
   }
 }
 
-@Serialize()
-@Deserialize()
+
+export interface Deserializable<T> {
+  deserialize(deserializer: Deserializer): T;
+}
+
+export class Vector<T extends Serializable> extends Serializable {
+  constructor(public values: Array<T>) {
+    super();
+  }
+
+  /**
+  * Allow for typecasting from an array of primitives or Serializable values to a Vector of the corresponding class
+  * NOTE: This does not have compile-time typechecking, so the compiler will not give you errors if you pass in the wrong type.
+  * NOTE: This only works with a depth of one. Do not use generics in the second argument. Vector.from([...], SomeSerializableType<T>) will not work.
+  * @example
+  * const v = Vector.from([true, false, true], Bool);
+  * const v2 = Vector.from([1, 2, 3], U64);
+  * const v3 = Vector.from(["abc", "def", "ghi"], MoveString);
+  * @params values: an array of primitive or Serializable values
+  * cls: the class to typecast the primitive or Serializable values to
+  * @returns a Vector of the corresponding class T
+  */
+  static from<T extends Serializable>(values: Array<SerializableOrPrimitive>, cls: new (...args: any[]) => T): Vector<T> {
+    return new Vector<T>(values.map((v) => new (cls as any)(v)) as Array<T>);
+  }
+
+  serialize(serializer: Serializer): void {
+    serializer.serializeVector(this.values);
+  }
+
+  static Bool(values: Array<boolean>): Vector<Bool> {
+    return new Vector<Bool>(values.map((v) => new Bool(v)));
+  }
+
+  static U8(values: Array<number>): Vector<U8> {
+    return new Vector<U8>(values.map((v) => new U8(v)));
+  }
+
+  static U16(values: Array<number>): Vector<U16> {
+    return new Vector<U16>(values.map((v) => new U16(v)));
+  }
+
+  static U32(values: Array<number>): Vector<U32> {
+    return new Vector<U32>(values.map((v) => new U32(v)));
+  }
+
+  static U64(values: Array<AnyNumber>): Vector<U64> {
+    return new Vector<U64>(values.map((v) => new U64(v)));
+  }
+
+  static U128(values: Array<AnyNumber>): Vector<U128> {
+    return new Vector<U128>(values.map((v) => new U128(v)));
+  }
+
+  static U256(values: Array<AnyNumber>): Vector<U256> {
+    return new Vector<U256>(values.map((v) => new U256(v)));
+  }
+
+  static String(values: Array<string>): Vector<MoveString> {
+    return new Vector<MoveString>(values.map((v) => new MoveString(v)));
+  }
+
+  static Option<T extends Serializable>(values: Array<SerializableOrPrimitive>, cls: new (...args: any[]) => T): Vector<Option<T>> {
+    return new Vector<Option<T>>(values.map((v) => new Option<T>(v, cls)));
+  }
+
+  static Object(values: Array<HexInput>): Vector<AptosObject> {
+    return new Vector<AptosObject>(values.map((v) => new AptosObject(Hex.fromHexInput({hexInput: v}))));
+  }
+
+
+  static deserialize<U extends Serializable>(deserializer: Deserializer, cls: Deserializable<U>): Vector<U> {
+    const values = new Array<U>();
+    for (let i = 0; i < length; i++) {
+      values.push(cls.deserialize(deserializer));
+    }
+    return new Vector<U>(values);
+  }
+}
+
 export class Bool extends Serializable {
   constructor(public value: boolean) {
     super();
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeBool(this.value);
+    serializer.serializeBool(this.value);
   }
 
   static deserialize(deserializer: Deserializer): Bool {
-      return new Bool(deserializer.deserializeBool());
+    return new Bool(deserializer.deserializeBool());
   }
 }
-
-
-export class Vector<T extends Serializable> extends Serializable {
-  constructor(public value: Array<T>) {
-    super();
-  }
-
-  serialize(serializer: Serializer): void {
-      serializer.serializeVector(this.value);
-  }
-
-  static deserialize<U extends Serializable>(deserializer: Deserializer, cls: Deserializable<U>): Vector<U> {
-      const values = new Array<U>();
-      const length = deserializer.deserializeUleb128AsU32();
-      for (let i = 0; i < length; i++) {
-          values.push(cls.deserialize(deserializer));
-      }
-      return new Vector<U>(values);
-  }
-}
-
 export class U8 extends Serializable {
   constructor(public value: number) {
     super();
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeU8(this.value);
+    serializer.serializeU8(this.value);
   }
 
   static deserialize(deserializer: Deserializer): U8 {
-      return new U8(deserializer.deserializeU8());
+    return new U8(deserializer.deserializeU8());
   }
 }
-
 export class U16 extends Serializable {
   constructor(public value: number) {
     super();
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeU16(this.value);
+    serializer.serializeU16(this.value);
   }
 
   static deserialize(deserializer: Deserializer): U16 {
-      return new U16(deserializer.deserializeU16());
+    return new U16(deserializer.deserializeU16());
   }
 }
-
 export class U32 extends Serializable {
   constructor(public value: number) {
     super();
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeU32(this.value);
+    serializer.serializeU32(this.value);
   }
 
   static deserialize(deserializer: Deserializer): U32 {
-      return new U32(deserializer.deserializeU32());
+    return new U32(deserializer.deserializeU32());
   }
 }
-
 export class U64 extends Serializable {
   value: bigint;
 
@@ -137,14 +165,13 @@ export class U64 extends Serializable {
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeU64(this.value);
+    serializer.serializeU64(this.value);
   }
 
   static deserialize(deserializer: Deserializer): U64 {
-      return new U64(deserializer.deserializeU64());
+    return new U64(deserializer.deserializeU64());
   }
 }
-
 export class U128 extends Serializable {
   value: bigint;
 
@@ -154,14 +181,13 @@ export class U128 extends Serializable {
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeU128(this.value);
+    serializer.serializeU128(this.value);
   }
 
   static deserialize(deserializer: Deserializer): U128 {
-      return new U128(deserializer.deserializeU128());
+    return new U128(deserializer.deserializeU128());
   }
 }
-
 export class U256 extends Serializable {
   value: bigint;
 
@@ -171,11 +197,11 @@ export class U256 extends Serializable {
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeU256(this.value);
+    serializer.serializeU256(this.value);
   }
 
   static deserialize(deserializer: Deserializer): U256 {
-      return new U256(deserializer.deserializeU256());
+    return new U256(deserializer.deserializeU256());
   }
 }
 
@@ -185,33 +211,96 @@ export class MoveString extends Serializable {
   }
 
   serialize(serializer: Serializer): void {
-      serializer.serializeStr(this.value);
+    serializer.serializeStr(this.value);
   }
 
   static deserialize(deserializer: Deserializer): MoveString {
-      return new MoveString(deserializer.deserializeStr());
+    return new MoveString(deserializer.deserializeStr());
   }
 }
 
-export class MoveOption<T extends Serializable> extends Serializable {
-  constructor(public value: T | null) {
-    super();
-  }
 
-  serialize(serializer: Serializer): void {
-    serializer.serializeU32AsUleb128(this.value ? 1 : 0);
-    if (this.value) {
-      this.value.serialize(serializer);
+// NEXT STEP:
+
+// Let's remove the cls values from all constructors and maybe `from` functions, or at least separate primitives from serializable values.
+// It's interfering with the composability of the serializable values.
+// Ultimately we want to focus on that composability, so we can serialize nested objects and vectors easily.
+// 
+// I think for the most part what you've done is good.
+// You just need to
+// 1. make it compatible with TypeTag type stuff, either convert those or convert this? most likely convert those
+// 2. Make Vectors/Options work with StructTags in the old sdk
+
+export class Option<T extends Serializable> extends Serializable {
+  private vec: Vector<T>;
+
+  constructor(value?: T | SerializableOrPrimitive, cls?: new (...args: any[]) => T) {
+    super();
+    if (typeof value !== 'undefined') {
+      if (cls) {
+        this.vec = Vector.from([value], cls);
+      } else {
+        this.vec = new Vector([value as T]);
+      }
+    } else {
+      this.vec = new Vector([]);
     }
   }
 
-  static deserialize<T extends Serializable>(deserializer: Deserializer): MoveOption<T> | null {
-    const isSome = deserializer.deserializeUleb128AsU32() === 1;
+  // Check if the Option has a value.
+  isSome(): boolean {
+    return this.vec.values.length === 1;
+  }
+
+  // Get the value, if it exists.
+  get(): T | undefined {
+    return this.isSome() ? this.vec.values[0] : undefined;
+  }
+
+  serialize(serializer: Serializer): void {
+    this.vec.serialize(serializer);
+  }
+
+  static from<T extends Serializable>(value: SerializableOrPrimitive, cls: new (...args: any[]) => T): Option<T> {
+    const vector = Vector.from([value], cls);
+    return new Option(vector.values[0]);
+  }
+
+  static deserialize<U extends Serializable>(deserializer: Deserializer, cls: Deserializable<U>): Option<U> {
+    const vector = Vector.deserialize(deserializer, cls);
+    return new Option(vector.values[0]);
+  }
+}
+
+
+export class MoveOption<T extends Serializable> extends Serializable {
+  public value: Vector<T>;
+
+  constructor(value?: T, cls?: new (...args: any[]) => T) {
+    super();
+    if (typeof value !== 'undefined') {
+      if (typeof cls === 'undefined') {
+        this.value = new Vector([value]);
+      } else {
+        this.value = Vector.from([value], cls);
+      }
+    } else {
+      this.value = new Vector<T>([]);
+    }
+  }
+
+  serialize(serializer: Serializer): void {
+    this.value.serialize(serializer);
+  }
+
+  static deserialize<U extends Serializable>(deserializer: Deserializer, cls: Deserializable<U>): MoveOption<U> | null {
+    const length = deserializer.deserializeUleb128AsU32();
+    const isSome = length === 1;
     if (isSome) {
-      const 
-      const value = deserializer.deserialize(T);
+      const value = cls.deserialize(deserializer);
       return new MoveOption<U>(value);
     } else {
+      assert(length === 0, "Invalid MoveOption length, expected 0 or 1, got " + length);
       return null;
     }
   }
--- ecosystem/typescript/sdk_v2/tests/unit/bcs-helper.test.ts
+++ ecosystem/typescript/sdk_v2/tests/unit/bcs-helper.test.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Deserializer } from "../../src/bcs/deserializer";
-import { Bool, MoveOption, MoveString, U128, U16, U256, U32, U64, U8 } from "../../src/bcs/serializable";
+import { Bool, Option, MoveString, U128, U16, U256, U32, U64, U8, Vector } from "../../src/bcs/serializable";
 import { Serializer } from "../../src/bcs/serializer";
 
 describe("Tests for the Serializable class", () => {
@@ -49,7 +49,7 @@ describe("Tests for the Serializable class", () => {
     expect(u256.toUint8Array()).toEqual(u256Bytes);
     expect(bool.toUint8Array()).toEqual(boolBytes);
     expect(str.toUint8Array()).toEqual(strBytes);
-    
+
     // Test the overall buffer
     expect(serializer.toUint8Array()).toEqual(new Uint8Array([...u8Bytes, ...u16Bytes, ...u32Bytes, ...u64Bytes, ...u128Bytes, ...u256Bytes, ...boolBytes, ...strBytes]));
 
@@ -83,26 +83,26 @@ describe("Tests for the Serializable class", () => {
     const u256 = new U256(6);
     const bool = new Bool(false);
     const str = new MoveString("some string");
-    
+
     // Create Option values for each Some(...) type and serialize them
-    const someOptionU8 = new MoveOption(u8);
-    const someOptionU16 = new MoveOption(u16);
-    const someOptionU32 = new MoveOption(u32);
-    const someOptionU64 = new MoveOption(u64);
-    const someOptionU128 = new MoveOption(u128);
-    const someOptionU256 = new MoveOption(u256);
-    const someOptionBool = new MoveOption(bool);
-    const someOptionString = new MoveOption(str);
+    const someOptionU8 = new Option(u8);
+    const someOptionU16 = new Option(u16);
+    const someOptionU32 = new Option(u32);
+    const someOptionU64 = new Option(u64);
+    const someOptionU128 = new Option(u128);
+    const someOptionU256 = new Option(u256);
+    const someOptionBool = new Option(bool);
+    const someOptionString = new Option(str);
 
     // Create Option values for each None(...) type and serialize them
-    const noneOptionU8 = new MoveOption(null);
-    const noneOptionU16 = new MoveOption(null);
-    const noneOptionU32 = new MoveOption(null);
-    const noneOptionU64 = new MoveOption(null);
-    const noneOptionU128 = new MoveOption(null);
-    const noneOptionU256 = new MoveOption(null);
-    const noneOptionBool = new MoveOption(null);
-    const noneOptionString = new MoveOption(null);
+    const noneOptionU8 = new Option();
+    const noneOptionU16 = new Option();
+    const noneOptionU32 = new Option();
+    const noneOptionU64 = new Option();
+    const noneOptionU128 = new Option();
+    const noneOptionU256 = new Option();
+    const noneOptionBool = new Option();
+    const noneOptionString = new Option();
 
     serializer.serialize(someOptionU8);
     serializer.serialize(someOptionU16);
@@ -112,7 +112,7 @@ describe("Tests for the Serializable class", () => {
     serializer.serialize(someOptionU256);
     serializer.serialize(someOptionBool);
     serializer.serialize(someOptionString);
-    
+
     serializer.serialize(noneOptionU8);
     serializer.serialize(noneOptionU16);
     serializer.serialize(noneOptionU32);
@@ -123,11 +123,12 @@ describe("Tests for the Serializable class", () => {
     serializer.serialize(noneOptionString);
 
     const someOptionU8Bytes = new Uint8Array([1, 1]);
+    console.log(someOptionU8.toUint8Array());
     const someOptionU16Bytes = new Uint8Array([1, 2, 0]);
     const someOptionU32Bytes = new Uint8Array([1, 3, 0, 0, 0]);
     const someOptionU64Bytes = new Uint8Array([1, 4, 0, 0, 0, 0, 0, 0, 0]);
-    const someOptionU128Bytes = new Uint8Array([1, 5, 0, 0, 0, 0, 0, 0, 0, 0]);
-    const someOptionU256Bytes = new Uint8Array([1, 6, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const someOptionU128Bytes = new Uint8Array([1, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const someOptionU256Bytes = new Uint8Array([1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     const someOptionBoolBytes = new Uint8Array([1, 0]);
     const someOptionStringBytes = new Uint8Array([1, 11, 115, 111, 109, 101, 32, 115, 116, 114, 105, 110, 103]);
 
@@ -164,17 +165,106 @@ describe("Tests for the Serializable class", () => {
 
     // Test the deserialization
     // const deserializer = new Deserializer(serializer.toUint8Array());
-    // const asdf = MoveOption.deserialize<Bool>(deserializer);
-    // const deserializedSomeOptionU8 = MoveOption.deserialize(deserializer, U8);
-    // const deserializedSomeOptionU16 = MoveOption.deserialize(deserializer, U16);
-    // const deserializedSomeOptionU32 = MoveOption.deserialize(deserializer, U32);
-    // const deserializedSomeOptionU64 = MoveOption.deserialize(deserializer, U64);
-    // const deserializedSomeOptionU128 = MoveOption.deserialize(deserializer, U128);
-    // const deserializedSomeOptionU256 = MoveOption.deserialize(deserializer, U256);
-    // const deserializedSomeOptionBool = MoveOption.deserialize(deserializer, Bool);
-    // const deserializedSomeOptionString = MoveOption.deserialize(deserializer, MoveString);
+    // const asdf = Option.deserialize<Bool>(deserializer);
+    // const deserializedSomeOptionU8 = Option.deserialize(deserializer, U8);
+    // const deserializedSomeOptionU16 = Option.deserialize(deserializer, U16);
+    // const deserializedSomeOptionU32 = Option.deserialize(deserializer, U32);
+    // const deserializedSomeOptionU64 = Option.deserialize(deserializer, U64);
+    // const deserializedSomeOptionU128 = Option.deserialize(deserializer, U128);
+    // const deserializedSomeOptionU256 = Option.deserialize(deserializer, U256);
+    // const deserializedSomeOptionBool = Option.deserialize(deserializer, Bool);
+    // const deserializedSomeOptionString = Option.deserialize(deserializer, MoveString);
 
 
+  });
+
+  it ("serializes and deserializes an option type correctly", () => {
+    const optionBoolVectorFrom = Vector.from([new Bool(true), new Bool(false), undefined], Option);
+    const optionBoolVectorFrom2 = new Vector([new Option(new Bool(true)), new Option(new Bool(false)), new Option()]);
+    const optionBoolVectorBytes = new Uint8Array([3, 1, 1, 1, 0, 0]);
+    optionBoolVectorFrom.values.forEach((v) => console.log(v));
+    expect(optionBoolVectorBytes).toEqual(optionBoolVectorFrom.toUint8Array());
+    expect(optionBoolVectorBytes).toEqual(optionBoolVectorFrom2.toUint8Array());
+
+    const deserializer = new Deserializer(optionBoolVectorBytes);
+    const deserializedOptionBoolVector = Vector.deserialize(deserializer, Option);
+
+
+  });
+
+  it("serializes and deserializes all vector types correctly", () => {
+    const boolVectorFrom = Vector.from([true, false, true], Bool);
+    const u8VectorFrom = Vector.from([1, 2, 3], U8);
+    const u16VectorFrom = Vector.from([1, 2, 3], U16);
+    const u32VectorFrom = Vector.from([1, 2, 3], U32);
+    const u64VectorFrom = Vector.from([1, 2, 3], U64);
+    const u128VectorFrom = Vector.from([1, 2, 3], U128);
+    const u256VectorFrom = Vector.from([1, 2, 3], U256);
+    const stringVectorFrom = Vector.from(["abc", "def", "ghi"], MoveString);
+    const optionBoolVectorFrom = Vector.from([new Bool(true), new Bool(false), undefined], Option);
+    const optionU64VectorFrom = Vector.from([new U64(1), undefined, new U64(3)], Option);
+    const optionStringVectorFrom = Vector.from([new MoveString("abc"), undefined, new MoveString("ghi")], Option);
+
+    const boolVectorBytes = new Uint8Array([3, 1, 0, 1]);
+    const u8VectorBytes = new Uint8Array([3, 1, 2, 3]);
+    const u16VectorBytes = new Uint8Array([3, 1, 0, 2, 0, 3, 0]);
+    const u32VectorBytes = new Uint8Array([3, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0]);
+    const u64VectorBytes = new Uint8Array([3, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0]);
+    const u128VectorBytes = new Uint8Array([3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const u256VectorBytes = new Uint8Array([3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const stringVectorBytes = new Uint8Array([3, 3, 97, 98, 99, 3, 100, 101, 102, 3, 103, 104, 105]);
+    const optionBoolVectorBytes = new Uint8Array([3, 1, 1, 1, 0, 0]);
+    const optionU64VectorBytes = new Uint8Array([3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 0, 0]);
+    const optionStringVectorBytes = new Uint8Array([3, 1, 3, 97, 98, 99, 0, 1, 3, 103, 104, 105]);
+
+    expect(boolVectorBytes).toEqual(boolVectorFrom.toUint8Array());
+    expect(u8VectorBytes).toEqual(u8VectorFrom.toUint8Array());
+    expect(u16VectorBytes).toEqual(u16VectorFrom.toUint8Array());
+    expect(u32VectorBytes).toEqual(u32VectorFrom.toUint8Array());
+    expect(u64VectorBytes).toEqual(u64VectorFrom.toUint8Array());
+    expect(u128VectorBytes).toEqual(u128VectorFrom.toUint8Array());
+    expect(u256VectorBytes).toEqual(u256VectorFrom.toUint8Array());
+    expect(stringVectorBytes).toEqual(stringVectorFrom.toUint8Array());
+    expect(optionBoolVectorBytes).toEqual(optionBoolVectorFrom.toUint8Array());
+    expect(optionU64VectorBytes).toEqual(optionU64VectorFrom.toUint8Array());
+    expect(optionStringVectorBytes).toEqual(optionStringVectorFrom.toUint8Array());
+  });
+
+  it("serializes and deserializes all vector types with the `.from` static method correctly", () => {
+    const boolVector = new Vector([new Bool(true), new Bool(false), new Bool(true)]);
+    const boolVectorFrom = Vector.from([true, false, true], Bool);
+    const u8Vector = new Vector([new U8(1), new U8(2), new U8(3)]);
+    const u8VectorFrom = Vector.from([1, 2, 3], U8);
+    const u16Vector = new Vector([new U16(1), new U16(2), new U16(3)]);
+    const u16VectorFrom = Vector.from([1, 2, 3], U16);
+    const u32Vector = new Vector([new U32(1), new U32(2), new U32(3)]);
+    const u32VectorFrom = Vector.from([1, 2, 3], U32);
+    const u64Vector = new Vector([new U64(1), new U64(2), new U64(3)]);
+    const u64VectorFrom = Vector.from([1, 2, 3], U64);
+    const u128Vector = new Vector([new U128(1), new U128(2), new U128(3)]);
+    const u128VectorFrom = Vector.from([1, 2, 3], U128);
+    const u256Vector = new Vector([new U256(1), new U256(2), new U256(3)]);
+    const u256VectorFrom = Vector.from([1, 2, 3], U256);
+    const stringVector = new Vector([new MoveString("abc"), new MoveString("def"), new MoveString("ghi")]);
+    const stringVectorFrom = Vector.from(["abc", "def", "ghi"], MoveString);
+    const optionBoolVector = new Vector([new Option(new Bool(true)), new Option(new Bool(false)), new Option()]);
+    const optionBoolVectorFrom = Vector.from([new Bool(true), new Bool(false), undefined], Option);
+    const optionU64Vector = new Vector([new Option(new U64(1)), new Option(undefined), new Option(new U64(3))]);
+    const optionU64VectorFrom = Vector.from([new U64(1), undefined, new U64(3)], Option);
+    const optionStringVector = new Vector([new Option(new MoveString("abc")), new Option(undefined), new Option(new MoveString("ghi"))]);
+    const optionStringVectorFrom = Vector.from([new MoveString("abc"), undefined, new MoveString("ghi")], Option);
+
+    expect(boolVector.toUint8Array()).toEqual(boolVectorFrom.toUint8Array());
+    expect(u8Vector.toUint8Array()).toEqual(u8VectorFrom.toUint8Array());
+    expect(u16Vector.toUint8Array()).toEqual(u16VectorFrom.toUint8Array());
+    expect(u32Vector.toUint8Array()).toEqual(u32VectorFrom.toUint8Array());
+    expect(u64Vector.toUint8Array()).toEqual(u64VectorFrom.toUint8Array());
+    expect(u128Vector.toUint8Array()).toEqual(u128VectorFrom.toUint8Array());
+    expect(u256Vector.toUint8Array()).toEqual(u256VectorFrom.toUint8Array());
+    expect(stringVector.toUint8Array()).toEqual(stringVectorFrom.toUint8Array());
+    expect(optionBoolVector.toUint8Array()).toEqual(optionBoolVectorFrom.toUint8Array());
+    expect(optionU64Vector.toUint8Array()).toEqual(optionU64VectorFrom.toUint8Array());
+    expect(optionStringVector.toUint8Array()).toEqual(optionStringVectorFrom.toUint8Array());
   });
 
 
