76e8b6fe6ac33c1cdb6be0d589b8d240693498d0
Satya Vusirikala <satyasatya123456@gmail.com>
2023-09-05 11:15:25 -0700

    Add native functions for Aggregator V2 (#9301)
    * Add aggregator v2 native functions
    
    * rust lint
    
    * Addressing PR comments
    
    * Addressing PR comments
    
    * Addressing PR comments

--- aptos-move/framework/src/natives/aggregator_natives/helpers_v2.rs
+++ aptos-move/framework/src/natives/aggregator_natives/helpers_v2.rs
@@ -1,11 +1,54 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
-use crate::natives::aggregator_natives::helpers::get_aggregator_field;
-use aptos_aggregator::aggregator_extension::extension_error;
-use move_binary_format::errors::PartialVMResult;
+
+use crate::natives::aggregator_natives::helpers_v1::get_aggregator_field;
+use aptos_aggregator::aggregator_extension::{extension_error, AggregatorID};
+use move_binary_format::errors::{PartialVMError, PartialVMResult};
+use move_core_types::vm_status::StatusCode;
 use move_vm_types::values::{Struct, StructRef, Value};
 
+/// Indices of `value` and `limit` fields in the `Aggregator` Move
+/// struct.
 const VALUE_FIELD_INDEX: usize = 0;
+const LIMIT_FIELD_INDEX: usize = 1;
+
+/// Returns ID and a limit of aggrgegator based on a reference to `Aggregator` Move struct.
+pub(crate) fn aggregator_value_as_u128(
+    aggregator: &StructRef,
+) -> PartialVMResult<(AggregatorID, u128)> {
+    let (value, limit) = get_aggregator_fields_u128(aggregator)?;
+    if value > u64::MAX as u128 {
+        return Err(
+            PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
+                .with_message("Aggregator identifier is too small".to_string()),
+        );
+    }
+    Ok((AggregatorID::ephemeral(value as u64), limit))
+}
+
+/// Returns ID and a limit of aggrgegator based on a reference to `Aggregator` Move struct.
+pub(crate) fn aggregator_value_as_u64(
+    aggregator: &StructRef,
+) -> PartialVMResult<(AggregatorID, u64)> {
+    let (value, limit) = get_aggregator_fields_u64(aggregator)?;
+    Ok((AggregatorID::ephemeral(value), limit))
+}
+
+/// Given a reference to `Aggregator` Move struct, returns a tuple of its
+/// fields: (`value`, `limit`).
+pub fn get_aggregator_fields_u128(aggregator: &StructRef) -> PartialVMResult<(u128, u128)> {
+    let value = get_aggregator_field(aggregator, VALUE_FIELD_INDEX)?.value_as::<u128>()?;
+    let limit = get_aggregator_field(aggregator, LIMIT_FIELD_INDEX)?.value_as::<u128>()?;
+    Ok((value, limit))
+}
+
+/// Given a reference to `Aggregator` Move struct, returns a tuple of its
+/// fields: (`value`, `limit`).
+pub fn get_aggregator_fields_u64(aggregator: &StructRef) -> PartialVMResult<(u64, u64)> {
+    let value = get_aggregator_field(aggregator, VALUE_FIELD_INDEX)?.value_as::<u64>()?;
+    let limit = get_aggregator_field(aggregator, LIMIT_FIELD_INDEX)?.value_as::<u64>()?;
+    Ok((value, limit))
+}
 
 /// Returns ID of aggregator snapshot based on a reference to `AggregatorSnapshot` Move struct.
 pub(crate) fn aggregator_snapshot_value_as_u128(
