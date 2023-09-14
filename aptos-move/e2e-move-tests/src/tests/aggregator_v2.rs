aba9a8494fd3fa1f861f90865a339c9b9f1314b4
Satya Vusirikala <satyasatya123456@gmail.com>
2023-09-14 16:07:15 -0700

    Fixing aggregator tests (#10059)

--- aptos-move/e2e-move-tests/src/tests/aggregator_v2.rs
+++ aptos-move/e2e-move-tests/src/tests/aggregator_v2.rs
@@ -4,10 +4,9 @@
 use crate::{
     aggregator_v2::{
         check, initialize, materialize, materialize_and_try_add, materialize_and_try_sub, new,
-        read_snapshot_u128, read_snapshot_u64, snapshot, snapshot_with_u64_limit, try_add,
-        try_add_and_materialize, try_add_and_read_snapshot_u128, try_sub, try_sub_add,
-        try_sub_and_materialize, verify_copy_snapshot, verify_copy_string_snapshot,
-        verify_string_concat, verify_string_snapshot_concat,
+        read_snapshot, snapshot, try_add, try_add_and_materialize, try_add_and_read_snapshot_u128,
+        try_sub, try_sub_add, try_sub_and_materialize, verify_copy_snapshot,
+        verify_copy_string_snapshot, verify_string_concat, verify_string_snapshot_concat,
     },
     assert_abort, assert_success,
     tests::common,
@@ -198,15 +197,11 @@ fn test_aggregator_snapshot() {
     let (mut h, acc) = setup();
     let txn1 = new(&mut h, &acc, 0, 400);
     let txn2 = snapshot(&mut h, &acc, 0);
-    let txn3 = snapshot_with_u64_limit(&mut h, &acc, 0);
-    let txn4 = read_snapshot_u128(&mut h, &acc, 0);
-    let txn5 = read_snapshot_u64(&mut h, &acc, 0);
-    let txn6 = try_add_and_read_snapshot_u128(&mut h, &acc, 0, 100);
+    let txn3 = read_snapshot(&mut h, &acc, 0);
+    let txn4 = try_add_and_read_snapshot_u128(&mut h, &acc, 0, 100);
 
     assert_success!(h.run(txn1));
     assert_success!(h.run(txn2));
     assert_success!(h.run(txn3));
     assert_success!(h.run(txn4));
-    assert_success!(h.run(txn5));
-    assert_success!(h.run(txn6));
 }
