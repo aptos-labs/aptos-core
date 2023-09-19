1b1a5ad454cac3f9d327369c295fe21ecd7e86e9
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2023-09-19 13:19:23 -0700

    agg v2 natives flag (#10069)
    review comments

--- aptos-move/vm-genesis/src/lib.rs
+++ aptos-move/vm-genesis/src/lib.rs
@@ -427,9 +427,10 @@ pub fn default_features() -> Vec<FeatureFlag> {
         FeatureFlag::EMIT_FEE_STATEMENT,
         FeatureFlag::STORAGE_DELETION_REFUND,
         FeatureFlag::SIGNATURE_CHECKER_V2_SCRIPT_FIX,
-        FeatureFlag::AGGREGATOR_SNAPSHOTS,
+        FeatureFlag::AGGREGATOR_API,
         FeatureFlag::SAFER_RESOURCE_GROUPS,
         FeatureFlag::SAFER_METADATA,
+        FeatureFlag::AGGREGATOR_EXECUTION,
     ]
 }
 
