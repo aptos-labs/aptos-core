253dd0df79eb0422bb7681627e4a2bd9224963f6
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2023-09-19 13:19:23 -0700

    agg v2 natives flag (#10069)
    review comments

--- aptos-move/aptos-release-builder/src/components/feature_flags.rs
+++ aptos-move/aptos-release-builder/src/components/feature_flags.rs
@@ -79,10 +79,11 @@ pub enum FeatureFlag {
     ModuleEvent,
     EmitFeeStatement,
     StorageDeletionRefund,
-    AggregatorSnapshots,
+    AggregatorApi,
     SignatureCheckerV2ScriptFix,
     SaferResourceGroups,
     SaferMetadata,
+    AggregatorExecution,
 }
 
 fn generate_features_blob(writer: &CodeWriter, data: &[u64]) {
@@ -210,12 +211,13 @@ impl From<FeatureFlag> for AptosFeatureFlag {
             FeatureFlag::ModuleEvent => AptosFeatureFlag::MODULE_EVENT,
             FeatureFlag::EmitFeeStatement => AptosFeatureFlag::EMIT_FEE_STATEMENT,
             FeatureFlag::StorageDeletionRefund => AptosFeatureFlag::STORAGE_DELETION_REFUND,
-            FeatureFlag::AggregatorSnapshots => AptosFeatureFlag::AGGREGATOR_SNAPSHOTS,
             FeatureFlag::SignatureCheckerV2ScriptFix => {
                 AptosFeatureFlag::SIGNATURE_CHECKER_V2_SCRIPT_FIX
             },
+            FeatureFlag::AggregatorApi => AptosFeatureFlag::AGGREGATOR_API,
             FeatureFlag::SaferResourceGroups => AptosFeatureFlag::SAFER_RESOURCE_GROUPS,
             FeatureFlag::SaferMetadata => AptosFeatureFlag::SAFER_METADATA,
+            FeatureFlag::AggregatorExecution => AptosFeatureFlag::AGGREGATOR_EXECUTION,
         }
     }
 }
@@ -266,12 +268,13 @@ impl From<AptosFeatureFlag> for FeatureFlag {
             AptosFeatureFlag::MODULE_EVENT => FeatureFlag::ModuleEvent,
             AptosFeatureFlag::EMIT_FEE_STATEMENT => FeatureFlag::EmitFeeStatement,
             AptosFeatureFlag::STORAGE_DELETION_REFUND => FeatureFlag::StorageDeletionRefund,
-            AptosFeatureFlag::AGGREGATOR_SNAPSHOTS => FeatureFlag::AggregatorSnapshots,
             AptosFeatureFlag::SIGNATURE_CHECKER_V2_SCRIPT_FIX => {
                 FeatureFlag::SignatureCheckerV2ScriptFix
             },
+            AptosFeatureFlag::AGGREGATOR_API => FeatureFlag::AggregatorApi,
             AptosFeatureFlag::SAFER_RESOURCE_GROUPS => FeatureFlag::SaferResourceGroups,
             AptosFeatureFlag::SAFER_METADATA => FeatureFlag::SaferMetadata,
+            AptosFeatureFlag::AGGREGATOR_EXECUTION => FeatureFlag::AggregatorExecution,
         }
     }
 }
