94d937c47be017982a3187f647d362651feb36d5
Brian Cho <brian@aptoslabs.com>
2023-09-21 21:48:48 -0700

    4 MB/s

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1367,12 +1367,12 @@ fn netbench_config_100_megabytes_per_sec(netbench_config: &mut NetbenchConfig) {
     netbench_config.direct_send_per_second = 1000;
 }
 
-fn netbench_config_3_megabytes_per_sec(netbench_config: &mut NetbenchConfig) {
+fn netbench_config_4_megabytes_per_sec(netbench_config: &mut NetbenchConfig) {
     netbench_config.enabled = true;
     netbench_config.max_network_channel_size = 1000;
     netbench_config.enable_direct_send_testing = true;
     netbench_config.direct_send_data_size = 100000;
-    netbench_config.direct_send_per_second = 30;
+    netbench_config.direct_send_per_second = 40;
 }
 
 fn net_bench() -> ForgeConfig {
@@ -1393,7 +1393,7 @@ fn net_bench_two_region_env() -> ForgeConfig {
         .with_validator_override_node_config_fn(Arc::new(|config, _| {
             // Not using 100 MBps here, as it will lead to throughput collapse
             let mut netbench_config = NetbenchConfig::default();
-            netbench_config_3_megabytes_per_sec(&mut netbench_config);
+            netbench_config_4_megabytes_per_sec(&mut netbench_config);
             config.netbench = Some(netbench_config);
         }))
 }
