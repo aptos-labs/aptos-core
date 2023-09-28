85965702144b71c5fc781ec3a0493a5997c6b7be
Jin <128556004+0xjinn@users.noreply.github.com>
2023-09-28 01:11:37 -0700

    [TSSDKv2][3/n] Account and AuthenticationKey Classes (#10210)
    * Add helper.ts and fixes based on comments
    
    * add asymmetric_crypto file which includes all crypto base classes as abstract. And have all concrete crypto classes to extend from based abstract classes
    
    * Update crypto classes name to include Ed25519 prefix
    
    * fixes based on comments
    
    * Add account and auth key classes
    
    * update Account class to accept abstract PublicKey and PrivateKey
    
    * update methods' comment

--- ecosystem/typescript/sdk_v2/src/crypto/asymmetric_crypto.ts
+++ ecosystem/typescript/sdk_v2/src/crypto/asymmetric_crypto.ts
@@ -8,6 +8,12 @@ export abstract class PublicKey implements Serializable, Deserializable<PublicKe
   // Verify the given message with the public key and signature.
   abstract verifySignature(args: { data: HexInput; signature: Signature }): boolean;
 
+  // Convert the public key to bytes or Uint8Array.
+  abstract toUint8Array(): Uint8Array;
+
+  // Convert the public key to a hex string with the 0x prefix.
+  abstract toString(): string;
+
   // TODO: This should be a static method.
   abstract deserialize(deserializer: Deserializer): PublicKey;
   abstract serialize(serializer: Serializer): void;
@@ -17,12 +23,24 @@ export abstract class PrivateKey implements Serializable, Deserializable<Private
   // Sign the given message with the private key.
   abstract sign(args: { message: HexInput }): Signature;
 
+  // Convert the private key to bytes or Uint8Array.
+  abstract toUint8Array(): Uint8Array;
+
+  // Convert the private key to a hex string with the 0x prefix.
+  abstract toString(): string;
+
   // TODO: This should be a static method.
   abstract deserialize(deserializer: Deserializer): PrivateKey;
   abstract serialize(serializer: Serializer): void;
 }
 
 export abstract class Signature implements Serializable, Deserializable<Signature> {
+  // Convert the signature to bytes or Uint8Array.
+  abstract toUint8Array(): Uint8Array;
+
+  // Convert the signature to a hex string with the 0x prefix.
+  abstract toString(): string;
+
   // TODO: This should be a static method.
   abstract deserialize(deserializer: Deserializer): Signature;
   abstract serialize(serializer: Serializer): void;
