72b3bcda0c1ae03b71852dfef533d0bbcb517f47
Oliver He <heliuchuan@gmail.com>
2023-09-28 02:45:04 -0700

    [ts-sdk-v2] Add getTransactions to the sdk-v2 transaction API (#10288)
    * Add getTransactions to the sdkv2 transaction API
    
    * Update ecosystem/typescript/sdk_v2/src/internal/transaction.ts
    
    Co-authored-by: Maayan <maayan@aptoslabs.com>
    
    * update after merge
    
    ---------
    
    Co-authored-by: Greg Nazario <greg@gnazar.io>
    Co-authored-by: Maayan <maayan@aptoslabs.com>

--- ecosystem/typescript/sdk_v2/src/internal/transaction.ts
+++ ecosystem/typescript/sdk_v2/src/internal/transaction.ts
@@ -7,8 +7,24 @@
 
 import { AptosConfig } from "../api/aptos_config";
 import { get } from "../client";
-import { GasEstimation } from "../types";
+import { GasEstimation, PaginationArgs, TransactionResponse } from "../types";
 import { AptosApiType } from "../utils/const";
+import { paginateWithCursor } from "../utils/paginate_with_cursor";
+
+export async function getTransactions(args: {
+  aptosConfig: AptosConfig;
+  options?: PaginationArgs;
+}): Promise<TransactionResponse[]> {
+  const { aptosConfig, options } = args;
+  const data = await paginateWithCursor<{}, TransactionResponse[]>({
+    url: aptosConfig.getRequestUrl(AptosApiType.FULLNODE),
+    endpoint: "transactions",
+    originMethod: "getTransactions",
+    params: { start: options?.start, limit: options?.limit },
+    overrides: { ...aptosConfig.clientConfig },
+  }, aptosConfig);
+  return data;
+}
 
 export async function getGasPriceEstimation(args: { aptosConfig: AptosConfig }) {
   const { aptosConfig } = args;
@@ -17,6 +33,6 @@ export async function getGasPriceEstimation(args: { aptosConfig: AptosConfig }) 
     endpoint: "estimate_gas_price",
     originMethod: "getGasPriceEstimation",
     overrides: { ...aptosConfig.clientConfig },
-  });
+  }, aptosConfig);
   return data;
 }
