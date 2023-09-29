054bd8e6aa7568ffd0f27c07d74c368ed560d251
Brian Cho <brian@aptoslabs.com>
2023-09-29 09:58:49 -0700

    2x backlog_txn_limit_count

--- config/src/config/quorum_store_config.rs
+++ config/src/config/quorum_store_config.rs
@@ -26,7 +26,7 @@ impl Default for QuorumStoreBackPressureConfig {
     fn default() -> QuorumStoreBackPressureConfig {
         QuorumStoreBackPressureConfig {
             // QS will be backpressured if the remaining total txns is more than this number
-            backlog_txn_limit_count: MAX_SENDING_BLOCK_TXNS_QUORUM_STORE_OVERRIDE * 3,
+            backlog_txn_limit_count: MAX_SENDING_BLOCK_TXNS_QUORUM_STORE_OVERRIDE * 6,
             // QS will create batches at the max rate until this number is reached
             backlog_per_validator_batch_limit_count: 4,
             decrease_duration_ms: 1000,
