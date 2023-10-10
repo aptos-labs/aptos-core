dc648ea050df2f06084e35ab3edde94c057391c3
Maayan <maayan@aptoslabs.com>
2023-10-10 13:47:06 -0700

    txn_submission_flow (#10348)
    Co-authored-by: Greg Nazario <greg@gnazar.io>

--- ecosystem/typescript/sdk_v2/src/utils/const.ts
+++ ecosystem/typescript/sdk_v2/src/utils/const.ts
@@ -8,3 +8,7 @@ export enum AptosApiType {
   INDEXER,
   FAUCET,
 }
+
+export const DEFAULT_MAX_GAS_AMOUNT = 200000;
+// Transaction expire timestamp
+export const DEFAULT_TXN_EXP_SEC_FROM_NOW = 20;
