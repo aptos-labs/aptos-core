c4a646f6845dfa32bad146c2fc3ed7a39296cb7f
David Wolinsky <isaac.wolinsky@gmail.com>
2023-10-17 08:17:32 -0700

    [authenticator] update open api

---
+++ ecosystem/typescript/sdk/src/generated/models/AccountSignature_SingleKeySignature.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { SingleKeySignature } from './SingleKeySignature';
+
+export type AccountSignature_SingleKeySignature = ({
+    type: string;
+} & SingleKeySignature);
+
