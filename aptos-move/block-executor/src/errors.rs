d2ca22f5191e49ed1837430ae5e56f3ac741bc48
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2023-10-19 11:01:39 -0700

    [agg_v2] Fallback To Sequential on PanicError::CodeInvariantError (#10531)

--- aptos-move/block-executor/src/errors.rs
+++ aptos-move/block-executor/src/errors.rs
@@ -2,14 +2,19 @@
 // Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use aptos_aggregator::types::PanicOr;
+
+/// The same module access path for module was both read & written during speculative executions.
+/// This may trigger a race due to the Move-VM loader cache implementation, and mitigation requires
+/// aborting the parallel execution pipeline and falling back to the sequential execution.
+/// TODO: (short-mid term) relax the limitation, and (mid-long term) provide proper multi-versioning
+/// for code (like data) for the cache.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct ModulePathReadWrite;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Error<E> {
-    /// The same module access path for module was both read & written during speculative executions.
-    /// This may trigger a race due to the Move-VM loader cache implementation, and mitigation requires
-    /// aborting the parallel execution pipeline and falling back to the sequential execution.
-    /// TODO: (short-med term) relax the limitation, and (mid-long term) provide proper multi-versioning
-    /// for code (like data) for the cache.
-    ModulePathReadWrite,
+    FallbackToSequential(PanicOr<ModulePathReadWrite>),
     /// Execution of a thread yields a non-recoverable error, such error will be propagated back to
     /// the caller.
     UserError(E),
