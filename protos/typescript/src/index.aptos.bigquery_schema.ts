af4067b6606c0c4823dbb640e8f2db4cb2a1a7f9
Daniel Porteous (dport) <daniel@dport.me>
2023-10-23 07:42:38 -0700

    Unify protos, add codegen for all languages, set up for package publishing (#10555)

---
+++ protos/typescript/src/index.aptos.bigquery_schema.ts
@@ -0,0 +1,3 @@
+/* eslint-disable */
+
+export * as transaction from "./index.aptos.bigquery_schema.transaction";
---
+++ protos/typescript/src/index.aptos.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+export * as bigquery_schema from "./index.aptos.bigquery_schema";
+export * as util from "./index.aptos.util";
+export * as transaction from "./index.aptos.transaction";
+export * as indexer from "./index.aptos.indexer";
+export * as internal from "./index.aptos.internal";
+export * as remote_executor from "./index.aptos.remote_executor";
