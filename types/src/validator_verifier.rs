1923d412724134dafb97ef5fcb613336a60080f7
Sital Kedia <skedia@aptoslabs.com>
2023-10-24 13:40:15 -0700

    2f + 1

--- types/src/validator_verifier.rs
+++ types/src/validator_verifier.rs
@@ -147,9 +147,7 @@ impl ValidatorVerifier {
         let quorum_voting_power = if validator_infos.is_empty() {
             0
         } else {
-            // FIXME(aldenhu)
-            // total_voting_power * 2 / 3 + 1
-            total_voting_power
+            total_voting_power * 2 / 3 + 1
         };
         Self::build_index(validator_infos, quorum_voting_power, total_voting_power)
     }
