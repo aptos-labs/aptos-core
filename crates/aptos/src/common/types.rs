7e49c084dda971c0e606e5f16a38cfb6ecb3fe16
Daniel Porteous <daniel@dport.me>
2023-10-24 09:30:07 +0100

    [CLI] Add support for setting an API key

--- crates/aptos/src/common/types.rs
+++ crates/aptos/src/common/types.rs
@@ -972,6 +972,12 @@ pub struct RestOptions {
     /// Connection timeout in seconds, used for the REST endpoint of the fullnode
     #[clap(long, default_value_t = DEFAULT_EXPIRATION_SECS, alias = "connection-timeout-s")]
     pub connection_timeout_secs: u64,
+
+    /// Key to use for ratelimiting purposes with the node API. This value will be used
+    /// as `Authorization: Bearer <key>`. You may also set this with the NODE_API_KEY
+    /// environment variable.
+    #[clap(long, env)]
+    pub node_api_key: Option<String>,
 }
 
 impl RestOptions {
@@ -979,6 +985,7 @@ impl RestOptions {
         RestOptions {
             url,
             connection_timeout_secs: connection_timeout_secs.unwrap_or(DEFAULT_EXPIRATION_SECS),
+            node_api_key: None,
         }
     }
 
@@ -1000,10 +1007,13 @@ impl RestOptions {
     }
 
     pub fn client(&self, profile: &ProfileOptions) -> CliTypedResult<Client> {
-        Ok(Client::builder(AptosBaseUrl::Custom(self.url(profile)?))
+        let mut client = Client::builder(AptosBaseUrl::Custom(self.url(profile)?))
             .timeout(Duration::from_secs(self.connection_timeout_secs))
-            .header(aptos_api_types::X_APTOS_CLIENT, X_APTOS_CLIENT_VALUE)?
-            .build())
+            .header(aptos_api_types::X_APTOS_CLIENT, X_APTOS_CLIENT_VALUE)?;
+        if let Some(node_api_key) = &self.node_api_key {
+            client = client.api_key(node_api_key)?;
+        }
+        Ok(client.build())
     }
 }
 
