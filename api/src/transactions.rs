073baece01a2f103c5eaaead0369404126b70f6f
Runtian Zhou <runtian@aptoslabs.com>
2023-10-26 11:27:12 -0700

    [api] Add contextual information to VMStatus::Error

--- api/src/transactions.rs
+++ api/src/transactions.rs
@@ -38,6 +38,7 @@ use aptos_types::{
     vm_status::StatusCode,
 };
 use aptos_vm::{data_cache::AsMoveResolver, AptosVM};
+use move_core_types::vm_status::VMStatus;
 use poem_openapi::{
     param::{Path, Query},
     payload::Json,
@@ -1190,7 +1191,7 @@ impl TransactionsApi {
 
         // Simulate transaction
         let state_view = self.context.latest_state_view_poem(&ledger_info)?;
-        let (_, output) = AptosVM::simulate_signed_transaction(&txn, &state_view);
+        let (vm_status, output) = AptosVM::simulate_signed_transaction(&txn, &state_view);
         let version = ledger_info.version();
 
         // Ensure that all known statuses return their values in the output (even if they aren't supposed to)
@@ -1232,7 +1233,22 @@ impl TransactionsApi {
                 let mut user_transactions = Vec::new();
                 for transaction in transactions.into_iter() {
                     match transaction {
-                        Transaction::UserTransaction(user_txn) => user_transactions.push(*user_txn),
+                        Transaction::UserTransaction(user_txn) => {
+                            let mut txn = *user_txn;
+                            match &vm_status {
+                                VMStatus::Error {
+                                    message: Some(msg), ..
+                                }
+                                | VMStatus::ExecutionFailure {
+                                    message: Some(msg), ..
+                                } => {
+                                    txn.info.vm_status +=
+                                        format!("\nExecution failed with status: {}", msg).as_str();
+                                },
+                                _ => (),
+                            }
+                            user_transactions.push(txn);
+                        },
                         _ => {
                             return Err(SubmitTransactionError::internal_with_code(
                                 "Simulation transaction resulted in a non-UserTransaction",
