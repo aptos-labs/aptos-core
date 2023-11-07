890f2e425fafadc189f287fb3f8adf23a9b8627e
Josh Lind <josh.lind@hotmail.com>
2023-11-07 14:58:33 -0500

    [Node Configs] Hard code specific features.

--- config/src/config/state_sync_config.rs
+++ config/src/config/state_sync_config.rs
@@ -16,8 +16,8 @@ const MAX_MESSAGE_SIZE: usize = 60 * 1024 * 1024; /* 60 MiB */
 // The maximum chunk sizes for data client requests and response
 const MAX_EPOCH_CHUNK_SIZE: u64 = 200;
 const MAX_STATE_CHUNK_SIZE: u64 = 4000;
-const MAX_TRANSACTION_CHUNK_SIZE: u64 = 20_000;
-const MAX_TRANSACTION_OUTPUT_CHUNK_SIZE: u64 = 20_000;
+const MAX_TRANSACTION_CHUNK_SIZE: u64 = 30_000;
+const MAX_TRANSACTION_OUTPUT_CHUNK_SIZE: u64 = 30_000;
 
 // The maximum number of concurrent requests to send
 const MAX_CONCURRENT_REQUESTS: u64 = 6;
