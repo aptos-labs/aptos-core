2a4f1492291775cc9a298e60cc49636c821b3eec
Piotr Olszewski <asmie@asmie.pl>
2023-11-16 17:23:01 +0100

    [move-mutator] creating Options from command line arguments

--- third_party/move/tools/move-mutator/src/lib.rs
+++ third_party/move/tools/move-mutator/src/lib.rs
@@ -1,3 +1,4 @@
+use clap::{Arg, Command};
 use serde::{Deserialize, Serialize};
 
 /// Move mutator options
@@ -16,6 +17,47 @@ impl Default for Options {
     }
 }
 
+impl Options {
+    /// Creates Options struct from command line arguments.
+    pub fn create_from_args(args: &[String]) -> anyhow::Result<Options> {
+        let cli = Command::new("mutate")
+            .version("0.1.0")
+            .about("The Move Mutator")
+            .author("Eiger Team")
+            .arg(
+                Arg::new("sources")
+                    .num_args(1..)
+                    .value_name("PATH_TO_SOURCE_FILE")
+                    .help("the source files to mutate"),
+            )
+            .after_help("See `move-mutator/doc` and `README.md` for documentation.");
+
+        // Parse the arguments. This will abort the program on parsing errors and print help.
+        // It will also accept options like --help.
+        let matches = cli.get_matches_from(args);
+
+        // Initialize options.
+        let get_vec = |s: &str| -> Vec<String> {
+            match matches.get_many::<String>(s) {
+                Some(vs) => vs.map(|v| v.to_string()).collect(),
+                _ => vec![],
+            }
+        };
+
+        let mut options = Options::default();
+
+        if matches
+            .get_many::<String>("sources")
+            .unwrap_or_default()
+            .len()
+            > 0
+        {
+            options.move_sources = get_vec("sources");
+        }
+
+        Ok(options)
+    }
+}
 
 /// Runs the Move mutator tool.
 /// Entry point for the Move mutator tool both for the CLI and the Rust API.
