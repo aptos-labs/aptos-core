aaeca606a79da397ddae7e00e695e3533d0ed953
Josh Lind <josh.lind@hotmail.com>
2023-11-16 17:05:45 -0500

    [Node Config] Make Chain ID optional in the sanitizer & optimizer.

--- config/src/config/execution_config.rs
+++ config/src/config/execution_config.rs
@@ -132,24 +132,27 @@ impl ConfigSanitizer for ExecutionConfig {
     fn sanitize(
         node_config: &NodeConfig,
         _node_type: NodeType,
-        chain_id: ChainId,
+        chain_id: Option<ChainId>,
     ) -> Result<(), Error> {
         let sanitizer_name = Self::get_sanitizer_name();
         let execution_config = &node_config.execution;
 
         // If this is a mainnet node, ensure that additional verifiers are enabled
-        if chain_id.is_mainnet() {
-            if !execution_config.paranoid_hot_potato_verification {
-                return Err(Error::ConfigSanitizerFailed(
-                    sanitizer_name,
-                    "paranoid_hot_potato_verification must be enabled for mainnet nodes!".into(),
-                ));
-            }
-            if !execution_config.paranoid_type_verification {
-                return Err(Error::ConfigSanitizerFailed(
-                    sanitizer_name,
-                    "paranoid_type_verification must be enabled for mainnet nodes!".into(),
-                ));
+        if let Some(chain_id) = chain_id {
+            if chain_id.is_mainnet() {
+                if !execution_config.paranoid_hot_potato_verification {
+                    return Err(Error::ConfigSanitizerFailed(
+                        sanitizer_name,
+                        "paranoid_hot_potato_verification must be enabled for mainnet nodes!"
+                            .into(),
+                    ));
+                }
+                if !execution_config.paranoid_type_verification {
+                    return Err(Error::ConfigSanitizerFailed(
+                        sanitizer_name,
+                        "paranoid_type_verification must be enabled for mainnet nodes!".into(),
+                    ));
+                }
             }
         }
 
@@ -179,7 +182,8 @@ mod test {
         };
 
         // Sanitize the config and verify that it succeeds
-        ExecutionConfig::sanitize(&node_config, NodeType::Validator, ChainId::mainnet()).unwrap();
+        ExecutionConfig::sanitize(&node_config, NodeType::Validator, Some(ChainId::mainnet()))
+            .unwrap();
     }
 
     #[test]
@@ -196,7 +200,7 @@ mod test {
 
         // Sanitize the config and verify that it fails
         let error =
-            ExecutionConfig::sanitize(&node_config, NodeType::Validator, ChainId::mainnet())
+            ExecutionConfig::sanitize(&node_config, NodeType::Validator, Some(ChainId::mainnet()))
                 .unwrap_err();
         assert!(matches!(error, Error::ConfigSanitizerFailed(_, _)));
     }
@@ -215,7 +219,7 @@ mod test {
 
         // Sanitize the config and verify that it fails
         let error =
-            ExecutionConfig::sanitize(&node_config, NodeType::Validator, ChainId::mainnet())
+            ExecutionConfig::sanitize(&node_config, NodeType::Validator, Some(ChainId::mainnet()))
                 .unwrap_err();
         assert!(matches!(error, Error::ConfigSanitizerFailed(_, _)));
     }
