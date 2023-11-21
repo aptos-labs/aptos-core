903e482e61e7b189daceeb6b7a381b797d68bd5e
Sherry Xiao <sherryxiao.py@gmail.com>
2023-11-21 09:43:52 -0800

    cherry-pick two improvements (#11020)
    * [vm][loader] optimize memory usage for types
    
    * Fix OOM for string transaction arguments (#189)
    
    ---------
    
    Co-authored-by: Victor Gao <vgao1996@gmail.com>
    Co-authored-by: George Mitenkov <georgemitenk0v@gmail.com>

--- third_party/move/move-vm/runtime/src/interpreter.rs
+++ third_party/move/move-vm/runtime/src/interpreter.rs
@@ -1040,7 +1040,8 @@ fn check_depth_of_type_impl(
         | Type::Signer => check_depth!(0),
         // Even though this is recursive this is OK since the depth of this recursion is
         // bounded by the depth of the type arguments, which we have already checked.
-        Type::Reference(ty) | Type::MutableReference(ty) | Type::Vector(ty) => {
+        Type::Vector(ty) => check_depth_of_type_impl(resolver, ty, max_depth, check_depth!(1))?,
+        Type::Reference(ty) | Type::MutableReference(ty) => {
             check_depth_of_type_impl(resolver, ty, max_depth, check_depth!(1))?
         },
         Type::Struct(si) => {
@@ -1658,7 +1659,7 @@ impl Frame {
                 }
                 interpreter
                     .operand_stack
-                    .push_ty(Type::Vector(Box::new(ty)))?;
+                    .push_ty(Type::Vector(Arc::new(ty)))?;
             },
             Bytecode::VecLen(si) => {
                 let ty = resolver.instantiate_single_type(*si, ty_args)?;
