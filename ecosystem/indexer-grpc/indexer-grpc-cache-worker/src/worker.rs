4e55fcb50b8903af15e8ad7d6b3b6424f4431f3a
Larry Liu <larry@aptoslabs.com>
2023-11-30 22:49:12 -0800

    add cache worker improvement.

--- ecosystem/indexer-grpc/indexer-grpc-cache-worker/src/worker.rs
+++ ecosystem/indexer-grpc/indexer-grpc-cache-worker/src/worker.rs
@@ -12,7 +12,7 @@ use aptos_indexer_grpc_utils::{
     counters::{log_grpc_step, IndexerGrpcStep},
     create_grpc_client,
     file_store_operator::{
-        FileStoreMetadata, FileStoreOperator, GcsFileStoreOperator, LocalFileStoreOperator, self,
+        FileStoreMetadata, FileStoreOperator, GcsFileStoreOperator, LocalFileStoreOperator,
     },
     types::RedisUrl,
 };
@@ -28,12 +28,6 @@ use url::Url;
 
 type ChainID = u32;
 type StartingVersion = u64;
-
-const GCS_LOOKUP_FREQUENCY_IN_SECS: u64 = 60;
-const FILE_STORE_VERSIONS_RESERVED: u64 = 30_000;
-// Cache worker will wait if filestore is behind by
-// `FILE_STORE_VERSIONS_RESERVED` versions
-const CACHE_WORKER_WAIT_FOR_FILE_STORE_IN_SECS: u64 = 1;
 
 const GCS_LOOKUP_FREQUENCY_IN_SECS: u64 = 60;
 const FILE_STORE_VERSIONS_RESERVED: u64 = 30_000;
@@ -326,8 +320,7 @@ async fn process_streaming_response(
         }
     }
     let mut current_version = starting_version;
-    let mut starting_time = std::time::Instant::now();
-    let mut last_file_update_check_timestamp = std::time::Instant::now();
+    let mut last_file_update_check_timestamp = std::time::Instant::now();    let mut starting_time = std::time::Instant::now();
 
     // 4. Process the streaming response.
     while let Some(received) = resp_stream.next().await {
