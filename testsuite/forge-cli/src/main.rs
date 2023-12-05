ce70eabee4dfaed048508a2dd554cba7cfccdb1a
Sital Kedia <skedia@aptoslabs.com>
2023-12-05 18:59:53 +0530

    [Forge] Remove system metrics threashold check for max load test

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1804,12 +1804,6 @@ fn realistic_env_max_load_test(
                     // Give at least 60s for catchup, give 10% of the run for longer durations.
                     (duration.as_secs() / 10).max(60),
                 )
-                .add_system_metrics_threshold(SystemMetricsThreshold::new(
-                    // Check that we don't use more than 16 CPU cores for 30% of the time.
-                    MetricsThreshold::new(16.0, max_cpu_threshold),
-                    // Check that we don't use more than 10 GB of memory for 30% of the time.
-                    MetricsThreshold::new_gb(10.0, 30),
-                ))
                 .add_latency_threshold(3.4, LatencyType::P50)
                 .add_latency_threshold(4.5, LatencyType::P90)
                 .add_latency_breakdown_threshold(LatencyBreakdownThreshold::new_with_breach_pct(
