e992380503df2495eaca96cba2c197c4c1793691
Zekun Wang <zekun.wang@aptoslabs.com>
2023-12-06 01:40:12 -0500

    change impl

--- third_party/move/move-compiler-v2/src/pipeline/ability_checker.rs
+++ third_party/move/move-compiler-v2/src/pipeline/ability_checker.rs
@@ -1,11 +1,11 @@
 //! Checks for ability violations.
 
 use move_binary_format::file_format::{Ability, AbilitySet};
-use move_model::{model::{FunctionEnv, Loc, QualifiedId, ModuleId, StructId, FunId}, ty::Type, ast::TempIndex};
+use move_model::{model::{FunctionEnv, Loc, QualifiedId, ModuleId, StructId, FunId}, ty::{Type, PrimitiveType}, ast::TempIndex};
 use move_stackless_bytecode::{
     function_target::{FunctionData, FunctionTarget},
     function_target_pipeline::{FunctionTargetProcessor, FunctionTargetsHolder},
-    stackless_bytecode::{AssignKind, AttrId, Bytecode, Operation},
+    stackless_bytecode::{AssignKind, Bytecode, Operation},
 };
 
 // Returns the abilities of the given type.
@@ -63,28 +63,10 @@ fn check_drop_for_temp_with_msg(func_target: &FunctionTarget, t: TempIndex, loc:
 fn check_write_ref(target: &FunctionTarget, t: TempIndex, loc: &Loc) {
     if let Type::Reference(_, ty) = target.get_local_type(t) {
         // todo: check key, store
-        check_drop(target, ty, loc, "cannot drop")
+        check_drop(target, ty, loc, "write_ref: cannot drop")
     } else {
         panic!("ICE typing error")
     }
-}
-
-/// Determines if the given type has the store constraint.
-fn has_store(func_target: &FunctionTarget, ty: &Type) -> bool {
-    type_abilities(func_target, ty).has_ability(Ability::Store)
-}
-
-// Checks if the given type has constraint store, and add diagnostics if not.
-fn check_store(func_target: &FunctionTarget, ty: &Type, loc: &Loc, err_msg: &str) {
-	if !has_store(func_target, ty) {
-		func_target.global_env().error(loc, err_msg)
-	}
-}
-
-// Checks if the given temporary variable has constraint store, and add diagnostics if not.
-fn check_store_for_temp_with_msg(func_target: &FunctionTarget, t: TempIndex, loc: &Loc, err_msg: &str) {
-	let ty = func_target.get_local_type(t);
-	check_store(func_target, ty, loc, err_msg)
 }
 
 /// Determines if the given type has the key constraint.
@@ -99,24 +81,26 @@ fn check_key(func_target: &FunctionTarget, ty: &Type, loc: &Loc, err_msg: &str) 
 	}
 }
 
-// Checks if the given temporary variable has constraint key, and add diagnostics if not.
-fn check_key_for_temp_with_msg(func_target: &FunctionTarget, t: TempIndex, loc: &Loc, err_msg: &str) {
-	let ty = func_target.get_local_type(t);
-	check_key(func_target, ty, loc, err_msg)
-}
-
 // checks that the given type is instantiated with types satisfying their ability constraints
 // on the type parameter
-fn check_struct_inst(target: &FunctionTarget, mid: ModuleId, sid: StructId, inst: &[Type], loc: &Loc) {
+fn check_struct_inst(target: &FunctionTarget, mid: ModuleId, sid: StructId, inst: &[Type], loc: &Loc) -> AbilitySet {
     let qid = QualifiedId{ module_id: mid, id: sid };
     let struct_env = target.global_env().get_struct(qid);
+    let struct_abilities = struct_env.get_abilities();
+    let mut ty_args_abilities_meet = AbilitySet::ALL;
     for (param, ty) in struct_env.get_type_parameters().iter().zip(inst.iter()) {
         let required_abilities = param.1.abilities;
-        let given_abilities = type_abilities(target, ty);
+        let given_abilities = check_instantiation(target, ty, loc);
         // todo: which field, why
         if !required_abilities.is_subset(given_abilities) {
             target.global_env().error(loc, "invalid instantiation")
         }
+        ty_args_abilities_meet = ty_args_abilities_meet.intersect(given_abilities);
+    }
+    if struct_abilities.has_ability(Ability::Key) && ty_args_abilities_meet.has_ability(Ability::Store) {
+        struct_abilities.intersect(ty_args_abilities_meet).add(Ability::Key)
+    } else {
+        struct_abilities.intersect(ty_args_abilities_meet).remove(Ability::Key)
     }
 }
 
@@ -125,11 +109,60 @@ fn check_fun_inst(target: &FunctionTarget, mid: ModuleId, fid: FunId, inst: &[Ty
     let fun_env = target.global_env().get_function(qid);
     for (param, ty) in fun_env.get_type_parameters().iter().zip(inst.iter()) {
         let required_abilities = param.1.abilities;
-        let given_abilities = type_abilities(target, ty);
+        let given_abilities = check_instantiation(target, ty, loc);
         // todo: which field, why
         if !required_abilities.is_subset(given_abilities) {
             target.global_env().error(loc, "invalid instantiation")
         }
+    }
+}
+
+// assuming all structs are well defined
+// checks whether the type is instantiated properly, i.e.,
+// - the type arguments given to the struct are instantiated properly
+// - the type arguments satisfy the ability constraints defined on the struct generics
+// and returns the abilities of the given type
+// `ty_params` specify the abilities held by type params
+pub fn check_instantiation(
+    target: &FunctionTarget,
+    ty: &Type,
+    loc: &Loc,
+) -> AbilitySet {
+    match ty {
+        Type::Primitive(p) => match p {
+            PrimitiveType::Bool
+            | PrimitiveType::U8
+            | PrimitiveType::U16
+            | PrimitiveType::U32
+            | PrimitiveType::U64
+            | PrimitiveType::U128
+            | PrimitiveType::U256
+            | PrimitiveType::Num
+            | PrimitiveType::Range
+            | PrimitiveType::EventStore
+            | PrimitiveType::Address => AbilitySet::PRIMITIVES,
+            PrimitiveType::Signer => AbilitySet::SIGNER,
+        },
+        Type::Vector(et) => {
+            AbilitySet::VECTOR.intersect(check_instantiation(target, et, loc))
+        },
+        Type::Struct(mid, sid, insts) => {
+            check_struct_inst(target, *mid, *sid, insts, loc)
+        },
+        Type::TypeParameter(i) => {
+            if let Some(tp) = target.get_type_parameters().get(*i as usize) {
+                tp.1.abilities
+            } else {
+                panic!("ICE unbound type parameter")
+            }
+        },
+        Type::Reference(_, _) => AbilitySet::REFERENCES,
+        Type::Fun(_, _)
+        | Type::Tuple(_)
+        | Type::TypeDomain(_)
+        | Type::ResourceDomain(_, _, _)
+        | Type::Error
+        | Type::Var(_) => AbilitySet::EMPTY,
     }
 }
 
@@ -168,7 +201,7 @@ fn check_bytecode(target: &FunctionTarget, bytecode: &Bytecode) {
                 check_copy_for_temp_with_msg(target, *src, &loc, "cannot copy");
             }
         },
-        Bytecode::Call(attr_id, dsts, op, srcs, _) => {
+        Bytecode::Call(attr_id, _, op, srcs, _) => {
             use Operation::*;
 			let loc = target.get_bytecode_loc(*attr_id);
             match op {
