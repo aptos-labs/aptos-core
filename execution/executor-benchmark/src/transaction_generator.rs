0fd7ef657f59f1c748a58a460eefaaf0165b5265
Igor <igor-aptos@users.noreply.github.com>
2023-12-07 09:47:43 -0800

    Adding new workloads to transaction-generator

--- execution/executor-benchmark/src/transaction_generator.rs
+++ execution/executor-benchmark/src/transaction_generator.rs
@@ -214,8 +214,8 @@ impl TransactionGenerator {
         TransactionFactory::new(ChainId::test())
             .with_transaction_expiration_time(300)
             .with_gas_unit_price(100)
-            // TODO(Gas): double check if this is correct
-            .with_max_gas_amount(100_000)
+        // TODO(Gas): double check if this is correct
+        // .with_max_gas_amount(100_000)
     }
 
     // Write metadata
