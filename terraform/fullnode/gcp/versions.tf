00e4874909adf19d2ad1465e4c80c0ef894c6e60
darren <50218308+darren-wangg@users.noreply.github.com>
2023-12-07 16:40:09 -0800

    Import Terraform & Helm changes.
    GitOrigin-RevId: bd2a2959ce1f27383aefdc45fcaeab73958a6170

--- terraform/fullnode/gcp/versions.tf
+++ terraform/fullnode/gcp/versions.tf
@@ -1,11 +1,13 @@
 terraform {
-  required_version = "~> 1.3.6"
+  required_version = "~> 1.5.6"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = "~> 5.0.0"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0.0"
     }
     helm = {
       source = "hashicorp/helm"
