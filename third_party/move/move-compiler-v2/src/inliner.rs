cce41cf6e63bab020443407e7f609427a09616c2
Brian R. Murphy <132495859+brmataptos@users.noreply.github.com>
2023-12-10 11:20:02 -0800

    make it easier to isolate V2 tests by turning off V1 comparison if MOVE_TEST_DEBUG is enabled

--- third_party/move/move-compiler-v2/src/inliner.rs
+++ third_party/move/move-compiler-v2/src/inliner.rs
@@ -1218,7 +1218,7 @@ mod tests {
             (9, BTreeSet::new()),
         ]);
         let result = postorder(&entries, &call_graph);
-        eprintln!("result is {:#?}", &result);
+        // eprintln!("result is {:#?}", &result);
         assert!(
             result == vec![8, 7, 5, 6, 4, 3, 2, 1]
                 || result == vec![8, 7, 6, 5, 4, 3, 2, 1]
