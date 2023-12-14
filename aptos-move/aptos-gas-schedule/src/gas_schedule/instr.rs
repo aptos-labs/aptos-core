c51fc721224d2366dac8880a6aef4276ce4257bb
Victor Gao <10379359+vgao1996@users.noreply.github.com>
2023-12-13 21:43:58 -0800

    [gas] add script for mass-modifying the gas schedule (#11318)

--- aptos-move/aptos-gas-schedule/src/gas_schedule/instr.rs
+++ aptos-move/aptos-gas-schedule/src/gas_schedule/instr.rs
@@ -36,34 +36,17 @@ crate::gas_schedule::macros::define_gas_parameters!(
         [ld_true: InternalGas, "ld_true", 1200],
         [ld_false: InternalGas, "ld_false", 1200],
         [ld_const_base: InternalGas, "ld_const.base", 13000],
-        [
-            ld_const_per_byte: InternalGasPerByte,
-            "ld_const.per_byte",
-            700,
-            LD_CONST_PER_BYTE
-        ],
+        [ld_const_per_byte: InternalGasPerByte, "ld_const.per_byte", 700],
         // borrow
         [imm_borrow_loc: InternalGas, "imm_borrow_loc", 1200],
         [mut_borrow_loc: InternalGas, "mut_borrow_loc", 1200],
         [imm_borrow_field: InternalGas, "imm_borrow_field", 4000],
         [mut_borrow_field: InternalGas, "mut_borrow_field", 4000],
-        [
-            imm_borrow_field_generic: InternalGas,
-            "imm_borrow_field_generic",
-            4000
-        ],
-        [
-            mut_borrow_field_generic: InternalGas,
-            "mut_borrow_field_generic",
-            4000
-        ],
+        [imm_borrow_field_generic: InternalGas, "imm_borrow_field_generic", 4000],
+        [mut_borrow_field_generic: InternalGas, "mut_borrow_field_generic", 4000],
         // locals
         [copy_loc_base: InternalGas, "copy_loc.base", 1600],
-        [
-            copy_loc_per_abs_val_unit: InternalGasPerAbstractValueUnit,
-            "copy_loc.per_abs_val_unit",
-            80
-        ],
+        [copy_loc_per_abs_val_unit: InternalGasPerAbstractValueUnit, "copy_loc.per_abs_val_unit", 80],
         [move_loc_base: InternalGas, "move_loc.base", 2400],
         [st_loc_base: InternalGas, "st_loc.base", 2400],
         // call
@@ -71,41 +54,21 @@ crate::gas_schedule::macros::define_gas_parameters!(
         [call_per_arg: InternalGasPerArg, "call.per_arg", 2000],
         [call_per_local: InternalGasPerArg, { 1.. => "call.per_local" }, 2000],
         [call_generic_base: InternalGas, "call_generic.base", 20000],
-        [
-            call_generic_per_ty_arg: InternalGasPerArg,
-            "call_generic.per_ty_arg",
-            2000
-        ],
-        [
-            call_generic_per_arg: InternalGasPerArg,
-            "call_generic.per_arg",
-            2000
-        ],
+        [call_generic_per_ty_arg: InternalGasPerArg, "call_generic.per_ty_arg", 2000],
+        [call_generic_per_arg: InternalGasPerArg, "call_generic.per_arg", 2000],
         [call_generic_per_local: InternalGasPerArg, { 1.. => "call_generic.per_local" }, 2000],
         // struct
         [pack_base: InternalGas, "pack.base", 4400],
         [pack_per_field: InternalGasPerArg, "pack.per_field", 800],
         [pack_generic_base: InternalGas, "pack_generic.base", 4400],
-        [
-            pack_generic_per_field: InternalGasPerArg,
-            "pack_generic.per_field",
-            800
-        ],
+        [pack_generic_per_field: InternalGasPerArg, "pack_generic.per_field", 800],
         [unpack_base: InternalGas, "unpack.base", 4400],
         [unpack_per_field: InternalGasPerArg, "unpack.per_field", 800],
         [unpack_generic_base: InternalGas, "unpack_generic.base", 4400],
-        [
-            unpack_generic_per_field: InternalGasPerArg,
-            "unpack_generic.per_field",
-            800
-        ],
+        [unpack_generic_per_field: InternalGasPerArg, "unpack_generic.per_field", 800],
         // ref
         [read_ref_base: InternalGas, "read_ref.base", 4000],
-        [
-            read_ref_per_abs_val_unit: InternalGasPerAbstractValueUnit,
-            "read_ref.per_abs_val_unit",
-            80
-        ],
+        [read_ref_per_abs_val_unit: InternalGasPerAbstractValueUnit, "read_ref.per_abs_val_unit", 80],
         [write_ref_base: InternalGas, "write_ref.base", 4000],
         [freeze_ref: InternalGas, "freeze_ref", 200],
         // casting
@@ -137,52 +100,20 @@ crate::gas_schedule::macros::define_gas_parameters!(
         [le: InternalGas, "le", 3200],
         [ge: InternalGas, "ge", 3200],
         [eq_base: InternalGas, "eq.base", 2000],
-        [
-            eq_per_abs_val_unit: InternalGasPerAbstractValueUnit,
-            "eq.per_abs_val_unit",
-            80
-        ],
+        [eq_per_abs_val_unit: InternalGasPerAbstractValueUnit, "eq.per_abs_val_unit", 80],
         [neq_base: InternalGas, "neq.base", 2000],
-        [
-            neq_per_abs_val_unit: InternalGasPerAbstractValueUnit,
-            "neq.per_abs_val_unit",
-            80
-        ],
+        [neq_per_abs_val_unit: InternalGasPerAbstractValueUnit, "neq.per_abs_val_unit", 80],
         // global
-        [
-            imm_borrow_global_base: InternalGas,
-            "imm_borrow_global.base",
-            10000
-        ],
-        [
-            imm_borrow_global_generic_base: InternalGas,
-            "imm_borrow_global_generic.base",
-            10000
-        ],
-        [
-            mut_borrow_global_base: InternalGas,
-            "mut_borrow_global.base",
-            10000
-        ],
-        [
-            mut_borrow_global_generic_base: InternalGas,
-            "mut_borrow_global_generic.base",
-            10000
-        ],
+        [imm_borrow_global_base: InternalGas, "imm_borrow_global.base", 10000],
+        [imm_borrow_global_generic_base: InternalGas, "imm_borrow_global_generic.base", 10000],
+        [mut_borrow_global_base: InternalGas, "mut_borrow_global.base", 10000],
+        [mut_borrow_global_generic_base: InternalGas, "mut_borrow_global_generic.base", 10000],
         [exists_base: InternalGas, "exists.base", 5000],
         [exists_generic_base: InternalGas, "exists_generic.base", 5000],
         [move_from_base: InternalGas, "move_from.base", 7000],
-        [
-            move_from_generic_base: InternalGas,
-            "move_from_generic.base",
-            7000
-        ],
+        [move_from_generic_base: InternalGas, "move_from_generic.base", 7000],
         [move_to_base: InternalGas, "move_to.base", 10000],
-        [
-            move_to_generic_base: InternalGas,
-            "move_to_generic.base",
-            10000
-        ],
+        [move_to_generic_base: InternalGas, "move_to_generic.base", 10000],
         // vec
         [vec_len_base: InternalGas, "vec_len.base", 4400],
         [vec_imm_borrow_base: InternalGas, "vec_imm_borrow.base", 6600],
@@ -191,16 +122,8 @@ crate::gas_schedule::macros::define_gas_parameters!(
         [vec_pop_back_base: InternalGas, "vec_pop_back.base", 5200],
         [vec_swap_base: InternalGas, "vec_swap.base", 6000],
         [vec_pack_base: InternalGas, "vec_pack.base", 12000],
-        [
-            vec_pack_per_elem: InternalGasPerArg,
-            "vec_pack.per_elem",
-            800
-        ],
+        [vec_pack_per_elem: InternalGasPerArg, "vec_pack.per_elem", 800],
         [vec_unpack_base: InternalGas, "vec_unpack.base", 10000],
-        [
-            vec_unpack_per_expected_elem: InternalGasPerArg,
-            "vec_unpack.per_expected_elem",
-            800
-        ],
+        [vec_unpack_per_expected_elem: InternalGasPerArg, "vec_unpack.per_expected_elem", 800],
     ]
 );
