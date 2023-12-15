4debfe4fb9a9f3a1ef62a82669b325303b2e85fb
Mihir Kuma Singh <mihir39nr@gmail.com>
2023-12-15 12:30:32 +0530

    Solved Linting Issue

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -715,10 +715,10 @@ function install_lld {
 
 function install_libdw {
   # Right now, only install libdw for linux
-  if [[ "$(uname)" == "Linux" && "$PACKAGE_MANAGER" != "pacman"]]; then
+  if [[ "$(uname)" == "Linux" && "$PACKAGE_MANAGER" != "pacman" ]]; then
     install_pkg libdw-dev "$PACKAGE_MANAGER"
   fi
-  if [[ "$(uname)" == "Linux" && "$PACKAGE_MANAGER" == "pacman"]]; then
+  if [[ "$(uname)" == "Linux" && "$PACKAGE_MANAGER" == "pacman" ]]; then
     install_pkg libelf "$PACKAGE_MANAGER"
   fi
 }
