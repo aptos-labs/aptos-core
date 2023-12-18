50839587abb266560ca6b98c28a002035a668fe1
zhoujun.ma <zhoujun@aptoslabs.com>
2023-12-17 17:52:31 -0800

    dkg jwk initial

---
+++ config/src/config/jwk_consensus_config.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(default, deny_unknown_fields)]
+pub struct JWKConsensusConfig {
+    pub max_network_channel_size: usize,
+}
+
+impl Default for JWKConsensusConfig {
+    fn default() -> Self {
+        Self {
+            max_network_channel_size: 256,
+        }
+    }
+}
