d20fab97b7311cf04fe65cb8226debf9b2a0bdc4
Josh Lind <josh.lind@hotmail.com>
2023-12-18 19:43:54 -0500

    [State Sync] Update tests for dedicated commit handler.

--- api/openapi-spec-generator/src/fake_context.rs
+++ api/openapi-spec-generator/src/fake_context.rs
@@ -10,7 +10,7 @@ use std::sync::Arc;
 
 // This is necessary for building the API with how the code is structured currently.
 pub fn get_fake_context() -> Context {
-    let mempool = MockSharedMempool::new();
+    let mempool = MockSharedMempool::new_with_runtime();
     Context::new(
         ChainId::test(),
         Arc::new(MockDbReaderWriter),
