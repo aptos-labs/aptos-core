e81d9c49a8d39e7593dc2acc5122e5fe05abc7c6
Zekun Li <li.zekun@gmail.com>
2023-12-20 16:43:19 -0800

    [rand gen] remove trait Proof and RandStorage for the new pulling model

--- consensus/src/rand/rand_gen/storage/in_memory.rs
+++ consensus/src/rand/rand_gen/storage/in_memory.rs
@@ -2,76 +2,27 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::rand::rand_gen::{
-    storage::interface::{AugDataStorage, RandStorage},
-    types::{
-        AugData, AugDataId, AugmentedData, CertifiedAugData, Proof, RandDecision, RandShare, Share,
-        ShareId,
-    },
+    storage::interface::AugDataStorage,
+    types::{AugData, AugDataId, AugmentedData, CertifiedAugData},
 };
-use aptos_consensus_types::randomness::RandMetadata;
 use aptos_infallible::RwLock;
 use std::collections::HashMap;
 
-pub struct InMemRandDb<S, P, D> {
-    shares: RwLock<HashMap<ShareId, RandShare<S>>>,
-    decisions: RwLock<HashMap<RandMetadata, RandDecision<P>>>,
+pub struct InMemRandDb<D> {
     aug_data: RwLock<HashMap<AugDataId, AugData<D>>>,
     certified_aug_data: RwLock<HashMap<AugDataId, CertifiedAugData<D>>>,
 }
 
-impl<S, P, D> InMemRandDb<S, P, D> {
+impl<D> InMemRandDb<D> {
     pub fn new() -> Self {
         Self {
-            shares: RwLock::new(HashMap::new()),
-            decisions: RwLock::new(HashMap::new()),
             aug_data: RwLock::new(HashMap::new()),
             certified_aug_data: RwLock::new(HashMap::new()),
         }
     }
 }
 
-impl<S: Share, P: Proof<Share = S>, D: AugmentedData> RandStorage<S, P> for InMemRandDb<S, P, D> {
-    fn save_share(&self, share: &RandShare<S>) -> anyhow::Result<()> {
-        self.shares
-            .write()
-            .insert(share.share_id().clone(), share.clone());
-        Ok(())
-    }
-
-    fn save_decision(&self, decision: &RandDecision<P>) -> anyhow::Result<()> {
-        self.decisions
-            .write()
-            .insert(decision.rand_metadata().clone(), decision.clone());
-        Ok(())
-    }
-
-    fn get_all_shares(&self) -> anyhow::Result<Vec<(ShareId, RandShare<S>)>> {
-        Ok(self.shares.read().clone().into_iter().collect())
-    }
-
-    fn get_all_decisions(&self) -> anyhow::Result<Vec<(RandMetadata, RandDecision<P>)>> {
-        Ok(self.decisions.read().clone().into_iter().collect())
-    }
-
-    fn remove_shares(&self, shares: impl Iterator<Item = RandShare<S>>) -> anyhow::Result<()> {
-        for share in shares {
-            self.shares.write().remove(&share.share_id());
-        }
-        Ok(())
-    }
-
-    fn remove_decisions(
-        &self,
-        decisions: impl Iterator<Item = RandDecision<P>>,
-    ) -> anyhow::Result<()> {
-        for decision in decisions {
-            self.decisions.write().remove(decision.rand_metadata());
-        }
-        Ok(())
-    }
-}
-
-impl<S: Share, P: Proof<Share = S>, D: AugmentedData> AugDataStorage<D> for InMemRandDb<S, P, D> {
+impl<D: AugmentedData> AugDataStorage<D> for InMemRandDb<D> {
     fn save_aug_data(&self, aug_data: &AugData<D>) -> anyhow::Result<()> {
         self.aug_data
             .write()
