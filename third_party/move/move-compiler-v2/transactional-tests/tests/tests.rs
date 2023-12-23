66d35fb770cf8908b4e79192c5a4a0cda063a7c2
Wolfgang Grieskamp <wg@aptoslabs.com>
2023-12-23 17:07:33 -0600

    [compiler-v2] Only aggressively copy if type has drop ability (#11510)
    * [compiler-v2] Only aggressively copy if type has drop ability
    
    The hack to work around #11223 in live var analysis may cause ability errors if the value does not have the drop ability. This is because if we copy to aggressively, we may need to destroy values later, and this requires those to have a drop ability. This change fixes this by requiring to have drop to apply the hack.
    
    * Enable the fix, demonstrating the delta

--- third_party/move/move-compiler-v2/transactional-tests/tests/tests.rs
+++ third_party/move/move-compiler-v2/transactional-tests/tests/tests.rs
@@ -14,6 +14,12 @@ datatest_stable::harness!(run, TEST_DIR, r".*\.move$");
 /// Tests containing this string in their path will skip v1-v2 comparison
 const SKIP_V1_COMPARISON_PATH: &str = "/no-v1-comparison/";
 
+/// Tests which should be run with the `no-safety` experiment on.
+const NO_SAFETY_PATH: &str = "/no-safety/";
+
+/// Experiment env var
+const MOVE_COMPILER_EXP: &str = "MOVE_COMPILER_EXP";
+
 fn move_test_debug() -> bool {
     static MOVE_TEST_DEBUG: Lazy<bool> = Lazy::new(|| read_bool_env_var("MOVE_TEST_DEBUG"));
     *MOVE_TEST_DEBUG
@@ -21,6 +27,9 @@ fn move_test_debug() -> bool {
 
 fn run(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let p = path.to_str().unwrap_or_default();
+    if p.contains(NO_SAFETY_PATH) {
+        std::env::set_var(MOVE_COMPILER_EXP, "no-safety")
+    }
     let test_config = if p.contains(SKIP_V1_COMPARISON_PATH) || move_test_debug() {
         TestRunConfig::CompilerV2
     } else {
