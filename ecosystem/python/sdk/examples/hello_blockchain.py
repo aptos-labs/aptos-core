a0d6d95cf3a4d33c63be4b5a3be431ba4eb15d86
David Wolinsky <isaac.wolinsky@gmail.com>
2023-12-27 16:47:34 -0800

    [python-sdk] improve hello_blockchain with better api
    recent updates to package publishing mean more shared code and less
    copypasta

--- ecosystem/python/sdk/examples/hello_blockchain.py
+++ ecosystem/python/sdk/examples/hello_blockchain.py
@@ -14,16 +14,14 @@ One method to do so is to use the CLI:
 """
 
 import asyncio
-import os
 import sys
 from typing import Any, Dict, Optional
 
+import aptos_sdk.cli as aptos_sdk_cli
 from aptos_sdk.account import Account
 from aptos_sdk.account_address import AccountAddress
-from aptos_sdk.aptos_cli_wrapper import AptosCLIWrapper
 from aptos_sdk.async_client import FaucetClient, ResourceNotFound, RestClient
 from aptos_sdk.bcs import Serializer
-from aptos_sdk.package_publisher import PackagePublisher
 from aptos_sdk.transactions import (
     EntryFunction,
     TransactionArgument,
@@ -67,30 +65,14 @@ async def publish_contract(package_dir: str) -> AccountAddress:
     rest_client = HelloBlockchainClient(NODE_URL)
     faucet_client = FaucetClient(FAUCET_URL, rest_client)
     await faucet_client.fund_account(contract_publisher.address(), 10_000_000)
-
-    AptosCLIWrapper.compile_package(
-        package_dir, {"hello_blockchain": contract_publisher.address()}
-    )
-
-    module_path = os.path.join(
-        package_dir, "build", "Examples", "bytecode_modules", "message.mv"
-    )
-    with open(module_path, "rb") as f:
-        module = f.read()
-
-    metadata_path = os.path.join(
-        package_dir, "build", "Examples", "package-metadata.bcs"
-    )
-    with open(metadata_path, "rb") as f:
-        metadata = f.read()
-
-    package_publisher = PackagePublisher(rest_client)
-    txn_hash = await package_publisher.publish_package(
-        contract_publisher, metadata, [module]
-    )
-    await rest_client.wait_for_transaction(txn_hash)
-
     await rest_client.close()
+
+    await aptos_sdk_cli.publish_package(
+        package_dir,
+        {"hello_blockchain": contract_publisher.address()},
+        contract_publisher,
+        NODE_URL,
+    )
 
     return contract_publisher.address()
 
