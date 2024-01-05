1aa480c67a77310312bfd38033690e40f2fd0c30
Igor <igor-aptos@users.noreply.github.com>
2024-01-05 15:01:36 -0800

    reduce max_execution_gas / max_io_gas

--- aptos-move/aptos-gas-schedule/src/gas_schedule/transaction.rs
+++ aptos-move/aptos-gas-schedule/src/gas_schedule/transaction.rs
@@ -176,12 +176,12 @@ crate::gas_schedule::macros::define_gas_parameters!(
         [
             max_execution_gas: InternalGas,
             { 7.. => "max_execution_gas" },
-            3_676_000_000,
+            1_800_000_000, // 180ms of execution at 10k gas per ms
         ],
         [
             max_io_gas: InternalGas,
             { 7.. => "max_io_gas" },
-            8_000_000_000,
+            1_000_000_000, // 100ms of IO at 10k gas per ms
         ],
         [
             max_storage_fee: Fee,
