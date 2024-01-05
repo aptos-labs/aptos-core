81ced688dcc950411c55637b84e569a3d9f412af
zhoujun.ma <zhoujun@aptoslabs.com>
2024-01-05 11:55:47 -0800

    add jwk patch smoke test

--- types/src/jwks/mod.rs
+++ types/src/jwks/mod.rs
@@ -2,7 +2,7 @@
 
 use crate::{
     jwks::{rsa::RSA_JWK, unsupported::UnsupportedJWK},
-    move_any::{Any, AsMoveAny},
+    move_any::{Any as MoveAny, AsMoveAny},
 };
 use aptos_crypto::bls12381;
 use aptos_crypto_derive::{BCSCryptoHash, CryptoHasher};
@@ -27,22 +27,43 @@ pub struct SupportedOIDCProviders {
     pub providers: Vec<OIDCProvider>,
 }
 
-/// Move type `0x1::jwks::JWK` in rust.
+/// Reflection of Move type `0x1::jwks::JWK`.
+/// When you load an on-chain config that contains some JWK(s), the JWK will be of this type.
+/// When you call a Move function from rust that takes some JWKs as input, pass in JWKs of this type.
+/// Otherwise, it is recommended to convert this to the rust enum `JWK` for better rust experience.
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, CryptoHasher, BCSCryptoHash)]
-pub struct JWK {
-    variant: Any,
+pub struct JWKMoveStruct {
+    variant: MoveAny,
 }
 
-impl JWK {
-    pub fn new_rsa(rsa: RSA_JWK) -> Self {
-        Self {
-            variant: rsa.as_move_any(),
-        }
-    }
+pub enum JWK {
+    RSA(RSA_JWK),
+    Unsupported(UnsupportedJWK),
+}
 
-    pub fn new_unsupported(unsupported: UnsupportedJWK) -> Self {
-        Self {
-            variant: unsupported.as_move_any(),
+impl From<JWK> for JWKMoveStruct {
+    fn from(jwk: JWK) -> Self {
+        let variant = match jwk {
+            JWK::RSA(variant) => variant.as_move_any(),
+            JWK::Unsupported(variant) => variant.as_move_any(),
+        };
+        JWKMoveStruct { variant }
+    }
+}
+
+impl From<JWKMoveStruct> for JWK {
+    fn from(value: JWKMoveStruct) -> Self {
+        match value.variant.type_name.as_str() {
+            RSA_JWK::MOVE_TYPE_NAME => {
+                let rsa_jwk = MoveAny::unpack(RSA_JWK::MOVE_TYPE_NAME, value.variant).unwrap();
+                Self::RSA(rsa_jwk)
+            },
+            UnsupportedJWK::MOVE_TYPE_NAME => {
+                let unsupported_jwk =
+                    MoveAny::unpack(UnsupportedJWK::MOVE_TYPE_NAME, value.variant).unwrap();
+                Self::Unsupported(unsupported_jwk)
+            },
+            _ => unreachable!(),
         }
     }
 }
@@ -52,22 +73,30 @@ impl JWK {
 pub struct ProviderJWKs {
     pub issuer: Issuer,
     pub version: u64,
-    pub jwks: Vec<JWK>,
+    pub jwks: Vec<JWKMoveStruct>,
 }
 
 impl ProviderJWKs {
-    pub fn jwks(&self) -> &Vec<JWK> {
+    pub fn jwks(&self) -> &Vec<JWKMoveStruct> {
         &self.jwks
     }
 }
 
 /// Move type `0x1::jwks::JWKs` in rust.
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct AllProvidersJWKs {
     pub entries: Vec<ProviderJWKs>,
 }
 
 /// Move type `0x1::jwks::ObservedJWKs` in rust.
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct ObservedJWKs {
+    pub jwks: AllProvidersJWKs,
+}
+
+/// Reflection of Move type `0x1::jwks::ObservedJWKs`.
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub struct PatchedJWKs {
     pub jwks: AllProvidersJWKs,
 }
 
