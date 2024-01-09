ad7a0d30661ff967c9e0ce835a6e513898723baa
Piotr Olszewski <asmie@asmie.pl>
2024-01-09 12:22:01 +0100

    [move-spec-test] `aptos` command integration

--- crates/aptos/src/common/types.rs
+++ crates/aptos/src/common/types.rs
@@ -113,6 +113,8 @@ pub enum CliError {
     CoverageError(String),
     #[error("Move mutator failed: {0}")]
     MoveMutatorError(String),
+    #[error("Move spec-test failed: {0}")]
+    MoveSpecTestError(String),
 }
 
 impl CliError {
@@ -134,6 +136,7 @@ impl CliError {
             CliError::UnexpectedError(_) => "UnexpectedError",
             CliError::SimulationError(_) => "SimulationError",
             CliError::CoverageError(_) => "CoverageError",
+            CliError::MoveSpecTestError(_) => "MoveSpecTestError",
         }
     }
 }
