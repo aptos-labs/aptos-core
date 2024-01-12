01897ad05fc657a5a5af698ed1dd781ac04b4bfa
zhoujun.ma <zhoujun@aptoslabs.com>
2024-01-11 16:35:18 -0800

    clean up

--- dkg/src/counters.rs
+++ dkg/src/counters.rs
@@ -7,7 +7,7 @@ use once_cell::sync::Lazy;
 pub static PENDING_SELF_MESSAGES: Lazy<IntGauge> = Lazy::new(|| {
     register_int_gauge!(
         "aptos_dkg_pending_self_messages",
-        "Count of the pending DKG messages sent to itself in the channel"
+        "Count of the pending messages sent to itself in the channel"
     )
     .unwrap()
 });
