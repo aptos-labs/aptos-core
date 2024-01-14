4142e7ac1051035b70f2880080d0d23c2669a3be
Zekun Wang <zekun.wang@aptoslabs.com>
2024-01-14 00:42:04 -0500

    automatic derive impl of abstract domain

--- third_party/move/move-model/bytecode/src/dataflow_domains.rs
+++ third_party/move/move-model/bytecode/src/dataflow_domains.rs
@@ -317,3 +317,105 @@ impl<K: Ord + Clone, V: AbstractDomain + Clone + PartialEq> MapDomain<K, V> {
         self.extend(new_values);
     }
 }
+
+impl<A: AbstractDomain, B: AbstractDomain> AbstractDomain for (A, B) {
+    fn join(&mut self, other: &Self) -> JoinResult {
+        let mut join_res = JoinResult::Unchanged;
+        join_res = JoinResult::combine(join_res, self.0.join(&other.0));
+        join_res = JoinResult::combine(join_res, self.1.join(&other.1));
+        join_res
+    }
+}
+
+impl<T: AbstractDomain> AbstractDomain for Vec<T> {
+    fn join(&mut self, other: &Self) -> JoinResult {
+        self.iter_mut()
+            .zip(other.iter())
+            .fold(JoinResult::Unchanged, |acc, (x, y)| {
+                JoinResult::combine(acc, x.join(y))
+            })
+    }
+}
+
+/// Add a formal top and bottom element to type `T`
+#[derive(PartialEq, Eq, Clone)]
+pub enum Plus2<T> {
+    Top,
+    Mid(T),
+    Bot,
+}
+
+impl<T: Eq + Clone> AbstractDomain for Plus2<T> {
+    fn join(&mut self, other: &Self) -> JoinResult {
+        match (&self, other) {
+            (Plus2::Top, _) | (_, Plus2::Bot) => JoinResult::Unchanged,
+            (Plus2::Mid(_), _ /* Mid or Top */) => {
+                *self = Plus2::Top;
+                JoinResult::Changed
+            },
+            (Plus2::Bot, _ /* Mid or Tops */) => {
+                *self = other.clone();
+                JoinResult::Changed
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use abstract_domain_derive::AbstractDomain;
+
+    #[derive(AbstractDomain)]
+    struct Unit;
+
+    type Three = Plus2<()>;
+
+    #[derive(Eq, PartialEq, Clone, AbstractDomain)]
+    struct Foo(Three, Three);
+
+    #[derive(AbstractDomain)]
+    struct Bar {
+        #[join]
+        x: Three,
+        y: Three,
+    }
+
+    #[test]
+    fn test_unit() {
+        let mut x = Unit;
+        let y = Unit;
+        assert!(x.join(&y) == JoinResult::Unchanged);
+    }
+
+    #[test]
+    fn test_plus2() {
+        let mut top: Three = Plus2::Top;
+        let mut bot: Three = Plus2::Bot;
+        assert!(top.join(&bot) == JoinResult::Unchanged);
+        assert!(bot.join(&top) == JoinResult::Changed);
+        assert!(bot == Plus2::Top);
+    }
+
+    #[test]
+    fn test_named_tuple_derive() {
+        let mut x = Foo(Plus2::Bot, Plus2::Bot);
+        let y = Foo(Plus2::Top, Plus2::Mid(()));
+        assert!(x.join(&y) == JoinResult::Changed);
+        assert!(x == Foo(Plus2::Top, Plus2::Mid(())));
+    }
+
+    #[test]
+    fn test_struct_derive() {
+        let mut x = Bar {
+            x: Plus2::Bot,
+            y: Plus2::Bot,
+        };
+        let y = Bar {
+            x: Plus2::Top,
+            y: Plus2::Top,
+        };
+        assert!(x.join(&y) == JoinResult::Changed);
+        assert!(x.x == Plus2::Top && x.y == Plus2::Bot);
+    }
+}
