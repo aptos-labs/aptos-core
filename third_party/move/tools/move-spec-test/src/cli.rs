7749c1d636dc153c358ef2b2b81aaf6fc929015e
Piotr Olszewski <asmie@asmie.pl>
2024-01-17 13:24:27 +0100

    [move-spec-test] initial Move Prover calls
    This commit reads the produced report (mutator report) and calls Move Prover
    on each mutated file.

--- third_party/move/tools/move-spec-test/src/cli.rs
+++ third_party/move/tools/move-spec-test/src/cli.rs
@@ -25,3 +25,34 @@ pub struct Options {
     #[clap(long, value_parser)]
     pub extra_prover_args: Option<Vec<String>>,
 }
+
+/// This function creates a mutator CLI options from the given spec-test options.
+/// It clones the move_sources, include_only_files, exclude_files, and configuration_file from the given options.
+/// The rest of the options are set to default.
+pub fn create_mutator_options(options: &Options) -> move_mutator::cli::Options {
+    move_mutator::cli::Options {
+        move_sources: options.move_sources.clone(),
+        include_only_files: options.include_only_files.clone(),
+        exclude_files: options.exclude_files.clone(),
+        configuration_file: options.mutator_conf.clone(),
+        ..Default::default()
+    }
+}
+
+/// This function generates a prover CLI options from the given spec-test options.
+/// It first checks if a prover configuration file is provided in the options.
+/// If it is, it creates the prover options from the configuration file.
+/// If it is not, it checks if extra prover arguments are provided in the options.
+/// If they are, it creates the prover options from the arguments.
+/// Otherwise, it creates the default prover options.
+pub fn generate_prover_options(options: &Options) -> anyhow::Result<move_prover::cli::Options> {
+    let prover_conf = if let Some(conf) = &options.prover_conf {
+        move_prover::cli::Options::create_from_toml_file(conf.to_str().unwrap_or(""))?
+    } else if let Some(args) = &options.extra_prover_args {
+        move_prover::cli::Options::create_from_args(args)?
+    } else {
+        move_prover::cli::Options::default()
+    };
+
+    Ok(prover_conf)
+}
--- third_party/move/tools/move-spec-test/src/lib.rs
+++ third_party/move/tools/move-spec-test/src/lib.rs
@@ -1,9 +1,12 @@
 pub mod cli;
+mod prover;
 
 extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
+use crate::prover::prove;
+use anyhow::anyhow;
 use move_package::BuildConfig;
 use std::path::PathBuf;
 
@@ -24,15 +27,79 @@ use std::path::PathBuf;
 ///
 /// * `anyhow::Result<()>` - The result of the spec test.
 pub fn run_spec_test(
-    _options: cli::Options,
-    _config: BuildConfig,
-    _package_path: PathBuf,
+    options: cli::Options,
+    config: BuildConfig,
+    package_path: PathBuf,
 ) -> anyhow::Result<()> {
-    pretty_env_logger::init();
+    // We need to initialize logger using try_init() as it might be already initialized in some other tool
+    // (e.g. spec-test). If we use init() instead, we will get an abort.
+    let _ = pretty_env_logger::try_init();
 
     info!("Running spec test");
 
-    // TODO: implement
+    let mut mutator_conf = cli::create_mutator_options(&options);
+    let prover_conf = cli::generate_prover_options(&options)?;
+
+    // Setup temporary directory structure
+    let outdir = tempfile::tempdir()?.into_path();
+    let outdir_mutant = outdir.join("mutants");
+    let outdir_original = outdir.join("base");
+
+    std::fs::create_dir_all(&outdir_mutant)?;
+    std::fs::create_dir_all(&outdir_original)?;
+
+    mutator_conf.out_mutant_dir = outdir_mutant.clone();
+
+    debug!("Running the move mutator tool");
+
+    move_mutator::run_move_mutator(mutator_conf, config.clone(), package_path.clone())?;
+
+    let report =
+        move_mutator::report::Report::load_from_json_file(&outdir_mutant.join("report.json"))?;
+
+    // Proving part
+    move_mutator::compiler::copy_dir_all(&package_path, &outdir_original)?;
+
+    let mut error_writer = termcolor::StandardStream::stderr(termcolor::ColorChoice::Auto);
+
+    let result = prove(&config, &package_path, &prover_conf, &mut error_writer);
+
+    if let Err(e) = result {
+        let msg = format!(
+            "Original code verification failed! Prover failed with error: {}",
+            e
+        );
+        error!("{msg}");
+        return Err(anyhow!(msg));
+    }
+
+    // TODO: change this to report generation
+    let mut total_mutants = 0;
+    let mut killed_mutants = 0;
+
+    for elem in report.get_mutants() {
+        total_mutants += 1;
+
+        let result = prover::prove_mutant(
+            &config,
+            &elem.get_mutant_path(),
+            &elem.get_original_file_path(),
+            &package_path,
+            &prover_conf,
+            &outdir.join("prove"),
+            &mut error_writer,
+        );
+
+        if let Err(e) = result {
+            trace!("Mutant killed! Prover failed with error: {}", e);
+            killed_mutants += 1;
+        } else {
+            trace!("Mutant hasn't been killed!");
+        }
+    }
+
+    println!("Total mutants: {}", total_mutants);
+    println!("Killed mutants: {}", killed_mutants);
 
     Ok(())
 }
