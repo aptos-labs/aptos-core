45da5e453f63cb8a4dace063d44a84717bbec266
zhoujun.ma <zhoujun@aptoslabs.com>
2024-01-22 01:15:53 -0800

    adjust consensus::consensus_fault_tolerance::test_no_failures

--- testsuite/smoke-test/src/consensus/consensus_fault_tolerance.rs
+++ testsuite/smoke-test/src/consensus/consensus_fault_tolerance.rs
@@ -176,11 +176,6 @@ async fn test_no_failures() {
                 "no progress with active consensus, only {} transactions",
                 executed_transactions
             );
-            assert!(
-                executed_rounds >= 2,
-                "no progress with active consensus, only {} rounds",
-                executed_rounds
-            );
             Ok(())
         }),
         true,
