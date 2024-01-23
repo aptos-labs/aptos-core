54993b2ddcf4e2d23c2079a2a25f7ee8a079fb4f
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2024-01-23 10:52:36 -0800

    TransactionGenerator Workflow  (#11365)
    More sophisticated smart contracts require moving users through a set of states, i.e. registering, joining, playing, collecting winnings.
    
    These changes enable that:
    - split out the publishing from creating generator - as we want to publish a single module, and use it for multiple transactions
    - create a workflow based on pools, so that you can go to next transaction after all users have move to that stage.
    
    
    ### Test Plan
    
    Usage shown in https://github.com/aptos-labs/aptos-core/pull/11384

--- crates/transaction-generator-lib/src/account_generator.rs
+++ crates/transaction-generator-lib/src/account_generator.rs
@@ -1,22 +1,21 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
-use crate::{create_account_transaction, TransactionGenerator, TransactionGeneratorCreator};
-use aptos_infallible::RwLock;
-use aptos_logger::{info, sample, sample::SampleRate};
+use crate::{
+    create_account_transaction, ObjectPool, TransactionGenerator, TransactionGeneratorCreator,
+};
 use aptos_sdk::{
     move_types::account_address::AccountAddress,
     transaction_builder::TransactionFactory,
     types::{transaction::SignedTransaction, LocalAccount},
 };
-use rand::{rngs::StdRng, Rng, SeedableRng};
-use std::{sync::Arc, time::Duration};
+use rand::{rngs::StdRng, SeedableRng};
+use std::sync::Arc;
 
 pub struct AccountGenerator {
     rng: StdRng,
     txn_factory: TransactionFactory,
-    addresses_pool: Arc<RwLock<Vec<AccountAddress>>>,
-    accounts_pool: Arc<RwLock<Vec<LocalAccount>>>,
-    add_created_accounts_to_pool: bool,
+    addresses_pool: Option<Arc<ObjectPool<AccountAddress>>>,
+    accounts_pool: Option<Arc<ObjectPool<LocalAccount>>>,
     max_working_set: usize,
     creation_balance: u64,
 }
@@ -25,9 +24,8 @@ impl AccountGenerator {
     pub fn new(
         rng: StdRng,
         txn_factory: TransactionFactory,
-        addresses_pool: Arc<RwLock<Vec<AccountAddress>>>,
-        accounts_pool: Arc<RwLock<Vec<LocalAccount>>>,
-        add_created_accounts_to_pool: bool,
+        addresses_pool: Option<Arc<ObjectPool<AccountAddress>>>,
+        accounts_pool: Option<Arc<ObjectPool<LocalAccount>>>,
         max_working_set: usize,
         creation_balance: u64,
     ) -> Self {
@@ -36,38 +34,9 @@ impl AccountGenerator {
             txn_factory,
             addresses_pool,
             accounts_pool,
-            add_created_accounts_to_pool,
             max_working_set,
             creation_balance,
         }
-    }
-}
-
-fn add_to_sized_pool<T>(
-    pool: &RwLock<Vec<T>>,
-    mut addition: Vec<T>,
-    max_working_set: usize,
-    rng: &mut StdRng,
-) {
-    let mut current = pool.write();
-    if current.len() < max_working_set {
-        current.append(&mut addition);
-        sample!(
-            SampleRate::Duration(Duration::from_secs(120)),
-            info!("Accounts working set increased to {}", current.len())
-        );
-    } else {
-        let start = rng.gen_range(0, current.len() - addition.len());
-        current[start..start + addition.len()].swap_with_slice(&mut addition);
-
-        sample!(
-            SampleRate::Duration(Duration::from_secs(120)),
-            info!(
-                "Already at limit {} > {}, so exchanged accounts in working set",
-                current.len(),
-                max_working_set
-            )
-        );
     }
 }
 
@@ -94,29 +63,25 @@ impl TransactionGenerator for AccountGenerator {
             new_account_addresses.push(receiver_address);
         }
 
-        if self.add_created_accounts_to_pool {
-            add_to_sized_pool(
-                self.accounts_pool.as_ref(),
-                new_accounts,
-                self.max_working_set,
-                &mut self.rng,
-            );
-            add_to_sized_pool(
-                self.addresses_pool.as_ref(),
+        if let Some(addresses_pool) = &self.addresses_pool {
+            addresses_pool.add_to_pool_bounded(
                 new_account_addresses,
                 self.max_working_set,
                 &mut self.rng,
             );
         }
+        if let Some(accounts_pool) = &self.accounts_pool {
+            accounts_pool.add_to_pool_bounded(new_accounts, self.max_working_set, &mut self.rng);
+        }
+
         requests
     }
 }
 
 pub struct AccountGeneratorCreator {
     txn_factory: TransactionFactory,
-    addresses_pool: Arc<RwLock<Vec<AccountAddress>>>,
-    accounts_pool: Arc<RwLock<Vec<LocalAccount>>>,
-    add_created_accounts_to_pool: bool,
+    addresses_pool: Option<Arc<ObjectPool<AccountAddress>>>,
+    accounts_pool: Option<Arc<ObjectPool<LocalAccount>>>,
     max_working_set: usize,
     creation_balance: u64,
 }
@@ -124,22 +89,15 @@ pub struct AccountGeneratorCreator {
 impl AccountGeneratorCreator {
     pub fn new(
         txn_factory: TransactionFactory,
-        addresses_pool: Arc<RwLock<Vec<AccountAddress>>>,
-        accounts_pool: Arc<RwLock<Vec<LocalAccount>>>,
-        add_created_accounts_to_pool: bool,
+        addresses_pool: Option<Arc<ObjectPool<AccountAddress>>>,
+        accounts_pool: Option<Arc<ObjectPool<LocalAccount>>>,
         max_working_set: usize,
         creation_balance: u64,
     ) -> Self {
-        if add_created_accounts_to_pool {
-            addresses_pool.write().reserve(max_working_set);
-            accounts_pool.write().reserve(max_working_set);
-        }
-
         Self {
             txn_factory,
             addresses_pool,
             accounts_pool,
-            add_created_accounts_to_pool,
             max_working_set,
             creation_balance,
         }
@@ -153,7 +111,6 @@ impl TransactionGeneratorCreator for AccountGeneratorCreator {
             self.txn_factory.clone(),
             self.addresses_pool.clone(),
             self.accounts_pool.clone(),
-            self.add_created_accounts_to_pool,
             self.max_working_set,
             self.creation_balance,
         ))
