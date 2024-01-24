3bfb3c655eefd35d1215c8c221a9601e1785ddca
George Mitenkov <georgemitenk0v@gmail.com>
2024-01-23 16:21:28 -0800

    remove native vector rust implementation

--- third_party/move/move-stdlib/src/main.rs
+++ third_party/move/move-stdlib/src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     {
         time_it("Generating stdlib documentation", || {
             std::fs::remove_dir_all(move_stdlib::move_stdlib_docs_full_path()).unwrap_or(());
-            //std::fs::create_dir_all(&move_stdlib::move_stdlib_docs_full_path()).unwrap();
             move_stdlib::build_stdlib_doc(&move_stdlib::move_stdlib_docs_full_path());
         });
 
