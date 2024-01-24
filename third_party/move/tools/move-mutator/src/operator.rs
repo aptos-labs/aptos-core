0544fe694f4690e3de9d97b9e43722a250300a01
Piotr Olszewski <asmie@asmie.pl>
2024-01-24 12:18:55 +0100

    [move-mutator] mutation operators refactoring
    Mutation operators has been refactored. Now, there is a main trait which needs to be
    implemented by each new operator.
    
    Operators are now separated from each other - they are under new subdirectory, each operator
    in its own file.

--- third_party/move/tools/move-mutator/src/operator.rs
+++ third_party/move/tools/move-mutator/src/operator.rs
@@ -1,7 +1,9 @@
-use crate::report::{Mutation, Range};
+use crate::operators::binary::BinaryOperator;
+use crate::operators::unary::UnaryOperator;
+use crate::report::Mutation;
 use move_command_line_common::files::FileHash;
-use move_compiler::parser::ast::{BinOp, BinOp_, UnaryOp};
 use std::fmt;
+use std::fmt::Debug;
 
 /// Mutation result that contains the mutated source code and the modification that was applied.
 pub struct MutantInfo {
@@ -21,14 +23,10 @@ impl MutantInfo {
     }
 }
 
-/// The mutation operator to apply.
-#[derive(Debug, Copy, Clone)]
-pub enum MutationOperator {
-    BinaryOperator(BinOp),
-    UnaryOperator(UnaryOp),
-}
-
-impl MutationOperator {
+/// Trait for mutation operators.
+/// Mutation operators are used to apply mutations to the source code. To keep adding new mutation operators simple,
+/// we use a trait that all mutation operators implement.
+pub trait MutationOperatorTrait {
     /// Applies the mutation operator to the given source code.
     /// Returns differently mutated source code listings in a vector.
     ///
@@ -39,78 +37,33 @@ impl MutationOperator {
     /// # Returns
     ///
     /// * `Vec<MutantInfo>` - A vector of `MutantInfo` instances representing the mutated source code.
-    pub fn apply(&self, source: &str) -> Vec<MutantInfo> {
+    fn apply(&self, source: &str) -> Vec<MutantInfo>;
+
+    /// Returns the file hash of the file that this mutation operator is in.
+    fn get_file_hash(&self) -> FileHash;
+}
+
+/// The mutation operator to apply.
+#[derive(Debug, Copy, Clone)]
+pub enum MutationOperator {
+    BinaryOperator(BinaryOperator),
+    UnaryOperator(UnaryOperator),
+}
+
+impl MutationOperatorTrait for MutationOperator {
+    fn apply(&self, source: &str) -> Vec<MutantInfo> {
         debug!("Applying mutation operator: {self}");
 
         match self {
-            MutationOperator::BinaryOperator(bin_op) => {
-                let start = bin_op.loc.start() as usize;
-                let end = bin_op.loc.end() as usize;
-                let cur_op = &source[start..end];
-
-                // Group of exchangeable binary operators - we only want to replace the operator with a different one
-                // within the same group.
-                let ops: Vec<&str> = match bin_op.value {
-                    BinOp_::Add | BinOp_::Sub | BinOp_::Mul | BinOp_::Div | BinOp_::Mod => {
-                        vec!["+", "-", "*", "/", "%"]
-                    },
-                    BinOp_::BitOr | BinOp_::BitAnd | BinOp_::Xor => {
-                        vec!["|", "&", "^"]
-                    },
-                    BinOp_::Shl | BinOp_::Shr => {
-                        vec!["<<", ">>"]
-                    },
-                    _ => vec![],
-                };
-
-                ops.into_iter()
-                    .filter(|v| cur_op != *v)
-                    .map(|op| {
-                        let mut mutated_source = source.to_string();
-                        mutated_source.replace_range(start..end, op);
-                        MutantInfo::new(
-                            mutated_source,
-                            Mutation::new(
-                                Range::new(start, end),
-                                "binary_operator_replacement".to_string(),
-                                cur_op.to_string(),
-                                op.to_string(),
-                            ),
-                        )
-                    })
-                    .collect()
-            },
-            MutationOperator::UnaryOperator(unary_op) => {
-                let start = unary_op.loc.start() as usize;
-                let end = unary_op.loc.end() as usize;
-                let cur_op = &source[start..end];
-
-                // For unary operator mutations, we only need to replace the operator with a space (to ensure the same file length).
-                vec![" "]
-                    .into_iter()
-                    .map(|op| {
-                        let mut mutated_source = source.to_string();
-                        mutated_source.replace_range(start..end, op);
-                        MutantInfo::new(
-                            mutated_source,
-                            Mutation::new(
-                                Range::new(start, end),
-                                "unary_operator_replacement".to_string(),
-                                cur_op.to_string(),
-                                op.to_string(),
-                            ),
-                        )
-                    })
-                    .collect()
-            },
+            MutationOperator::BinaryOperator(bin_op) => bin_op.apply(source),
+            MutationOperator::UnaryOperator(unary_op) => unary_op.apply(source),
         }
     }
 
-    /// Returns the file hash of the file that this mutation operator is in.
-    pub fn get_file_hash(&self) -> FileHash {
+    fn get_file_hash(&self) -> FileHash {
         match self {
-            MutationOperator::BinaryOperator(bin_op) => bin_op.loc.file_hash(),
-            MutationOperator::UnaryOperator(unary_op) => unary_op.loc.file_hash(),
+            MutationOperator::BinaryOperator(bin_op) => bin_op.get_file_hash(),
+            MutationOperator::UnaryOperator(unary_op) => unary_op.get_file_hash(),
         }
     }
 }
@@ -118,22 +71,8 @@ impl MutationOperator {
 impl fmt::Display for MutationOperator {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            MutationOperator::BinaryOperator(bin_op) => write!(
-                f,
-                "BinaryOperator({}, location: file hash: {}, index start: {}, index stop: {})",
-                bin_op.value,
-                bin_op.loc.file_hash(),
-                bin_op.loc.start(),
-                bin_op.loc.end()
-            ),
-            MutationOperator::UnaryOperator(unary_op) => write!(
-                f,
-                "UnaryOperator({}, location: file hash: {}, index start: {}, index stop: {})",
-                unary_op.value,
-                unary_op.loc.file_hash(),
-                unary_op.loc.start(),
-                unary_op.loc.end()
-            ),
+            MutationOperator::BinaryOperator(bin_op) => write!(f, "{}", bin_op),
+            MutationOperator::UnaryOperator(unary_op) => write!(f, "{}", unary_op),
         }
     }
 }
@@ -152,7 +91,7 @@ mod tests {
             value: BinOp_::Mul,
             loc,
         };
-        let operator = MutationOperator::BinaryOperator(bin_op);
+        let operator = MutationOperator::BinaryOperator(BinaryOperator::new(bin_op));
         let source = "*";
         let expected = vec!["+", "-", "/", "%"];
         let result = operator.apply(source);
@@ -169,7 +108,7 @@ mod tests {
             value: UnaryOp_::Not,
             loc,
         };
-        let operator = MutationOperator::UnaryOperator(unary_op);
+        let operator = MutationOperator::UnaryOperator(UnaryOperator::new(unary_op));
         let source = "!";
         let expected = vec![" "];
         let result = operator.apply(source);
@@ -186,7 +125,7 @@ mod tests {
             value: BinOp_::Add,
             loc,
         };
-        let operator = MutationOperator::BinaryOperator(bin_op);
+        let operator = MutationOperator::BinaryOperator(BinaryOperator::new(bin_op));
         assert_eq!(operator.get_file_hash(), FileHash::new(""));
     }
 }
---
+++ third_party/move/tools/move-mutator/src/operators/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod binary;
+pub(crate) mod unary;
