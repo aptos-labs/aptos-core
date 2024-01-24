e87269ee4ec5fb1e5eb8e956ef8a6818e4e86f9e
Vineeth Kashyap <vineeth.rv@gmail.com>
2024-01-24 12:43:15 -0500

    [compiler-v2] Invalid friend declaration warnings (#11733)

--- third_party/move/move-model/src/lib.rs
+++ third_party/move/move-model/src/lib.rs
@@ -385,19 +385,8 @@ fn run_move_checker(env: &mut GlobalEnv, program: E::Program) {
         let module_def = expansion_script_to_module(script_def);
         module_translator.translate(loc, module_def, None);
     }
-
-    let module_table = std::mem::take(&mut builder.module_table);
-    // Patch up information about friend module ids, as all the modules have ids by now.
-    for module in env.module_data.iter_mut() {
-        let mut friend_modules = BTreeSet::new();
-        for friend_decl in module.friend_decls.iter_mut() {
-            if let Some(friend_mod_id) = module_table.get(&friend_decl.module_name) {
-                friend_decl.module_id = Some(*friend_mod_id);
-                friend_modules.insert(*friend_mod_id);
-            } // else: unresolved friend module, which should be reported elsewhere.
-        }
-        module.friend_modules = friend_modules;
-    }
+    // Perform any remaining friend-declaration checks and update friend module id information.
+    check_and_update_friend_info(builder);
     // Compute information derived from AST (currently callgraph)
     for module in env.module_data.iter_mut() {
         for fun_data in module.function_data.values_mut() {
@@ -409,6 +398,86 @@ fn run_move_checker(env: &mut GlobalEnv, program: E::Program) {
                     .unwrap_or_default(),
             )
         }
+    }
+}
+
+/// Checks if any friend declarations are invalid because:
+///     - they are self-friend declarations
+///     - they are out-of-address friend declarations
+///     - they refer to unbound modules
+/// If so, report errors.
+/// Also, update friend module id information: this function assumes all modules have been assigned ids.
+///
+/// Note: we assume (a) friend declarations creating cyclic dependencies (cycle size > 1),
+///                 (b) duplicate friend declarations
+/// have been reported already. Currently, these checks happen in the expansion phase.
+fn check_and_update_friend_info(mut builder: ModelBuilder) {
+    let module_table = std::mem::take(&mut builder.module_table);
+    let env = builder.env;
+    // To save (loc, name) info about self friend decls.
+    let mut self_friends = vec![];
+    // To save (loc, friend_module_name, current_module_name) info about out-of-address friend decls.
+    let mut out_of_address_friends = vec![];
+    // To save (loc, friend_module_name) info about unbound modules in friend decls.
+    let mut unbound_friend_modules = vec![];
+    // Patch up information about friend module ids, as all the modules have ids by now.
+    for module in env.module_data.iter_mut() {
+        let mut friend_modules = BTreeSet::new();
+        for friend_decl in module.friend_decls.iter_mut() {
+            // Save information of out-of-address friend decls to report error later.
+            if friend_decl.module_name.addr() != module.name.addr() {
+                out_of_address_friends.push((
+                    friend_decl.loc.clone(),
+                    friend_decl.module_name.clone(),
+                    module.name.clone(),
+                ));
+            }
+            if let Some(friend_mod_id) = module_table.get(&friend_decl.module_name) {
+                friend_decl.module_id = Some(*friend_mod_id);
+                friend_modules.insert(*friend_mod_id);
+                // Save information of self-friend decls to report error later.
+                if module.id == *friend_mod_id {
+                    self_friends.push((friend_decl.loc.clone(), friend_decl.module_name.clone()));
+                }
+            } else {
+                // Save information of unbound modules in friend decls to report error later.
+                unbound_friend_modules
+                    .push((friend_decl.loc.clone(), friend_decl.module_name.clone()));
+            }
+        }
+        module.friend_modules = friend_modules;
+    }
+    // Report self-friend errors.
+    for (loc, module_name) in self_friends {
+        env.error(
+            &loc,
+            &format!(
+                "cannot declare module `{}` as a friend of itself",
+                module_name.display_full(env)
+            ),
+        );
+    }
+    // Report out-of-address friend errors.
+    for (loc, friend_mod_name, cur_mod_name) in out_of_address_friends {
+        env.error(
+            &loc,
+            &format!(
+                "friend modules of `{}` must have the same address, \
+                    but the declared friend module `{}` has a different address",
+                cur_mod_name.display_full(env),
+                friend_mod_name.display_full(env),
+            ),
+        );
+    }
+    // Report unbound friend errors.
+    for (loc, friend_mod_name) in unbound_friend_modules {
+        env.error(
+            &loc,
+            &format!(
+                "unbound module `{}` in friend declaration",
+                friend_mod_name.display_full(env)
+            ),
+        );
     }
 }
 
