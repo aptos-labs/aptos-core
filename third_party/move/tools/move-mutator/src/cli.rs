6383fcc0249da1b7d7ad87bb171c03172c80d7ad
Piotr Olszewski <asmie@asmie.pl>
2024-01-26 00:12:10 +0100

    [move-mutator] downsampling added
    Downsampling has been implemented using `ratio` factor.

--- third_party/move/tools/move-mutator/src/cli.rs
+++ third_party/move/tools/move-mutator/src/cli.rs
@@ -26,9 +26,9 @@ pub struct CLIOptions {
     /// Name of the filter to use for downsampling. Downsampling reduces the amount of mutants to the desired amount.
     #[clap(long, hide = true)]
     pub downsample_filter: Option<String>,
-    /// Maximum number of mutants to be generated. If not specified, downsampling will be disabled. Currently only random filter is supported (mutants are removed randomly).
+    /// Remove averagely n-th mutant. 2 means 50% reduction, 3 means 33% reduction, etc. See the doc for more details.
     #[clap(long)]
-    pub downsample_num: Option<u64>,
+    pub downsample_ratio: Option<u64>,
     /// Optional configuration file. If provided, it will override the default configuration.
     #[clap(long, short, value_parser)]
     pub configuration_file: Option<PathBuf>,
@@ -46,7 +46,7 @@ impl Default for CLIOptions {
             verify_mutants: true,
             no_overwrite: None,
             downsample_filter: None,
-            downsample_num: None,
+            downsample_ratio: None,
             configuration_file: None,
         }
     }
