a71b8bce1f76f98255cdca3a0bfe41aeb520c8d2
Justin Chang <justin@aptoslabs.com>
2024-01-26 13:30:49 -0800

    Enable better quality from configs + add IPFS gateway auth

--- ecosystem/nft-metadata-crawler-parser/src/utils/constants.rs
+++ ecosystem/nft-metadata-crawler-parser/src/utils/constants.rs
@@ -11,3 +11,12 @@ pub const MAX_JSON_REQUEST_RETRY_SECONDS: u64 = 30;
 
 /// Allocate 90 seconds for downloading large image files
 pub const MAX_IMAGE_REQUEST_RETRY_SECONDS: u64 = 90;
+
+/// Default maximum file size for files to be downloaded
+pub const DEFAULT_MAX_FILE_SIZE_BYTES: u32 = 5_000_000;
+
+/// Default image quality for image optimization
+pub const DEFAULT_IMAGE_QUALITY: u8 = 100;
+
+/// Default maximum image dimensions for image optimization
+pub const DEFAULT_MAX_IMAGE_DIMENSIONS: u32 = 4096;
