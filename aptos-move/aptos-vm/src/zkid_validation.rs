c0c61bbd95ce5f38fbd701a487c929e482634813
Oliver <heliuchuan@gmail.com>
2024-01-29 05:35:53 +0700

    use macro

--- aptos-move/aptos-vm/src/zkid_validation.rs
+++ aptos-move/aptos-vm/src/zkid_validation.rs
@@ -16,6 +16,12 @@ use aptos_vm_logging::log_schema::AdapterLogSchema;
 use move_binary_format::errors::Location;
 use move_core_types::{language_storage::CORE_CODE_ADDRESS, move_resource::MoveStructType};
 
+macro_rules! invalid_signature {
+    ($message:expr) => {
+        VMStatus::error(StatusCode::INVALID_SIGNATURE, Some($message.to_owned()))
+    };
+}
+
 fn get_current_time_onchain(
     resolver: &impl AptosMoveResolver,
 ) -> anyhow::Result<CurrentTimeMicroseconds, VMStatus> {
@@ -45,27 +51,15 @@ fn get_jwk_for_zk_authenticator(
     zkid_pub_key: &ZkIdPublicKey,
     zkid_sig: &ZkIdSignature,
 ) -> anyhow::Result<JWK, VMStatus> {
-    let jwt_header_parsed = zkid_sig.parse_jwt_header().map_err(|_| {
-        VMStatus::error(
-            StatusCode::INVALID_SIGNATURE,
-            Some("Failed to get JWT header".to_owned()),
-        )
-    })?;
+    let jwt_header_parsed = zkid_sig
+        .parse_jwt_header()
+        .map_err(|_| invalid_signature!("Failed to get JWT header"))?;
     let jwk_move_struct = jwks
         .get_jwk(&zkid_pub_key.iss, &jwt_header_parsed.kid)
-        .map_err(|_| {
-            VMStatus::error(
-                StatusCode::INVALID_SIGNATURE,
-                Some("JWK not found".to_owned()),
-            )
-        })?;
+        .map_err(|_| invalid_signature!("JWK not found"))?;
 
-    let jwk = JWK::try_from(jwk_move_struct).map_err(|_| {
-        VMStatus::error(
-            StatusCode::INVALID_SIGNATURE,
-            Some("Could not parse JWK".to_owned()),
-        )
-    })?;
+    let jwk =
+        JWK::try_from(jwk_move_struct).map_err(|_| invalid_signature!("Could not parse JWK"))?;
     Ok(jwk)
 }
 
@@ -76,24 +70,15 @@ pub fn validate_zkid_authenticators(
     _log_context: &AdapterLogSchema,
     chain_id: ChainId,
 ) -> anyhow::Result<(), VMStatus> {
-    // TODO(ZkIdGroth16Zkp): The ZKP/OpenID sig verification does not charge gas. So, we could have DoS attacks.
-    let zkid_authenticators =
-        aptos_types::zkid::get_zkid_authenticators(transaction).map_err(|_| {
-            VMStatus::error(
-                StatusCode::INVALID_SIGNATURE,
-                Some("Failed to fetch zkid authenticators".to_owned()),
-            )
-        })?;
+    let zkid_authenticators = aptos_types::zkid::get_zkid_authenticators(transaction)
+        .map_err(|_| invalid_signature!("Failed to fetch zkid authenticators"))?;
 
     if zkid_authenticators.is_empty() {
         return Ok(());
     }
 
     if zkid_authenticators.len() > MAX_ZK_ID_AUTHENTICATORS_ALLOWED {
-        return Err(VMStatus::error(
-            StatusCode::INVALID_SIGNATURE,
-            Some("Too many zkid authenticators".to_owned()),
-        ));
+        return Err(invalid_signature!("Too many zkid authenticators"));
     }
 
     let onchain_timestamp_obj = get_current_time_onchain(resolver)?;
@@ -101,12 +86,7 @@ pub fn validate_zkid_authenticators(
     for (_, zkid_sig) in &zkid_authenticators {
         zkid_sig
             .verify_expiry(&onchain_timestamp_obj)
-            .map_err(|_| {
-                VMStatus::error(
-                    StatusCode::INVALID_SIGNATURE,
-                    Some("The ephemeral keypair has expired".to_owned()),
-                )
-            })?;
+            .map_err(|_| invalid_signature!("The ephemeral keypair has expired"))?;
     }
 
     let patched_jwks = get_jwks_onchain(resolver)?;
@@ -119,26 +99,13 @@ pub fn validate_zkid_authenticators(
                 JWK::RSA(rsa_jwk) => {
                     let public_inputs_hash =
                         get_public_inputs_hash(zkid_sig, zkid_pub_key, &rsa_jwk).map_err(|_| {
-                            VMStatus::error(
-                                StatusCode::INVALID_SIGNATURE,
-                                Some("Could not compute public inputs hash".to_owned()),
-                            )
+                            invalid_signature!("Could not compute public inputs hash")
                         })?;
                     proof
                         .verify_proof(public_inputs_hash, chain_id)
-                        .map_err(|_| {
-                            VMStatus::error(
-                                StatusCode::INVALID_SIGNATURE,
-                                Some("Proof verification failed".to_owned()),
-                            )
-                        })?;
+                        .map_err(|_| invalid_signature!("Proof verification failed"))?;
                 },
-                JWK::Unsupported(_) => {
-                    return Err(VMStatus::error(
-                        StatusCode::INVALID_SIGNATURE,
-                        Some("JWK is not supported".to_owned()),
-                    ))
-                },
+                JWK::Unsupported(_) => return Err(invalid_signature!("JWK is not supported")),
             },
             ZkpOrOpenIdSig::OpenIdSig(openid_sig) => {
                 match jwk {
@@ -157,21 +124,12 @@ pub fn validate_zkid_authenticators(
                         openid_sig
                             .verify_jwt_signature(rsa_jwk, &zkid_sig.jwt_header)
                             .map_err(|_| {
-                                VMStatus::error(
-                                    StatusCode::INVALID_SIGNATURE,
-                                    Some(
-                                        "RSA Signature verification failed for OpenIdSig"
-                                            .to_owned(),
-                                    ),
+                                invalid_signature!(
+                                    "RSA Signature verification failed for OpenIdSig"
                                 )
                             })?;
                     },
-                    JWK::Unsupported(_) => {
-                        return Err(VMStatus::error(
-                            StatusCode::INVALID_SIGNATURE,
-                            Some("JWK is not supported".to_owned()),
-                        ))
-                    },
+                    JWK::Unsupported(_) => return Err(invalid_signature!("JWK is not supported")),
                 }
             },
         }
