e319a8e9b99e40f48e09bb4b95f8b1c186e61ef4
Piotr Olszewski <asmie@asmie.pl>
2024-01-29 14:38:10 +0100

    [move-mutator] additional cases for numeric values (comments updated too)

--- third_party/move/tools/move-mutator/src/operators/literal.rs
+++ third_party/move/tools/move-mutator/src/operators/literal.rs
@@ -6,15 +6,15 @@ use move_compiler::expansion::ast::Value_;
 use std::fmt;
 use std::fmt::Debug;
 
-/// Break and continue mutation operator.
-/// Replaces break and continue statements with each other or deletes them.
+/// Literal replacement mutation operator.
+/// Replaces literal statements with other ones but withing the same type.
 #[derive(Debug, Clone)]
 pub struct Literal {
     operation: ast::Value,
 }
 
 impl Literal {
-    /// Creates a new instance of the break/continue mutation operator.
+    /// Creates a new instance of the literal mutation operator.
     #[must_use]
     pub fn new(operation: ast::Value) -> Self {
         Self { operation }
@@ -27,40 +27,65 @@ impl MutationOperator for Literal {
         let end = self.operation.loc.end() as usize;
         let cur_op = &source[start..end];
 
-        // Group of exchangeable break/continue statements.
-        let ops: Vec<&str> = match &self.operation.value {
+        // Group of literal statements for possible Value types.
+        let ops: Vec<String> = match &self.operation.value {
             Value_::Address(_addr) => {
                 vec![
-                    "0x0",
-                    "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                    "0x0".to_owned(),
+                    "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF".to_owned(),
                 ]
             },
             Value_::Bool(_bool_val) => {
-                vec!["true", "false"]
+                vec!["true".to_owned(), "false".to_owned()]
             },
             Value_::Bytearray(_bytearray) => {
-                vec!["b\"121112\""]
+                vec!["b\"121112\"".to_owned()]
             },
-            Value_::U8(_u8_val) => {
-                vec!["0u8", "255u8"]
+            Value_::U8(u8_val) => {
+                vec![
+                    "0u8".to_owned(),
+                    "255u8".to_owned(),
+                    (u8_val + 1).to_string(),
+                    (u8_val - 1).to_string(),
+                ]
             },
-            Value_::U16(_u16_val) => {
-                vec!["0u16", "65535u16"]
+            Value_::U16(u16_val) => {
+                vec![
+                    "0u16".to_owned(),
+                    "65535u16".to_owned(),
+                    (u16_val + 1).to_string(),
+                    (u16_val - 1).to_string(),
+                ]
             },
-            Value_::U32(_u32_val) => {
-                vec!["0u32", "4294967295u32"]
+            Value_::U32(u32_val) => {
+                vec![
+                    "0u32".to_owned(),
+                    "4294967295u32".to_owned(),
+                    (u32_val + 1).to_string(),
+                    (u32_val - 1).to_string(),
+                ]
             },
-            Value_::U64(_u64_val) => {
-                vec!["0u64", "18446744073709551615u64"]
+            Value_::U64(u64_val) => {
+                vec![
+                    "0u64".to_owned(),
+                    "18446744073709551615u64".to_owned(),
+                    (u64_val + 1).to_string(),
+                    (u64_val - 1).to_string(),
+                ]
             },
-            Value_::U128(_u128_val) => {
-                vec!["0u128", "340282366920938463463374607431768211455u128"]
+            Value_::U128(u128_val) => {
+                vec![
+                    "0u128".to_owned(),
+                    "340282366920938463463374607431768211455u128".to_owned(),
+                    (u128_val + 1).to_string(),
+                    (u128_val - 1).to_string(),
+                ]
             },
             Value_::U256(_u256_val) => {
-                vec!["0u256", "115792089237316195423570985008687907853269984665640564039457584007913129639935u256"]
+                vec!["0u256".to_owned(), "115792089237316195423570985008687907853269984665640564039457584007913129639935u256".to_owned()]
             },
             Value_::InferredNum(_inferred_num) => {
-                vec!["0", "115792089237316195423570985008687907853269984665640564039457584007913129639935"]
+                vec!["0".to_owned(), "115792089237316195423570985008687907853269984665640564039457584007913129639935".to_owned()]
             },
         };
 
@@ -68,7 +93,7 @@ impl MutationOperator for Literal {
             .filter(|v| cur_op != *v)
             .map(|op| {
                 let mut mutated_source = source.to_string();
-                mutated_source.replace_range(start..end, op);
+                mutated_source.replace_range(start..end, op.as_str());
                 MutantInfo::new(
                     mutated_source,
                     Mutation::new(
@@ -111,12 +136,12 @@ mod tests {
     fn test_apply_u8() {
         let loc = Loc::new(FileHash::new(""), 0, 2);
         let val = Value {
-            value: Value_::U8(99),
+            value: Value_::U8(51),
             loc,
         };
         let operator = Literal::new(val);
         let source = "51";
-        let expected = vec!["0u8", "255u8"];
+        let expected = vec!["0u8", "255u8", "52", "50"];
         let result = operator.apply(source);
         assert_eq!(result.len(), expected.len());
         for (i, r) in result.iter().enumerate() {
@@ -133,7 +158,7 @@ mod tests {
         };
         let operator = Literal::new(val);
         let source = "963";
-        let expected = vec!["0u16", "65535u16"];
+        let expected = vec!["0u16", "65535u16", "964", "962"];
         let result = operator.apply(source);
         assert_eq!(result.len(), expected.len());
         for (i, r) in result.iter().enumerate() {
@@ -150,7 +175,7 @@ mod tests {
         };
         let operator = Literal::new(val);
         let source = "1000963";
-        let expected = vec!["0u32", "4294967295u32"];
+        let expected = vec!["0u32", "4294967295u32", "1000964", "1000962"];
         let result = operator.apply(source);
         assert_eq!(result.len(), expected.len());
         for (i, r) in result.iter().enumerate() {
@@ -167,7 +192,7 @@ mod tests {
         };
         let operator = Literal::new(val);
         let source = "963251000963";
-        let expected = vec!["0u64", "18446744073709551615u64"];
+        let expected = vec!["0u64", "18446744073709551615u64", "963251000964", "963251000962"];
         let result = operator.apply(source);
         assert_eq!(result.len(), expected.len());
         for (i, r) in result.iter().enumerate() {
@@ -184,7 +209,7 @@ mod tests {
         };
         let operator = Literal::new(val);
         let source = "123963251000963";
-        let expected = vec!["0u128", "340282366920938463463374607431768211455u128"];
+        let expected = vec!["0u128", "340282366920938463463374607431768211455u128", "123963251000964", "123963251000962"];
         let result = operator.apply(source);
         assert_eq!(result.len(), expected.len());
         for (i, r) in result.iter().enumerate() {
