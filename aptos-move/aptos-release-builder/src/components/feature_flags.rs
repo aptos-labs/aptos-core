ef680a95ead81199236bd052a3373d79a7d872c3
zhoujun.ma <zhoujun@aptoslabs.com>
2024-02-01 01:54:54 -0800

    update

--- aptos-move/aptos-release-builder/src/components/feature_flags.rs
+++ aptos-move/aptos-release-builder/src/components/feature_flags.rs
@@ -99,6 +99,7 @@ pub enum FeatureFlag {
     ZkIdSignature,
     ZkIdZkLessSignature,
     RemoveDetailedError,
+    JWKConsensus,
 }
 
 fn generate_features_blob(writer: &CodeWriter, data: &[u64]) {
@@ -256,6 +257,7 @@ impl From<FeatureFlag> for AptosFeatureFlag {
             FeatureFlag::ZkIdSignature => AptosFeatureFlag::ZK_ID_SIGNATURES,
             FeatureFlag::ZkIdZkLessSignature => AptosFeatureFlag::ZK_ID_ZKLESS_SIGNATURE,
             FeatureFlag::RemoveDetailedError => AptosFeatureFlag::REMOVE_DETAILED_ERROR_FROM_HASH,
+            FeatureFlag::JWKConsensus => AptosFeatureFlag::JWK_CONSENSUS,
         }
     }
 }
@@ -336,6 +338,7 @@ impl From<AptosFeatureFlag> for FeatureFlag {
             AptosFeatureFlag::ZK_ID_SIGNATURES => FeatureFlag::ZkIdSignature,
             AptosFeatureFlag::ZK_ID_ZKLESS_SIGNATURE => FeatureFlag::ZkIdZkLessSignature,
             AptosFeatureFlag::REMOVE_DETAILED_ERROR_FROM_HASH => FeatureFlag::RemoveDetailedError,
+            AptosFeatureFlag::JWK_CONSENSUS => FeatureFlag::JWKConsensus,
         }
     }
 }
