40a6fb8d9b4ce942dc440a91f43f68c9d61a0ad1
Teng Zhang <rahxephon89@163.com>
2024-02-06 21:07:17 -0800

    temp

--- testsuite/comparison-testing-online.sh
+++ testsuite/comparison-testing-online.sh
@@ -4,5 +4,6 @@ for ((x=$1; x<($1+$2); x++))
 do
     dir="$3net-$x-$((x+1))m"
     target/release/aptos-comparison-testing \
-    --begin-version $((x*1000000)) --limit 1000000 online --execution-mode=compare https://api.$3net.aptoslabs.com/v1 $dir
+    --begin-version $((x*1000000)) --limit 1000000 dump https://api.$3net.aptoslabs.com/v1 $dir
+    cat $dir/err_log.txt
 done
