bfd9c2e3706656e2535fdbf4482847c261860021
Sital Kedia <skedia@aptoslabs.com>
2024-02-12 15:06:21 -0800

    30 nodes

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -599,7 +599,7 @@ fn get_land_blocking_test(
 ) -> Option<ForgeConfig> {
     let test = match test_name {
         "land_blocking" => land_blocking_test_suite(duration), // TODO: remove land_blocking, superseded by below
-        "realistic_env_max_load" => realistic_env_max_load_test(duration, test_cmd, 100, 50),
+        "realistic_env_max_load" => realistic_env_max_load_test(duration, test_cmd, 30, 10),
         "compat" => compat(),
         "framework_upgrade" => framework_upgrade(),
         _ => return None, // The test name does not match a land-blocking test
