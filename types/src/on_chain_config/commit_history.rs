64668ac639fb68d72c5b339d4763f4bf5a05b3b0
Zekun Li <zekunli@Zekuns-MacBook-Pro.local>
2024-02-13 13:48:23 -0800

    [dag] connect commit history resource to adapter

--- types/src/on_chain_config/commit_history.rs
+++ types/src/on_chain_config/commit_history.rs
@@ -7,17 +7,35 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
 struct TableWithLength {
     handle: TableHandle,
-    length: u32,
+    length: u64,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
-pub struct CommitHistory {
+pub struct CommitHistoryResource {
     max_capacity: u32,
-    current_idx: u32,
+    next_idx: u32,
     table: TableWithLength,
 }
 
-impl OnChainConfig for CommitHistory {
+impl CommitHistoryResource {
+    pub fn max_capacity(&self) -> u32 {
+        self.max_capacity
+    }
+
+    pub fn next_idx(&self) -> u32 {
+        self.next_idx
+    }
+
+    pub fn table_handle(&self) -> &TableHandle {
+        &self.table.handle
+    }
+
+    pub fn length(&self) -> u64 {
+        self.table.length
+    }
+}
+
+impl OnChainConfig for CommitHistoryResource {
     const MODULE_IDENTIFIER: &'static str = "block";
     const TYPE_IDENTIFIER: &'static str = "CommitHistory";
 }
