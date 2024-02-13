e6930f0475c1428fe2313e181870ca6b8b83f04d
zhoujun.ma <zhoujun@aptoslabs.com>
2024-02-13 12:09:11 -0800

    update

--- testsuite/smoke-test/src/randomness/enable_feature_0.rs
+++ testsuite/smoke-test/src/randomness/enable_feature_0.rs
@@ -41,7 +41,6 @@ async fn enable_feature_0() {
         swarm.root_key(),
         root_addr,
     );
-    swarm.aptos_public_info().mint(root_addr, 999999999999999).await.unwrap();
 
     let decrypt_key_map = decrypt_key_map(&swarm);
 
@@ -53,6 +52,7 @@ async fn enable_feature_0() {
         .await
         .expect("Waited too long for epoch 2.");
     println!("Enabling the feature.");
+    swarm.aptos_public_info().mint(root_addr, 999999999999999).await.unwrap();
     let enable_dkg_script = r#"
 script {
     use aptos_framework::aptos_governance;
