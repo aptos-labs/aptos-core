2a46e92622583966b488709e454e1c0ea2a538d2
Brian Cho <brian@aptoslabs.com>
2024-02-17 23:15:06 -0800

    remove wait for full blocks

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1857,10 +1857,10 @@ fn realistic_network_tuned_for_throughput_test() -> ForgeConfig {
             optimize_for_maximum_throughput(config);
 
             // Other consensus / Quorum store configs
-            config
-                .consensus
-                .wait_for_full_blocks_above_recent_fill_threshold = 0.2;
-            config.consensus.wait_for_full_blocks_above_pending_blocks = 8;
+            // config
+            //     .consensus
+            //     .wait_for_full_blocks_above_recent_fill_threshold = 0.2;
+            // config.consensus.wait_for_full_blocks_above_pending_blocks = 8;
             config.consensus.quorum_store_pull_timeout_ms = 200;
 
             // Experimental storage optimizations
