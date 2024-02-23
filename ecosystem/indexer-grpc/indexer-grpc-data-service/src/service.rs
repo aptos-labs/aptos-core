03ed54748854a1c1fdbf9b191f7d6b36f510026a
rtso <8248583+rtso@users.noreply.github.com>
2024-02-23 11:24:09 -0500

    Min 1 task

--- ecosystem/indexer-grpc/indexer-grpc-data-service/src/service.rs
+++ ecosystem/indexer-grpc/indexer-grpc-data-service/src/service.rs
@@ -225,7 +225,8 @@ async fn get_data_with_tasks(
         Ok(CacheCoverageStatus::CacheEvicted) => match transactions_count {
             None => MAX_FETCH_TASKS_PER_REQUEST,
             Some(transactions_count) => (transactions_count / TRANSACTIONS_PER_STORAGE_BLOCK)
-                .min(MAX_FETCH_TASKS_PER_REQUEST),
+                .min(MAX_FETCH_TASKS_PER_REQUEST)
+                .max(1),
         },
         Err(_) => {
             error!("[Data Service] Failed to get cache coverage status.");
