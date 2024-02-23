28ac249c5c310f5f87a96ee5133d21147ba0633b
rtso <8248583+rtso@users.noreply.github.com>
2024-02-23 11:30:12 -0500

    Logging

--- ecosystem/indexer-grpc/indexer-grpc-data-service/src/service.rs
+++ ecosystem/indexer-grpc/indexer-grpc-data-service/src/service.rs
@@ -160,7 +160,7 @@ impl RawData for RawDataServerWrapper {
             SERVICE_TYPE,
             IndexerGrpcStep::DataServiceNewRequestReceived,
             Some(current_version as i64),
-            None,
+            transactions_count.map(|v| (v as i64 + current_version as i64 - 1)),
             None,
             None,
             None,
