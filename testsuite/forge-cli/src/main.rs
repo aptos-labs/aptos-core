f850786bba8053dc012e4989eb53c8b9784bb7e1
Balaji Arun <balaji@aptoslabs.com>
2024-02-24 22:07:18 -0800

    test

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -279,7 +279,7 @@ fn main() -> Result<()> {
     let args = Args::parse();
     let duration = Duration::from_secs(args.duration_secs as u64);
     // let suite_name: &str = args.suite.as_ref();
-    let suite_name = "dag_realistic_env_max_load";
+    let suite_name = "dag_realistic_network_tuned_for_throughput_test";
 
     let runtime = Runtime::new()?;
     match args.cli_cmd {
