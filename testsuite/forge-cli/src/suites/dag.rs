91d4577cb9a3a6aae49ed414b5cee41eddec4dd0
Balaji Arun <balaji@aptoslabs.com>
2024-02-24 20:53:33 -0800

    more mempool backlog

--- testsuite/forge-cli/src/suites/dag.rs
+++ testsuite/forge-cli/src/suites/dag.rs
@@ -63,7 +63,7 @@ fn dag_realistic_env_max_load_test(
         .add_network_test(wrap_with_realistic_env(TwoTrafficsTest {
             inner_traffic: EmitJobRequest::default()
                 .mode(EmitJobMode::MaxLoad {
-                    mempool_backlog: 50000,
+                    mempool_backlog: 400_000,
                 })
                 .init_gas_price_multiplier(20),
             inner_success_criteria: SuccessCriteria::new(
