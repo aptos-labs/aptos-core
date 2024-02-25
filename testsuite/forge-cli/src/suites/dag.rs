1ace0865e3a969c45935530bc8b280ee8532c7c7
Balaji Arun <balaji@aptoslabs.com>
2024-02-24 22:07:18 -0800

    test

--- testsuite/forge-cli/src/suites/dag.rs
+++ testsuite/forge-cli/src/suites/dag.rs
@@ -1,9 +1,13 @@
 // Copyright © Aptos Foundation
 
-use crate::{changing_working_quorum_test_helper, wrap_with_realistic_env, TestCommand};
+use crate::{
+    changing_working_quorum_test_helper, optimize_for_maximum_throughput,
+    optimize_state_sync_for_throughput, realistic_network_tuned_for_throughput_test,
+    wrap_with_realistic_env, TestCommand,
+};
 use aptos_forge::{
     success_criteria::{LatencyType, StateProgressThreshold, SuccessCriteria},
-    EmitJobMode, EmitJobRequest, ForgeConfig,
+    EmitJobMode, EmitJobRequest, ForgeConfig, NodeResourceOverride,
 };
 use aptos_sdk::types::on_chain_config::{
     BlockGasLimitType, ConsensusAlgorithmConfig, DagConsensusConfigV1, OnChainConsensusConfig,
@@ -11,7 +15,8 @@ use aptos_sdk::types::on_chain_config::{
 };
 use aptos_testcases::{
     consensus_reliability_tests::ChangingWorkingQuorumTest,
-    dag_onchain_enable_test::DagOnChainEnableTest, two_traffics_test::TwoTrafficsTest,
+    dag_onchain_enable_test::DagOnChainEnableTest,
+    multi_region_network_test::MultiRegionNetworkEmulationTest, two_traffics_test::TwoTrafficsTest,
 };
 use std::{num::NonZeroUsize, sync::Arc, time::Duration};
 
@@ -33,6 +38,9 @@ fn get_dag_on_realistic_env_test(
         "dag_realistic_env_max_load" => dag_realistic_env_max_load_test(duration, test_cmd, 100, 0),
         "dag_changing_working_quorum_test" => dag_changing_working_quorum_test(),
         "dag_reconfig_enable_test" => dag_reconfig_enable_test(),
+        "dag_realistic_network_tuned_for_throughput_test" => {
+            dag_realistic_network_tuned_for_throughput_test()
+        },
         _ => return None, // The test name does not match a dag realistic-env test
     };
     Some(test)
@@ -228,4 +236,135 @@ fn dag_reconfig_enable_test() -> ForgeConfig {
                     max_round_gap: 20,
                 }),
         )
+}
+
+fn dag_realistic_network_tuned_for_throughput_test() -> ForgeConfig {
+    // THE MOST COMMONLY USED TUNE-ABLES:
+    const USE_CRAZY_MACHINES: bool = false;
+    const ENABLE_VFNS: bool = false;
+    const VALIDATOR_COUNT: usize = 100;
+
+    // Config is based on these values. The target TPS should be a slight overestimate of
+    // the actual throughput to be able to have reasonable queueing but also so throughput
+    // will improve as performance improves.
+    // Overestimate: causes mempool and/or batch queueing. Underestimate: not enough txns in blocks.
+    const TARGET_TPS: usize = 15_000 * 15;
+    // Overestimate: causes blocks to be too small. Underestimate: causes blocks that are too large.
+    // Ideally, want the block size to take 200-250ms of execution time to match broadcast RTT.
+    const MAX_TXNS_PER_BLOCK: usize = 20000;
+    // Overestimate: causes batch queueing. Underestimate: not enough txns in quorum store.
+    // This is validator latency, minus mempool queueing time.
+    const VN_LATENCY_S: f64 = 2.5;
+    // Overestimate: causes mempool queueing. Underestimate: not enough txns incoming.
+    const VFN_LATENCY_S: f64 = 4.0;
+
+    let mut forge_config = ForgeConfig::default()
+        .with_initial_validator_count(NonZeroUsize::new(VALIDATOR_COUNT).unwrap())
+        .add_network_test(MultiRegionNetworkEmulationTest::default())
+        .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::MaxLoad {
+            mempool_backlog: (TARGET_TPS as f64 * VFN_LATENCY_S) as usize,
+        }))
+        .with_validator_override_node_config_fn(Arc::new(|config, _| {
+            // Increase the state sync chunk sizes (consensus blocks are much larger than 1k)
+            optimize_state_sync_for_throughput(config);
+
+            optimize_for_maximum_throughput(config, TARGET_TPS, MAX_TXNS_PER_BLOCK, VN_LATENCY_S);
+
+            // Other consensus / Quroum store configs
+            config.consensus.quorum_store_pull_timeout_ms = 200;
+
+            // Experimental storage optimizations
+            config.storage.rocksdb_configs.enable_storage_sharding = true;
+
+            // Increase the concurrency level
+            if USE_CRAZY_MACHINES {
+                config.execution.concurrency_level = 48;
+            }
+        }))
+        .with_genesis_helm_config_fn(Arc::new(move |helm_values| {
+            let onchain_consensus_config = OnChainConsensusConfig::V3 {
+                alg: ConsensusAlgorithmConfig::DAG(DagConsensusConfigV1::default()),
+                vtxn: ValidatorTxnConfig::default_for_genesis(),
+            };
+
+            helm_values["chain"]["on_chain_consensus_config"] =
+                serde_yaml::to_value(onchain_consensus_config).expect("must serialize");
+
+            let mut on_chain_execution_config = OnChainExecutionConfig::default_for_genesis();
+            // Need to update if the default changes
+            match &mut on_chain_execution_config {
+                OnChainExecutionConfig::Missing
+                | OnChainExecutionConfig::V1(_)
+                | OnChainExecutionConfig::V2(_)
+                | OnChainExecutionConfig::V3(_) => {
+                    unreachable!("Unexpected on-chain execution config type, if OnChainExecutionConfig::default_for_genesis() has been updated, this test must be updated too.")
+                }
+                OnChainExecutionConfig::V4(config_v4) => {
+                    config_v4.block_gas_limit_type = BlockGasLimitType::NoLimit;
+                    config_v4.transaction_shuffler_type = TransactionShufflerType::Fairness {
+                        sender_conflict_window_size: 256,
+                        module_conflict_window_size: 2,
+                        entry_fun_conflict_window_size: 3,
+                    };
+                }
+            }
+            helm_values["chain"]["on_chain_execution_config"] =
+                serde_yaml::to_value(on_chain_execution_config).expect("must serialize");
+        }));
+
+    if ENABLE_VFNS {
+        forge_config = forge_config
+            .with_initial_fullnode_count(VALIDATOR_COUNT)
+            .with_fullnode_override_node_config_fn(Arc::new(|config, _| {
+                // Increase the state sync chunk sizes (consensus blocks are much larger than 1k)
+                optimize_state_sync_for_throughput(config);
+
+                // Experimental storage optimizations
+                config.storage.rocksdb_configs.enable_storage_sharding = true;
+
+                // Increase the concurrency level
+                if USE_CRAZY_MACHINES {
+                    config.execution.concurrency_level = 48;
+                }
+            }));
+    }
+
+    if USE_CRAZY_MACHINES {
+        forge_config = forge_config
+            .with_validator_resource_override(NodeResourceOverride {
+                cpu_cores: Some(58),
+                memory_gib: Some(200),
+            })
+            .with_fullnode_resource_override(NodeResourceOverride {
+                cpu_cores: Some(58),
+                memory_gib: Some(200),
+            })
+            .with_success_criteria(
+                SuccessCriteria::new(25000)
+                    .add_no_restarts()
+                    /* This test runs at high load, so we need more catchup time */
+                    .add_wait_for_catchup_s(120),
+                /* Doesn't work without event indices
+                .add_chain_progress(StateProgressThreshold {
+                    max_no_progress_secs: 10.0,
+                    max_round_gap: 4,
+                }),
+                 */
+            );
+    } else {
+        forge_config = forge_config.with_success_criteria(
+            SuccessCriteria::new(12000)
+                .add_no_restarts()
+                /* This test runs at high load, so we need more catchup time */
+                .add_wait_for_catchup_s(120),
+            /* Doesn't work without event indices
+                .add_chain_progress(StateProgressThreshold {
+                     max_no_progress_secs: 10.0,
+                     max_round_gap: 4,
+                 }),
+            */
+        );
+    }
+
+    forge_config
 }
