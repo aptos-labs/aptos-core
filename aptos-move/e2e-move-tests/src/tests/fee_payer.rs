92eec588799bea3b64fc8b13d96e1d957430430f
aldenhu <msmouse@gmail.com>
2024-02-27 19:31:54 +0000

    fix StorageGasParameters::unlimited()
    1. make it use latest logic, easier to maintain going forward
    2. use latest() instead of unlimited() when proper

--- aptos-move/e2e-move-tests/src/tests/fee_payer.rs
+++ aptos-move/e2e-move-tests/src/tests/fee_payer.rs
@@ -14,6 +14,7 @@ use aptos_types::{
     on_chain_config::FeatureFlag,
     transaction::{EntryFunction, ExecutionStatus, Script, TransactionPayload, TransactionStatus},
 };
+use aptos_vm_types::storage::StorageGasParameters;
 use move_core_types::{move_resource::MoveStructType, vm_status::StatusCode};
 use once_cell::sync::Lazy;
 
@@ -400,13 +401,11 @@ impl FeePayerPricingInfo {
 }
 
 static PRICING: Lazy<FeePayerPricingInfo> = Lazy::new(|| {
-    use aptos_vm_types::storage::space_pricing::DiskSpacePricing;
-
     let h = MoveHarness::new();
 
     let (_feature_version, params) = h.get_gas_params();
     let params = params.vm.txn;
-    let pricing = DiskSpacePricing::latest();
+    let pricing = StorageGasParameters::latest().space_pricing;
 
     FeePayerPricingInfo {
         estimated_per_new_account_fee_octas: u64::from(
