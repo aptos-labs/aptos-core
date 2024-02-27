6a8a2b66e05250056769dc12b9c826a952e44eb5
zhoujun.ma <zhoujun@aptoslabs.com>
2024-02-27 02:58:48 -0800

    Squashed commit of the following to fix jwk smoke tests:
    commit 3bd015468bb85aefb7ebc37c91c36c039399a187
    Author: zhoujun.ma <zhoujun@aptoslabs.com>
    Date:   Tue Feb 27 02:47:57 2024 -0800
    
        update
    
    commit 2eb6add04d4d973b8c8d9a1945a0ae251427003a
    Author: zhoujun.ma <zhoujun@aptoslabs.com>
    Date:   Tue Feb 27 02:12:27 2024 -0800
    
        update
    
    commit 9d821519ad3236763da53980fda6ea8d73e8fbb4
    Author: zhoujun.ma <zhoujun@aptoslabs.com>
    Date:   Tue Feb 27 01:51:08 2024 -0800
    
        debug

---
+++ aptos-move/framework/src/natives/randomness.rs
@@ -0,0 +1,58 @@
+// Copyright © Aptos Foundation
+
+use aptos_native_interface::{
+    RawSafeNative, SafeNativeBuilder, SafeNativeContext, SafeNativeResult,
+};
+use better_any::{Tid, TidAble};
+use move_vm_runtime::native_functions::NativeFunction;
+use move_vm_types::{loaded_data::runtime_types::Type, values::Value};
+use smallvec::{smallvec, SmallVec};
+use std::collections::VecDeque;
+
+/// A txn-local counter that increments each time a random 32-byte blob is requested.
+#[derive(Tid, Default)]
+pub struct RandomnessContext {
+    txn_local_state: Vec<u8>, //   8-byte counter
+}
+
+impl RandomnessContext {
+    pub fn new() -> Self {
+        Self {
+            txn_local_state: vec![0; 8],
+        }
+    }
+
+    pub fn increment(&mut self) {
+        for byte in self.txn_local_state.iter_mut() {
+            if *byte < 255 {
+                *byte += 1;
+                break;
+            } else {
+                *byte = 0;
+            }
+        }
+    }
+}
+
+pub fn fetch_and_increment_txn_counter(
+    context: &mut SafeNativeContext,
+    _ty_args: Vec<Type>,
+    _args: VecDeque<Value>,
+) -> SafeNativeResult<SmallVec<[Value; 1]>> {
+    // TODO: charge gas?
+    let rand_ctxt = context.extensions_mut().get_mut::<RandomnessContext>();
+    let ret = rand_ctxt.txn_local_state.to_vec();
+    rand_ctxt.increment();
+    Ok(smallvec![Value::vector_u8(ret)])
+}
+
+pub fn make_all(
+    builder: &SafeNativeBuilder,
+) -> impl Iterator<Item = (String, NativeFunction)> + '_ {
+    let natives = vec![(
+        "fetch_and_increment_txn_counter",
+        fetch_and_increment_txn_counter as RawSafeNative,
+    )];
+
+    builder.make_named_natives(natives)
+}
