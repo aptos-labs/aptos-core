76e2c37073bb52a7613b9fffb691e91cca8d2bc8
George Mitenkov <georgemitenk0v@gmail.com>
2024-02-27 11:03:59 +0100

    [aggregator] Improve event testing (#12113)
    Tests compare events in a nicer fashion, and all transaction outputs are compared for
    both sequential/parallel and enabled/disabled flags.

--- aptos-move/e2e-move-tests/src/tests/aggregator_v2_events.rs
+++ aptos-move/e2e-move-tests/src/tests/aggregator_v2_events.rs
@@ -2,21 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    assert_success,
-    tests::{aggregator_v2::EAGGREGATOR_OVERFLOW, common},
-    MoveHarness,
+    aggregator_v2::AggV2TestHarness,
+    tests::aggregator_v2::{AggregatorMode, EAGGREGATOR_OVERFLOW},
+    BlockSplit, SUCCESS,
 };
-use aptos_language_e2e_tests::account::Account;
-use aptos_types::{
-    on_chain_config::FeatureFlag,
-    transaction::{ExecutionStatus, SignedTransaction, TransactionOutput},
-};
-use claims::{assert_matches, assert_ok, assert_some};
-use move_core_types::{
-    account_address::AccountAddress, ident_str, language_storage::ModuleId,
-    parser::parse_struct_tag, vm_status::AbortLocation,
-};
+use aptos_language_e2e_tests::{account::Account, executor::ExecutorMode};
+use aptos_types::transaction::SignedTransaction;
+use claims::{assert_none, assert_ok, assert_some};
+use move_core_types::{language_storage::TypeTag, parser::parse_struct_tag};
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use test_case::test_case;
 
 #[derive(Deserialize, Serialize)]
 struct Aggregator {
@@ -25,13 +21,27 @@ struct Aggregator {
 }
 
 #[derive(Deserialize, Serialize)]
-struct AggregatorSnapshot {
-    value: u64,
+struct Counter {
+    value: Aggregator,
+}
+
+macro_rules! assert_counter_value_eq {
+    ($h:ident, $v:expr) => {
+        let c = assert_some!($h.harness.read_resource::<Counter>(
+            $h.account.address(),
+            parse_struct_tag(&format!(
+                "{}::events_with_aggregators::Counter",
+                $h.account.address()
+            ))
+            .unwrap(),
+        ));
+        assert_eq!(c.value.value, $v);
+    };
 }
 
 #[derive(Deserialize, Serialize)]
-struct Counter {
-    value: Aggregator,
+struct AggregatorSnapshot {
+    value: u64,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -39,199 +49,146 @@ struct Event {
     value: AggregatorSnapshot,
 }
 
-fn publish_test_package(h: &mut MoveHarness, aptos_framework_account: &Account) {
-    let path_buf = common::test_dir_path("aggregator_v2.data/pack");
-    assert_success!(h.publish_package_cache_building(aptos_framework_account, path_buf.as_path()));
+macro_rules! assert_event_value_eq {
+    ($ce:expr, $v:expr) => {
+        let event = assert_ok!(bcs::from_bytes::<Event>($ce.event_data()));
+        assert_eq!(event.value.value, $v);
+    };
 }
 
-fn create_test_txn(
-    h: &mut MoveHarness,
-    aptos_framework_account: &Account,
-    name: &str,
-) -> SignedTransaction {
-    h.create_entry_function(
-        aptos_framework_account,
-        str::parse(name).unwrap(),
-        vec![],
-        vec![bcs::to_bytes(aptos_framework_account.address()).unwrap()],
-    )
+fn create_test_txn(h: &mut AggV2TestHarness, account: &Account, name: &str) -> SignedTransaction {
+    h.harness
+        .create_entry_function(account, str::parse(name).unwrap(), vec![], vec![
+            bcs::to_bytes(account.address()).unwrap(),
+        ])
 }
 
-fn run_entry_functions<F: Fn(ExecutionStatus)>(
-    h: &mut MoveHarness,
-    aptos_framework_account: &Account,
-    func_names: Vec<&str>,
-    check_status: F,
-) -> Vec<TransactionOutput> {
-    publish_test_package(h, aptos_framework_account);
-
-    // Make sure aggregators are enabled, so that we can test.
-    h.enable_features(
-        vec![
-            FeatureFlag::AGGREGATOR_V2_API,
-            FeatureFlag::AGGREGATOR_V2_DELAYED_FIELDS,
-            FeatureFlag::RESOURCE_GROUPS_SPLIT_IN_VM_CHANGE_SET,
-        ],
-        vec![],
+fn run(data: Vec<(u64, String, Option<u64>)>) -> AggV2TestHarness {
+    let mut h = crate::tests::aggregator_v2::setup(
+        ExecutorMode::BothComparison,
+        AggregatorMode::BothComparison,
+        data.len(),
     );
 
-    let txns = func_names
+    let account = h.account.clone();
+    let (txns, event_values): (_, Vec<_>) = data
         .into_iter()
-        .map(|name| create_test_txn(h, aptos_framework_account, name))
-        .collect();
+        .map(|(status_code, name, event_value)| {
+            (
+                (
+                    status_code,
+                    create_test_txn(
+                        &mut h,
+                        &account,
+                        &format!("{}::{}", account.address(), name),
+                    ),
+                ),
+                event_value,
+            )
+        })
+        .unzip();
 
-    let outputs = h.run_block_get_output(txns);
-    for output in outputs.iter() {
-        let execution_status = assert_ok!(output.status().as_kept_status());
-        check_status(execution_status);
-    }
+    let outputs = h.run_block_in_parts_and_check(BlockSplit::Whole, txns);
+
+    let event_v1_tag = assert_ok!(TypeTag::from_str(&format!(
+        "{}::events_with_aggregators::EventV1",
+        account.address()
+    )));
+    let event_v2_tag = assert_ok!(TypeTag::from_str(&format!(
+        "{}::events_with_aggregators::EventV2",
+        account.address()
+    )));
+
     outputs
+        .into_iter()
+        .zip(event_values)
+        .for_each(|(output, event_value)| {
+            let (_write_set, events) = output.into();
+            let mut events: Vec<_> = events
+                .into_iter()
+                .filter(|e| e.type_tag() == &event_v1_tag || e.type_tag() == &event_v2_tag)
+                .collect();
+            if events.is_empty() {
+                assert_none!(event_value);
+            } else {
+                assert_eq!(events.len(), 1);
+                let value = assert_some!(event_value);
+                assert_event_value_eq!(events.pop().unwrap(), value);
+            }
+        });
+
+    h
 }
 
-fn check_events(outputs: Vec<TransactionOutput>) {
-    // We always emit fee statement event. Currently this is hardcoded
-    // for existing test case.
-    assert_eq!(outputs[0].events().len(), 1);
-    assert_eq!(outputs[1].events().len(), 1);
-
-    assert_eq!(outputs[2].events().len(), 2);
-    let event = assert_ok!(bcs::from_bytes::<Event>(
-        outputs[2].events()[0].event_data()
-    ));
-    assert_eq!(event.value.value, 2);
-
-    assert_eq!(outputs[3].events().len(), 1);
-    assert_eq!(outputs[4].events().len(), 1);
-
-    assert_eq!(outputs[5].events().len(), 2);
-    let event = assert_ok!(bcs::from_bytes::<Event>(
-        outputs[5].events()[0].event_data()
-    ));
-    assert_eq!(event.value.value, 5);
-
-    assert_eq!(outputs[6].events().len(), 1);
-
-    assert_eq!(outputs[7].events().len(), 2);
-    let event = assert_ok!(bcs::from_bytes::<Event>(
-        outputs[7].events()[0].event_data()
-    ));
-    assert_eq!(event.value.value, 6);
-
-    assert_eq!(outputs[8].events().len(), 1);
-    assert_eq!(outputs[9].events().len(), 1);
+macro_rules! increment_counter {
+    () => {
+        (
+            SUCCESS,
+            "events_with_aggregators::increment_counter".to_string(),
+            None,
+        )
+    };
 }
 
-#[test]
-fn test_events_v1_with_snapshots() {
-    let func_names = vec![
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::test_emit_event_v1",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::test_increment_counter_and_emit_event_v1",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::test_emit_event_v1",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
+macro_rules! emit_event {
+    ($v:expr, $w:expr) => {
+        (
+            SUCCESS,
+            format!("events_with_aggregators::test_emit_event_v{}", $v),
+            $w,
+        )
+    };
+}
+
+macro_rules! increment_counter_emit_event {
+    ($s:expr, $v:expr, $w:expr) => {
+        (
+            $s,
+            format!(
+                "events_with_aggregators::test_increment_counter_and_emit_event_v{}",
+                $v
+            ),
+            $w,
+        )
+    };
+}
+
+#[test_case(1)]
+#[test_case(2)]
+fn test_events_with_snapshots(event_version: u64) {
+    let data = vec![
+        increment_counter!(),
+        increment_counter!(),
+        emit_event!(event_version, Some(2)),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter_emit_event!(SUCCESS, event_version, Some(5)),
+        increment_counter!(),
+        emit_event!(event_version, Some(6)),
+        increment_counter!(),
+        increment_counter!(),
+    ];
+    let h = run(data);
+    assert_counter_value_eq!(h, 8);
+}
+
+#[test_case(1)]
+#[test_case(2)]
+fn test_events_with_snapshots_not_emitted_on_abort(event_version: u64) {
+    let data = vec![
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter!(),
+        increment_counter_emit_event!(EAGGREGATOR_OVERFLOW, event_version, None),
     ];
 
-    let mut h = MoveHarness::new();
-    let aptos_framework_account = h.aptos_framework_account();
-    let check_status = |status: ExecutionStatus| {
-        assert_matches!(status, ExecutionStatus::Success);
-    };
-    let outputs = run_entry_functions(&mut h, &aptos_framework_account, func_names, check_status);
-
-    // Counter has been incremented 8 times.
-    let c = assert_some!(h.read_resource::<Counter>(
-        aptos_framework_account.address(),
-        parse_struct_tag("0x1::events_with_aggregators::Counter").unwrap(),
-    ));
-    assert_eq!(c.value.value, 8);
-    check_events(outputs);
-}
-
-#[test]
-fn test_events_v2_with_snapshots() {
-    let func_names = vec![
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::test_emit_event_v2",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::test_increment_counter_and_emit_event_v2",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::test_emit_event_v2",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-    ];
-
-    let mut h = MoveHarness::new();
-    let aptos_framework_account = h.aptos_framework_account();
-    let check_status = |status: ExecutionStatus| {
-        assert_matches!(status, ExecutionStatus::Success);
-    };
-    let outputs = run_entry_functions(&mut h, &aptos_framework_account, func_names, check_status);
-
-    let c = assert_some!(h.read_resource::<Counter>(
-        aptos_framework_account.address(),
-        parse_struct_tag("0x1::events_with_aggregators::Counter").unwrap(),
-    ));
-    assert_eq!(c.value.value, 8);
-    check_events(outputs);
-}
-
-fn check_failed_output(failed_output: TransactionOutput) {
-    let status = assert_ok!(failed_output.status().as_kept_status());
-    let aggregator_location =
-        ModuleId::new(AccountAddress::ONE, ident_str!("aggregator_v2").to_owned());
-    if let ExecutionStatus::MoveAbort {
-        location: AbortLocation::Module(id),
-        code: EAGGREGATOR_OVERFLOW,
-        info: Some(_),
-    } = status
-    {
-        assert_eq!(id, aggregator_location);
-    } else {
-        unreachable!("Expected Move abort, got {:?}", status);
-    }
-    assert_eq!(failed_output.events().len(), 1);
-}
-
-#[test]
-fn test_events_with_snapshots_not_emitted_on_abort() {
-    let func_names = vec![
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        "0x1::events_with_aggregators::increment_counter",
-        // Overflows here.
-        "0x1::events_with_aggregators::test_increment_counter_and_emit_event_v1",
-        "0x1::events_with_aggregators::test_increment_counter_and_emit_event_v2",
-    ];
-
-    let mut h = MoveHarness::new();
-    let aptos_framework_account = h.aptos_framework_account();
-    let check_status = |_: ExecutionStatus| {
-        // no-op.
-    };
-    let mut outputs =
-        run_entry_functions(&mut h, &aptos_framework_account, func_names, check_status);
-
-    let c = assert_some!(h.read_resource::<Counter>(
-        aptos_framework_account.address(),
-        parse_struct_tag("0x1::events_with_aggregators::Counter").unwrap(),
-    ));
-    assert_eq!(c.value.value, 10);
-
-    // Last two transactions overflow.
-    check_failed_output(outputs.pop().unwrap());
-    check_failed_output(outputs.pop().unwrap());
+    let h = run(data);
+    assert_counter_value_eq!(h, 10);
 }
