988e91acce4cc292e1e0023af1a72459541f6804
Balaji Arun <balaji@aptoslabs.com>
2024-02-27 11:22:35 -0800

    [consensus] stop reverifying self commit decisions

--- consensus/src/network.rs
+++ consensus/src/network.rs
@@ -112,6 +112,7 @@ pub struct IncomingDAGRequest {
 #[derive(Debug)]
 pub struct IncomingCommitRequest {
     pub req: CommitMessage,
+    pub author: Author,
     pub protocol: ProtocolId,
     pub response_sender: oneshot::Sender<Result<Bytes, RpcError>>,
 }
@@ -700,6 +701,7 @@ impl NetworkTask {
                             let req_with_callback =
                                 IncomingRpcRequest::CommitRequest(IncomingCommitRequest {
                                     req: CommitMessage::Vote(*commit_vote),
+                                    author: peer_id,
                                     protocol: RPC[0],
                                     response_sender: tx,
                                 });
@@ -715,6 +717,7 @@ impl NetworkTask {
                             let req_with_callback =
                                 IncomingRpcRequest::CommitRequest(IncomingCommitRequest {
                                     req: CommitMessage::Decision(*commit_decision),
+                                    author: peer_id,
                                     protocol: RPC[0],
                                     response_sender: tx,
                                 });
@@ -811,6 +814,7 @@ impl NetworkTask {
                         ConsensusMsg::CommitMessage(req) => {
                             IncomingRpcRequest::CommitRequest(IncomingCommitRequest {
                                 req: *req,
+                                author: peer_id,
                                 protocol,
                                 response_sender: callback,
                             })
