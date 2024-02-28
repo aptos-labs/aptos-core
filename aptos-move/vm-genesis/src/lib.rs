4bc685878bfb3527f60d9925901095955310d88c
zhoujun.ma <zhoujun@aptoslabs.com>
2024-02-28 07:47:03 -0800

    initialize randomness in genesis

--- aptos-move/vm-genesis/src/lib.rs
+++ aptos-move/vm-genesis/src/lib.rs
@@ -57,6 +57,7 @@ const VERSION_MODULE_NAME: &str = "version";
 const JWKS_MODULE_NAME: &str = "jwks";
 const CONFIG_BUFFER_MODULE_NAME: &str = "config_buffer";
 const DKG_MODULE_NAME: &str = "dkg";
+const RANDOMNESS_MODULE_NAME: &str = "randomness";
 const RECONFIGURATION_STATE_MODULE_NAME: &str = "reconfiguration_state";
 
 const NUM_SECONDS_PER_YEAR: u64 = 365 * 24 * 60 * 60;
@@ -256,6 +257,7 @@ pub fn encode_genesis_change_set(
     initialize_config_buffer(&mut session);
     initialize_dkg(&mut session);
     initialize_reconfiguration_state(&mut session);
+    initialize_randomness(&mut session);
     initialize_on_chain_governance(&mut session, genesis_config);
     create_and_initialize_validators(&mut session, validators);
     if genesis_config.is_test {
@@ -461,6 +463,16 @@ fn initialize_dkg(session: &mut SessionExt) {
     exec_function(
         session,
         DKG_MODULE_NAME,
+        "initialize",
+        vec![],
+        serialize_values(&vec![MoveValue::Signer(CORE_CODE_ADDRESS)]),
+    );
+}
+
+fn initialize_randomness(session: &mut SessionExt) {
+    exec_function(
+        session,
+        RANDOMNESS_MODULE_NAME,
         "initialize",
         vec![],
         serialize_values(&vec![MoveValue::Signer(CORE_CODE_ADDRESS)]),
