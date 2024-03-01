e4fb5bce98501306a0971569c79aa8db70020d80
Satya Vusirikala <satyasatya123456@gmail.com>
2024-03-01 10:33:42 -0800

    Minor fix

--- crates/transaction-generator-lib/src/args.rs
+++ crates/transaction-generator-lib/src/args.rs
@@ -61,6 +61,9 @@ pub enum TransactionTypeArg {
     EconiaBasic1Market,
     EconiaAdvanced1Market,
     EconiaAdvanced10Market,
+    EconiaBasic1MarketReuseAccounts
+    EconiaAdvanced1MarketReuseAccounts,
+    EconiaAdvanced10MarketReuseAccounts,
 }
 
 impl TransactionTypeArg {
