09305476eeacb1e0efc8629a50cb5b92c4a44dc8
Rex Fernando <rex.fernando@aptoslabs.com>
2024-03-03 13:41:03 -0800

    build_lib bug

--- keyless/rust-rapidsnark/rapidsnark/build_lib.sh
+++ keyless/rust-rapidsnark/rapidsnark/build_lib.sh
@@ -7,7 +7,8 @@ export CXX=/usr/bin/clang++
 
 if [[ $(uname -s) == "Darwin" ]]; then
   ./build_gmp.sh macos_arm64 || true
-  mkdir build_prover_macos_arm64 && cd build_prover_macos_arm64
+  mkdir -p build_prover_macos_arm64 
+  cd build_prover_macos_arm64
   cmake .. -DTARGET_PLATFORM=macos_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package
   make -j4 && make install
 else
