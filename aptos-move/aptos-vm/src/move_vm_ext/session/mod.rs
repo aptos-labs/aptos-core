cf8170e3e61b48500d47b0b750f0621507425d08
aldenhu <msmouse@gmail.com>
2024-03-05 19:20:42 +0000

    trivial: restructure files relavent to sessions

--- aptos-move/aptos-vm/src/move_vm_ext/session/mod.rs
+++ aptos-move/aptos-vm/src/move_vm_ext/session/mod.rs
@@ -4,10 +4,7 @@
 use crate::{
     data_cache::get_resource_group_from_metadata,
     move_vm_ext::{resource_state_key, write_op_converter::WriteOpConverter, AptosMoveResolver},
-    transaction_metadata::TransactionMetadata,
 };
-use aptos_crypto::{hash::CryptoHash, HashValue};
-use aptos_crypto_derive::{BCSCryptoHash, CryptoHasher};
 use aptos_framework::natives::{
     aggregator_natives::{AggregatorChangeSet, AggregatorChangeV1, NativeAggregatorContext},
     code::{NativeCodeContext, PublishRequest},
@@ -15,15 +12,12 @@ use aptos_framework::natives::{
 };
 use aptos_table_natives::{NativeTableContext, TableChangeSet};
 use aptos_types::{
-    access_path::AccessPath, block_metadata::BlockMetadata, block_metadata_ext::BlockMetadataExt,
-    contract_event::ContractEvent, state_store::state_key::StateKey,
-    validator_txn::ValidatorTransaction,
+    access_path::AccessPath, contract_event::ContractEvent, state_store::state_key::StateKey,
 };
 use aptos_vm_types::{change_set::VMChangeSet, storage::change_set_configs::ChangeSetConfigs};
 use bytes::Bytes;
 use move_binary_format::errors::{Location, PartialVMError, PartialVMResult, VMResult};
 use move_core_types::{
-    account_address::AccountAddress,
     effects::{AccountChanges, Changes, Op as MoveStorageOp},
     language_storage::{ModuleId, StructTag},
     value::MoveTypeLayout,
@@ -31,12 +25,15 @@ use move_core_types::{
 };
 use move_vm_runtime::{move_vm::MoveVM, session::Session};
 use move_vm_types::{value_serde::serialize_and_allow_delayed_values, values::Value};
-use serde::{Deserialize, Serialize};
 use std::{
     collections::BTreeMap,
     ops::{Deref, DerefMut},
     sync::Arc,
 };
+
+pub mod respawned_session;
+pub mod session_id;
+pub mod view_with_change_set;
 
 pub(crate) enum ResourceGroupChangeSet {
     // Merged resource groups op.
@@ -47,141 +44,6 @@ pub(crate) enum ResourceGroupChangeSet {
 type AccountChangeSet = AccountChanges<Bytes, BytesWithResourceLayout>;
 type ChangeSet = Changes<Bytes, BytesWithResourceLayout>;
 pub type BytesWithResourceLayout = (Bytes, Option<Arc<MoveTypeLayout>>);
-
-#[derive(BCSCryptoHash, CryptoHasher, Deserialize, Serialize)]
-pub enum SessionId {
-    Txn {
-        sender: AccountAddress,
-        sequence_number: u64,
-        script_hash: Vec<u8>,
-    },
-    BlockMeta {
-        // block id
-        id: HashValue,
-    },
-    Genesis {
-        // id to identify this specific genesis build
-        id: HashValue,
-    },
-    Prologue {
-        sender: AccountAddress,
-        sequence_number: u64,
-        script_hash: Vec<u8>,
-    },
-    Epilogue {
-        sender: AccountAddress,
-        sequence_number: u64,
-        script_hash: Vec<u8>,
-    },
-    // For those runs that are not a transaction and the output of which won't be committed.
-    Void,
-    RunOnAbort {
-        sender: AccountAddress,
-        sequence_number: u64,
-        script_hash: Vec<u8>,
-    },
-    BlockMetaExt {
-        // block id
-        id: HashValue,
-    },
-    ValidatorTxn {
-        script_hash: Vec<u8>,
-    },
-}
-
-impl SessionId {
-    pub fn txn_meta(txn_metadata: &TransactionMetadata) -> Self {
-        Self::Txn {
-            sender: txn_metadata.sender,
-            sequence_number: txn_metadata.sequence_number,
-            script_hash: txn_metadata.script_hash.clone(),
-        }
-    }
-
-    pub fn genesis(id: HashValue) -> Self {
-        Self::Genesis { id }
-    }
-
-    pub fn block_meta(block_meta: &BlockMetadata) -> Self {
-        Self::BlockMeta {
-            id: block_meta.id(),
-        }
-    }
-
-    pub fn block_meta_ext(block_meta_ext: &BlockMetadataExt) -> Self {
-        Self::BlockMetaExt {
-            id: block_meta_ext.id(),
-        }
-    }
-
-    pub fn prologue_meta(txn_metadata: &TransactionMetadata) -> Self {
-        Self::Prologue {
-            sender: txn_metadata.sender,
-            sequence_number: txn_metadata.sequence_number,
-            script_hash: txn_metadata.script_hash.clone(),
-        }
-    }
-
-    pub fn run_on_abort(txn_metadata: &TransactionMetadata) -> Self {
-        Self::RunOnAbort {
-            sender: txn_metadata.sender,
-            sequence_number: txn_metadata.sequence_number,
-            script_hash: txn_metadata.script_hash.clone(),
-        }
-    }
-
-    pub fn epilogue_meta(txn_metadata: &TransactionMetadata) -> Self {
-        Self::Epilogue {
-            sender: txn_metadata.sender,
-            sequence_number: txn_metadata.sequence_number,
-            script_hash: txn_metadata.script_hash.clone(),
-        }
-    }
-
-    pub fn void() -> Self {
-        Self::Void
-    }
-
-    pub fn validator_txn(txn: &ValidatorTransaction) -> Self {
-        Self::ValidatorTxn {
-            script_hash: txn.hash().to_vec(),
-        }
-    }
-
-    pub fn as_uuid(&self) -> HashValue {
-        self.hash()
-    }
-
-    pub(crate) fn into_script_hash(self) -> Vec<u8> {
-        match self {
-            Self::Txn {
-                sender: _,
-                sequence_number: _,
-                script_hash,
-            }
-            | Self::Prologue {
-                sender: _,
-                sequence_number: _,
-                script_hash,
-            }
-            | Self::Epilogue {
-                sender: _,
-                sequence_number: _,
-                script_hash,
-            }
-            | Self::RunOnAbort {
-                sender: _,
-                sequence_number: _,
-                script_hash,
-            }
-            | Self::ValidatorTxn { script_hash } => script_hash,
-            Self::BlockMeta { id: _ }
-            | Self::Genesis { id: _ }
-            | Self::Void
-            | Self::BlockMetaExt { id: _ } => vec![],
-        }
-    }
-}
 
 pub struct SessionExt<'r, 'l> {
     inner: Session<'r, 'l>,
