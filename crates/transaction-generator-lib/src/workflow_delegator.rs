2c83b68c67ac564a2ba3d21edac45d2358fd26fa
Satya Vusirikala <satyasatya123456@gmail.com>
2024-03-06 02:06:47 -0800

    Updated timer

--- crates/transaction-generator-lib/src/workflow_delegator.rs
+++ crates/transaction-generator-lib/src/workflow_delegator.rs
@@ -136,8 +136,8 @@ impl TransactionGenerator for WorkflowTxnGenerator {
                 ..
             } => {
                 if stage_start_time.load(Ordering::Relaxed) > StageTracking::current_timestamp() {
-                    info!("Waiting for next stage");
-                    thread::sleep(Duration::from_secs(stage_start_time.fetch_sub(StageTracking::current_timestamp(), Ordering::Relaxed)));
+                    info!("Waiting for next stage for {} seconds", stage_start_time.fetch_sub(StageTracking::current_timestamp(), Ordering::Relaxed));
+                    thread::sleep(Duration::from_secs(5));
                 }
                 stage_counter.load(Ordering::Relaxed)
             },
