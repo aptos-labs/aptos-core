f43d010014400b42190dd9039ffe8b5c2a9fee45
danielxiangzl <xiangzhuolun@gmail.com>
2024-03-05 21:11:05 -0800

    test

--- types/src/on_chain_config/aptos_features.rs
+++ types/src/on_chain_config/aptos_features.rs
@@ -112,7 +112,7 @@ impl FeatureFlag {
             FeatureFlag::RESOURCE_GROUPS_SPLIT_IN_VM_CHANGE_SET,
             FeatureFlag::COMMISSION_CHANGE_DELEGATION_POOL,
             FeatureFlag::WEBAUTHN_SIGNATURE,
-            // FeatureFlag::RECONFIGURE_WITH_DKG, //TODO: re-enable once randomness is ready.
+            FeatureFlag::RECONFIGURE_WITH_DKG, //TODO: re-enable once randomness is ready.
             FeatureFlag::KEYLESS_ACCOUNTS,
             FeatureFlag::KEYLESS_BUT_ZKLESS_ACCOUNTS,
             FeatureFlag::JWK_CONSENSUS,
@@ -121,7 +121,7 @@ impl FeatureFlag {
             FeatureFlag::MAX_OBJECT_NESTING_CHECK,
             FeatureFlag::KEYLESS_ACCOUNTS_WITH_PASSKEYS,
             FeatureFlag::TRANSACTION_CONTEXT_EXTENSION,
-            // FeatureFlag::FAST_RANDOMNESS,
+            FeatureFlag::FAST_RANDOMNESS,
         ]
     }
 }
