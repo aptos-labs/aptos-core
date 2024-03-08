570c2b2b50a7e88eeb5aa8c502d3ac67e14dc227
igor-aptos <110557261+igor-aptos@users.noreply.github.com>
2024-03-07 23:53:25 -0800

    improved txn emitter fund accounting (#12352)
    Make computation of needed funds more precise:
    - accounting for min balance needed
    - accounting for difference between actual loadtest traffic and minting
      - check if accounts exist, and then use transfer gas costs, otherwise account creation gas costs for minting
      - init_max_gas_per_txn now refers to non-account creation initialization - i.e. module publishing. I.e. if we need
        to publish one module, we don't need to require larger balances and gas consumption for the whole acount minting stage
    - make all consumption from root (i.e. module publishing) require approval - so that we don't consume unexpected amounts
    - make more things optional and have sane defaults. init multiplier is 2x (enough to get to second bucket), transfer/creating gas needed is provided, expected-gas-per-txn defaults to max-gas-per-txn if not set.
    
    So this is the full set of flags required for very low consumption load test:
    
    --expected-max-txns 12000 --init-max-gas-per-txn=200000 --max-gas-per-txn=3 --max-transactions-per-account 5 --transaction-type no-op --account-minter-seed=...
    
    you can also add --init-gas-price-multiplier=1 for slightly lower initialization costs.

--- crates/transaction-generator-lib/src/lib.rs
+++ crates/transaction-generator-lib/src/lib.rs
@@ -200,9 +200,34 @@ impl CounterState {
     }
 }
 
+#[async_trait::async_trait]
+pub trait RootAccountHandle: Send + Sync {
+    async fn approve_funds(&self, amount: u64, reason: &str);
+
+    fn get_root_account(&self) -> &LocalAccount;
+}
+
+pub struct AlwaysApproveRootAccountHandle<'t> {
+    pub root_account: &'t LocalAccount,
+}
+
+#[async_trait::async_trait]
+impl<'t> RootAccountHandle for AlwaysApproveRootAccountHandle<'t> {
+    async fn approve_funds(&self, amount: u64, reason: &str) {
+        println!(
+            "Consuming funds from root/source account: up to {} for {}",
+            amount, reason
+        );
+    }
+
+    fn get_root_account(&self) -> &LocalAccount {
+        self.root_account
+    }
+}
+
 pub async fn create_txn_generator_creator(
     transaction_mix_per_phase: &[Vec<(TransactionType, usize)>],
-    root_account: &mut LocalAccount,
+    root_account: impl RootAccountHandle,
     source_accounts: &mut [LocalAccount],
     initial_burner_accounts: Vec<LocalAccount>,
     txn_executor: &dyn ReliableTransactionSubmitter,
@@ -308,7 +333,7 @@ pub async fn create_txn_generator_creator(
                         CustomModulesDelegationGeneratorCreator::new(
                             txn_factory.clone(),
                             init_txn_factory.clone(),
-                            root_account,
+                            &root_account,
                             txn_executor,
                             *num_modules,
                             entry_point.package_name(),
@@ -339,7 +364,7 @@ pub async fn create_txn_generator_creator(
                         *workflow_kind,
                         txn_factory.clone(),
                         init_txn_factory.clone(),
-                        root_account,
+                        &root_account,
                         txn_executor,
                         *num_modules,
                         use_account_pool.then(|| accounts_pool.clone()),
