bac93e4cb1d7788114d4aaa85345e72e390d2f1e
Balaji Arun <balaji@aptoslabs.com>
2024-03-10 09:57:21 -0700

    test

--- config/src/config/dag_consensus_config.rs
+++ config/src/config/dag_consensus_config.rs
@@ -117,7 +117,7 @@ impl Default for ReliableBroadcastConfig {
     fn default() -> Self {
         Self {
             // A backoff policy that starts at 200ms and doubles each iteration up to 10secs.
-            backoff_policy_base_ms: 2,
+            backoff_policy_base_ms: 4,
             backoff_policy_factor: 100,
             backoff_policy_max_delay_ms: 10000,
 
@@ -135,7 +135,7 @@ pub struct DagRoundStateConfig {
 impl Default for DagRoundStateConfig {
     fn default() -> Self {
         Self {
-            adaptive_responsive_minimum_wait_time_ms: 500,
+            adaptive_responsive_minimum_wait_time_ms: 5000,
         }
     }
 }
