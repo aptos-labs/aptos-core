7d0c64598104fcc83a863676e5886084b4f67606
Brian Cho <brian@aptoslabs.com>
2024-03-21 10:47:46 -0700

    make pfn test the landblocking one

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -605,7 +605,7 @@ fn get_land_blocking_test(
 ) -> Option<ForgeConfig> {
     let test = match test_name {
         "land_blocking" => land_blocking_test_suite(duration), // TODO: remove land_blocking, superseded by below
-        "realistic_env_max_load" => realistic_env_max_load_test(duration, test_cmd, 7, 5),
+        "realistic_env_max_load" => pfn_performance(duration, true, true, false),
         "compat" => compat(),
         "framework_upgrade" => framework_upgrade(),
         _ => return None, // The test name does not match a land-blocking test
