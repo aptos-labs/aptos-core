6b34adca6869a247f0fa4489681955aded0caa60
Vineeth Kashyap <vineeth.rv@gmail.com>
2024-03-22 17:50:12 -0400

    Fixed bug in output of disassembled code.

--- third_party/move/tools/move-disassembler/src/disassembler.rs
+++ third_party/move/tools/move-disassembler/src/disassembler.rs
@@ -1035,7 +1035,6 @@ impl<'a> Disassembler<'a> {
         &self,
         function_source_map: &FunctionSourceMap,
         locals_idx: SignatureIndex,
-        parameter_len: usize,
     ) -> Result<Vec<String>> {
         if !self.options.print_locals {
             return Ok(vec![]);
@@ -1045,11 +1044,9 @@ impl<'a> Disassembler<'a> {
         let locals_names_tys = function_source_map
             .locals
             .iter()
-            .skip(parameter_len)
             .enumerate()
             .map(|(local_idx, (name, _))| {
-                let ty =
-                    self.type_for_local(parameter_len + local_idx, signature, function_source_map)?;
+                let ty = self.type_for_local(local_idx, signature, function_source_map)?;
                 Ok(format!("{}: {}", name, ty))
             })
             .collect::<Result<Vec<String>>>()?;
@@ -1143,8 +1140,7 @@ impl<'a> Disassembler<'a> {
 
         let body = match code {
             Some(code) => {
-                let locals =
-                    self.disassemble_locals(function_source_map, code.locals, params.len())?;
+                let locals = self.disassemble_locals(function_source_map, code.locals)?;
                 let bytecode =
                     self.disassemble_bytecode(function_source_map, name, parameters, code)?;
                 Self::format_function_body(locals, bytecode)
