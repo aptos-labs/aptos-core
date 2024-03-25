a97c6c4e6d5a4349fc1c24e3ebc4eead08852ded
zhoujun.ma <zhoujun@aptoslabs.com>
2024-03-24 18:45:30 -0700

    disable randomness

--- types/src/on_chain_config/randomness_config.rs
+++ types/src/on_chain_config/randomness_config.rs
@@ -112,7 +112,7 @@ impl OnChainRandomnessConfig {
     }
 
     pub fn default_for_genesis() -> Self {
-        OnChainRandomnessConfig::V1(ConfigV1::default())
+        OnChainRandomnessConfig::Off
     }
 
     pub fn randomness_enabled(&self) -> bool {
