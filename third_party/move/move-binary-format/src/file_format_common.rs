13fa81694bcfec2787e791e7d05f6b9cc8618184
Teng Zhang <rahxephon89@163.com>
2024-03-25 14:07:17 -0700

    change version

--- third_party/move/move-binary-format/src/file_format_common.rs
+++ third_party/move/move-binary-format/src/file_format_common.rs
@@ -478,8 +478,12 @@ pub const VERSION_6: u32 = 6;
 /// + access specifiers (read/write set)
 pub const VERSION_7: u32 = 7;
 
+/// Version 8: changes compare to version 7
+/// place holder for next version
+pub const VERSION_8: u32 = 8;
+
 /// Mark which version is the default version
-pub const VERSION_DEFAULT: u32 = VERSION_6;
+pub const VERSION_DEFAULT: u32 = VERSION_7;
 
 /// Mark which version is the latest version
 pub const VERSION_MAX: u32 = VERSION_7;
@@ -490,7 +494,7 @@ pub const VERSION_MAX: u32 = VERSION_7;
 ///
 /// This is currently set to VERSION_7, the next major version, and should be updated once
 /// this version is ready for production.
-pub const VERSION_NEXT: u32 = VERSION_7;
+pub const VERSION_NEXT: u32 = VERSION_8;
 
 // Mark which oldest version is supported.
 // TODO(#145): finish v4 compatibility; as of now, only metadata is implemented
