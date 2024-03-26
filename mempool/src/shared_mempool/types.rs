e46d9bde248abe3a151cf09fe4ebb88b9807d6ff
Joshua Lind <josh.lind@hotmail.com>
2024-03-26 14:50:45 -0400

    [Mempool] Improve peer selection. (#12688)

--- mempool/src/shared_mempool/types.rs
+++ mempool/src/shared_mempool/types.rs
@@ -17,9 +17,7 @@ use aptos_consensus_types::common::{
 };
 use aptos_crypto::HashValue;
 use aptos_infallible::{Mutex, RwLock};
-use aptos_network::{
-    application::interface::NetworkClientInterface, transport::ConnectionMetadata,
-};
+use aptos_network::application::interface::NetworkClientInterface;
 use aptos_storage_interface::DbReader;
 use aptos_types::{
     mempool_status::MempoolStatus, transaction::SignedTransaction, vm_status::DiscardedVMStatus,
@@ -244,15 +242,13 @@ pub type MempoolEventsReceiver = mpsc::Receiver<MempoolClientRequest>;
 pub(crate) struct PeerSyncState {
     pub timeline_id: MultiBucketTimelineIndexIds,
     pub broadcast_info: BroadcastInfo,
-    pub metadata: ConnectionMetadata,
 }
 
 impl PeerSyncState {
-    pub fn new(metadata: ConnectionMetadata, num_broadcast_buckets: usize) -> Self {
+    pub fn new(num_broadcast_buckets: usize) -> Self {
         PeerSyncState {
             timeline_id: MultiBucketTimelineIndexIds::new(num_broadcast_buckets),
             broadcast_info: BroadcastInfo::new(),
-            metadata,
         }
     }
 }
