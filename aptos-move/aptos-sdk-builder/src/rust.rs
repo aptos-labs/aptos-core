c9b450c864f5aa723e1fe08405e4f479b908b9f9
Brian R. Murphy <132495859+brmataptos@users.noreply.github.com>
2024-03-26 10:48:11 -0700

    convert files that add copyright notices to files

--- aptos-move/aptos-sdk-builder/src/rust.rs
+++ aptos-move/aptos-sdk-builder/src/rust.rs
@@ -1,5 +1,5 @@
-// Copyright © Aptos Foundation
-// Parts of the project are originally copyright © Meta Platforms, Inc.
+// Copyright (c) Aptos Foundation
+// Parts of the project are originally copyright (c) Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::common;
@@ -123,7 +123,7 @@ where
             writeln!(
                 self.out,
                 r#"
-// Copyright © Aptos Foundation
+// Copyright (c) Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
 // This file was generated. Do not modify!
