e4ca52b618fc7a207ef4f76b392ecae62383f82e
Satya Vusirikala <satyasatya123456@gmail.com>
2024-03-27 10:46:54 -0700

    Enable allow_batches_without_pos_in_proposal flag

--- config/src/config/quorum_store_config.rs
+++ config/src/config/quorum_store_config.rs
@@ -108,7 +108,7 @@ impl Default for QuorumStoreConfig {
             // number of batch coordinators to handle QS batch messages, should be >= 1
             num_workers_for_remote_batches: 10,
             batch_buckets: DEFAULT_BUCKETS.to_vec(),
-            allow_batches_without_pos_in_proposal: false,
+            allow_batches_without_pos_in_proposal: true,
         }
     }
 }
