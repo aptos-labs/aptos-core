dc8a97d55a5e7eebc607195ff237470e50c8ce23
zhoujunma <zjma@users.noreply.github.com>
2024-03-27 11:16:46 -0700

    support 0 shares in PVSS (#12658)
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * revert testing stuff
    
    * debug
    
    * debug
    
    * ut
    
    * polish uts
    
    * augment existing uts instead of creating new ones

--- crates/aptos-dkg/src/pvss/test_utils.rs
+++ crates/aptos-dkg/src/pvss/test_utils.rs
@@ -165,6 +165,9 @@ pub fn get_weighted_configs_for_testing() -> Vec<WeightedConfig> {
     // 50-out-of-100, weights [11, 13, 9, 10, 12, 8, 7, 14, 10, 6]
     wcs.push(WeightedConfig::new(50, vec![11, 13, 9, 10, 12, 8, 7, 14, 10, 6]).unwrap());
 
+    // 7-out-of-15, weights [0, 0, 0, 2, 2, 2, 0, 0, 0, 3, 3, 3, 0, 0, 0]
+    wcs.push(WeightedConfig::new(7, vec![0, 0, 0, 2, 2, 2, 0, 0, 0, 3, 3, 3, 0, 0, 0]).unwrap());
+
     wcs
 }
 
