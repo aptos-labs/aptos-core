afdfe7a32961bb2b4752f0f55ce464fb220fa5be
Larry Liu <larry@aptoslabs.com>
2024-03-27 13:54:25 -0700

    add the proto breaking check.

---
+++ protos/scripts/check_proto_compatibility.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script checks the compatibility of the protos in this repository under current branch against
+# the protos in the aptos-core repository under the specified tag.
+# The goal is to provide a way for developers to check if their changes to the protos are compatible with
+# the version of aptos-core that they are targeting.
+
+set -ex
+
+# Change to the protos directory
+cd "$(git rev-parse --show-toplevel)/protos"
+
+APTOS_CORE_VERSION="${1:?missing commit or tag to compare against}"
+
+repo_url="https://github.com/aptos-labs/aptos-core.git#tag=$APTOS_CORE_VERSION,subdir=protos/proto"
+buf breaking --against "$repo_url"
