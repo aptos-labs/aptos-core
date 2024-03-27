8688e843b1d6d22f105c42e4fd67bafabfb98677
Larry Liu <larry@aptoslabs.com>
2024-03-27 14:38:50 -0700

    test.

--- protos/scripts/check_external_proto_compatibility.sh
+++ protos/scripts/check_external_proto_compatibility.sh
@@ -5,12 +5,17 @@
 # The goal is to provide a way for developers to check if their changes to the protos are compatible with
 # the version of aptos-core that they are targeting.
 
+# Note: external protos are:
+#       1. transaction stream related.
+#       2. TBA.
+
 set -ex
 
 # Change to the protos directory
-cd "$(git rev-parse --show-toplevel)/protos"
+cd "$(git rev-parse --show-toplevel)/protos/proto"
 
 APTOS_CORE_VERSION="${1:?missing commit or tag to compare against}"
 
+buf build -o current.bin
 repo_url="https://github.com/aptos-labs/aptos-core.git#tag=$APTOS_CORE_VERSION,subdir=protos/proto"
-buf breaking --against "$repo_url" --verbose
+buf breaking current.bin --against "$repo_url" --verbose 
