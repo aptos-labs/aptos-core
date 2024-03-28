c02ffeb746d59d356c8e7ce62ac89b4798af20f1
Josh Lind <josh.lind@hotmail.com>
2024-03-28 16:02:10 -0400

    [CI/CD] Split up rust lint and cached packages scripts.

--- scripts/rust_lint.sh
+++ scripts/rust_lint.sh
@@ -5,14 +5,14 @@
 #
 # The best way to do this however is to run scripts/dev_setup.sh
 #
-# If you want to run this from anywhere in aptos-core, try adding this wrapepr
+# If you want to run this from anywhere in aptos-core, try adding the wrapper
 # script to your path:
 # https://gist.github.com/banool/e6a2b85e2fff067d3a215cbfaf808032
 
 # Make sure we're in the root of the repo.
 if [ ! -d ".github" ]
 then
-    echo "Please run this from the root of aptos-core"
+    echo "Please run this from the root of aptos-core!"
     exit 1
 fi
 
@@ -22,9 +22,11 @@ if [ "$1" = "--check" ]; then
     CHECK_ARG="--check"
 fi
 
+# Set appropriate script flags
 set -e
 set -x
 
+# Run clippy with the aptos-core specific configuration.
 cargo xclippy
 
 # We require the nightly build of cargo fmt
@@ -35,14 +37,3 @@ cargo +nightly fmt $CHECK_ARG
 # we can move to cleaner workspace dependency notation.
 # See: https://github.com/DevinR528/cargo-sort/issues/47
 cargo sort --grouped --workspace $CHECK_ARG
-
-# Ensure that aptos-cached-packages have been built correctly.
-unset SKIP_FRAMEWORK_BUILD
-cargo build -p aptos-cached-packages
-if [ -n "$CHECK_ARG" ]; then
-    if [ -n "$(git status --porcelain -uno aptos-move)" ]; then
-      git diff
-      echo "There are unstaged changes after running 'cargo build -p aptos-cached-packages'! Are you sure aptos-cached-packages is up-to-date?"
-      exit 1
-    fi
-fi
