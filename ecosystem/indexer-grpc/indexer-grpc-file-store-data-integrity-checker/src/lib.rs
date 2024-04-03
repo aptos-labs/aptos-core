6a6fe2ea3da54f86d52ee4754cb7038a150e2849
Larry Liu <larry@aptoslabs.com>
2024-04-03 14:19:57 -0700

    add file store integrity check worker.

---
+++ ecosystem/indexer-grpc/indexer-grpc-file-store-data-integrity-checker/src/lib.rs
@@ -0,0 +1,48 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+mod worker;
+
+use crate::worker::Worker;
+use anyhow::{Context, Result};
+use aptos_indexer_grpc_server_framework::RunnableConfig;
+use aptos_indexer_grpc_utils::config::IndexerGrpcFileStoreConfig;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct IndexerGrpcFileStoreDataIntegrityCheckerConfig {
+    pub file_store_config: IndexerGrpcFileStoreConfig,
+    pub starting_version: Option<u64>,
+}
+
+impl IndexerGrpcFileStoreDataIntegrityCheckerConfig {
+    pub fn new(
+        file_store_config: IndexerGrpcFileStoreConfig,
+        starting_version: Option<u64>,
+    ) -> Self {
+        Self {
+            file_store_config,
+            starting_version,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl RunnableConfig for IndexerGrpcFileStoreDataIntegrityCheckerConfig {
+    async fn run(&self) -> Result<()> {
+        let mut worker = Worker::new(self.file_store_config.clone(), self.starting_version)
+            .await
+            .context("Failed to create data integrity checker")?;
+        worker
+            .run()
+            .await
+            .context("Failed to run data integrity checker")
+            .expect("Cache worker failed");
+        Ok(())
+    }
+
+    fn get_server_name(&self) -> String {
+        "idxdataitg".to_string()
+    }
+}
---
+++ ecosystem/indexer-grpc/indexer-grpc-file-store-data-integrity-checker/src/worker.rs
@@ -0,0 +1,84 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use aptos_indexer_grpc_utils::config::IndexerGrpcFileStoreConfig;
+use std::{collections::BTreeMap, sync::Arc};
+use tokio::sync::Mutex;
+
+pub(crate) struct Worker {
+    file_store_config: IndexerGrpcFileStoreConfig,
+    starting_version: Option<u64>,
+}
+
+impl Worker {
+    pub async fn new(
+        file_store_config: IndexerGrpcFileStoreConfig,
+        starting_version: Option<u64>,
+    ) -> Result<Self> {
+        Ok(Self {
+            file_store_config,
+            starting_version,
+        })
+    }
+
+    pub async fn run(&mut self) -> Result<()> {
+        let file_store_operator = self.file_store_config.create();
+        let starting_vresion = self.starting_version.unwrap_or(0);
+        let cursor = Arc::new(Mutex::new(starting_vresion));
+        let data_to_process = Arc::new(Mutex::new(BTreeMap::new()));
+        let task_count = 32;
+        let current_metadata = file_store_operator.get_file_store_metadata().await.unwrap();
+        let ending_version = current_metadata.version;
+        for _ in 0..task_count {
+            let file_store_operator = file_store_operator.clone_box();
+            let cursor = cursor.clone();
+            let data_to_process = data_to_process.clone();
+            tokio::spawn(async move {
+                loop {
+                    let version_to_process = {
+                        let mut cursor_now = cursor.lock().await;
+                        let version_to_process = *cursor_now;
+                        *cursor_now += 1000;
+                        version_to_process
+                    };
+                    if version_to_process >= ending_version {
+                        break;
+                    }
+
+                    let transactions = file_store_operator
+                        .get_transactions(version_to_process, 3)
+                        .await
+                        .unwrap();
+                    let mut data_to_process_now = data_to_process.lock().await;
+                    data_to_process_now.insert(version_to_process, transactions);
+                }
+            });
+        }
+        // Checker loop.
+        let mut version_to_check = 0;
+        loop {
+            if version_to_check >= ending_version {
+                break;
+            }
+            // Get the lowest version in data_to_process.
+            let transactions = {
+                let mut data_to_process_now = data_to_process.lock().await;
+                if !data_to_process_now.contains_key(&version_to_check) {
+                    tokio::time::sleep(tokio::time::Duration::from_millis(20)).await;
+                    continue;
+                }
+                data_to_process_now.remove(&version_to_check).unwrap()
+            };
+            for transaction in transactions {
+                assert_eq!(transaction.version, version_to_check);
+                version_to_check += 1;
+            }
+        }
+
+        loop {
+            tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
+            tracing::info!("Data Integrity Checker is done!");
+        }
+    }
+}
