b1a93882052665904a8a4a57ee7adbed03785a04
Balaji Arun <balaji@aptoslabs.com>
2024-04-06 16:40:03 -0700

    increase rb concurrent bound

--- consensus/src/consensus_provider.rs
+++ consensus/src/consensus_provider.rs
@@ -71,7 +71,7 @@ pub fn start_consensus(
         aptos_channels::new_unbounded(&counters::PENDING_SELF_MESSAGES);
     let consensus_network_client = ConsensusNetworkClient::new(network_client);
     let rand_storage = Arc::new(RandDb::new(node_config.storage.dir()));
-    let bounded_executor = BoundedExecutor::new(200, runtime.handle().clone());
+    let bounded_executor = BoundedExecutor::new(1000, runtime.handle().clone());
 
     let execution_client = Arc::new(ExecutionProxyClient::new(
         node_config.consensus.clone(),
