59586fee4ebb88d659f0f74afa094d728cf32b5d
Teng Zhang <rahxephon89@163.com>
2024-04-10 16:29:45 -0700

    [Compiler-v2] check access for structs (#12821)
    * check field
    
    * handle comments

--- third_party/move/move-compiler-v2/tests/testsuite.rs
+++ third_party/move/move-compiler-v2/tests/testsuite.rs
@@ -333,7 +333,8 @@ const TEST_CONFIGS: Lazy<BTreeMap<&str, TestConfig>> = Lazy::new(|| {
             include: vec!["/acquires-checker/"],
             exclude: vec![],
             exp_suffix: None,
-            options: opts.clone(),
+            // Skip access check to avoid error message change in the acquires-checker
+            options: opts.clone().set_experiment(Experiment::ACCESS_CHECK, false),
             // Run the full compiler pipeline to double-check the result.
             stop_after: StopAfter::FileFormat,
             dump_ast: DumpLevel::None,
