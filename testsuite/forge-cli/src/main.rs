a2e45de237f33e699aaa276c1a24ee75cba4061b
Sital Kedia <skedia@aptoslabs.com>
2024-04-11 13:18:17 -0700

    Contented resource = 1

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1457,7 +1457,7 @@ fn realistic_env_graceful_overload() -> ForgeConfig {
 }
 
 fn adverserial_workload_test() -> ForgeConfig {
-    let num_contended_resources = 4;
+    let num_contended_resources = 1;
     let transaction_mix = (0..num_contended_resources)
         .map(|_| {
             (
