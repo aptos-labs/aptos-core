54a1805abd154be17e201500734adbceea1147ca
zhoujun.ma <zhoujun@aptoslabs.com>
2024-04-13 15:27:25 -0700

    update install-deps.sh

--- keyless/circuit-data/install-deps.sh
+++ keyless/circuit-data/install-deps.sh
@@ -3,7 +3,6 @@
 set -e
 install_node() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    source ~/.bashrc
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
