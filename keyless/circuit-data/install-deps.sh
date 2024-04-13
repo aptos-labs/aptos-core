4a8048682b024928bcf30155e00b58d23d7b0258
zhoujun.ma <zhoujun@aptoslabs.com>
2024-04-13 16:25:19 -0700

    fix ci script

--- keyless/circuit-data/install-deps.sh
+++ keyless/circuit-data/install-deps.sh
@@ -2,6 +2,7 @@
 
 set -e
 install_node() {
+    echo -n "nodejs installation started."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -9,9 +10,11 @@ install_node() {
     nvm install --lts
     node -v
     npm -v
+    echo -n "nodejs installation finished."
 }
 
 install_git_lfs() {
+    echo -n "git lfs installation started."
     if which brew > /dev/null; then
         brew install git-lfs
     elif which apt-get > /dev/null; then
@@ -20,32 +23,32 @@ install_git_lfs() {
         echo "Can't figure out what platform you are on. Currently this script only supports MacOS and Debian."
     fi
     git lfs install
+    echo -n "git lfs installation finished."
 }
 
 install_circom() {
-    echo -n "Checking if $1 is installed..."
-    if which $1 > /dev/null; then
-        echo "yes."
-    else
-        echo "no, installing..."
-
-        original_dir=$(pwd)
-        cd `mktemp -d`
-        git clone http://github.com/iden3/circom
-        cd circom
-        git switch -d v2.1.7
-        cargo build --release
-        cargo install --path circom
-        cd "$original_dir"
-    fi
+    echo -n "circom installation started."
+    original_dir=$(pwd)
+    cd `mktemp -d`
+    git clone https://github.com/iden3/circom
+    cd circom
+    git switch -d v2.1.7
+    cargo build --release
+    cargo install --path circom
+    cd "$original_dir"
+    echo -n "circom installation finished."
 }
 
 install_pip3_deps() {
-  pip3 install virtualenv pyjwt cryptography pycryptodome
+    echo -n "pip3 deps installation started."
+    pip3 install virtualenv pyjwt cryptography pycryptodome
+    echo -n "pip3 deps installation finished."
 }
   
 install_npm_deps() {
+    echo -n "snarkjs installation started."
     npm install -g snarkjs
+    echo -n "snarkjs installation finished."
 }
 
 install_node
