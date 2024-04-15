a9d7d1f3579a5280c2babd300f7f3e34adbf2196
Balaji Arun <balaji@aptoslabs.com>
2024-04-15 08:17:59 -0700

    fine counters for dag

--- consensus/src/dag/observability/counters.rs
+++ consensus/src/dag/observability/counters.rs
@@ -7,12 +7,23 @@ use aptos_metrics_core::{
 };
 use once_cell::sync::Lazy;
 
+const HIGH_LATENCY_BUCKETS: &[f64] = &[
+    0.005, 0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 
+    1.6, 1.7, 1.8, 1.9, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
+];
+
+const LOW_LATENCY_BUCKETS: &[f64] = &[
+    0.001, 0.002, 0.005, 0.01, 0.02, 0.03, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 
+    1.6, 1.8, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
+];
+
 /// Traces node latency movement throughout the DAG
 pub static NODE_TRACING: Lazy<HistogramVec> = Lazy::new(|| {
     register_histogram_vec!(
         "aptos_consensus_dag_node_tracing",
         "Histogram for different stages of a node",
-        &["stage"]
+        &["stage"],
+        HIGH_LATENCY_BUCKETS.to_vec(),
     )
     .unwrap()
 });
@@ -22,7 +33,8 @@ pub static ROUND_TRACING: Lazy<HistogramVec> = Lazy::new(|| {
     register_histogram_vec!(
         "aptos_consensus_dag_round_tracing",
         "Histogram for different stages of a round",
-        &["stage"]
+        &["stage"],
+        HIGH_LATENCY_BUCKETS.to_vec(),
     )
     .unwrap()
 });
@@ -112,7 +124,8 @@ pub static DAG_RPC_CHANNEL: Lazy<IntCounterVec> = Lazy::new(|| {
 pub static INCOMING_MSG_PROCESSING: Lazy<Histogram> = Lazy::new(|| {
     register_histogram!(
         "aptos_consensus_dag_incoming_msg_process",
-        "dag incoming message processing"
+        "dag incoming message processing",
+        LOW_LATENCY_BUCKETS.to_vec(),
     )
     .unwrap()
 });
@@ -125,11 +138,14 @@ pub static TIMEOUT_WAIT_VOTING_POWER_COUNT: Lazy<IntCounter> = Lazy::new(|| {
     .unwrap()
 });
 
+
+
 pub static RPC_PROCESS_DURATION: Lazy<HistogramVec> = Lazy::new(|| {
     register_histogram_vec!(
         "aptos_consensus_dag_rpc_process_duration",
         "dag incoming message processing",
         &["step"],
+        LOW_LATENCY_BUCKETS.to_vec(),
     )
     .unwrap()
 });
