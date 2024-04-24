7c9dec422b2378ba6b4cb9ed947ea0a4d3692e0e
Brian Cho <brian@aptoslabs.com>
2024-04-24 16:40:44 -0700

    run land-blocking

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -605,7 +605,7 @@ fn get_land_blocking_test(
 ) -> Option<ForgeConfig> {
     let test = match test_name {
         "land_blocking" | "realistic_env_max_load" => {
-            pfn_performance(duration, true, true, true, 7, 7, true)
+            realistic_env_max_load_test(duration, test_cmd, 7, 5)
         },
         "compat" => compat(),
         "framework_upgrade" => framework_upgrade(),
