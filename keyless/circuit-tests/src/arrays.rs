874fae2203ea8aa5b1fa98464cc8352e2e53c56a
Michael Straka <mstraka100@gmail.com>
2024-04-25 18:23:31 -0500

    more tests

--- keyless/circuit-tests/src/arrays.rs
+++ keyless/circuit-tests/src/arrays.rs
@@ -632,28 +632,6 @@ fn check_substr_inclusion_poly_small_boolean_test() {
     assert!(result.is_ok());
 }
 
-/*#[test]
-fn check_substr_inclusion_poly_boolean_test() {
-    let circuit_handle = TestCircuitHandle::new("check_substr_inclusion_poly_boolean_test.circom").unwrap();
-
-    let max_str_len = 100;
-    let max_substr_len = 20;
-    let config = CircuitPaddingConfig::new().max_length("str", max_str_len).max_length("substr", max_substr_len);
-    let string = "Hello World!";
-    let string_hash = poseidon_bn254::pad_and_hash_string(&string, max_str_len).unwrap();
-    let substring = "lo Wor";
-    let substring_len = substring.len();
-    let start_index = 3;
-    let expected_output = 1;
-
-
-    let circuit_input_signals = CircuitInputSignals::new().str_input("str", string).str_input("substr", substring).u64_input("substr_len", substring_len as u64).u64_input("start_index", start_index).fr_input("str_hash", string_hash).u64_input("expected_output", expected_output).pad(&config).unwrap();
-
-
-    let result = circuit_handle.gen_witness(circuit_input_signals);
-    assert!(result.is_ok());
-}*/
-
 #[test]
 fn concatenation_check_test() {
     let circuit_handle = TestCircuitHandle::new("concatenation_check_test.circom").unwrap();
@@ -663,15 +641,129 @@ fn concatenation_check_test() {
     let max_right_str_len = 70;
     let config = CircuitPaddingConfig::new().max_length("full_string", max_full_str_len).max_length("left", max_left_str_len).max_length("right", max_right_str_len);
     let full_string = "Hello World!";
-    let left_string = "Hello ";
-    let right_string = "World!";
-    let left_len = left_string.len();
+    let str_len = full_string.len();
+    // Subcircuit fails if the left string is empty
+    for sep_index in 1..str_len {
+        let left_string = &full_string[0..sep_index];
+        let right_string = &full_string[sep_index..full_string.len()];
+        let left_len = left_string.len();
+        let right_len = right_string.len();
+        let circuit_input_signals = CircuitInputSignals::new().str_input("full_string", full_string).str_input("left", left_string).str_input("right", right_string).u64_input("left_len", left_len as u64).u64_input("right_len", right_len as u64).pad(&config).unwrap();
+
+        let result = circuit_handle.gen_witness(circuit_input_signals);
+        assert!(result.is_ok());
+    }
+}
+
+#[test]
+#[should_panic]
+fn concatenation_check_left_len_wrong_test() {
+    let circuit_handle = TestCircuitHandle::new("concatenation_check_test.circom").unwrap();
+
+    let max_full_str_len = 100;
+    let max_left_str_len = 70;
+    let max_right_str_len = 70;
+    let config = CircuitPaddingConfig::new().max_length("full_string", max_full_str_len).max_length("left", max_left_str_len).max_length("right", max_right_str_len);
+    let full_string = "Hello World!";
+    let str_len = full_string.len();
+    let sep_index = 3;
+    let left_string = &full_string[0..sep_index];
+    let right_string = &full_string[sep_index..str_len];
+    let left_len = 72;
     let right_len = right_string.len();
-
-
     let circuit_input_signals = CircuitInputSignals::new().str_input("full_string", full_string).str_input("left", left_string).str_input("right", right_string).u64_input("left_len", left_len as u64).u64_input("right_len", right_len as u64).pad(&config).unwrap();
 
-     
+    let result = circuit_handle.gen_witness(circuit_input_signals);
+    assert!(result.is_ok());
+}
+
+#[test]
+#[should_panic]
+fn concatenation_check_left_string_wrong_test() {
+    let circuit_handle = TestCircuitHandle::new("concatenation_check_test.circom").unwrap();
+
+    let max_full_str_len = 100;
+    let max_left_str_len = 70;
+    let max_right_str_len = 70;
+    let config = CircuitPaddingConfig::new().max_length("full_string", max_full_str_len).max_length("left", max_left_str_len).max_length("right", max_right_str_len);
+    let full_string = "Hello World!";
+    let str_len = full_string.len();
+    let sep_index = 3;
+    let left_string = &full_string[0..sep_index-1];
+    let right_string = &full_string[sep_index..str_len];
+    let left_len = left_string.len();
+    let right_len = right_string.len();
+    let circuit_input_signals = CircuitInputSignals::new().str_input("full_string", full_string).str_input("left", left_string).str_input("right", right_string).u64_input("left_len", left_len as u64).u64_input("right_len", right_len as u64).pad(&config).unwrap();
+
+    let result = circuit_handle.gen_witness(circuit_input_signals);
+    assert!(result.is_ok());
+}
+
+#[test]
+#[should_panic]
+fn concatenation_check_right_string_wrong_test() {
+    let circuit_handle = TestCircuitHandle::new("concatenation_check_test.circom").unwrap();
+
+    let max_full_str_len = 100;
+    let max_left_str_len = 70;
+    let max_right_str_len = 70;
+    let config = CircuitPaddingConfig::new().max_length("full_string", max_full_str_len).max_length("left", max_left_str_len).max_length("right", max_right_str_len);
+    let full_string = "Hello World!";
+    let str_len = full_string.len();
+    // Subcircuit fails if the left string is empty
+    let sep_index = 3;
+    let left_string = &full_string[0..sep_index];
+    let right_string = &full_string[sep_index..str_len-1];
+    let left_len = left_string.len();
+    let right_len = right_string.len();
+    let circuit_input_signals = CircuitInputSignals::new().str_input("full_string", full_string).str_input("left", left_string).str_input("right", right_string).u64_input("left_len", left_len as u64).u64_input("right_len", right_len as u64).pad(&config).unwrap();
+
+    let result = circuit_handle.gen_witness(circuit_input_signals);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn concatenation_check_small_test() {
+    let circuit_handle = TestCircuitHandle::new("concatenation_check_small_test.circom").unwrap();
+
+    let max_full_str_len = 2;
+    let max_left_str_len = 1;
+    let max_right_str_len = 1;
+    let config = CircuitPaddingConfig::new().max_length("full_string", max_full_str_len).max_length("left", max_left_str_len).max_length("right", max_right_str_len);
+    let full_string = "ab";
+    let str_len = full_string.len();
+    let sep_index = 1;
+    let left_string = &full_string[0..sep_index];
+    let right_string = &full_string[sep_index..str_len];
+    let left_len = left_string.len();
+    let right_len = right_string.len();
+    let circuit_input_signals = CircuitInputSignals::new().str_input("full_string", full_string).str_input("left", left_string).str_input("right", right_string).u64_input("left_len", left_len as u64).u64_input("right_len", right_len as u64).pad(&config).unwrap();
+
+    let result = circuit_handle.gen_witness(circuit_input_signals);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn concatenation_check_large_test() {
+    let circuit_handle = TestCircuitHandle::new("concatenation_check_large_test.circom").unwrap();
+
+    let max_full_str_len = 1600;
+    let max_left_str_len = 1000;
+    let max_right_str_len = 1000;
+    let config = CircuitPaddingConfig::new().max_length("full_string", max_full_str_len).max_length("left", max_left_str_len).max_length("right", max_right_str_len);
+    let full_string = "Once upon a midnight dreary, while I pondered, weak and weary,
+    Over many a quaint and curious volume of forgotten lore—
+    While I nodded, nearly napping, suddenly there came a tapping,
+    As of some one gently rapping, rapping at my chamber door.
+    “’Tis some visitor,” I muttered, “tapping at my chamber door—";
+    let str_len = full_string.len();
+    let sep_index = 31;
+    let left_string = &full_string[0..sep_index];
+    let right_string = &full_string[sep_index..str_len];
+    let left_len = left_string.len();
+    let right_len = right_string.len();
+    let circuit_input_signals = CircuitInputSignals::new().str_input("full_string", full_string).str_input("left", left_string).str_input("right", right_string).u64_input("left_len", left_len as u64).u64_input("right_len", right_len as u64).pad(&config).unwrap();
+
     let result = circuit_handle.gen_witness(circuit_input_signals);
     assert!(result.is_ok());
 }
@@ -684,6 +776,45 @@ fn check_are_ascii_digits_test() {
     let len = 5;
     let config = CircuitPaddingConfig::new().max_length("in", max_input_len);
     let circuit_input_signals = CircuitInputSignals::new().u64_input("len", len).bytes_input("in", &input_arr).pad(&config).unwrap();
+     
+    let result = circuit_handle.gen_witness(circuit_input_signals);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn check_are_ascii_digits_max_len_test() {
+    let circuit_handle = TestCircuitHandle::new("check_are_ascii_digits_max_len_test.circom").unwrap();
+    let max_input_len = 8;
+    let input_arr = [48,49,50,52,55,48,50,54];
+    let len = input_arr.len();
+    let config = CircuitPaddingConfig::new().max_length("in", max_input_len);
+    let circuit_input_signals = CircuitInputSignals::new().u64_input("len", len as u64).bytes_input("in", &input_arr).pad(&config).unwrap();
+     
+    let result = circuit_handle.gen_witness(circuit_input_signals);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn check_are_ascii_digits_small_test() {
+    let circuit_handle = TestCircuitHandle::new("check_are_ascii_digits_test.circom").unwrap();
+    let max_input_len = 20;
+    let input_arr = [51];
+    let len = 1;
+    let config = CircuitPaddingConfig::new().max_length("in", max_input_len);
+    let circuit_input_signals = CircuitInputSignals::new().u64_input("len", len).bytes_input("in", &input_arr).pad(&config).unwrap();
+     
+    let result = circuit_handle.gen_witness(circuit_input_signals);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn check_are_ascii_digits_large_test() {
+    let circuit_handle = TestCircuitHandle::new("check_are_ascii_digits_large_test.circom").unwrap();
+    let max_input_len = 2000;
+    let input_arr = [48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,51,52,53,54,55,56,57,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,51,52,53,54,55,56,57,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,51,52,53,54,55,56,57,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,51,52,53,54,55,56,57,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,52,55,48,49,50,51,52,53,54,55,56,57];
+    let len = input_arr.len();
+    let config = CircuitPaddingConfig::new().max_length("in", max_input_len);
+    let circuit_input_signals = CircuitInputSignals::new().u64_input("len", len as u64).bytes_input("in", &input_arr).pad(&config).unwrap();
      
     let result = circuit_handle.gen_witness(circuit_input_signals);
     assert!(result.is_ok());
