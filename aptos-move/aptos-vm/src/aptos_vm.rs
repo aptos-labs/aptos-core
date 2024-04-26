fd9bf1dc3159bf1cdd0c807c2578c6cd4d60cb38
zhoujun.ma <zhoujun@aptoslabs.com>
2024-04-25 19:48:51 -0700

    debug

--- aptos-move/aptos-vm/src/aptos_vm.rs
+++ aptos-move/aptos-vm/src/aptos_vm.rs
@@ -2429,7 +2429,7 @@ impl AptosVM {
             TransactionPayload::EntryFunction(entry_func) => {
                 if let Some(gas_amount) = self.randomness_api_v0_required_deposit.gas_amount {
                     if has_randomness_attribute(resolver, session, entry_func).unwrap_or(false) {
-                        if gas_amount < u64::from(txn_metadata.max_gas_amount) {
+                        if gas_amount > u64::from(txn_metadata.max_gas_amount) {
                             return Err(VMStatus::error(
                                 StatusCode::REQUIRED_DEPOSIT_INCONSISTENT_WITH_TXN_MAX_GAS,
                                 None,
