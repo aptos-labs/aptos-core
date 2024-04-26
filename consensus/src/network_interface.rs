a8a84d7da05d3240d3fede7394347b68930ef3b4
Satya Vusirikala <satyasatya123456@gmail.com>
2024-04-26 15:07:14 -0700

    Removing compressed from protocolids

--- consensus/src/network_interface.rs
+++ consensus/src/network_interface.rs
@@ -132,7 +132,6 @@ pub const RPC: &[ProtocolId] = &[
 
 /// Supported protocols in preferred order (from highest priority to lowest).
 pub const DIRECT_SEND: &[ProtocolId] = &[
-    ProtocolId::ConsensusDirectSendCompressed,
     ProtocolId::ConsensusDirectSendBcs,
     ProtocolId::ConsensusDirectSendJson,
 ];
