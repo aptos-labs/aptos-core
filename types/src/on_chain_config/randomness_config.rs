edac9b44df27a72f38c5fdff73c6ff27c93d44cf
zhoujun.ma <zhoujun@aptoslabs.com>
2024-04-26 11:34:38 -0700

    debug

--- types/src/on_chain_config/randomness_config.rs
+++ types/src/on_chain_config/randomness_config.rs
@@ -151,6 +151,7 @@ impl OnChainRandomnessConfig {
     }
 
     pub fn randomness_enabled(&self) -> bool {
+        println!("hah");
         match self {
             OnChainRandomnessConfig::Off => false,
             OnChainRandomnessConfig::V1(_) => true,
