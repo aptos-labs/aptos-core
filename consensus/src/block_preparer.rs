ffe50e640316e8306e6a8738af2a225fe5fc013f
Balaji Arun <balaji@aptoslabs.com>
2024-05-03 11:31:51 -0700

    simplify prepare block

--- consensus/src/block_preparer.rs
+++ consensus/src/block_preparer.rs
@@ -36,30 +36,31 @@ impl BlockPreparer {
     }
 
     pub async fn prepare_block(&self, block: &Block) -> ExecutorResult<Vec<SignedTransaction>> {
-        let (txns, max_txns_from_block_to_execute) =
+        let (txns, _max_txns_from_block_to_execute) =
             self.payload_manager.get_transactions(block).await?;
-        let txn_filter = self.txn_filter.clone();
-        let txn_deduper = self.txn_deduper.clone();
-        let txn_shuffler = self.txn_shuffler.clone();
-        let block_id = block.id();
-        let block_timestamp_usecs = block.timestamp_usecs();
-        // Transaction filtering, deduplication and shuffling are CPU intensive tasks, so we run them in a blocking task.
-        tokio::task::spawn_blocking(move || {
-            let filtered_txns = txn_filter.filter(block_id, block_timestamp_usecs, txns);
-            let deduped_txns = txn_deduper.dedup(filtered_txns);
-            let mut shuffled_txns = {
-                let _timer = TXN_SHUFFLE_SECONDS.start_timer();
+        // let txn_filter = self.txn_filter.clone();
+        // let txn_deduper = self.txn_deduper.clone();
+        // let txn_shuffler = self.txn_shuffler.clone();
+        // let block_id = block.id();
+        // let block_timestamp_usecs = block.timestamp_usecs();
+        // // Transaction filtering, deduplication and shuffling are CPU intensive tasks, so we run them in a blocking task.
+        // tokio::task::spawn_blocking(move || {
+        //     let filtered_txns = txn_filter.filter(block_id, block_timestamp_usecs, txns);
+        //     let deduped_txns = txn_deduper.dedup(filtered_txns);
+        //     let mut shuffled_txns = {
+        //         let _timer = TXN_SHUFFLE_SECONDS.start_timer();
 
-                txn_shuffler.shuffle(deduped_txns)
-            };
+        //         txn_shuffler.shuffle(deduped_txns)
+        //     };
 
-            if let Some(max_txns_from_block_to_execute) = max_txns_from_block_to_execute {
-                shuffled_txns.truncate(max_txns_from_block_to_execute);
-            }
-            MAX_TXNS_FROM_BLOCK_TO_EXECUTE.observe(shuffled_txns.len() as f64);
-            Ok(shuffled_txns)
-        })
-        .await
-        .expect("Failed to spawn blocking task for transaction generation")
+        //     if let Some(max_txns_from_block_to_execute) = max_txns_from_block_to_execute {
+        //         shuffled_txns.truncate(max_txns_from_block_to_execute);
+        //     }
+        //     MAX_TXNS_FROM_BLOCK_TO_EXECUTE.observe(shuffled_txns.len() as f64);
+        //     Ok(shuffled_txns)
+        // })
+        // .await
+        // .expect("Failed to spawn blocking task for transaction generation")
+        Ok(txns)
     }
 }
