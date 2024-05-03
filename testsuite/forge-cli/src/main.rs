3fbf1ec2dec5296e3b92e1a3bc16f6cc4482a39a
Brian Cho <brian@aptoslabs.com>
2024-05-03 13:05:15 -0700

    workload sweep

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -604,7 +604,7 @@ fn get_land_blocking_test(
     test_cmd: &TestCommand,
 ) -> Option<ForgeConfig> {
     let test = match test_name {
-        "land_blocking" | "realistic_env_max_load" => realistic_env_graceful_overload(),
+        "land_blocking" | "realistic_env_max_load" => realistic_env_workload_sweep_test(),
         "compat" => compat(),
         "framework_upgrade" => framework_upgrade(),
         _ => return None, // The test name does not match a land-blocking test
