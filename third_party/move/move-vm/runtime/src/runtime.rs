a2a26b68203472ffff328d774b1173c4b3c70447
George Mitenkov <georgemitenk0v@gmail.com>
2024-05-15 23:48:39 +0100

    fix format

--- third_party/move/move-vm/runtime/src/runtime.rs
+++ third_party/move/move-vm/runtime/src/runtime.rs
@@ -14,7 +14,6 @@ use crate::{
 };
 use move_binary_format::{
     access::ModuleAccess,
-    binary_views::BinaryIndexedView,
     compatibility::Compatibility,
     errors::{verification_error, Location, PartialVMError, PartialVMResult, VMResult},
     normalized, CompiledModule, IndexKind,
