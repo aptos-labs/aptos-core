b143626538c532d701e290dab19cf53433a7a03f
Igor <igor-aptos@users.noreply.github.com>
2024-05-17 10:30:58 -0700

    [not for review] test landed apt fa speed after migration complete

--- crates/transaction-generator-lib/src/p2p_transaction_generator.rs
+++ crates/transaction-generator-lib/src/p2p_transaction_generator.rs
@@ -177,9 +177,9 @@ impl P2PTransactionGenerator {
         num_coins: u64,
         txn_factory: &TransactionFactory,
     ) -> SignedTransaction {
-        from.sign_with_transaction_builder(
-            txn_factory.payload(aptos_stdlib::aptos_coin_transfer(*to, num_coins)),
-        )
+        from.sign_with_transaction_builder(txn_factory.payload(
+            aptos_stdlib::apt_primary_fungible_store_transfer(*to, num_coins),
+        ))
     }
 
     fn generate_invalid_transaction(
