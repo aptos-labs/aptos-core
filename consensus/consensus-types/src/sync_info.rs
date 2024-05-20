b8beeec675b3ce0e8396416c97b9f645670520f5
Alden Hu <msmouse@gmail.com>
2024-05-20 10:12:21 -0700

    [cherry-pick][1.13] schemadb: make iterator lazy #13341 (#13351)
    * schemadb: make iterator lazy
    
    Meaning, do not advance underlying raw iter early
    Sometimes the second advance on the state value CF is exetremely costly
    due to deletions by the pruner.
    
    * [consensus] add missing check for commit cert

--- consensus/consensus-types/src/sync_info.rs
+++ consensus/consensus-types/src/sync_info.rs
@@ -156,6 +156,11 @@ impl SyncInfo {
             "HLI has empty commit info"
         );
 
+        ensure!(
+            !self.highest_commit_cert.commit_info().is_ordered_only(),
+            "HLI has ordered only commit info"
+        );
+
         self.highest_quorum_cert
             .verify(validator)
             .and_then(|_| {
