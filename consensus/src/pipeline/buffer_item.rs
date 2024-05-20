ab7e993eb93b49529eb2618a11a44d11a6ea1154
Balaji Arun <balaji@aptoslabs.com>
2024-05-19 18:23:39 -0700

    joltean failures

--- consensus/src/pipeline/buffer_item.rs
+++ consensus/src/pipeline/buffer_item.rs
@@ -328,7 +328,10 @@ impl BufferItem {
     pub fn try_advance_to_aggregated(self, validator: &ValidatorVerifier) -> Self {
         match self {
             Self::Signed(signed_item) => {
-                if signed_item.partial_commit_proof.signatures().len() == validator.len() {
+                if validator
+                    .check_voting_power(signed_item.partial_commit_proof.signatures().keys(), true)
+                    .is_ok()
+                {
                     Self::Aggregated(Box::new(AggregatedItem {
                         executed_blocks: signed_item.executed_blocks,
                         commit_proof: aggregate_commit_proof(
