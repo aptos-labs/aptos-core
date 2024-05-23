a061e5b2e433e6ff5789f8ea76abc9d3e225293c
Oliver He <heliuchuan@gmail.com>
2024-05-23 03:17:56 -0400

    Refactor the test JWKs into JSON and pem files. (#13082)
    * Refactor the test JWKs into JSON and pem files.
    
    * use include_str instead of read file
    
    * lint
    
    * fix type

--- testsuite/smoke-test/src/jwks/jwk_consensus_provider_change_mind.rs
+++ testsuite/smoke-test/src/jwks/jwk_consensus_provider_change_mind.rs
@@ -15,10 +15,7 @@ use aptos_forge::{NodeExt, Swarm, SwarmExt};
 use aptos_logger::{debug, info};
 use aptos_types::{
     jwks::{
-        jwk::{JWKMoveStruct, JWK},
-        rsa::RSA_JWK,
-        unsupported::UnsupportedJWK,
-        AllProvidersJWKs, ProviderJWKs,
+        jwk::JWK, secure_test_rsa_jwk, unsupported::UnsupportedJWK, AllProvidersJWKs, ProviderJWKs,
     },
     keyless::test_utils::get_sample_iss,
     on_chain_config::{JWKConsensusConfigV1, OIDCProvider, OnChainJWKConsensusConfig},
@@ -112,7 +109,7 @@ async fn jwk_consensus_provider_change_mind() {
                 ProviderJWKs {
                     issuer: get_sample_iss().into_bytes(),
                     version: 0,
-                    jwks: vec![JWKMoveStruct::from(RSA_JWK::secure_test_jwk())],
+                    jwks: vec![secure_test_rsa_jwk().into()],
                 },
             ]
         },
