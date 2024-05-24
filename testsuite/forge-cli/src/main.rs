d9bf0469289a4b5c751b0ec2f00d1a731a92d952
Brian Cho <brian@aptoslabs.com>
2024-05-24 16:02:36 -0700

    run pfn test on PR

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -605,7 +605,7 @@ fn get_land_blocking_test(
 ) -> Option<ForgeConfig> {
     let test = match test_name {
         "land_blocking" | "realistic_env_max_load" => {
-            realistic_env_max_load_test(duration, test_cmd, 7, 5)
+            pfn_performance(duration, true, true, false, 7, 1, false)
         },
         "compat" => compat(),
         "framework_upgrade" => framework_upgrade(),
