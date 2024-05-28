feaa846fffd35742eabe2800b52a09162ea32e43
Satya Vusirikala <satyasatya123456@gmail.com>
2024-05-28 11:06:15 -0700

    Reverting forge params

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1048,7 +1048,7 @@ fn realistic_env_sweep_wrap(
 }
 
 fn realistic_env_load_sweep_test() -> ForgeConfig {
-    realistic_env_sweep_wrap(100, 10, LoadVsPerfBenchmark {
+    realistic_env_sweep_wrap(20, 10, LoadVsPerfBenchmark {
         test: Box::new(PerformanceBenchmark),
         workloads: Workloads::TPS(vec![10, 100, 1000, 3000, 5000]),
         criteria: [
