6b2424730bcbbc68303f23999222cd81da4b25e9
Zijie Zhao <zijie.zhao@aptoslabs.com>
2024-05-31 16:37:07 -0700

    [move-smith] add transactional target

---
+++ third_party/move/tools/move-smith/fuzz/fuzz_targets/transactional.rs
@@ -0,0 +1,19 @@
+// Copyright (c) Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_main]
+
+use arbitrary::Unstructured;
+use libfuzzer_sys::fuzz_target;
+use move_smith::{utils::run_transactional_test, CodeGenerator, MoveSmith};
+
+fuzz_target!(|data: &[u8]| {
+    let u = &mut Unstructured::new(data);
+    let mut smith = MoveSmith::default();
+    match smith.generate(u) {
+        Ok(()) => (),
+        Err(_) => return,
+    };
+    let code = smith.get_compile_unit().emit_code();
+    run_transactional_test(code).unwrap();
+});
