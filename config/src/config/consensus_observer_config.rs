0b4dd6a6c542eb6b973edc0b2075c8975a81c750
Josh Lind <josh.lind@hotmail.com>
2024-06-03 00:04:45 -0400

    [Consensus Observer] Add config optimizer support for CO.

--- config/src/config/consensus_observer_config.rs
+++ config/src/config/consensus_observer_config.rs
@@ -1,7 +1,12 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::config::{
+    config_optimizer::ConfigOptimizer, node_config_loader::NodeType, Error, NodeConfig,
+};
+use aptos_types::chain_id::ChainId;
 use serde::{Deserialize, Serialize};
+use serde_yaml::Value;
 
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
 #[serde(default, deny_unknown_fields)]
@@ -28,5 +33,18 @@ impl ConsensusObserverConfig {
     /// Returns true iff the observer or publisher is enabled
     pub fn is_enabled(&self) -> bool {
         self.observer_enabled || self.publisher_enabled
+    }
+}
+
+impl ConfigOptimizer for ConsensusObserverConfig {
+    fn optimize(
+        _node_config: &mut NodeConfig,
+        _local_config_yaml: &Value,
+        _node_type: NodeType,
+        _chain_id: Option<ChainId>,
+    ) -> Result<bool, Error> {
+        // TODO: use me to enable consensus observer for validators and VFNs
+        // in controlled environments, e.g., devnet.
+        Ok(false)
     }
 }
