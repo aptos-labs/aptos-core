d63ac210f24307960f4e8ab0419be65a98997620
Zijie Zhao <zijie.zhao@aptoslabs.com>
2024-06-04 11:59:47 -0700

    [move-smith] add comments

--- third_party/move/tools/move-smith/src/ast.rs
+++ third_party/move/tools/move-smith/src/ast.rs
@@ -1,16 +1,25 @@
 // Copyright (c) Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
+//! An abstract syntax tree for the Move language used by the MoveSmith fuzzer.
+//! The AST is taken mostly from `third_party/move/move-compiler/src/parser/ast.rs`.
+//! Ideally when the fuzzer becomes more mature, this AST will converge to the
+//! parser's AST and we might be able to reuse the parser's AST directly.
+
 use crate::{names::Identifier, types::Type};
 use arbitrary::Arbitrary;
 use num_bigint::BigUint;
 
+/// The collection of modules and scripts that make up a Move program.
+/// This is the final output of the MoveSmith fuzzer.
+/// This should be runnable as a transactional test.
 #[derive(Debug, Clone)]
 pub struct CompileUnit {
     pub modules: Vec<Module>,
     pub scripts: Vec<Script>,
 }
 
+/// A Move module.
 #[derive(Debug, Clone)]
 pub struct Module {
     // pub attributes: Vec<Attributes>,
@@ -25,12 +34,18 @@ pub struct Module {
     // pub specs: Vec<SpecBlock>,
 }
 
+/// A simplified Move Script.
+/// The script only contains a `main` function.
+/// The `main` function only consists of a sequence of function calls.
 #[derive(Debug, Clone)]
 pub struct Script {
     // pub uses: Vec<UseDecl>,
     pub main: Vec<FunctionCall>,
 }
 
+/// A function definition.
+/// The return statement is separated from the body to simplify verifying the
+/// generated function has a valid return.
 #[derive(Debug, Clone)]
 pub struct Function {
     // pub attributes: Vec<Attributes>,
@@ -46,11 +61,14 @@ pub struct Function {
     pub return_stmt: Option<Expression>,
 }
 
+/// The Visibility
 #[derive(Debug, Clone)]
 pub struct Visibility {
+    // TODO: add friend
     pub public: bool,
 }
 
+/// A function signature.
 #[derive(Debug, Clone)]
 pub struct FunctionSignature {
     // pub type_parameters: Vec<(Name, Vec<Ability>)>,
@@ -58,11 +76,15 @@ pub struct FunctionSignature {
     pub return_type: Option<Type>,
 }
 
+/// The body of a function excluding the return statement.
 #[derive(Debug, Clone)]
 pub struct FunctionBody {
     pub stmts: Vec<Statement>,
 }
 
+/// The definition of a struct.
+/// Cyclic data is not allowed.
+/// Struct used in fields must have the all the abilities of the parent struct.
 #[derive(Debug, Clone)]
 pub struct StructDefinition {
     pub name: Identifier,
@@ -72,6 +94,8 @@ pub struct StructDefinition {
     pub fields: Vec<(Identifier, Type)>,
 }
 
+/// Abilities of a struct.
+/// Key requires storage.
 #[derive(Debug, Clone, PartialEq, Eq, Arbitrary)]
 pub enum Ability {
     Copy,
@@ -80,6 +104,7 @@ pub enum Ability {
     Key,
 }
 
+/// A statement in a function body.
 #[derive(Debug, Clone)]
 pub enum Statement {
     // If(If),
@@ -92,7 +117,11 @@ pub enum Statement {
     Expr(Expression),
 }
 
+/// Declare a new variable.
+/// Optionally initialize the variable with an expression.
+/// Currently type annotations will always be generated.
 // TODO: Support multiple declarations in a single statement
+// TODO: Randomly ignore type annotation
 #[derive(Debug, Clone)]
 pub struct Declaration {
     pub typ: Type,
@@ -100,6 +129,7 @@ pub struct Declaration {
     pub value: Option<Expression>,
 }
 
+/// An expression.
 #[derive(Debug, Clone)]
 pub enum Expression {
     NumberLiteral(NumberLiteral),
@@ -109,18 +139,23 @@ pub enum Expression {
     StructInitialization(StructInitialization),
 }
 
+/// A number literal.
+/// Currently the number literal will always have the type suffix.
 #[derive(Debug, Clone)]
 pub struct NumberLiteral {
     pub value: BigUint,
     pub typ: Type,
 }
 
+/// An inline struct initialization.
 #[derive(Debug, Clone)]
 pub struct StructInitialization {
     pub name: Identifier,
     pub fields: Vec<(Identifier, Expression)>,
 }
 
+/// A function call.
+/// Currently the generated doesn't allow the argument to be another function call.
 #[derive(Debug, Clone)]
 pub struct FunctionCall {
     pub name: Identifier,
