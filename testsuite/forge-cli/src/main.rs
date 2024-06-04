5ed6509f36c0d3c2df557b9733beaadd6789f26b
Brian Cho <brian@aptoslabs.com>
2024-06-04 16:54:42 -0700

    reduce mempool backlog as haproxy is slower

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1979,13 +1979,18 @@ fn realistic_env_max_load_test(
     }
 
     // Create the test
+    let memory_backlog = if ha_proxy {
+        30000
+    } else {
+        40000
+    };
     ForgeConfig::default()
         .with_initial_validator_count(NonZeroUsize::new(num_validators).unwrap())
         .with_initial_fullnode_count(num_fullnodes)
         .add_network_test(wrap_with_realistic_env(TwoTrafficsTest {
             inner_traffic: EmitJobRequest::default()
                 .mode(EmitJobMode::MaxLoad {
-                    mempool_backlog: 40000,
+                    mempool_backlog: memory_backlog,
                 })
                 .init_gas_price_multiplier(20),
             inner_success_criteria: SuccessCriteria::new(
