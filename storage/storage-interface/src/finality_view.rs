bf51211444855e7d42de27212d1e712a02bba3ab
Mikhail Zabaluev <mikhail.zabaluev@movementlabs.xyz>
2024-06-07 15:19:07 +0200

    Revert "storage: improve FinalityView set method"
    This reverts commit 1731cce36e3810d4034240deeefffd3745f8dedc.

--- storage/storage-interface/src/finality_view.rs
+++ storage/storage-interface/src/finality_view.rs
@@ -1,6 +1,12 @@
 use std::sync::RwLock;
 
-use aptos_types::{ledger_info::LedgerInfoWithSignatures, transaction::Version};
+use aptos_crypto::HashValue;
+use aptos_types::{
+    aggregate_signature::AggregateSignature,
+    block_info::BlockInfo,
+    ledger_info::{LedgerInfo, LedgerInfoWithSignatures},
+    transaction::Version,
+};
 
 use crate::{AptosDbError, DbReader, Result};
 
@@ -21,15 +27,27 @@ impl<Db> FinalityView<Db> {
 }
 
 impl<Db: DbReader> FinalityView<Db> {
-    /// Updates the view for the latest finalized block at the specified height.
+    /// Updates the information on the latest finalized block at the specified height.
     pub fn set_finalized_block_height(&self, height: u64) -> Result<()> {
-        let (start_ver, _, _) = self.reader.get_block_info_by_height(height)?;
-        let ledger_info = match self.reader.get_epoch_ending_ledger_info(start_ver) {
-            Ok(li) => li,
-            Err(AptosDbError::NotFound(_)) => self.reader.get_latest_ledger_info()?,
-            Err(e) => return Err(e),
-        };
-
+        let (_start_ver, end_ver, block_event) = self.reader.get_block_info_by_height(height)?;
+        let block_hash = block_event.hash()?;
+        let block_info = BlockInfo::new(
+            block_event.epoch(),
+            block_event.round(),
+            block_hash,
+            self.reader.get_accumulator_root_hash(end_ver)?,
+            end_ver,
+            block_event.proposed_time(),
+            None,
+        );
+        // FinalityView is created for Movement, where we don't use the consensus hash
+        // or the ledger info signatures. So we leave them empty here and can still construct
+        // a valid ledger info for the view.
+        // In a more general implementation, this API could accept LedgerInfoWithSignatures
+        // which is either preserved from an earlier version, or fudged like in our case.
+        let ledger_info = LedgerInfo::new(block_info, HashValue::zero());
+        let aggregate_signature = AggregateSignature::empty();
+        let ledger_info = LedgerInfoWithSignatures::new(ledger_info, aggregate_signature);
         let mut fin_legder_info = self.finalized_ledger_info.write().unwrap();
         *fin_legder_info = Some(ledger_info);
         Ok(())
@@ -77,6 +95,9 @@ mod tests {
 
     #[test]
     fn test_get_latest_ledger_info() -> anyhow::Result<()> {
+        // If the mock is changed to be stateful, this should be ref-counted
+        // and shared with the view.
+        let mock = MockDbReaderWriter;
         let view = FinalityView::new(MockDbReaderWriter);
 
         let ledger_info = view.get_latest_ledger_info_option()?;
@@ -86,13 +107,28 @@ mod tests {
         // Set the finalized ledger info
         view.set_finalized_block_height(blockheight)?;
 
+        // Capture the block event once
+        let (_start_ver, end_ver, block_event) =
+            mock.get_block_info_by_height(blockheight)?;
+        let block_hash = block_event.hash()?; // Used to verify hash is generated
+
+        let block_info = BlockInfo::new(
+            block_event.epoch(),
+            block_event.round(),
+            block_hash,
+            HashValue::zero(),
+            end_ver,
+            block_event.proposed_time(),
+            None,
+        );
+        let ledger_info = LedgerInfo::new(block_info, HashValue::zero());
+        let expected_ledger_info =
+            LedgerInfoWithSignatures::new(ledger_info, AggregateSignature::empty());
+
         // Get the latest ledger info after setting it
         let ledger_info = view.get_latest_ledger_info_option()?.unwrap();
 
-        assert_eq!(
-            ledger_info.ledger_info().commit_info().id(),
-            HashValue::new([1; HashValue::LENGTH]),
-        );
+        assert_eq!(ledger_info, expected_ledger_info);
 
         Ok(())
     }
