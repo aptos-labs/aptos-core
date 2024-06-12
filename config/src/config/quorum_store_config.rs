042c47ee5226f214c6f49ce6070b08a88c120b3e
Satya Vusirikala <satyasatya123456@gmail.com>
2024-06-12 13:44:14 -0700

    Increasing expiry time to 4sec

--- config/src/config/quorum_store_config.rs
+++ config/src/config/quorum_store_config.rs
@@ -123,7 +123,7 @@ impl Default for QuorumStoreConfig {
             batch_request_retry_interval_ms: 1000,
             batch_request_rpc_timeout_ms: 5000,
             batch_expiry_gap_when_init_usecs: Duration::from_secs(60).as_micros() as u64,
-            remote_batch_expiry_gap_when_init_usecs: Duration::from_millis(500).as_micros() as u64,
+            remote_batch_expiry_gap_when_init_usecs: Duration::from_millis(4000).as_micros() as u64,
             memory_quota: 120_000_000,
             db_quota: 300_000_000,
             batch_quota: 300_000,
