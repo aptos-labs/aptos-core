29951a933dc609572b40e49fbdace8500031a298
Satya Vusirikala <satyasatya123456@gmail.com>
2024-06-13 10:35:18 -0700

    Add increase fraction

--- consensus/src/quorum_store/batch_generator.rs
+++ consensus/src/quorum_store/batch_generator.rs
@@ -399,9 +399,7 @@ impl BatchGenerator {
             Duration::from_millis(self.config.back_pressure.increase_duration_ms);
         let mut back_pressure_decrease_latest = start;
         let mut back_pressure_increase_latest = start;
-        let mut dynamic_pull_txn_per_s = (self.config.back_pressure.dynamic_min_txn_per_s
-            + self.config.back_pressure.dynamic_max_txn_per_s)
-            / 2;
+        let mut dynamic_pull_txn_per_s = self.config.back_pressure.dynamic_max_txn_per_s;
 
         loop {
             let _timer = counters::BATCH_GENERATOR_MAIN_LOOP.start_timer();
@@ -431,7 +429,7 @@ impl BatchGenerator {
                         if back_pressure_increase_latest.elapsed() >= back_pressure_increase_duration {
                             back_pressure_increase_latest = tick_start;
                             dynamic_pull_txn_per_s = std::cmp::min(
-                                dynamic_pull_txn_per_s + self.config.back_pressure.dynamic_min_txn_per_s,
+                                (dynamic_pull_txn_per_s as f64 * self.config.back_pressure.increase_fraction) as u64,
                                 self.config.back_pressure.dynamic_max_txn_per_s,
                             );
                             trace!("QS: dynamic_max_pull_txn_per_s: {}", dynamic_pull_txn_per_s);
