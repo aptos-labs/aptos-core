1dfd730bfa68036d08aa861ee37c7c69c3799d77
Larry Liu <larry@aptoslabs.com>
2024-06-14 16:21:40 -0700

    remove extra header.

--- keyless/common/src/input_processing/bits.rs
+++ keyless/common/src/input_processing/bits.rs
@@ -1,8 +1,6 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-// Copyright © Aptos Foundation
-
 use anyhow::{bail, Result};
 use std::ops::{self, Add, AddAssign};
 
--- keyless/common/src/input_processing/sha.rs
+++ keyless/common/src/input_processing/sha.rs
@@ -1,8 +1,6 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-// Copyright © Aptos Foundation
-
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
