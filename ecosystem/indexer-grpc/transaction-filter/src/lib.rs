cae03afabd7acf5acfe9597222b3882609340407
CapCap <max@aptoslabs.com>
2024-06-14 16:45:00 -0700

    [GRPC] Simple Transaction Filtering

---
+++ ecosystem/indexer-grpc/transaction-filter/src/lib.rs
@@ -0,0 +1,21 @@
+// Copyright (c) Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod boolean_transaction_filter;
+pub mod errors;
+pub mod filters;
+pub mod traits;
+
+// re-export for convenience
+pub use boolean_transaction_filter::BooleanTransactionFilter;
+pub use traits::Filterable;
+
+#[cfg(test)]
+pub mod test_lib;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {}
+}
