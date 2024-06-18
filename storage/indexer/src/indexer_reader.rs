bcc33e72b891c31a85eb17c94d75f5e2ebb02453
Bo Wu <bo@aptoslabs.com>
2024-06-18 10:51:11 -0700

    [internalindexer] add indexer db to state sync

--- storage/indexer/src/indexer_reader.rs
+++ storage/indexer/src/indexer_reader.rs
@@ -2,13 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{db_indexer::DBIndexer, db_v2::IndexerAsyncV2};
-use anyhow::{bail, Result};
+use anyhow::anyhow;
 use aptos_types::{
     account_address::AccountAddress,
     contract_event::EventWithVersion,
     event::EventKey,
     indexer::indexer_db_reader::{IndexerReader, Order},
-    state_store::table::{TableHandle, TableInfo},
+    state_store::{
+        state_key::{prefix::StateKeyPrefix, StateKey},
+        state_value::StateValue,
+        table::{TableHandle, TableInfo},
+    },
     transaction::{AccountTransactionsWithProof, Version},
 };
 use std::sync::Arc;
@@ -35,11 +39,11 @@ impl IndexerReaders {
 }
 
 impl IndexerReader for IndexerReaders {
-    fn get_table_info(&self, handle: TableHandle) -> Result<Option<TableInfo>> {
+    fn get_table_info(&self, handle: TableHandle) -> anyhow::Result<Option<TableInfo>> {
         if let Some(table_info_reader) = &self.table_info_reader {
             return Ok(table_info_reader.get_table_info_with_retry(handle)?);
         }
-        bail!("Table info reader is not available")
+        anyhow::bail!("Table info reader is not available")
     }
 
     fn get_events(
@@ -49,21 +53,21 @@ impl IndexerReader for IndexerReaders {
         order: Order,
         limit: u64,
         ledger_version: Version,
-    ) -> Result<Vec<EventWithVersion>> {
+    ) -> anyhow::Result<Vec<EventWithVersion>> {
         if let Some(db_indexer_reader) = &self.db_indexer_reader {
             if db_indexer_reader.event_enabled() {
-                return db_indexer_reader.get_events(
+                return Ok(db_indexer_reader.get_events(
                     event_key,
                     start,
                     order,
                     limit,
                     ledger_version,
-                );
+                )?);
             } else {
-                bail!("Event index is not enabled")
+                anyhow::bail!("Event index is not enabled")
             }
         }
-        bail!("DB Indexer reader is not available")
+        anyhow::bail!("DB Indexer reader is not available")
     }
 
     fn get_events_by_event_key(
@@ -73,21 +77,21 @@ impl IndexerReader for IndexerReaders {
         order: Order,
         limit: u64,
         ledger_version: Version,
-    ) -> Result<Vec<EventWithVersion>> {
+    ) -> anyhow::Result<Vec<EventWithVersion>> {
         if let Some(db_indexer_reader) = &self.db_indexer_reader {
             if db_indexer_reader.event_enabled() {
-                return db_indexer_reader.get_events_by_event_key(
+                return Ok(db_indexer_reader.get_events_by_event_key(
                     event_key,
                     start_seq_num,
                     order,
                     limit,
                     ledger_version,
-                );
+                )?);
             } else {
-                bail!("Event index is not enabled")
+                anyhow::bail!("Event index is not enabled")
             }
         }
-        bail!("DB indexer reader is not available")
+        anyhow::bail!("DB indexer reader is not available")
     }
 
     fn get_account_transactions(
@@ -97,20 +101,48 @@ impl IndexerReader for IndexerReaders {
         limit: u64,
         include_events: bool,
         ledger_version: Version,
-    ) -> Result<AccountTransactionsWithProof> {
+    ) -> anyhow::Result<AccountTransactionsWithProof> {
         if let Some(db_indexer_reader) = &self.db_indexer_reader {
             if db_indexer_reader.transaction_enabled() {
-                return db_indexer_reader.get_account_transactions(
+                return Ok(db_indexer_reader.get_account_transactions(
                     address,
                     start_seq_num,
                     limit,
                     include_events,
                     ledger_version,
-                );
+                )?);
             } else {
-                bail!("Transaction by account index is not enabled")
+                anyhow::bail!("Transaction by account index is not enabled")
             }
         }
-        bail!("DB indexer reader is not available")
+        anyhow::bail!("DB indexer reader is not available")
+    }
+
+    fn get_prefixed_state_value_iterator(
+        &self,
+        key_prefix: &StateKeyPrefix,
+        cursor: Option<&StateKey>,
+        version: Version,
+    ) -> anyhow::Result<Box<dyn Iterator<Item = anyhow::Result<(StateKey, StateValue)>> + '_>> {
+        if let Some(db_indexer_reader) = &self.db_indexer_reader {
+            if db_indexer_reader.statekeys_enabled() {
+                return Ok(Box::new(
+                    db_indexer_reader
+                        .get_prefixed_state_value_iterator(key_prefix, cursor, version)
+                        .map_err(|err| {
+                            anyhow!(format!(
+                                "failed to get prefixed state value iterator {}",
+                                err
+                            ))
+                        })?,
+                )
+                    as Box<
+                        dyn Iterator<Item = anyhow::Result<(StateKey, StateValue)>>,
+                    >);
+            } else {
+                anyhow::bail!("StateKeys index is not enabled")
+            }
+        }
+        anyhow::bail!("DB indexer reader is not available")
     }
 }
