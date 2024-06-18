5653980bac46c30b07fd687eaf67591068b71214
Zijie Zhao <zijie.zhao@aptoslabs.com>
2024-06-18 16:09:43 -0700

    [move-smith] add a bin for getting flamegraph

---
+++ third_party/move/tools/move-smith/src/cli/flame.rs
@@ -0,0 +1,21 @@
+// Copyright (c) Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use arbitrary::Unstructured;
+use move_smith::{codegen::*, config::*, move_smith::*, utils::*};
+
+fn main() {
+    let raw_data = get_random_bytes(21289, 8192);
+    let mut u = Unstructured::new(&raw_data);
+
+    let mut smith = MoveSmith::default();
+    smith.generate(&mut u).unwrap();
+    let compile_unit = smith.get_compile_unit();
+    let code = compile_unit.emit_code();
+
+    compile_modules(code.clone());
+
+    println!("{}", code.clone());
+
+    run_transactional_test(code.clone(), &Config::default()).unwrap();
+}
