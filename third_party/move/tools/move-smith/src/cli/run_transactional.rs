24338b2a7dcc06f6f5ba8b2d5e002ffbcac3ca50
Zijie Zhao <zijie.zhao@aptoslabs.com>
2024-06-18 17:47:42 -0700

    [move-smith] add cli helper to run transactionl test

---
+++ third_party/move/tools/move-smith/src/cli/run_transactional.rs
@@ -0,0 +1,31 @@
+// Copyright (c) Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+//! Simple CLI tool that reads transactional test from a file and runs it.
+
+use move_smith::{
+    config::Config,
+    utils::{compile_modules, run_transactional_test},
+};
+use std::{fs, path::PathBuf};
+
+fn main() {
+    // Read the input file path from the first command line argument
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: run_transactional <input_file>");
+        std::process::exit(1);
+    }
+
+    let file_path = PathBuf::from(&args[1]);
+    let code = fs::read_to_string(&file_path).unwrap();
+    println!("Loaded code from file: {:?}", file_path);
+
+    println!("Compiling the Move code with V1");
+    compile_modules(code.clone());
+    println!("Successfully compiled with V1");
+
+    println!("Running the transactional test");
+    run_transactional_test(code, &Config::default()).unwrap();
+    println!("Transactional test passed");
+}
