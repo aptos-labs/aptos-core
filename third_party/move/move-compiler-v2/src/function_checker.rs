0bf7f80c8fc5115f3922ed5c2f4b07663a1f87b0
Zekun Wang <zekun.wang@aptoslabs.com>
2024-06-19 22:00:05 -0400

    err msg

--- third_party/move/move-compiler-v2/src/function_checker.rs
+++ third_party/move/move-compiler-v2/src/function_checker.rs
@@ -254,8 +254,42 @@ pub fn check_access_and_use(env: &mut GlobalEnv, before_inlining: bool) {
                                     if callee_func.module_env.has_friend(&caller_module_id) {
                                         true
                                     } else {
-                                        not_a_friend_error(env, sites, &caller_func, &callee_func);
-                                        false
+                                        if callee_func.has_package_visibility() {
+                                            if callee_func.module_env.self_address()
+                                                == caller_func.module_env.self_address()
+                                            {
+                                                if callee_func
+                                                    .module_env
+                                                    .in_same_package(&caller_func.module_env)
+                                                {
+                                                    true
+                                                } else {
+                                                    diff_package_error(
+                                                        env,
+                                                        sites,
+                                                        &caller_func,
+                                                        &callee_func,
+                                                    );
+                                                    false
+                                                }
+                                            } else {
+                                                diff_addr_error(
+                                                    env,
+                                                    sites,
+                                                    &caller_func,
+                                                    &callee_func,
+                                                );
+                                                false
+                                            }
+                                        } else {
+                                            not_a_friend_error(
+                                                env,
+                                                sites,
+                                                &caller_func,
+                                                &callee_func,
+                                            );
+                                            false
+                                        }
                                     }
                                 },
                                 Visibility::Private => {
@@ -336,7 +370,7 @@ fn generic_error(
     let msg = format!(
         "{}function `{}` cannot be called from {}\
          because {}",
-        if callee.is_inline() { "inline " } else { "" },
+        if callee.is_inline() { "inline " } else if callee.has_package_visibility() { "public(package) " } else { "" },
         callee_name,
         called_from,
         why,
@@ -390,4 +424,26 @@ fn not_a_friend_error(
         callee.module_env.get_full_name_str()
     );
     cannot_call_error(env, &why, sites, caller, callee);
+}
+
+fn diff_package_error(
+    env: &GlobalEnv,
+    sites: &BTreeSet<NodeId>,
+    caller: &FunctionEnv,
+    callee: &FunctionEnv,
+) {
+    let why = format!(
+        "they are from different packages",
+    );
+    cannot_call_error(env, &why, sites, caller, callee);
+}
+
+fn diff_addr_error(
+    env: &GlobalEnv,
+    sites: &BTreeSet<NodeId>,
+    caller: &FunctionEnv,
+    callee: &FunctionEnv,
+) {
+    let why = "they are from different addresses";
+    cannot_call_error(env,why, sites, caller, callee);
 }
