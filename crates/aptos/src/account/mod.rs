3bf4321598fcc61fd95ca4bd84da46751e3b428c
Greg Nazario <greg@gnazar.io>
2024-06-20 23:57:46 -0700

    [cli] Add balance command to make it easier to lookup balances (#13634)

--- crates/aptos/src/account/mod.rs
+++ crates/aptos/src/account/mod.rs
@@ -4,6 +4,7 @@
 use crate::common::types::{CliCommand, CliResult};
 use clap::Subcommand;
 
+pub mod balance;
 pub mod create;
 pub mod create_resource_account;
 pub mod derive_resource_account;
@@ -23,6 +24,7 @@ pub enum AccountTool {
     CreateResourceAccount(create_resource_account::CreateResourceAccount),
     DeriveResourceAccountAddress(derive_resource_account::DeriveResourceAccount),
     FundWithFaucet(fund::FundWithFaucet),
+    Balance(balance::Balance),
     List(list::ListAccount),
     LookupAddress(key_rotation::LookupAddress),
     RotateKey(key_rotation::RotateKey),
@@ -36,6 +38,7 @@ impl AccountTool {
             AccountTool::CreateResourceAccount(tool) => tool.execute_serialized().await,
             AccountTool::DeriveResourceAccountAddress(tool) => tool.execute_serialized().await,
             AccountTool::FundWithFaucet(tool) => tool.execute_serialized().await,
+            AccountTool::Balance(tool) => tool.execute_serialized().await,
             AccountTool::List(tool) => tool.execute_serialized().await,
             AccountTool::LookupAddress(tool) => tool.execute_serialized().await,
             AccountTool::RotateKey(tool) => tool.execute_serialized().await,
