ea1805e0cb3ec70fe5c9b298e6aa799fa635ffe9
zhoujunma <zjma@users.noreply.github.com>
2024-06-21 12:47:17 -0700

    fix doc error

--- keyless/common/src/input_processing/bits.rs
+++ keyless/common/src/input_processing/bits.rs
@@ -8,7 +8,7 @@ use std::ops::{self, Add, AddAssign};
 
 /// Type for bit representation during conversion. Represents bits using strings, for easy
 /// manipulation:
-/// ```
+/// ```ignore
 /// use prover_service::input_processing::bits::Bits;
 /// let b = Bits::raw("00001111");
 /// assert_eq!(b.as_bytes().unwrap()[0], 15u8);
