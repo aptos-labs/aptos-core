fc87dcaf8c72c197c32913d492dd741af56b47fb
janolk <jan.olkowski.a@gmail.com>
2024-06-28 16:22:36 -0700

    32:16; 60:120; 32:60 tokio runtimes workers

--- execution/executor-service/src/remote_executor_client.rs
+++ execution/executor-service/src/remote_executor_client.rs
@@ -112,7 +112,7 @@ impl<S: StateView + Sync + Send + 'static> RemoteExecutorClient<S> {
         let num_threads = num_threads.unwrap_or_else(num_cpus::get);
         let thread_pool = Arc::new(
             rayon::ThreadPoolBuilder::new()
-                .num_threads(8)
+                .num_threads(16)
                 .build()
                 .unwrap(),
         );
@@ -155,7 +155,7 @@ impl<S: StateView + Sync + Send + 'static> RemoteExecutorClient<S> {
         let cmd_tx_thread_pool = Arc::new(
             rayon::ThreadPoolBuilder::new()
                 .thread_name(move |index| format!("rmt-exe-cli-cmd-tx-{}", index))
-                .num_threads(16)//num_cpus::get() / 2)
+                .num_threads(32)//num_cpus::get() / 2)
                 .build()
                 .unwrap(),
         );
