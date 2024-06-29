8dee85df78f59f6d95547eb28c188944a5742139
WGB5445 <919603023@qq.com>
2024-06-29 15:40:34 +0800

    fix deserialize

--- aptos-move/aptos-intent/index.js
+++ aptos-move/aptos-intent/index.js
@@ -1,9 +1,19 @@
+const { BatchArgument } = require("./pkg");
+
 const rust = import("./pkg");
 
 rust
   .then((m) => {
-    return  m.BatchedFunctionCallBuilder.single_signer().load_module("testnet","0x0000000000000000000000000000000000000000000000000000000000000001 account").then((data) => {
-      console.log(data);
-    });
+    let builder = m.BatchedFunctionCallBuilder.single_signer();
+    return builder.load_module("testnet", "0x0000000000000000000000000000000000000000000000000000000000000001 aptos_account")
+      .then(() => {
+        builder.add_batched_call(
+          "0x0000000000000000000000000000000000000000000000000000000000000001 aptos_account",
+          "transfer",
+          [],
+          [BatchArgument.new_bytes(Buffer.from( "0x77201cdd810bbd83fad933ee490104e384579b937573a949ebc65264da243a12", "hex")), BatchArgument.new_bytes(Buffer.from("1"))]
+        ).then(console.log)
+      })
+      .catch(console.error);
   })
   .catch(console.error);
