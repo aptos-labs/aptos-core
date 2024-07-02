986995996411f0886957bc8645c608601e9010ef
Satya Vusirikala <satyasatya123456@gmail.com>
2024-07-02 15:45:17 -0700

    Revert "Proof queue with more accurate calculation of remaining txns in the pipeline (#13703)"
    This reverts commit 387c64917755498d16a5b5905ad3a936767c4a7f.

--- consensus/src/quorum_store/batch_coordinator.rs
+++ consensus/src/quorum_store/batch_coordinator.rs
@@ -76,12 +76,7 @@ impl BatchCoordinator {
             let peer_id = persist_requests[0].author();
             let batches = persist_requests
                 .iter()
-                .map(|persisted_value| {
-                    (
-                        persisted_value.batch_info().clone(),
-                        persisted_value.summary(),
-                    )
-                })
+                .map(|persisted_value| persisted_value.batch_info().clone())
                 .collect();
             let signed_batch_infos = batch_store.persist(persist_requests);
             if !signed_batch_infos.is_empty() {
