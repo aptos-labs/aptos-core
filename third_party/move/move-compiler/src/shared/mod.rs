c1da96458a1aca74fa96716e9620cf41cc360a8f
Brian R. Murphy <132495859+brmataptos@users.noreply.github.com>
2024-07-02 17:03:26 -0700

    rustfmt

--- third_party/move/move-compiler/src/shared/mod.rs
+++ third_party/move/move-compiler/src/shared/mod.rs
@@ -117,13 +117,13 @@ pub fn shortest_cycle<'a, T: Ord + Hash>(
             );
             match (shortest_path, path_opt) {
                 (p, None) | (None, p) => p,
-                (Some((acc_len, acc_path)), Some((cur_len, cur_path))) => {
-                    Some(if cur_len < acc_len {
+                (Some((acc_len, acc_path)), Some((cur_len, cur_path))) => Some(
+                    if cur_len < acc_len {
                         (cur_len, cur_path)
                     } else {
                         (acc_len, acc_path)
-                    })
-                },
+                    },
+                ),
             }
         });
     let (_, mut path) = shortest_path.unwrap();
