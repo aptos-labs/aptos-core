71988de23577b86c1f041f6002457480527ba2e5
Satya Vusirikala <satyasatya123456@gmail.com>
2024-07-08 21:19:08 -0700

    Decrease graceful overload tps

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1425,7 +1425,7 @@ fn realistic_env_graceful_overload() -> ForgeConfig {
         .with_initial_fullnode_count(20)
         .add_network_test(wrap_with_realistic_env(TwoTrafficsTest {
             inner_traffic: EmitJobRequest::default()
-                .mode(EmitJobMode::ConstTps { tps: 40000 })
+                .mode(EmitJobMode::ConstTps { tps: 25000 })
                 .init_gas_price_multiplier(20),
             inner_success_criteria: SuccessCriteria::new(7500),
         }))
