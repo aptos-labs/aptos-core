b53e8a0c3c0ed151852be5910fccc0b8387a5242
Mikhail Zabaluev <mikhail.zabaluev@movementlabs.xyz>
2024-07-11 01:19:29 +0300

    fix(aptos-testcases): rename imports
    crate aptos renamed to movement.

--- testsuite/testcases/src/dag_onchain_enable_test.rs
+++ testsuite/testcases/src/dag_onchain_enable_test.rs
@@ -3,7 +3,7 @@
 
 use crate::{generate_onchain_config_blob, NetworkLoadTest};
 use anyhow::Ok;
-use aptos::test::CliTestFramework;
+use movement::test::CliTestFramework;
 use aptos_forge::{NetworkContextSynchronizer, NetworkTest, NodeExt, SwarmExt, Test};
 use aptos_logger::info;
 use aptos_sdk::bcs;
