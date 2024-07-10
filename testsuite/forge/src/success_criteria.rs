ec980f9986a7ca8e06d1e64711f727bb0b865aef
Mikhail Zabaluev <mikhail.zabaluev@movementlabs.xyz>
2024-07-11 01:12:49 +0300

    fix: rename some aptos imports to movement

--- testsuite/forge/src/success_criteria.rs
+++ testsuite/forge/src/success_criteria.rs
@@ -9,7 +9,7 @@ use crate::{
     Swarm, SwarmExt, TestReport,
 };
 use anyhow::{bail, Context};
-use aptos::node::analyze::fetch_metadata::FetchMetadata;
+use movement::node::analyze::fetch_metadata::FetchMetadata;
 use aptos_sdk::types::PeerId;
 use aptos_transaction_emitter_lib::{TxnStats, TxnStatsRate};
 use prometheus_http_query::response::Sample;
