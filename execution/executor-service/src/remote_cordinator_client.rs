17ccb739c65146256d14eced393fab866639c4cd
janolk <jan.olkowski.a@gmail.com>
2024-07-11 10:34:23 -0700

    Serial prefetching on shards

--- execution/executor-service/src/remote_cordinator_client.rs
+++ execution/executor-service/src/remote_cordinator_client.rs
@@ -162,7 +162,7 @@ impl RemoteCoordinatorClient {
                     let init_prefetch_timer = REMOTE_EXECUTOR_TIMER
                         .with_label_values(&[&shard_id.to_string(), "init_prefetch"])
                         .start_timer();
-                    cmd_rx_thread_pool_clone.spawn_fifo(move || {
+                    //cmd_rx_thread_pool_clone.spawn_fifo(move || {
 
 
                         let batch_start_index = txns.batch_start_index;
@@ -173,7 +173,7 @@ impl RemoteCoordinatorClient {
                         let _ = transactions.into_iter().enumerate().for_each(|(idx, txn)| {
                             blocking_transactions_provider_clone.set_txn(idx + batch_start_index, txn);
                         });
-                    });
+                    //});
                 },
                 Err(_) => { break; }
             }
