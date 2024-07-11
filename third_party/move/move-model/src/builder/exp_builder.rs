a01ca773e124d480562c534d7683e3e4f6597c91
Zekun Wang <zekun.wang@aptoslabs.com>
2024-07-11 16:28:48 +0800

    refactor

--- third_party/move/move-model/src/builder/exp_builder.rs
+++ third_party/move/move-model/src/builder/exp_builder.rs
@@ -2801,6 +2801,8 @@ impl<'env, 'translator, 'module_translator> ExpTranslator<'env, 'translator, 'mo
             context,
         ) {
             return value;
+        } else if maccess.value.is_struct_access() {
+            return self.new_error_exp()
         }
 
         // Treat this as a call to a global function.
