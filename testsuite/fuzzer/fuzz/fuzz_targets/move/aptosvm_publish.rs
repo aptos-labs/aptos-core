7b930c1e46521916c28e3ee68aa32b2315345404
zi0Black <13380579+zi0Black@users.noreply.github.com>
2024-07-16 12:14:42 +0200

    Moved some of shared code to util.rs & fixed typos

--- testsuite/fuzzer/fuzz/fuzz_targets/move/aptosvm_publish.rs
+++ testsuite/fuzzer/fuzz/fuzz_targets/move/aptosvm_publish.rs
@@ -3,90 +3,28 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use aptos_cached_packages::aptos_stdlib::code_publish_package_txn;
-use aptos_framework::natives::code::{
-    ModuleMetadata, MoveOption, PackageDep, PackageMetadata, UpgradePolicy,
-};
+mod utils;
 use aptos_language_e2e_tests::{
-    data_store::GENESIS_CHANGE_SET_HEAD, executor::FakeExecutor, account::Account,
+    data_store::GENESIS_CHANGE_SET_HEAD, executor::FakeExecutor,
 };
 use aptos_types::{
     chain_id::ChainId,
-    transaction::{
-        ExecutionStatus, TransactionPayload, TransactionStatus
-    },
+
     write_set::WriteSet,
 };
 use aptos_vm::AptosVM;
-use arbitrary::Arbitrary;
 use libfuzzer_sys::{fuzz_target, Corpus};
 use move_binary_format::{
     access::ModuleAccess,
     deserializer::DeserializerConfig,
-    file_format::{CompiledModule, CompiledScript, FunctionDefinitionIndex},
-};
-use move_core_types::{
-    language_storage::{ModuleId, TypeTag},
-    value::MoveValue,
-    vm_status::{StatusType, VMStatus},
+    file_format::{CompiledModule, CompiledScript},
 };
 use once_cell::sync::Lazy;
+use utils::*;
 use std::{
-    collections::{BTreeMap, BTreeSet, HashSet},
+    collections::{BTreeMap, HashSet},
     sync::Arc,
 };
-
-#[derive(Debug, Arbitrary, Eq, PartialEq, Clone, Copy)]
-pub enum FundAmount {
-    Zero,
-    Poor,
-    Rich,
-}
-
-#[derive(Debug, Arbitrary, Eq, PartialEq, Clone, Copy)]
-pub struct UserAccount {
-    is_inited_and_funded: bool,
-    fund: FundAmount,
-}
-
-#[derive(Debug, Arbitrary, Eq, PartialEq, Clone)]
-pub enum Authenticator {
-    Ed25519 {
-        sender: UserAccount,
-    },
-    MultiAgent {
-        sender: UserAccount,
-        secondary_signers: Vec<UserAccount>,
-    },
-    FeePayer {
-        sender: UserAccount,
-        secondary_signers: Vec<UserAccount>,
-        fee_payer: UserAccount,
-    },
-}
-
-
-#[derive(Debug, Arbitrary, Eq, PartialEq, Clone)]
-pub enum ExecVariant {
-    Script {
-        script: CompiledScript,
-        type_args: Vec<TypeTag>,
-        args: Vec<MoveValue>,
-    },
-    CallFunction {
-        module: ModuleId,
-        function: FunctionDefinitionIndex,
-        type_args: Vec<TypeTag>,
-        args: Vec<Vec<u8>>,
-    },
-}
-
-#[derive(Debug, Arbitrary, Eq, PartialEq, Clone)]
-pub struct RunnableState {
-    pub dep_modules: Vec<CompiledModule>,
-    pub exec_variant: ExecVariant,
-    pub tx_auth_type: Authenticator,
-}
 
 // genesis write set generated once for each fuzzing session
 static VM: Lazy<WriteSet> = Lazy::new(|| GENESIS_CHANGE_SET_HEAD.write_set().clone());
@@ -101,156 +39,6 @@ static TP: Lazy<Arc<rayon::ThreadPool>> = Lazy::new(|| {
             .unwrap(),
     )
 });
-
-// small debug macro which can be enabled or disabled
-const DEBUG: bool = false;
-macro_rules! tdbg {
-    () => {
-        ()
-    };
-    ($val:expr $(,)?) => {
-        if DEBUG {
-            dbg!($val)
-        } else {
-            ($val)
-        }
-    };
-    ($($val:expr),+ $(,)?) => {
-        if DEBUG {
-            dbg!($(($val)),+,)
-        } else {
-            ($(($val)),+,)
-        }
-    };
-}
-
-// used for ordering modules topologically
-fn sort_by_deps(
-    map: &BTreeMap<ModuleId, CompiledModule>,
-    order: &mut Vec<ModuleId>,
-    id: ModuleId,
-    visited: &mut HashSet<ModuleId>,
-) -> Result<(), Corpus> {
-    if visited.contains(&id) {
-        return Err(Corpus::Keep);
-    }
-    visited.insert(id.clone());
-    if order.contains(&id) {
-        return Ok(());
-    }
-    let compiled = &map.get(&id).unwrap();
-    for dep in compiled.immediate_dependencies() {
-        // Only consider deps which are actually in this package. Deps for outside
-        // packages are considered fine because of package deployment order. Note
-        // that because of this detail, we can't use existing topsort from Move utils.
-        if map.contains_key(&dep) {
-            sort_by_deps(map, order, dep, visited)?;
-        }
-    }
-    order.push(id);
-    Ok(())
-}
-
-// panic to catch invariant violations
-fn check_for_invariant_violation(e: VMStatus) {
-    if e.status_type() == StatusType::InvariantViolation {
-        // known false positive
-        if e.message() != Some(&"moving container with dangling references".to_string()) {
-            panic!("invariant violation {:?}", e);
-        }
-    }
-}
-
-fn publish_transaction_payload(modules: &[CompiledModule]) -> TransactionPayload {
-    let modules_metadatas: Vec<_> = modules
-        .iter()
-        .map(|cm| ModuleMetadata {
-            name: cm.name().to_string(),
-            source: vec![],
-            source_map: vec![],
-            extension: MoveOption::default(),
-        })
-        .collect();
-
-    let all_immediate_deps: Vec<_> = modules
-        .iter()
-        .flat_map(|cm| cm.immediate_dependencies())
-        .map(|mi| PackageDep {
-            account: mi.address,
-            package_name: mi.name.to_string(),
-        })
-        .collect::<BTreeSet<_>>() // leave only uniques
-        .into_iter()
-        .filter(|c| &c.account != modules[0].address()) // filter out package itself
-        .collect::<Vec<_>>();
-
-    let metadata = PackageMetadata {
-        name: "fuzz_package".to_string(),
-        upgrade_policy: UpgradePolicy::compat(), // TODO: currently does not matter. Maybe fuzz compat checks specifically at some point.
-        upgrade_number: 1,
-        source_digest: "".to_string(),
-        manifest: vec![],
-        modules: modules_metadatas,
-        deps: all_immediate_deps,
-        extension: MoveOption::default(),
-    };
-    let pkg_metadata = bcs::to_bytes(&metadata).expect("PackageMetadata must serialize");
-    let mut pkg_code: Vec<Vec<u8>> = vec![];
-    for module in modules {
-        let mut module_code: Vec<u8> = vec![];
-        module
-            .serialize(&mut module_code)
-            .expect("Module must serialize");
-        pkg_code.push(module_code);
-    }
-    code_publish_package_txn(pkg_metadata, pkg_code)
-}
-
-fn publish_group(vm: &mut FakeExecutor, acc: &Account, group: &[CompiledModule], sequence_number: u64) -> Result<(), Corpus> {
-    let tx = acc
-        .transaction()
-        .gas_unit_price(100)
-        .sequence_number(sequence_number)
-        .payload(publish_transaction_payload(group))
-        .sign();
-
-    tdbg!("publishing");
-    let res = vm
-        .execute_block(vec![tx])
-        .map_err(|e| {
-            check_for_invariant_violation(e);
-            Corpus::Keep
-        })?
-        .pop()
-        .expect("expected 1 output");
-    // if error exit gracefully
-    tdbg!(&res);
-    let status = match tdbg!(res.status()) {
-        TransactionStatus::Keep(status) => status,
-        TransactionStatus::Discard(e) => {
-            if e.status_type() == StatusType::InvariantViolation {
-                panic!("invariant violation {:?}", e);
-            }
-            return Err(Corpus::Keep);
-        },
-        _ => return Err(Corpus::Keep),
-    };
-    tdbg!(&status);
-    // apply write set to commit published packages
-    vm.apply_write_set(res.write_set());
-    match tdbg!(status) {
-        ExecutionStatus::Success => Ok(()),
-        ExecutionStatus::MiscellaneousError(e) => {
-            if let Some(e) = e {
-                if e.status_type() == StatusType::InvariantViolation {
-                    panic!("invariant violation {:?}", e);
-                }
-            }
-            Err(Corpus::Keep)
-        },
-        _ => Err(Corpus::Keep),
-    }
-}
 
 fn run_case(mut input: RunnableState) -> Result<(), Corpus> {
     tdbg!(&input);
