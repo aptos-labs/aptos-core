56043eb9b8f499ebe24521f994f30510812fdef9
Brian Cho <brian@aptoslabs.com>
2024-07-18 11:21:44 -0700

    Add window and block retrieval to block_tree, block_store and persistent_liveness_storage. Validator should be able to retrieve blocks and recover with blocks up to window start of the next block.
    Also adds OrderedBlockWindow into PipelinedBlock.
    Actual execution of the block will be added to a separate PR.

--- consensus/consensus-types/src/pipelined_block.rs
+++ consensus/consensus-types/src/pipelined_block.rs
@@ -11,7 +11,7 @@ use crate::{
 };
 use aptos_crypto::hash::{HashValue, ACCUMULATOR_PLACEHOLDER_HASH};
 use aptos_executor_types::StateComputeResult;
-use aptos_logger::{error, warn};
+use aptos_logger::{error, info, warn};
 use aptos_types::{
     block_info::BlockInfo,
     contract_event::ContractEvent,
@@ -27,6 +27,25 @@ use std::{
     time::{Duration, Instant},
 };
 
+#[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct OrderedBlockWindow {
+    blocks: Vec<Block>,
+}
+
+impl OrderedBlockWindow {
+    pub fn new(blocks: Vec<Block>) -> Self {
+        Self { blocks }
+    }
+
+    pub fn empty() -> Self {
+        Self { blocks: vec![] }
+    }
+
+    pub fn blocks(&self) -> &Vec<Block> {
+        &self.blocks
+    }
+}
+
 /// A representation of a block that has been added to the execution pipeline. It might either be in ordered
 /// or in executed state. In the ordered state, the block is waiting to be executed. In the executed state,
 /// the block has been executed and the output is available.
@@ -34,6 +53,8 @@ use std::{
 pub struct PipelinedBlock {
     /// Block data that cannot be regenerated.
     block: Block,
+    /// A window of blocks that are needed for execution with the execution pool, excluding the current block
+    block_window: OrderedBlockWindow,
     /// Input transactions in the order of execution
     input_transactions: Vec<SignedTransaction>,
     /// The state_compute_result is calculated for all the pending blocks prior to insertion to
@@ -54,6 +75,7 @@ impl Serialize for PipelinedBlock {
         #[serde(rename = "PipelineBlock")]
         struct SerializedBlock<'a> {
             block: &'a Block,
+            block_window: &'a OrderedBlockWindow,
             input_transactions: &'a Vec<SignedTransaction>,
             state_compute_result: &'a StateComputeResult,
             randomness: Option<&'a Randomness>,
@@ -61,6 +83,7 @@ impl Serialize for PipelinedBlock {
 
         let serialized = SerializedBlock {
             block: &self.block,
+            block_window: &self.block_window,
             input_transactions: &self.input_transactions,
             state_compute_result: &self.state_compute_result,
             randomness: self.randomness.get(),
@@ -78,6 +101,7 @@ impl<'de> Deserialize<'de> for PipelinedBlock {
         #[serde(rename = "PipelineBlock")]
         struct SerializedBlock {
             block: Block,
+            block_window: OrderedBlockWindow,
             input_transactions: Vec<SignedTransaction>,
             state_compute_result: StateComputeResult,
             randomness: Option<Randomness>,
@@ -85,6 +109,7 @@ impl<'de> Deserialize<'de> for PipelinedBlock {
 
         let SerializedBlock {
             block,
+            block_window,
             input_transactions,
             state_compute_result,
             randomness,
@@ -92,6 +117,7 @@ impl<'de> Deserialize<'de> for PipelinedBlock {
 
         let block = PipelinedBlock {
             block,
+            block_window,
             input_transactions,
             state_compute_result,
             randomness: OnceCell::new(),
@@ -188,8 +214,18 @@ impl PipelinedBlock {
         input_transactions: Vec<SignedTransaction>,
         state_compute_result: StateComputeResult,
     ) -> Self {
+        info!(
+            "New PipelinedBlock with block_id: {}, parent_id: {}, round: {}, epoch: {}, txns: {}",
+            block.id(),
+            block.parent_id(),
+            block.round(),
+            block.epoch(),
+            block.payload().map_or(0, |p| p.len())
+        );
+
         Self {
             block,
+            block_window: OrderedBlockWindow::new(vec![]),
             input_transactions,
             state_compute_result,
             randomness: OnceCell::new(),
@@ -198,9 +234,18 @@ impl PipelinedBlock {
         }
     }
 
-    pub fn new_ordered(block: Block) -> Self {
+    pub fn new_ordered(block: Block, window: OrderedBlockWindow) -> Self {
+        info!(
+            "New Ordered PipelinedBlock with block_id: {}, parent_id: {}, round: {}, epoch: {}, txns: {}",
+            block.id(),
+            block.parent_id(),
+            block.round(),
+            block.epoch(),
+            block.payload().map_or(0, |p| p.len())
+        );
         Self {
             block,
+            block_window: window,
             input_transactions: vec![],
             state_compute_result: StateComputeResult::new_dummy(),
             randomness: OnceCell::new(),
@@ -211,6 +256,11 @@ impl PipelinedBlock {
 
     pub fn block(&self) -> &Block {
         &self.block
+    }
+
+    // TODO: make this an Option?
+    pub fn block_window(&self) -> &OrderedBlockWindow {
+        &self.block_window
     }
 
     pub fn id(&self) -> HashValue {
--- consensus/src/block_storage/block_tree.rs
+++ consensus/src/block_storage/block_tree.rs
@@ -10,15 +10,18 @@ use crate::{
 };
 use anyhow::bail;
 use aptos_consensus_types::{
-    pipelined_block::PipelinedBlock, quorum_cert::QuorumCert,
-    timeout_2chain::TwoChainTimeoutCertificate, wrapped_ledger_info::WrappedLedgerInfo,
+    block::Block,
+    pipelined_block::{OrderedBlockWindow, PipelinedBlock},
+    quorum_cert::QuorumCert,
+    timeout_2chain::TwoChainTimeoutCertificate,
+    wrapped_ledger_info::WrappedLedgerInfo,
 };
 use aptos_crypto::HashValue;
 use aptos_logger::prelude::*;
 use aptos_types::{block_info::BlockInfo, ledger_info::LedgerInfoWithSignatures};
 use mirai_annotations::{checked_verify_eq, precondition};
 use std::{
-    collections::{vec_deque::VecDeque, HashMap, HashSet},
+    collections::{vec_deque::VecDeque, BTreeMap, HashMap, HashSet},
     sync::Arc,
 };
 
@@ -72,6 +75,7 @@ pub struct BlockTree {
     ordered_root_id: HashValue,
     /// Commit Root id: this is the root of commit phase
     commit_root_id: HashValue,
+    window_root_id: HashValue,
     /// A certified block id with highest round
     highest_certified_block_id: HashValue,
 
@@ -89,26 +93,28 @@ pub struct BlockTree {
     pruned_block_ids: VecDeque<HashValue>,
     /// Num pruned blocks to keep in memory.
     max_pruned_blocks_in_mem: usize,
+    window_size: usize,
 }
 
 impl BlockTree {
     pub(super) fn new(
-        root: PipelinedBlock,
+        root_block_id: HashValue,
+        // TODO: need the certs?
+        window_root: PipelinedBlock,
         root_quorum_cert: QuorumCert,
         root_ordered_cert: WrappedLedgerInfo,
         root_commit_cert: WrappedLedgerInfo,
         max_pruned_blocks_in_mem: usize,
+        window_size: usize,
         highest_2chain_timeout_cert: Option<Arc<TwoChainTimeoutCertificate>>,
     ) -> Self {
-        assert_eq!(
-            root.id(),
-            root_ordered_cert.commit_info().id(),
-            "inconsistent root and ledger info"
-        );
-        let root_id = root.id();
+        assert_eq!(window_root.epoch(), root_ordered_cert.commit_info().epoch());
+        assert!(window_root.round() <= root_ordered_cert.commit_info().round());
+        let window_root_id = window_root.id();
 
+        // Build the tree from the window root block which is <= the commit root block.
         let mut id_to_block = HashMap::new();
-        id_to_block.insert(root_id, LinkableBlock::new(root));
+        id_to_block.insert(window_root_id, LinkableBlock::new(window_root));
         counters::NUM_BLOCKS_IN_TREE.set(1);
 
         let root_quorum_cert = Arc::new(root_quorum_cert);
@@ -122,15 +128,17 @@ impl BlockTree {
 
         BlockTree {
             id_to_block,
-            ordered_root_id: root_id,
-            commit_root_id: root_id, // initially we set commit_root_id = root_id
-            highest_certified_block_id: root_id,
+            ordered_root_id: root_block_id,
+            commit_root_id: root_block_id, // initially we set commit_root_id = root_id
+            window_root_id,
+            highest_certified_block_id: root_block_id,
             highest_quorum_cert: Arc::clone(&root_quorum_cert),
             highest_ordered_cert: Arc::new(root_ordered_cert),
             highest_commit_cert: Arc::new(root_commit_cert),
             id_to_quorum_cert,
             pruned_block_ids,
             max_pruned_blocks_in_mem,
+            window_size,
             highest_2chain_timeout_cert,
         }
     }
@@ -163,7 +171,13 @@ impl BlockTree {
             .expect("Root must exist")
     }
 
+    fn linkable_window_root(&self) -> &LinkableBlock {
+        self.get_linkable_block(&self.window_root_id)
+            .expect("Window root must exist")
+    }
+
     fn remove_block(&mut self, block_id: HashValue) {
+        info!("remove_block: {}", block_id);
         // Remove the block from the store
         self.id_to_block.remove(&block_id);
         self.id_to_quorum_cert.remove(&block_id);
@@ -221,6 +235,63 @@ impl BlockTree {
         self.id_to_quorum_cert.get(block_id).cloned()
     }
 
+    // TODO: return an error when not enough blocks?
+    // TODO: how to know if the window is complete?
+    pub fn get_block_window(&self, block: &Block) -> Option<OrderedBlockWindow> {
+        // TODO: any other special cases that need to always have an empty window?
+        if block.is_nil_block() {
+            return Some(OrderedBlockWindow::new(vec![]));
+        }
+
+        let window_start_round = (block.round() + 1).saturating_sub(self.window_size as u64);
+        let window_size = (block.round() + 1) - window_start_round;
+        assert!(window_size > 0, "window_size must be greater than 0");
+        if window_size == 1 {
+            return Some(OrderedBlockWindow::new(vec![]));
+        }
+
+        let mut window = vec![];
+        let mut current_block = block.clone();
+        loop {
+            if current_block.parent_id() == HashValue::zero() {
+                info!(
+                    "Break at block: {}, for window of block: {}",
+                    current_block, block
+                );
+                break;
+            }
+            if let Some(parent_block) = self.get_block(&current_block.parent_id()) {
+                current_block = parent_block.block().clone();
+                info!(
+                    "Visiting block: {}, for window of block: {}",
+                    current_block, block
+                );
+                if current_block.round() < window_start_round {
+                    info!(
+                        "Break at block: {}, for window of block: {}",
+                        current_block, block
+                    );
+                    break;
+                }
+                info!(
+                    "Added block: {}, for window of block: {}",
+                    current_block, block
+                );
+                window.push(current_block.clone());
+            } else {
+                info!(
+                    "Visiting block: {} was not found, parent of block: {}, for window of block: {}",
+                    current_block.parent_id(),
+                    current_block,
+                    block
+                );
+                return None;
+            }
+        }
+        assert!(window.len() <= window_size as usize - 1);
+        Some(OrderedBlockWindow::new(window))
+    }
+
     pub(super) fn insert_block(
         &mut self,
         block: PipelinedBlock,
@@ -250,6 +321,12 @@ impl BlockTree {
         if new_commit_cert.commit_info().round() > self.highest_commit_cert.commit_info().round() {
             self.highest_commit_cert = Arc::new(new_commit_cert);
             self.update_commit_root(self.highest_commit_cert.commit_info().id());
+        } else {
+            warn!(
+                "Trying to update highest commit cert with lower round: {} <= {}",
+                new_commit_cert.commit_info().round(),
+                self.highest_commit_cert.commit_info().round()
+            );
         }
     }
 
@@ -309,19 +386,22 @@ impl BlockTree {
     /// B3--> B4, root = B3
     ///
     /// Note this function is read-only, use with process_pruned_blocks to do the actual prune.
-    pub(super) fn find_blocks_to_prune(&self, next_root_id: HashValue) -> VecDeque<HashValue> {
-        // Nothing to do if this is the commit root
-        if next_root_id == self.commit_root_id {
+    pub(super) fn find_blocks_to_prune(
+        &self,
+        next_window_root_id: HashValue,
+    ) -> VecDeque<HashValue> {
+        // Nothing to do if this is the window root
+        if next_window_root_id == self.window_root_id {
             return VecDeque::new();
         }
 
         let mut blocks_pruned = VecDeque::new();
-        let mut blocks_to_be_pruned = vec![self.linkable_root()];
+        let mut blocks_to_be_pruned = vec![self.linkable_window_root()];
         while let Some(block_to_remove) = blocks_to_be_pruned.pop() {
             // Add the children to the blocks to be pruned (if any), but stop when it reaches the
             // new root
             for child_id in block_to_remove.children() {
-                if next_root_id == *child_id {
+                if next_window_root_id == *child_id {
                     continue;
                 }
                 blocks_to_be_pruned.push(
@@ -343,6 +423,28 @@ impl BlockTree {
     pub(super) fn update_commit_root(&mut self, root_id: HashValue) {
         assert!(self.block_exists(&root_id));
         self.commit_root_id = root_id;
+    }
+
+    pub(super) fn update_window_root(&mut self, root_id: HashValue) {
+        assert!(self.block_exists(&root_id));
+        self.window_root_id = root_id;
+    }
+
+    pub(super) fn find_window_root(&self, commit_root_id: HashValue) -> HashValue {
+        let mut curr_block_id = commit_root_id;
+        while let Some(block) = self.get_block(&curr_block_id) {
+            if block.round()
+                <= self
+                    .commit_root()
+                    .round()
+                    .saturating_add(1)
+                    .saturating_sub(self.window_size as u64)
+            {
+                break;
+            }
+            curr_block_id = block.parent_id();
+        }
+        curr_block_id
     }
 
     /// Process the data returned by the prune_tree, they're separated because caller might
@@ -442,7 +544,9 @@ impl BlockTree {
             block_id = block_to_commit.id(),
         );
 
-        let ids_to_remove = self.find_blocks_to_prune(block_to_commit.id());
+        let window_root_id = self.find_window_root(block_to_commit.id());
+        let ids_to_remove = self.find_blocks_to_prune(window_root_id);
+        info!("Pruning blocks: {:?}", ids_to_remove);
         if let Err(e) = storage.prune_tree(ids_to_remove.clone().into_iter().collect()) {
             // it's fine to fail here, as long as the commit succeeds, the next restart will clean
             // up dangling blocks, and we need to prune the tree to keep the root consistent with
@@ -451,6 +555,7 @@ impl BlockTree {
         }
         self.process_pruned_blocks(ids_to_remove);
         self.update_highest_commit_cert(commit_proof);
+        self.update_window_root(window_root_id);
     }
 }
 
