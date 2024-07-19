e235d79d540b791e8c29ca0810a13e1458b573cb
zhoujun.ma <zhoujun@aptoslabs.com>
2024-07-19 15:24:14 -0700

    update smoke test

--- testsuite/smoke-test/src/mpc.rs
+++ testsuite/smoke-test/src/mpc.rs
@@ -10,6 +10,7 @@ use serde::{Deserialize, Serialize};
 use std::{collections::BTreeMap, str::FromStr, sync::Arc, time::Duration};
 use std::ops::Mul;
 use std::time::Instant;
+use diesel::CombineDsl;
 use rand::rngs::OsRng;
 use rand::thread_rng;
 use tokio::time::sleep;
@@ -17,7 +18,9 @@ use aptos_crypto::bls12381;
 use aptos_types::mpc::MpcState;
 use crate::utils::get_on_chain_resource;
 use ff::Field;
+use group::{Curve, Group};
 use group::prime::PrimeCurveAffine;
+use aptos::common::types::{CliTypedResult, TransactionSummary};
 
 #[tokio::test]
 async fn raise_by_secret() {
@@ -51,49 +54,37 @@ async fn raise_by_secret() {
     info!("Publishing a testing module.");
     publish_module(&mut cli, 0).await;
 
-    info!("Trigger raise.");
+    info!("Trigger raises for P and P^2.");
+    let my_esk_0 = blstrs::Scalar::from(55555);
+    let my_epk_0 = blstrs::G1Affine::generator().mul(&my_esk_0);
+    let my_epk_1 = my_epk_0.double();
+    let my_epk_0_bytes = my_epk_0.to_compressed().to_vec();
+    let my_epk_1_bytes = my_epk_1.to_compressed().to_vec();
+    trigger_raise(&cli, 0, my_epk_0_bytes).await.unwrap();
+    trigger_raise(&cli, 0, my_epk_1_bytes).await.unwrap();
     let account = cli.account_id(0).to_hex_literal();
-    let trigger_func_id = MemberId::from_str(&format!("{}::mpc_example::trigger_raise", account)).unwrap();
-    let mut rng = thread_rng();
 
-    let gas_options = GasOptions {
-        gas_unit_price: Some(100),
-        max_gas: Some(10_000),
-        expiration_secs: 60,
-    };
+    let tasks = rest_cli
+        .get_account_resource_bcs::<PendingResults>(
+            root_address,
+            format!("{}::mpc_example::PendingResults", account).as_str(),
+        )
+        .await
+        .unwrap()
+        .into_inner();
 
-    let my_secret = blstrs::Scalar::from(55555);
-    let my_epk = blstrs::G1Affine::generator().mul(&my_secret);
-    let my_epk_bytes = my_epk.to_compressed().to_vec();
-    let arg0 = format!("hex:0x{}", hex::encode(my_epk_bytes));
-    let txn_summary = cli
-        .run_function(0, Some(gas_options), trigger_func_id.clone(), vec![arg0.as_str()], vec![])
-        .await;
-    println!("txn_summary={:?}", txn_summary);
-    txn_summary.unwrap();
+    let t0 = tasks.tasks[0] as usize;
+    let t1 = tasks.tasks[1] as usize;
+
+    tokio::time::sleep(Duration::from_secs(10)).await;
     let mpc_state = get_on_chain_resource::<MpcState>(&rest_cli).await;
-    let chain_pk_bytes = <[u8; 48]>::try_from(mpc_state.tasks[0].result.clone().unwrap()).unwrap();
-    let chain_pk = blstrs::G1Affine::from_compressed(&chain_pk_bytes).unwrap();
-    let expected = chain_pk.mul(&my_secret);
-    let expected_bytes = expected.to_compressed().to_vec();
-    let arg0 = format!("hex:0x{}", hex::encode(expected_bytes));
-    let vrfy_func_id = MemberId::from_str(&format!("{}::mpc_example::fetch_and_verify", account)).unwrap();
-    info!("Wait for correct result.");
-    let timer = Instant::now();
-    let time_limit = Duration::from_secs(10);
-    while timer.elapsed() < time_limit {
-        let gas_options = GasOptions {
-            gas_unit_price: Some(100),
-            max_gas: Some(10_000),
-            expiration_secs: 60,
-        };
-        let txn_summary = cli
-            .run_function(0, Some(gas_options), vrfy_func_id.clone(), vec![arg0.as_str()], vec![])
-            .await;
-        println!("txn_summary={:?}", txn_summary);
-        txn_summary.unwrap();
-        sleep(Duration::from_secs(1)).await;
-    }
+    let result_0_bytes = <[u8; 48]>::try_from(mpc_state.tasks[t0].result.clone().unwrap()).unwrap();
+    let result_1_bytes = <[u8; 48]>::try_from(mpc_state.tasks[t1].result.clone().unwrap()).unwrap();
+    let result_0 = blstrs::G1Affine::from_compressed(&result_0_bytes).unwrap();
+    let result_1 = blstrs::G1Affine::from_compressed(&result_1_bytes).unwrap();
+    let two = blstrs::Scalar::from(2);
+    let result_0_doubled = result_0.mul(two).to_affine();
+    assert_eq!(result_0_doubled, result_1);
 }
 
 async fn publish_module(
@@ -127,4 +118,25 @@ async fn publish_module(
     cli.publish_package(0, None, named_addresses, None)
         .await
         .unwrap();
+}
+
+async fn trigger_raise(aptos_cli: &CliTestFramework, user_idx: usize, arg: Vec<u8>) -> CliTypedResult<TransactionSummary> {
+    let account = aptos_cli.account_id(user_idx).to_hex_literal();
+    let trigger_func_id = MemberId::from_str(&format!("{}::mpc_example::trigger_raise", account)).unwrap();
+
+    let gas_options = GasOptions {
+        gas_unit_price: Some(100),
+        max_gas: Some(10_000),
+        expiration_secs: 60,
+    };
+
+    let arg_str = format!("hex:0x{}", hex::encode(arg));
+    aptos_cli
+        .run_function(user_idx, Some(gas_options), trigger_func_id.clone(), vec![arg_str.as_str()], vec![])
+        .await
+}
+
+#[derive(Deserialize, Serialize)]
+struct PendingResults {
+    tasks: Vec<u64>,
 }
