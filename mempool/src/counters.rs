b236d6fd7370687f05646234fe1749022ac58eb3
Satya Vusirikala <satyasatya123456@gmail.com>
2024-08-06 16:12:37 -0700

    Addressed PR comments

--- mempool/src/counters.rs
+++ mempool/src/counters.rs
@@ -306,9 +306,9 @@ pub static CORE_MEMPOOL_GC_LATENCY: Lazy<HistogramVec> = Lazy::new(|| {
     .unwrap()
 });
 
-pub static CORE_MEMPOOL_TXN_CONSENSUS_PULLED: Lazy<Histogram> = Lazy::new(|| {
+pub static CORE_MEMPOOL_TXN_CONSENSUS_PULLED_BY_BUCKET: Lazy<Histogram> = Lazy::new(|| {
     register_histogram_vec!(
-        "aptos_core_mempool_txn_consensus_pulled",
+        "aptos_core_mempool_txn_consensus_pulled_by_bucket",
         "Number of times a txn was pulled from core mempool by consensus",
         &["bucket"],
         TXN_CONSENSUS_PULLED_BUCKETS.to_vec()
