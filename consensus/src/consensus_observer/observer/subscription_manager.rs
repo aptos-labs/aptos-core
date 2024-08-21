cca3ed038173df83fba5a62f2864bd03a6a33f17
Josh Lind <josh.lind@hotmail.com>
2024-08-21 19:12:13 -0400

    [Consensus Observer] Add simple unit tests for subscription manager.

--- consensus/src/consensus_observer/observer/subscription_manager.rs
+++ consensus/src/consensus_observer/observer/subscription_manager.rs
@@ -410,3 +410,446 @@ impl SubscriptionManager {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use aptos_config::network_id::NetworkId;
+    use aptos_network::{application::storage::PeersAndMetadata, transport::ConnectionMetadata};
+    use aptos_peer_monitoring_service_types::{
+        response::NetworkInformationResponse, PeerMonitoringMetadata,
+    };
+    use aptos_types::{transaction::Version, PeerId};
+    use claims::assert_matches;
+    use maplit::hashmap;
+    use mockall::mock;
+    use std::{collections::BTreeMap, time::Duration};
+
+    // This is a simple mock of the DbReader (it generates a MockDatabaseReader)
+    mock! {
+        pub DatabaseReader {}
+        impl DbReader for DatabaseReader {
+            fn get_latest_ledger_info_version(&self) -> aptos_storage_interface::Result<Version>;
+        }
+    }
+
+    #[tokio::test]
+    async fn test_check_active_subscription_connected() {
+        // Create a consensus observer client
+        let network_id = NetworkId::Public;
+        let (peers_and_metadata, consensus_observer_client) =
+            create_consensus_observer_client(&[network_id]);
+
+        // Create a new subscription manager
+        let consensus_observer_config = ConsensusObserverConfig::default();
+        let db_reader = create_mock_db_reader();
+        let mut subscription_manager = SubscriptionManager::new(
+            consensus_observer_client,
+            consensus_observer_config,
+            None,
+            db_reader.clone(),
+            TimeService::mock(),
+        );
+
+        // Create a new subscription
+        let observer_subscription = ConsensusObserverSubscription::new(
+            consensus_observer_config,
+            db_reader.clone(),
+            PeerNetworkId::random(),
+            TimeService::mock(),
+        );
+        subscription_manager.active_observer_subscription = Some(observer_subscription);
+
+        // Check the active subscription and verify that it is removed (the peer is not connected)
+        assert_matches!(
+            subscription_manager.check_active_subscription(),
+            Err(Error::SubscriptionDisconnected(_))
+        );
+        assert!(subscription_manager.active_observer_subscription.is_none());
+
+        // Add a new connected peer
+        let connected_peer =
+            create_peer_and_connection(network_id, peers_and_metadata.clone(), 1, None);
+
+        // Create a subscription to the new peer
+        create_observer_subscription(
+            &mut subscription_manager,
+            consensus_observer_config,
+            db_reader,
+            connected_peer,
+            TimeService::mock(),
+        );
+
+        // Check the active subscription and verify that it is still active (the peer is connected)
+        assert!(subscription_manager.check_active_subscription().is_ok());
+        let active_subscription = subscription_manager.active_observer_subscription.unwrap();
+        assert_eq!(active_subscription.get_peer_network_id(), connected_peer);
+    }
+
+    #[tokio::test]
+    async fn test_check_active_subscription_progress_stopped() {
+        // Create a consensus observer config
+        let consensus_observer_config = ConsensusObserverConfig {
+            max_subscription_timeout_ms: 100_000_000, // Use a large value so that we don't time out
+            ..ConsensusObserverConfig::default()
+        };
+
+        // Create a consensus observer client
+        let network_id = NetworkId::Public;
+        let (peers_and_metadata, consensus_observer_client) =
+            create_consensus_observer_client(&[network_id]);
+
+        // Create a new subscription manager
+        let db_reader = create_mock_db_reader();
+        let time_service = TimeService::mock();
+        let mut subscription_manager = SubscriptionManager::new(
+            consensus_observer_client,
+            consensus_observer_config,
+            None,
+            db_reader.clone(),
+            time_service.clone(),
+        );
+
+        // Add a new connected peer
+        let connected_peer =
+            create_peer_and_connection(network_id, peers_and_metadata.clone(), 1, None);
+
+        // Create a subscription to the new peer
+        create_observer_subscription(
+            &mut subscription_manager,
+            consensus_observer_config,
+            db_reader.clone(),
+            connected_peer,
+            time_service.clone(),
+        );
+
+        // Elapse time to simulate a DB progress error
+        let mock_time_service = time_service.clone().into_mock();
+        mock_time_service.advance(Duration::from_millis(
+            consensus_observer_config.max_synced_version_timeout_ms + 1,
+        ));
+
+        // Check the active subscription and verify that it is removed (the DB is not syncing)
+        assert_matches!(
+            subscription_manager.check_active_subscription(),
+            Err(Error::SubscriptionProgressStopped(_))
+        );
+        assert!(subscription_manager.active_observer_subscription.is_none());
+    }
+
+    #[tokio::test]
+    async fn test_check_active_subscription_timeout() {
+        // Create a consensus observer client
+        let network_id = NetworkId::Public;
+        let (peers_and_metadata, consensus_observer_client) =
+            create_consensus_observer_client(&[network_id]);
+
+        // Create a new subscription manager
+        let consensus_observer_config = ConsensusObserverConfig::default();
+        let db_reader = create_mock_db_reader();
+        let time_service = TimeService::mock();
+        let mut subscription_manager = SubscriptionManager::new(
+            consensus_observer_client,
+            consensus_observer_config,
+            None,
+            db_reader.clone(),
+            time_service.clone(),
+        );
+
+        // Add a new connected peer
+        let connected_peer =
+            create_peer_and_connection(network_id, peers_and_metadata.clone(), 1, None);
+
+        // Create a subscription to the new peer
+        create_observer_subscription(
+            &mut subscription_manager,
+            consensus_observer_config,
+            db_reader.clone(),
+            connected_peer,
+            time_service.clone(),
+        );
+
+        // Elapse time to simulate a timeout
+        let mock_time_service = time_service.clone().into_mock();
+        mock_time_service.advance(Duration::from_millis(
+            consensus_observer_config.max_subscription_timeout_ms + 1,
+        ));
+
+        // Check the active subscription and verify that it is removed (the subscription timed out)
+        assert_matches!(
+            subscription_manager.check_active_subscription(),
+            Err(Error::SubscriptionTimeout(_))
+        );
+        assert!(subscription_manager.active_observer_subscription.is_none());
+    }
+
+    #[tokio::test]
+    async fn test_check_active_subscription_suboptimal() {
+        // Create a consensus observer config
+        let consensus_observer_config = ConsensusObserverConfig {
+            max_subscription_timeout_ms: 100_000_000, // Use a large value so that we don't time out
+            max_synced_version_timeout_ms: 100_000_000, // Use a large value so that we don't get DB progress errors
+            ..ConsensusObserverConfig::default()
+        };
+
+        // Create a consensus observer client
+        let network_id = NetworkId::Validator;
+        let (peers_and_metadata, consensus_observer_client) =
+            create_consensus_observer_client(&[network_id]);
+
+        // Create a new subscription manager
+        let db_reader = create_mock_db_reader();
+        let time_service = TimeService::mock();
+        let mut subscription_manager = SubscriptionManager::new(
+            consensus_observer_client,
+            consensus_observer_config,
+            None,
+            db_reader.clone(),
+            time_service.clone(),
+        );
+
+        // Add an optimal validator peer
+        create_peer_and_connection(network_id, peers_and_metadata.clone(), 0, Some(0.1));
+
+        // Add a suboptimal validator peer
+        let suboptimal_peer =
+            create_peer_and_connection(network_id, peers_and_metadata.clone(), 0, None);
+
+        // Create a new subscription to the suboptimal peer
+        create_observer_subscription(
+            &mut subscription_manager,
+            consensus_observer_config,
+            db_reader.clone(),
+            suboptimal_peer,
+            time_service.clone(),
+        );
+
+        // Elapse enough time to trigger the peer optimality check
+        let mock_time_service = time_service.clone().into_mock();
+        mock_time_service.advance(Duration::from_millis(
+            consensus_observer_config.peer_optimality_check_interval_ms + 1,
+        ));
+
+        // Check the active subscription and verify that it is removed (the peer is suboptimal)
+        assert_matches!(
+            subscription_manager.check_active_subscription(),
+            Err(Error::SubscriptionSuboptimal(_))
+        );
+        assert!(subscription_manager.active_observer_subscription.is_none());
+    }
+
+    #[tokio::test]
+    async fn test_sort_peers_for_subscription() {
+        // Create a consensus observer client
+        let network_ids = &[NetworkId::Validator, NetworkId::Vfn, NetworkId::Public];
+        let (peers_and_metadata, consensus_observer_client) =
+            create_consensus_observer_client(network_ids);
+
+        // Create a new subscription manager
+        let consensus_observer_config = ConsensusObserverConfig::default();
+        let db_reader = create_mock_db_reader();
+        let mut subscription_manager = SubscriptionManager::new(
+            consensus_observer_client,
+            consensus_observer_config,
+            None,
+            db_reader.clone(),
+            TimeService::mock(),
+        );
+
+        // Sort the peers for a subscription and verify that no peers are returned
+        let sorted_peers = subscription_manager
+            .sort_peers_for_subscription(None)
+            .unwrap();
+        assert!(sorted_peers.is_empty());
+
+        // Add a connected validator peer, VFN peer and public peer
+        for network_id in network_ids {
+            let distance_from_validators = match network_id {
+                NetworkId::Validator => 0,
+                NetworkId::Vfn => 1,
+                NetworkId::Public => 2,
+            };
+            create_peer_and_connection(
+                *network_id,
+                peers_and_metadata.clone(),
+                distance_from_validators,
+                None,
+            );
+        }
+
+        // Sort the peers for a subscription and verify the ordering (according to distance)
+        let sorted_peers = subscription_manager
+            .sort_peers_for_subscription(None)
+            .unwrap();
+        assert_eq!(sorted_peers[0].network_id(), NetworkId::Validator);
+        assert_eq!(sorted_peers[1].network_id(), NetworkId::Vfn);
+        assert_eq!(sorted_peers[2].network_id(), NetworkId::Public);
+        assert_eq!(sorted_peers.len(), 3);
+
+        // Sort the peers, but mark the validator as the last subscribed peer
+        let previous_subscription_peer = sorted_peers[0];
+        let sorted_peer_subset = subscription_manager
+            .sort_peers_for_subscription(Some(previous_subscription_peer))
+            .unwrap();
+        assert_eq!(sorted_peer_subset[0].network_id(), NetworkId::Vfn);
+        assert_eq!(sorted_peer_subset[1].network_id(), NetworkId::Public);
+        assert_eq!(sorted_peer_subset.len(), 2);
+
+        // Remove all the peers and verify that no peers are returned
+        for peer_network_id in sorted_peers {
+            remove_peer_and_connection(peers_and_metadata.clone(), peer_network_id);
+        }
+
+        // Add multiple validator peers, with different latencies
+        let mut validator_peers = vec![];
+        for ping_latency_secs in [0.9, 0.8, 0.5, 0.1, 0.05] {
+            let validator_peer = create_peer_and_connection(
+                NetworkId::Validator,
+                peers_and_metadata.clone(),
+                0,
+                Some(ping_latency_secs),
+            );
+            validator_peers.push(validator_peer);
+        }
+
+        // Sort the peers for a subscription and verify the ordering (according to latency)
+        let sorted_peers = subscription_manager
+            .sort_peers_for_subscription(None)
+            .unwrap();
+        let expected_peers = validator_peers.into_iter().rev().collect::<Vec<_>>();
+        assert_eq!(sorted_peers, expected_peers);
+    }
+
+    #[tokio::test]
+    async fn test_verify_message_sender() {
+        // Create a consensus observer client
+        let network_id = NetworkId::Public;
+        let (_, consensus_observer_client) = create_consensus_observer_client(&[network_id]);
+
+        // Create a new subscription manager
+        let consensus_observer_config = ConsensusObserverConfig::default();
+        let db_reader = Arc::new(MockDatabaseReader::new());
+        let mut subscription_manager = SubscriptionManager::new(
+            consensus_observer_client,
+            consensus_observer_config,
+            None,
+            db_reader.clone(),
+            TimeService::mock(),
+        );
+
+        // Check that message verification fails (we have no active subscription)
+        assert!(subscription_manager
+            .verify_message_sender(PeerNetworkId::random())
+            .is_err());
+
+        // Create a new subscription
+        let subscription_peer = PeerNetworkId::random();
+        create_observer_subscription(
+            &mut subscription_manager,
+            consensus_observer_config,
+            db_reader.clone(),
+            subscription_peer,
+            TimeService::mock(),
+        );
+
+        // Check that message verification fails if the peer doesn't match the subscription
+        assert!(subscription_manager
+            .verify_message_sender(PeerNetworkId::random())
+            .is_err());
+
+        // Check that message verification passes if the peer matches the subscription
+        assert!(subscription_manager
+            .verify_message_sender(subscription_peer)
+            .is_ok());
+    }
+
+    /// Creates a new consensus observer client and a peers and metadata container
+    fn create_consensus_observer_client(
+        network_ids: &[NetworkId],
+    ) -> (
+        Arc<PeersAndMetadata>,
+        Arc<ConsensusObserverClient<NetworkClient<ConsensusObserverMessage>>>,
+    ) {
+        let peers_and_metadata = PeersAndMetadata::new(network_ids);
+        let network_client =
+            NetworkClient::new(vec![], vec![], hashmap![], peers_and_metadata.clone());
+        let consensus_observer_client = Arc::new(ConsensusObserverClient::new(network_client));
+
+        (peers_and_metadata, consensus_observer_client)
+    }
+
+    /// Creates a mock DB reader that always returns 0 for the latest version
+    fn create_mock_db_reader() -> Arc<MockDatabaseReader> {
+        let mut mock_db_reader = MockDatabaseReader::new();
+        mock_db_reader
+            .expect_get_latest_ledger_info_version()
+            .returning(move || Ok(0));
+        Arc::new(mock_db_reader)
+    }
+
+    /// Creates a new observer subscription for the specified peer
+    fn create_observer_subscription(
+        subscription_manager: &mut SubscriptionManager,
+        consensus_observer_config: ConsensusObserverConfig,
+        db_reader: Arc<MockDatabaseReader>,
+        subscription_peer: PeerNetworkId,
+        time_service: TimeService,
+    ) {
+        let observer_subscription = ConsensusObserverSubscription::new(
+            consensus_observer_config,
+            db_reader.clone(),
+            subscription_peer,
+            time_service,
+        );
+        subscription_manager.active_observer_subscription = Some(observer_subscription);
+    }
+
+    /// Creates a new peer with the specified connection metadata
+    fn create_peer_and_connection(
+        network_id: NetworkId,
+        peers_and_metadata: Arc<PeersAndMetadata>,
+        distance_from_validators: u64,
+        ping_latency_secs: Option<f64>,
+    ) -> PeerNetworkId {
+        // Create the peer and connection metadata
+        let peer_network_id = PeerNetworkId::new(network_id, PeerId::random());
+        let connection_metadata = ConnectionMetadata::mock(peer_network_id.peer_id());
+
+        // Insert the connection into peers and metadata
+        peers_and_metadata
+            .insert_connection_metadata(peer_network_id, connection_metadata.clone())
+            .unwrap();
+
+        // Update the peer monitoring metadata
+        let latest_network_info_response = NetworkInformationResponse {
+            connected_peers: BTreeMap::new(),
+            distance_from_validators,
+        };
+        let monitoring_metdata = PeerMonitoringMetadata::new(
+            ping_latency_secs,
+            ping_latency_secs,
+            Some(latest_network_info_response),
+            None,
+            None,
+        );
+        peers_and_metadata
+            .update_peer_monitoring_metadata(peer_network_id, monitoring_metdata.clone())
+            .unwrap();
+
+        peer_network_id
+    }
+
+    /// Removes the peer and connection metadata for the given peer
+    fn remove_peer_and_connection(
+        peers_and_metadata: Arc<PeersAndMetadata>,
+        peer_network_id: PeerNetworkId,
+    ) {
+        let peer_metadata = peers_and_metadata
+            .get_metadata_for_peer(peer_network_id)
+            .unwrap();
+        let connection_id = peer_metadata.get_connection_metadata().connection_id;
+        peers_and_metadata
+            .remove_peer_metadata(peer_network_id, connection_id)
+            .unwrap();
+    }
+}
