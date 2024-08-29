254551ef40ec40138c5cdd03e71c8d7fcf487ede
aldenhu <msmouse@gmail.com>
2024-08-29 21:37:26 +0000

    disable pre commit

--- config/src/config/consensus_config.rs
+++ config/src/config/consensus_config.rs
@@ -353,7 +353,7 @@ impl Default for ConsensusConfig {
                 rpc_timeout_ms: 10000,
             },
             num_bounded_executor_tasks: 16,
-            enable_pre_commit: true,
+            enable_pre_commit: false,
         }
     }
 }
