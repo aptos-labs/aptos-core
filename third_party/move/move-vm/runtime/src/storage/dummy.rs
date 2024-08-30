166929766bb8c0157666455228f015659da60820
George Mitenkov <georgemitenk0v@gmail.com>
2024-08-30 10:22:05 +0100

    [loader] Enable V2 implementation as default, V1 uses an env var

--- third_party/move/move-vm/runtime/src/storage/dummy.rs
+++ third_party/move/move-vm/runtime/src/storage/dummy.rs
@@ -1,7 +1,8 @@
 // Copyright (c) The Move Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// TODO(loader_v2): Temporary infra to enable loader V2 to test & run things e2e locally, remove.
-pub fn use_loader_v2_based_on_env() -> bool {
-    std::env::var("USE_LOADER_V2").is_ok()
+// TODO(loader_v2): Temporary infra to still have loader V1 to test, run
+//                  and compare things e2e locally.
+pub fn use_loader_v1_based_on_env() -> bool {
+    std::env::var("USE_LOADER_V1").is_ok()
 }
