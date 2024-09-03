3172f6547f91223b501bdfed13347fbf73f48e13
George Mitenkov <georgemitenk0v@gmail.com>
2024-09-03 21:15:49 +0100

    address comments:
      - Move verified cache into its own file, add nits
      - Move env Loader V1 variable to lib.rs
      - hash() returns a reference
      - deleted commented out cargo dependency in async testsuite

--- third_party/move/move-vm/runtime/src/lib.rs
+++ third_party/move/move-vm/runtime/src/lib.rs
@@ -45,5 +45,10 @@ pub use storage::{
     },
     module_storage::{ambassador_impl_ModuleStorage, ModuleBytesStorage, ModuleStorage},
     publishing::TemporaryModuleStorage,
-    use_loader_v1_based_on_env,
 };
+
+// TODO(loader_v2): Temporary infra to still have loader V1 to test, run
+//                  and compare things e2e locally.
+pub fn use_loader_v1_based_on_env() -> bool {
+    std::env::var("USE_LOADER_V1").is_ok()
+}
