0cd83155bd67cf98d0f8409639e342117b9d4394
George Mitenkov <georgemitenk0v@gmail.com>
2024-09-04 12:53:46 +0100

    address comments:
    - rollback shifted indices in favour of version pub type alias
    - add captured read module storage cache
    - minor nits

--- aptos-move/block-executor/src/view.rs
+++ aptos-move/block-executor/src/view.rs
@@ -1582,7 +1582,7 @@ impl<'a, T: Transaction, S: TStateView<Key = T::Key>, X: Executable> TModuleView
                     .insert(state_key.clone());
                 state
                     .unsync_map
-                    .fetch_module_for_loader_v2(state_key)
+                    .fetch_module_for_loader_v1(state_key)
                     .map_or_else(
                         || self.get_raw_base_value(state_key),
                         |v| Ok(v.as_state_value()),
