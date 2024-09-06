d5de09a98f81a410d898d36e7cfc970e55b73e79
Vineeth Kashyap <vineeth.rv@gmail.com>
2024-09-06 13:34:27 -0400

    [compiler-v2][lint] Needless reference in field access (#14542)

--- third_party/move/move-compiler-v2/src/lint_common.rs
+++ third_party/move/move-compiler-v2/src/lint_common.rs
@@ -20,6 +20,7 @@ pub enum LintChecker {
     AvoidCopyOnIdentityComparison,
     BlocksInConditions,
     NeedlessBool,
+    NeedlessRefInFieldAccess,
     SimplerNumericExpression,
     UnnecessaryBooleanIdentityComparison,
     UnnecessaryNumericalExtremeComparison,
