8f4963c5bc96e03830d1bc025d25f2d677d5535a
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2024-09-11 11:40:37 -0700

    Sync Terraform & Helm changes
    GitOrigin-RevId: fe824dfdfa1dd4543969154c974702f04a34b3b8

--- terraform/aptos-node-testnet/gcp/variables.tf
+++ terraform/aptos-node-testnet/gcp/variables.tf
@@ -218,7 +218,7 @@ variable "utility_instance_type" {
 variable "validator_instance_type" {
   description = "Instance type used for validator and fullnodes"
   type        = string
-  default     = "t2d-standard-16"
+  default     = "t2d-standard-60"
 }
 
 variable "utility_instance_enable_taint" {
