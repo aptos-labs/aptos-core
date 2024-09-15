14d4c3548c4e51fff4947801e667f774d981e91d
Satya Vusirikala <satyasatya123456@gmail.com>
2024-09-14 17:42:09 -0700

    Resetting unnecessary changes

--- consensus/src/quorum_store/network_listener.rs
+++ consensus/src/quorum_store/network_listener.rs
@@ -39,75 +39,75 @@ impl NetworkListener {
 
     pub async fn start(mut self) {
         info!("QS: starting networking");
-        loop {
-            tokio::select! {
-                Some(msg) = self.network_msg_rx.next() => monitor!("qs_network_listener_main_loop", {
-                    match msg {
-                        // TODO: does the assumption have to be that network listener is shutdown first?
-                        VerifiedEvent::Shutdown(ack_tx) => {
-                            counters::QUORUM_STORE_MSG_COUNT
-                              .with_label_values(&["NetworkListener::shutdown"])
-                              .inc();
-                            info!("QS: shutdown network listener received");
-                            ack_tx
-                                .send(())
-                                .expect("Failed to send shutdown ack to QuorumStore");
-                            break;
-                        },
-                        VerifiedEvent::SignedBatchInfo(signed_batch_infos) => {
-                            counters::QUORUM_STORE_MSG_COUNT
-                                .with_label_values(&["NetworkListener::signedbatchinfo"])
-                                .inc();
-                            let cmd = ProofCoordinatorCommand::AppendSignature((*signed_batch_infos, true));
-                            self.proof_coordinator_tx
-                                .send(cmd)
-                                .await
-                                .expect("Could not send signed_batch_info to proof_coordinator");
-                        },
-                        VerifiedEvent::UnverifiedSignedBatchInfo(signed_batch_infos) => {
-                            counters::QUORUM_STORE_MSG_COUNT
-                                .with_label_values(&["NetworkListener::signedbatchinfo"])
-                                .inc();
-                            let cmd = ProofCoordinatorCommand::AppendSignature((*signed_batch_infos, false));
-                            self.proof_coordinator_tx
-                                .send(cmd)
-                                .await
-                                .expect("Could not send signed_batch_info to proof_coordinator");
-                        },
-                        VerifiedEvent::BatchMsg(batch_msg) => {
-                            let author = batch_msg.author();
-                            let batches = batch_msg.take();
-                            counters::RECEIVED_BATCH_MSG_COUNT.inc();
+        while let Some(msg) = self.network_msg_rx.next().await {
+            monitor!("qs_network_listener_main_loop", {
+                match msg {
+                    // TODO: does the assumption have to be that network listener is shutdown first?
+                    VerifiedEvent::Shutdown(ack_tx) => {
+                        counters::QUORUM_STORE_MSG_COUNT
+                            .with_label_values(&["NetworkListener::shutdown"])
+                            .inc();
+                        info!("QS: shutdown network listener received");
+                        ack_tx
+                            .send(())
+                            .expect("Failed to send shutdown ack to QuorumStore");
+                        break;
+                    },
+                    VerifiedEvent::SignedBatchInfo(signed_batch_infos) => {
+                        counters::QUORUM_STORE_MSG_COUNT
+                            .with_label_values(&["NetworkListener::signedbatchinfo"])
+                            .inc();
+                        let cmd =
+                            ProofCoordinatorCommand::AppendSignature((*signed_batch_infos, true));
+                        self.proof_coordinator_tx
+                            .send(cmd)
+                            .await
+                            .expect("Could not send signed_batch_info to proof_coordinator");
+                    },
+                    VerifiedEvent::UnverifiedSignedBatchInfo(signed_batch_infos) => {
+                        counters::QUORUM_STORE_MSG_COUNT
+                            .with_label_values(&["NetworkListener::signedbatchinfo"])
+                            .inc();
+                        let cmd =
+                            ProofCoordinatorCommand::AppendSignature((*signed_batch_infos, false));
+                        self.proof_coordinator_tx
+                            .send(cmd)
+                            .await
+                            .expect("Could not send signed_batch_info to proof_coordinator");
+                    },
+                    VerifiedEvent::BatchMsg(batch_msg) => {
+                        let author = batch_msg.author();
+                        let batches = batch_msg.take();
+                        counters::RECEIVED_BATCH_MSG_COUNT.inc();
 
-                            let idx =
-                                author.to_vec()[0] as usize % self.remote_batch_coordinator_tx.len();
-                            trace!(
-                                "QS: peer_id {:?},  # network_worker {}, hashed to idx {}",
-                                author,
-                                self.remote_batch_coordinator_tx.len(),
-                                idx
-                            );
-                            self.remote_batch_coordinator_tx[idx]
-                                .send(BatchCoordinatorCommand::NewBatches(author, batches))
-                                .await
-                                .expect("Could not send remote batch");
-                        },
-                        VerifiedEvent::ProofOfStoreMsg(proofs) => {
-                            counters::QUORUM_STORE_MSG_COUNT
-                              .with_label_values(&["NetworkListener::proofofstore"])
-                              .inc();
-                            let cmd = ProofManagerCommand::ReceiveProofs(*proofs);
-                            self.proof_manager_tx
-                                .send(cmd)
-                                .await
-                                .expect("could not push Proof proof_of_store");
-                        },
-                        _ => {
-                            unreachable!()
-                        },
-                    }
-                }),
-            }
+                        let idx =
+                            author.to_vec()[0] as usize % self.remote_batch_coordinator_tx.len();
+                        trace!(
+                            "QS: peer_id {:?},  # network_worker {}, hashed to idx {}",
+                            author,
+                            self.remote_batch_coordinator_tx.len(),
+                            idx
+                        );
+                        self.remote_batch_coordinator_tx[idx]
+                            .send(BatchCoordinatorCommand::NewBatches(author, batches))
+                            .await
+                            .expect("Could not send remote batch");
+                    },
+                    VerifiedEvent::ProofOfStoreMsg(proofs) => {
+                        counters::QUORUM_STORE_MSG_COUNT
+                            .with_label_values(&["NetworkListener::proofofstore"])
+                            .inc();
+                        let cmd = ProofManagerCommand::ReceiveProofs(*proofs);
+                        self.proof_manager_tx
+                            .send(cmd)
+                            .await
+                            .expect("could not push Proof proof_of_store");
+                    },
+                    _ => {
+                        unreachable!()
+                    },
+                }
+            });
         }
     }
 }
