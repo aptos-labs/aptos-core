4f9df1c8623be3d0d3f561a49c47ff74b6b3530d
Josh Lind <josh.lind@hotmail.com>
2024-09-20 20:20:24 -0400

    [State Sync] Handle sync_for_duration() requests.

--- state-sync/state-sync-driver/src/tests/continuous_syncer.rs
+++ state-sync/state-sync-driver/src/tests/continuous_syncer.rs
@@ -19,7 +19,9 @@ use crate::{
     utils::OutputFallbackHandler,
 };
 use aptos_config::config::ContinuousSyncingMode;
-use aptos_consensus_notifications::ConsensusSyncTargetNotification;
+use aptos_consensus_notifications::{
+    ConsensusSyncDurationNotification, ConsensusSyncTargetNotification,
+};
 use aptos_data_streaming_service::{
     data_notification::{DataNotification, DataPayload, NotificationId},
     streaming_client::{NotificationAndFeedback, NotificationFeedback},
@@ -31,7 +33,10 @@ use aptos_types::transaction::{TransactionOutputListWithProof, Version};
 use claims::assert_matches;
 use futures::SinkExt;
 use mockall::{predicate::eq, Sequence};
-use std::{sync::Arc, time::Duration};
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 #[tokio::test]
 async fn test_critical_timeout() {
@@ -111,7 +116,89 @@ async fn test_critical_timeout() {
 }
 
 #[tokio::test]
-async fn test_data_stream_transactions_with_target() {
+async fn test_data_stream_transactions_with_sync_duration() {
+    // Create test data
+    let current_synced_epoch = 10;
+    let current_synced_version = 1000;
+    let notification_id = 900;
+
+    // Create a driver configuration
+    let mut driver_configuration = create_full_node_driver_configuration();
+    driver_configuration.config.continuous_syncing_mode =
+        ContinuousSyncingMode::ExecuteTransactions;
+
+    // Create the mock streaming client
+    let mut mock_streaming_client = create_mock_streaming_client();
+    let mut expectation_sequence = Sequence::new();
+    let (mut notification_sender_1, data_stream_listener_1) = create_data_stream_listener();
+    let (_notification_sender_2, data_stream_listener_2) = create_data_stream_listener();
+    let data_stream_id_1 = data_stream_listener_1.data_stream_id;
+    for data_stream_listener in [data_stream_listener_1, data_stream_listener_2] {
+        mock_streaming_client
+            .expect_continuously_stream_transactions()
+            .times(1)
+            .with(
+                eq(current_synced_version),
+                eq(current_synced_epoch),
+                eq(false),
+                eq(None),
+            )
+            .return_once(move |_, _, _, _| Ok(data_stream_listener))
+            .in_sequence(&mut expectation_sequence);
+    }
+    mock_streaming_client
+        .expect_terminate_stream_with_feedback()
+        .with(
+            eq(data_stream_id_1),
+            eq(Some(NotificationAndFeedback::new(
+                notification_id,
+                NotificationFeedback::EmptyPayloadData,
+            ))),
+        )
+        .return_const(Ok(()));
+
+    // Create the continuous syncer
+    let (mut continuous_syncer, _) = create_continuous_syncer(
+        driver_configuration,
+        mock_streaming_client,
+        None,
+        true,
+        current_synced_version,
+        current_synced_epoch,
+    );
+
+    // Drive progress to initialize the transaction output stream for the sync duration
+    let (sync_duration_notification, _) =
+        ConsensusSyncDurationNotification::new(Duration::from_secs(1));
+    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new_with_duration(
+        Instant::now(),
+        sync_duration_notification,
+    ))));
+    drive_progress(&mut continuous_syncer, &sync_request).await;
+
+    // Send an invalid output along the stream
+    let data_notification = DataNotification::new(
+        notification_id,
+        DataPayload::ContinuousTransactionOutputsWithProof(
+            create_epoch_ending_ledger_info(),
+            TransactionOutputListWithProof::new_empty(),
+        ),
+    );
+    notification_sender_1.send(data_notification).await.unwrap();
+
+    // Drive progress again and ensure we get a verification error
+    let error = continuous_syncer
+        .drive_progress(sync_request.clone())
+        .await
+        .unwrap_err();
+    assert_matches!(error, Error::VerificationError(_));
+
+    // Drive progress to initialize the transaction output stream.
+    drive_progress(&mut continuous_syncer, &sync_request).await;
+}
+
+#[tokio::test]
+async fn test_data_stream_transactions_with_sync_target() {
     // Create test data
     let current_synced_epoch = 5;
     let current_synced_version = 234;
@@ -163,9 +250,9 @@ async fn test_data_stream_transactions_with_target() {
         current_synced_epoch,
     );
 
-    // Drive progress to initialize the transaction output stream
+    // Drive progress to initialize the transaction output stream for the sync target
     let (sync_target_notification, _) = ConsensusSyncTargetNotification::new(target_ledger_info);
-    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new(
+    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new_with_target(
         sync_target_notification,
     ))));
     drive_progress(&mut continuous_syncer, &sync_request).await;
@@ -187,7 +274,7 @@ async fn test_data_stream_transactions_with_target() {
         .unwrap_err();
     assert_matches!(error, Error::VerificationError(_));
 
-    // Drive progress to initialize the transaction output stream.
+    // Drive progress to initialize the transaction output stream
     drive_progress(&mut continuous_syncer, &sync_request).await;
 }
 
@@ -242,7 +329,7 @@ async fn test_data_stream_transaction_outputs() {
         current_synced_epoch,
     );
 
-    // Drive progress to initialize the transaction output stream
+    // Drive progress to initialize the transaction output stream (without a sync target)
     let no_sync_request = Arc::new(Mutex::new(None));
     drive_progress(&mut continuous_syncer, &no_sync_request).await;
 
@@ -271,7 +358,89 @@ async fn test_data_stream_transaction_outputs() {
 }
 
 #[tokio::test]
-async fn test_data_stream_transactions_or_outputs_with_target() {
+async fn test_data_stream_transactions_or_outputs_with_sync_duration() {
+    // Create test data
+    let current_synced_epoch = 100;
+    let current_synced_version = 1000;
+    let notification_id = 100;
+
+    // Create a driver configuration with a genesis waypoint and transactions or output syncing
+    let mut driver_configuration = create_full_node_driver_configuration();
+    driver_configuration.config.continuous_syncing_mode =
+        ContinuousSyncingMode::ExecuteTransactionsOrApplyOutputs;
+
+    // Create the mock streaming client
+    let mut mock_streaming_client = create_mock_streaming_client();
+    let mut expectation_sequence = Sequence::new();
+    let (mut notification_sender_1, data_stream_listener_1) = create_data_stream_listener();
+    let (_notification_sender_2, data_stream_listener_2) = create_data_stream_listener();
+    let data_stream_id_1 = data_stream_listener_1.data_stream_id;
+    for data_stream_listener in [data_stream_listener_1, data_stream_listener_2] {
+        mock_streaming_client
+            .expect_continuously_stream_transactions_or_outputs()
+            .times(1)
+            .with(
+                eq(current_synced_version),
+                eq(current_synced_epoch),
+                eq(false),
+                eq(None),
+            )
+            .return_once(move |_, _, _, _| Ok(data_stream_listener))
+            .in_sequence(&mut expectation_sequence);
+    }
+    mock_streaming_client
+        .expect_terminate_stream_with_feedback()
+        .with(
+            eq(data_stream_id_1),
+            eq(Some(NotificationAndFeedback::new(
+                notification_id,
+                NotificationFeedback::EmptyPayloadData,
+            ))),
+        )
+        .return_const(Ok(()));
+
+    // Create the continuous syncer
+    let (mut continuous_syncer, _) = create_continuous_syncer(
+        driver_configuration,
+        mock_streaming_client,
+        None,
+        true,
+        current_synced_version,
+        current_synced_epoch,
+    );
+
+    // Drive progress to initialize the transaction output stream for the sync duration
+    let (sync_duration_notification, _) =
+        ConsensusSyncDurationNotification::new(Duration::from_secs(1));
+    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new_with_duration(
+        Instant::now(),
+        sync_duration_notification,
+    ))));
+    drive_progress(&mut continuous_syncer, &sync_request).await;
+
+    // Send an invalid output along the stream
+    let data_notification = DataNotification::new(
+        notification_id,
+        DataPayload::ContinuousTransactionOutputsWithProof(
+            create_epoch_ending_ledger_info(),
+            TransactionOutputListWithProof::new_empty(),
+        ),
+    );
+    notification_sender_1.send(data_notification).await.unwrap();
+
+    // Drive progress again and ensure we get a verification error
+    let error = continuous_syncer
+        .drive_progress(sync_request.clone())
+        .await
+        .unwrap_err();
+    assert_matches!(error, Error::VerificationError(_));
+
+    // Drive progress to initialize the transaction output stream
+    drive_progress(&mut continuous_syncer, &sync_request).await;
+}
+
+#[tokio::test]
+async fn test_data_stream_transactions_or_outputs_with_sync_target() {
     // Create test data
     let current_synced_epoch = 5;
     let current_synced_version = 234;
@@ -323,9 +492,9 @@ async fn test_data_stream_transactions_or_outputs_with_target() {
         current_synced_epoch,
     );
 
-    // Drive progress to initialize the transaction output stream
+    // Drive progress to initialize the transaction output stream for the sync target
     let (sync_target_notification, _) = ConsensusSyncTargetNotification::new(target_ledger_info);
-    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new(
+    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new_with_target(
         sync_target_notification,
     ))));
     drive_progress(&mut continuous_syncer, &sync_request).await;
@@ -352,7 +521,138 @@ async fn test_data_stream_transactions_or_outputs_with_target() {
 }
 
 #[tokio::test]
-async fn test_data_stream_transactions_or_outputs_with_target_fallback() {
+async fn test_data_stream_transactions_or_outputs_with_sync_duration_fallback() {
+    // Create test data
+    let current_synced_epoch = 50;
+    let current_synced_version = 1234;
+    let notification_id = 101;
+
+    // Create a driver configuration with a genesis waypoint and transactions or output syncing
+    let mut driver_configuration = create_full_node_driver_configuration();
+    driver_configuration.config.continuous_syncing_mode =
+        ContinuousSyncingMode::ExecuteTransactionsOrApplyOutputs;
+
+    // Create the mock streaming client
+    let mut mock_streaming_client = create_mock_streaming_client();
+
+    // Set expectations for stream creations and terminations
+    let mut expectation_sequence = Sequence::new();
+    let (_notification_sender_1, data_stream_listener_1) = create_data_stream_listener();
+    let data_stream_id_1 = data_stream_listener_1.data_stream_id;
+    let (_notification_sender_2, data_stream_listener_2) = create_data_stream_listener();
+    let data_stream_id_2 = data_stream_listener_2.data_stream_id;
+    let (_notification_sender_3, data_stream_listener_3) = create_data_stream_listener();
+    mock_streaming_client
+        .expect_continuously_stream_transactions_or_outputs()
+        .times(1)
+        .with(
+            eq(current_synced_version),
+            eq(current_synced_epoch),
+            eq(false),
+            eq(None),
+        )
+        .return_once(move |_, _, _, _| Ok(data_stream_listener_1))
+        .in_sequence(&mut expectation_sequence);
+    mock_streaming_client
+        .expect_terminate_stream_with_feedback()
+        .times(1)
+        .with(
+            eq(data_stream_id_1),
+            eq(Some(NotificationAndFeedback::new(
+                notification_id,
+                NotificationFeedback::PayloadProofFailed,
+            ))),
+        )
+        .return_const(Ok(()))
+        .in_sequence(&mut expectation_sequence);
+    mock_streaming_client
+        .expect_continuously_stream_transaction_outputs()
+        .times(1)
+        .with(
+            eq(current_synced_version),
+            eq(current_synced_epoch),
+            eq(None),
+        )
+        .return_once(move |_, _, _| Ok(data_stream_listener_2))
+        .in_sequence(&mut expectation_sequence);
+    mock_streaming_client
+        .expect_terminate_stream_with_feedback()
+        .times(1)
+        .with(
+            eq(data_stream_id_2),
+            eq(Some(NotificationAndFeedback::new(
+                notification_id,
+                NotificationFeedback::InvalidPayloadData,
+            ))),
+        )
+        .return_const(Ok(()))
+        .in_sequence(&mut expectation_sequence);
+    mock_streaming_client
+        .expect_continuously_stream_transactions_or_outputs()
+        .times(1)
+        .with(
+            eq(current_synced_version),
+            eq(current_synced_epoch),
+            eq(false),
+            eq(None),
+        )
+        .return_once(move |_, _, _, _| Ok(data_stream_listener_3))
+        .in_sequence(&mut expectation_sequence);
+
+    // Create the continuous syncer
+    let time_service = TimeService::mock();
+    let (mut continuous_syncer, mut output_fallback_handler) = create_continuous_syncer(
+        driver_configuration.clone(),
+        mock_streaming_client,
+        Some(time_service.clone()),
+        true,
+        current_synced_version,
+        current_synced_epoch,
+    );
+    assert!(!output_fallback_handler.in_fallback_mode());
+
+    // Drive progress to initialize the transactions or output stream for the sync duration
+    let (sync_duration_notification, _) =
+        ConsensusSyncDurationNotification::new(Duration::from_secs(1));
+    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new_with_duration(
+        Instant::now(),
+        sync_duration_notification,
+    ))));
+    drive_progress(&mut continuous_syncer, &sync_request).await;
+
+    // Send a storage synchronizer error to the continuous syncer so that it falls back
+    // to output syncing and drive progress for the new stream type.
+    handle_storage_synchronizer_error(
+        &mut continuous_syncer,
+        notification_id,
+        NotificationFeedback::PayloadProofFailed,
+    )
+    .await;
+    drive_progress(&mut continuous_syncer, &sync_request).await;
+    assert!(output_fallback_handler.in_fallback_mode());
+
+    // Elapse enough time so that fallback mode is now disabled
+    time_service
+        .into_mock()
+        .advance_async(Duration::from_secs(
+            driver_configuration.config.fallback_to_output_syncing_secs,
+        ))
+        .await;
+
+    // Send another storage synchronizer error to the bootstrapper and drive progress
+    // so that a regular stream is created.
+    handle_storage_synchronizer_error(
+        &mut continuous_syncer,
+        notification_id,
+        NotificationFeedback::InvalidPayloadData,
+    )
+    .await;
+    drive_progress(&mut continuous_syncer, &sync_request).await;
+    assert!(!output_fallback_handler.in_fallback_mode());
+}
+
+#[tokio::test]
+async fn test_data_stream_transactions_or_outputs_with_sync_target_fallback() {
     // Create test data
     let current_synced_epoch = 5;
     let current_synced_version = 234;
@@ -445,7 +745,7 @@ async fn test_data_stream_transactions_or_outputs_with_target_fallback() {
 
     // Drive progress to initialize the transactions or output stream
     let (sync_target_notification, _) = ConsensusSyncTargetNotification::new(target_ledger_info);
-    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new(
+    let sync_request = Arc::new(Mutex::new(Some(ConsensusSyncRequest::new_with_target(
         sync_target_notification,
     ))));
     drive_progress(&mut continuous_syncer, &sync_request).await;
