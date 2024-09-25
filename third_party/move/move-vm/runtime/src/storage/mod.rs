78be70792a06a651dd15ee020ae858ceabc40df0
George Mitenkov <georgemitenk0v@gmail.com>
2024-09-25 13:25:50 +0100

    [move] Re-implement type cache for loaders

--- third_party/move/move-vm/runtime/src/storage/mod.rs
+++ third_party/move/move-vm/runtime/src/storage/mod.rs
@@ -3,12 +3,12 @@
 
 pub(crate) mod loader;
 pub(crate) mod struct_name_index_map;
+pub(crate) mod ty_cache;
+mod verified_module_cache;
 
 pub mod code_storage;
 pub mod environment;
-pub mod module_storage;
-pub mod verifier;
-
 pub mod implementations;
+pub mod module_storage;
 pub mod publishing;
-mod verified_module_cache;
+pub mod verifier;
