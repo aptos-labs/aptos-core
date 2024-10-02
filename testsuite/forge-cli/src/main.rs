f9a86e0de56d973de682207ab344f50561be89e6
danielxiangzl <xiangzhuolun@gmail.com>
2024-10-02 16:28:55 -0700

    fix

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -1117,7 +1117,7 @@ fn background_traffic_for_sweep_with_latency(criteria: &[(f32, f32)]) -> Option<
 }
 
 fn realistic_env_load_sweep_test() -> ForgeConfig {
-    realistic_env_sweep_wrap(20, 5, LoadVsPerfBenchmark {
+    realistic_env_sweep_wrap(20, 10, LoadVsPerfBenchmark {
         test: Box::new(PerformanceBenchmark),
         workloads: Workloads::TPS(vec![10000]),
         criteria: [
