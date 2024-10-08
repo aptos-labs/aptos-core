cb54fd5b00b128b7aae046dc3951593286777eb4
yuunlimm <dla0267@gmail.com>
2024-10-08 13:31:49 -0700

    add readme and update comments

--- ecosystem/indexer-grpc/indexer-test-transactions/build.rs
+++ ecosystem/indexer-grpc/indexer-test-transactions/build.rs
@@ -8,7 +8,9 @@ const IMPORTED_TESTNET_TXNS: &str = "imported_testnet_txns";
 const SCRIPTED_TRANSACTIONS_TXNS: &str = "scripted_transactions";
 #[derive(Default)]
 pub struct TransactionCodeBuilder {
+    // Holds the generated Rust code for transaction constants
     transactions_code: String,
+    // Holds the match arms for the name generation function for scripted txns (optional)
     name_function_code: String,
 }
 
@@ -26,10 +28,12 @@ impl TransactionCodeBuilder {
         let json_dir = Path::new("json_transactions").join(dir_name);
         let mut all_constants = String::new();
 
+        // Iterates over all files in the directory
         for entry in fs::read_dir(json_dir).expect("Failed to read directory") {
             let entry = entry.expect("Failed to get directory entry");
             let path = entry.path();
 
+            // Checks if the file has a `.json` extension
             if path.extension().and_then(|s| s.to_str()) == Some("json") {
                 let file_name = path.file_stem().unwrap().to_str().unwrap();
                 let const_name = format!(
@@ -38,6 +42,7 @@ impl TransactionCodeBuilder {
                     file_name.to_uppercase().replace('-', "_")
                 );
 
+                // Generates a constant for the JSON file and appends it to the `transactions_code` string
                 self.transactions_code.push_str(&format!(
                     r#"
                     pub const {const_name}: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/json_transactions/{dir_name}/{file_name}.json"));
@@ -47,8 +52,10 @@ impl TransactionCodeBuilder {
                     file_name = file_name,
                 ));
 
+                // Adds the constant to the list of all constants
                 all_constants.push_str(&format!("{},", const_name));
 
+                // If name function generation is requested, adds the corresponding match arm
                 if generate_name_function {
                     self.name_function_code.push_str(&format!(
                         "        {const_name} => Some(\"{file_name}\"),\n",
@@ -59,6 +66,7 @@ impl TransactionCodeBuilder {
             }
         }
 
+        // If any constants were created, generate an array holding all of them
         if !all_constants.is_empty() {
             self.transactions_code.push_str(&format!(
                 "pub const ALL_{}: &[&[u8]] = &[{}];\n",
@@ -70,6 +78,7 @@ impl TransactionCodeBuilder {
         self
     }
 
+    // Adds the transaction name lookup function if any name match arms were created
     pub fn add_transaction_name_function(mut self) -> Self {
         if !self.name_function_code.is_empty() {
             self.transactions_code.push_str(
