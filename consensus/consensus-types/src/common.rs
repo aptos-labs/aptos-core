a7b55f9ba16497a690391932ca315f1017e5500c
Balaji Arun <balaji@aptoslabs.com>
2024-10-09 09:17:39 -0700

    allow handling OptQS payload by default

--- consensus/consensus-types/src/common.rs
+++ consensus/consensus-types/src/common.rs
@@ -520,8 +520,7 @@ impl Payload {
             (true, Payload::OptQuorumStore(opt_quorum_store)) => {
                 let proof_with_data = opt_quorum_store.proof_with_data();
                 Self::verify_with_cache(&proof_with_data.batch_summary, validator, proof_cache)?;
-                // TODO(ibalajiarun): Remove this log when OptQS is enabled.
-                bail!("OptQuorumStore Payload is not expected yet");
+                Ok(())
             },
             (_, _) => Err(anyhow::anyhow!(
                 "Wrong payload type. Expected Payload::InQuorumStore {} got {} ",
