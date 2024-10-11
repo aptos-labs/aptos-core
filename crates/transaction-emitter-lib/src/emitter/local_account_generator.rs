e87c92c564c3e9982371cf8b382e88722ff6fa9f
Oliver <heliuchuan@gmail.com>
2024-10-11 05:20:27 -0400

    commit

--- crates/transaction-emitter-lib/src/emitter/local_account_generator.rs
+++ crates/transaction-emitter-lib/src/emitter/local_account_generator.rs
@@ -2,9 +2,16 @@ use anyhow::bail;
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 use aptos_crypto::ed25519::Ed25519PrivateKey;
-use aptos_sdk::types::{AccountKey, EphemeralKeyPair, KeylessAccount, LocalAccount};
+use aptos_crypto::CryptoMaterialError;
+use aptos_sdk::types::{
+    extract_claims_from_jwt, extract_header_json_from_jwt, AccountKey, EphemeralKeyPair,
+    EphemeralPrivateKey, FederatedKeylessAccount, KeylessAccount, LocalAccount,
+};
 use aptos_transaction_generator_lib::ReliableTransactionSubmitter;
-use aptos_types::keyless::{Claims, OpenIdSig, Pepper, ZeroKnowledgeSig};
+use aptos_types::{
+    account_address::AccountAddress,
+    keyless::{Claims, OpenIdSig, Pepper, ZeroKnowledgeSig},
+};
 use async_trait::async_trait;
 use futures::future::try_join_all;
 use rand::rngs::StdRng;
@@ -32,13 +39,11 @@ pub fn create_keyless_account_generator(
     epk_expiry_date_secs: u64,
     jwt: &str,
     proof_file_path: Option<&str>,
+    jwk_addr: Option<AccountAddress>,
 ) -> anyhow::Result<Box<dyn LocalAccountGenerator>> {
-    let parts: Vec<&str> = jwt.split('.').collect();
-    let header_bytes = base64::decode(parts[0]).unwrap();
-    let jwt_header_json = String::from_utf8(header_bytes).unwrap();
-    let jwt_payload_json = base64::decode_config(parts[1], base64::URL_SAFE).unwrap();
-    let claims: Claims = serde_json::from_slice(&jwt_payload_json)?;
-    Ok(Box::new(KeylessAccountGenerator {
+    let jwt_header_json = extract_header_json_from_jwt(jwt)?;
+    let claims: Claims = extract_claims_from_jwt(jwt)?;
+    let generator = KeylessAccountGenerator {
         proof_file_path: proof_file_path.map(|s| s.to_string()),
         ephemeral_secret_key,
         epk_expiry_date_secs,
@@ -47,7 +52,15 @@ pub fn create_keyless_account_generator(
         uid_key: "sub".to_owned(),
         uid_val: claims.oidc_claims.sub,
         jwt_header_json,
-    }))
+    };
+    if let Some(jwk_addr) = jwk_addr {
+        Ok(Box::new(FederatedKeylessAccountGenerator {
+            generator,
+            jwk_addr,
+        }))
+    } else {
+        Ok(Box::new(NormalKeylessAccountGenerator { generator }))
+    }
 }
 
 pub struct PrivateKeyAccountGenerator;
@@ -90,6 +103,88 @@ impl LocalAccountGenerator for PrivateKeyAccountGenerator {
     }
 }
 
+pub struct NormalKeylessAccountGenerator {
+    generator: KeylessAccountGenerator,
+}
+
+#[async_trait]
+impl LocalAccountGenerator for NormalKeylessAccountGenerator {
+    async fn gen_local_accounts(
+        &self,
+        txn_executor: &dyn ReliableTransactionSubmitter,
+        num_accounts: usize,
+        _rng: &mut StdRng,
+    ) -> anyhow::Result<Vec<LocalAccount>> {
+        self.generator
+            .generate_accounts(
+                txn_executor,
+                num_accounts,
+                |i, zk_sig, ephemeral_key_pair| {
+                    KeylessAccount::new(
+                        &self.generator.iss,
+                        &self.generator.aud,
+                        &self.generator.uid_key,
+                        &self.generator.uid_val,
+                        &self.generator.jwt_header_json,
+                        ephemeral_key_pair,
+                        Pepper::from_number(i.try_into().unwrap()),
+                        zk_sig,
+                    )
+                    .map(|account| {
+                        LocalAccount::new_keyless(
+                            account.authentication_key().account_address(),
+                            account,
+                            0,
+                        )
+                    })
+                },
+            )
+            .await
+    }
+}
+
+pub struct FederatedKeylessAccountGenerator {
+    generator: KeylessAccountGenerator,
+    jwk_addr: AccountAddress,
+}
+
+#[async_trait]
+impl LocalAccountGenerator for FederatedKeylessAccountGenerator {
+    async fn gen_local_accounts(
+        &self,
+        txn_executor: &dyn ReliableTransactionSubmitter,
+        num_accounts: usize,
+        _rng: &mut StdRng,
+    ) -> anyhow::Result<Vec<LocalAccount>> {
+        self.generator
+            .generate_accounts(
+                txn_executor,
+                num_accounts,
+                |i, zk_sig, ephemeral_key_pair| {
+                    FederatedKeylessAccount::new(
+                        &self.generator.iss,
+                        &self.generator.aud,
+                        &self.generator.uid_key,
+                        &self.generator.uid_val,
+                        &self.generator.jwt_header_json,
+                        ephemeral_key_pair,
+                        Pepper::from_number(i.try_into().unwrap()),
+                        zk_sig,
+                        self.jwk_addr,
+                    )
+                    .map(|account| {
+                        LocalAccount::new_federated_keyless(
+                            account.authentication_key().account_address(),
+                            account,
+                            0,
+                        )
+                    })
+                },
+            )
+            .await
+    }
+}
+
 pub struct KeylessAccountGenerator {
     proof_file_path: Option<String>,
     ephemeral_secret_key: Ed25519PrivateKey,
@@ -101,56 +196,74 @@ pub struct KeylessAccountGenerator {
     jwt_header_json: String,
 }
 
-#[async_trait]
-impl LocalAccountGenerator for KeylessAccountGenerator {
-    async fn gen_local_accounts(
-        &self,
-        txn_executor: &dyn ReliableTransactionSubmitter,
-        num_accounts: usize,
-        _rng: &mut StdRng,
-    ) -> anyhow::Result<Vec<LocalAccount>> {
-        let mut keyless_accounts = vec![];
-        let mut addresses = vec![];
-        let mut i = 0;
-        let lines: Box<dyn Iterator<Item = Result<String, io::Error>>> = match &self.proof_file_path
-        {
+impl KeylessAccountGenerator {
+    fn load_lines(&self) -> Box<dyn Iterator<Item = Result<String, io::Error>>> {
+        match &self.proof_file_path {
             None => {
                 let proofs = include_str!("test_proofs_for_localnet_txn_emitter.txt");
-                let lines = proofs.lines().map(|line| Ok(line.to_string()));
-                Box::new(lines)
+                Box::new(proofs.lines().map(|line| Ok(line.to_string())))
             },
             Some(path) => {
                 let file = File::open(path).unwrap();
                 let reader = io::BufReader::new(file);
                 Box::new(reader.lines())
             },
-        };
+        }
+    }
+
+    fn create_ephemeral_key(&self) -> Result<EphemeralPrivateKey, CryptoMaterialError> {
+        let serialized: &[u8] = &(self.ephemeral_secret_key.to_bytes());
+        Ok(EphemeralPrivateKey::Ed25519 {
+            inner_private_key: Ed25519PrivateKey::try_from(serialized)?,
+        })
+    }
+
+    async fn set_sequence_numbers(
+        txn_executor: &dyn ReliableTransactionSubmitter,
+        accounts: Vec<LocalAccount>,
+    ) -> anyhow::Result<Vec<LocalAccount>> {
+        let result_futures = accounts
+            .iter()
+            .map(|account| txn_executor.query_sequence_number(account.address()))
+            .collect::<Vec<_>>();
+        let seq_nums: Vec<_> = try_join_all(result_futures).await?.into_iter().collect();
+
+        Ok(accounts
+            .into_iter()
+            .zip(seq_nums)
+            .map(|(account, sequence_number)| {
+                account.set_sequence_number(sequence_number);
+                account
+            })
+            .collect())
+    }
+
+    async fn generate_accounts<F>(
+        &self,
+        txn_executor: &dyn ReliableTransactionSubmitter,
+        num_accounts: usize,
+        create_account: F,
+    ) -> anyhow::Result<Vec<LocalAccount>>
+    where
+        F: Fn(usize, ZeroKnowledgeSig, EphemeralKeyPair) -> anyhow::Result<LocalAccount>,
+    {
+        let mut accounts = vec![];
+        let mut i = 0;
+        let lines = self.load_lines();
 
         for line in lines {
             let serialized_proof = line?;
             let zk_sig_bytes = hex::decode(serialized_proof)?;
             let zk_sig = ZeroKnowledgeSig::try_from(zk_sig_bytes.as_slice())?;
 
-            // Cloning is disabled outside #[cfg(test)]
-            let serialized: &[u8] = &(self.ephemeral_secret_key.to_bytes());
-            let esk = Ed25519PrivateKey::try_from(serialized)?;
-
-            let keyless_account = KeylessAccount::new(
-                &self.iss,
-                &self.aud,
-                &self.uid_key,
-                &self.uid_val,
-                &self.jwt_header_json,
-                EphemeralKeyPair::new(
-                    esk,
-                    self.epk_expiry_date_secs,
-                    vec![0; OpenIdSig::EPK_BLINDER_NUM_BYTES],
-                )?,
-                Pepper::from_number(i.try_into().unwrap()),
-                zk_sig,
+            let ephemeral_key_pair = EphemeralKeyPair::new(
+                self.create_ephemeral_key()?,
+                self.epk_expiry_date_secs,
+                vec![0; OpenIdSig::EPK_BLINDER_NUM_BYTES],
             )?;
-            addresses.push(keyless_account.authentication_key().account_address());
-            keyless_accounts.push(keyless_account);
+
+            let account = create_account(i, zk_sig, ephemeral_key_pair)?;
+            accounts.push(account);
             i += 1;
 
             if i == num_accounts {
@@ -162,23 +275,6 @@ impl LocalAccountGenerator for KeylessAccountGenerator {
             bail!("not enough proofs - {num_accounts} num_accounts, {i} found")
         }
 
-        let result_futures = addresses
-            .iter()
-            .map(|address| txn_executor.query_sequence_number(*address))
-            .collect::<Vec<_>>();
-        let seq_nums: Vec<_> = try_join_all(result_futures).await?.into_iter().collect();
-
-        let accounts = keyless_accounts
-            .into_iter()
-            .zip(seq_nums)
-            .map(|(keyless_account, sequence_number)| {
-                LocalAccount::new_keyless(
-                    keyless_account.authentication_key().account_address(),
-                    keyless_account,
-                    sequence_number,
-                )
-            })
-            .collect();
-        Ok(accounts)
+        KeylessAccountGenerator::set_sequence_numbers(txn_executor, accounts).await
     }
 }
