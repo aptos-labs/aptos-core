fc421efc6d9f2a77acbdd2ecfdefec3861058d3a
George Mitenkov <georgemitenk0v@gmail.com>
2024-10-14 16:30:50 +0100

    [refactoring] Improve module cache API to be able to implement traversal generically with base value initialization.

--- types/src/vm/code_cache.rs
+++ types/src/vm/code_cache.rs
@@ -29,8 +29,15 @@ pub trait ModuleCache {
     /// Stores the module to the code cache.
     fn store_module(&self, key: Self::Key, module: Self::Module);
 
-    /// Returns the module if it has been cached before, or [None] otherwise.
-    fn fetch_module(&self, key: &Self::Key) -> Option<Self::Module>;
+    /// Ensures that the entry in the module cache is initialized based on the default value, if it
+    /// was not stored before. Returns the stored module, or [None] if it does not exist.
+    fn fetch_module_or_store_with<F, E>(
+        &self,
+        key: &Self::Key,
+        default: F,
+    ) -> Result<Option<Self::Module>, E>
+    where
+        F: FnOnce() -> Result<Option<Self::Module>, E>;
 
     /// Removes all cached modules from the cache.
     fn flush_modules(&self);
