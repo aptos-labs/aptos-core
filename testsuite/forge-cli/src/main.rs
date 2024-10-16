8a540dce3f4d77d7b4d470a709947cf8dc38a51b
Balaji Arun <balaji@aptoslabs.com>
2024-10-16 16:50:06 -0700

    [forge][chaos] Expand chaos simulation to six regions (#14860)
    * [forge][chaos] expand to six regions
    
    * increase chaos timeout interval
    
    * 4 region is default

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -811,9 +811,12 @@ fn get_multi_region_test(test_name: &str) -> Option<ForgeConfig> {
     Some(test)
 }
 
-fn wrap_with_realistic_env<T: NetworkTest + 'static>(test: T) -> CompositeNetworkTest {
+fn wrap_with_realistic_env<T: NetworkTest + 'static>(
+    num_validators: usize,
+    test: T,
+) -> CompositeNetworkTest {
     CompositeNetworkTest::new_with_two_wrappers(
-        MultiRegionNetworkEmulationTest::default(),
+        MultiRegionNetworkEmulationTest::default_for_validator_count(num_validators),
         CpuChaosTest::default(),
         test,
     )
@@ -858,8 +861,9 @@ fn wrap_with_two_region_env<T: NetworkTest + 'static>(test: T) -> CompositeNetwo
 }
 
 fn run_consensus_only_realistic_env_max_tps() -> ForgeConfig {
+    let num_validators = 20;
     ForgeConfig::default()
-        .with_initial_validator_count(NonZeroUsize::new(20).unwrap())
+        .with_initial_validator_count(NonZeroUsize::new(num_validators).unwrap())
         .with_emit_job(
             EmitJobRequest::default()
                 .mode(EmitJobMode::MaxLoad {
@@ -868,7 +872,7 @@ fn run_consensus_only_realistic_env_max_tps() -> ForgeConfig {
                 .txn_expiration_time_secs(5 * 60),
         )
         .add_network_test(CompositeNetworkTest::new(
-            MultiRegionNetworkEmulationTest::default(),
+            MultiRegionNetworkEmulationTest::default_for_validator_count(num_validators),
             CpuChaosTest::default(),
         ))
         .with_genesis_helm_config_fn(Arc::new(|helm_values| {
@@ -1119,7 +1123,7 @@ fn realistic_env_sweep_wrap(
         .with_validator_override_node_config_fn(Arc::new(|config, _| {
             config.execution.processed_transactions_detailed_counters = true;
         }))
-        .add_network_test(wrap_with_realistic_env(test))
+        .add_network_test(wrap_with_realistic_env(num_validators, test))
         // Test inherits the main EmitJobRequest, so update here for more precise latency measurements
         .with_emit_job(
             EmitJobRequest::default().latency_polling_interval(Duration::from_millis(100)),
@@ -1388,10 +1392,11 @@ fn workload_vs_perf_benchmark() -> ForgeConfig {
 }
 
 fn realistic_env_graceful_overload(duration: Duration) -> ForgeConfig {
+    let num_validators = 20;
     ForgeConfig::default()
-        .with_initial_validator_count(NonZeroUsize::new(20).unwrap())
+        .with_initial_validator_count(NonZeroUsize::new(num_validators).unwrap())
         .with_initial_fullnode_count(20)
-        .add_network_test(wrap_with_realistic_env(TwoTrafficsTest {
+        .add_network_test(wrap_with_realistic_env(num_validators, TwoTrafficsTest {
             inner_traffic: EmitJobRequest::default()
                 .mode(EmitJobMode::ConstTps { tps: 15000 })
                 .init_gas_price_multiplier(20),
@@ -1952,7 +1957,7 @@ fn realistic_env_max_load_test(
     ForgeConfig::default()
         .with_initial_validator_count(NonZeroUsize::new(num_validators).unwrap())
         .with_initial_fullnode_count(num_fullnodes)
-        .add_network_test(wrap_with_realistic_env(TwoTrafficsTest {
+        .add_network_test(wrap_with_realistic_env(num_validators, TwoTrafficsTest {
             inner_traffic: EmitJobRequest::default()
                 .mode(EmitJobMode::MaxLoad { mempool_backlog })
                 .init_gas_price_multiplier(20),
@@ -2013,7 +2018,7 @@ fn realistic_network_tuned_for_throughput_test() -> ForgeConfig {
 
     let mut forge_config = ForgeConfig::default()
         .with_initial_validator_count(NonZeroUsize::new(VALIDATOR_COUNT).unwrap())
-        .add_network_test(MultiRegionNetworkEmulationTest::default())
+        .add_network_test(MultiRegionNetworkEmulationTest::default_for_validator_count(VALIDATOR_COUNT))
         .with_emit_job(EmitJobRequest::default().mode(EmitJobMode::MaxLoad {
             mempool_backlog: (TARGET_TPS as f64 * VFN_LATENCY_S) as usize,
         }))
@@ -2326,8 +2331,9 @@ fn quorum_store_reconfig_enable_test() -> ForgeConfig {
 }
 
 fn mainnet_like_simulation_test() -> ForgeConfig {
+    let num_validators = 20;
     ForgeConfig::default()
-        .with_initial_validator_count(NonZeroUsize::new(20).unwrap())
+        .with_initial_validator_count(NonZeroUsize::new(num_validators).unwrap())
         .with_emit_job(
             EmitJobRequest::default()
                 .mode(EmitJobMode::MaxLoad {
@@ -2336,7 +2342,7 @@ fn mainnet_like_simulation_test() -> ForgeConfig {
                 .txn_expiration_time_secs(5 * 60),
         )
         .add_network_test(CompositeNetworkTest::new(
-            MultiRegionNetworkEmulationTest::default(),
+            MultiRegionNetworkEmulationTest::default_for_validator_count(num_validators),
             CpuChaosTest::default(),
         ))
         .with_genesis_helm_config_fn(Arc::new(|helm_values| {
