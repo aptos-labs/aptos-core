b1d0201e45e438c123e39302b415ac41084500dd
runtianz <runtian@aptoslabs.com>
2024-10-21 11:47:57 -0700

    fix WG's comments

--- aptos-move/dynamic-transaction-composer/src/lib.rs
+++ aptos-move/dynamic-transaction-composer/src/lib.rs
@@ -21,76 +21,105 @@ mod helpers;
 #[cfg(test)]
 pub mod tests;
 
-// CompiledScript generated from script builder will have this key in its metadata to distinguish from other scripts.
-pub static APTOS_SCRIPT_BUILDER_KEY: &[u8] = "aptos::script_builder".as_bytes();
+/// CompiledScript generated from script composer will have this key in its metadata to
+/// distinguish from other scripts.
+pub static APTOS_SCRIPT_COMPOSER_KEY: &[u8] = "aptos::script_composer".as_bytes();
 
+/// Representing a returned value from a previous list of `MoveFunctionCall`s.
 #[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub struct PreviousResult {
+    /// Refering to the return value in the `call_idx`th call.
     call_idx: u16,
+    /// Refering to the `return_idx`th return value in that call, since move function call can
+    /// return multiple values.
     return_idx: u16,
+    /// How this result would be used.
     operation_type: ArgumentOperation,
 }
 
+/// Arguments to the `MoveFunctionCall`.
 #[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
-pub enum BatchArgument {
+pub enum CallArgument {
+    /// Passing raw bytes to the function. The bytes must follows the existing constraints for
+    /// transaction arguments.
     Raw(Vec<u8>),
+    /// Refering to signer of the transaction. If it's a single signer transaction you will only
+    /// be able to access `Signer(0)`. You will be able to access other signers if it's a multi
+    /// agent transaction.
     Signer(u16),
+    /// The arugment came from the returned value of a previous `MoveFunctionCall`.
     PreviousResult(PreviousResult),
 }
 
+/// How to consume the returned value coming from a previous `MoveFunctionCall`.
 #[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub enum ArgumentOperation {
+    /// Move the returned value to the next caller. This can be used for values that don't have
+    /// `copy` ability.
     Move,
+    /// Copy the returned value and pass it to the next caller.
     Copy,
+    /// Borrow an immutable reference from a returned value and pass it to the next caller.
     Borrow,
+    /// Borrow a mutable reference from a returned value and pass it to the next caller.
     BorrowMut,
 }
 
+/// Calling a Move function.
+///
+/// Similar to a public entry function call, but the arguments could specified as `CallArgument`,
+/// which can be a return value of a previous `MoveFunctionCall`.
 #[wasm_bindgen]
-/// Call a Move entry function.
 #[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
-pub struct BatchedFunctionCall {
+pub struct MoveFunctionCall {
     module: ModuleId,
     function: Identifier,
     ty_args: Vec<TypeTag>,
-    args: Vec<BatchArgument>,
+    args: Vec<CallArgument>,
 }
 
-impl BatchedFunctionCall {
-    pub fn into_inner(self) -> (ModuleId, Identifier, Vec<TypeTag>, Vec<BatchArgument>) {
+/// Version of the script composer so that decompiler will know how to decode the script as we
+/// upgrade.
+#[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub enum ComposerVersion {
+    V1,
+}
+
+impl MoveFunctionCall {
+    pub fn into_inner(self) -> (ModuleId, Identifier, Vec<TypeTag>, Vec<CallArgument>) {
         (self.module, self.function, self.ty_args, self.args)
     }
 }
 
 #[wasm_bindgen]
-impl BatchArgument {
+impl CallArgument {
     pub fn new_bytes(bytes: Vec<u8>) -> Self {
-        BatchArgument::Raw(bytes)
+        CallArgument::Raw(bytes)
     }
 
     pub fn new_signer(signer_idx: u16) -> Self {
-        BatchArgument::Signer(signer_idx)
+        CallArgument::Signer(signer_idx)
     }
 
-    pub fn borrow(&self) -> Result<BatchArgument, String> {
+    pub fn borrow(&self) -> Result<CallArgument, String> {
         self.change_op_type(ArgumentOperation::Borrow)
     }
 
-    pub fn borrow_mut(&self) -> Result<BatchArgument, String> {
+    pub fn borrow_mut(&self) -> Result<CallArgument, String> {
         self.change_op_type(ArgumentOperation::BorrowMut)
     }
 
-    pub fn copy(&self) -> Result<BatchArgument, String> {
+    pub fn copy(&self) -> Result<CallArgument, String> {
         self.change_op_type(ArgumentOperation::Copy)
     }
 
-    fn change_op_type(&self, operation_type: ArgumentOperation) -> Result<BatchArgument, String> {
+    fn change_op_type(&self, operation_type: ArgumentOperation) -> Result<CallArgument, String> {
         match &self {
-            BatchArgument::PreviousResult(r) => {
+            CallArgument::PreviousResult(r) => {
                 let mut result = r.clone();
                 result.operation_type = operation_type;
-                Ok(BatchArgument::PreviousResult(result))
+                Ok(CallArgument::PreviousResult(result))
             },
             _ => Err(
                 "Unexpected argument type, can only borrow from previous function results"
