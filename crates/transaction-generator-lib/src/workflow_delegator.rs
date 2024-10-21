148dbb829d8387af0a46520cd2b171ef2523654a
Satya Vusirikala <satyasatya123456@gmail.com>
2024-10-21 16:17:02 -0700

    Increase the coins to 50 APT

--- crates/transaction-generator-lib/src/workflow_delegator.rs
+++ crates/transaction-generator-lib/src/workflow_delegator.rs
@@ -273,7 +273,7 @@ impl WorkflowTxnGeneratorCreator {
                     txn_executor,
                     num_modules,
                     mint_entry_point.package_name(),
-                    Some(20_00000000),
+                    Some(40_0000_0000),
                 )
                 .await;
 
