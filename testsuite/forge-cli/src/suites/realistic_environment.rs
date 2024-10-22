12a113967a84bf9a5870e98c982f80d134125c6d
hariria <hariria@usc.edu>
2024-10-22 08:57:21 -0700

    [forge] Bump realistic_env_workload_sweep CoinTransfer ordered_to_commit

--- testsuite/forge-cli/src/suites/realistic_environment.rs
+++ testsuite/forge-cli/src/suites/realistic_environment.rs
@@ -124,7 +124,7 @@ pub(crate) fn realistic_env_workload_sweep_test() -> ForgeConfig {
         ]),
         // Investigate/improve to make latency more predictable on different workloads
         criteria: [
-            (7000, 100, 0.3 + 0.5, 0.5, 0.4),
+            (7000, 100, 0.3 + 0.5, 0.5, 0.5),
             (8500, 100, 0.3 + 0.5, 0.5, 0.4),
             (2000, 300, 0.3 + 1.0, 0.6, 1.0),
             (3200, 500, 0.3 + 1.0, 0.7, 0.6),
