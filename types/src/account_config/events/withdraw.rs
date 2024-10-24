1c681cee2dc7fdceaae551a02b5a3c1ca37a6130
Aaron Gao <lightmark@gmail.com>
2024-10-24 01:51:24 -0700

    fix rosetta

--- types/src/account_config/events/withdraw.rs
+++ types/src/account_config/events/withdraw.rs
@@ -4,6 +4,7 @@
 use anyhow::Result;
 use move_core_types::{ident_str, identifier::IdentStr, move_resource::MoveStructType};
 use serde::{Deserialize, Serialize};
+use move_core_types::account_address::AccountAddress;
 
 /// Struct that represents a SentPaymentEvent.
 #[derive(Debug, Serialize, Deserialize)]
@@ -25,4 +26,22 @@ impl WithdrawEvent {
 impl MoveStructType for WithdrawEvent {
     const MODULE_NAME: &'static IdentStr = ident_str!("coin");
     const STRUCT_NAME: &'static IdentStr = ident_str!("WithdrawEvent");
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CoinWithdraw {
+    pub coin_type: String,
+    pub account: AccountAddress,
+    pub amount: u64,
+}
+
+impl CoinWithdraw {
+    pub fn try_from_bytes(bytes: &[u8]) -> Result<Self> {
+        bcs::from_bytes(bytes).map_err(Into::into)
+    }
+}
+
+impl MoveStructType for CoinWithdraw {
+    const MODULE_NAME: &'static IdentStr = ident_str!("coin");
+    const STRUCT_NAME: &'static IdentStr = ident_str!("CoinWithdraw");
 }
