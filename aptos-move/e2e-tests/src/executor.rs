5c1868c77948a609b1081f293ef8bdc6826f3f7b
aldenhu <msmouse@gmail.com>
2024-10-28 17:01:11 +0000

    trivial: fix copyright banner

--- aptos-move/e2e-tests/src/executor.rs
+++ aptos-move/e2e-tests/src/executor.rs
@@ -1,8 +1,4 @@
-// Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
-
 // Copyright © Aptos Foundation
-
 // Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
