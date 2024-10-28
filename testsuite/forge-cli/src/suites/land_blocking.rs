73eaddaa637606da3de7e399f1cfde13ac6d1bd6
Satya Vusirikala <satyasatya123456@gmail.com>
2024-10-28 11:53:04 -0700

    Minor changes

--- testsuite/forge-cli/src/suites/land_blocking.rs
+++ testsuite/forge-cli/src/suites/land_blocking.rs
@@ -18,7 +18,7 @@ pub(crate) fn get_land_blocking_test(
 ) -> Option<ForgeConfig> {
     let test = match test_name {
         "land_blocking" | "realistic_env_max_load" => {
-            pfn_const_tps(duration, true, true, false)
+            pfn_const_tps(duration, true, true, false, TransactionType::default())
         },
         "compat" => compat(),
         "framework_upgrade" => framework_upgrade(),
