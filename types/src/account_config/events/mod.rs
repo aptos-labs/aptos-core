641ccd865fb3fe61699219aa0968d86101623353
Aaron <lightmark@gmail.com>
2024-11-01 17:18:49 -0600

    fix aptos_goverance::CreateProposal event recognition (#15143) (#15162)

--- types/src/account_config/events/mod.rs
+++ types/src/account_config/events/mod.rs
@@ -11,3 +11,8 @@ pub use deposit::*;
 pub use new_block::*;
 pub use new_epoch::*;
 pub use withdraw::*;
+
+pub fn is_aptos_governance_create_proposal_event(event_type: &str) -> bool {
+    event_type == "0x1::aptos_governance::CreateProposal"
+        || event_type == "0x1::aptos_governance::CreateProposalEvent"
+}
