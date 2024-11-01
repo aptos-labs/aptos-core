a06d9ef43953764edf5598dc1a06ecbe374a6e53
Balaji Arun <balaji@aptoslabs.com>
2024-11-01 15:31:54 -0700

    test load sweep

--- testsuite/forge-cli/src/main.rs
+++ testsuite/forge-cli/src/main.rs
@@ -237,7 +237,8 @@ fn main() -> Result<()> {
 
     let args = Args::parse();
     let duration = Duration::from_secs(args.duration_secs as u64);
-    let suite_name: &str = args.suite.as_ref();
+    // let suite_name: &str = args.suite.as_ref();
+    let suite_name = "realistic_env_load_sweep";
 
     let runtime = Runtime::new()?;
     match args.cli_cmd {
