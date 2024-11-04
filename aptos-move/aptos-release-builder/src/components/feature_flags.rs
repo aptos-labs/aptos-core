0240ee6e33d41da7003ac928b7d02a495dfddf5b
Teng Zhang <rahxephon89@163.com>
2024-11-04 12:45:17 -0800

    enable loader v2

--- aptos-move/aptos-release-builder/src/components/feature_flags.rs
+++ aptos-move/aptos-release-builder/src/components/feature_flags.rs
@@ -355,9 +355,6 @@ impl From<FeatureFlag> for AptosFeatureFlag {
             FeatureFlag::CollectionOwner => AptosFeatureFlag::COLLECTION_OWNER,
             FeatureFlag::NativeMemoryOperations => AptosFeatureFlag::NATIVE_MEMORY_OPERATIONS,
             FeatureFlag::EnableLoaderV2 => AptosFeatureFlag::ENABLE_LOADER_V2,
-            FeatureFlag::TransactionContextHashFunctionUpdate => {
-                AptosFeatureFlag::TRANSACTION_CONTEXT_HASH_FUNCTION_UPDATE
-            },
             FeatureFlag::FakeFeatureForComparisonTesting => {
                 AptosFeatureFlag::FAKE_FEATURE_FOR_COMPARISON_TESTING
             },
@@ -508,9 +505,6 @@ impl From<AptosFeatureFlag> for FeatureFlag {
             AptosFeatureFlag::COLLECTION_OWNER => FeatureFlag::CollectionOwner,
             AptosFeatureFlag::NATIVE_MEMORY_OPERATIONS => FeatureFlag::NativeMemoryOperations,
             AptosFeatureFlag::ENABLE_LOADER_V2 => FeatureFlag::EnableLoaderV2,
-            AptosFeatureFlag::TRANSACTION_CONTEXT_HASH_FUNCTION_UPDATE => {
-                FeatureFlag::TransactionContextHashFunctionUpdate
-            },
             AptosFeatureFlag::FAKE_FEATURE_FOR_COMPARISON_TESTING => {
                 FeatureFlag::FakeFeatureForComparisonTesting
             },
