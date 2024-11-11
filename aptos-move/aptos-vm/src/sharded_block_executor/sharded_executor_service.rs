04a2733eda478d7bcb98eef9fd8182c78f5925cb
George Mitenkov <georgemitenk0v@gmail.com>
2024-11-11 21:14:40 +0000

    [fix] Add done --> ready transition for unit tests

--- aptos-move/aptos-vm/src/sharded_block_executor/sharded_executor_service.rs
+++ aptos-move/aptos-vm/src/sharded_block_executor/sharded_executor_service.rs
@@ -136,13 +136,16 @@ impl<S: StateView + Sync + Send + 'static> ShardedExecutorService<S> {
                 );
             });
             s.spawn(move |_| {
+                // Since we execute blocks in parallel, we cannot share module caches, so each
+                // thread has its own caches.
+                let module_cache_manager = ModuleCacheManager::new();
+                module_cache_manager.mark_ready(None, None);
+
                 let ret = BlockAptosVM::execute_block_on_thread_pool(
                     executor_thread_pool,
                     &signature_verified_transactions,
                     aggr_overridden_state_view.as_ref(),
-                    // Since we execute blocks in parallel, we cannot share module caches, so each
-                    // thread has its own caches.
-                    &ModuleCacheManager::new(),
+                    &module_cache_manager,
                     config,
                     cross_shard_commit_sender,
                 )
