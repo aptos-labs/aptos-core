2541069e0879e0faa23cda2d34a7488f90fc6af4
George Mitenkov <georgemitenk0v@gmail.com>
2024-11-16 12:44:24 +0000

    Move read-before-write to VM

--- execution/executor-benchmark/src/lib.rs
+++ execution/executor-benchmark/src/lib.rs
@@ -948,10 +948,12 @@ mod tests {
         AptosVM::set_num_shards_once(1);
         AptosVM::set_concurrency_level_once(4);
         AptosVM::set_processed_transactions_detailed_counters();
-        test_generic_benchmark::<AptosVMBlockExecutor>(
-            Some(TransactionTypeArg::RepublishAndCall),
-            true,
-        );
+        for _ in 0..10 {
+            test_generic_benchmark::<AptosVMBlockExecutor>(
+                Some(TransactionTypeArg::RepublishAndCall),
+                true,
+            );
+        }
     }
 
     #[test]
