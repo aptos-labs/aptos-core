48820c560c351ee7f9f6485d4759d046a0702bf4
Josh Lind <josh.lind@hotmail.com>
2024-11-25 16:58:19 -0500

    [cp] Temporarily loosen backpressures, until module loading improvement speeds up blocks

--- config/src/config/consensus_config.rs
+++ config/src/config/consensus_config.rs
@@ -196,10 +196,10 @@ impl Default for ConsensusConfig {
             min_max_txns_in_block_after_filtering_from_backpressure: MIN_BLOCK_TXNS_AFTER_FILTERING,
             execution_backpressure: Some(ExecutionBackpressureConfig {
                 num_blocks_to_look_at: 20,
-                min_blocks_to_activate: 4,
+                min_blocks_to_activate: 10,
                 percentile: 0.5,
-                target_block_time_ms: 200,
-                min_block_time_ms_to_activate: 100,
+                target_block_time_ms: 350,
+                min_block_time_ms_to_activate: 200,
                 // allow at least two spreading group from reordering in a single block, to utilize paralellism
                 min_calibrated_txns_per_block: 8,
             }),
