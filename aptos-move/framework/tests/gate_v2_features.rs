fc5f7f012b347f04b8c3f083bcaa47c44871475b
Teng Zhang <rahxephon89@163.com>
2024-11-26 12:43:36 -0800

    [test-only] set v2 flag for unit tests

--- aptos-move/framework/tests/gate_v2_features.rs
+++ aptos-move/framework/tests/gate_v2_features.rs
@@ -28,26 +28,31 @@ fn compile_pkg_with_v1(path_to_pkg: impl Into<String>) {
 }
 
 #[test]
+#[ignore]
 fn compile_aptos_framework_with_v1() {
     compile_pkg_with_v1("aptos-framework");
 }
 
 #[test]
+#[ignore]
 fn compile_aptos_stdlib_with_v1() {
     compile_pkg_with_v1("aptos-stdlib");
 }
 
 #[test]
+#[ignore]
 fn compile_move_stdlib_with_v1() {
     compile_pkg_with_v1("move-stdlib");
 }
 
 #[test]
+#[ignore]
 fn compile_aptos_token_with_v1() {
     compile_pkg_with_v1("aptos-token");
 }
 
 #[test]
+#[ignore]
 fn compile_aptos_token_objects_with_v1() {
     compile_pkg_with_v1("aptos-token-objects");
 }
