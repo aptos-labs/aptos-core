8b379273e803fcf3b8dc831bf27dfd786914daee
Brian R. Murphy <132495859+brmataptos@users.noreply.github.com>
2024-11-27 02:23:28 -0800

    address PR 15365 comments as of 11/26

--- third_party/move/move-compiler-v2/src/env_pipeline/lambda_lifter.rs
+++ third_party/move/move-compiler-v2/src/env_pipeline/lambda_lifter.rs
@@ -748,7 +748,7 @@ impl<'a> ExpRewriterFunctions for LambdaLifter<'a> {
             env.error(
                 &loc,
                 // TODO(LAMBDA)
-                "Lambdas expressions with `store` ability currently may only be a simple call to an existing `public` function (see release notes for details).  This lambda expression requires the compiler to generate a `public` helper function, which might affect module upgradeability and is not yet supported."
+                "The body of a lambdas expression with `store` ability currently must be a simple call to an existing `public` function, with lambda params the same as the *final* arguments to the function call."
             );
             return None;
         };
--- third_party/move/move-compiler-v2/transactional-tests/tests/inlining/function_name_shadowing.move
+++ third_party/move/move-compiler-v2/transactional-tests/tests/inlining/function_name_shadowing.move
@@ -1,16 +1,29 @@
 //# publish
+module 0x42::OtherModule {
+    public fun g(a: u64, b: u64): u64 {
+        a + b
+    }
+
+    public fun h(a: u64, b: u64): u64 {
+        2 * a + b
+    }
+}
+
+//# publish
 module 0x42::Test {
+    use 0x42::OtherModule::g;
 
     public fun f(a: u64, b: u64): u64 {
         a * b
     }
 
-    public inline fun quux(f:|u64, u64|u64, a: u64, b: u64): u64 {
-        f(a, b)
+    public inline fun quux(f:|u64, u64|u64, g:|u64|u64, i:|u8|u8, a: u64, b: u64): u64 {
+        use 0x42::OtherModule::h;
+        f(a, b) * g(a, b) * h(a, b)
     }
 
     public fun test_shadowing(): u64 {
-        quux(|a, b| a - b, 10, 2)
+        quux(|a, b| a - b, |a| a + 2, |b| 255u8-b, 10, 2)
     }
 }
 
