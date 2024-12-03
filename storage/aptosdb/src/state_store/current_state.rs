a73e88da0faf7d46baae6f6bc4b57337ac2bfbe1
aldenhu <msmouse@gmail.com>
2024-12-03 03:31:11 +0000

    temp

--- storage/aptosdb/src/state_store/current_state.rs
+++ storage/aptosdb/src/state_store/current_state.rs
@@ -3,24 +3,26 @@
 
 use aptos_storage_interface::state_store::{state::LedgerState, state_delta::StateDelta};
 use derive_more::{Deref, DerefMut};
-use aptos_storage_interface::state_store::state_summary::LedgerStateSummary;
 
 #[derive(Clone, Debug, Deref, DerefMut)]
 pub(crate) struct CurrentState {
-    state: LedgerState,
-    state_summary: LedgerStateSummary,
+    #[deref]
+    #[deref_mut]
+    latest_state: LedgerState,
 }
 
 impl CurrentState {
     pub fn new_dummy() -> Self {
         Self {
-            state: LedgerState::new_empty(),
-            state_summary: LedgerStateSummary::new_empty(),
+            latest_state: LedgerState::new_empty(),
         }
     }
 
-    pub fn set(&mut self, state: LedgerState, state_summary: LedgerStateSummary) {
-        self.state = state;
-        self.state_summary = state_summary;
+    pub fn set(&mut self, latest_state: LedgerState) {
+        self.latest_state = latest_state
+    }
+
+    pub fn get(&self) -> &LedgerState {
+        &self.latest_state
     }
 }
