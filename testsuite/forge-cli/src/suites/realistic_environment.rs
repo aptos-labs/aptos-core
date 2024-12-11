2f93c9deaf4e565b31d0fea0ae9e29dde2b9d714
Brian Cho <brian@aptoslabs.com>
2024-12-10 20:50:36 -0800

    no agg

--- testsuite/forge-cli/src/suites/realistic_environment.rs
+++ testsuite/forge-cli/src/suites/realistic_environment.rs
@@ -114,7 +114,7 @@ pub(crate) fn bcho_test() -> ForgeConfig {
         test: Box::new(PerformanceBenchmark),
         // Sequential workloads
         workloads: Workloads::TRANSACTIONS(vec![
-            TransactionWorkload::new(TransactionTypeArg::ModifyGlobalResourceAggV2, 20_000)
+            TransactionWorkload::new(TransactionTypeArg::ModifyGlobalResource, 20_000)
                 .with_gas_price(5 * aptos_global_constants::GAS_UNIT_PRICE)
                 .with_transactions_per_account(20),
             TransactionWorkload::new(TransactionTypeArg::SmartTablePicture1MWith256Change, 20)
