3ad340cbbc8347f21595665bc7b38963f6a96d28
Balaji Arun <balaji@aptoslabs.com>
2024-12-12 01:44:42 +0000

    fix internal image tag

--- testsuite/forge/src/backend/k8s/cluster_helper.rs
+++ testsuite/forge/src/backend/k8s/cluster_helper.rs
@@ -647,7 +647,7 @@ pub async fn install_testnet_resources(
         kube_client.clone(),
         kube_namespace.clone(),
         FORGE_TESTNET_DEPLOYER_DOCKER_IMAGE_REPO.to_string(),
-        Some("9ab46e9e78392fe66102c3145e2d10045e58e6c6".into()),
+        Some("7ab32f3eb20e28fbc2103b0fbe82148063794b30".into()),
     );
 
     testnet_deployer.start(config).await?;
