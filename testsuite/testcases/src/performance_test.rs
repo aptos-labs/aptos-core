e3eb8e39113fd4a37d1f5a0faa9ac117f8d6335e
danielxiangzl <xiangzhuolun@gmail.com>
2024-12-12 18:24:20 -0800

    test leader equivocation

--- testsuite/testcases/src/performance_test.rs
+++ testsuite/testcases/src/performance_test.rs
@@ -2,8 +2,11 @@
 // Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::{sync::Arc, time::Duration};
+use anyhow::{anyhow, bail, Context};
+
 use crate::NetworkLoadTest;
-use aptos_forge::{NetworkContextSynchronizer, NetworkTest, Result, Test};
+use aptos_forge::{NetworkContextSynchronizer, NetworkTest, Result, Swarm, SwarmExt, Test, TestReport};
 use async_trait::async_trait;
 
 pub struct PerformanceBenchmark;
@@ -14,7 +17,34 @@ impl Test for PerformanceBenchmark {
     }
 }
 
-impl NetworkLoadTest for PerformanceBenchmark {}
+#[async_trait]
+impl NetworkLoadTest for PerformanceBenchmark {
+    async fn test(
+        &self,
+        swarm: Arc<tokio::sync::RwLock<Box<dyn Swarm>>>,
+        _report: &mut TestReport,
+        duration: Duration,
+    ) -> Result<()> {
+        let validators = { swarm.read().await.get_validator_clients_with_names() };
+        let num_bad_leaders = validators.len() / 5;
+        for (name, validator)  in validators[..num_bad_leaders].iter() {
+            validator
+                    .set_failpoint(
+                        "consensus::leader_equivocation".to_string(),
+                        "return".to_string(),
+                    )
+                    .await
+                    .map_err(|e| {
+                        anyhow!(
+                            "set_failpoint to set consensus leader equivocation on {} failed, {:?}",
+                            name,
+                            e
+                        )
+                    })?;
+        };
+        Ok(())
+    }
+}
 
 #[async_trait]
 impl NetworkTest for PerformanceBenchmark {
