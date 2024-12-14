632792c32239d5f21d191da0f7c8632b48939133
danielxiangzl <xiangzhuolun@gmail.com>
2024-12-13 16:39:58 -0800

    disable failover

--- config/src/config/mempool_config.rs
+++ config/src/config/mempool_config.rs
@@ -117,7 +117,7 @@ impl Default for MempoolConfig {
             capacity: 2_000_000,
             capacity_bytes: 2 * 1024 * 1024 * 1024,
             capacity_per_user: 100,
-            default_failovers: 1,
+            default_failovers: 0,
             enable_intelligent_peer_prioritization: true,
             shared_mempool_peer_update_interval_ms: 1_000,
             shared_mempool_priority_update_interval_secs: 600, // 10 minutes (frequent reprioritization is expensive)
