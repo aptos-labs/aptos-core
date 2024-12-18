f865df015c1e17e63710948916203641d4a989fd
hariria <hariria@usc.edu>
2024-12-18 15:37:38 -0800

    [execution-pool] SenderAwareTransaction trait

---
+++ types/src/transaction/sender_aware.rs
@@ -0,0 +1,27 @@
+use crate::transaction::{
+    signature_verified_transaction::SignatureVerifiedTransaction, SignedTransaction,
+};
+use move_core_types::account_address::AccountAddress;
+
+pub trait SenderAwareTransaction {
+    fn parse_sender(&self) -> AccountAddress;
+}
+
+impl SenderAwareTransaction for SignedTransaction {
+    fn parse_sender(&self) -> AccountAddress {
+        self.sender()
+    }
+}
+
+impl SenderAwareTransaction for SignatureVerifiedTransaction {
+    fn parse_sender(&self) -> AccountAddress {
+        let txn = match self {
+            SignatureVerifiedTransaction::Valid(txn) => txn,
+            SignatureVerifiedTransaction::Invalid(txn) => txn,
+        };
+        match txn {
+            crate::transaction::Transaction::UserTransaction(txn) => txn.parse_sender(),
+            _ => unreachable!("SenderAwareTransaction should not be given non-UserTransaction"),
+        }
+    }
+}
