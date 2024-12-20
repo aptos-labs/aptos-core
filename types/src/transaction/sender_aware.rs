2428f75b49d1a78fa28276b131a2b35566fbccf3
hariria <hariria@usc.edu>
2024-12-20 12:52:23 -0800

    [execution-pool] Sender aware copyright lint fix

--- types/src/transaction/sender_aware.rs
+++ types/src/transaction/sender_aware.rs
@@ -1,3 +1,6 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::transaction::{
     signature_verified_transaction::SignatureVerifiedTransaction, SignedTransaction,
 };
