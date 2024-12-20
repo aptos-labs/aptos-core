51961817f007d308122e90ba9dcfa8e85fa161f1
hariria <hariria@usc.edu>
2024-12-19 16:52:25 -0800

    [execution-pool] Establish a SenderAware Transaction Iterator
    Also decouples logic for iterator into a separate next_to_add function that is separate from shuffle function

--- consensus/src/transaction_shuffler/sender_aware.rs
+++ consensus/src/transaction_shuffler/sender_aware.rs
@@ -3,10 +3,100 @@
 
 use crate::{counters::NUM_SENDERS_IN_BLOCK, transaction_shuffler::TransactionShuffler};
 use aptos_types::transaction::{
+    sender_aware::SenderAwareTransaction,
     signature_verified_transaction::SignatureVerifiedTransaction, SignedTransaction,
 };
 use move_core_types::account_address::AccountAddress;
-use std::collections::{HashMap, VecDeque};
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt::Debug,
+};
+
+#[derive(Clone, Debug)]
+pub struct Config {
+    conflict_window_size: usize,
+}
+
+#[derive(Debug)]
+pub struct ShuffledTransactionIterator<Txn> {
+    orig_txns: VecDeque<Txn>,
+    sliding_window_state: SlidingWindowState<Txn>,
+    pending_txns: PendingTransactions<Txn>,
+}
+
+// TODO revisit the trait derives on Txn, used to be UseCaseAwareTransaction, may
+// have to make a SenderAwareTransaction trait...
+impl<Txn> ShuffledTransactionIterator<Txn>
+where
+    Txn: SenderAwareTransaction + Debug + Clone + PartialEq,
+{
+    pub fn new(config: Config, orig_txns: Vec<Txn>) -> Self {
+        let mut pending_txns = PendingTransactions::new();
+        let num_transactions = orig_txns.len();
+        Self {
+            orig_txns: VecDeque::from(orig_txns),
+            sliding_window_state: SlidingWindowState::new(
+                config.conflict_window_size,
+                num_transactions,
+            ),
+            pending_txns,
+        }
+    }
+
+    pub(super) fn select_next_txn_inner(&mut self) -> Txn {
+        todo!()
+    }
+
+    pub(super) fn select_next_txn(&mut self) -> Option<Txn> {
+        todo!()
+    }
+}
+
+impl<Txn> Iterator for ShuffledTransactionIterator<Txn>
+where
+    Txn: SenderAwareTransaction + Debug + Clone + PartialEq,
+{
+    type Item = Txn;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.select_next_txn()
+    }
+}
+
+pub struct SenderAwareShuffler {
+    pub config: Config,
+}
+
+impl SenderAwareShuffler {
+    fn next_to_add<Txn: SenderAwareTransaction + Debug + Clone + PartialEq>(
+        &self,
+        sliding_window_state: &mut SlidingWindowState<Txn>,
+        pending_txns: &mut PendingTransactions<Txn>,
+        orig_txns: &mut VecDeque<Txn>,
+    ) -> Txn {
+        // First check if we have a sender dropped off of conflict window in previous step, if so,
+        // we try to find pending transaction from the corresponding sender and add it to the block.
+        if let Some(sender) = sliding_window_state.last_dropped_sender() {
+            if let Some(txn) = pending_txns.remove_pending_from_sender(sender) {
+                return txn;
+            }
+        }
+        // If we can't find any transaction from a sender dropped off of conflict window, then
+        // iterate through the original transactions and try to find the next candidate
+        while let Some(txn) = orig_txns.pop_front() {
+            if !sliding_window_state.has_conflict(&txn.parse_sender()) {
+                return txn;
+            }
+            pending_txns.add_transaction(txn);
+        }
+
+        // If we can't find any candidate in above steps, then lastly
+        // add pending transactions in the order if we can't find any other candidate
+        pending_txns
+            .remove_first_pending()
+            .expect("Pending should return a transaction")
+    }
+}
 
 /// An implementation of transaction shuffler, which tries to spread transactions from same senders
 /// in a block in order to reduce conflict. On a high level, it works as follows - It defines a
@@ -31,11 +121,6 @@ use std::collections::{HashMap, VecDeque};
 ///         else we add it to the block
 ///   else
 ///       take the first transaction from the pending transactions and add it to the block
-
-pub struct SenderAwareShuffler {
-    conflict_window_size: usize,
-}
-
 impl TransactionShuffler for SenderAwareShuffler {
     fn shuffle(&self, txns: Vec<SignedTransaction>) -> Vec<SignedTransaction> {
         // Early return for performance reason if there are no transactions to shuffle
@@ -44,40 +129,18 @@ impl TransactionShuffler for SenderAwareShuffler {
         }
 
         // handle the corner case of conflict window being 0, in which case we don't do any shuffling
-        if self.conflict_window_size == 0 {
+        if self.config.conflict_window_size == 0 {
             return txns;
         }
 
         // maintains the intermediate state of the shuffled transactions
-        let mut sliding_window = SlidingWindowState::new(self.conflict_window_size, txns.len());
+        let mut sliding_window =
+            SlidingWindowState::new(self.config.conflict_window_size, txns.len());
         let mut pending_txns = PendingTransactions::new();
         let num_transactions = txns.len();
         let mut orig_txns = VecDeque::from(txns);
-        let mut next_to_add = |sliding_window: &mut SlidingWindowState| -> SignedTransaction {
-            // First check if we have a sender dropped off of conflict window in previous step, if so,
-            // we try to find pending transaction from the corresponding sender and add it to the block.
-            if let Some(sender) = sliding_window.last_dropped_sender() {
-                if let Some(txn) = pending_txns.remove_pending_from_sender(sender) {
-                    return txn;
-                }
-            }
-            // If we can't find any transaction from a sender dropped off of conflict window, then
-            // iterate through the original transactions and try to find the next candidate
-            while let Some(txn) = orig_txns.pop_front() {
-                if !sliding_window.has_conflict(&txn.sender()) {
-                    return txn;
-                }
-                pending_txns.add_transaction(txn);
-            }
-
-            // If we can't find any candidate in above steps, then lastly
-            // add pending transactions in the order if we can't find any other candidate
-            pending_txns
-                .remove_first_pending()
-                .expect("Pending should return a transaction")
-        };
         while sliding_window.num_txns() < num_transactions {
-            let txn = next_to_add(&mut sliding_window);
+            let txn = self.next_to_add(&mut sliding_window, &mut pending_txns, &mut orig_txns);
             sliding_window.add_transaction(txn)
         }
         sliding_window.finalize()
@@ -103,7 +166,9 @@ impl TransactionShuffler for SenderAwareShuffler {
 impl SenderAwareShuffler {
     pub fn new(conflict_window_size: usize) -> Self {
         Self {
-            conflict_window_size,
+            config: Config {
+                conflict_window_size,
+            },
         }
     }
 }
@@ -113,14 +178,18 @@ impl SenderAwareShuffler {
 /// so that the final block preserves the ordering of transactions by sender. It also maintains a vector
 /// to preserve the original order of the transactions. This is needed in case we can't find
 /// any non-conflicting transactions and we need to add the first pending transaction to the block.
-struct PendingTransactions {
-    txns_by_senders: HashMap<AccountAddress, VecDeque<SignedTransaction>>,
+#[derive(Debug)]
+struct PendingTransactions<Txn> {
+    txns_by_senders: HashMap<AccountAddress, VecDeque<Txn>>,
     // Transactions are kept in the original order. This is not kept in sync with pending transactions,
     // so this can contain a bunch of transactions that are already added to the block.
-    ordered_txns: VecDeque<SignedTransaction>,
+    ordered_txns: VecDeque<Txn>,
 }
 
-impl PendingTransactions {
+impl<Txn> PendingTransactions<Txn>
+where
+    Txn: SenderAwareTransaction + Clone + Debug + PartialEq,
+{
     pub fn new() -> Self {
         Self {
             txns_by_senders: HashMap::new(),
@@ -128,10 +197,10 @@ impl PendingTransactions {
         }
     }
 
-    pub fn add_transaction(&mut self, txn: SignedTransaction) {
+    pub fn add_transaction(&mut self, txn: Txn) {
         self.ordered_txns.push_back(txn.clone());
         self.txns_by_senders
-            .entry(txn.sender())
+            .entry(txn.parse_sender())
             .or_default()
             .push_back(txn);
     }
@@ -139,18 +208,15 @@ impl PendingTransactions {
     /// Removes the first pending transaction from the sender. Please note that the transaction is not
     /// removed from the `ordered_txns`, so the `ordered_txns` will contain a set of transactions that
     /// are removed from pending transactions already.
-    pub fn remove_pending_from_sender(
-        &mut self,
-        sender: AccountAddress,
-    ) -> Option<SignedTransaction> {
+    pub fn remove_pending_from_sender(&mut self, sender: AccountAddress) -> Option<Txn> {
         self.txns_by_senders
             .get_mut(&sender)
             .and_then(|txns| txns.pop_front())
     }
 
-    pub fn remove_first_pending(&mut self) -> Option<SignedTransaction> {
+    pub fn remove_first_pending(&mut self) -> Option<Txn> {
         while let Some(txn) = self.ordered_txns.pop_front() {
-            let sender = txn.sender();
+            let sender = txn.parse_sender();
             if let Some(sender_queue) = self.txns_by_senders.get(&sender) {
                 if Some(txn).as_ref() == sender_queue.front() {
                     return self.remove_pending_from_sender(sender);
@@ -165,7 +231,8 @@ impl PendingTransactions {
 /// high level, it maintains a sliding window of the conflicting transactions, which helps the payload
 /// generator include a set of transactions which are non-conflicting with each other within a particular
 /// window size.
-struct SlidingWindowState {
+#[derive(Debug)]
+struct SlidingWindowState<Txn> {
     // Please note that the start index can be negative in case the window size is larger than the
     // end_index.
     start_index: i64,
@@ -173,10 +240,13 @@ struct SlidingWindowState {
     // sender.
     senders_in_window: HashMap<AccountAddress, usize>,
     // Partially ordered transactions, needs to be updated every time add_transactions is called.
-    txns: Vec<SignedTransaction>,
+    txns: Vec<Txn>,
 }
 
-impl SlidingWindowState {
+impl<Txn> SlidingWindowState<Txn>
+where
+    Txn: SenderAwareTransaction + Debug + Clone + PartialEq,
+{
     pub fn new(window_size: usize, num_txns: usize) -> Self {
         Self {
             start_index: -(window_size as i64),
@@ -187,21 +257,21 @@ impl SlidingWindowState {
 
     /// Slides the current window. Essentially, it increments the start_index and
     /// updates the senders_in_window map if start_index is greater than 0
-    pub fn add_transaction(&mut self, txn: SignedTransaction) {
+    pub fn add_transaction(&mut self, txn: Txn) {
         if self.start_index >= 0 {
             // if the start_index is negative, then no sender falls out of the window.
             let sender = self
                 .txns
                 .get(self.start_index as usize)
                 .expect("Transaction expected")
-                .sender();
+                .parse_sender();
             self.senders_in_window
                 .entry(sender)
                 .and_modify(|count| *count -= 1);
         }
         let count = self
             .senders_in_window
-            .entry(txn.sender())
+            .entry(txn.parse_sender())
             .or_insert_with(|| 0);
         *count += 1;
         self.txns.push(txn);
@@ -221,7 +291,7 @@ impl SlidingWindowState {
             if let Some(last_sender) = self
                 .txns
                 .get(prev_start_index as usize)
-                .map(|txn| txn.sender())
+                .map(|txn| txn.parse_sender())
             {
                 if let Some(&count) = self.senders_in_window.get(&last_sender) {
                     if count == 0 {
@@ -237,7 +307,7 @@ impl SlidingWindowState {
         self.txns.len()
     }
 
-    pub fn finalize(self) -> Vec<SignedTransaction> {
+    pub fn finalize(self) -> Vec<Txn> {
         NUM_SENDERS_IN_BLOCK.set(self.senders_in_window.len() as f64);
         self.txns
     }
@@ -249,7 +319,10 @@ mod tests {
     use aptos_crypto::{ed25519::Ed25519PrivateKey, PrivateKey, SigningKey, Uniform};
     use aptos_types::{
         chain_id::ChainId,
-        transaction::{RawTransaction, Script, SignedTransaction, TransactionPayload},
+        transaction::{
+            sender_aware::SenderAwareTransaction, RawTransaction, Script, SignedTransaction,
+            TransactionPayload,
+        },
     };
     use move_core_types::account_address::AccountAddress;
     use rand::{rngs::OsRng, Rng};
@@ -306,7 +379,7 @@ mod tests {
             let mut senders = Vec::new();
             for _ in 0..num_senders {
                 let mut sender_txns = create_signed_transaction(1);
-                senders.push(sender_txns.first().unwrap().sender());
+                senders.push(sender_txns.first().unwrap().parse_sender());
                 txns.append(&mut sender_txns);
             }
             let txn_shuffer = SenderAwareShuffler::new(10);
@@ -324,7 +397,7 @@ mod tests {
         let mut senders = Vec::new();
         for _ in 0..num_senders {
             let mut sender_txns = create_signed_transaction(10);
-            senders.push(sender_txns.first().unwrap().sender());
+            senders.push(sender_txns.first().unwrap().parse_sender());
             txns.append(&mut sender_txns);
         }
 
@@ -333,7 +406,7 @@ mod tests {
         assert_eq!(txns.len(), optimized_txns.len());
         let mut sender_index = 0;
         for txn in optimized_txns {
-            assert_eq!(&txn.sender(), senders.get(sender_index).unwrap());
+            assert_eq!(&txn.parse_sender(), senders.get(sender_index).unwrap());
             sender_index = (sender_index + 1) % senders.len()
         }
     }
@@ -345,7 +418,7 @@ mod tests {
         let mut senders = Vec::new();
         for _ in 0..num_senders {
             let mut sender_txns = create_signed_transaction(10);
-            senders.push(sender_txns.first().unwrap().sender());
+            senders.push(sender_txns.first().unwrap().parse_sender());
             txns.append(&mut sender_txns);
         }
 
@@ -365,7 +438,10 @@ mod tests {
         let mut orig_txns_by_sender = HashMap::new();
         for _ in 0..num_senders {
             let mut sender_txns = create_signed_transaction(rng.gen_range(1, max_txn_per_sender));
-            orig_txns_by_sender.insert(sender_txns.first().unwrap().sender(), sender_txns.clone());
+            orig_txns_by_sender.insert(
+                sender_txns.first().unwrap().parse_sender(),
+                sender_txns.clone(),
+            );
             orig_txns.append(&mut sender_txns);
         }
         let txn_shuffler = SenderAwareShuffler::new(num_senders - 1);
@@ -373,7 +449,7 @@ mod tests {
         let mut optimized_txns_by_sender = HashMap::new();
         for txn in optimized_txns {
             optimized_txns_by_sender
-                .entry(txn.sender())
+                .entry(txn.parse_sender())
                 .or_insert_with(Vec::new)
                 .push(txn);
         }
@@ -513,7 +589,7 @@ mod tests {
         let mut orig_txn_set = HashSet::new();
         for _ in 0..num_senders {
             let mut sender_txns = create_signed_transaction(rng.gen_range(1, max_txn_per_sender));
-            senders.push(sender_txns.first().unwrap().sender());
+            senders.push(sender_txns.first().unwrap().parse_sender());
             orig_txns.append(&mut sender_txns);
         }
         for txn in orig_txns.clone() {
@@ -545,7 +621,7 @@ mod tests {
         let mut senders = Vec::new();
         for _ in 0..num_senders {
             let mut sender_txns = create_signed_transaction(rng.gen_range(1, max_txn_per_sender));
-            senders.push(sender_txns.first().unwrap().sender());
+            senders.push(sender_txns.first().unwrap().parse_sender());
             orig_txns.append(&mut sender_txns);
         }
 
