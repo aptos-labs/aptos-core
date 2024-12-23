8aa53e8937a4b027fe226b65d57e6524db308188
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-12-23 17:21:10 +0100

    add licenses to new files

--- third_party/move/move-compiler-v2/src/diagnostics/human.rs
+++ third_party/move/move-compiler-v2/src/diagnostics/human.rs
@@ -1,3 +1,6 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::diagnostics::Emitter;
 use codespan::{FileId, Files};
 use codespan_reporting::{
--- third_party/move/move-compiler-v2/src/diagnostics/mod.rs
+++ third_party/move/move-compiler-v2/src/diagnostics/mod.rs
@@ -1,3 +1,6 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::{
     diagnostics::{human::HumanEmitter, json::JsonEmitter},
     options, Experiment,
