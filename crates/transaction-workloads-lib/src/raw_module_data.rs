2aabb74c4952a7163aa804cbea71ede9ead8e157
Igor <igor-aptos@users.noreply.github.com>
2025-01-09 22:35:29 -0800

    addressing comments

--- crates/transaction-workloads-lib/src/raw_module_data.rs
+++ crates/transaction-workloads-lib/src/raw_module_data.rs
@@ -12,9 +12,9 @@
 // This crate should also provide a Rust file that allows proper manipulation of each
 // module defined below.
 
+use aptos_transaction_generator_lib::entry_point_trait::PreBuiltPackages;
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
-use aptos_transaction_generator_lib::entry_point_trait::PreBuiltPackages;
 
 #[rustfmt::skip]
 pub static PACKAGE_SIMPLE_METADATA: Lazy<Vec<u8>> = Lazy::new(|| {
