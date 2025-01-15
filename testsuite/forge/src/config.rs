4e560107406937b11e70d006d527f743fb57c67b
aldenhu <msmouse@gmail.com>
2025-01-15 10:56:37 -0800

    disable the new pipeline for forge

--- testsuite/forge/src/config.rs
+++ testsuite/forge/src/config.rs
@@ -170,7 +170,6 @@ impl ForgeConfig {
         let existing_db_tag = self.existing_db_tag.clone();
         let validator_resource_override = self.validator_resource_override;
         let fullnode_resource_override = self.fullnode_resource_override;
-        let suite_name = self.get_suite_name();
 
         // Override specific helm values. See reference: terraform/helm/aptos-node/values.yaml
         Some(Arc::new(move |helm_values: &mut serde_yaml::Value| {
@@ -239,21 +238,6 @@ impl ForgeConfig {
                 ["enable_storage_sharding"] = true.into();
             helm_values["validator"]["config"]["indexer_db_config"]["enable_event"] = true.into();
             helm_values["fullnode"]["config"]["indexer_db_config"]["enable_event"] = true.into();
-
-            // This is a temporary hack to disable new pipeline for compat tests.
-            if !suite_name
-                .as_ref()
-                .is_some_and(|name| name.eq_ignore_ascii_case("compat"))
-            {
-                // enable new pipeline
-                helm_values["validator"]["config"]["consensus"]["enable_pipeline"] = true.into();
-                helm_values["fullnode"]["config"]["consensus_observer"]["enable_pipeline"] =
-                    true.into();
-            }
-
-            // enable optqs
-            helm_values["validator"]["config"]["consensus"]["quorum_store"]
-                ["enable_opt_quorum_store"] = true.into();
         }))
     }
 
