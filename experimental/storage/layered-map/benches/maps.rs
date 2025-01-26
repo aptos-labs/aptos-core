ee2732c908177b463bc3606a7eb7b77b46d77b3e
aldenhu <msmouse@gmail.com>
2025-01-25 21:41:18 -0800

    cache the map between get_existing / get_non_existing benches

--- experimental/storage/layered-map/benches/maps.rs
+++ experimental/storage/layered-map/benches/maps.rs
@@ -71,7 +71,7 @@ fn gen_update_batches(
         .entry(n_batches)
         .or_insert_with(|| {
             println!();
-            println!("Generating batch. {batch_size_k}k per batch, {n_batches} batches.");
+            println!("    Generating batch. {batch_size_k}k per batch, {n_batches} batches.");
             let timer = std::time::Instant::now();
             let ret = repeat_with(|| {
                 repeat_with(|| (random(), random()))
@@ -80,7 +80,7 @@ fn gen_update_batches(
             })
             .take(n_batches)
             .collect_vec();
-            println!("done in {} secs.", timer.elapsed().as_secs());
+            println!("    Done in {} secs.", timer.elapsed().as_secs());
             ret
         })
 }
@@ -148,124 +148,179 @@ fn get(
     batch_cache: &mut BatchCache,
     keys_cache: &mut KeysCache,
     map_size_k: usize,
-    existing: bool,
 ) {
     let n_keys_to_get = map_size_k.min(10) * K;
 
     group.throughput(criterion::Throughput::Elements(n_keys_to_get as u64));
 
-    let name = format!("hash_map_{map_size_k}k_items");
+    get_hash_map(group, batch_cache, keys_cache, map_size_k, n_keys_to_get);
+
+    get_btree_map(group, batch_cache, keys_cache, map_size_k, n_keys_to_get);
+
+    get_layered_map(group, batch_cache, keys_cache, map_size_k, n_keys_to_get);
+
+    get_rocksdb(group, batch_cache, keys_cache, map_size_k, n_keys_to_get);
+}
+
+fn get_hash_map(
+    group: &mut BenchmarkGroup<WallTime>,
+    batch_cache: &mut BatchCache,
+    keys_cache: &mut KeysCache,
+    map_size_k: usize,
+    n_keys_to_get: usize,
+) {
     let map: OnceCell<HashMap<Key, Value>> = OnceCell::new();
-    let keys: OnceCell<Vec<Key>> = OnceCell::new();
 
-    group.bench_function(&name, |b| {
-        b.iter_batched(
-            || {
-                let (items, keys_) =
-                    gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
-                let map = map.get_or_init(|| items.iter().cloned().collect());
-                let keys = keys.get_or_init(|| keys_.clone());
-                (map, keys)
-            },
-            |(map, keys)| keys.iter().map(|key| map.get(key)).collect_vec(),
-            BatchSize::SmallInput,
-        )
-    });
+    for (prefix, existing) in [("existing", true), ("non_existing", false)] {
+        let name = format!("{prefix}/hash_map_{map_size_k}k_items");
+        let keys: OnceCell<Vec<Key>> = OnceCell::new();
 
-    let name = format!("btree_map_{map_size_k}k_items");
+        group.bench_function(&name, |b| {
+            b.iter_batched(
+                || {
+                    let (items, keys_) =
+                        gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
+                    let map = map.get_or_init(|| items.iter().cloned().collect());
+                    let keys = keys.get_or_init(|| keys_.clone());
+                    (map, keys)
+                },
+                |(map, keys)| keys.iter().map(|key| map.get(key)).collect_vec(),
+                BatchSize::SmallInput,
+            )
+        });
+    }
+}
+
+fn get_btree_map(
+    group: &mut BenchmarkGroup<WallTime>,
+    batch_cache: &mut BatchCache,
+    keys_cache: &mut KeysCache,
+    map_size_k: usize,
+    n_keys_to_get: usize,
+) {
     let map: OnceCell<BTreeMap<Key, Value>> = OnceCell::new();
-    let keys: OnceCell<Vec<Key>> = OnceCell::new();
 
-    group.bench_function(&name, |b| {
-        b.iter_batched(
-            || {
-                let (items, keys_) =
-                    gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
-                let map = map.get_or_init(|| items.iter().cloned().collect());
-                let keys = keys.get_or_init(|| keys_.clone());
-                (map, keys)
-            },
-            |(map, keys)| keys.iter().map(|key| map.get(key)).collect_vec(),
-            BatchSize::SmallInput,
-        )
-    });
+    for (prefix, existing) in [("existing", true), ("non_existing", false)] {
+        let name = format!("{prefix}/btree_map_{map_size_k}k_items");
+        let keys: OnceCell<Vec<Key>> = OnceCell::new();
 
-    let name = format!("layered_map_{map_size_k}k_items");
+        group.bench_function(&name, |b| {
+            b.iter_batched(
+                || {
+                    let (items, keys_) =
+                        gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
+                    let map = map.get_or_init(|| items.iter().cloned().collect());
+                    let keys = keys.get_or_init(|| keys_.clone());
+                    (map, keys)
+                },
+                |(map, keys)| keys.iter().map(|key| map.get(key)).collect_vec(),
+                BatchSize::SmallInput,
+            )
+        });
+    }
+}
+
+fn get_layered_map(
+    group: &mut BenchmarkGroup<WallTime>,
+    batch_cache: &mut BatchCache,
+    keys_cache: &mut KeysCache,
+    map_size_k: usize,
+    n_keys_to_get: usize,
+) {
     let map: OnceCell<LayeredMap<Key, Value>> = OnceCell::new();
-    let keys: OnceCell<Vec<Key>> = OnceCell::new();
-    group.bench_function(&name, |b| {
-        b.iter_batched(
-            || {
-                let (items, keys_) =
-                    gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
-                let map = map.get_or_init(|| {
-                    let root_layer = MapLayer::new_family("bench");
-                    let top_layer = root_layer.view_layers_after(&root_layer).new_layer(items);
-                    top_layer.into_layers_view_after(root_layer)
-                });
-                let keys = keys.get_or_init(|| keys_.clone());
-                (map, keys)
-            },
-            |(map, keys)| keys.iter().map(|key| map.get(key)).collect_vec(),
-            BatchSize::SmallInput,
-        )
-    });
+    for (prefix, existing) in [("existing", true), ("non_existing", false)] {
+        let name = format!("{prefix}/layered_map_{map_size_k}k_items");
+        let keys: OnceCell<Vec<Key>> = OnceCell::new();
 
-    let name = format!("rocksdb_{map_size_k}k_items");
+        group.bench_function(&name, |b| {
+            b.iter_batched(
+                || {
+                    let (items, keys_) =
+                        gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
+                    let map = map.get_or_init(|| {
+                        let root_layer = MapLayer::new_family("bench");
+                        let top_layer = root_layer.view_layers_after(&root_layer).new_layer(items);
+                        top_layer.into_layers_view_after(root_layer)
+                    });
+                    let keys = keys.get_or_init(|| keys_.clone());
+                    (map, keys)
+                },
+                |(map, keys)| keys.iter().map(|key| map.get(key)).collect_vec(),
+                BatchSize::SmallInput,
+            )
+        });
+    }
+}
+
+fn get_rocksdb(
+    group: &mut BenchmarkGroup<WallTime>,
+    batch_cache: &mut BatchCache,
+    keys_cache: &mut KeysCache,
+    map_size_k: usize,
+    n_keys_to_get: usize,
+) {
     let map: OnceCell<DB> = OnceCell::new();
-    let keys: OnceCell<Vec<Key>> = OnceCell::new();
-    group.bench_function(&name, |b| {
-        b.iter_batched(
-            || {
-                let (items, keys_) =
-                    gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
-                let keys = keys.get_or_init(|| keys_.clone());
-                let map = map.get_or_init(|| {
-                    println!();
-                    println!("    Opening rocksdb.");
-                    let mut db_path = TempPath::new();
-                    db_path.persist(); // leak the temp dir
 
-                    let mut options = rocksdb::Options::default();
-                    options.create_if_missing(true);
+    for (prefix, existing) in [("existing", true), ("non_existing", false)] {
+        let name = format!("{prefix}/rocksdb_{map_size_k}k_items");
+        let keys: OnceCell<Vec<Key>> = OnceCell::new();
+        group.bench_function(&name, |b| {
+            b.iter_batched(
+                || {
+                    let (items, keys_) =
+                        gen_get(batch_cache, keys_cache, map_size_k, n_keys_to_get, existing);
+                    let keys = keys.get_or_init(|| keys_.clone());
+                    let map = map.get_or_init(|| {
+                        println!();
+                        println!("    Opening rocksdb.");
+                        let mut db_path = TempPath::new();
+                        db_path.persist(); // leak the temp dir
 
-                    let db = DB::open(
-                        &db_path,
-                        "bench",
-                        vec![DEFAULT_COLUMN_FAMILY_NAME],
-                        &options,
-                    )
-                    .unwrap();
+                        let mut options = rocksdb::Options::default();
+                        options.create_if_missing(true);
 
-                    items.iter().chunks(1_000_000).into_iter().for_each(|kvs| {
-                        println!("    Inserting.");
-                        let mut batch = db.new_native_batch();
-                        kvs.for_each(|(key, value)| batch.put::<DbMap>(key, value).unwrap());
-                        db.write_schemas(batch).unwrap();
+                        let db = DB::open(
+                            &db_path,
+                            "bench",
+                            vec![DEFAULT_COLUMN_FAMILY_NAME],
+                            &options,
+                        )
+                        .unwrap();
+
+                        let mut total = 0;
+                        items.iter().chunks(1_000_000).into_iter().for_each(|kvs| {
+                            let mut batch = db.new_native_batch();
+                            total += kvs
+                                .map(|(key, value)| batch.put::<DbMap>(key, value).unwrap())
+                                .count();
+                            db.write_schemas(batch).unwrap();
+
+                            println!("    Inserted {total}.");
+                        });
+
+                        println!("    Flushing mem-tables.");
+                        db.flush_cf(DEFAULT_COLUMN_FAMILY_NAME).unwrap();
+
+                        println!("    Preheat all keys to be queried.");
+                        keys.iter().for_each(|key| {
+                            db.get::<DbMap>(key).unwrap();
+                        });
+
+                        println!("    RocksDB ready.");
+                        db
                     });
 
-                    println!("    Flushing to mem-tables.");
-                    db.flush_cf(DEFAULT_COLUMN_FAMILY_NAME).unwrap();
-
-                    println!("    Preheat all keys to be queried.");
-                    keys.iter().for_each(|key| {
-                        db.get::<DbMap>(key).unwrap();
-                    });
-
-                    println!("    RocksDB ready.");
-                    db
-                });
-
-                (map, keys)
-            },
-            |(map, keys)| {
-                keys.iter()
-                    .map(|key| map.get::<DbMap>(key).unwrap())
-                    .collect_vec()
-            },
-            BatchSize::SmallInput,
-        )
-    });
+                    (map, keys)
+                },
+                |(map, keys)| {
+                    keys.iter()
+                        .map(|key| map.get::<DbMap>(key).unwrap())
+                        .collect_vec()
+                },
+                BatchSize::SmallInput,
+            )
+        });
+    }
 }
 
 type KeysCache = HashMap<usize, HashMap<bool, Vec<Key>>>;
@@ -307,28 +362,9 @@ fn compare_maps(c: &mut Criterion) {
     }
 
     {
-        let mut group = c.benchmark_group("get_existing");
+        let mut group = c.benchmark_group("get");
         for map_size_k in [100, 1000, 4_000, 16_000, 128_000] {
-            get(
-                &mut group,
-                &mut batch_cache,
-                &mut keys_cache,
-                map_size_k,
-                true,
-            );
-        }
-    }
-
-    {
-        let mut group = c.benchmark_group("get_non_existing");
-        for map_size_k in [100, 1000, 4_000, 16_000, 128_000] {
-            get(
-                &mut group,
-                &mut batch_cache,
-                &mut keys_cache,
-                map_size_k,
-                false,
-            );
+            get(&mut group, &mut batch_cache, &mut keys_cache, map_size_k);
         }
     }
 }
