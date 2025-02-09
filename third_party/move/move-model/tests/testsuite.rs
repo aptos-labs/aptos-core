a2d0299332b12a834cad51d6e5403abc047189b8
Teng Zhang <rahxephon89@163.com>
2025-02-08 16:46:43 -0800

    remove v1 from model

--- third_party/move/move-model/tests/testsuite.rs
+++ third_party/move/move-model/tests/testsuite.rs
@@ -4,25 +4,39 @@
 
 use codespan_reporting::{diagnostic::Severity, term::termcolor::Buffer};
 use move_command_line_common::testing::get_compiler_exp_extension;
-use move_compiler::shared::{known_attributes::KnownAttribute, PackagePaths};
-use move_model::{options::ModelBuilderOptions, run_model_builder_with_options};
+use move_compiler::shared::known_attributes::KnownAttribute;
+use move_model::{
+    metadata::LanguageVersion, options::ModelBuilderOptions, run_model_builder_in_compiler_mode,
+    PackageInfo,
+};
 use move_prover_test_utils::baseline_test::verify_or_update_baseline;
 use std::path::Path;
 
 fn test_runner(path: &Path, options: ModelBuilderOptions) -> datatest_stable::Result<()> {
-    let targets = vec![PackagePaths {
-        name: None,
-        paths: vec![path.to_str().unwrap().to_string()],
-        named_address_map: std::collections::BTreeMap::<String, _>::new(),
-    }];
-    let env = run_model_builder_with_options(
-        targets,
-        vec![],
-        vec![],
-        options,
+    let mut options = options;
+    options.compile_via_model = true;
+    let source_info = PackageInfo {
+        sources: vec![path.to_str().unwrap().to_string()],
+        address_map: std::collections::BTreeMap::<String, _>::new(),
+    };
+    let source_dep_info = PackageInfo {
+        sources: vec![],
+        address_map: std::collections::BTreeMap::<String, _>::new(),
+    };
+    let dep_info = vec![];
+    let env = run_model_builder_in_compiler_mode(
+        source_info,
+        source_dep_info,
+        dep_info,
         false,
         KnownAttribute::get_all_attribute_names(),
+        LanguageVersion::latest(),
+        false,
+        false,
+        false,
+        true,
     )?;
+
     let diags = if env.diag_count(Severity::Warning) > 0 {
         let mut writer = Buffer::no_color();
         env.report_diag(&mut writer, Severity::Warning);
