8f551ef3860a4d68b453beef63e1820600443fb3
Jeeyong Um <conr2d@proton.me>
2025-02-18 14:28:16 +0900

    [consensus] fix typo

--- consensus/src/epoch_manager.rs
+++ consensus/src/epoch_manager.rs
@@ -1262,7 +1262,7 @@ impl<P: OnChainConfigProvider> EpochManager<P> {
             )
             .await
         } else {
-            self.start_new_epoch_with_joltean(
+            self.start_new_epoch_with_jolteon(
                 loaded_consensus_key.clone(),
                 epoch_state,
                 consensus_config,
@@ -1316,7 +1316,7 @@ impl<P: OnChainConfigProvider> EpochManager<P> {
         )
     }
 
-    async fn start_new_epoch_with_joltean(
+    async fn start_new_epoch_with_jolteon(
         &mut self,
         consensus_key: Arc<PrivateKey>,
         epoch_state: Arc<EpochState>,
