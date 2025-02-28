f35071808796b26e5b97c0caad842ce0578cb655
George Mitenkov <georgemitenk0v@gmail.com>
2025-02-28 23:46:57 +0000

    [7/n] Tests to use V2 loader
      - VM tests to use V2 loader
      - Genesis to use V2 loader
      - Remove config from VM, publish from session (pt 1)
      - Use V2 loader for randomness annotation

--- third_party/move/move-vm/runtime/src/move_vm.rs
+++ third_party/move/move-vm/runtime/src/move_vm.rs
@@ -3,21 +3,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    config::VMConfig,
     data_cache::TransactionDataCache,
-    loader::{LegacyModuleStorage, LegacyModuleStorageAdapter, Loader},
+    loader::{LegacyModuleStorage, LegacyModuleStorageAdapter},
     native_extensions::NativeContextExtensions,
     runtime::VMRuntime,
     session::Session,
     RuntimeEnvironment,
 };
-use move_binary_format::{
-    errors::{Location, PartialVMError, VMResult},
-    CompiledModule,
-};
-use move_core_types::{language_storage::ModuleId, metadata::Metadata, vm_status::StatusCode};
+use move_core_types::{language_storage::ModuleId, metadata::Metadata};
 use move_vm_types::resolver::MoveResolver;
-use std::{ops::Deref, sync::Arc};
 
 pub struct MoveVM {
     pub(crate) runtime: VMRuntime,
@@ -29,11 +23,6 @@ impl MoveVM {
         Self {
             runtime: VMRuntime::new(runtime_environment),
         }
-    }
-
-    /// Returns VM configuration used to initialize the VM.
-    pub fn vm_config(&self) -> &VMConfig {
-        self.runtime.loader().vm_config()
     }
 
     /// Create a new Session backed by the given storage.
@@ -72,40 +61,6 @@ impl MoveVM {
             ),
             module_store: LegacyModuleStorageAdapter::new(self.runtime.module_storage_v1()),
             native_extensions,
-        }
-    }
-
-    /// DO NOT USE THIS API!
-    ///
-    /// Existing uses of this API is to fetch metadata from compiled modules on the client
-    /// side. With loader V2 design clients can fetch it directly from the module storage.
-    #[deprecated]
-    pub fn load_module(
-        &self,
-        module_id: &ModuleId,
-        remote: &impl MoveResolver,
-    ) -> VMResult<Arc<CompiledModule>> {
-        match self.runtime.loader() {
-            Loader::V1(loader) => {
-                let module = loader.load_module(
-                    module_id,
-                    &mut TransactionDataCache::new(
-                        self.runtime
-                            .loader()
-                            .vm_config()
-                            .deserializer_config
-                            .clone(),
-                        remote,
-                    ),
-                    &LegacyModuleStorageAdapter::new(self.runtime.module_storage_v1()),
-                )?;
-                Ok(module.as_ref().deref().clone())
-            },
-            Loader::V2(_) => Err(PartialVMError::new(
-                StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR,
-            )
-            .with_message("Loader V2 implementation never calls move_vm::load_module".to_string())
-            .finish(Location::Undefined)),
         }
     }
 
