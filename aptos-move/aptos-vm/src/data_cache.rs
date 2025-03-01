fe4762d1b67746ddc816594e6d3a9fcbd6c2c60a
George Mitenkov <georgemitenk0v@gmail.com>
2025-03-01 12:19:59 +0000

    [13/n] Remove ModuleResolver as obsolete

--- aptos-move/aptos-vm/src/data_cache.rs
+++ aptos-move/aptos-vm/src/data_cache.rs
@@ -24,9 +24,7 @@ use aptos_types::{
         StateView, StateViewId,
     },
 };
-use aptos_vm_environment::{
-    gas::get_gas_feature_version, prod_configs::aptos_prod_deserializer_config,
-};
+use aptos_vm_environment::gas::get_gas_feature_version;
 use aptos_vm_types::{
     resolver::{
         ExecutorView, ResourceGroupSize, ResourceGroupView, StateStorageView, TResourceGroupView,
@@ -34,16 +32,14 @@ use aptos_vm_types::{
     resource_group_adapter::ResourceGroupAdapter,
 };
 use bytes::Bytes;
-use move_binary_format::{deserializer::DeserializerConfig, errors::*, CompiledModule};
+use move_binary_format::errors::*;
 use move_core_types::{
-    account_address::AccountAddress,
-    language_storage::{ModuleId, StructTag},
-    metadata::Metadata,
+    account_address::AccountAddress, language_storage::StructTag, metadata::Metadata,
     value::MoveTypeLayout,
 };
 use move_vm_types::{
     delayed_values::delayed_field_id::DelayedFieldID,
-    resolver::{resource_size, ModuleResolver, ResourceResolver},
+    resolver::{resource_size, ResourceResolver},
 };
 use std::{
     cell::RefCell,
@@ -70,7 +66,6 @@ pub fn get_resource_group_member_from_metadata(
 /// for (non-group) resources and subsequent handling in the StorageAdapter itself.
 pub struct StorageAdapter<'e, E> {
     executor_view: &'e E,
-    deserializer_config: DeserializerConfig,
     resource_group_view: ResourceGroupAdapter<'e>,
     accessed_groups: RefCell<HashSet<StateKey>>,
 }
@@ -82,7 +77,6 @@ impl<'e, E: ExecutorView> StorageAdapter<'e, E> {
         features: &Features,
         maybe_resource_group_view: Option<&'e dyn ResourceGroupView>,
     ) -> Self {
-        let deserializer_config = aptos_prod_deserializer_config(features);
         let resource_group_adapter = ResourceGroupAdapter::new(
             maybe_resource_group_view,
             executor_view,
@@ -90,17 +84,12 @@ impl<'e, E: ExecutorView> StorageAdapter<'e, E> {
             features.is_resource_groups_split_in_vm_change_set_enabled(),
         );
 
-        Self::new(executor_view, deserializer_config, resource_group_adapter)
+        Self::new(executor_view, resource_group_adapter)
     }
 
-    fn new(
-        executor_view: &'e E,
-        deserializer_config: DeserializerConfig,
-        resource_group_view: ResourceGroupAdapter<'e>,
-    ) -> Self {
+    fn new(executor_view: &'e E, resource_group_view: ResourceGroupAdapter<'e>) -> Self {
         Self {
             executor_view,
-            deserializer_config,
             resource_group_view,
             accessed_groups: RefCell::new(HashSet::new()),
         }
@@ -181,27 +170,6 @@ impl<'e, E: ExecutorView> ResourceResolver for StorageAdapter<'e, E> {
         maybe_layout: Option<&MoveTypeLayout>,
     ) -> PartialVMResult<(Option<Bytes>, usize)> {
         self.get_any_resource_with_layout(address, struct_tag, metadata, maybe_layout)
-    }
-}
-
-impl<'e, E: ExecutorView> ModuleResolver for StorageAdapter<'e, E> {
-    fn get_module_metadata(&self, module_id: &ModuleId) -> Vec<Metadata> {
-        let module_bytes = match self.get_module(module_id) {
-            Ok(Some(bytes)) => bytes,
-            _ => return vec![],
-        };
-        let module =
-            match CompiledModule::deserialize_with_config(&module_bytes, &self.deserializer_config)
-            {
-                Ok(module) => module,
-                _ => return vec![],
-            };
-        module.metadata
-    }
-
-    fn get_module(&self, module_id: &ModuleId) -> PartialVMResult<Option<Bytes>> {
-        self.executor_view
-            .get_module_bytes(&StateKey::module_id(module_id))
     }
 }
 
@@ -298,8 +266,6 @@ pub trait AsMoveResolver<S> {
 impl<S: StateView> AsMoveResolver<S> for S {
     fn as_move_resolver(&self) -> StorageAdapter<S> {
         let features = Features::fetch_config(self).unwrap_or_default();
-        let deserializer_config = aptos_prod_deserializer_config(&features);
-
         let gas_feature_version = get_gas_feature_version(self);
         let resource_group_adapter = ResourceGroupAdapter::new(
             None,
@@ -307,7 +273,7 @@ impl<S: StateView> AsMoveResolver<S> for S {
             gas_feature_version,
             features.is_resource_groups_split_in_vm_change_set_enabled(),
         );
-        StorageAdapter::new(self, deserializer_config, resource_group_adapter)
+        StorageAdapter::new(self, resource_group_adapter)
     }
 }
 
@@ -368,8 +334,6 @@ pub(crate) mod tests {
             resource_groups_split_in_vm_change_set_enabled,
         );
 
-        let features = Features::fetch_config(state_view).unwrap_or_default();
-        let deserializer_config = aptos_prod_deserializer_config(&features);
-        StorageAdapter::new(state_view, deserializer_config, group_adapter)
+        StorageAdapter::new(state_view, group_adapter)
     }
 }
--- third_party/move/move-vm/runtime/src/move_vm.rs
+++ third_party/move/move-vm/runtime/src/move_vm.rs
@@ -6,7 +6,7 @@ use crate::{
     data_cache::TransactionDataCache, native_extensions::NativeContextExtensions,
     runtime::VMRuntime, session::Session,
 };
-use move_vm_types::resolver::MoveResolver;
+use move_vm_types::resolver::ResourceResolver;
 
 pub struct MoveVM {
     pub(crate) runtime: VMRuntime,
@@ -35,14 +35,14 @@ impl MoveVM {
     ///     cases where this may not be necessary, with the most notable one being the common module
     ///     publishing flow: you can keep using the same Move VM if you publish some modules in a Session
     ///     and apply the effects to the storage when the Session ends.
-    pub fn new_session<'r>(&self, remote: &'r impl MoveResolver) -> Session<'r, '_> {
+    pub fn new_session<'r>(&self, remote: &'r impl ResourceResolver) -> Session<'r, '_> {
         self.new_session_with_extensions(remote, NativeContextExtensions::default())
     }
 
     /// Create a new session, as in `new_session`, but provide native context extensions.
     pub fn new_session_with_extensions<'r>(
         &self,
-        remote: &'r impl MoveResolver,
+        remote: &'r impl ResourceResolver,
         native_extensions: NativeContextExtensions<'r>,
     ) -> Session<'r, '_> {
         Session {
