225dcccb2010cf695720d06316184d275f77f880
George Mitenkov <georgemitenk0v@gmail.com>
2025-03-01 10:15:45 +0000

    [8/n] Remove deprecated APIs
     Removes deprecated APIs from runtime, session, data cache,
     Move and Aptos VM.

--- third_party/move/move-vm/runtime/src/runtime.rs
+++ third_party/move/move-vm/runtime/src/runtime.rs
@@ -15,16 +15,10 @@ use crate::{
     AsFunctionValueExtension, LayoutConverter, RuntimeEnvironment, StorageLayoutConverter,
 };
 use move_binary_format::{
-    access::ModuleAccess,
-    compatibility::Compatibility,
-    errors::{verification_error, Location, PartialVMError, PartialVMResult, VMResult},
+    errors::{Location, PartialVMError, PartialVMResult, VMResult},
     file_format::LocalIndex,
-    normalized, CompiledModule, IndexKind,
 };
-use move_core_types::{
-    account_address::AccountAddress, language_storage::TypeTag, value::MoveTypeLayout,
-    vm_status::StatusCode,
-};
+use move_core_types::{language_storage::TypeTag, value::MoveTypeLayout, vm_status::StatusCode};
 use move_vm_metrics::{Timer, VM_TIMER};
 use move_vm_types::{
     gas::GasMeter,
@@ -32,7 +26,7 @@ use move_vm_types::{
     value_serde::ValueSerDeContext,
     values::{Locals, Reference, VMValueCast, Value},
 };
-use std::{borrow::Borrow, collections::BTreeSet, sync::Arc};
+use std::{borrow::Borrow, sync::Arc};
 
 /// An instantiation of the MoveVM.
 pub(crate) struct VMRuntime {
@@ -58,176 +52,6 @@ impl VMRuntime {
             //   remove it in the future together with other V1 components.
             module_cache: Arc::new(LegacyModuleCache::new()),
         }
-    }
-
-    #[deprecated]
-    pub(crate) fn publish_module_bundle(
-        &self,
-        modules: Vec<Vec<u8>>,
-        sender: AccountAddress,
-        data_store: &mut TransactionDataCache,
-        module_store: &LegacyModuleStorageAdapter,
-        _gas_meter: &mut impl GasMeter,
-        compat: Compatibility,
-    ) -> VMResult<()> {
-        // We must be using V1 loader flow here.
-        let loader = match &self.loader {
-            Loader::V1(loader) => loader,
-            Loader::V2(_) => {
-                let msg =
-                    "Loader V2 cannot be used in V1 runtime::publish_module_bundle".to_string();
-                return Err(
-                    PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
-                        .with_message(msg)
-                        .finish(Location::Undefined),
-                );
-            },
-        };
-
-        // Deserialize the modules. Perform bounds check. After this indices can be
-        // used with the `[]` operator
-        let compiled_modules = match modules
-            .iter()
-            .map(|blob| {
-                CompiledModule::deserialize_with_config(
-                    blob,
-                    &loader.vm_config().deserializer_config,
-                )
-            })
-            .collect::<PartialVMResult<Vec<_>>>()
-        {
-            Ok(modules) => modules,
-            Err(err) => {
-                return Err(err
-                    .append_message_with_separator(
-                        '\n',
-                        "[VM] module deserialization failed".to_string(),
-                    )
-                    .finish(Location::Undefined));
-            },
-        };
-
-        // Make sure all modules' self addresses matches the transaction sender. The self address is
-        // where the module will actually be published. If we did not check this, the sender could
-        // publish a module under anyone's account.
-        for module in &compiled_modules {
-            if module.address() != &sender {
-                return Err(verification_error(
-                    StatusCode::MODULE_ADDRESS_DOES_NOT_MATCH_SENDER,
-                    IndexKind::AddressIdentifier,
-                    module.self_handle_idx().0,
-                )
-                .finish(Location::Undefined));
-            }
-        }
-
-        // Collect ids for modules that are published together
-        let mut bundle_unverified = BTreeSet::new();
-
-        // For now, we assume that all modules can be republished, as long as the new module is
-        // backward compatible with the old module.
-        //
-        // TODO: in the future, we may want to add restrictions on module republishing, possibly by
-        // changing the bytecode format to include an `is_upgradable` flag in the CompiledModule.
-        for module in &compiled_modules {
-            let module_id = module.self_id();
-
-            #[allow(deprecated)]
-            if data_store.exists_module(&module_id)? && compat.need_check_compat() {
-                let old_module_ref = loader.load_module(&module_id, data_store, module_store)?;
-                let old_module = old_module_ref.as_ref().as_ref();
-                if loader.vm_config().use_compatibility_checker_v2 {
-                    compat
-                        .check(old_module_ref.as_ref().as_ref(), module)
-                        .map_err(|e| e.finish(Location::Undefined))?
-                } else {
-                    #[allow(deprecated)]
-                    let old_m = normalized::Module::new(old_module)
-                        .map_err(|e| e.finish(Location::Undefined))?;
-                    #[allow(deprecated)]
-                    let new_m = normalized::Module::new(module)
-                        .map_err(|e| e.finish(Location::Undefined))?;
-                    compat
-                        .legacy_check(&old_m, &new_m)
-                        .map_err(|e| e.finish(Location::Undefined))?
-                }
-            }
-            if !bundle_unverified.insert(module_id) {
-                return Err(PartialVMError::new(StatusCode::DUPLICATE_MODULE_NAME)
-                    .finish(Location::Undefined));
-            }
-        }
-
-        // Perform bytecode and loading verification. Modules must be sorted in topological order.
-        loader.verify_module_bundle_for_publication(&compiled_modules, data_store, module_store)?;
-
-        // NOTE: we want to (informally) argue that all modules pass the linking check before being
-        // published to the data store.
-        //
-        // The linking check consists of two checks actually
-        // - dependencies::verify_module(module, all_imm_deps)
-        // - cyclic_dependencies::verify_module(module, fn_imm_deps, fn_imm_friends)
-        //
-        // [Claim 1]
-        // We show that the `dependencies::verify_module` check is always satisfied whenever a
-        // module M is published or updated and the `all_imm_deps` contains the actual modules
-        // required by M.
-        //
-        // Suppose M depends on D, and we now consider the following scenarios:
-        // 1) D does not appear in the bundle together with M
-        // -- In this case, D must be either in the code cache or in the data store which can be
-        //    loaded into the code cache (and pass all checks on D).
-        //    - If D is missing, the linking will fail and return an error.
-        //    - If D exists, D will be added to the `all_imm_deps` arg when checking M.
-        //
-        // 2) D appears in the bundle *before* M
-        // -- In this case, regardless of whether D is in code cache or not, D will be put into the
-        //    `bundle_verified` argument and modules in `bundle_verified` will be prioritized before
-        //    returning a module in code cache.
-        //
-        // 3) D appears in the bundle *after* M
-        // -- This technically should be discouraged but this is user input so we cannot have this
-        //    assumption here. But nevertheless, we can still make the claim 1 even in this case.
-        //    When M is verified, flow 1) is effectively activated, which means:
-        //    - If the code cache or the data store does not contain a D' which has the same name
-        //      with D, then the linking will fail and return an error.
-        //    - If D' exists, and M links against D', then when verifying D in a later time point,
-        //      a compatibility check will be invoked to ensure that D is compatible with D',
-        //      meaning, whichever module that links against D' will have to link against D as well.
-        //
-        // [Claim 2]
-        // We show that the `cyclic_dependencies::verify_module` check is always satisfied whenever
-        // a module M is published or updated and the dep/friend modules returned by the transitive
-        // dependency closure functions are valid.
-        //
-        // Currently, the code is written in a way that, from the view point of the
-        // `cyclic_dependencies::verify_module` check, modules checked prior to module M in the same
-        // bundle looks as if they have already been published and loaded to the code cache.
-        //
-        // Therefore, if M forms a cyclic dependency with module A in the same bundle that is
-        // checked prior to M, such an error will be detected. However, if M forms a cyclic
-        // dependency with a module X that appears in the same bundle *after* M. The cyclic
-        // dependency can only be caught when X is verified.
-        //
-        // In summary: the code is written in a way that, certain checks are skipped while checking
-        // each individual module in the bundle in order. But if every module in the bundle pass
-        // all the checks, then the whole bundle can be published/upgraded together. Otherwise,
-        // none of the module can be published/updated.
-
-        // All modules verified, publish them to data cache
-        for (module, blob) in compiled_modules.into_iter().zip(modules.into_iter()) {
-            #[allow(deprecated)]
-            let is_republishing = data_store.exists_module(&module.self_id())?;
-            if is_republishing {
-                // This is an upgrade, so invalidate the loader cache, which still contains the
-                // old module.
-                #[allow(deprecated)]
-                self.loader.mark_v1_as_invalid();
-            }
-            #[allow(deprecated)]
-            data_store.publish_module(&module.self_id(), blob, is_republishing)?;
-        }
-        Ok(())
     }
 
     fn deserialize_arg(
@@ -291,7 +115,9 @@ impl VMRuntime {
         // Create a list of dummy locals. Each value stored will be used be borrowed and passed
         // by reference to the invoked function
         let mut dummy_locals = Locals::new(param_tys.len());
+
         // Arguments for the invoked function. These can be owned values or references
+        let vm_config = module_storage.runtime_environment().vm_config();
         let deserialized_args = param_tys
             .into_iter()
             .zip(serialized_args)
@@ -301,7 +127,7 @@ impl VMRuntime {
                     dummy_locals.store_loc(
                         idx,
                         self.deserialize_arg(module_storage, inner_ty, arg_bytes)?,
-                        self.loader.vm_config().check_invariant_in_swap_loc,
+                        vm_config.check_invariant_in_swap_loc,
                     )?;
                     dummy_locals.borrow_loc(idx)
                 },
--- third_party/move/move-vm/runtime/src/storage/loader.rs
+++ third_party/move/move-vm/runtime/src/storage/loader.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::config::VMConfig;
-use move_vm_types::loaded_data::runtime_types::TypeBuilder;
 
 /// V2 implementation of loader, which is stateless - i.e., it does not contain module or script
 /// cache. Instead, module and script storages are passed to all APIs by reference.
@@ -17,9 +16,5 @@ impl LoaderV2 {
 
     pub(crate) fn vm_config(&self) -> &VMConfig {
         &self.vm_config
-    }
-
-    pub(crate) fn ty_builder(&self) -> &TypeBuilder {
-        &self.vm_config.ty_builder
     }
 }
