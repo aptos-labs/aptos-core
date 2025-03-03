fc8e30da33fda65aae4278b3f615897e2dba8c03
hariria <hariria@usc.edu>
2025-03-02 20:27:21 -0800

    [exec-pool] Address Zekun comments + refactor tests

---
+++ consensus/src/block_storage/execution_pool/mod.rs
@@ -0,0 +1,11 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+#[cfg(test)]
+pub(crate) mod common;
+
+#[cfg(test)]
+mod block_window_test;
+
+#[cfg(test)]
+mod target_block_retrieval_test;
