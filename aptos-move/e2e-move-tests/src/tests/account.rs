ee736875ea208d2c6e2b2057d4c4dbc978db98a7
Aaron Gao <lightmark@gmail.com>
2025-03-04 14:52:40 -0700

    stateless account w/ default feature

--- aptos-move/e2e-move-tests/src/tests/account.rs
+++ aptos-move/e2e-move-tests/src/tests/account.rs
@@ -4,8 +4,7 @@
 use crate::MoveHarness;
 use aptos_cached_packages::aptos_stdlib::aptos_account_transfer;
 use aptos_language_e2e_tests::account::Account;
-use claims::assert_err_eq;
-use move_core_types::vm_status::StatusCode;
+use claims::assert_ok;
 
 #[test]
 fn non_existent_sender() {
@@ -21,5 +20,5 @@ fn non_existent_sender() {
         .sign();
 
     let status = h.run(txn);
-    assert_err_eq!(status.status(), StatusCode::SENDING_ACCOUNT_DOES_NOT_EXIST);
+    assert_ok!(status.status());
 }
