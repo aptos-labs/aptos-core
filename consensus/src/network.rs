7dd222bdf8ae898282f819448058004816b23242
Andrew Hariri <hariria@usc.edu>
2025-03-18 14:34:27 -0700

    [exec-pool] Use DeprecatedBlockRetrievalRequest until next release

--- consensus/src/network.rs
+++ consensus/src/network.rs
@@ -256,7 +256,18 @@ impl NetworkSender {
         });
 
         ensure!(from != self.author, "Retrieve block from self");
-        let msg = ConsensusMsg::BlockRetrievalRequest(Box::new(retrieval_request.clone()));
+        // TODO @bchcho @hariria the sending of new ConsensusMsg::BlockRetrievalRequest must be
+        // phased over multiple releases to avoid `warn!(remote_peer = peer_id, "Unexpected msg: {:?}", msg);`
+        // Uncomment and replace after release
+        // let msg = ConsensusMsg::BlockRetrievalRequest(Box::new(retrieval_request.clone()));
+        let msg = match &retrieval_request {
+            BlockRetrievalRequest::V1(v1) => {
+                ConsensusMsg::DeprecatedBlockRetrievalRequest(Box::new(v1.clone()))
+            },
+            BlockRetrievalRequest::V2(_) => {
+                panic!("Unexpected BlockRetrievalRequest::V2, should be using ConsensusMsg::DeprecatedBlockRetrievalRequest with BlockRetrievalRequestV1")
+            },
+        };
         counters::CONSENSUS_SENT_MSGS
             .with_label_values(&[msg.name()])
             .inc();
@@ -265,6 +276,8 @@ impl NetworkSender {
             ConsensusMsg::BlockRetrievalResponse(resp) => *resp,
             _ => return Err(anyhow!("Invalid response to request")),
         };
+
+        // Verify response against retrieval request
         response
             .verify(retrieval_request, &self.validators)
             .map_err(|e| {
