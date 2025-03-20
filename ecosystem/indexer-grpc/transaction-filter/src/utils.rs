62d2c909d49f09d81843824d8ad79568dec82f56
Justin Chang <justin@aptoslabs.com>
2025-03-19 17:32:09 -0700

    unit tests

--- ecosystem/indexer-grpc/transaction-filter/src/utils.rs
+++ ecosystem/indexer-grpc/transaction-filter/src/utils.rs
@@ -17,3 +17,44 @@ pub fn standardize_address(address: &str) -> String {
     result.push_str(trimmed);
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_standardize_address() {
+        assert_eq!(
+            standardize_address("0x1"),
+            "0x0000000000000000000000000000000000000000000000000000000000000001"
+        );
+    }
+
+    #[test]
+    fn test_standardize_medium_length_address() {
+        assert_eq!(
+            standardize_address("0x123abc"),
+            "0x0000000000000000000000000000000000000000000000000000000000123abc"
+        );
+    }
+
+    #[test]
+    fn test_standardize_full_length_address() {
+        assert_eq!(
+            standardize_address(
+                "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+            ),
+            "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+        );
+    }
+
+    #[test]
+    fn test_standardize_address_with_leading_zero() {
+        assert_eq!(
+            standardize_address(
+                "0x234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+            ),
+            "0x0234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+        );
+    }
+}
