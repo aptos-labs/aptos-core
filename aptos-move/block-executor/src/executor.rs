393f205bb45fdfa51fae6bab0ffa0169bd18d979
Jeeyong Um <conr2d@proton.me>
2025-03-20 16:48:13 +0900

    Remove Sync trait bound from ExecutorTask

--- aptos-move/block-executor/src/executor.rs
+++ aptos-move/block-executor/src/executor.rs
@@ -82,7 +82,7 @@ pub struct BlockExecutor<T, E, S, L, TP> {
     config: BlockExecutorConfig,
     executor_thread_pool: Arc<rayon::ThreadPool>,
     transaction_commit_hook: Option<L>,
-    phantom: PhantomData<(T, E, S, L, TP)>,
+    phantom: PhantomData<fn() -> (T, E, S, L, TP)>,
 }
 
 impl<T, E, S, L, TP> BlockExecutor<T, E, S, L, TP>
