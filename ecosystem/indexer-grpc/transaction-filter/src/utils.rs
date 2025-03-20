0e3cb1b8bc1013561e3a0f8114cff4450323ee2d
Justin Chang <justin@aptoslabs.com>
2025-03-19 18:10:56 -0700

    display format

--- ecosystem/indexer-grpc/transaction-filter/src/utils.rs
+++ ecosystem/indexer-grpc/transaction-filter/src/utils.rs
@@ -4,14 +4,31 @@
 // 64 "0"s
 const ZEROS: &str = "0000000000000000000000000000000000000000000000000000000000000000";
 
-/// Converts a "0x" prefixed address to its LONG format
+/// Converts a "0x" prefixed address to display format (short for special addresses, long for all other addresses):
+/// https://github.com/aptos-foundation/AIPs/blob/main/aips/aip-40.md#display-format
 #[inline]
 pub fn standardize_address(address: &str) -> String {
-    // Preallocate exactly the required capacity.
+    // Remove "0x" prefix
     let trimmed = &address[2..];
-    let mut result = String::with_capacity(66);
 
-    // Push the appropriate slice of zeros rather than iterating.
+    // Check if the address is a special address by seeing if the first 31 bytes are zero and the last byte is smaller than 0b10000
+    if let Some(last_char) = trimmed.chars().last() {
+        if trimmed[..trimmed.len().saturating_sub(1)]
+            .chars()
+            .all(|c| c == '0')
+            && last_char.is_ascii_hexdigit()
+            && last_char <= 'f'
+        {
+            // Return special addresses in short format
+            let mut result = String::with_capacity(3);
+            result.push_str("0x");
+            result.push_str(&trimmed.trim_start_matches('0'));
+            return result;
+        }
+    }
+
+    // Return non-special addresses in long format
+    let mut result = String::with_capacity(66);
     result.push_str("0x");
     result.push_str(&ZEROS[..64 - trimmed.len()]);
     result.push_str(trimmed);
@@ -23,10 +40,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_standardize_address() {
+    fn test_standardize_special_address() {
+        assert_eq!(standardize_address("0x1"), "0x1");
+        assert_eq!(standardize_address("0x01"), "0x1");
+        assert_eq!(standardize_address("0x001"), "0x1");
+        assert_eq!(standardize_address("0x000000001"), "0x1");
+        assert_eq!(standardize_address("0xf"), "0xf");
+        assert_eq!(standardize_address("0x0f"), "0xf");
         assert_eq!(
-            standardize_address("0x1"),
-            "0x0000000000000000000000000000000000000000000000000000000000000001"
+            standardize_address(
+                "0x0000000000000000000000000000000000000000000000000000000000000001"
+            ),
+            "0x1"
+        );
+    }
+
+    #[test]
+    fn test_standardize_not_special_address() {
+        assert_eq!(
+            standardize_address("0x10"),
+            "0x0000000000000000000000000000000000000000000000000000000000000010"
         );
     }
 
