62a0a538e0c0edaeefa66154230a17de948717c5
Rustie Lin <rustie117@gmail.com>
2025-03-24 10:06:21 -0700

    [forge] pin the deployer version

--- testsuite/forge/src/backend/k8s_deployer/constants.rs
+++ testsuite/forge/src/backend/k8s_deployer/constants.rs
@@ -10,7 +10,7 @@ pub const INDEXER_GRPC_DOCKER_IMAGE_REPO: &str =
     "us-docker.pkg.dev/aptos-registry/docker/indexer-grpc";
 
 /// The version of the forge deployer image to use.
-pub const DEFAULT_FORGE_DEPLOYER_IMAGE_TAG: &str = "main"; // default to the latest stable build from the main branch
+pub const DEFAULT_FORGE_DEPLOYER_IMAGE_TAG: &str = "260a30838eea7eb40bef50b1b0ad81334ac154f4"; // the latest stable forge deployer
 
 /// This is the service account name that the deployer will use to deploy the forge components. It may require extra permissions and additonal setup
 pub const FORGE_DEPLOYER_SERVICE_ACCOUNT_NAME: &str = "forge";
