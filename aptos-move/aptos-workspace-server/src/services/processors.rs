fef35b1c3cd19a5617f1f864e22346f1feb53538
rtso <8248583+rtso@users.noreply.github.com>
2025-03-28 16:45:55 -0400

    timeout

--- aptos-move/aptos-workspace-server/src/services/processors.rs
+++ aptos-move/aptos-workspace-server/src/services/processors.rs
@@ -70,14 +70,15 @@ fn start_processor(
             transaction_stream_config: TransactionStreamConfig {
                 indexer_grpc_data_service_address: get_data_service_url(indexer_grpc_port),
                 auth_token: "notused".to_string(),
-                starting_version: None,
+                starting_version: Some(0),
                 request_ending_version: None,
                 request_name_header: "notused".to_string(),
                 additional_headers: Default::default(),
                 indexer_grpc_http2_ping_interval_secs: Default::default(),
                 indexer_grpc_http2_ping_timeout_secs: Default::default(),
                 indexer_grpc_reconnection_timeout_secs: Default::default(),
-                indexer_grpc_response_item_timeout_secs: Default::default(),
+                indexer_grpc_response_item_timeout_secs: 10,
+                reconnection_max_retries: 1000,
                 transaction_filter: Default::default(),
             },
             db_config: DbConfig::PostgresConfig(PostgresConfig {
