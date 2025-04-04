1310578846e9ba7231e60b4b7b3ea0d824a162ec
Satya Vusirikala <satyasatya123456@gmail.com>
2025-04-04 14:21:59 -0700

    Update API endpoints for orderless transactions

--- api/types/src/transaction.rs
+++ api/types/src/transaction.rs
@@ -489,6 +489,7 @@ pub struct UserTransactionRequestInner {
 
 impl VerifyInput for UserTransactionRequestInner {
     fn verify(&self) -> anyhow::Result<()> {
+
         if let Ok(now) = SystemTime::now().duration_since(UNIX_EPOCH) {
             if self.expiration_timestamp_secs.0 <= now.as_secs() {
                 bail!(
