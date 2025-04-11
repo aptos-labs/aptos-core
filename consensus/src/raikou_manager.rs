f783585c2f336ee2e960fe462bdd915081b1ca23
Balaji Arun <balaji@aptoslabs.com>
2025-04-11 18:26:40 +0000

    raptr 1pct drop all nodes

--- consensus/src/raikou_manager.rs
+++ consensus/src/raikou_manager.rs
@@ -197,7 +197,7 @@ impl RaikouManager {
             delta: Duration::from_secs_f64(delta),
             end_of_run: Instant::now() + Duration::from_secs_f64(delta) * total_duration_in_delta,
             extra_wait_before_qc_vote: Duration::from_secs_f64(delta * 0.1),
-            enable_partial_qc_votes: false,
+            enable_partial_qc_votes: true,
             enable_commit_votes: true,
             status_interval: Duration::from_secs_f64(delta) * 10,
             round_sync_interval: Duration::from_secs_f64(delta) * 15,
