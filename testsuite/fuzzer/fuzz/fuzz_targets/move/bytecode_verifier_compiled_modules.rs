92c868b99ac1edae39de96f9058fde2d829a3284
zi0Black <13380579+zi0Black@users.noreply.github.com>
2025-04-17 18:46:20 +0000

    Refactor fuzzer scripts and update README for seed corpus management
    - Removed obsolete bytecode verifier fuzz targets.
    - Updated `fuzz.sh` to streamline seed corpus handling by copying the main corpus for multiple targets.
    - Enhanced README with detailed steps for bulk building and managing seed corpora.
    - New workflow that update corpus

---
+++ testsuite/fuzzer/fuzz/fuzz_targets/move/bytecode_verifier_compiled_modules.rs
@@ -0,0 +1,34 @@
+// Copyright (c) The Move Contributors
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_main]
+use move_core_types::vm_status::StatusType;
+use move_binary_format::errors::VMError;
+use move_bytecode_verifier::VerifierConfig;
+use libfuzzer_sys::{fuzz_target, Corpus};
+mod utils;
+use utils::vm::RunnableState;
+
+fn check_for_invariant_violation_vmerror(e: VMError) {
+    if e.status_type() == StatusType::InvariantViolation
+        // ignore known false positive
+        && !e
+            .message()
+            .is_some_and(|m| m.starts_with("too many type parameters/arguments in the program"))
+    {
+        panic!("invariant violation {:?}", e);
+    }
+}
+
+fuzz_target!(|fuzz_data: RunnableState| -> Corpus {
+    let verifier_config = VerifierConfig::production();
+
+    for m in fuzz_data.dep_modules.iter() {
+        if let Err(e) = move_bytecode_verifier::verify_module_with_config(&verifier_config, m) {
+            //check_for_invariant_violation_vmerror(e);
+            return Corpus::Keep;
+        }
+    }
+    Corpus::Keep
+});
