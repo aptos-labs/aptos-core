26aca7e9c64a12e2c710da6e2b0c30649d427e74
George Mitenkov <georgemitenk0v@gmail.com>
2025-04-29 00:04:39 +0100

    [vm] Enhance event construction

--- execution/executor-benchmark/src/native/parallel_uncoordinated_block_executor.rs
+++ execution/executor-benchmark/src/native/parallel_uncoordinated_block_executor.rs
@@ -129,8 +129,11 @@ impl IncrementalOutput {
     }
 
     fn into_success_output(mut self, gas: u64) -> Result<TransactionOutput> {
-        self.events
-            .push(FeeStatement::new(gas, gas, 0, 0, 0).create_event_v2());
+        self.events.push(
+            FeeStatement::new(gas, gas, 0, 0, 0)
+                .create_event_v2()
+                .expect("Creating FeeStatement should always succeed"),
+        );
 
         Ok(TransactionOutput::new(
             WriteSetMut::new(self.write_set).freeze()?,
@@ -554,7 +557,8 @@ impl CommonNativeRawTransactionExecutor for NativeRawTransactionExecutor {
                     store: sender_store_address,
                     amount: transfer_amount,
                 }
-                .create_event_v2(),
+                .create_event_v2()
+                .expect("Creating WithdrawFAEvent should always succeed"),
             );
         }
 
@@ -666,7 +670,8 @@ impl CommonNativeRawTransactionExecutor for NativeRawTransactionExecutor {
                     store: recipient_store_address,
                     amount: transfer_amount,
                 }
-                .create_event_v2(),
+                .create_event_v2()
+                .expect("Creating DepositFAEvent should always succeed"),
             )
         }
 
@@ -691,7 +696,8 @@ impl CommonNativeRawTransactionExecutor for NativeRawTransactionExecutor {
                             account: AccountAddress::ONE,
                             type_info: DbAccessUtil::new_type_info_resource::<AptosCoinType>()?,
                         }
-                        .create_event_v2(),
+                        .create_event_v2()
+                        .expect("Creating CoinRegister should always succeed"),
                     );
                     (
                         DbAccessUtil::new_apt_coin_store(0, recipient_address),
--- types/src/proptest_types.rs
+++ types/src/proptest_types.rs
@@ -734,7 +734,7 @@ impl ContractEventGen {
     ) -> ContractEvent {
         let account_info = universe.get_account_info_mut(account_index);
         if self.use_event_v2 {
-            ContractEvent::new_v2(self.type_tag, self.payload)
+            ContractEvent::new_v2(self.type_tag, self.payload).unwrap()
         } else {
             let event_handle = if self.use_sent_key {
                 &mut account_info.sent_event_handle
@@ -745,7 +745,7 @@ impl ContractEventGen {
             *event_handle.count_mut() += 1;
             let event_key = event_handle.key();
 
-            ContractEvent::new_v1(*event_key, sequence_number, self.type_tag, self.payload)
+            ContractEvent::new_v1(*event_key, sequence_number, self.type_tag, self.payload).unwrap()
         }
     }
 }
@@ -845,7 +845,7 @@ impl ContractEvent {
             vec(any::<u8>(), 1..10),
         )
             .prop_map(|(event_key, seq_num, type_tag, event_data)| {
-                ContractEvent::new_v1(event_key, seq_num, type_tag, event_data)
+                ContractEvent::new_v1(event_key, seq_num, type_tag, event_data).unwrap()
             })
     }
 }
@@ -1290,7 +1290,8 @@ impl BlockGen {
                 vec![ContractEvent::new_v2(
                     NEW_EPOCH_EVENT_V2_MOVE_TYPE_TAG.clone(),
                     bcs::to_bytes(&NewEpochEvent::dummy()).unwrap(),
-                )]
+                )
+                .unwrap()]
             } else {
                 vec![]
             },
