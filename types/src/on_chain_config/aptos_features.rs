bca36d1e80aba06e4ccc295575908db52e604828
George Mitenkov <georgemitenk0v@gmail.com>
2025-05-01 00:53:08 +0100

    [lazy-loading] Enable lazy loading as default

--- types/src/on_chain_config/aptos_features.rs
+++ types/src/on_chain_config/aptos_features.rs
@@ -236,6 +236,7 @@ impl FeatureFlag {
             FeatureFlag::JWK_CONSENSUS_PER_KEY_MODE,
             FeatureFlag::TRANSACTION_PAYLOAD_V2,
             FeatureFlag::ORDERLESS_TRANSACTIONS,
+            FeatureFlag::ENABLE_LAZY_LOADING,
         ]
     }
 }
