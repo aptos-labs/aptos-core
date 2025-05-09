7751e1210584681e564c8fc1945ec51047c8432c
zi0Black <13380579+zi0Black@users.noreply.github.com>
2025-05-09 09:33:35 +0000

    fmt 2

--- third_party/move/move-vm/runtime/src/storage/module_storage.rs
+++ third_party/move/move-vm/runtime/src/storage/module_storage.rs
@@ -9,12 +9,12 @@ use crate::{
 use ambassador::delegatable_trait;
 use bytes::Bytes;
 use hashbrown::HashSet;
+#[cfg(fuzzing)]
+use move_binary_format::access::ModuleAccess;
 use move_binary_format::{
     errors::{Location, PartialVMError, PartialVMResult, VMResult},
     CompiledModule,
 };
-#[cfg(fuzzing)]
-use move_binary_format::access::ModuleAccess;
 use move_core_types::{
     account_address::AccountAddress,
     function::FUNCTION_DATA_SERIALIZATION_FORMAT_V1,
