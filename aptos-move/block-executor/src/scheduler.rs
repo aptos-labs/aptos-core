e580ca43d8cd5083ddc721b1066f0052c0c57e65
Rati <gelash@gmail.com>
2025-05-12 17:35:44 +0200

    SchedulerV2 & tests, refactor to use Statuses

--- aptos-move/block-executor/src/scheduler.rs
+++ aptos-move/block-executor/src/scheduler.rs
@@ -21,6 +21,7 @@ const TXN_IDX_MASK: u64 = (1 << 32) - 1;
 
 pub type Wave = u32;
 
+// try_lock succeeds when the lock is unlocked and armed (there is work to do).
 #[derive(Debug)]
 pub struct ArmedLock {
     // Last bit:   1 -> unlocked; 0 -> locked
