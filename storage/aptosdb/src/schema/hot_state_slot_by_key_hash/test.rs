9755349eade64e375faac38303fda26aa163d925
wqfish <1171005+wqfish@users.noreply.github.com>
2025-05-15 13:34:57 -0700

    [storage] add column family and db schema for hot state
    The structure should be identical to the cold state, i.e. the current global
    state. The only difference is the column family name.

---
+++ storage/aptosdb/src/schema/hot_state_slot_by_key_hash/test.rs
@@ -0,0 +1,20 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use super::*;
+use aptos_crypto::HashValue;
+use aptos_schemadb::{schema::fuzzing::assert_encode_decode, test_no_panic_decoding};
+use proptest::prelude::*;
+
+proptest! {
+    #[test]
+    fn test_encode_decode(
+        state_key in any::<HashValue>(),
+        version in any::<Version>(),
+        v in any::<StateSlot>(),
+    ) {
+        assert_encode_decode::<HotStateSlotByKeyHashSchema>(&(state_key, version), &v);
+    }
+}
+
+test_no_panic_decoding!(HotStateSlotByKeyHashSchema);
