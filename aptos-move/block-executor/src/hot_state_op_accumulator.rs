511d7dad27bbd7cd5306cb55caaf7342e38c6810
aldenhu <msmouse@gmail.com>
2025-05-16 16:56:30 -0700

    update test_speculative_state_workflow to use HotStateOpAccumulator

--- aptos-move/block-executor/src/hot_state_op_accumulator.rs
+++ aptos-move/block-executor/src/hot_state_op_accumulator.rs
@@ -1,92 +1,109 @@
 // Copyright (c) Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::types::{InputOutputKey, ReadWriteSummary};
 use aptos_logger::error;
 use aptos_types::{
     state_store::{state_slot::StateSlot, TStateView},
-    transaction::{BlockExecutableTransaction, Version},
+    transaction::Version,
 };
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, fmt::Debug, hash::Hash};
 
-pub struct BlockHotStateOpAccumulator<'base_view, Txn: BlockExecutableTransaction, BaseView> {
+pub struct BlockHotStateOpAccumulator<'base_view, Key, BaseView> {
     first_version: Version,
     base_view: &'base_view BaseView,
-    to_make_hot: BTreeMap<Txn::Key, StateSlot>,
-    keys_written: hashbrown::HashSet<Txn::Key>,
+    /// Keys read but never written to across the entire block are to be made hot (or refreshed
+    /// `hot_since_version` one is already hot but last refresh is far in the history) as the side
+    /// effect of the block epilogue (subject to per block limit)
+    to_make_hot: BTreeMap<Key, StateSlot>,
+    /// Keep track of all the keys that are written to across the whole block, these keys are made
+    /// hot (or have a refreshed `hot_since_version`) immediately at the version they got changed,
+    /// so no need to issue separate HotStateOps to promote them to the hot state.
+    writes: hashbrown::HashSet<Key>,
+    /// To prevent the block epilogue from being too heavy.
+    max_promotions_per_block: usize,
+    /// Every now and then refresh `hot_since_version` for hot items to prevent them from being
+    /// evicted.
+    refresh_interval_versions: usize,
 }
 
-impl<'base_view, Txn, BaseView> BlockHotStateOpAccumulator<'base_view, Txn, BaseView>
+impl<'base_view, Key, BaseView> BlockHotStateOpAccumulator<'base_view, Key, BaseView>
 where
-    Txn: BlockExecutableTransaction,
-    BaseView: TStateView<Key = Txn::Key>,
+    Key: PartialOrd + Ord + Send + Sync + Clone + Hash + Eq + Debug,
+    BaseView: TStateView<Key = Key>,
 {
-    /// TODO(HotState): make configurable, make on-chain config
+    /// TODO(HotState): make on-chain config
     const MAX_PROMOTIONS_PER_BLOCK: usize = 1024 * 10;
+    /// TODO(HotState): make on-chain config
     const REFRESH_INTERVAL_VERSIONS: usize = 1_000_000;
 
     pub fn new(base_view: &'base_view BaseView) -> Self {
+        Self::new_with_config(
+            base_view,
+            Self::MAX_PROMOTIONS_PER_BLOCK,
+            Self::REFRESH_INTERVAL_VERSIONS,
+        )
+    }
+
+    pub fn new_with_config(
+        base_view: &'base_view BaseView,
+        max_promotions_per_block: usize,
+        refresh_interval_versions: usize,
+    ) -> Self {
         Self {
             first_version: base_view.next_version(),
             base_view,
             to_make_hot: BTreeMap::new(),
-            keys_written: hashbrown::HashSet::new(),
+            writes: hashbrown::HashSet::new(),
+            max_promotions_per_block,
+            refresh_interval_versions,
         }
     }
 
-    pub fn add_transaction(&mut self, rw_summary: &ReadWriteSummary<Txn>) {
-        for write_key in rw_summary.writes.iter() {
-            match write_key {
-                InputOutputKey::Resource(key) | InputOutputKey::Group(key, _) => {
-                    self.to_make_hot.remove(key);
-                    self.keys_written.get_or_insert_owned(key);
-                },
-                InputOutputKey::DelayedField(_id) => {
-                    // TODO(HotState): revisit -- hotness can't change for a in-place change,
-                    //                 otherwise it's a problem when we charge differently for hot
-                    //                 and cold reads
-                },
-            }
+    pub fn add_transaction<'a>(
+        &mut self,
+        writes: impl Iterator<Item = &'a Key>,
+        read_only: impl Iterator<Item = &'a Key>,
+    ) where
+        Key: 'a,
+    {
+        for key in writes {
+            self.to_make_hot.remove(key);
+            self.writes.get_or_insert_owned(key);
         }
 
-        for read_key in rw_summary.reads.iter() {
-            match read_key {
-                InputOutputKey::Resource(key) | InputOutputKey::Group(key, _) => {
-                    if self.to_make_hot.len() >= Self::MAX_PROMOTIONS_PER_BLOCK {
-                        continue;
-                    }
-                    if self.to_make_hot.contains_key(key) {
-                        continue;
-                    }
-                    if self.keys_written.contains(key) {
-                        continue;
-                    }
-                    let slot = self
-                        .base_view
-                        .get_state_slot(key)
-                        .expect("base_view.get_slot() failed.");
-                    let make_hot = match slot.hot_since_version_opt() {
-                        None => true,
-                        Some(hot_since_version) => {
-                            if hot_since_version >= self.first_version {
-                                error!("Unexpected: hot_since_version > block first version");
-                                false
-                            } else {
-                                hot_since_version + Self::REFRESH_INTERVAL_VERSIONS as Version
-                                    >= self.first_version
-                            }
-                        },
-                    };
-                    if make_hot {
-                        self.to_make_hot.insert(key.clone(), slot);
+        for key in read_only {
+            if self.to_make_hot.len() >= self.max_promotions_per_block {
+                continue;
+            }
+            if self.to_make_hot.contains_key(key) {
+                continue;
+            }
+            if self.writes.contains(key) {
+                continue;
+            }
+            let slot = self
+                .base_view
+                .get_state_slot(key)
+                .expect("base_view.get_slot() failed.");
+            let make_hot = match slot.hot_since_version_opt() {
+                None => true,
+                Some(hot_since_version) => {
+                    if hot_since_version >= self.first_version {
+                        error!("Unexpected: hot_since_version > block first version");
+                        false
+                    } else {
+                        hot_since_version + self.refresh_interval_versions as Version
+                            >= self.first_version
                     }
                 },
-                InputOutputKey::DelayedField(_id) => {},
+            };
+            if make_hot {
+                self.to_make_hot.insert(key.clone(), slot);
             }
         }
     }
 
-    pub fn get_slots_to_make_hot(&self) -> BTreeMap<Txn::Key, StateSlot> {
+    pub fn get_slots_to_make_hot(&self) -> BTreeMap<Key, StateSlot> {
         self.to_make_hot.clone()
     }
 }
