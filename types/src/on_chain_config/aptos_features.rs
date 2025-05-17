00e0d4d670d3c934860f52e214b1ebcdd5c2ede1
zhoujunma <zhoujun@aptoslabs.com>
2025-05-16 18:30:17 -0700

    enable_perkey_jwk_consensus (#16445)

--- types/src/on_chain_config/aptos_features.rs
+++ types/src/on_chain_config/aptos_features.rs
@@ -233,6 +233,7 @@ impl FeatureFlag {
             FeatureFlag::ENABLE_FUNCTION_VALUES,
             FeatureFlag::NEW_ACCOUNTS_DEFAULT_TO_FA_STORE,
             FeatureFlag::DEFAULT_ACCOUNT_RESOURCE,
+            FeatureFlag::JWK_CONSENSUS_PER_KEY_MODE,
             FeatureFlag::TRANSACTION_PAYLOAD_V2,
             FeatureFlag::ORDERLESS_TRANSACTIONS,
         ]
