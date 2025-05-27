db2d2ca3d996ae44fb9663655c7413c66c5f02d1
Jose Garcia Crosta <jgcrosta@gmail.com>
2025-05-27 17:08:09 -0300

    Add detector to base

--- third_party/move/tools/move-linter/src/model_ast_lints.rs
+++ third_party/move/tools/move-linter/src/model_ast_lints.rs
@@ -13,6 +13,7 @@ mod needless_deref_ref;
 mod needless_ref_deref;
 mod needless_ref_in_field_access;
 mod nonminimal_bool;
+mod redundant_boolean_expressions;
 mod self_assignment;
 mod simpler_numeric_expression;
 mod unnecessary_boolean_identity_comparison;
@@ -34,6 +35,7 @@ pub fn get_default_linter_pipeline(config: &BTreeMap<String, String>) -> Vec<Box
         Box::<needless_bool::NeedlessBool>::default(),
         Box::<needless_ref_in_field_access::NeedlessRefInFieldAccess>::default(),
         Box::<needless_deref_ref::NeedlessDerefRef>::default(),
+        Box::<redundant_boolean_expressions::RedundantBooleanExpression>::default(),
         Box::<needless_ref_deref::NeedlessRefDeref>::default(),
         Box::<nonminimal_bool::NonminimalBool>::default(),
         Box::<self_assignment::SelfAssignment>::default(),
