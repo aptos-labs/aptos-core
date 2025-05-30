d1a36fc9d01746d5139cc0e3c50b271a20c80ef1
vusirikala <satyasatya123456@gmail.com>
2025-05-30 12:19:08 -0700

    Print transaction infos for each block

--- execution/executor-types/src/execution_output.rs
+++ execution/executor-types/src/execution_output.rs
@@ -139,6 +139,10 @@ impl ExecutionOutput {
     pub fn expect_last_version(&self) -> Version {
         self.first_version + self.num_transactions_to_commit() as Version - 1
     }
+
+    pub fn first_version(&self) -> Version {
+        self.first_version
+}
 }
 
 #[derive(Debug)]
