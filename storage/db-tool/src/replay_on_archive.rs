d6dd93a34592d2e70e0147fb924fc69ef7264121
Balaji Arun <balaji@aptoslabs.com>
2025-06-03 14:48:20 -0700

    More memory and split heavy ranges

--- storage/db-tool/src/replay_on_archive.rs
+++ storage/db-tool/src/replay_on_archive.rs
@@ -219,7 +219,7 @@ impl Verifier {
             // timeout check
             if let Some(duration) = self.timeout_secs {
                 if self.replay_stat.get_elapsed_secs() >= duration {
-                    info!(
+                    error!(
                         "Verify timeout: {}s elapsed. Deadline: {}s",
                         self.replay_stat.get_elapsed_secs(),
                         duration
