208a521bc830f705cdb8c3a7f79ad4d341c94079
Josh Lind <josh.lind@hotmail.com>
2025-06-03 16:56:50 -0400

    [Transaction Filter] Add mempool filtering support.

--- config/src/config/node_config.rs
+++ config/src/config/node_config.rs
@@ -8,10 +8,11 @@ use crate::{
         internal_indexer_db_config::InternalIndexerDBConfig,
         jwk_consensus_config::JWKConsensusConfig, netbench_config::NetbenchConfig,
         node_config_loader::NodeConfigLoader, node_startup_config::NodeStartupConfig,
-        persistable_config::PersistableConfig, utils::RootPath, AdminServiceConfig, ApiConfig,
-        BaseConfig, ConsensusConfig, Error, ExecutionConfig, IndexerConfig, IndexerGrpcConfig,
-        InspectionServiceConfig, LoggerConfig, MempoolConfig, NetworkConfig,
-        PeerMonitoringServiceConfig, SafetyRulesTestConfig, StateSyncConfig, StorageConfig,
+        persistable_config::PersistableConfig, transaction_filter_config::TransactionFilterConfig,
+        utils::RootPath, AdminServiceConfig, ApiConfig, BaseConfig, ConsensusConfig, Error,
+        ExecutionConfig, IndexerConfig, IndexerGrpcConfig, InspectionServiceConfig, LoggerConfig,
+        MempoolConfig, NetworkConfig, PeerMonitoringServiceConfig, SafetyRulesTestConfig,
+        StateSyncConfig, StorageConfig,
     },
     network_id::NetworkId,
 };
@@ -82,6 +83,8 @@ pub struct NodeConfig {
     pub state_sync: StateSyncConfig,
     #[serde(default)]
     pub storage: StorageConfig,
+    #[serde(default)]
+    pub transaction_filter: TransactionFilterConfig,
     #[serde(default)]
     pub validator_network: Option<NetworkConfig>,
     #[serde(default)]
