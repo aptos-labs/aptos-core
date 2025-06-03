e48b824e55a4fe92706f384336c7d9afd1e67d20
wqfish <1171005+wqfish@users.noreply.github.com>
2025-06-02 23:30:54 -0700

    Fix unused import warning

--- types/src/dkg/real_dkg/mod.rs
+++ types/src/dkg/real_dkg/mod.rs
@@ -1,5 +1,5 @@
 // Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.1
 
 use crate::{
     dkg::{
@@ -10,7 +10,9 @@ use crate::{
     validator_verifier::{ValidatorConsensusInfo, ValidatorVerifier},
 };
 use anyhow::{anyhow, bail, ensure, Context};
-use aptos_crypto::{bls12381, bls12381::PrivateKey, Uniform};
+#[cfg(any(test, feature = "testing"))]
+use aptos_crypto::Uniform;
+use aptos_crypto::{bls12381, bls12381::PrivateKey};
 use aptos_dkg::{
     pvss,
     pvss::{
