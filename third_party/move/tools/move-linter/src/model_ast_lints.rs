87679d6bdd110fad6d9ac9c1bfafaf3dce01d9fb
Facundo Lerena <faculerena@gmail.com>
2025-06-11 11:53:36 -0300

    [linter] Added aborting_overflow_checks lint

--- third_party/move/tools/move-linter/src/model_ast_lints.rs
+++ third_party/move/tools/move-linter/src/model_ast_lints.rs
@@ -6,7 +6,12 @@
 mod almost_swapped;
 mod assert_const;
 mod blocks_in_conditions;
+<<<<<<< HEAD
 mod equal_operands_in_bin_op;
+||||||| parent of b8abccb6b6 ([linter] Added aborting_overflow_checks lint)
+=======
+mod aborting_overflow_checks;
+>>>>>>> b8abccb6b6 ([linter] Added aborting_overflow_checks lint)
 mod needless_bool;
 mod needless_deref_ref;
 mod needless_ref_deref;
@@ -25,6 +30,7 @@ use std::collections::BTreeMap;
 pub fn get_default_linter_pipeline(config: &BTreeMap<String, String>) -> Vec<Box<dyn ExpChecker>> {
     // Start with the default set of checks.
     let checks: Vec<Box<dyn ExpChecker>> = vec![
+        Box::<aborting_overflow_checks::AbortingOverflowChecks>::default(),
         Box::<almost_swapped::AlmostSwapped>::default(),
         Box::<assert_const::AssertConst>::default(),
         Box::<blocks_in_conditions::BlocksInConditions>::default(),
@@ -39,6 +45,7 @@ pub fn get_default_linter_pipeline(config: &BTreeMap<String, String>) -> Vec<Box
         Box::<unnecessary_boolean_identity_comparison::UnnecessaryBooleanIdentityComparison>::default(),
         Box::<unnecessary_numerical_extreme_comparison::UnnecessaryNumericalExtremeComparison>::default(),
         Box::<while_true::WhileTrue>::default(),
+        
     ];
     let checks_category = config.get("checks").map_or("default", |s| s.as_str());
     if checks_category == "strict" || checks_category == "experimental" {
