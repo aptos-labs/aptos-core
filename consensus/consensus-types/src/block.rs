b7b07300910c8ee0c148f6c6978b134e6589feaf
danielxiangzl <xiangzhuolun@gmail.com>
2025-06-18 15:47:01 -0700

    fix batch counters

--- consensus/consensus-types/src/block.rs
+++ consensus/consensus-types/src/block.rs
@@ -125,6 +125,83 @@ impl Block {
         }
     }
 
+    /// Returns the number of proofs, the number of txns in the proofs, and the bytes of txns in the proofs
+    pub fn proof_statics(&self) -> (usize, usize, usize) {
+        match self.block_data.payload() {
+            None => (0, 0, 0),
+            Some(payload) => match payload {
+                Payload::InQuorumStore(pos) => (pos.num_proofs(), pos.num_txns(), pos.num_bytes()),
+                Payload::DirectMempool(_txns) => (0, 0, 0),
+                Payload::InQuorumStoreWithLimit(pos) => (
+                    pos.proof_with_data.num_proofs(),
+                    pos.proof_with_data.num_txns(),
+                    pos.proof_with_data.num_bytes(),
+                ),
+                Payload::QuorumStoreInlineHybrid(_inline_batches, proof_with_data, _)
+                | Payload::QuorumStoreInlineHybridV2(_inline_batches, proof_with_data, _) => (
+                    proof_with_data.num_proofs(),
+                    proof_with_data.num_txns(),
+                    proof_with_data.num_bytes(),
+                ),
+                Payload::OptQuorumStore(opt_quorum_store_payload) => (
+                    opt_quorum_store_payload.proof_with_data().num_proofs(),
+                    opt_quorum_store_payload.proof_with_data().num_txns(),
+                    opt_quorum_store_payload.proof_with_data().num_bytes(),
+                ),
+            },
+        }
+    }
+
+    /// Returns the number of inline batches, the number of txns in the inline batches, and the bytes of txns in the inline batches
+    pub fn inline_batch_statics(&self) -> (usize, usize, usize) {
+        match self.block_data.payload() {
+            None => (0, 0, 0),
+            Some(payload) => match payload {
+                Payload::QuorumStoreInlineHybrid(inline_batches, _proof_with_data, _)
+                | Payload::QuorumStoreInlineHybridV2(inline_batches, _proof_with_data, _) => (
+                    inline_batches.len(),
+                    inline_batches
+                        .iter()
+                        .map(|(b, _)| b.num_txns() as usize)
+                        .sum(),
+                    inline_batches
+                        .iter()
+                        .map(|(b, _)| b.num_bytes() as usize)
+                        .sum(),
+                ),
+                Payload::OptQuorumStore(opt_quorum_store_payload) => (
+                    opt_quorum_store_payload.inline_batches().num_batches(),
+                    opt_quorum_store_payload.inline_batches().num_txns(),
+                    opt_quorum_store_payload.inline_batches().num_bytes(),
+                ),
+                _ => (0, 0, 0),
+            },
+        }
+    }
+
+    /// Returns the number of opt batches, the number of txns in the opt batches, and the bytes of txns in the opt batches
+    pub fn opt_batch_statics(&self) -> (usize, usize, usize) {
+        match self.block_data.payload() {
+            None => (0, 0, 0),
+            Some(payload) => match payload {
+                Payload::OptQuorumStore(opt_quorum_store_payload) => (
+                    opt_quorum_store_payload.opt_batches().len(),
+                    opt_quorum_store_payload
+                        .opt_batches()
+                        .iter()
+                        .map(|b| b.num_txns() as usize)
+                        .sum(),
+                    opt_quorum_store_payload
+                        .opt_batches()
+                        .iter()
+                        .map(|b| b.num_bytes() as usize)
+                        .sum(),
+                ),
+                _ => (0, 0, 0),
+            },
+        }
+    }
+
     pub fn quorum_cert(&self) -> &QuorumCert {
         self.block_data.quorum_cert()
     }
