428d426f96b352040e0720e0fd581756af078089
Rati <gelash@gmail.com>
2025-06-20 10:11:29 -0400

    BlockSTMv2 PR [8/n]: delayed fields, AggregatorV1 & tests

--- aptos-move/block-executor/src/combinatorial_tests/mod.rs
+++ aptos-move/block-executor/src/combinatorial_tests/mod.rs
@@ -2,15 +2,19 @@
 // Parts of the project are originally copyright © Meta Platforms, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+#[cfg(test)]
 pub(crate) mod baseline;
+#[cfg(test)]
 pub mod bencher;
 #[cfg(test)]
 mod delta_tests;
 #[cfg(test)]
 mod group_tests;
+#[cfg(test)]
 pub(crate) mod mock_executor;
 #[cfg(test)]
 mod module_tests;
 #[cfg(test)]
 mod resource_tests;
+#[cfg(test)]
 pub(crate) mod types;
