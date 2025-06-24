862e7a9b3e3e3ddb97a0913ae3f18e065cac00fa
Guoteng Rao <3603304+grao1991@users.noreply.github.com>
2025-06-24 22:06:13 +0000

    [Storage] Fix calculation and add tests.

--- storage/aptosdb/src/ledger_db/persisted_auxiliary_info_db_test.rs
+++ storage/aptosdb/src/ledger_db/persisted_auxiliary_info_db_test.rs
@@ -7,6 +7,66 @@ use aptos_temppath::TempPath;
 use aptos_types::transaction::PersistedAuxiliaryInfo;
 use proptest::{collection::vec, prelude::*};
 
+fn get_persisted_auxiliary_info(
+    db: &AptosDB,
+    start_version: u64,
+    count: usize,
+) -> Vec<PersistedAuxiliaryInfo> {
+    db.ledger_db
+        .persisted_auxiliary_info_db()
+        .get_persisted_auxiliary_info_iter(start_version, count)
+        .unwrap()
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap()
+}
+
+#[test]
+pub fn test_iterator() {
+    let tmp_dir = TempPath::new();
+    let db = AptosDB::new_for_test(&tmp_dir);
+    let v1_info = PersistedAuxiliaryInfo::V1 {
+        transaction_index: 0,
+    };
+
+    {
+        assert_eq!(
+            get_persisted_auxiliary_info(&db, 90, 20),
+            vec![PersistedAuxiliaryInfo::None; 20]
+        );
+    }
+
+    let persisted_info = vec![v1_info; 100];
+    put_persisted_auxiliary_info(&db, 100, &persisted_info);
+
+    {
+        let mut expected = vec![];
+        for _ in 0..10 {
+            expected.push(PersistedAuxiliaryInfo::None);
+        }
+        for _ in 0..10 {
+            expected.push(v1_info);
+        }
+        assert_eq!(get_persisted_auxiliary_info(&db, 90, 20), expected);
+    }
+
+    assert_eq!(
+        get_persisted_auxiliary_info(&db, 0, 20),
+        vec![PersistedAuxiliaryInfo::None; 20]
+    );
+
+    assert_eq!(get_persisted_auxiliary_info(&db, 100, 20), vec![
+        v1_info;
+        20
+    ]);
+
+    assert_eq!(get_persisted_auxiliary_info(&db, 190, 20), vec![
+        v1_info;
+        10
+    ]);
+
+    assert_eq!(get_persisted_auxiliary_info(&db, 200, 20), vec![]);
+}
+
 proptest! {
     #![proptest_config(ProptestConfig::with_cases(10))]
 
