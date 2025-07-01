f0b7bf6e4a3489b68f7616df0188707ff3a2df37
Teng Zhang <rahxephon89@163.com>
2025-06-30 22:58:36 -0700

    add visibility

---
+++ aptos-move/e2e-move-tests/src/tests/public_structs_enum_upgrade.rs
@@ -0,0 +1,213 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+//! Tests for upgrade compatibility of public/package structs/enums
+
+use crate::{assert_success, assert_vm_status, MoveHarness};
+use aptos_framework::BuildOptions;
+use aptos_language_e2e_tests::account::Account;
+use aptos_package_builder::PackageBuilder;
+use aptos_types::{account_address::AccountAddress, transaction::TransactionStatus};
+use move_core_types::vm_status::StatusCode;
+
+#[test]
+fn public_enum_upgrade() {
+    let mut h = MoveHarness::new();
+    let acc = h.new_account_at(AccountAddress::from_hex_literal("0x815").unwrap());
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m {
+            enum Data {
+               V1{x: u64}
+            }
+        }
+    "#,
+    );
+    assert_success!(result);
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m {
+            public enum Data {
+               V1{x: u64},
+               V2{x: u64, y: u8},
+            }
+        }
+    "#,
+    );
+    assert_success!(result);
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m {
+            enum Data {
+               V1{x: u64},
+               V2{x: u64, y: u8},
+            }
+        }
+    "#,
+    );
+    assert_vm_status!(result, StatusCode::BACKWARD_INCOMPATIBLE_MODULE_UPDATE);
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m {
+            package enum Data {
+               V1{x: u64},
+               V2{x: u64, y: u8},
+            }
+        }
+    "#,
+    );
+    assert_vm_status!(result, StatusCode::BACKWARD_INCOMPATIBLE_MODULE_UPDATE);
+}
+
+#[test]
+fn friend_enum_struct_upgrade() {
+    let mut h = MoveHarness::new();
+    let acc = h.new_account_at(AccountAddress::from_hex_literal("0x815").unwrap());
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m {
+            friend 0x815::m2;
+            enum Data {
+               V1{x: u64}
+            }
+        }
+        module 0x815::m2 {
+        }
+    "#,
+    );
+    assert_success!(result);
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m2 {
+            use 0x815::m::Data;
+            fun get_data(): Data {
+                Data::V2{x: 1, y: 2}
+            }
+        }
+        module 0x815::m {
+            friend 0x815::m2;
+            friend enum Data {
+               V1{x: u64},
+               V2{x: u64, y: u8},
+            }
+        }
+    "#,
+    );
+    assert_success!(result);
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m2 {
+        }
+        module 0x815::m {
+            enum Data {
+               V1{x: u64},
+               V2{x: u64, y: u8},
+            }
+        }
+    "#,
+    );
+    assert_success!(result);
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+        module 0x815::m2 {
+            use 0x815::m::Data;
+            use 0x815::m::S;
+            fun get_data(): Data {
+                Data::V1{x: 1}
+            }
+            fun pack_struct(): S {
+                S { x: 22 }
+            }
+        }
+        module 0x815::m {
+            package enum Data {
+               V1{x: u64},
+               V2{x: u64, y: u8},
+            }
+
+            package struct S {
+                x: u64,
+            }
+        }
+    "#,
+    );
+    assert_success!(result);
+
+    let result = publish(
+        &mut h,
+        &acc,
+        r#"
+            module 0x815::m2 {
+                use 0x815::m::Data;
+                use 0x815::m::S;
+                use 0x815::m::Predicate;
+                use 0x815::m::Holder;
+                fun get_data(): Data {
+                    Data::V1{x: 1}
+                }
+                public fun pack_struct(): S {
+                S { x: 22 }
+                }
+
+                public fun apply_pred(p: Predicate<u64>, val: u64): bool {
+                    p(&val)
+                }
+
+                public fun apply_holder(h: Holder<u64>, val: u64): bool {
+                    let Holder(p) = h;
+                    p(&val)
+                }
+            }
+            module 0x815::m {
+                public enum Data {
+                   V1{x: u64},
+                   V2{x: u64, y: u8},
+                }
+
+                public struct S {
+                    x: u64,
+                }
+
+                package struct Predicate<T>(|&T| bool) has copy, drop;
+
+                package struct Holder<T>(Predicate<T>) has copy, drop;
+            }
+        "#,
+    );
+    assert_success!(result);
+}
+
+fn publish(h: &mut MoveHarness, account: &Account, source: &str) -> TransactionStatus {
+    let mut builder = PackageBuilder::new("Package");
+    builder.add_source("m.move", source);
+    let path = builder.write_to_temp().unwrap();
+    h.publish_package_with_options(
+        account,
+        path.path(),
+        BuildOptions::move_2().set_latest_language(),
+    )
+}
--- third_party/move/move-compiler-v2/legacy-move-compiler/src/parser/syntax.rs
+++ third_party/move/move-compiler-v2/legacy-move-compiler/src/parser/syntax.rs
@@ -3096,11 +3096,12 @@ fn parse_struct_decl(
     context: &mut Context,
 ) -> Result<StructDefinition, Box<Diagnostic>> {
     let Modifiers {
-        visibility,
+        visibility: visibility_opt,
         entry,
         native,
     } = modifiers;
-    match visibility {
+    let mut visibility = Visibility::Internal;
+    match visibility_opt {
         Some(vis) if !context.env.flags().lang_v2() => {
             let msg = format!(
                 "Invalid struct declaration. Structs cannot have visibility modifiers as they are \
@@ -3110,6 +3111,9 @@ fn parse_struct_decl(
             context
                 .env
                 .add_diag(diag!(Syntax::InvalidModifier, (vis.loc().unwrap(), msg)));
+        },
+        Some(vis) => {
+            visibility = vis;
         },
         _ => {},
     }
@@ -3199,6 +3203,7 @@ fn parse_struct_decl(
         name,
         type_parameters,
         layout,
+        visibility,
     })
 }
 
@@ -3541,7 +3546,10 @@ fn parse_use_alias(context: &mut Context) -> Result<Option<Name>, Box<Diagnostic
 fn is_friend_declaration(context: &mut Context) -> bool {
     if context.tokens.peek() == Tok::Friend {
         if let Ok((tok, content)) = context.tokens.lookahead_content() {
-            if ![Tok::Fun, Tok::Inline, Tok::Native].contains(&tok) && content != ENTRY_MODIFIER {
+            if ![Tok::Fun, Tok::Inline, Tok::Native, Tok::Struct].contains(&tok)
+                && content != ENTRY_MODIFIER
+                && content != ENUM_MODIFIER
+            {
                 return true;
             }
         }
@@ -3650,7 +3658,7 @@ fn parse_module(
                             Tok::Struct => ModuleMember::Struct(parse_struct_decl(
                                 false, attributes, start_loc, modifiers, context,
                             )?),
-                            Tok::Identifier if context.tokens.content() == "enum" => {
+                            Tok::Identifier if context.tokens.content() == ENUM_MODIFIER => {
                                 ModuleMember::Struct(parse_struct_decl(
                                     true, attributes, start_loc, modifiers, context,
                                 )?)
