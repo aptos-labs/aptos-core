d56656b4eb213cf0f48e93bc97cde2ef45e6de00
Josh Lind <josh.lind@hotmail.com>
2025-07-03 14:46:49 -0400

    [Transaction Filtering] Add round manager tests.

--- consensus/src/round_manager_tests/mod.rs
+++ consensus/src/round_manager_tests/mod.rs
@@ -17,9 +17,13 @@ use crate::{
     network::{IncomingBlockRetrievalRequest, NetworkSender},
     network_interface::{CommitMessage, ConsensusMsg, ConsensusNetworkClient, DIRECT_SEND, RPC},
     network_tests::{NetworkPlayground, TwinId},
-    payload_manager::DirectMempoolPayloadManager,
+    payload_manager::{
+        DirectMempoolPayloadManager, QuorumStorePayloadManager, TPayloadManager,
+        TQuorumStoreCommitNotifier,
+    },
     persistent_liveness_storage::RecoveryData,
     pipeline::buffer_manager::OrderedBlocks,
+    quorum_store::batch_store::BatchReader,
     round_manager::RoundManager,
     test_utils::{
         mock_execution_client::MockExecutionClient, MockOptQSPayloadProvider,
@@ -39,6 +43,7 @@ use aptos_consensus_types::{
     opt_proposal_msg::OptProposalMsg,
     order_vote_msg::OrderVoteMsg,
     pipeline::commit_decision::CommitDecision,
+    proof_of_store::BatchInfo,
     proposal_msg::ProposalMsg,
     round_timeout::RoundTimeoutMsg,
     utils::PayloadTxnsSize,
@@ -46,6 +51,7 @@ use aptos_consensus_types::{
     wrapped_ledger_info::WrappedLedgerInfo,
 };
 use aptos_crypto::HashValue;
+use aptos_executor_types::ExecutorResult;
 use aptos_infallible::Mutex;
 use aptos_logger::prelude::info;
 use aptos_network::{
@@ -72,12 +78,17 @@ use aptos_types::{
     validator_signer::ValidatorSigner,
     validator_verifier::{random_validator_verifier, ValidatorVerifier},
     waypoint::Waypoint,
+    PeerId,
 };
-use futures::{channel::mpsc, executor::block_on, stream::select, FutureExt, Stream, StreamExt};
+use futures::{
+    channel::mpsc, executor::block_on, future::Shared, stream::select, FutureExt, Stream, StreamExt,
+};
 use maplit::hashmap;
 use std::{
     collections::VecDeque,
+    future::Future,
     iter::FromIterator,
+    pin::Pin,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -88,6 +99,7 @@ use tokio::{runtime::Handle, task::JoinHandle};
 
 mod consensus_test;
 mod opt_proposal_test;
+mod txn_filter_proposal_test;
 mod vtxn_on_proposal_test;
 
 fn config_with_round_timeout_msg_disabled() -> ConsensusConfig {
@@ -173,6 +185,7 @@ pub struct NodeSetup {
     _state_sync_receiver: mpsc::UnboundedReceiver<Vec<SignedTransaction>>,
     id: usize,
     onchain_consensus_config: OnChainConsensusConfig,
+    block_txn_filter_config: BlockTransactionFilterConfig,
     local_consensus_config: ConsensusConfig,
     onchain_randomness_config: OnChainRandomnessConfig,
     onchain_jwk_consensus_config: OnChainJWKConsensusConfig,
@@ -183,6 +196,7 @@ pub struct NodeSetup {
     round_timeout_queue: VecDeque<RoundTimeoutMsg>,
     commit_decision_queue: VecDeque<CommitDecision>,
     processed_opt_proposal_rx: aptos_channels::UnboundedReceiver<OptBlockData>,
+    use_quorum_store_payloads: bool,
 }
 
 impl NodeSetup {
@@ -203,9 +217,11 @@ impl NodeSetup {
         num_nodes: usize,
         proposer_indices: Option<Vec<usize>>,
         onchain_consensus_config: Option<OnChainConsensusConfig>,
+        block_txn_filter_config: Option<BlockTransactionFilterConfig>,
         local_consensus_config: Option<ConsensusConfig>,
         onchain_randomness_config: Option<OnChainRandomnessConfig>,
         onchain_jwk_consensus_config: Option<OnChainJWKConsensusConfig>,
+        use_quorum_store_payloads: bool,
     ) -> Vec<Self> {
         Self::create_nodes_with_validator_set(
             playground,
@@ -213,10 +229,12 @@ impl NodeSetup {
             num_nodes,
             proposer_indices,
             onchain_consensus_config,
+            block_txn_filter_config,
             local_consensus_config,
             onchain_randomness_config,
             onchain_jwk_consensus_config,
             None,
+            use_quorum_store_payloads,
         )
     }
 
@@ -226,10 +244,12 @@ impl NodeSetup {
         num_nodes: usize,
         proposer_indices: Option<Vec<usize>>,
         onchain_consensus_config: Option<OnChainConsensusConfig>,
+        block_txn_filter_config: Option<BlockTransactionFilterConfig>,
         local_consensus_config: Option<ConsensusConfig>,
         onchain_randomness_config: Option<OnChainRandomnessConfig>,
         onchain_jwk_consensus_config: Option<OnChainJWKConsensusConfig>,
         validator_set: Option<(Vec<ValidatorSigner>, ValidatorVerifier)>,
+        use_quorum_store_payloads: bool,
     ) -> Vec<Self> {
         let mut onchain_consensus_config = onchain_consensus_config.unwrap_or_default();
         // With order votes feature, the validators additionally send order votes.
@@ -251,6 +271,7 @@ impl NodeSetup {
             onchain_randomness_config.unwrap_or_else(OnChainRandomnessConfig::default_if_missing);
         let onchain_jwk_consensus_config = onchain_jwk_consensus_config
             .unwrap_or_else(OnChainJWKConsensusConfig::default_if_missing);
+        let block_txn_filter_config = block_txn_filter_config.unwrap_or_default();
         let local_consensus_config = local_consensus_config.unwrap_or_default();
         let (signers, validators) =
             validator_set.unwrap_or_else(|| random_validator_verifier(num_nodes, None, false));
@@ -301,9 +322,11 @@ impl NodeSetup {
                 safety_rules_manager,
                 id,
                 onchain_consensus_config.clone(),
+                block_txn_filter_config.clone(),
                 local_consensus_config.clone(),
                 onchain_randomness_config.clone(),
                 onchain_jwk_consensus_config.clone(),
+                use_quorum_store_payloads,
             ));
         }
         nodes
@@ -319,9 +342,11 @@ impl NodeSetup {
         safety_rules_manager: SafetyRulesManager,
         id: usize,
         onchain_consensus_config: OnChainConsensusConfig,
+        block_txn_filter_config: BlockTransactionFilterConfig,
         local_consensus_config: ConsensusConfig,
         onchain_randomness_config: OnChainRandomnessConfig,
         onchain_jwk_consensus_config: OnChainJWKConsensusConfig,
+        use_quorum_store_payloads: bool,
     ) -> Self {
         let _entered_runtime = executor.enter();
         let epoch_state = Arc::new(EpochState::new(1, storage.get_validator_set().into()));
@@ -368,6 +393,19 @@ impl NodeSetup {
         ));
         let time_service = Arc::new(ClockTimeService::new(executor));
 
+        let payload_manager: Arc<dyn TPayloadManager> = if use_quorum_store_payloads {
+            Arc::new(QuorumStorePayloadManager::new(
+                Arc::new(MockBatchReader) as Arc<dyn BatchReader>,
+                Box::new(MockQuorumStoreCommitNotifier) as Box<dyn TQuorumStoreCommitNotifier>,
+                None,
+                vec![],
+                hashmap![],
+                false,
+            ))
+        } else {
+            Arc::new(DirectMempoolPayloadManager::new())
+        };
+
         let window_size = onchain_consensus_config.window_size();
         let block_store = Arc::new(BlockStore::new(
             storage.clone(),
@@ -376,7 +414,7 @@ impl NodeSetup {
             10, // max pruned blocks in mem
             time_service.clone(),
             10,
-            Arc::from(DirectMempoolPayloadManager::new()),
+            payload_manager,
             false,
             window_size,
             Arc::new(Mutex::new(PendingBlocks::new())),
@@ -435,7 +473,7 @@ impl NodeSetup {
             storage.clone(),
             onchain_consensus_config.clone(),
             round_manager_tx,
-            BlockTransactionFilterConfig::default(),
+            block_txn_filter_config.clone(),
             local_config,
             onchain_randomness_config.clone(),
             onchain_jwk_consensus_config.clone(),
@@ -458,6 +496,7 @@ impl NodeSetup {
             _state_sync_receiver,
             id,
             onchain_consensus_config,
+            block_txn_filter_config,
             local_consensus_config,
             onchain_randomness_config,
             onchain_jwk_consensus_config,
@@ -468,6 +507,7 @@ impl NodeSetup {
             round_timeout_queue: VecDeque::new(),
             commit_decision_queue: VecDeque::new(),
             processed_opt_proposal_rx: opt_proposal_loopback_rx,
+            use_quorum_store_payloads,
         }
     }
 
@@ -489,9 +529,11 @@ impl NodeSetup {
             self.safety_rules_manager,
             self.id,
             self.onchain_consensus_config.clone(),
+            self.block_txn_filter_config.clone(),
             self.local_consensus_config.clone(),
             self.onchain_randomness_config.clone(),
             self.onchain_jwk_consensus_config.clone(),
+            self.use_quorum_store_payloads,
         )
     }
 
@@ -685,5 +727,35 @@ impl NodeSetup {
             .commit_to_storage(ordered_blocks)
             .await
             .unwrap();
+    }
+}
+
+/// A mock quorum store commit notifier that provides limited functionality
+struct MockQuorumStoreCommitNotifier;
+
+impl TQuorumStoreCommitNotifier for MockQuorumStoreCommitNotifier {
+    fn notify(&self, _block_timestamp: u64, _batches: Vec<BatchInfo>) {
+        unimplemented!()
+    }
+}
+
+/// A mock batch reader that provides limited functionality
+struct MockBatchReader;
+
+impl BatchReader for MockBatchReader {
+    fn exists(&self, _digest: &HashValue) -> Option<PeerId> {
+        None
+    }
+
+    fn get_batch(
+        &self,
+        _batch_info: BatchInfo,
+        _signers: Vec<PeerId>,
+    ) -> Shared<Pin<Box<dyn Future<Output = ExecutorResult<Vec<SignedTransaction>>> + Send>>> {
+        unimplemented!()
+    }
+
+    fn update_certified_timestamp(&self, _certified_time: u64) {
+        unimplemented!()
     }
 }
