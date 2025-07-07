ec71811557062710e449d3cd95fb8b23ac70ccb6
wqfish <1171005+wqfish@users.noreply.github.com>
2025-07-07 13:22:09 -0700

    test

--- storage/aptosdb/src/state_store/hot_state.rs
+++ storage/aptosdb/src/state_store/hot_state.rs
@@ -9,7 +9,11 @@ use aptos_metrics_core::{IntCounterHelper, IntGaugeHelper, TimerHelper};
 use aptos_storage_interface::state_store::{
     state::State, state_view::hot_state_view::HotStateView,
 };
-use aptos_types::state_store::{hot_state::LRUEntry, state_key::StateKey, state_slot::StateSlot};
+use aptos_types::state_store::{
+    hot_state::{LRUEntry, SpeculativeLRUEntry},
+    state_key::StateKey,
+    state_slot::StateSlot,
+};
 use dashmap::{mapref::one::Ref, DashMap};
 use std::sync::{
     mpsc::{Receiver, SyncSender, TryRecvError},
@@ -212,278 +216,57 @@ impl Committer {
         let mut n_insert = 0;
 
         let delta = to_commit.make_delta(&self.committed.lock());
-        let mut all_updates = delta
-            .shards
-            .iter()
-            .flat_map(|shard| shard.iter())
-            .collect::<Vec<_>>();
-        // We will update the LRU next. Here we put the deletions at the
-        // beginning, then the older updates, and the newest updates are at the
-        // end.
-        all_updates.sort_unstable_by_key(|(_key, slot)| {
-            slot.hot_since_version_opt().map_or(-1, |v| v as i64)
-        });
 
-        let mut updater = LRUUpdater::new(Arc::clone(&self.base), &mut self.head, &mut self.tail);
-        for (key, slot) in all_updates {
-            let has_old_entry = if let Some(old_slot) = self.base.get_state_slot(&key) {
-                self.total_key_bytes -= key.size();
-                self.total_value_bytes -= old_slot.size();
-                true
-            } else {
-                false
-            };
-
-            if slot.is_cold() {
-                // deletion
-                if has_old_entry {
-                    n_delete += 1;
-                    updater.delete(&key);
-                }
-            } else {
-                if has_old_entry {
-                    n_update += 1;
-                } else {
-                    n_insert += 1;
-                };
-
-                self.total_key_bytes += key.size();
-                self.total_value_bytes += slot.size();
-
-                updater.insert(key, slot);
-            }
-        }
-
-        COUNTER.inc_with_by(&["hot_state_delete"], n_delete);
-        COUNTER.inc_with_by(&["hot_state_too_large"], n_too_large);
-        COUNTER.inc_with_by(&["hot_state_update"], n_update);
-        COUNTER.inc_with_by(&["hot_state_insert"], n_insert);
-
-        {
-            let _timer = OTHER_TIMERS_SECONDS.timer_with(&["hot_state_evict"]);
-
-            let evicted = updater.evict();
-            if evicted.is_empty() {
-                return;
-            }
-            for (key, slot) in &evicted {
-                self.total_key_bytes -= key.size();
-                self.total_value_bytes -= slot.size();
-            }
-
-            let head = self
-                .head
-                .as_ref()
-                .expect("LRU must not be empty when eviction has happened.");
-            let latest_version = self
-                .base
-                .inner
-                .get(head)
-                .expect("Head must exist.")
-                .data
-                .expect_hot_since_version();
-            let max_evicted_version = evicted.last().unwrap().1.expect_hot_since_version();
-            GAUGE.set_with(
-                &["hot_state_item_evict_age_versions"],
-                (latest_version - max_evicted_version) as i64,
-            );
-            COUNTER.inc_with_by(&["hot_state_evict"], evicted.len() as u64);
-        }
-    }
-}
-
-struct LRUUpdater<'a, K, V>
-where
-    K: Eq + std::hash::Hash,
-{
-    base: Arc<HotStateBase<K, V>>,
-    head: &'a mut Option<K>,
-    tail: &'a mut Option<K>,
-}
-
-impl<'a, K, V> LRUUpdater<'a, K, V>
-where
-    K: Clone + std::fmt::Debug + Eq + std::hash::Hash,
-    V: Clone + std::fmt::Debug,
-{
-    fn new(
-        base: Arc<HotStateBase<K, V>>,
-        head: &'a mut Option<K>,
-        tail: &'a mut Option<K>,
-    ) -> Self {
-        Self { base, head, tail }
-    }
-
-    fn insert(&mut self, key: K, value: V) {
-        if self.base.inner.contains_key(&key) {
-            self.delete(&key);
-        }
-        self.insert_to_front(key, value);
-    }
-
-    /// Deletes and returns the oldest entry.
-    fn delete_lru(&mut self) -> Option<(K, V)> {
-        let key = match &self.tail {
-            Some(k) => k.clone(),
-            None => return None,
-        };
-        let value = self.delete(&key).expect("Tail must exist.");
-        Some((key, value))
-    }
-
-    fn delete(&mut self, key: &K) -> Option<V> {
-        let old_entry = match self.base.inner.remove(key) {
-            Some((_k, e)) => e,
-            None => return None,
-        };
-
-        match &old_entry.lru.prev {
-            Some(prev_key) => {
-                let mut prev_entry = self
-                    .base
-                    .inner
-                    .get_mut(prev_key)
-                    .expect("The previous key must exist");
-                prev_entry.lru.next = old_entry.lru.next.clone();
-            },
-            None => {
-                // There is no newer entry. The current key was the head.
-                *self.head = old_entry.lru.next.clone();
-            },
-        }
-
-        match &old_entry.lru.next {
-            Some(next_key) => {
-                let mut next_entry = self
-                    .base
-                    .inner
-                    .get_mut(next_key)
-                    .expect("The next key must exist.");
-                next_entry.lru.prev = old_entry.lru.prev;
-            },
-            None => {
-                // There is no older entry. The current key was the tail.
-                *self.tail = old_entry.lru.prev;
-            },
-        }
-
-        Some(old_entry.data)
-    }
-
-    fn insert_to_front(&mut self, key: K, value: V) {
-        assert_eq!(self.head.is_some(), self.tail.is_some());
-        match self.head.take() {
-            Some(head) => {
-                {
-                    // Release the reference to the old entry ASAP to avoid deadlock when inserting
-                    // the new entry below.
-                    let mut old_head_entry =
-                        self.base.inner.get_mut(&head).expect("Head must exist.");
-                    old_head_entry.lru.prev = Some(key.clone());
-                }
-                let entry = Entry {
-                    data: value,
-                    lru: LRUEntry {
-                        prev: None,
-                        next: Some(head),
+        let all_hot_updates: Vec<_> = delta.lru.iter().collect();
+        println!(
+            "new head: {:?}, new tail: {:?}, all_hot_updates: {:?}",
+            delta.get_newest_key(),
+            delta.get_oldest_key(),
+            all_hot_updates
+        );
+        self.head = delta.get_newest_key();
+        self.tail = delta.get_oldest_key();
+        for (key, update_opt) in all_hot_updates {
+            match update_opt {
+                SpeculativeLRUEntry::Existing(update) => match delta.get_state_slot(&key) {
+                    Some(slot) => {
+                        assert!(slot.is_hot());
+                        // This means that both the metadata (prev and next) and the actual state
+                        // value are updated.
+                        let entry = Entry {
+                            data: slot,
+                            lru: LRUEntry {
+                                prev: update.prev,
+                                next: update.next,
+                            },
+                        };
+                        self.base.inner.insert(key, entry);
                     },
-                };
-                self.base.inner.insert(key.clone(), entry);
-                *self.head = Some(key);
-            },
-            None => {
-                let entry = Entry {
-                    data: value,
-                    lru: LRUEntry {
-                        prev: None,
-                        next: None,
-                    },
-                };
-                self.base.inner.insert(key.clone(), entry);
-                *self.head = Some(key.clone());
-                *self.tail = Some(key);
-            },
-        }
-    }
-
-    fn evict(&mut self) -> Vec<(K, V)> {
-        if !self.should_evict() {
-            return Vec::new();
-        }
-
-        let mut items = Vec::with_capacity(self.base.inner.len() - self.base.max_items);
-        while self.should_evict() {
-            items.push(self.delete_lru().unwrap());
-        }
-        items
-    }
-
-    fn should_evict(&self) -> bool {
-        self.base.inner.len() > self.base.max_items
-    }
-
-    #[cfg(test)]
-    fn collect_all(&self) -> Vec<(K, V)> {
-        assert_eq!(self.head.is_some(), self.tail.is_some());
-
-        let mut keys = Vec::new();
-        let mut values = Vec::new();
-
-        let mut current_key = self.head.clone();
-        while let Some(key) = current_key {
-            let entry = self.base.inner.get(&key).unwrap();
-            assert_eq!(entry.lru.prev, keys.last().cloned());
-            keys.push(key);
-            values.push(entry.data.clone());
-            current_key = entry.lru.next.clone();
-        }
-        itertools::zip_eq(keys, values).collect()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{HotStateBase, LRUUpdater};
-    use lru::LruCache;
-    use proptest::{collection::vec, option, prelude::*};
-    use std::sync::Arc;
-
-    const MAX_BYTES: usize = 10000;
-    const MAX_SINGLE_VALUE_BYTES: usize = 100;
-
-    proptest! {
-        #[test]
-        fn test_hot_state_lru(
-            max_items in 1..10usize,
-            updates in vec((0..20u64, option::weighted(0.8, 0..1000u64)), 1..50),
-        ) {
-            let base = Arc::new(HotStateBase::new_empty(
-                max_items,
-                MAX_BYTES,
-                MAX_SINGLE_VALUE_BYTES,
-            ));
-            let mut head = None;
-            let mut tail = None;
-
-            let mut updater = LRUUpdater::new(base, &mut head, &mut tail);
-            let mut cache = LruCache::new(max_items);
-
-            for (key, value_opt) in updates {
-                match value_opt {
-                    Some(value) => {
-                        updater.insert(key, value);
-                        cache.put(key, value);
-                    }
                     None => {
-                        updater.delete(&key);
-                        cache.pop(&key);
-                    }
-                }
-                updater.evict();
+                        // This means that the metadata (prev and next) is updated, but the actual
+                        // state is not updated. For example, a key next to it is updated.
+                        let mut old_entry = self.base.inner.get_mut(&key).unwrap();
+                        old_entry.lru.prev = update.prev;
+                        old_entry.lru.next = update.next;
+                    },
+                },
+                SpeculativeLRUEntry::Deleted => {
+                    // `None` indicates deletion from the LRU meta.
+                    self.base.inner.remove(&key);
+                },
+            }
+        }
 
-                prop_assert_eq!(updater.base.inner.len(), cache.len());
-                let items = updater.collect_all();
-                prop_assert_eq!(items, cache.iter().map(|(k, v)| (*k, *v)).collect::<Vec<_>>());
+        println!(
+            "Committed hot state. Head: {:?}. Tail: {:?}. Entries: {:?}",
+            self.head, self.tail, self.base.inner
+        );
+
+        for (key, slot) in delta.shards.iter().flat_map(|shard| shard.iter()) {
+            if self.base.inner.contains_key(&key) {
+                assert!(slot.is_hot());
+            } else {
+                assert!(slot.is_cold());
             }
         }
     }
