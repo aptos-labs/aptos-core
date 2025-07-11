9de61d51a478dfa63b3c4e3ff87122c6b563c00b
George Mitenkov <georgemitenk0v@gmail.com>
2025-07-11 23:25:58 +0100

    fix lint

--- third_party/move/tools/move-resource-viewer/src/lib.rs
+++ third_party/move/tools/move-resource-viewer/src/lib.rs
@@ -24,7 +24,7 @@ use move_core_types::{
     account_address::AccountAddress,
     function::{ClosureMask, MoveClosure},
     identifier::{IdentStr, Identifier},
-    language_storage::{FunctionParamOrReturnTag, ModuleId, StructTag, TypeTag},
+    language_storage::{ModuleId, StructTag, TypeTag},
     transaction_argument::{convert_txn_args, TransactionArgument},
     u256,
     value::{MoveStruct, MoveTypeLayout, MoveValue},
@@ -457,20 +457,9 @@ impl<V: CompiledModuleView> MoveValueAnnotator<V> {
             TypeTag::U128 => FatType::U128,
             TypeTag::Vector(ty) => FatType::Vector(Box::new(self.resolve_type_impl(ty, limit)?)),
             TypeTag::Function(function_tag) => {
-                let mut convert_tags = |tags: &[FunctionParamOrReturnTag]| {
+                let mut convert_tags = |tags: &[TypeTag]| {
                     tags.iter()
-                        .map(|t| {
-                            use FunctionParamOrReturnTag::*;
-                            Ok(match t {
-                                Reference(t) => {
-                                    FatType::Reference(Box::new(self.resolve_type_impl(t, limit)?))
-                                },
-                                MutableReference(t) => FatType::MutableReference(Box::new(
-                                    self.resolve_type_impl(t, limit)?,
-                                )),
-                                Value(t) => self.resolve_type_impl(t, limit)?,
-                            })
-                        })
+                        .map(|t| self.resolve_type_impl(t, limit))
                         .collect::<anyhow::Result<Vec<_>>>()
                 };
                 FatType::Function(Box::new(FatFunctionType {
