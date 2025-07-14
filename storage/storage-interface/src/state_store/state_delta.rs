a4c443a08403982e0fff056e391a861e1215a9c8
wqfish <1171005+wqfish@users.noreply.github.com>
2025-07-14 16:19:39 -0700

    [hot state] StateView APIs
    Adding a few APIs that will be used later.

--- storage/storage-interface/src/state_store/state_delta.rs
+++ storage/storage-interface/src/state_store/state_delta.rs
@@ -1,10 +1,10 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::state_store::{state::State, NUM_STATE_SHARDS};
+use crate::state_store::state::State;
 use aptos_experimental_layered_map::LayeredMap;
 use aptos_types::{
-    state_store::{state_key::StateKey, state_slot::StateSlot},
+    state_store::{state_key::StateKey, state_slot::StateSlot, NUM_STATE_SHARDS},
     transaction::Version,
 };
 use itertools::Itertools;
