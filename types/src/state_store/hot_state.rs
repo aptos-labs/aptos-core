4ca4da24e00dc02ba09b97d3824772bd60ae62fd
wqfish <1171005+wqfish@users.noreply.github.com>
2025-07-14 12:48:13 -0700

    [hot state] Put lru pointers inside StateSlot
    The type of the speculative state is `Map<StateKey, StateSlot>`, having these
    pointers inside `StateSlot` means that we do not need to change the current type
    of the speculative state, or create a separate speculative state.

--- types/src/state_store/hot_state.rs
+++ types/src/state_store/hot_state.rs
@@ -1,10 +1,29 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct LRUEntry<K> {
     /// The key that is slightly newer than the current entry. `None` for the newest entry.
     pub prev: Option<K>,
     /// The key that is slightly older than the current entry. `None` for the oldest entry.
     pub next: Option<K>,
+}
+
+impl<K> LRUEntry<K> {
+    pub fn uninitialized() -> Self {
+        Self {
+            prev: None,
+            next: None,
+        }
+    }
+}
+
+pub trait THotStateSlot {
+    type Key;
+
+    fn prev(&self) -> Option<&Self::Key>;
+    fn next(&self) -> Option<&Self::Key>;
+
+    fn set_prev(&mut self, prev: Option<Self::Key>);
+    fn set_next(&mut self, next: Option<Self::Key>);
 }
