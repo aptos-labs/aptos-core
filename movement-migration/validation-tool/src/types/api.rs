ea63add8a9930a16a45efa68b135cfdc6c744dab
Sebastian Bach <sebtomba@gmail.com>
2025-07-16 14:28:52 +0200

    feature: add active features check to validation tool (#192)
    * feature: add active features check to validation tool
    
    * fix: increase delay between requests in the active feature check

--- movement-migration/validation-tool/src/types/api.rs
+++ movement-migration/validation-tool/src/types/api.rs
@@ -4,25 +4,25 @@
 use aptos_rest_client::Client;
 use std::ops::Deref;
 
-pub struct MovementRestClient(Client);
-
-impl MovementRestClient {
-    pub fn new(url: &str) -> Result<Self, anyhow::Error> {
-        let client = Client::new(
-            url.parse()
-                .map_err(|e| anyhow::anyhow!("failed to parse Movement rest api url: {}", e))?,
-        );
-        Ok(Self(client))
-    }
-}
-
-impl Deref for MovementRestClient {
-    type Target = Client;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
+// pub struct MovementRestClient(Client);
+//
+// impl MovementRestClient {
+//     pub fn new(url: &str) -> Result<Self, anyhow::Error> {
+//         let client = Client::new(
+//             url.parse()
+//                 .map_err(|e| anyhow::anyhow!("failed to parse Movement rest api url: {}", e))?,
+//         );
+//         Ok(Self(client))
+//     }
+// }
+//
+// impl Deref for MovementRestClient {
+//     type Target = Client;
+//
+//     fn deref(&self) -> &Self::Target {
+//         &self.0
+//     }
+// }
 
 pub struct MovementAptosRestClient(Client);
 
