974d6029d301f6b881596f746b1891cfa19ee5f3
wqfish <1171005+wqfish@users.noreply.github.com>
2025-07-23 11:11:19 -0700

    [hot state] Populate hot state promotions
    This was originally done in #16485. However, subsequent changes (mostly priority
    fee implementation) undid some of the changes, so while
    `BlockHotStateOpAccumulator` still computes `slots_to_make_hot`, the result is
    never populated to the hot state storage. Therefore, the read-only keys are
    never cached.
    
    This change poluates the infomation again by putting it inside `BlockOutput`.
    
    The tricky part is that the read write summary can sometimes be inaccurate. For
    example, occasionally a key is written to, but it's not in the write summary,
    then the code would generate both a `Modification` op and a `MakeHot` op for
    the same key. To be defensive, if `Creation`/`Modification`/`Deletion` is
    followed by `MakeHot` on the same key when computing `BatchedStateUpdateRefs`,
    we have to discard the `MakeHot`.

--- storage/storage-interface/src/state_store/state_update_refs.rs
+++ storage/storage-interface/src/state_store/state_update_refs.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{metrics::TIMER, state_store::versioned_state_value::StateUpdateRef};
+use aptos_logger::{sample, sample::SampleRate, warn};
 use aptos_metrics_core::TimerHelper;
 use aptos_types::{
     state_store::{state_key::StateKey, NUM_STATE_SHARDS},
@@ -11,8 +12,12 @@ use aptos_types::{
 use arr_macro::arr;
 use itertools::Itertools;
 use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
-use std::collections::HashMap;
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    time::Duration,
+};
 
+#[derive(Debug)]
 pub struct PerVersionStateUpdateRefs<'kv> {
     pub first_version: Version,
     pub num_versions: usize,
@@ -94,6 +99,7 @@ impl BatchedStateUpdateRefs<'_> {
     }
 }
 
+#[derive(Debug)]
 pub struct StateUpdateRefs<'kv> {
     pub per_version: PerVersionStateUpdateRefs<'kv>,
     /// Batched updates (updates for the same keys are merged) from the
@@ -204,11 +210,41 @@ impl<'kv> StateUpdateRefs<'kv> {
             .par_iter_mut()
             .zip_eq(ret.shards.par_iter_mut())
             .for_each(|(shard_iter, dedupped)| {
-                dedupped.extend(
-                    shard_iter
-                        // n.b. take_while_ref so that in the next step we can process the rest of the entries from the iters.
-                        .take_while_ref(|(_k, u)| u.version < end_version),
-                )
+                // n.b. take_while_ref so that in the next step we can process the rest of the
+                // entries from the iters.
+                for (k, u) in shard_iter.take_while_ref(|(_k, u)| u.version < end_version) {
+                    // If it's a value write op (Creation/Modification/Deletion), just insert and
+                    // overwrite the previous op.
+                    if u.state_op.is_value_write_op() {
+                        dedupped.insert(k, u);
+                        continue;
+                    }
+
+                    // If we see a hotness op, we check if there is a value write op with the same
+                    // key before. This is unlikely, but if it does happen (e.g. if the write
+                    // summary used to compute MakeHot is missing keys), we must discard the
+                    // hotness op to avoid overwriting the value write op.
+                    match dedupped.entry(k) {
+                        Entry::Occupied(mut entry) => {
+                            let prev_op = &entry.get().state_op;
+                            sample!(
+                                SampleRate::Duration(Duration::from_secs(10)),
+                                warn!(
+                                    "Key: {:?}. Previous write op: {}. Current write op: {}",
+                                    k,
+                                    prev_op.as_ref(),
+                                    u.state_op.as_ref()
+                                )
+                            );
+                            if !prev_op.is_value_write_op() {
+                                entry.insert(u);
+                            }
+                        },
+                        Entry::Vacant(entry) => {
+                            entry.insert(u);
+                        },
+                    }
+                }
             });
         ret
     }
