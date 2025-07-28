2436ef2e3f003470e8966ce00a7e8a1a4b1502ea
George Mitenkov <georgemitenk0v@gmail.com>
2025-07-28 21:44:28 +0100

    [lazy-loading] Module metadata integration (#16590)

--- third_party/move/move-vm/runtime/src/interpreter.rs
+++ third_party/move/move-vm/runtime/src/interpreter.rs
@@ -1135,6 +1135,7 @@ where
         ty: &Type,
     ) -> PartialVMResult<DataCacheEntry> {
         let (entry, bytes_loaded) = TransactionDataCache::create_data_cache_entry(
+            self.loader,
             self.layout_converter,
             gas_meter,
             traversal_context,
