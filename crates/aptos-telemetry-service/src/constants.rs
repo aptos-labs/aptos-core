807eb181bc701e0f645a2a9847502f833d8d5ecc
Balaji Arun <balaji@aptoslabs.com>
2025-08-05 16:28:33 -0700

    [telemetry] stop pinging httpbin
    use xff header

--- crates/aptos-telemetry-service/src/constants.rs
+++ crates/aptos-telemetry-service/src/constants.rs
@@ -17,3 +17,5 @@ pub const GCP_SERVICE_PROJECT_ID_ENV: &str = "GCP_METADATA_PROJECT_ID";
 /// Environment variable with the container identifier for this cloud run revision
 /// This variable must be set by calling the metadata server
 pub const GCP_CLOUD_RUN_INSTANCE_ID_ENV: &str = "GCP_CLOUD_RUN_INSTANCE_ID";
+/// The IP address key
+pub const IP_ADDRESS_KEY: &str = "IP_ADDRESS";
