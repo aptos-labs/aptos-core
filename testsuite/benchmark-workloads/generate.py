7fdcb9f767b059d82c21805d41af9b5fccdf355f
George Mitenkov <georgemitenk0v@gmail.com>
2025-08-06 20:00:48 +0100

    [benchmark] Benchmark generator to use a single binary, include it and auto-generate traits (#17237)

--- testsuite/benchmark-workloads/generate.py
+++ testsuite/benchmark-workloads/generate.py
@@ -4,31 +4,57 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import subprocess
+import pathlib
 
-# Specifies directories and the latest (unstable) language version is needed when building the packages there.
-DIRECTORIES = [
-    ("packages", ""),
-    ("../../aptos-move/move-examples/token_objects/ambassador", ""),
-    ("../../aptos-move/move-examples/aggregator_examples", ""),
-    ("../../aptos-move/move-examples/bcs-stream", ""),
-    ("packages-experimental", " --latest-language"),
+ROOT = pathlib.Path(__file__).absolute().parents[2]
+
+# Specifies directories for regular packages.
+# Paths are relative to the root of the aptos-core repository.
+PACKAGES = [
+    "testsuite/benchmark-workloads/packages",
+    "aptos-move/move-examples/token_objects/ambassador",
+    "aptos-move/move-examples/aggregator_examples",
+    "aptos-move/move-examples/bcs-stream"
 ]
-# Directory where all binaries (package metadata, modules, scripts) are saved.
-PREBUILT_PACKAGES_DIR = "../../crates/transaction-workloads-lib/prebuilt-packages"
+# Specifies directories for experimental packages (will be compiled with latest, possibly unstable) language version.
+# Paths are relative to the root of the aptos-core repository.
+EXPERIMENTAL_PACKAGES = [
+    "testsuite/benchmark-workloads/packages-experimental/experimental_usecases",
+]
+
+# Directory where the prebuilt binary is saved.
+PREBUILT_PACKAGES_DIR = "crates/transaction-workloads-lib"
+
+
+def run_command(command):
+    with subprocess.Popen(
+            command,
+            shell=True,
+            text=True,
+            stdout=subprocess.PIPE,
+            bufsize=1,
+            universal_newlines=True,
+    ) as p:
+        if p.stdout is not None:
+            for line in p.stdout:
+                print(line, end="")
+
+    if p.returncode != 0:
+        print("Failed to generate the workloads!")
+        exit(p.returncode)
+
 
 if __name__ == "__main__":
-    subprocess.run(f"rm -rf {PREBUILT_PACKAGES_DIR}/*", shell=True)
+    package_paths = " ".join(f"--packages-path {ROOT / package}" for package in PACKAGES)
+    experimental_package_paths = " ".join(
+        f"--experimental-packages-path {ROOT / package}" for package in EXPERIMENTAL_PACKAGES)
 
-    for dir, latest_language_arg in DIRECTORIES:
-        # By default, we built in dev mode with local framework.
-        command = (
-            f"cargo run --package package-generator -- --dev --use-local-std --packages-path {dir} "
-            f"--prebuilt-packages-path {PREBUILT_PACKAGES_DIR}{latest_language_arg}"
-        )
-        print(command)
-        subprocess.run(command, shell=True)
-
-    # Also, update the packages using the legacy flow (generating Rust files with inline binaries).
-    command = "cargo run -p module-publish"
-    print(f"[WARNING] Generating packages using the deprecated flow: {command}")
-    subprocess.run(command, shell=True)
+    # By default, we built in with local framework.
+    command = (f"cargo run --package package-generator -- "
+               f"--use-local-std "
+               f"{package_paths} "
+               f"{experimental_package_paths} "
+               f"--prebuilt-packages-file-dir {ROOT / PREBUILT_PACKAGES_DIR} "
+               f"--prebuilt-packages-rust-dir {ROOT / PREBUILT_PACKAGES_DIR}/src")
+    print(command)
+    run_command(command)
