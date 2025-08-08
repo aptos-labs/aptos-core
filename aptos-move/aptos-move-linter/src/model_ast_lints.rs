0ba1b867ac120f24dfeba71e57b11bbbc6328f15
Facundo Lerena <faculerena@gmail.com>
2025-08-08 12:51:00 -0300

    [lint] Add zero_address check and tests

--- aptos-move/aptos-move-linter/src/model_ast_lints.rs
+++ aptos-move/aptos-move-linter/src/model_ast_lints.rs
@@ -3,13 +3,9 @@
 
 //! This module (and its submodules) contain various model-AST-based lint checks.
 
-mod almost_swapped;
-
 use move_compiler_v2::external_checks::ExpChecker;
 
 /// Returns a default pipeline of "expression linters" to run.
 pub fn get_default_linter_pipeline() -> Vec<Box<dyn ExpChecker>> {
-    vec![
-        Box::<almost_swapped::AlmostSwapped>::default(),
-    ]
+    vec![]
 }
--- aptos-move/aptos-move-linter/src/stackless_bytecode_lints.rs
+++ aptos-move/aptos-move-linter/src/stackless_bytecode_lints.rs
@@ -4,10 +4,11 @@
 //! This module (and its submodules) contain various stackless-bytecode-based lint checks.
 //! Live variable analysis is a prerequisite for this lint processor.
 //! The lint checks also assume that all the correctness checks have already been performed.
+mod zero_address;
 
 use move_compiler_v2::external_checks::StacklessBytecodeChecker;
 
 /// Get default pipeline of "stackless bytecode linters" to run.
 pub fn get_default_linter_pipeline() -> Vec<Box<dyn StacklessBytecodeChecker>> {
-    vec![]
+    vec![Box::<zero_address::ZeroAddress>::default()]
 }
