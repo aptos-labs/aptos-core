3128feba0fc25fa192122609401a69b80eb7d365
James Kao <bleach94539@gmail.com>
2025-08-11 11:31:55 -0700

    rename vars

--- docker/wait-images-ci.mjs
+++ docker/wait-images-ci.mjs
@@ -21,13 +21,13 @@ import {
 async function main() {
   const REQUIRED_ARGS = ["GIT_SHA", "GCP_DOCKER_ARTIFACT_REPO"];
   const OPTIONAL_ARGS = ["WAIT_FOR_IMAGE_SECONDS"];
-  const BOOLEAN_ARGS = ["RELEASE_DEFAULT", "PERF", "FAILPOINTS"];
+  const BOOLEAN_ARGS = ["PROFILE_RELEASE", "PROFILE_PERF", "FEATURES_FAILPOINTS"];
 
   const parsedArgs = parseArgsFromFlagOrEnv(REQUIRED_ARGS, OPTIONAL_ARGS, BOOLEAN_ARGS);
 
   // if none are set, check all images
-  if (!parsedArgs.RELEASE_DEFAULT && !parsedArgs.PERF && !parsedArgs.FAILPOINTS) {
-    parsedArgs.RELEASE_DEFAULT = parsedArgs.PERF = parsedArgs.FAILPOINTS = true;
+  if (!parsedArgs.PROFILE_RELEASE && !parsedArgs.PROFILE_PERF && !parsedArgs.FEATURES_FAILPOINTS) {
+    parsedArgs.PROFILE_RELEASE = parsedArgs.PROFILE_PERF = parsedArgs.FEATURES_FAILPOINTS = true;
   }
 
   await assertExecutingInRepoRoot();
