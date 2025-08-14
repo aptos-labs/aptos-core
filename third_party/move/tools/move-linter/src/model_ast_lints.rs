6602a1bc535ba1c702cccb243c0adc0a565b9a94
Jose Garcia Crosta <jgcrosta@gmail.com>
2025-08-14 10:50:54 -0300

    Add redundant linter and test-cases

--- third_party/move/tools/move-linter/src/model_ast_lints.rs
+++ third_party/move/tools/move-linter/src/model_ast_lints.rs
@@ -13,6 +13,7 @@ mod needless_deref_ref;
 mod needless_ref_deref;
 mod needless_ref_in_field_access;
 mod nonminimal_bool;
+mod redundant_comparison;
 mod self_assignment;
 mod simpler_numeric_expression;
 mod unnecessary_boolean_identity_comparison;
@@ -36,6 +37,7 @@ pub fn get_default_linter_pipeline(config: &BTreeMap<String, String>) -> Vec<Box
         Box::<needless_deref_ref::NeedlessDerefRef>::default(),
         Box::<needless_ref_deref::NeedlessRefDeref>::default(),
         Box::<nonminimal_bool::NonminimalBool>::default(),
+        Box::<redundant_comparison::RedundantComparison>::default(),
         Box::<self_assignment::SelfAssignment>::default(),
         Box::<simpler_numeric_expression::SimplerNumericExpression>::default(),
         Box::<unnecessary_boolean_identity_comparison::UnnecessaryBooleanIdentityComparison>::default(),
