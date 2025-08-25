ce18326eb6b737ea7ac14cd5da849ee2d7c0cfe4
George Mitenkov <georgemitenk0v@gmail.com>
2025-08-25 14:36:26 +0100

    [EXPERIMENT] Option as enum

--- aptos-move/framework/move-stdlib/src/natives/bcs.rs
+++ aptos-move/framework/move-stdlib/src/natives/bcs.rs
@@ -28,7 +28,10 @@ use smallvec::{smallvec, SmallVec};
 use std::collections::VecDeque;
 
 pub fn create_option_u64(value: Option<u64>) -> Value {
-    Value::struct_(Struct::pack(vec![Value::vector_u64(value)]))
+    match value {
+        Some(value) => Value::struct_(Struct::pack_variant(1, vec![Value::u64(value)])),
+        None => Value::struct_(Struct::pack_variant(0, vec![])),
+    }
 }
 
 /***************************************************************************************************
