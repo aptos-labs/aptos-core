a37394737830450d147fc0023688ef8d64595f0d
Facundo Lerena <faculerena@gmail.com>
2025-08-25 15:25:50 -0300

    [lint] Added `empty_range` lint and tests

--- third_party/move/tools/move-linter/src/model_ast_lints.rs
+++ third_party/move/tools/move-linter/src/model_ast_lints.rs
@@ -6,6 +6,7 @@
 mod almost_swapped;
 mod assert_const;
 mod blocks_in_conditions;
+mod empty_range;
 mod equal_operands_in_bin_op;
 mod needless_bool;
 mod needless_deref_ref;
@@ -29,6 +30,7 @@ pub fn get_default_linter_pipeline(config: &BTreeMap<String, String>) -> Vec<Box
         Box::<assert_const::AssertConst>::default(),
         Box::<blocks_in_conditions::BlocksInConditions>::default(),
         Box::<equal_operands_in_bin_op::EqualOperandsInBinOp>::default(),
+        Box::<empty_range::EmptyRange>::default(),
         Box::<needless_bool::NeedlessBool>::default(),
         Box::<needless_ref_in_field_access::NeedlessRefInFieldAccess>::default(),
         Box::<needless_deref_ref::NeedlessDerefRef>::default(),
