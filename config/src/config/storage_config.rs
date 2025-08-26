eb4727d75f102176ed527d3d1ec108729e00bb09
wqfish <1171005+wqfish@users.noreply.github.com>
2025-08-26 00:05:21 -0700

    [Storage] Set cache_index_and_filter_blocks by default
    I'm not sure the current configuration makes a lot of sense in terms of memory
    management.
    
    `max_open_files = 5000` is quite high. Given that this limit is per database and
    we have at least 40~50 of them (16 state kv shards, 16 state merkle shards,
    etc), we probably rarely hit this limit. So the files remain open once they are
    touched.
    
    In addition, we have `cache_index_and_filter_blocks = false`.
    
    The combination of these means that index/filter blocks also stay in memory
    forever once they are loaded, even if they are just used once and never touched
    again. Therefore, we can waste quite some memory on unused things. We can verify
    this by looking at some full nodes that are running at 30~50GB RSS: the stats
    show that `rocksdb.estimate-table-readers-mem` is a major contributor, often
    more than 70%. See https://gist.github.com/wqfish/4b2b3bccba6466d31ab7732524967ed6
    for an example.
    
    This change puts index/filter blocks in the block cache, so the unused ones can
    be evicted when the cache is full. I think for now the numbers are relatively
    conservative, and we might able to reduce them further. Also we are using
    separate caches for different things (bigger ones for each state kv shard and
    smaller ones for everything else), but we can also consider experimenting with
    using a single big cache for everything, so it's even easier to manage.

--- config/src/config/storage_config.rs
+++ config/src/config/storage_config.rs
@@ -134,12 +134,12 @@ impl Default for RocksdbConfig {
             // This includes threads for flashing and compaction. Rocksdb will decide the # of
             // threads to use internally.
             max_background_jobs: 16,
-            // Default block cache size is 8MB,
-            block_cache_size: 8 * (1u64 << 20),
+            // Default block cache size is 64MB,
+            block_cache_size: 64 * (1 << 20),
             // Default block cache size is 4KB,
             block_size: 4 * (1u64 << 10),
             // Whether cache index and filter blocks into block cache.
-            cache_index_and_filter_blocks: false,
+            cache_index_and_filter_blocks: true,
         }
     }
 }
@@ -165,7 +165,11 @@ impl Default for RocksdbConfigs {
         Self {
             ledger_db_config: RocksdbConfig::default(),
             state_merkle_db_config: RocksdbConfig::default(),
-            state_kv_db_config: RocksdbConfig::default(),
+            state_kv_db_config: RocksdbConfig {
+                // 1GB. Note that this is per shard.
+                block_cache_size: 1 << 30,
+                ..Default::default()
+            },
             index_db_config: RocksdbConfig {
                 max_open_files: 1000,
                 ..Default::default()
