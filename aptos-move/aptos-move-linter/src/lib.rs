b72f2142122412dd337c6299cc2740d17eb0b4dc
Víctor M. González <victor.gonzalez@coinfabrik.com>
2025-08-29 16:16:49 -0300

    ...

--- aptos-move/aptos-move-linter/src/lib.rs
+++ aptos-move/aptos-move-linter/src/lib.rs
@@ -5,26 +5,41 @@ mod model_ast_lints;
 mod stackless_bytecode_lints;
 mod assignment_tracker;
 mod utils;
-
 use move_compiler_v2::external_checks::{ExpChecker, ExternalChecks, StacklessBytecodeChecker};
 use std::sync::Arc;
+use std::collections::BTreeMap;
 
 /// Holds collection of lint checks for Move.
-pub struct SecurityChecks {}
+pub struct SecurityChecks {
+    config: BTreeMap<String, String>,
+}
 
 impl ExternalChecks for SecurityChecks {
     fn get_exp_checkers(&self) -> Vec<Box<dyn ExpChecker>> {
-        model_ast_lints::get_default_linter_pipeline()
+        model_ast_lints::get_default_linter_pipeline(&self.config)
     }
 
     fn get_stackless_bytecode_checkers(&self) -> Vec<Box<dyn StacklessBytecodeChecker>> {
-        stackless_bytecode_lints::get_default_linter_pipeline()
+        stackless_bytecode_lints::get_default_linter_pipeline(&self.config)
     }
 }
 
 impl SecurityChecks {
     /// Make an instance of lint checks for Move, provided as `ExternalChecks`.
-    pub fn make() -> Arc<dyn ExternalChecks> {
-        Arc::new(SecurityChecks {})
+    /// Will panic if the configuration is not valid.
+    pub fn make(config: BTreeMap<String, String>) -> Arc<dyn ExternalChecks> {
+        // Check whether the config map is valid.
+        // Currently, we expect it to contain a single key "checks" that can map to
+        // one of the three string values: "default", "strict", or "experimental".
+        if config.len() != 1 {
+            panic!("config should have a single key `checks`");
+        }
+        let checks_value = config
+            .get("checks")
+            .expect("config is missing the `checks` key");
+        if !matches!(checks_value.as_str(), "default" | "strict" | "experimental") {
+            panic!("Invalid value for `checks` key in the config, expected one of: `default`, `strict`, or `experimental`");
+        }
+        Arc::new(SecurityChecks { config })
     }
 }
--- aptos-move/aptos-move-linter/src/stackless_bytecode_lints.rs
+++ aptos-move/aptos-move-linter/src/stackless_bytecode_lints.rs
@@ -7,8 +7,16 @@
 
 mod contains_in_table;
 use move_compiler_v2::external_checks::StacklessBytecodeChecker;
+use std::collections::BTreeMap;
 
 /// Get default pipeline of "stackless bytecode linters" to run.
-pub fn get_default_linter_pipeline() -> Vec<Box<dyn StacklessBytecodeChecker>> {
-    vec![Box::<contains_in_table::ContainsInTable>::default()]
+pub fn get_default_linter_pipeline(config: &BTreeMap<String, String>) -> Vec<Box<dyn StacklessBytecodeChecker>> {
+    let mut ret: Vec<Box<dyn StacklessBytecodeChecker>> = vec![];
+
+    let checks_category = config.get("checks").map_or("default", |s| s.as_str());
+    if checks_category == "experimental" {
+        ret.push(Box::<contains_in_table::ContainsInTable>::default());
+    }
+
+    ret
 }
