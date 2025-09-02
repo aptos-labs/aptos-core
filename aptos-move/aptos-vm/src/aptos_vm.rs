5a1d26092e4cb43a1bedaf30f4d307c33db31ca0
Wolfgang Grieskamp <wg@aptoslabs.com>
2025-09-01 21:29:21 -0700

    [vm] Suppress paranoid checks for trusted code
    This introduces the concept of 'trusted code' in the VM. For now this is defined as code at a 'special' address (0x1-0xf), but this can later be extended.
    
    When trusted code is executed, paranoid mode is deactivated. Specifically, for trusted code it can be assumed that the deployed bytecode is not maliciously engineerd but generated by the Move compiler and has been gone through rigerous testing, auditing, and verification steps.

--- aptos-move/aptos-vm/src/aptos_vm.rs
+++ aptos-move/aptos-vm/src/aptos_vm.rs
@@ -2802,9 +2802,9 @@ impl AptosVM {
                             speculative_error!(
                                 log_context,
                                 format!(
-                                    "[aptos_vm] Transaction breaking invariant violation. txn: {:?}, status: {:?}",
+                                    "[aptos_vm] Transaction breaking invariant violation: {:?}\ntxn: {:?}",
+                                    vm_status,
                                     bcs::to_bytes::<SignedTransaction>(txn),
-                                    vm_status
                                 ),
                             );
                         },
@@ -2815,9 +2815,9 @@ impl AptosVM {
                             {
                                 error!(
                                 *log_context,
-                                "[aptos_vm] Transaction breaking paranoid mode. txn: {:?}, status: {:?}",
-                                bcs::to_bytes::<SignedTransaction>(txn),
+                                "[aptos_vm] Transaction breaking paranoid mode: {:?}\ntxn: {:?}",
                                 vm_status,
+                                bcs::to_bytes::<SignedTransaction>(txn),
                             );
                             },
                         // Paranoid mode failure but with reference counting
@@ -2827,9 +2827,9 @@ impl AptosVM {
                             {
                                 error!(
                                 *log_context,
-                                "[aptos_vm] Transaction breaking paranoid mode. txn: {:?}, status: {:?}",
-                                bcs::to_bytes::<SignedTransaction>(txn),
+                                "[aptos_vm] Transaction breaking paranoid mode: {:?}\ntxn: {:?}",
                                 vm_status,
+                                bcs::to_bytes::<SignedTransaction>(txn),
                             );
                             },
                         // Ignore DelayedFields speculative errors as it can be intentionally triggered by parallel execution.
@@ -2840,9 +2840,9 @@ impl AptosVM {
                         _ => {
                             error!(
                                 *log_context,
-                                "[aptos_vm] Transaction breaking invariant violation. txn: {:?}, status: {:?}",
-                                bcs::to_bytes::<SignedTransaction>(txn),
+                                "[aptos_vm] Transaction breaking invariant violation: {:?}\ntxn: {:?}, ",
                                 vm_status,
+                                bcs::to_bytes::<SignedTransaction>(txn),
                             );
                         },
                     }
