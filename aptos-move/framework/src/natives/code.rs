60ed9cae1eb4b0c7d8a352c3f551cbb564fa3eb5
George Mitenkov <georgemitenk0v@gmail.com>
2025-09-04 12:11:40 +0100

    [session][1/n] Introduce versioned native extensions

--- aptos-move/framework/src/natives/code.rs
+++ aptos-move/framework/src/natives/code.rs
@@ -14,7 +14,9 @@ use aptos_types::{
 use better_any::{Tid, TidAble};
 use move_binary_format::errors::{PartialVMError, PartialVMResult};
 use move_core_types::{account_address::AccountAddress, gas_algebra::NumBytes};
-use move_vm_runtime::native_functions::NativeFunction;
+use move_vm_runtime::{
+    native_extensions::VersionControlledNativeExtension, native_functions::NativeFunction,
+};
 use move_vm_types::{
     loaded_data::runtime_types::Type,
     values::{Struct, Value},
@@ -201,6 +203,22 @@ pub struct NativeCodeContext {
     /// using the native code defined in this context. It is later extracted by the VM for further
     /// checks and processing the actual publish.
     requested_module_bundle: Option<PublishRequest>,
+}
+
+impl VersionControlledNativeExtension for NativeCodeContext {
+    fn undo(&mut self) {
+        // No-op: nothing to undo.
+    }
+
+    fn save(&mut self) {
+        // No-op: nothing to save.
+    }
+
+    fn reset(&mut self, _txn_hash: &[u8; 32], _script_hash: &[u8], _session_counter: u8) {
+        // TODO(sessions): consider not enabling context for prologue.
+        self.enabled = true;
+        self.requested_module_bundle = None;
+    }
 }
 
 impl NativeCodeContext {
