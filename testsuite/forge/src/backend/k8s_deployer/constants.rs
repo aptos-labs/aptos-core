9eac2b7b13d8234f1d1d88dd80a7294897a54d47
rustielin <12578616+rustielin@users.noreply.github.com>
2025-09-05 00:05:18 +0000

    [forge] bump deployer version

--- testsuite/forge/src/backend/k8s_deployer/constants.rs
+++ testsuite/forge/src/backend/k8s_deployer/constants.rs
@@ -10,7 +10,7 @@ pub const INDEXER_GRPC_DOCKER_IMAGE_REPO: &str =
     "us-docker.pkg.dev/aptos-registry/docker/indexer-grpc";
 
 /// The version of the forge deployer image to use.
-pub const DEFAULT_FORGE_DEPLOYER_IMAGE_TAG: &str = "0769f1eb0c17e101e6e9d852cfe1c22e2ca82190"; // default to the latest stable build from the main branch (2025-08-02)
+pub const DEFAULT_FORGE_DEPLOYER_IMAGE_TAG: &str = "43a9d27a800b44fe1c4331f6f3bfb845f9fe94a1"; // default to the latest stable build from the main branch (2025-09-04)
 
 /// This is the service account name that the deployer will use to deploy the forge components. It may require extra permissions and additonal setup
 pub const FORGE_DEPLOYER_SERVICE_ACCOUNT_NAME: &str = "forge";
