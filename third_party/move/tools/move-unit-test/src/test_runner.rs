9e9e4afaee9ede80f5a455b75f4aab62e1d1e84b
Jos Dehaes <jos.dehaes@gmail.com>
2025-09-10 16:35:28 +0200

    fix: no need to change the method signature

--- third_party/move/tools/move-unit-test/src/test_runner.rs
+++ third_party/move/tools/move-unit-test/src/test_runner.rs
@@ -182,12 +182,7 @@ impl TestRunner {
                     .module_tests
                     .par_iter()
                     .map(|(_, plan)| {
-                        self.testing_config.exec_module_tests(
-                            plan,
-                            writer,
-                            options,
-                            self.testing_config.fail_fast,
-                        )
+                        self.testing_config.exec_module_tests( plan, writer, options)
                     })
                     .try_reduce(TestStatistics::new, |a, b| Ok(a.combine(b)));
                 // If we got an error, it must be a fail-fast error, so extract the stats until the first failure.
@@ -360,7 +355,6 @@ impl SharedTestingConfig {
         test_plan: &ModuleTestPlan,
         output: &TestOutput<impl Write>,
         factory: &Mutex<F>,
-        fail_fast: bool,
     ) -> Result<TestStatistics> {
         let mut stats = TestStatistics::new();
 
@@ -433,7 +427,7 @@ impl SharedTestingConfig {
                                 ),
                                 test_plan,
                             );
-                            if fail_fast {
+                            if self.fail_fast {
                                 return Err(FailFast(stats).into());
                             }
                         },
@@ -451,7 +445,7 @@ impl SharedTestingConfig {
                                 ),
                                 test_plan,
                             );
-                            if fail_fast {
+                            if self.fail_fast {
                                 return Err(FailFast(stats).into());
                             }
                         },
@@ -467,7 +461,7 @@ impl SharedTestingConfig {
                                 ),
                                 test_plan,
                             );
-                            if fail_fast {
+                            if self.fail_fast {
                                 return Err(FailFast(stats).into());
                             }
                         },
@@ -482,7 +476,7 @@ impl SharedTestingConfig {
                                 ),
                                 test_plan,
                             );
-                            if fail_fast {
+                            if self.fail_fast {
                                 return Err(FailFast(stats).into());
                             }
                         },
@@ -518,9 +512,8 @@ impl SharedTestingConfig {
         test_plan: &ModuleTestPlan,
         writer: &Mutex<impl Write>,
         factory: &Mutex<F>,
-        fail_fast: bool,
     ) -> Result<TestStatistics> {
         let output = TestOutput { test_plan, writer };
-        self.exec_module_tests_move_vm_and_stackless_vm(test_plan, &output, factory, fail_fast)
+        self.exec_module_tests_move_vm_and_stackless_vm(test_plan, &output, factory)
     }
 }
