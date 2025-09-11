3f74b07043135c20f2f606ee7e0cb02bc17ebe24
Balaji Arun <balaji@aptoslabs.com>
2025-09-11 14:38:16 -0700

    test

--- types/src/on_chain_config/consensus_config.rs
+++ types/src/on_chain_config/consensus_config.rs
@@ -486,22 +486,23 @@ impl Default for ConsensusConfigV1 {
             back_pressure_limit: 10,
             exclude_round: 40,
             max_failed_authors_to_store: 10,
-            proposer_election_type: ProposerElectionType::LeaderReputation(
-                LeaderReputationType::ProposerAndVoterV2(ProposerAndVoterConfig {
-                    active_weight: 1000,
-                    inactive_weight: 10,
-                    failed_weight: 1,
-                    failure_threshold_percent: 10, // = 10%
-                    // In each round we get stastics for the single proposer
-                    // and large number of validators. So the window for
-                    // the proposers needs to be significantly larger
-                    // to have enough useful statistics.
-                    proposer_window_num_validators_multiplier: 10,
-                    voter_window_num_validators_multiplier: 1,
-                    weight_by_voting_power: true,
-                    use_history_from_previous_epoch_max_count: 5,
-                }),
-            ),
+            proposer_election_type: ProposerElectionType::FixedProposer(1),
+            // proposer_election_type: ProposerElectionType::LeaderReputation(
+            //     LeaderReputationType::ProposerAndVoterV2(ProposerAndVoterConfig {
+            //         active_weight: 1000,
+            //         inactive_weight: 10,
+            //         failed_weight: 1,
+            //         failure_threshold_percent: 10, // = 10%
+            //         // In each round we get stastics for the single proposer
+            //         // and large number of validators. So the window for
+            //         // the proposers needs to be significantly larger
+            //         // to have enough useful statistics.
+            //         proposer_window_num_validators_multiplier: 10,
+            //         voter_window_num_validators_multiplier: 1,
+            //         weight_by_voting_power: true,
+            //         use_history_from_previous_epoch_max_count: 5,
+            //     }),
+            // ),
         }
     }
 }
