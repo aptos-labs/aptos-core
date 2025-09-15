bedf77fbcea913bee84b677c4649dd3bca7e7f71
George Mitenkov <georgemitenk0v@gmail.com>
2025-09-15 13:50:23 +0100

    [vm] Generic per-interpreter function frame caches
    Benefits:
      - Reduced drop overhead (only on interpreter finish)
      - Generic function frame caches are cached for different call-sites

--- third_party/move/move-vm/runtime/src/interpreter_caches.rs
+++ third_party/move/move-vm/runtime/src/interpreter_caches.rs
@@ -1,43 +1,29 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
+//! Interpreter caches keep a record of cached data for each function call observed during
+//! execution (e.g., caching [FrameTypeCache]). As a result, when calling function `foo<u64>`
+//! at multiple call-sites, cached information from `foo<u64>` is re-used and only computed once
+//! on the first call.
+
 use crate::{
     frame_type_cache::{FrameTypeCache, RuntimeCacheTraits},
-    Function, LoadedFunction,
+    loader::{FunctionPtr, GenericFunctionPtr},
+    LoadedFunction,
 };
-use std::{
-    cell::RefCell,
-    collections::HashMap,
-    hash::{Hash, Hasher},
-    rc::Rc,
-    sync::Arc,
-};
-
-/// Stable pointer identity for a [Function] within a single interpreter invocation.
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub(crate) struct FunctionPtr(*const Function);
-
-impl FunctionPtr {
-    pub(crate) fn from_loaded_function(function: &LoadedFunction) -> Self {
-        FunctionPtr(Arc::as_ptr(&function.function))
-    }
-}
-
-impl Hash for FunctionPtr {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        state.write_usize(self.0 as usize);
-    }
-}
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 /// Interpreter-level caches for function data (single-threaded)
 pub struct InterpreterFunctionCaches {
     function_instruction_caches: HashMap<FunctionPtr, Rc<RefCell<FrameTypeCache>>>,
+    generic_function_instruction_caches: HashMap<GenericFunctionPtr, Rc<RefCell<FrameTypeCache>>>,
 }
 
 impl InterpreterFunctionCaches {
     pub fn new() -> Self {
         Self {
             function_instruction_caches: HashMap::new(),
+            generic_function_instruction_caches: HashMap::new(),
         }
     }
 
@@ -75,7 +61,12 @@ impl InterpreterFunctionCaches {
         &mut self,
         function: &LoadedFunction,
     ) -> Rc<RefCell<FrameTypeCache>> {
-        // TODO(caches): cache per instantiation!
-        FrameTypeCache::make_rc_for_function(function)
+        debug_assert!(!function.ty_args().is_empty());
+
+        let ptr = GenericFunctionPtr::from_loaded_function(function);
+        self.generic_function_instruction_caches
+            .entry(ptr)
+            .or_insert_with(|| FrameTypeCache::make_rc_for_function(function))
+            .clone()
     }
 }
