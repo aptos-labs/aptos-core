6c828f0cfbf30ca85d4e5ece0bc95f920c53f8c6
Josh Lind <josh.lind@hotmail.com>
2025-10-01 14:15:59 -0400

    [Keyless] Move Pepper Service VUF env variables to binary args.

--- keyless/pepper/scripts/start-pepper-service.sh
+++ keyless/pepper/scripts/start-pepper-service.sh
@@ -10,7 +10,7 @@ if [ "$#" -ne 0 ]; then
 fi
 
 # Specify the VUF private key in hex format. This is a dummy key for testing purposes.
-export VUF_KEY_SEED_HEX=ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+VUF_KEY_SEED_HEX=ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 
 # Specify the URLs for the on-chain keyless configuration and groth16 verification key (for local development, we use devnet)
 export ONCHAIN_GROTH16_VK_URL=https://fullnode.devnet.aptoslabs.com/v1/accounts/0x1/resource/0x1::keyless_account::Groth16VerificationKey
@@ -18,4 +18,4 @@ export ONCHAIN_KEYLESS_CONFIG_URL=https://fullnode.devnet.aptoslabs.com/v1/accou
 
 # Start the pepper service
 echo "Starting the pepper service in local development mode, connecting to Aptos devnet for on-chain data!"
-cargo run -p aptos-keyless-pepper-service -- --local-development-mode
+cargo run -p aptos-keyless-pepper-service -- --local-development-mode --vuf-private-key-seed-hex=${VUF_KEY_SEED_HEX}
