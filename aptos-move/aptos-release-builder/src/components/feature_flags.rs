1a795d4b4cb4785e9f394e79501d4ac3cc83e5de
Teng Zhang <rahxephon89@163.com>
2025-10-10 06:52:59 -0700

    add enable_framework_for_option

--- aptos-move/aptos-release-builder/src/components/feature_flags.rs
+++ aptos-move/aptos-release-builder/src/components/feature_flags.rs
@@ -153,6 +153,7 @@ pub enum FeatureFlag {
     EnableCaptureOption,
     EnableTrustedCode,
     EnableEnumOption,
+    EnableFrameworkForOption,
 }
 
 fn generate_features_blob(writer: &CodeWriter, data: &[u64]) {
@@ -405,6 +406,7 @@ impl From<FeatureFlag> for AptosFeatureFlag {
             FeatureFlag::EnableCaptureOption => AptosFeatureFlag::ENABLE_CAPTURE_OPTION,
             FeatureFlag::EnableTrustedCode => AptosFeatureFlag::ENABLE_TRUSTED_CODE,
             FeatureFlag::EnableEnumOption => AptosFeatureFlag::ENABLE_ENUM_OPTION,
+            FeatureFlag::EnableFrameworkForOption => AptosFeatureFlag::ENABLE_FRAMEWORK_FOR_OPTION,
         }
     }
 }
@@ -584,6 +586,7 @@ impl From<AptosFeatureFlag> for FeatureFlag {
             AptosFeatureFlag::ENABLE_CAPTURE_OPTION => FeatureFlag::EnableCaptureOption,
             AptosFeatureFlag::ENABLE_TRUSTED_CODE => FeatureFlag::EnableTrustedCode,
             AptosFeatureFlag::ENABLE_ENUM_OPTION => FeatureFlag::EnableEnumOption,
+            AptosFeatureFlag::ENABLE_FRAMEWORK_FOR_OPTION => FeatureFlag::EnableFrameworkForOption,
         }
     }
 }
