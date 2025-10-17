b15a89cecdacc7d47b966183c4c96cd36c492eb0
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-10-17 21:09:20 +0300

    fix formatting

--- third_party/move/move-vm/runtime/src/interpreter.rs
+++ third_party/move/move-vm/runtime/src/interpreter.rs
@@ -901,10 +901,7 @@ where
     /// Native functions do not push a frame at the moment and as such errors from a native
     /// function are incorrectly attributed to the caller.
     #[cfg_attr(feature = "force-inline", inline(always))]
-    fn make_call_frame<
-        RTTCheck: RuntimeTypeCheck,
-        RTRCheck: RuntimeRefCheck,
-    >(
+    fn make_call_frame<RTTCheck: RuntimeTypeCheck, RTRCheck: RuntimeRefCheck>(
         &mut self,
         current_frame: &Frame,
         gas_meter: &mut impl GasMeter,
