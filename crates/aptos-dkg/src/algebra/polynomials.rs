ea92e7901b94836cb2e6472ac11e0f164bf51390
Wicher Malten <w.malten@gmail.com>
2025-10-19 18:19:29 +0200

    Addressed comments

--- crates/aptos-dkg/src/algebra/polynomials.rs
+++ crates/aptos-dkg/src/algebra/polynomials.rs
@@ -27,6 +27,10 @@ pub(crate) fn differentiate_in_place<F: Field>(coeffs: &mut Vec<F>) {
 /// Computes a batch of quotient evaluations of the form `(f_i - y) / (x_i - x)`
 /// for slices of field elements `f_vals` and `x_vals`.
 ///
+/// Useful to compute the scalars needed to interpolate a KZG proof (i.e., a quotient commitment) for $f(\gamma) = y$ as
+///    $\pi = sum_i ( f(\omega^i) - f(\gamma) ) / (\omega^i - \gamma) ) * L_i$
+/// Here, $y_i = f(\omega^i), x_i$ = \omega^i, y=f(\gamma) and $x=\gamma$.
+///
 /// # Arguments
 /// * `f_vals` - Slice of field elements representing `f_i`.
 /// * `x_vals` - Slice of field elements representing `x_i`. Must have the same length as `f_vals`.
@@ -58,7 +62,7 @@ pub(crate) fn quotient_evaluations_batch<F: Field>(
         .collect()
 }
 
-/// Evaluate a polynomial given by its values at roots of unity
+/// Evaluate a polynomial given in Lagrange basis (i.e., given by its values at roots of unity)
 /// using the barycentric formula in a field `F`.
 ///
 /// `evals[j] = f(roots_of_unity_in_eval_dom[j])`
@@ -80,18 +84,16 @@ pub fn barycentric_eval<F: Field>(
     let n = evals.len();
     assert_eq!(n, roots_of_unity_in_eval_dom.len());
 
-    // If x exactly matches a root, return the corresponding evaluation
-    for (root, val) in roots_of_unity_in_eval_dom.iter().zip(evals.iter()) {
-        if *root == x {
-            return *val;
-        }
-    }
+    let mut denoms = Vec::with_capacity(roots_of_unity_in_eval_dom.len());
 
-    // Compute denominators (x - omega^j)
-    let mut denoms: Vec<F> = roots_of_unity_in_eval_dom
-        .iter()
-        .map(|&omega_j| x - omega_j)
-        .collect();
+    for (&omega_j, &val) in roots_of_unity_in_eval_dom.iter().zip(evals.iter()) {
+        let denom = x - omega_j;
+        if denom.is_zero() {
+            // x exactly matches a root, so we can return early
+            return val;
+        }
+        denoms.push(denom);
+    }
 
     // Compute prefactor: (x^n - 1) / n
     let mut z_pow_n = x.pow([n as u64]);
@@ -99,7 +101,7 @@ pub fn barycentric_eval<F: Field>(
     let prefactor = z_pow_n * n_inv;
 
     // Efficient batch inversion
-    ark_ff::batch_inversion(&mut denoms); // Using batch_inversion_and_mul here shouldn't speed things up
+    ark_ff::batch_inversion(&mut denoms); // Using batch_inversion_and_mul here instead shouldn't speed things up
 
     // Compute sum_j (omega^j * f(omega^j) * (prefactor / (x - omega^j)))
     let mut sum = F::zero();
