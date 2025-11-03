0ac4ccbec7af11684755fa1f93f9ed34b2704a3b
Balaji Arun <balaji@aptoslabs.com>
2025-11-03 11:37:41 -0800

    [forge] upgrade deployer

--- testsuite/forge/src/backend/k8s_deployer/constants.rs
+++ testsuite/forge/src/backend/k8s_deployer/constants.rs
@@ -10,7 +10,7 @@ pub const INDEXER_GRPC_DOCKER_IMAGE_REPO: &str =
     "us-docker.pkg.dev/aptos-registry/docker/indexer-grpc";
 
 /// The version of the forge deployer image to use.
-pub const DEFAULT_FORGE_DEPLOYER_IMAGE_TAG: &str = "0769f1eb0c17e101e6e9d852cfe1c22e2ca82190"; // default to the latest stable build from the main branch (2025-08-02)
+pub const DEFAULT_FORGE_DEPLOYER_IMAGE_TAG: &str = "cab76806a255586b12a2ab94e3b2973c2b5fac40"; // default to the latest stable build from the main branch (2025-11-03)
 
 /// This is the service account name that the deployer will use to deploy the forge components. It may require extra permissions and additonal setup
 pub const FORGE_DEPLOYER_SERVICE_ACCOUNT_NAME: &str = "forge";
