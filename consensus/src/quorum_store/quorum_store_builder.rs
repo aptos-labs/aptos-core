4f27983a40765dc22dba6515207aad963939afb2
Balaji Arun <balaji@aptoslabs.com>
2025-11-07 08:47:31 -0800

    [qs] introduce BatchInfoExt enum to support extensible BatchInfo

--- consensus/src/quorum_store/quorum_store_builder.rs
+++ consensus/src/quorum_store/quorum_store_builder.rs
@@ -30,9 +30,7 @@ use crate::{
 use aptos_channels::{aptos_channel, message_queues::QueueStyle};
 use aptos_config::config::{BatchTransactionFilterConfig, QuorumStoreConfig};
 use aptos_consensus_types::{
-    common::Author,
-    proof_of_store::{BatchInfo, ProofCache},
-    request_response::GetPayloadCommand,
+    common::Author, proof_of_store::ProofCache, request_response::GetPayloadCommand,
 };
 use aptos_crypto::bls12381::PrivateKey;
 use aptos_logger::prelude::*;
@@ -134,7 +132,7 @@ pub struct InnerBuilder {
     aptos_db: Arc<dyn DbReader>,
     network_sender: NetworkSender,
     verifier: Arc<ValidatorVerifier>,
-    proof_cache: ProofCache<BatchInfo>,
+    proof_cache: ProofCache,
     coordinator_tx: Sender<CoordinatorCommand>,
     coordinator_rx: Option<Receiver<CoordinatorCommand>>,
     batch_generator_cmd_tx: tokio::sync::mpsc::Sender<BatchGeneratorCommand>,
@@ -170,7 +168,7 @@ impl InnerBuilder {
         aptos_db: Arc<dyn DbReader>,
         network_sender: NetworkSender,
         verifier: Arc<ValidatorVerifier>,
-        proof_cache: ProofCache<BatchInfo>,
+        proof_cache: ProofCache,
         quorum_store_storage: Arc<dyn QuorumStoreStorage>,
         broadcast_proofs: bool,
         consensus_key: Arc<PrivateKey>,
