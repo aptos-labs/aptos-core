1c4361e4ab90fa1f061252b108247d3e1ea64d32
Teng Zhang <rahxephon89@163.com>
2025-11-11 10:56:10 -0800

    file format code gen

--- third_party/move/move-model/src/well_known.rs
+++ third_party/move/move-model/src/well_known.rs
@@ -25,6 +25,13 @@ pub fn is_verify_only_attribute_name(s: &str) -> bool {
     s == "verify_only"
 }
 
+// For public struct/enum APIs
+pub const PUBLIC_STRUCT_DELIMITER: &str = "$";
+pub const PACK: &str = "pack";
+pub const UNPACK: &str = "unpack";
+pub const TEST_VARIANT: &str = "test_variant";
+pub const PARAM_NAME_FOR_STRUCT_API: &str = "_s";
+
 pub const VECTOR_MODULE: &str = "vector";
 pub const VECTOR_BORROW_MUT: &str = "vector::borrow_mut";
 pub const EVENT_EMIT_EVENT: &str = "event::emit_event";
