cf9da8282caf45f8d50446dfd9c6d6750c3c5c8e
wqfish <1171005+wqfish@users.noreply.github.com>
2025-11-17 14:07:38 -0800

    cache reads in DbStateView

--- storage/storage-interface/src/state_store/state_view/db_state_view.rs
+++ storage/storage-interface/src/state_store/state_view/db_state_view.rs
@@ -1,8 +1,9 @@
 // Copyright (c) Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::DbReader;
+use crate::{state_store::state_view::cached_state_view::ShardedStateCache, DbReader};
 use aptos_crypto::HashValue;
+use aptos_logger::info;
 use aptos_types::{
     ledger_info::LedgerInfo,
     state_store::{
@@ -12,19 +13,37 @@ use aptos_types::{
     },
     transaction::Version,
 };
-use std::sync::Arc;
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
 
-#[derive(Clone)]
 pub struct DbStateView {
     db: Arc<dyn DbReader>,
     version: Option<Version>,
     /// DB doesn't support returning proofs for buffered state, so only optionally verify proof.
     /// TODO: support returning state proof for buffered state.
     maybe_verify_against_state_root_hash: Option<HashValue>,
+    memorized: ShardedStateCache,
+    n_total_get: AtomicUsize,
+    n_memorized: AtomicUsize,
+}
+
+impl Drop for DbStateView {
+    fn drop(&mut self) {
+        let n_total = self.n_total_get.load(Ordering::Relaxed);
+        let n_cached = self.n_memorized.load(Ordering::Relaxed);
+        info!(
+            "total get: {}, cached: {}. Hit rate: {}%",
+            n_total,
+            n_cached,
+            n_cached as f64 * 100.0 / n_total as f64,
+        )
+    }
 }
 
 impl DbStateView {
-    fn get(&self, key: &StateKey) -> StateViewResult<Option<(Version, StateValue)>> {
+    fn get_unmemorized(&self, key: &StateKey) -> StateViewResult<Option<(Version, StateValue)>> {
         if let Some(version) = self.version {
             if let Some(root_hash) = self.maybe_verify_against_state_root_hash {
                 // TODO(aldenhu): sample-verify proof inside DB
@@ -60,7 +79,15 @@ impl TStateView for DbStateView {
     }
 
     fn get_state_slot(&self, state_key: &StateKey) -> StateViewResult<StateSlot> {
-        Ok(StateSlot::from_db_get(self.get(state_key)?))
+        self.n_total_get.fetch_add(1, Ordering::Relaxed);
+        if let Some(slot) = self.memorized.get_cloned(state_key) {
+            self.n_memorized.fetch_add(1, Ordering::Relaxed);
+            return Ok(slot);
+        }
+
+        let slot = StateSlot::from_db_get(self.get_unmemorized(state_key)?);
+        self.memorized.try_insert(state_key, &slot);
+        Ok(slot)
     }
 
     fn get_usage(&self) -> StateViewResult<StateStorageUsage> {
@@ -80,12 +107,16 @@ pub trait LatestDbStateCheckpointView {
 
 impl LatestDbStateCheckpointView for Arc<dyn DbReader> {
     fn latest_state_checkpoint_view(&self) -> StateViewResult<DbStateView> {
+        let version = self
+            .get_latest_state_checkpoint_version()
+            .map_err(Into::<StateViewError>::into)?;
         Ok(DbStateView {
             db: self.clone(),
-            version: self
-                .get_latest_state_checkpoint_version()
-                .map_err(Into::<StateViewError>::into)?,
+            version,
             maybe_verify_against_state_root_hash: None,
+            memorized: ShardedStateCache::new_empty(version),
+            n_total_get: AtomicUsize::new(0),
+            n_memorized: AtomicUsize::new(0),
         })
     }
 }
@@ -100,6 +131,9 @@ impl DbStateViewAtVersion for Arc<dyn DbReader> {
             db: self.clone(),
             version,
             maybe_verify_against_state_root_hash: None,
+            memorized: ShardedStateCache::new_empty(version),
+            n_total_get: AtomicUsize::new(0),
+            n_memorized: AtomicUsize::new(0),
         })
     }
 }
@@ -135,12 +169,18 @@ impl VerifiedStateViewAtVersion for Arc<dyn DbReader> {
                 db,
                 version: Some(version),
                 maybe_verify_against_state_root_hash: Some(state_root_hash),
+                memorized: ShardedStateCache::new_empty(Some(version)),
+                n_total_get: AtomicUsize::new(0),
+                n_memorized: AtomicUsize::new(0),
             })
         } else {
             Ok(DbStateView {
                 db,
                 version: None,
                 maybe_verify_against_state_root_hash: None,
+                memorized: ShardedStateCache::new_empty(None),
+                n_total_get: AtomicUsize::new(0),
+                n_memorized: AtomicUsize::new(0),
             })
         }
     }
