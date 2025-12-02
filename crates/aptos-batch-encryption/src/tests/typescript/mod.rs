0fd291d2812e531f242b86b87fc84961d5780365
Rex Fernando <rex1fernando@gmail.com>
2025-12-02 09:03:13 +0000

    Disable typescript unit tests for now

--- crates/aptos-batch-encryption/src/tests/typescript/mod.rs
+++ crates/aptos-batch-encryption/src/tests/typescript/mod.rs
@@ -1,6 +1,6 @@
 // Copyright (c) Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
-pub mod ciphertext;
-pub mod curve_serialization;
-pub mod runner;
-pub mod symmetric;
+//pub mod ciphertext;
+//pub mod curve_serialization;
+//pub mod runner;
+//pub mod symmetric;
