7c81317672ace4263a4dac1713adfc2796091e0e
Josh Lind <josh.lind@hotmail.com>
2025-12-03 16:18:17 -0500

    [License] Update license header in mempool

--- execution/executor-service/src/remote_executor_client.rs
+++ execution/executor-service/src/remote_executor_client.rs
@@ -1,4 +1,3 @@
-// Copyright © Aptos Foundation
 // Copyright (c) Aptos Foundation
 // Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 use crate::{
