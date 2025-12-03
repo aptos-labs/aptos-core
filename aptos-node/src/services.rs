ef059af0d856d8b007d06e51a7d60cc696181e7e
Josh Lind <josh.lind@hotmail.com>
2025-12-03 15:20:02 -0500

    [License] Update license header in aptos-node

--- aptos-node/src/services.rs
+++ aptos-node/src/services.rs
@@ -1,5 +1,5 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) Aptos Foundation
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::{bootstrap_api, indexer, mpsc::Receiver, network::ApplicationNetworkInterfaces};
 use aptos_admin_service::AdminService;
