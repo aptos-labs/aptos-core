43087dbb28bded4886d79ed7768a190f763137d5
Josh Lind <josh.lind@hotmail.com>
2025-12-03 16:05:16 -0500

    [License] Update license header in ecosystem

--- ecosystem/indexer-grpc/indexer-grpc-utils/src/compression_util.rs
+++ ecosystem/indexer-grpc/indexer-grpc-utils/src/compression_util.rs
@@ -1,5 +1,5 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) Aptos Foundation
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::default_file_storage_format;
 use aptos_protos::{indexer::v1::TransactionsInStorage, transaction::v1::Transaction};
