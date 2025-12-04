4eb47e051736912d615b90155cf7540e11866842
Josh Lind <josh.lind@hotmail.com>
2025-12-04 14:26:56 -0500

    [Pre-Commit] Remove shared UTF8 license header.

--- third_party/move/move-model/bytecode/ast-generator-tests/tests/testsuite.rs
+++ third_party/move/move-model/bytecode/ast-generator-tests/tests/testsuite.rs
@@ -1,5 +1,6 @@
-// Copyright (c) Aptos Foundation
-// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
+// Copyright © Aptos Foundation
+// Parts of the project are originally copyright © Meta Platforms, Inc.
+// SPDX-License-Identifier: Apache-2.0
 
 use codespan_reporting::term::termcolor::Buffer;
 use move_compiler_v2::{logging, run_move_compiler_for_analysis, Options};
