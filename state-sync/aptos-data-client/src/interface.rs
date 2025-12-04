8963fd1e1fd36166c814f7e0a3dec35d81a4ac81
Josh Lind <josh.lind@hotmail.com>
2025-12-04 13:55:59 -0500

    [License] Update license header in state-sync

--- state-sync/aptos-data-client/src/interface.rs
+++ state-sync/aptos-data-client/src/interface.rs
@@ -1,5 +1,5 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) Aptos Foundation
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::{error, error::Error, global_summary::GlobalDataSummary};
 use aptos_storage_service_types::{responses::TransactionOrOutputListWithProofV2, Epoch};
