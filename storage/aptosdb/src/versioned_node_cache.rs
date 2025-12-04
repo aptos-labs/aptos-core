139021515c0977ccb6df936b1066680cbc9d76cd
Josh Lind <josh.lind@hotmail.com>
2025-12-04 13:59:59 -0500

    [License] Update license header in storage

--- storage/aptosdb/src/versioned_node_cache.rs
+++ storage/aptosdb/src/versioned_node_cache.rs
@@ -1,5 +1,5 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) Aptos Foundation
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::{lru_node_cache::LruNodeCache, metrics::OTHER_TIMERS_SECONDS, state_merkle_db::Node};
 use aptos_experimental_runtimes::thread_manager::THREAD_MANAGER;
