9131ffe6355861396146546ca9124986a9755d85
George Mitenkov <georgemitenk0v@gmail.com>
2025-12-05 15:46:50 +0000

    [vm] Small fixes to interpreter caches (#18233)
    Ensure ability info is part of function type, fix feature gating for frame cache for native dynamic dispatch.
    
    Co-authored-by: Calin Tataru <calintat@gmail.com>

--- third_party/move/move-vm/runtime/src/config.rs
+++ third_party/move/move-vm/runtime/src/config.rs
@@ -51,6 +51,8 @@ pub struct VMConfig {
     pub enable_layout_caches: bool,
     pub propagate_dependency_limit_error: bool,
     pub enable_framework_for_option: bool,
+    /// Same as enable_function_caches, but gates missed gating for native dynamic dispatch.
+    pub enable_function_caches_for_native_dynamic_dispatch: bool,
 }
 
 impl Default for VMConfig {
@@ -78,6 +80,7 @@ impl Default for VMConfig {
             enable_layout_caches: true,
             propagate_dependency_limit_error: true,
             enable_framework_for_option: true,
+            enable_function_caches_for_native_dynamic_dispatch: true,
         }
     }
 }
