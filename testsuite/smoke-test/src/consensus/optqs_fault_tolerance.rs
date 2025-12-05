d703654623c75af9adc142b2d6c7c19a1e3702f6
Josh Lind <josh.lind@hotmail.com>
2025-12-04 20:35:38 -0500

    [License] Update original license header in testsuite

--- testsuite/smoke-test/src/consensus/optqs_fault_tolerance.rs
+++ testsuite/smoke-test/src/consensus/optqs_fault_tolerance.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::{
     consensus::helpers::generate_traffic_and_assert_committed, smoke_test_environment::SwarmBuilder,
