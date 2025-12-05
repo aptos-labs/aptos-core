124db55b0c7a8996de4325b4c7a65e6743133607
Josh Lind <josh.lind@hotmail.com>
2025-12-04 20:35:09 -0500

    [License] Update original license header in storage

--- storage/indexer/src/event_v2_translator.rs
+++ storage/indexer/src/event_v2_translator.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use aptos_db_indexer_schemas::schema::event_sequence_number::EventSequenceNumberSchema;
 use aptos_schemadb::DB;
--- storage/storage-interface/src/state_store/state_update_refs.rs
+++ storage/storage-interface/src/state_store/state_update_refs.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::{metrics::TIMER, state_store::versioned_state_value::StateUpdateRef};
 use aptos_logger::{sample, sample::SampleRate, warn};
