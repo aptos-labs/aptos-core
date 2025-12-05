2649b6fede35a70c5c9f1bd20988e4c60458477d
Josh Lind <josh.lind@hotmail.com>
2025-12-04 20:35:38 -0500

    [License] Update original license header in testsuite

--- testsuite/fuzzer/fuzz/fuzz_targets/use_case_aware_shuffler.rs
+++ testsuite/fuzzer/fuzz/fuzz_targets/use_case_aware_shuffler.rs
@@ -1,6 +1,6 @@
 #![no_main]
 // Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use aptos_consensus::transaction_shuffler_fuzzing::use_case_aware::{Config, UseCaseAwareShuffler};
 use aptos_types::transaction::use_case::{UseCaseAwareTransaction, UseCaseKey};
