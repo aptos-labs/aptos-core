99a942b0f68a030bb281ab66eab54a7f053c3d41
Josh Lind <josh.lind@hotmail.com>
2025-12-04 20:30:16 -0500

    [License] Update original license header in ecosystem

--- ecosystem/indexer-grpc/indexer-grpc-data-service-v2/src/live_data_service/data_manager.rs
+++ ecosystem/indexer-grpc/indexer-grpc-data-service-v2/src/live_data_service/data_manager.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::metrics::{
     CACHE_END_VERSION, CACHE_SIZE_BYTES, CACHE_SIZE_LIMIT_BYTES, CACHE_START_VERSION, COUNTER,
