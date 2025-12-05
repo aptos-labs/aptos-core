19f3179a35ed4e8d7a5ed6e9dacbe74b7cf4c91b
Josh Lind <josh.lind@hotmail.com>
2025-12-04 20:30:47 -0500

    [License] Update original license header in execution

--- execution/executor-benchmark/src/native/mod.rs
+++ execution/executor-benchmark/src/native/mod.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 pub mod aptos_vm_uncoordinated;
 pub mod native_config;
