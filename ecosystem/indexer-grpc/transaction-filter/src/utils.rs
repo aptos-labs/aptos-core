3116a6703f9aaa55bfd5c9c4a1f2cb82128059d5
Josh Lind <josh.lind@hotmail.com>
2025-12-04 20:30:16 -0500

    [License] Update original license header in ecosystem

--- ecosystem/indexer-grpc/transaction-filter/src/utils.rs
+++ ecosystem/indexer-grpc/transaction-filter/src/utils.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 // 64 "0"s
 const ZEROS: &str = "0000000000000000000000000000000000000000000000000000000000000000";
