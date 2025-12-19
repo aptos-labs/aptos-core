8e80f4fb72e43133b2735e2ed43799f4125f9c13
Justin Chang <justin@aptoslabs.com>
2025-12-19 14:48:53 -0800

    revert

--- ecosystem/indexer-grpc/indexer-grpc-fullnode/src/fullnode_data_service.rs
+++ ecosystem/indexer-grpc/indexer-grpc-fullnode/src/fullnode_data_service.rs
@@ -72,11 +72,7 @@ impl FullnodeData for FullnodeDataService {
         let r = req.into_inner();
         let starting_version = match r.starting_version {
             Some(version) => version,
-            None => match self.service_context.context.db.get_synced_version() {
-                Ok(Some(version)) => version,
-                Ok(None) => return Err(Status::unavailable("No synced version available yet")),
-                Err(e) => return Err(Status::internal(format!("{e}"))),
-            },
+            None => return Err(Status::invalid_argument("Starting version must be set")),
         };
         let processor_task_count = self.service_context.processor_task_count;
         let processor_batch_size = self.service_context.processor_batch_size;
