6c5507d13bd87e84d9d5391e485bf3792d2059c0
George Mitenkov <georgemitenk0v@gmail.com>
2025-12-20 11:23:02 +0000

    Change license comment to SPDX format
    Updated license comment to SPDX format.

--- aptos-move/e2e-move-tests/src/tests/function_caches.rs
+++ aptos-move/e2e-move-tests/src/tests/function_caches.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Aptos Foundation
-// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
+// SPDX-License-Identifier: Apache-2.0
 
 //! Tests that interpreter caches frees all allocated data structures for recursively called
 //! functions.
