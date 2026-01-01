995e216171d6c26ca79397e863c1b44a9bbbb43f
wqfish <1171005+wqfish@users.noreply.github.com>
2026-01-01 11:27:07 -0800

    [Hot State] Use config to replace hard-coded parameters

--- storage/aptosdb/src/state_store/persisted_state.rs
+++ storage/aptosdb/src/state_store/persisted_state.rs
@@ -2,6 +2,7 @@
 // Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use crate::{metrics::OTHER_TIMERS_SECONDS, state_store::hot_state::HotState};
+use aptos_config::config::HotStateConfig;
 use aptos_infallible::Mutex;
 use aptos_metrics_core::TimerHelper;
 use aptos_scratchpad::SUBTREE_DROPPER;
@@ -9,7 +10,6 @@ use aptos_storage_interface::state_store::{
     state::State, state_summary::StateSummary, state_view::hot_state_view::HotStateView,
     state_with_summary::StateWithSummary,
 };
-use aptos_types::state_store::hot_state::HotStateConfig;
 use std::sync::Arc;
 
 #[derive(Clone)]
