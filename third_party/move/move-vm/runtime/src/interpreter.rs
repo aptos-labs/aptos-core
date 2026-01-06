830d2dfd6f12f23ab54dd4c98c1ebda82567daf5
Calin Tataru <calintat@gmail.com>
2026-01-06 11:30:55 +0000

    Cap length of abort message

--- third_party/move/move-vm/runtime/src/interpreter.rs
+++ third_party/move/move-vm/runtime/src/interpreter.rs
@@ -1767,6 +1767,8 @@ const OPERAND_STACK_SIZE_LIMIT: usize = 1024;
 const CALL_STACK_SIZE_LIMIT: usize = 1024;
 pub(crate) const ACCESS_STACK_SIZE_LIMIT: usize = 256;
 
+const ABORT_MESSAGE_SIZE_LIMIT: usize = 4096;
+
 /// The operand and runtime-type stacks.
 pub(crate) struct Stack {
     pub(crate) value: Vec<Value>,
@@ -2760,6 +2762,17 @@ impl Frame {
 
                         // Gas is charged per byte to account for the cost of UTF-8 validation.
                         gas_meter.charge_abort_message(&bytes)?;
+
+                        if bytes.len() > ABORT_MESSAGE_SIZE_LIMIT {
+                            return Err(PartialVMError::new(
+                                StatusCode::ABORT_MESSAGE_LIMIT_EXCEEDED,
+                            )
+                            .with_message(format!(
+                                "Expected at most {} bytes, got {} bytes",
+                                ABORT_MESSAGE_SIZE_LIMIT,
+                                bytes.len()
+                            )));
+                        }
 
                         let error_message = String::from_utf8(bytes).map_err(|err| {
                             PartialVMError::new(StatusCode::INVALID_ABORT_MESSAGE)
