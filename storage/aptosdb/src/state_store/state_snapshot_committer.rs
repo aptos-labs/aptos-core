7b8a45e3c4d04e57796cb67e4e1b9019d07fcf2c
wqfish <1171005+wqfish@users.noreply.github.com>
2026-01-10 23:12:38 +0000

    [Storage] Move log out of loop in StateSnapshotCommitter
    It should only be logged once instead of every time a message is received.

--- storage/aptosdb/src/state_store/state_snapshot_committer.rs
+++ storage/aptosdb/src/state_store/state_snapshot_committer.rs
@@ -14,7 +14,7 @@ use crate::{
     versioned_node_cache::VersionedNodeCache,
 };
 use aptos_experimental_runtimes::thread_manager::THREAD_MANAGER;
-use aptos_logger::trace;
+use aptos_logger::info;
 use aptos_metrics_core::TimerHelper;
 use aptos_storage_interface::{
     jmt_update_refs, state_store::state_with_summary::StateWithSummary, Result,
@@ -201,8 +201,8 @@ impl StateSnapshotCommitter {
                     break;
                 },
             }
-            trace!("State snapshot committing thread exit.")
         }
+        info!("State snapshot committing thread exit.");
     }
 }
 
