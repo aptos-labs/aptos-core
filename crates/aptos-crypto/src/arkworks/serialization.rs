52740cf1b4fd10aa9d7072de32fd78cee48a44e7
Wicher Malten <w.malten@gmail.com>
2026-01-17 12:21:27 +0100

    Edit to deserialization

--- crates/aptos-crypto/src/arkworks/serialization.rs
+++ crates/aptos-crypto/src/arkworks/serialization.rs
@@ -1,12 +1,13 @@
 // Copyright (c) Aptos Foundation
 // Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
-//! Copied from https://github.com/arkworks-rs/algebra/issues/178#issuecomment-1413219278
+//! Adapted from https://github.com/arkworks-rs/algebra/issues/178#issuecomment-1413219278
 
 use ark_ec::pairing::Pairing;
 use ark_serialize::{
     CanonicalDeserialize, CanonicalSerialize, Compress, SerializationError, Validate, Write,
 };
+use bytes::{Buf, Bytes};
 
 /// Serializes a type implementing `CanonicalSerialize` into bytes (with compression) using the
 /// [`ark_serialize`](https://docs.rs/ark-serialize) format and writes it to a Serde serializer.
@@ -31,8 +32,8 @@ pub fn ark_de<'de, D, A: CanonicalDeserialize>(data: D) -> Result<A, D::Error>
 where
     D: serde::de::Deserializer<'de>,
 {
-    let s: Vec<u8> = serde::de::Deserialize::deserialize(data)?;
-    let a = A::deserialize_with_mode(s.as_slice(), Compress::Yes, Validate::Yes);
+    let s: Bytes = serde::de::Deserialize::deserialize(data)?;
+    let a = A::deserialize_with_mode(s.reader(), Compress::Yes, Validate::Yes);
     a.map_err(serde::de::Error::custom)
 }
 
