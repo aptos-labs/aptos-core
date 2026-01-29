90382e5da59fa3d9af807e536a182caccb8ff77b
Balaji Arun <balaji@aptoslabs.com>
2026-01-29 09:19:45 -0800

    [dkg] Add chunky DKG types and module structure
    - Add chunky module with types, placeholder submodules
    - Add ChunkyDKG message types to DKGMessage enum
    - Add chunky DKG metrics and observability functions
    - Add required dependencies to Cargo.toml

---
+++ dkg/src/chunky/agg_subtrx_producer.rs
@@ -0,0 +1,5 @@
+// Copyright (c) Aptos Foundation
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
+
+//! Aggregated subtranscript producer for Chunky DKG.
+//! Implementation will be added in a subsequent PR.
--- dkg/src/types.rs
+++ dkg/src/types.rs
@@ -1,10 +1,15 @@
 // Copyright (c) Aptos Foundation
 // Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
+pub use crate::chunky::types::{
+    ChunkyDKGSubtranscriptSignatureRequest, ChunkyDKGSubtranscriptSignatureResponse,
+    ChunkyDKGTranscriptRequest, ChunkyDKGTranscriptResponse, MissingTranscriptRequest,
+    MissingTranscriptResponse, ValidatedAggregatedTranscript,
+};
 use aptos_crypto_derive::CryptoHasher;
 use aptos_enum_conversion_derive::EnumConversion;
 use aptos_reliable_broadcast::RBMessage;
-pub use aptos_types::dkg::DKGTranscript;
+pub use aptos_types::dkg::{DKGTranscript, DKGTranscriptMetadata};
 use serde::{Deserialize, Serialize};
 
 /// Once DKG starts, a validator should send this message to peers in order to collect DKG transcripts from peers.
@@ -26,6 +31,12 @@ impl DKGTranscriptRequest {
 pub enum DKGMessage {
     TranscriptRequest(DKGTranscriptRequest),
     TranscriptResponse(DKGTranscript),
+    ChunkyTranscriptRequest(ChunkyDKGTranscriptRequest),
+    ChunkyTranscriptResponse(ChunkyDKGTranscriptResponse),
+    SubtranscriptSignatureRequest(ChunkyDKGSubtranscriptSignatureRequest),
+    SubtranscriptSignatureResponse(ChunkyDKGSubtranscriptSignatureResponse),
+    MissingTranscriptRequest(MissingTranscriptRequest),
+    MissingTranscriptResponse(MissingTranscriptResponse),
 }
 
 impl DKGMessage {
@@ -33,6 +44,12 @@ impl DKGMessage {
         match self {
             DKGMessage::TranscriptRequest(request) => request.dealer_epoch,
             DKGMessage::TranscriptResponse(response) => response.metadata.epoch,
+            DKGMessage::ChunkyTranscriptRequest(request) => request.dealer_epoch,
+            DKGMessage::ChunkyTranscriptResponse(response) => response.metadata.epoch,
+            DKGMessage::SubtranscriptSignatureRequest(request) => request.dealer_epoch,
+            DKGMessage::SubtranscriptSignatureResponse(response) => response.dealer_epoch,
+            DKGMessage::MissingTranscriptRequest(request) => request.dealer_epoch,
+            DKGMessage::MissingTranscriptResponse(response) => response.metadata.epoch,
         }
     }
 
@@ -40,6 +57,12 @@ impl DKGMessage {
         match self {
             DKGMessage::TranscriptRequest(_) => "DKGTranscriptRequest",
             DKGMessage::TranscriptResponse(_) => "DKGTranscriptResponse",
+            DKGMessage::ChunkyTranscriptRequest(_) => "ChunkyDKGTranscriptRequest",
+            DKGMessage::ChunkyTranscriptResponse(_) => "ChunkyDKGTranscriptResponse",
+            DKGMessage::SubtranscriptSignatureRequest(_) => "DKGSubtranscriptSignatureRequest",
+            DKGMessage::SubtranscriptSignatureResponse(_) => "DKGSubtranscriptSignatureResponse",
+            DKGMessage::MissingTranscriptRequest(_) => "MissingTranscriptRequest",
+            DKGMessage::MissingTranscriptResponse(_) => "MissingTranscriptResponse",
         }
     }
 }
