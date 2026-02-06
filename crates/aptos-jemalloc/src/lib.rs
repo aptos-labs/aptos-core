5bdd3dc344a8020f40932cf27ddab9e8261a72f0
wqfish <1171005+wqfish@users.noreply.github.com>
2026-02-06 15:15:15 +0000

    thp

--- crates/aptos-jemalloc/src/lib.rs
+++ crates/aptos-jemalloc/src/lib.rs
@@ -17,6 +17,7 @@ macro_rules! setup_jemalloc {
         #[used]
         #[unsafe(no_mangle)]
         pub static mut malloc_conf: *const ::std::ffi::c_char = c"abort_conf:true,\
+              thp:always,metadata_thp:always,\
               lg_tcache_max:16,tcache_nslots_large:32,\
               percpu_arena:percpu,\
               prof:true,\
