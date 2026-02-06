b54934d891745b684000733e5525adfb2cc26c21
wqfish <1171005+wqfish@users.noreply.github.com>
2026-02-06 15:06:41 +0000

    dummy

---
+++ crates/aptos-jemalloc/src/lib.rs
@@ -0,0 +1,25 @@
+// Copyright (c) Aptos Foundation
+// Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
+
+/// Sets up jemalloc as the global allocator and configures `malloc_conf`.
+///
+/// Must be invoked at the top level of a binary crate (`main.rs`).
+/// The `malloc_conf` value can be overridden at runtime via the `MALLOC_CONF` env var.
+#[macro_export]
+macro_rules! setup_jemalloc {
+    () => {
+        #[cfg(unix)]
+        #[global_allocator]
+        static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+        #[allow(unsafe_code, non_upper_case_globals)]
+        #[cfg(unix)]
+        #[used]
+        #[unsafe(no_mangle)]
+        pub static mut malloc_conf: *const ::std::ffi::c_char = c"abort_conf:true,\
+              prof:true,\
+              lg_prof_sample:23"
+            .as_ptr()
+            .cast();
+    };
+}
