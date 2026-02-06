d334aaed831dd059b6657cd0b0ac78e693eaa8fd
wqfish <1171005+wqfish@users.noreply.github.com>
2026-02-06 17:47:20 +0000

    phycpu

--- crates/aptos-jemalloc/src/lib.rs
+++ crates/aptos-jemalloc/src/lib.rs
@@ -17,6 +17,7 @@ macro_rules! setup_jemalloc {
         #[used]
         #[unsafe(no_mangle)]
         pub static mut malloc_conf: *const ::std::ffi::c_char = c"abort_conf:true,\
+              percpu_arena:phycpu,\
               prof:true,\
               lg_prof_sample:23"
             .as_ptr()
