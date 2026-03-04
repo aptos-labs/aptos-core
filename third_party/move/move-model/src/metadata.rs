7b347111d9788ab952dea23cafea15275e01ae81
Vineeth Kashyap <vineeth.rv@gmail.com>
2026-03-04 16:31:17 -0500

    address more comments

--- third_party/move/move-model/src/metadata.rs
+++ third_party/move/move-model/src/metadata.rs
@@ -37,6 +37,8 @@ pub mod lang_feature_versions {
     pub const LANGUAGE_VERSION_FOR_PUBLIC_STRUCT: LanguageVersion = LanguageVersion::V2_4;
     /// This version guards checks for unused private functions, private structs, and constants.
     pub const LANGUAGE_VERSION_FOR_UNUSED_CHECK: LanguageVersion = LanguageVersion::V2_4;
+    /// This version guards match support for primitive types.
+    pub const LANGUAGE_VERSION_FOR_PRIMITIVE_MATCH: LanguageVersion = LanguageVersion::V2_4;
     pub const LANGUAGE_VERSION_FOR_RAC: LanguageVersion =
         crate::metadata::LATEST_LANGUAGE_VERSION_VALUE;
 }
