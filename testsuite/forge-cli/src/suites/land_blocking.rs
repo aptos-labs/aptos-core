aaae4eb5798d6cb55f72ec8e65c5b7f77a0c7b8f
Josh Lind <josh.lind@hotmail.com>
2026-04-16 08:51:05 -0400

    [Forge] Bump num validators/VFNs in land-blocking.

--- testsuite/forge-cli/src/suites/land_blocking.rs
+++ testsuite/forge-cli/src/suites/land_blocking.rs
@@ -2,7 +2,7 @@
 // Licensed pursuant to the Innovation-Enabling Source Code License, available at https://github.com/aptos-labs/aptos-core/blob/main/LICENSE
 
 use super::ungrouped::mixed_compatible_emit_job;
-use crate::{suites::realistic_environment::realistic_env_proposer_only_pfn_test, TestCommand};
+use crate::{suites::realistic_environment::realistic_env_no_proposer_pfn_test, TestCommand};
 use aptos_forge::{
     prometheus_metrics::LatencyBreakdownSlice,
     success_criteria::{
@@ -28,7 +28,7 @@ pub(crate) fn get_land_blocking_test(
 ) -> Option<ForgeConfig> {
     let test = match test_name {
         "land_blocking" | "realistic_env_max_load" => {
-            realistic_env_proposer_only_pfn_test(duration, test_cmd, 7, 0, 3, 3)
+            realistic_env_no_proposer_pfn_test(duration, test_cmd, 8, 0, 4, 4)
         },
         "compat" => compat(),
         "framework_upgrade" => framework_upgrade(),
