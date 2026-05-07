82ddffcc10ea21ada3681b3c31b266ef63dc972e
Wolfgang Grieskamp <wg@aptoslabs.com>
2026-05-07 16:10:09 +0000

    deploy move book from e75a07442b

--- move-book/searcher-c2a407aa.js
+++ move-book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-542db50c.js',
+                path_to_root + 'searchindex-ec2ebe67.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
