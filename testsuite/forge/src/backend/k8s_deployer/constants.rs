4048fea84c9ed77a130a90d9e35e391a0c53f321
Stelian Ionescu <stelian@aptoslabs.com>
2026-05-15 18:03:47 -0600

    Backport latest fixes to Forge deployer

--- testsuite/forge/src/backend/k8s_deployer/constants.rs
+++ testsuite/forge/src/backend/k8s_deployer/constants.rs
@@ -13,7 +13,7 @@ pub const INDEXER_GRPC_DOCKER_IMAGE_REPO: &str =
 
 /// The version of the forge deployer image to use.
 pub const DEFAULT_FORGE_DEPLOYER_IMAGE_TAG: &str =
-    "release_a5ad1dc3548e99def34645a3845168f2fa3fa436"; // latest stable build (2026-04-27)
+    "release_5af9feed8f04b4caa5edaeece71998ade1865d58"; // latest stable build (2026-05-15)
 
 /// This is the service account name that the deployer will use to deploy the forge components. It may require extra permissions and additonal setup
 pub const FORGE_DEPLOYER_SERVICE_ACCOUNT_NAME: &str = "forge";
