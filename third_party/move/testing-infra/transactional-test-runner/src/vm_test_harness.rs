0f4185afadf513288bb555fab73f10b706e63c43
Vineeth Kashyap <vineeth.rv@gmail.com>
2026-09-09 12:03:40 -0400

    Add MonoVM transactional test harness with publishing support

--- third_party/move/testing-infra/transactional-test-runner/src/vm_test_harness.rs
+++ third_party/move/testing-infra/transactional-test-runner/src/vm_test_harness.rs
@@ -11,22 +11,24 @@ use crate::{
     tasks::{EmptyCommand, InitCommand, SyntaxChoice, TaskInput},
 };
 use anyhow::{anyhow, bail, Result};
+use bytes::Bytes;
 use clap::Parser;
 use legacy_move_compiler::{
     compiled_unit::{AnnotatedCompiledModule, AnnotatedCompiledUnit},
     shared::known_attributes::KnownAttribute,
 };
 use move_binary_format::{
-    access::ModuleAccess,
     compatibility::Compatibility,
     errors,
-    errors::{PartialVMResult, VMResult},
+    errors::{PartialVMResult, VMError, VMResult},
     file_format::CompiledScript,
     CompiledModule,
 };
 use move_bytecode_verifier::VerifierConfig;
 use move_command_line_common::{
-    address::ParsedAddress, env::read_bool_env_var, files::verify_and_create_named_address_mapping,
+    address::{NumericalAddress, ParsedAddress},
+    env::read_bool_env_var,
+    files::verify_and_create_named_address_mapping,
     testing::EXP_EXT,
 };
 use move_core_types::{
@@ -39,7 +41,6 @@ use move_core_types::{
 use move_model::metadata::LanguageVersion;
 use move_resource_viewer::MoveValueAnnotator;
 use move_stdlib::move_stdlib_named_addresses;
-use move_symbol_pool::Symbol;
 use move_vm_runtime::{
     config::VMConfig,
     data_cache::{MoveVmDataCacheAdapter, TransactionDataCache},
@@ -51,7 +52,7 @@ use move_vm_runtime::{
     native_functions::{make_table_from_iter, NativeContext, NativeFunction},
     AsFunctionValueExtension, AsUnsyncCodeStorage, AsUnsyncModuleStorage, CodeStorage,
     InstantiatedFunctionLoader, LegacyLoaderConfig, RuntimeEnvironment, ScriptLoader,
-    StagingModuleStorage, TypeChecker,
+    StagingModuleStorage, TypeChecker, VerifiedModuleBundle,
 };
 use move_vm_test_utils::{
     gas_schedule::{CostTable, Gas, GasStatus},
@@ -159,85 +160,21 @@ impl<'a> MoveTestAdapter<'a> for SimpleVMTestAdapter<'a> {
         // Set stable test display of VM Errors so we can use the --verbose flag in baseline tests
         errors::set_stable_test_display();
 
-        let additional_mapping = match task_opt.map(|t| t.command) {
-            Some((InitCommand { named_addresses }, _)) => {
-                verify_and_create_named_address_mapping(named_addresses).unwrap()
-            },
-            None => BTreeMap::new(),
-        };
-
-        let mut named_address_mapping = move_stdlib_named_addresses();
-        for (name, addr) in additional_mapping {
-            if named_address_mapping.contains_key(&name) {
-                panic!(
-                    "Invalid init. The named address '{}' is reserved by the move-stdlib",
-                    name
-                )
-            }
-            named_address_mapping.insert(name, addr);
+        let runtime_environment = create_runtime_environment(run_config.vm_config.clone());
+        let mut storage = InMemoryStorage::new_with_runtime_environment(runtime_environment);
+        for (module_id, bytes) in stage_precompiled_stdlib(&storage, pre_compiled_deps_v2) {
+            storage.add_module_bytes(module_id.address(), module_id.name(), bytes);
         }
 
-        let vm_config = &run_config.vm_config;
-        let runtime_environment = create_runtime_environment(vm_config.clone());
-        let storage = InMemoryStorage::new_with_runtime_environment(runtime_environment);
-        let max_binary_format_version = storage.max_binary_format_version();
-
-        let mut adapter = Self {
-            compiled_state: CompiledState::new(named_address_mapping, pre_compiled_deps_v2, None),
+        let adapter = Self {
+            compiled_state: compiled_state_with_stdlib(
+                pre_compiled_deps_v2,
+                task_opt.map(|task| task.command.0),
+            ),
             default_syntax,
             run_config,
             storage,
         };
-
-        let module_storage = adapter.storage.clone().into_unsync_module_storage();
-
-        let addresses = pre_compiled_deps_v2
-            .get_pre_compiled_modules()
-            .iter()
-            .map(|tmod| *tmod.named_module.module.self_addr())
-            .collect::<BTreeSet<_>>();
-        assert_eq!(addresses.len(), 1);
-
-        let sender = *addresses.first().unwrap();
-        let module_bundle = pre_compiled_deps_v2
-            .get_pre_compiled_modules()
-            .into_iter()
-            .map(|tmod| {
-                let mut module_bytes = vec![];
-                tmod.named_module
-                    .module
-                    .serialize_for_version(Some(max_binary_format_version), &mut module_bytes)
-                    .unwrap();
-                module_bytes.into()
-            })
-            .collect();
-
-        StagingModuleStorage::create(&sender, &module_storage, module_bundle)
-            .expect("All modules should publish")
-            .release_verified_module_bundle()
-            .into_iter()
-            .for_each(|(module_id, bytes)| {
-                adapter
-                    .storage
-                    .add_module_bytes(module_id.address(), module_id.name(), bytes);
-            });
-
-        let mut addr_to_name_mapping = BTreeMap::new();
-        for (name, addr) in move_stdlib_named_addresses() {
-            let prev = addr_to_name_mapping.insert(addr, Symbol::from(name));
-            assert!(prev.is_none());
-        }
-        let missing_modules: Vec<_> = pre_compiled_deps_v2
-            .get_pre_compiled_modules()
-            .into_iter()
-            .map(|tmod| &tmod.named_module.module)
-            .filter(|module| !adapter.compiled_state.is_precompiled_dep(&module.self_id()))
-            .collect();
-        for module in missing_modules {
-            adapter
-                .compiled_state
-                .add_and_generate_interface_file(module.clone())
-        }
         (adapter, None)
     }
 
@@ -249,49 +186,23 @@ impl<'a> MoveTestAdapter<'a> for SimpleVMTestAdapter<'a> {
         extra_args: Self::ExtraPublishArgs,
     ) -> Result<(Option<String>, CompiledModule)> {
         let module_storage = self.storage.clone().into_unsync_module_storage();
-
-        let mut module_bytes = vec![];
-        module.serialize_for_version(
-            Some(self.storage.max_binary_format_version()),
-            &mut module_bytes,
-        )?;
-
+        let module_bytes = serialize_module(&self.storage, &module)?;
         let id = module.self_id();
         let sender = *id.address();
-        let verbose = extra_args.verbose;
 
-        let compat = if extra_args.skip_check_struct_and_pub_function_linking
-            || self.run_config.verifier_disabled()
-        {
-            Compatibility::no_check()
-        } else {
-            Compatibility::new(
-                !extra_args.skip_check_struct_layout,
-                !extra_args.skip_check_friend_linking,
-                true,
-                false,
-                true,
-            )
-        };
         let staging_module_storage = StagingModuleStorage::create_with_compat_config(
             &sender,
-            compat,
+            self.run_config.publish_compatibility(&extra_args),
             &module_storage,
-            vec![module_bytes.into()],
+            vec![module_bytes],
         )
-        .map_err(|err| {
-            anyhow!(
-                "Unable to publish module '{}'. Got VMError: {}",
-                module.self_id(),
-                err.format_test_output(move_test_debug() || verbose)
-            )
-        })?;
+        .map_err(|err| publish_error(&id, &err, extra_args.verbose))?;
         for (module_id, bytes) in staging_module_storage
             .release_verified_module_bundle()
             .into_iter()
         {
             self.storage
-                .add_module_bytes(module_id.address(), module.name(), bytes);
+                .add_module_bytes(module_id.address(), module_id.name(), bytes);
         }
         Ok((None, module))
     }
@@ -445,13 +356,38 @@ impl<'a> MoveTestAdapter<'a> for SimpleVMTestAdapter<'a> {
     }
 
     fn deserialize(&self, bytes: &[u8], layout: &MoveTypeLayout) -> Option<Value> {
-        let module_storage = self.storage.as_unsync_module_storage();
-        let function_extension = module_storage.as_function_value_extension();
-        let max_value_nest_depth = function_extension.max_value_nest_depth();
-        ValueSerDeContext::new(max_value_nest_depth)
-            .with_func_args_deserialization(&function_extension)
-            .deserialize(bytes, layout)
+        deserialize_value(&self.storage, bytes, layout)
     }
+}
+
+/// Formats a publishing failure for transactional test baselines.
+pub fn publish_error(module_id: &ModuleId, err: &VMError, verbose: bool) -> anyhow::Error {
+    anyhow!(
+        "Unable to publish module '{}'. Got VMError: {}",
+        module_id,
+        err.format_test_output(move_test_debug() || verbose)
+    )
+}
+
+/// Deserializes a value using `storage` to resolve function argument types
+/// for any function values it contains.
+pub fn deserialize_value(
+    storage: &InMemoryStorage,
+    bytes: &[u8],
+    layout: &MoveTypeLayout,
+) -> Option<Value> {
+    let module_storage = storage.as_unsync_module_storage();
+    let function_extension = module_storage.as_function_value_extension();
+    ValueSerDeContext::new(function_extension.max_value_nest_depth())
+        .with_func_args_deserialization(&function_extension)
+        .deserialize(bytes, layout)
+}
+
+/// Serializes `module` at the maximum binary format version supported by `storage`.
+pub fn serialize_module(storage: &InMemoryStorage, module: &CompiledModule) -> Result<Bytes> {
+    let mut module_bytes = vec![];
+    module.serialize_for_version(Some(storage.max_binary_format_version()), &mut module_bytes)?;
+    Ok(module_bytes.into())
 }
 
 impl SimpleVMTestAdapter<'_> {
@@ -572,7 +508,8 @@ fn native_compare(
     Ok(NativeResult::ok(0.into(), smallvec![result]))
 }
 
-fn create_runtime_environment(vm_config: VMConfig) -> RuntimeEnvironment {
+/// Creates the test runtime environment with stdlib natives and `cmp::compare`.
+pub fn create_runtime_environment(vm_config: VMConfig) -> RuntimeEnvironment {
     let mut natives = move_stdlib::natives::all_natives(
         STD_ADDR,
         // TODO: come up with a suitable gas schedule
@@ -584,6 +521,65 @@ fn create_runtime_environment(vm_config: VMConfig) -> RuntimeEnvironment {
         Arc::new(native_compare) as NativeFunction,
     )]));
     RuntimeEnvironment::new_with_config(natives, vm_config)
+}
+
+/// Combines stdlib named addresses with those declared by `//# init`.
+/// Panics if the declarations are invalid or reuse a stdlib name.
+fn test_named_address_mapping(init: Option<InitCommand>) -> BTreeMap<String, NumericalAddress> {
+    let additional_mapping = match init {
+        Some(InitCommand { named_addresses }) => {
+            verify_and_create_named_address_mapping(named_addresses)
+                .expect("the `//# init` task declares well-formed named addresses")
+        },
+        None => BTreeMap::new(),
+    };
+    let mut named_address_mapping = move_stdlib_named_addresses();
+    for (name, addr) in additional_mapping {
+        if named_address_mapping.contains_key(&name) {
+            panic!(
+                "Invalid init. The named address '{}' is reserved by the move-stdlib",
+                name
+            )
+        }
+        named_address_mapping.insert(name, addr);
+    }
+    named_address_mapping
+}
+
+/// Initializes compiled state with the precompiled stdlib and the test's named addresses.
+pub fn compiled_state_with_stdlib<'a>(
+    pre_compiled_deps_v2: &'a PrecompiledFilesModules,
+    init: Option<InitCommand>,
+) -> CompiledState<'a> {
+    CompiledState::new(test_named_address_mapping(init), pre_compiled_deps_v2, None)
+}
+
+/// Stages the precompiled stdlib with V1's publishing checks against empty
+/// module storage. Returns the verified bundle for the caller to store.
+pub fn stage_precompiled_stdlib(
+    storage: &InMemoryStorage,
+    pre_compiled_deps_v2: &PrecompiledFilesModules,
+) -> VerifiedModuleBundle<ModuleId, Bytes> {
+    let modules = pre_compiled_deps_v2.get_pre_compiled_modules();
+    let addresses = modules
+        .iter()
+        .map(|tmod| *tmod.named_module.module.self_addr())
+        .collect::<BTreeSet<_>>();
+    assert_eq!(addresses.len(), 1);
+    let sender = *addresses
+        .first()
+        .expect("the precompiled stdlib lives at one address");
+
+    let module_bundle = modules
+        .into_iter()
+        .map(|tmod| {
+            serialize_module(storage, &tmod.named_module.module)
+                .expect("the precompiled stdlib serializes")
+        })
+        .collect();
+    StagingModuleStorage::create(&sender, &storage.as_unsync_module_storage(), module_bundle)
+        .expect("All modules should publish")
+        .release_verified_module_bundle()
 }
 
 fn get_gas_status(cost_table: &CostTable, gas_budget: Option<u64>) -> Result<GasStatus> {
@@ -739,6 +735,21 @@ impl TestRunConfig {
         self.vm_config.verifier_config.verify_nothing()
     }
 
+    /// Selects compatibility checks for a `//# publish` task from its flags and VM config.
+    pub fn publish_compatibility(&self, args: &AdapterPublishArgs) -> Compatibility {
+        if args.skip_check_struct_and_pub_function_linking || self.verifier_disabled() {
+            Compatibility::no_check()
+        } else {
+            Compatibility::new(
+                !args.skip_check_struct_layout,
+                !args.skip_check_friend_linking,
+                true,
+                false,
+                true,
+            )
+        }
+    }
+
     pub fn with_runtime_ref_checks(self) -> Self {
         Self {
             vm_config: self.vm_config.set_paranoid_ref_checks(true),
@@ -751,7 +762,7 @@ pub fn run_test(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     run_test_with_config(TestRunConfig::new(LanguageVersion::default(), vec![]), path)
 }
 
-fn precompiled_v2_stdlib() -> &'static PrecompiledFilesModules {
+pub fn precompiled_v2_stdlib() -> &'static PrecompiledFilesModules {
     &PRECOMPILED_MOVE_STDLIB_V2
 }
 
